acbca93e5a2aff3bcbcf63caa82456179c5caaf9
NgYaoDong <e0957740@u.nus.edu>
2024-01-14 22:25:40 +0800

    Level-0

---
+++ src/main/java/Faiz.java
@@ -0,0 +1,6 @@
+public class Faiz {
+    public static void main(String[] args) {
+        String HorLine = " _________________________________________\n";
+        System.out.println(HorLine + " Hello! I'm Faiz!\n What can I do for you? \n" + HorLine + " Bye. Hope to see you again soon!\n" + HorLine);
+    }
+}
