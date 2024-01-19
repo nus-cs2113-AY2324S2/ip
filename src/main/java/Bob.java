b11d55f94e5720df264dca4c09ea26d63cc21ba0
Ningsong Shen <ningsongshen123@outlook.com>
2024-01-19 17:06:51 +0800

    Change chatbot name from Duke to Bob

---
+++ src/main/java/Bob.java
@@ -0,0 +1,11 @@
+public class Bob {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                    + "|  _ \\_____ _|__ _\n"
+                    + "| |_| | | | | |   \\ \n"
+                    + "| |_| | |_| |   O  /\n"
+                    + "|____/ \\__,_|_\\__/\n";
+        System.out.println("Hello from\n" + logo);
+
+    }
+}
