b8543a9123c3e754be90a79ba0dcf7e069c3fafd
nyh3 <e0970473@u.nus.edu>
2024-01-19 22:27:14 +0800

    Change Duke to Quill

---
+++ src/main/java/Quill.java
@@ -0,0 +1,7 @@
+public class Quill {
+    public static void main(String[] args) {
+        String partition = "____________________________________________________________\n";
+        System.out.println(partition + "Hello! I'm Quill.\nWhat can i do for you?");
+        System.out.println(partition + "Bye! Hope to see you again soon!\n" + partition);
+    }
+}
