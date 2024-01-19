71952d3cd0793090fc6adef53f63300a40af1083
YHWong20 <yihao.wong@u.nus.edu>
2024-01-19 18:36:24 +0800

    Add Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,23 @@
+public class Ui {
+    private static final String welcome = " Hello! I'm Bob\n What can I do for you?";
+    private static final String exit = " Bye. Hope to see you again soon!";
+    private static final String separator = "____________________________________________________________";
+
+    void printOldWelcome() {
+        // Method to print old welcome message
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+
+    void generateUi() {
+        System.out.println(separator);
+        System.out.println(welcome);
+        System.out.println(separator);
+        System.out.println(exit);
+        System.out.println(separator);
+    }
+}
