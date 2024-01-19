9e4b324e81500045d461fcf54613388f409d488a
rismm <e0958178@u.nus.edu>
2024-01-19 21:00:12 +0800

    Rename Duke to Joe

---
+++ src/main/java/Joe.java
@@ -0,0 +1,7 @@
+public class Joe {
+    private static final String H_LINE = "____________________________________________________________\n";
+    public static void main(String[] args) {
+        System.out.println(H_LINE + "HI I'M JOE\n" + "WHAT CAN I DO FOR YOU\n" + H_LINE);
+        System.out.println(H_LINE + "GOODBYE. PLEASE COME BACK AGAIN\n" + H_LINE);
+    }
+}
