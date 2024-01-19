f9559d26f1a6f51b5cc886fa60aff3761679118c
soongensayo <e0959972@u.nus.edu>
2024-01-19 20:43:43 +0800

    Assign new chatbot name and implement initial skeleton version

---
+++ src/main/java/Sayo.java
@@ -0,0 +1,6 @@
+public class Sayo {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello! I'm Sayo \n" + "What can I do for you? \n" + "\n" + "Bye. Hope to see you again soon!");
+    }
+}
