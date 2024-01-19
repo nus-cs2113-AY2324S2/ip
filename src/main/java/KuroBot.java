9fa4223ba8e87c9672cd181f1bf31fd071601d18
LuoYu-uwu <E0968957@u.nus.edu>
2024-01-19 19:55:16 +0800

    Remove unnecessary comments

--- src/main/java/KuroBot.java
+++ src/main/java/KuroBot.java
@@ -1,18 +1,5 @@
 public class KuroBot {
     public static void main(String[] args) {
-        /*String logo =
-                  " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-                String logo =
-                  " ___  ___   ___    ___ \n"
-                + "|   |/  /   |  |   |  | \n"
-                + "|      /    |  |   |  | \n"
-                + "|   |\  \   |_ |___| _| \n"
-                + "|___| \__\    |_____|   \n";
-        System.out.println("Hello from\n" + logo);*/
         String logo =
                 " ___   ___    ___    ___ \n"
                         + "|   |/   /   |  |   |  | \n"
