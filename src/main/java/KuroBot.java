7ca41cbaec707c6ef6627f113ce3ecf14155ffb2
LuoYu-uwu <E0968957@u.nus.edu>
2024-01-19 19:54:04 +0800

    Add greating message

---
+++ src/main/java/KuroBot.java
@@ -0,0 +1,30 @@
+public class KuroBot {
+    public static void main(String[] args) {
+        /*String logo =
+                  " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+                String logo =
+                  " ___  ___   ___    ___ \n"
+                + "|   |/  /   |  |   |  | \n"
+                + "|      /    |  |   |  | \n"
+                + "|   |\  \   |_ |___| _| \n"
+                + "|___| \__\    |_____|   \n";
+        System.out.println("Hello from\n" + logo);*/
+        String logo =
+                " ___   ___    ___    ___ \n"
+                        + "|   |/   /   |  |   |  | \n"
+                        + "|       /    |  |   |  | \n"
+                        + "|   |\\   \\   |_ |___| _| \n"
+                        + "|___| \\___\\    |_____|   \n";
+        String line = "____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello! I'm KuroBot\n" + "What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+        System.out.println(logo);
+    }
+}
