0b875a3788c1f08f669788e11368ff6ab6921640
RyanTDL <teodeliangryan@gmail.com>
2024-01-20 19:27:43 +0800

    Completion of Increment (Level 0)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greet = 
+        "____________________________________________________________ \n"
+        + "Hello! I'm J@RV15\n"
+        + "What can I do for you? \n"
+        + "____________________________________________________________ \n"
+        + "Bye. Hope to see you again soon! \n"
+        + "____________________________________________________________ \n";
+
+        System.out.println(greet);
     }
 }
