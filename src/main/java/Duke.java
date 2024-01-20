984eaf7231c86570ea73f2b56e9c364f335ab380
djleong01 <dengjunleong2@gmail.com>
2024-01-20 23:11:29 +0800

    Changed into correct github account to commit.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
         String name = "Chad";
-        System.out.println("Hello I am " + name);
+        System.out.println("Hello I am " + name + ".");
         System.out.println("How can I help you today?\n");
         System.out.println("Goodbye. Hope to see you again!");
     }
