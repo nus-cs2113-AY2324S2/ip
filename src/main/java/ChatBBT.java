bfbf7d5e5b30a4460c7d696f4dcc9b1ff6a872a0
XiaoBo <xiaobo990219@gmail.com>
2024-01-21 20:30:15 +0800

    level-0 :rename + greeting added

---
+++ src/main/java/ChatBBT.java
@@ -0,0 +1,7 @@
+public class ChatBBT {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm ChatBBT");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
