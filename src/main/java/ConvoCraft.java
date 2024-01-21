121408fb47938ccd9d5f6e683408ad35782ced3b
STeng618 <shantengyap@gmail.com>
2024-01-21 22:34:43 +0800

    Create ConvoCraft.java for level 0

---
+++ src/main/java/ConvoCraft.java
@@ -0,0 +1,13 @@
+public class ConvoCraft {
+    public static void printHorizontal() {
+        System.out.println("-----------------------------------------------");
+    }
+    public static void main(String[] args) {
+        printHorizontal();
+        System.out.println("Hello! I'm ConvoCraft");
+        System.out.println("What can I do for you?");
+        printHorizontal();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHorizontal();
+    }
+}
