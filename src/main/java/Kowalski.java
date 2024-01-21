29a97dbbb1d6e356fb6c6c0ae98f1bef02b3b4f5
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-01-21 22:18:36 +0800

    Add Kowalski Class

---
+++ src/main/java/Kowalski.java
@@ -0,0 +1,6 @@
+public class Kowalski {
+    public static void main(String[] args){
+        System.out.println("Hello, I'm Kowalski! Reporting for duty! \n" +
+                "What can I do for you today, Skipper?" );
+    }
+}
