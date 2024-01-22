e85aa692772b3fe9ec78959f067c70114e11f89e
sxfoo <fsx159@gmail.com>
2024-01-22 13:37:09 +0800

    Implement chatbot basic greeting and exit functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,5 +7,14 @@ public class Duke {
                         " | |___| (_| | |  | | | (_) | |_ \n" +
                         "  \\_____\\__,_|_|  |_|  \\___/ \\__|\n";
         System.out.println("Hello from\n" + logo);
+
+        // Chatbot functionality
+        String horLines = "-------------------------------------";
+        System.out.println(horLines);
+        System.out.println("Hello! I'm Carrot!");
+        System.out.println("What can I do for you?");
+        System.out.println(horLines);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horLines);
     }
 }
