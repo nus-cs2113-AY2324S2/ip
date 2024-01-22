f75d3ec4d2b9f4f6d7c067106e5439f374182037
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2024-01-22 23:31:42 +0800

    Abstract greeting into its own function greet()

--- src/main/java/Hachi.java
+++ src/main/java/Hachi.java
@@ -1,10 +1,14 @@
 public class Hachi {
-    public static void main(String[] args) {
+    public static void greet(){
         String logo ="._. ._.  ._____.  ._____.  ._. ._.  ._.\n"
                 + "| | | |  | ._. |  |  ___|  | | | |  | |\n"
                 + "| |_| |  | |_| |  | |      | |_| |  | |\n"
                 + "| ._. |  | ._. |  | |___   |  _  |  | |\n"
                 + "|_| |_|  |_| |_|  |_____|  |_| |_|  |_|\n";
-        System.out.println("Hey, Hachi Here!\n" + logo);
+        System.out.println("Hey, Hachi Here!\n" + logo + "\n"
+                            + "How can I assist you today?\n");
+    }
+    public static void main(String[] args) {
+        greet();
     }
 }
