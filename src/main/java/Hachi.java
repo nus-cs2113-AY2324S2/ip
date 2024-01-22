002c9c31a8a20878cb13ca7ef340e947efbeec5b
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2024-01-22 23:24:05 +0800

    Renamed chatbot to Hachi; Updated all instances of Duke to Hachi. Changed logo to match new name.

---
+++ src/main/java/Hachi.java
@@ -0,0 +1,10 @@
+public class Hachi {
+    public static void main(String[] args) {
+        String logo ="._. ._.  ._____.  ._____.  ._. ._.  ._.\n"
+                + "| | | |  | ._. |  |  ___|  | | | |  | |\n"
+                + "| |_| |  | |_| |  | |      | |_| |  | |\n"
+                + "| ._. |  | ._. |  | |___   |  _  |  | |\n"
+                + "|_| |_|  |_| |_|  |_____|  |_| |_|  |_|\n";
+        System.out.println("Hey, Hachi Here!\n" + logo);
+    }
+}
