d6e8803c3bd1eeda209e5c17b84c2ce03d1eadf0
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2024-01-22 23:53:24 +0800

    Added simple comments

--- src/main/java/Hachi.java
+++ src/main/java/Hachi.java
@@ -1,31 +1,31 @@
 public class Hachi {
-    public static void greet(){
+    public static void greet() {
         String logo ="._. ._.  ._____.  ._____.  ._. ._.  ._.\n"
                 + "| | | |  | ._. |  |  ___|  | | | |  | |\n"
                 + "| |_| |  | |_| |  | |      | |_| |  | |\n"
                 + "| ._. |  | ._. |  | |___   |  _  |  | |\n"
                 + "|_| |_|  |_| |_|  |_____|  |_| |_|  |_|\n";
-        System.out.println("Hey, Hachi Here!\n" + logo
-                            + "How can I assist you today?\n");
+
+        System.out.println("Hey, Hachi Here!\n" + logo + "How can I assist you today?\n");
         spacerInsert("medium");
     }
-    public static void spacerInsert(String length){
+    public static void spacerInsert(String length) {
         String spacer;
         switch (length) {
-            case "small":
+            case "small": // 20 tildes
                 spacer = "~~~~~~~~~~~~~~~~~~~~";
                 break;
-            case "medium":
+            case "medium": // 40 tildes
             default:
                 spacer = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
                 break;
-            case "large":
+            case "large": // 60 tildes
                 spacer = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
                 break;
         }
         System.out.println(spacer);
     }
-    public static void goodbye(){
+    public static void goodbye() {
         System.out.println("Goodbye! Hope you have a marvelous day.");
         spacerInsert("medium");
     }
