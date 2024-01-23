1eb1856c15b66aec1ff47f2f40830d3dae745924
runxinghuan <rxinghuan@gmail.com>
2024-01-23 14:29:53 +0800

    Level-0

---
+++ src/main/java/Ava.java
@@ -0,0 +1,19 @@
+public class Ava {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        printLine();
+        System.out.println(" Hello! I'm Ava");
+        System.out.println(" What can I do for you?");
+        printLine();
+        System.out.println(" Bye. Hope to see you again soon!");
+        printLine();
+    }
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+}
