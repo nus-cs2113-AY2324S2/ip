12e08fc9eb311fdb56055f3c0e669a7e4e26f8cf
fxe025 <e0968867@u.nus.edu>
2024-01-23 15:06:49 +0800

    Add increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,11 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String horizon = "____________________________"
+                + "________________________________\n";
+        String botName = "Battch";
+        System.out.println(horizon + "Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(horizon + "Bye. Hope to see you again soon!");
+        System.out.print(horizon);
     }
 }
