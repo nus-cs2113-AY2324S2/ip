9231e0d122271e6d00a07c18f268d61ff3006725
JustinSoh <hoggersoh@gmail.com>
2024-01-23 13:54:27 +0800

    Initial structure of mimichat
    - Initial structure implemented

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String appName = "mimichat";
+        System.out.println("-------------------------------------------");
+        System.out.println("Hello! I'm " + appName);
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
