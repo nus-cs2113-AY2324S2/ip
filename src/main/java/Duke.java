416e6c2f7355ee5ef5a527a93018e99fa9205c6e
Lin Htet <earthangle007@gmail.com>
2024-01-23 13:57:33 +0800

    Implement an initial skeletal version

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatBot = "Hello! I'm TONY\n"
+                + "What can I do for you?\n"
+                + "_________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                + "_________________________";
+        System.out.println(chatBot);
     }
 }
