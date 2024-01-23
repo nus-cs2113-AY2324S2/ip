ddf873ccdf9ed759fa39741f38a2e04ff3161616
tayponghee <tayponghee@gmail.com>
2024-01-23 19:52:15 +0800

    change: indentation of code

--- src/main/java/DavinciBot.java
+++ src/main/java/DavinciBot.java
@@ -2,8 +2,8 @@ public class DavinciBot {
         public static void main(String[] args) {
             System.out.println(
                     "Hello! I'm DavinciBot! \n" +
-                            "I would love to help you but it seems that I have no functionalities yet. \n" +
-                            "Bye. Hope to see you again soon! \n"
+                    "I would love to help you but it seems that I have no functionalities yet. \n" +
+                    "Bye. Hope to see you again soon! \n"
             );
     }
 }
