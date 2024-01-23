d04d9d37229c47c2208a2426f122c427309b396b
liuy1103 <e0968020@u.nus.edu>
2024-01-23 12:55:14 +0800

    Increment Level 0 Rename, Greet, Exit

---
+++ src/main/java/Hubert.java
@@ -0,0 +1,17 @@
+public class Hubert {
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        
+        String horizontal = "____________________________________________________________";
+        String botName = "Hubert";
+        System.out.println("Hello! I'm " + botName);
+        System.out.println("What can I do for you?");
+        System.out.println(horizontal);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(horizontal);
+    }
+}
