eb70cbbb80d17835dafabfa7577aef4900f89fd4
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-01-23 13:48:28 +0800

    Add Increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String greeting = " ____________________________________________________________\n" +
+                " Hello! I'm Cody\n" +
+                " What can I do for you?\n" +
+                "_____________________________________________________________\n";
+        String goodbye = "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "_____________________________________________________________";
+
+        System.out.println(greeting);
+        System.out.println(goodbye);
     }
 }
