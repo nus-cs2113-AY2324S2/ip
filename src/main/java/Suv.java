305b0324e6482f9f6dcbe2a5a0f1c32ec9edf470
SuveenE <suveen.te1@gmail.com>
2024-01-24 14:28:45 +0800

    Add Greet and Exit message

--- src/main/java/Suv.java
+++ src/main/java/Suv.java
@@ -1,10 +1,11 @@
 public class Suv {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Suv";
+        System.out.println("____________________________________________________________\n" +
+                " Hello! I'm " + name + "\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n");
     }
 }
