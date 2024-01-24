cbe0abddcd969d00e7b029125fcd5d88f99d6300
owx0130 <weixiang0130@gmail.com>
2024-01-24 15:22:33 +0800

    rename bot, add greeting and exit message

---
+++ src/main/java/GeePee.java
@@ -0,0 +1,10 @@
+public class GeePee {
+    public static void main(String[] args) {
+        System.out.println("________________________________________________");
+        System.out.println("Hello! I'm GeePee, your friendly chatbot assistant!");
+        System.out.println("What can I do for you?");
+        System.out.println("________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("________________________________________________");
+    }
+}
