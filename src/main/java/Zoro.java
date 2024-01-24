d5e9b3cf68bd88cb23be8ac6bda628b143ee94e4
Vavinan <vavithamarai2000@gmail.com>
2024-01-24 15:59:25 +0800

    Level 0: Add Greet and Exit message

--- src/main/java/Zoro.java
+++ src/main/java/Zoro.java
@@ -1,10 +1,11 @@
 public class Zoro {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String name = "Zoro";
+        String line = "_____________________________________________________________";
+        System.out.println(line);
+        System.out.println("Hello I'm "+ name + "\nWhat can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
     }
 }
