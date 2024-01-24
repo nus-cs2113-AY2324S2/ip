43aa29f9c40d29b757b22c22ab45024de196c28e
blackmirag3 <shuihon@gmail.com>
2024-01-24 17:06:25 +0800

    Update class name to NyanBot Add hello and bye message

---
+++ src/main/java/NyanBot.java
@@ -0,0 +1,15 @@
+public class NyanBot {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String greet = "お帰りなさいませ、ご主人様。ニャンー♡♡";
+        String prompt = "なにをしたいの？";
+        String bye = "じゃー、またね〜！！";
+
+        System.out.println(line);
+        System.out.println(greet);
+        System.out.println(prompt);
+        System.out.println(line);
+        System.out.println(bye);
+        System.out.println(line);
+    }
+}
