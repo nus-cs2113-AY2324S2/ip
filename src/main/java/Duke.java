f569b164847edb6ab4b7721baa07df8f0f6226b6
ZhangWenyue3325 <zhangwenyuekyrene@gmail.com>
2024-01-25 03:34:14 +0800

    Initialise following week 2 instructions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "  _____  __       \n"
+                    + " /  _  \\|  | ____ ___  ___ \n"
+                    + "|  | |  |  |/ ___ \\  \\/  /\n"
+                    + "|  |_|  |  |  ____/\\    /\n"
+                    + " \\_____/|__|\\_____|/   /\n"
+                    + "                  /___/";
         System.out.println("Hello from\n" + logo);
+        System.out.println(" ");
+        System.out.println("————————————————————————————");
+        System.out.println("Hello, I'm Oley.");
+        System.out.println("What can I do for you?");
+        System.out.println(" ");
+        System.out.println("————————————————————————————");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(" ");
+        System.out.println("————————————————————————————");
     }
 }
