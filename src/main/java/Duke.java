c3acad7a88f242c641e2b4ea3b24d808c30a8883
OLEY的REDMIBOOK\22137 <e0968938@u.nus.edu>
2024-01-25 02:16:13 +0800

    week2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        final String LOGO = " _   _  _    _  ____   ____  _   _  ____\n"
+                + "| | / /\\ \\  / /|  _ \\ | ___|| \\ | || ___|\n"
+                + "| |/ /  \\ \\/ / | |_| || ===||  \\| || ===|\n"
+                + "| |\\ \\   |  |  | |\\ / | |__ | |\\  || |__\n"
+                + "|_| \\_\\  |__|  |_| \\_\\|____||_| \\_||____|   by Zhou Junmin\n";
+        final String DIVIDER = "____________________________________________________________\n";
+        final String GREETING = "Hi, I am Kyrene, your private reminder assistant.\n"
+                + "What can I do for you?\n";
+        final String BYE = "Bye! Wish to see you again soon!\n";
+
+        System.out.println(DIVIDER);
+        System.out.println(LOGO);
+        System.out.println(DIVIDER);
+        System.out.println(GREETING);
+        System.out.println(DIVIDER);
+        System.out.println(BYE);
+        System.out.println(DIVIDER);
     }
 }
