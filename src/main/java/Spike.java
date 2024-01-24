220253c9c17287c6f243cf2363ec0f2e7ad8ac49
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-01-24 15:14:01 +0800

    level-0: Add greet and exit message

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,10 +1,8 @@
 public class Spike {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String chatbot = "Spike";
+        System.out.println("Hello! I'm " + chatbot);
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
