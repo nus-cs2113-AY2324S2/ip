9efcb2c4d5067a608e20d3b5f0aaee08fc0e3f7b
Mmaxx15 <leongxwmax@gmail.com>
2024-01-24 13:31:26 +0800

    spacing

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -1,13 +1,13 @@
 public class Dobby {
     public static void main(String[] args) {
            String logo = " _____        _     _           \n"
-                      + "|  __ \\      | |   | |\n"
-                      + "| |  | | ___ | |__ | |__  _   _ \n"
-                      + "| |  | |/ _ \\| '_ \\| '_ \\| | | |\n"
-                      + "| |__| | |_| | |_) | |_) | |_| |\n"
-                      + "|_____/ \\___/|_.__/|_.__/ \\__, |\n"
-                      + "                           __/ |\n"
-                      + "                          |___/ \n";
+                       + "|  __ \\      | |   | |\n"
+                       + "| |  | | ___ | |__ | |__  _   _ \n"
+                       + "| |  | |/ _ \\| '_ \\| '_ \\| | | |\n"
+                       + "| |__| | |_| | |_) | |_) | |_| |\n"
+                       + "|_____/ \\___/|_.__/|_.__/ \\__, |\n"
+                       + "                           __/ |\n"
+                       + "                          |___/ \n";
         System.out.println(logo);
         System.out.println("Hello! I'm Dobby");
         System.out.println("How can I help you?\n");
