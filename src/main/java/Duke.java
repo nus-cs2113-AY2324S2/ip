cf0f0946687c3626ae05bc002c83e886e4474574
B1G-SAM <54065426+B1G-SAM@users.noreply.github.com>
2024-01-24 15:50:58 +0800

    Add increment level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,12 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo   = "   JJJ   EEEEE  FFFFFF   FFFFFF\n"
+                + "    J     E      F       F     \n"
+                + "    J     EEEE   FFFF    FFFF  \n"
+                + "J   J     E      F       F     \n"
+                + " JJJ      EEEEE  F       F     \n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
