0e04baf273bec8e2feec64d3c98e1a2c13b90e95
ChinYanXu <yanxu6267@gmail.com>
2024-01-24 16:09:30 +0800

    Update Duke.java and rename Duke.java to Serf.java

---
+++ src/main/java/Serf.java
@@ -0,0 +1,12 @@
+public class Serf {
+    public static void main(String[] args) {
+        String chatBotName = "Serf";
+        String message = "____________________________________________________________\n"
+                + " Hello! I'm " + chatBotName + "\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(message);
+    }
+}
