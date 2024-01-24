5f3fc9fd22f609f2f97a96822bc1f816412562c8
Mmaxx15 <leongxwmax@gmail.com>
2024-01-24 13:21:56 +0800

    Rename Duke to Dobby

---
+++ src/main/java/Dobby.java
@@ -0,0 +1,20 @@
+public class Dobby {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+          String logo = " _____        _     _           \n"
+                      +  "|  __ \\      | |   | |\n"
+                      + "| |  | | ___ | |__ | |__  _   _ \n"
+                      + "| |  | |/ _ \\| '_ \\| '_ \\| | | |\n"
+                      + "| |__| | |_| | |_) | |_) | |_| |\n"
+                      + "|_____/ \\___/|_.__/|_.__/ \\__, |\n"
+                      + "                           __/ |\n"
+                      + "                          |___/ \n";
+        System.out.println(logo);
+        System.out.println("Hello! I'm Dobby");
+        System.out.println("What can I do for you?\n");
+    }
+}
