8be7a1ca17b184794cb1a3c92130bd79d4f782bd
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-01-24 15:08:28 +0800

    Rename chatbot to Spike

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class Spike {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
