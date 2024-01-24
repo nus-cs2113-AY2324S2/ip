c57d58b88506a83cb67137c95559147affa019c2
Daviancold <110613918+Daviancold@users.noreply.github.com>
2024-01-25 02:02:41 +0800

    Add Davvy.java
    Created a skeletal new chatbot Davvy that greets user and exit

---
+++ src/main/java/Davvy.java
@@ -0,0 +1,12 @@
+public class Davvy {
+
+    public static void main(String[] args) {
+        String greetings = "____________________________________________________________\n" +
+                " Hello! I'm [YOUR CHATBOT NAME]\n" +
+                " What can I do for you?\n" +
+                "____________________________________________________________\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "____________________________________________________________\n";
+        System.out.println(greetings);
+    }
+}
