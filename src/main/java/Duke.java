b57650cd19956b3f93f6ba03b678a5a60f3ab3e6
Mahesh1772 <87927591+Mahesh1772@users.noreply.github.com>
2024-01-25 21:22:03 +0800

    Level 0: change name with skeletal code

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,14 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        //System.out.println("Hello from\n" + logo);
+
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Hello! I'm Tate!");
+        System.out.println("What can I do for you?\n");
+        System.out.println("------------------------------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------------------------");
+
     }
 }
