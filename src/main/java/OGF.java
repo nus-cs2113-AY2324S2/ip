9a9bccc39b387423e9c2eda1f827be5fb9b249c2
samuelory <samuelong7@gmail.com>
2024-01-25 17:25:51 +0800

    increment level-0

---
+++ src/main/java/OGF.java
@@ -0,0 +1,14 @@
+public class OGF {
+    public static void main(String[] args) {
+        String logo = "\n" +
+                " _____  _    _                   _____ ______ \n" +
+                "|  _  || |  | |                 |  __ \\|  ___|\n" +
+                "| | | || |_ | |__    ___  _ __  | |  \\/| |_   \n" +
+                "| | | || __|| '_ \\  / _ \\| '__| | | __ |  _|  \n" +
+                "\\ \\_/ /| |_ | | | ||  __/| |    | |_\\ \\| |    \n" +
+                " \\___/  \\__||_| |_| \\___||_|     \\____/\\_|    \n" +
+                "                                              \n" +
+                "                                              \n";
+        System.out.println("Welcome! I'm your \n" + logo + "Nice to meet you!");
+    }
+}
