bbf4e1479ec6d401864e4314aa34a74e15805e50
Claribel Ho <hoclaribel@gmail.com>
2024-01-26 01:51:04 +0800

    Remove unused Duke logo from Soot.java

--- src/main/java/Soot.java
+++ src/main/java/Soot.java
@@ -1,13 +1,6 @@
 public class Soot {
     public static void main(String[] args) {
         drawLine();
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println(logo);
-
         System.out.println("Hello! I'm Soot, your personal chatbot companion :)");
         System.out.println("What can I help you with?");
         drawLine();
