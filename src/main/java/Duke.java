b3adc6ee7506118d8c4fdd51721b07aa4f4e8f92
JiahaoGan <u3585924@connect.hku.hk>
2024-01-25 23:11:39 +0800

    Rename as Ruby, add greet and exit function

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+//        System.out.println("Hello from\n" + logo);
+        greet();
+        exit();
+    }
+
+    private static void greet(){
+        System.out.println("Hi, I am here. Greets from Ruby.");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void exit(){
+        System.out.println("Bye. Always feels good work with you.");
     }
 }
