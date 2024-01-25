8544ebb611c3a6aced5059b736106f898d55dabb
LowTL <e0957642@u.nus.edu>
2024-01-25 15:46:41 +0800

    Replace Duke.java with Katleen.java

---
+++ src/main/java/Katleen.java
@@ -0,0 +1,14 @@
+public class Katleen {
+    public static final String line = "____________________________________________________________";
+
+    public static void main(String[] args) {
+
+        System.out.println(line);
+        System.out.println("Hello! I'm Katleen.");
+        System.out.println("What can I do for you today?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+        return;
+    }
+}
