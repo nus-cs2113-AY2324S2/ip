440bad955eadc53e45a4f160ccdc66bf6c7b8902
yeozongyao <yeozongyao@gmail.com>
2024-01-25 23:02:36 +0800

    Change 'Duke' Placeholder to 'Yeos' Introduce Greetings

---
+++ src/main/java/Yeos.java
@@ -0,0 +1,8 @@
+public class Yeos {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Yeos!\n"
+                + "What can I do for your?\n\n"
+                + "Bye. Hope to see you again soon!\n";
+        System.out.println(greeting);
+    }
+}
