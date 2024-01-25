64320255bcf8f8546f440ddfc1a42811fbe6034b
Incy <85068563+IncyBot@users.noreply.github.com>
2024-01-25 20:52:47 +0800

    Updated Name and added initial skeleton

---
+++ src/main/java/Incy.java
@@ -0,0 +1,17 @@
+public class Incy {
+    public static void main(String[] args) {
+        String logo = " ____  _  _  ___  _  _ \n"
+                + "(_  _)( \\( )/ __)( \\/ )\n"
+                + " _)(_  )  (( (__  \\  / \n"
+                + "(____)(_)\\_)\\___) (__) \n";
+        
+        System.out.println("____________________________________________________________\n"
+                            + "Hello! I'm\n" + logo
+                            + "What can I do for you?\n"
+                            + "____________________________________________________________"
+        );
+        
+        System.out.println("Bye. Hope to see you again soon!"
+                            + "\n____________________________________________________________");
+    }
+}
