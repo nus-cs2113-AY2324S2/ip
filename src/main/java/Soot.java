fc7930fa6181c162fbe7e105e9ff284cec8ca04d
Claribel Ho <hoclaribel@gmail.com>
2024-01-26 01:50:04 +0800

    Add drawLine class that prints a horizontal line

--- src/main/java/Soot.java
+++ src/main/java/Soot.java
@@ -1,5 +1,6 @@
 public class Soot {
     public static void main(String[] args) {
+        drawLine();
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -9,7 +10,17 @@ public class Soot {
 
         System.out.println("Hello! I'm Soot, your personal chatbot companion :)");
         System.out.println("What can I help you with?");
+        drawLine();
 
         System.out.println("Bye! Till the next time we meet...");
+        drawLine();
+    }
+
+    public static void drawLine() {
+        int length = 60;
+        for (int i = 0; i < length; i++) {
+            System.out.print("_");
+        }
+        System.out.println("");
     }
 }
