a32f4e8c501a2ef5d5254ce648460a5c8b8a3f6d
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2024-01-26 03:19:27 +0800

    feat: Add greetings

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,20 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        // ASCII Art Generated from: https://patorjk.com/software/taag/#p=display&f=Big&t=anonBot
+        String logo = "                          ____        _   \n"
+                + "                         |  _ \\      | |  \n"
+                + "   __ _ _ __   ___  _ __ | |_) | ___ | |_ \n"
+                + "  / _` | '_ \\ / _ \\| '_ \\|  _ < / _ \\| __|\n"
+                + " | (_| | | | | (_) | | | | |_) | (_) | |_ \n"
+                + "  \\__,_|_| |_|\\___/|_| |_|____/ \\___/ \\__|\n";
+        String sectionBar = "____________________________________________________________";
+
+        System.out.println(logo);
+        System.out.println(sectionBar);
+        System.out.println("Hello! I'm anonBot");
+        System.out.println("What can I do for you?");
+        System.out.println(sectionBar);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(sectionBar);
     }
 }
