0ff2890cf9673fa58a351ff2bd5450cecf06836f
nicknamenic <ranjnicoli1@gmail.com>
2024-01-25 22:42:35 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello! I'm Nocturne!");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
