a59b4d0249d3f97b2eba8393c0fa50881b780a1a
Ahmed <shaiyan.alam16@gmail.com>
2024-01-25 23:20:11 +0800

    File updated

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = 
+              "   ____ ____  \n"
+            + " / ___/ ___| \n"
+            + "| |   \\___ \\ \n"
+            + "| |___ ___) |\n"
+            + " \\____|____/ \n";
+        System.out.println("Hello from Shaiyan\r\n" +
+                " Hello! I'm CS\r\n" + 
+                " What can I do for you?\r\n" +
+                "____________________________________________________________\r\n" +
+                " Bye. Hope to see you again soon!\r\n" +
+                "____________________________________________________________\r\n" +
+                "\n" + logo);
     }
 }
