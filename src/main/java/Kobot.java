ca44bed4618fd39c18d11bb81a058fd601128068
Kobot7 <notkobot@gmail.com>
2024-01-26 00:39:03 +0800

    Rename Duke to Kobot. Greet user and exit.

---
+++ src/main/java/Kobot.java
@@ -0,0 +1,16 @@
+public class Kobot {
+    public static void main(String[] args) {
+        String logo = "#########################################\n"
+                + "##     _   __      _           _       ##\n"
+                + "##    | | / /     | |         | |      ##\n"
+                + "##    | |/ /  ___ | |__   ___ | |_     ##\n"
+                + "##    |    \\ / _ \\| '_ \\ / _ \\| __|    ##\n"
+                + "##    | |\\  \\ (_) | |_) | (_) | |_     ##\n"
+                + "##    \\_| \\_/\\___/|_.__/ \\___/ \\__|    ##\n"
+                + "##                                     ##\n"
+                + "#########################################\n";
+        System.out.println(logo);
+        System.out.println("Hello! I'm Kobot. How may I assist you?");
+        System.out.println("Bye, hope to see you again!");
+    }
+}
