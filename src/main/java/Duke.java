812fd5eaf36740900876f83b02f72d7138e1f0a6
joshualeejunyi <joshua.lee.99@hotmail.com>
2024-01-25 16:38:42 +0800

    Level-0: Renamed to artemis with greeting and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "  __ _ _ __| |_ ___ _ __ ___ (_)___ \n" +
+                " / _` | '__| __/ _ \\ '_ ` _ \\| / __|\n" +
+                "| (_| | |  | ||  __/ | | | | | \\__ \\\n" +
+                " \\__,_|_|   \\__\\___|_| |_| |_|_|___/\n" +
+                "                                    ";
+        System.out.println("======================================\n");
+        System.out.println(logo);
+        System.out.println("Hello! I'm Artemis.\n");
+        System.out.println("What can I do for you?\n");
+
+        System.out.println("======================================\n");
+        System.out.println("Bye. Hope to see you again soon!\n");
+        System.out.println("======================================\n");
     }
 }
