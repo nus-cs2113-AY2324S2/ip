53b4485f63ea89ceff3dffc1e4f6f3b568a7bdad
Claribel Ho <hoclaribel@gmail.com>
2024-01-26 01:28:26 +0800

    Add user greeting to Soot.java

--- src/main/java/Soot.java
+++ src/main/java/Soot.java
@@ -5,6 +5,9 @@ public class Soot {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(logo);
+
+        System.out.println("Hello! I'm Soot");
+        System.out.println("What can I do for you?");
     }
 }
