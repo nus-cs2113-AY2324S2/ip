c3bd598d42aee7a5f05ce391fd28455ecbb746f4
leongxingyu <leongxingyu02@gmail.com>
2024-01-26 16:14:07 +0800

    Add Task class for each class

--- src/main/java/Gab.java
+++ src/main/java/Gab.java
@@ -2,47 +2,78 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Gab {
-    private static ArrayList<String> taskList = new ArrayList<>();
 
-    public static String getTask() {
-        String task;
+    public static class Task { //each task represent one instance of this class
+        private String description;
+        private boolean isDone;
+
+        public Task (String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void markAsDone() {
+            isDone = true;
+        }
+
+        public String getStatusIcon() { //if it is done mark as [X]
+            return (isDone ? "[X]" : "[ ]");
+        }
+    }
+
+    private static ArrayList<Task> taskList = new ArrayList<>(); //array of Task instances
+
+    public static Task getTask() {
+        String taskDescription;
         Scanner in = new Scanner(System.in);
         System.out.println("\tEnter your new task: ");
-        task = in.nextLine();
-        taskList.add(task);
-        return task;
+        taskDescription = in.nextLine();
+        checkKeywords(taskDescription);
+        Task newTask = new Task(taskDescription);
+        taskList.add(newTask); //adding the instances of the new task to the array
+        getTask();
+        return newTask; //create new instance of the task class
     }
 
-    public static void listTask() {
-        for (int i = 0; i < taskList.size(); i++) {
-            String displayList = taskList.get(i);
-            System.out.println((i + 1) + ". " + displayList);
-        }
-        displayTask(getTask());
-    }
-
-    public static void checkKeywords (String keywords) {
+    public static void checkKeywords (String task) {
         final String exitCode = "bye";
         final String displayCode = "list";
-        keywords = keywords.toLowerCase();
-        switch (keywords) {
+        switch (task) {
             case exitCode:
                 System.out.println("I hope you complete them!");
+                System.exit(0);
                 break;
             case displayCode:
-                Gab.listTask();
+                listTask();
                 break;
             default:
-                Gab.displayTask(getTask());
+                displayTask(task);
                 break;
         }
     }
+
+
+
+    public static void listTask() { //using the array to list the tasks
+        for (int i = 0; i < taskList.size(); i++) {
+            Task task = taskList.get(i); //instance of task in the display list
+            String taskName = task.getDescription();
+            String status = task.getStatusIcon(); //getting the status from the instance
+            System.out.println((i + 1) + ". " + status + taskName);
+        }
+        getTask();
+    }
+
     public static void displayTask(String task) {
-        String exitCode = "Bye";
+        //String exitCode = "Bye";
         System.out.println("_____________");
         System.out.println("\tadded: " + task);
         System.out.println("_____________");
-        checkKeywords(task);
+        //checkKeywords();
     }
 
 
@@ -59,15 +90,11 @@ public class Gab {
         System.out.println("\tI am Gab the Bot! Nice to meet you!");
         System.out.println("\tAnything I can help you with?");
 
-        String task = Gab.getTask();
-        Gab.displayTask(task);
+
+        Task task = Gab.getTask();
+        //Gab.checkKeywords(task);
 
     }
-
-
-
-
-
 
 }
 
