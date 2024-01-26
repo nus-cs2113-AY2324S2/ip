3131c873d539cee2aeba3c31f7486306a8b88236
pqienso <buckslimyuan@gmail.com>
2024-01-26 15:36:33 +0800

    Rename to bean; add skeletal code

---
+++ src/main/java/Bean.java
@@ -0,0 +1,9 @@
+public class Bean {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Bean.\n" +
+                "What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
+
