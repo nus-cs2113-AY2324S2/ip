cfe27b718ed9ad72b5354a6ef24b8c9530c40ff0
yuhengr <e0957964@u.nus.edu>
2024-01-26 14:13:35 +0800

    Add chatbot skeleton

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,7 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("I'm Ekud! What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon.");
     }
 }
