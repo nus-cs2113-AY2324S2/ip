65c0b7543093e4f28fbba2881eaa19bfe66f8f9c
ZhuSijia0711 <e0968116@u.nus.edu>
2024-01-26 14:20:31 +0800

    level 1-0

---
+++ src/main/java/Evelyn.java
@@ -0,0 +1,11 @@
+public class Evelyn {
+    public static void main(String[] args) {
+        System.out.print("____________________________________________________________\n");
+        System.out.println("Hello! I'm Evelyn");
+        System.out.println("What can I do for you?");
+        System.out.print("____________________________________________________________\n");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.print("____________________________________________________________");
+    }
+
+}
