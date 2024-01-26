fd053fcc87a432784e1ff934056583f35f944bbc
Chew Jing Xiang <chewjingxiangjumps@gmail.com>
2024-01-26 21:31:54 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String name = "Jonas";
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Hello! I'm " + name + ".\n");
-        System.out.println("What can I do for you?\n");
-        System.out.println("____________________________________________________________\n");
-        System.out.println("Bye. Hope to see you again soon!\n");
-        System.out.println("____________________________________________________________\n");
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + name + ".");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String userInput = scanner.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println("____________________________________________________________");
+                System.out.println(userInput);
+                System.out.println("____________________________________________________________");
+            }
+        }
+
+        // Close the scanner to prevent resource leaks
+        scanner.close();
     }
 }
