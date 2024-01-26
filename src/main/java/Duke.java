ccd3b932218d9a18e5bf08895656e934ce075088
nur-haziq <nhaziq01@gmail.com>
2024-01-26 16:52:27 +0800

    Add greet and exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,5 +6,25 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        for (int i = 0; i < 59; i++) {
+            System.out.print("_");
+        }
+        System.out.println("_");
+
+        System.out.println("Hello! I'm Nanalan");
+        System.out.println("What can I do for you?");
+
+        for (int i = 0; i < 59; i++) {
+            System.out.print("_");
+        }
+        System.out.println("_");
+
+        System.out.println("Bye. Hope to see you again soon!");
+
+        for (int i = 0; i < 59; i++) {
+            System.out.print("_");
+        }
+        System.out.println("_");
     }
 }
