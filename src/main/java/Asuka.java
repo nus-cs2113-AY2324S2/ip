7bf219233887cfc3c633b739141d41ec10e598af
Luo Zhiyang <e0969568@u.nus.edu>
2024-01-26 14:25:00 +0800

    Level-0

---
+++ src/main/java/Asuka.java
@@ -0,0 +1,8 @@
+public class Asuka {
+    public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Asuka\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
