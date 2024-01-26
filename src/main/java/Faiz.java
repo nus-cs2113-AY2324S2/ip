b720e4481bf724ee07b8dadd3b9fd382f40366a3
NgYaoDong <e0957740@u.nus.edu>
2024-01-27 01:08:28 +0800

    Add Level-3: Mark as Done + A-CodingStandard

--- src/main/java/Faiz.java
+++ src/main/java/Faiz.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 public class Faiz {
     public static final String HorLine = "\t_________________________________________\n";
+    static Task[] tasks = new Task[100];
 
     public static void printHi() {
         System.out.println(HorLine + "\t Hello! I'm Faiz!\n\t What can I do for you?\n" + HorLine);
@@ -15,17 +16,26 @@ public class Faiz {
         System.out.println(HorLine + "\t added: " + line + System.lineSeparator() + HorLine);
     }
 
-    private static void listTasks(String[] tasks, int ind) {
-        System.out.print(HorLine);
+    public static void listTasks(Task[] tasks, int ind) {
+        System.out.print(Faiz.HorLine + "\t Here are the tasks in your list:\n");
         for (int i = 0; i < ind; i++) {
-            System.out.println("\t " + (i + 1) + ". " + tasks[i]);
+            System.out.println("\t " + (i + 1) + "." + tasks[i].toString());
         }
-        System.out.println(HorLine);
+        System.out.println(Faiz.HorLine);
+    }
+
+    public static void markAsDone(int index) {
+        tasks[index].markAsDone();
+        System.out.print(Faiz.HorLine + "\t Nice! I've marked this task as done:\n\t " + tasks[index].toString() + System.lineSeparator() + HorLine);
+    }
+
+    public static void markAsUndone(int index) {
+        tasks[index].markAsUndone();
+        System.out.print(Faiz.HorLine + "\t OK, I've marked this task as not done yet:\n\t " + tasks[index].toString() + System.lineSeparator() + HorLine);
     }
 
     public static void main(String[] args) {
         String line;
-        String[] tasks = new String[100];
         int ind = 0;
         Scanner in = new Scanner(System.in);
         printHi();
@@ -35,9 +45,17 @@ public class Faiz {
                 break;
             } else if (line.equals("list")) {
                 listTasks(tasks, ind);
+            } else if (line.contains("mark")) {
+                String[] words = line.split(" ");
+                if (words[0].equals("mark")) {
+                    markAsDone(Integer.parseInt(words[1]) - 1);
+                } else if (words[0].equals("unmark")) {
+                    markAsUndone(Integer.parseInt(words[1]) - 1);
+                }
             } else {
                 echo(line);
-                tasks[ind] = line;
+                Task t = new Task(line);
+                tasks[ind] = t;
                 ind++;
             }
         }
