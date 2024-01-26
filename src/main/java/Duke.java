c579eafcf83a6fd4201768fd594e844472b3e090
Chew Jing Xiang <chewjingxiangjumps@gmail.com>
2024-01-26 22:11:36 +0800

    Update Duke.java

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,15 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 public class Duke {
-    private static List<String> commands = new ArrayList<>();
+    private static Task[] tasks = new Task[100];
+    private static int taskCount = 0;
 
     public static void main(String[] args) {
         String name = "Jonas";
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + name + ".");
+        System.out.println("안녕하세요, Hello! I'm " + name + ".");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
@@ -19,29 +18,74 @@ public class Duke {
 
             if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("Kamxia. Hope to see you again soon!");
                 System.out.println("____________________________________________________________");
                 break;
             } else if (userInput.equalsIgnoreCase("list")) {
-                displayHistory();
+                displayTasks();
+            } else if (userInput.startsWith("mark ")) {
+                markTask(userInput);
+            } else if (userInput.startsWith("unmark ")) {
+                unmarkTask(userInput);
             } else {
-                commands.add(userInput);
-                System.out.println("____________________________________________________________");
-                System.out.println("Added: " + userInput);
-                System.out.println("____________________________________________________________");
+                addTask(userInput);
             }
         }
 
-        // Close the scanner to prevent resource leaks
         scanner.close();
     }
 
-    private static void displayHistory() {
+    private static void displayTasks() {
         System.out.println("____________________________________________________________");
-        System.out.println("Command History:");
-        for (String command : commands) {
-            System.out.println(command);
+        System.out.println("LaiLaiLai, Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + "." + tasks[i]);
         }
         System.out.println("____________________________________________________________");
+    }
+
+    private static void addTask(String description) {
+        if (description.isEmpty()) {
+            System.out.println("Anyhow one...write your task properly leh :(");
+            return;
+        }
+        tasks[taskCount++] = new Task(description);
+        System.out.println("____________________________________________________________");
+        System.out.println("Added: " + description);
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void markTask(String userInput) {
+        try {
+            int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice one lah! I've marked this task as done:");
+                System.out.println("  " + tasks[taskIndex]);
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("Invalid task has been chosen :(((((");
+            }
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            System.out.println("Invalid request :((((((");
+        }
+    }
+
+    private static void unmarkTask(String userInput) {
+        try {
+            int taskIndex = Integer.parseInt(userInput.split(" ")[1]) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markAsNotDone();
+                System.out.println("____________________________________________________________");
+                System.out.println("OK can, I've marked this task as not done yet:");
+                System.out.println("  " + tasks[taskIndex]);
+                System.out.println("____________________________________________________________");
+            } else {
+                System.out.println("Invalid task has been chosen :(((((");
+            }
+        } catch (NumberFormatException | IndexOutOfBoundsException e) {
+            System.out.println("Invalid request :((((((");
+        }
     }
 }
