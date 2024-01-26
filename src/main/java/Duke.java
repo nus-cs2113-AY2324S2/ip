f3626adb2a91df5159cdf860788b03c5a6b40d52
jensonjenkins <dave.martheen@gmail.com>
2024-01-26 17:46:55 +0800

    Add Increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "   ___ __ _ ___ _ __   ___ _ __\n"
+                + "  / __/ _` / __| '_ \\ / _ \\ '__|\n"
+                + " | (_| (_| \\__ \\ |_) |  __/ |   \n"
+                + "  \\___\\__,_|___/ .__/ \\___|_|   \n"
+                + "               | |              \n"
+                +"               |_|               ";
+        String separator = "_____________________________________________";
+//        System.out.println("Starting\n" + logo);
+        System.out.println(separator);
+        System.out.println("Hello! I'm Casper\nWhat can I do for you?");
+        System.out.println(separator);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(separator);
     }
 }
