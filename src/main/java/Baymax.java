6e81a8bc21b961a410effaee21bcaaa32838b6eb
isaaceng7 <>
2024-01-26 11:26:02 +0800

    Baymax: Level-0: Rename, Greet, Exit

---
+++ src/main/java/Baymax.java
@@ -0,0 +1,6 @@
+public class Baymax {
+    public static void main(String[] args) {
+        String name = "Baymax";
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n\n" + "Bye. Hope to see  you again soon!");
+    }
+}
