311f197e63eb5544b54bbca5013bf011f0dfee3f
nigelheng <e0970002@u.nus.edu>
2024-01-26 10:03:22 +0800

    Add increment Level-0 Renamed File and Class Prints Greeting and Farewell

---
+++ src/main/java/Eln.java
@@ -0,0 +1,16 @@
+public class Eln {
+    public static void main(String[] args) {
+        /*String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+                */
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Eln");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+}
