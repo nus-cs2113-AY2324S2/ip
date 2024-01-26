1ab0f32330cf0137f62197a863825e4706ae11d6
JingHaoooo <jinghaoooo17@gmail.com>
2024-01-26 16:43:32 +0800

    Add greet and exit message

---
+++ src/main/java/JingHao.java
@@ -0,0 +1,12 @@
+public class JingHao {
+    public static void main(String[] args) {
+        String line = "____________________________________________________________";
+        String name = "JingHao";
+        System.out.println(line);
+        System.out.println("Hello! I'm " + name);
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(line);
+    }
+}
