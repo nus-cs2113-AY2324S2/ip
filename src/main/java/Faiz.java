268510fd57406dbfa9578ee1d09c59b0783d2923
NgYaoDong <e0957740@u.nus.edu>
2024-01-26 23:51:26 +0800

    Add Level-1: Echo

--- src/main/java/Faiz.java
+++ src/main/java/Faiz.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+
 public class Faiz {
+    public static final String HorLine = "\t_________________________________________\n";
+    public  static void printHi() {
+        System.out.println(HorLine + "\t Hello! I'm Faiz!\n\t What can I do for you?\n" + HorLine);
+    }
+
+    public static void printBye() {
+        System.out.println(HorLine + "\t Bye. Hope to see you again soon!\n" + HorLine);;
+    }
+
+    public static void echo(String line) {
+        System.out.println(HorLine + "\t " + line + System.lineSeparator() + HorLine);
+    }
+
     public static void main(String[] args) {
-        String HorLine = " _________________________________________\n";
-        System.out.println(HorLine + " Hello! I'm Faiz!\n What can I do for you? \n" + HorLine + " Bye. Hope to see you again soon!\n" + HorLine);
+        String line;
+        Scanner in = new Scanner(System.in);
+        printHi();
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            } else {
+                echo(line);
+            }
+        }
+        printBye();
     }
 }
