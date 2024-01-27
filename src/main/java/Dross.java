7958e5ba731c02113e582c8645bcaeb51c569c0c
lordgareth10 <51813599+lordgareth10@users.noreply.github.com>
2024-01-27 18:43:40 +0800

    Update Dross.java
    Update Duke to Dross

---
+++ src/main/java/Dross.java
@@ -0,0 +1,7 @@
+public class Dross {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Dross");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
