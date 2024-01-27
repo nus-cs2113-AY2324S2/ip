64ff27890de7f36cd1ebe2cc6d515ab44dcc765a
ZhuSijia0711 <e0968116@u.nus.edu>
2024-01-27 14:07:54 +0800

    Echo response

--- src/main/java/Evelyn.java
+++ src/main/java/Evelyn.java
@@ -1,11 +1,37 @@
+import java.util.Scanner;
 public class Evelyn {
-    public static void main(String[] args) {
+    public  static void echo(){
+        String line;
+        System.out.println("type your command: ");
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        printLine();
+        if(line.equals("bye")){
+            return;
+        }
+        else {
+            System.out.println("this is your command: " + line);
+            printLine();
+            echo();
+        }
+    }
+    public static void printLine(){
         System.out.print("____________________________________________________________\n");
+    }
+    public static void greeting(){
+        printLine();
         System.out.println("Hello! I'm Evelyn");
         System.out.println("What can I do for you?");
-        System.out.print("____________________________________________________________\n");
+        printLine();
+    }
+    public static void end(){
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.print("____________________________________________________________");
+        printLine();
+    }
+    public static void main(String[] args) {
+        greeting();
+        echo();
+        end();
     }
 
 }
