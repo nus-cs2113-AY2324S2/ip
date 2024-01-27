157ce57f595068c0a214beefaf053c36599033a0
itsmejr257 <chanjunrong2@gmail.com>
2024-01-27 13:54:58 +0800

    Add exit messeges

--- src/main/java/JunBot.java
+++ src/main/java/JunBot.java
@@ -2,6 +2,8 @@ public class JunBot {
     public static void main(String[] args) {
         String line = "____________________________________________________________\n";
         String greeting = "Hello! I'm JunBot\nWhat can I do for you?\n";
+        String goodbye = "Bye. Hope to see you again soon!\n";
         System.out.println(line + greeting + line);
+        System.out.println(line + goodbye + line);
     }
 }
