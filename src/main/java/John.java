1cc5c0d874e7bef9afaf1a50608392639408b068
Cryolian <tanyixin@gmail.com>
2024-01-27 23:13:45 +0800

    Add level-3 functionalities

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -4,18 +4,48 @@ import java.util.List;
 
 public class John {
 
-    private static List<Task> taskList = new ArrayList<Task>();
+    private static List<Task> taskList = new ArrayList<>();
+    private static Scanner in = new Scanner(System.in);
 
     public static void parseInput(String input) {
+
+        int taskID;
 
         switch(input.toLowerCase()) {
         case "list":
             for (int i = 1; i <= taskList.size(); i += 1) {
-                System.out.println(i + ": " + taskList.get(i - 1).getName());
+                System.out.print(i + ".");
+                taskList.get(i - 1).printFull();
             }
             break;
 
         case "bye":
+            break;
+
+        case "mark":
+            taskID = in.nextInt();
+
+            if (taskID > taskList.size()) {
+                System.out.println("Out of range");
+                break;
+            }
+
+            taskList.get(taskID - 1).markCompleted();
+            System.out.println("Marking as done:");
+            taskList.get(taskID - 1).printFull();
+            break;
+
+        case "unmark":
+            taskID = in.nextInt();
+
+            if (taskID > taskList.size()) {
+                System.out.println("Out of range");
+                break;
+            }
+
+            taskList.get(taskID - 1).markUncompleted();
+            System.out.println("Marking as undone:");
+            taskList.get(taskID - 1).printFull();
             break;
 
         default:
@@ -27,15 +57,14 @@ public class John {
     }
     public static void main(String[] args) {
 
-        System.out.println("Hello! I'm John Chatbot.\n" + "What can I do for you?\n");
+        System.out.println("Hello! I'm John Chadbot.\n" + "What can I do for you?\n");
 
-        Scanner in = new Scanner(System.in);
-        String userInput = in.nextLine();
+        String userInput = in.next();
 
         while (!userInput.equalsIgnoreCase("bye")) {
 
             parseInput(userInput);
-            userInput = in.nextLine();
+            userInput = in.next();
         }
 
         System.out.println("Bye. Hope to see you again soon!");
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,21 +1,53 @@
 public class Task {
 
-    String Name;
+    private String name;
+    private boolean completed = false;
 
     public Task() {
-        Name = "Unnamed";
+        name = "Unnamed";
     }
 
     public Task(String input) {
-        this.Name = input;
+        this.name = input;
     }
 
     public String getName() {
-        return this.Name;
+        return this.name;
     }
 
     public void setName(String name) {
-        this.Name = name;
+        this.name = name;
+    }
+
+    public boolean getCompletedStatus() {
+        return completed;
+    }
+
+    public void markCompleted() {
+        completed = true;
+    }
+
+    public void markUncompleted() {
+        completed = false;
+    }
+
+    public void printBox() {
+        if (completed) {
+            System.out.print("[X]");
+        } else {
+            System.out.print("[ ]");
+        }
+    }
+
+    public void printName() {
+        System.out.print(name);
+    }
+
+    public void printFull() {
+        printBox();
+        System.out.print(" ");
+        printName();
+        System.out.println();
     }
 
 }
