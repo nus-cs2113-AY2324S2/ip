e61f402345bcb4ee88ee5c45256268f3f66fd803
yuki-zmstr <yukihide99@gmail.com>
2024-01-27 14:52:40 +0800

    Add echo function
    Says bye when user says bye, echoes otherwise

--- src/main/java/Yuki.java
+++ src/main/java/Yuki.java
@@ -1,11 +1,26 @@
+import java.util.Scanner;
 public class Yuki {
-    public static void main(String[] args) {
+
+    public static void printLine() {
         System.out.println("---------------------------------------------");
+    }
+    public static void main(String[] args) {
+        printLine();
         System.out.println("Hello! I am Yuki, your personal chat bot.");
         System.out.println("What can I do for you?");
-        System.out.println("---------------------------------------------");
-        System.out.println("---------------------------------------------");
-        System.out.println("Bye. Hope to see you soon!");
-        System.out.println("---------------------------------------------");
+        printLine();
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+
+        printLine();
+        if (line.equals("bye")) {
+            System.out.println("Bye! See you again.");
+        } else {
+            System.out.println("You said:" + line);
+        }
+        printLine();
+
     }
 }
