3aa8a6ffbd2442ce885eecc3bbe3f0b42ff7be8c
Damien Wee <damiwee@gmail.com>
2024-01-28 01:04:17 +0800

    Feature: Add echo user command

--- src/main/java/BossMan.java
+++ src/main/java/BossMan.java
@@ -1,9 +1,17 @@
+import java.util.Scanner;
+
 public class BossMan {
 
+    private final Scanner scanner;
     private final String sep = "____________________________________________________________";
+    private final String botName = "BossMan";
+
+    public BossMan() {
+        this.scanner = new Scanner(System.in);
+    }
 
     public void greetUser(){
-        String greet = "Hello! I'm BossMan";
+        String greet = "Hello! I'm " + botName;
         String offerService = "What can I do for you?";
         System.out.println(sep + "\n" + greet);
         System.out.println(offerService + "\n" + sep);
@@ -13,5 +21,16 @@ public class BossMan {
         String exit = "Bye. Hope to see you again soon!";
         System.out.println(exit + "\n" + sep);
     }
+
+    public void startChat() {
+        String userInput;
+
+        do {
+            System.out.print("You: ");
+            userInput = scanner.nextLine();
+            System.out.println(botName + ": " + userInput + "\n" + sep);
+        } while (!userInput.equalsIgnoreCase("bye"));
+    }
 }
+
 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         BossMan chatBot = new BossMan();
         chatBot.greetUser();
+        chatBot.startChat();
         chatBot.endChat();
     }
 }
