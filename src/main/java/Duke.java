6b018211c7e93fdb2c1d96de0a6975cf2189eebd
Hws2209 <wansing03@gmail.com>
2024-01-27 10:20:49 +0800

    Add echoing of commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "  ____   _   _      __     ______    _       _  _____\n" +
@@ -9,6 +11,18 @@ public class Duke {
 
         System.out.println("Hello! I'm Charlie!\n" + logo);
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner in = new Scanner(System.in);
+        String line;
+
+        while (true) {
+            line = in.nextLine();
+            if (line.contains("bye")) {
+                System.out.println("     Bye. Hope to see you again soon!");
+                break;
+            } else {
+                System.out.println("     " + line);
+            }
+        }
     }
 }
