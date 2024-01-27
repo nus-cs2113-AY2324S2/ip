b99897b317ad4ee6f63a4c1f39576e36cac65bb1
sweijie24 <e0958828@u.nus.edu>
2024-01-27 17:21:13 +0800

    Add tasks to storage Returns tasks when prompted

--- src/main/java/Quokka.java
+++ src/main/java/Quokka.java
@@ -1,6 +1,29 @@
 import java.util.Scanner;
 
 public class Quokka {
+    private static final int MAX_TASKS = 100;
+    private static String[] tasks = new String[MAX_TASKS];
+    private static int taskCount = 0;
+
+    private static void addTask(String task) {
+        if (taskCount < MAX_TASKS) {
+            tasks[taskCount++] = task;
+            System.out.println("    added: " + task);
+        } else {
+            System.out.println("    Sorry, the task list is full. You cannot add more tasks.");
+        }
+    }
+
+    private static void displayTasks() {
+        if (taskCount == 0) {
+            System.out.println("    No tasks added yet.");
+        } else {
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println("    " + (i + 1) + ". " + tasks[i]);
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -11,12 +34,17 @@ public class Quokka {
             // Read user input
             String userInput = scanner.nextLine();
 
-            // Echo the user's command
-            System.out.println("    " + userInput);
+            // Check if the user wants to list tasks
+            if (userInput.equalsIgnoreCase("list")) {
+                displayTasks();
+            } else {
+                // Add Task based on input
+                addTask(userInput);
+            }
 
             // Check if the user wants to exit
             if (userInput.equalsIgnoreCase("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("    Bye. Hope to see you again soon!");
                 break;
             }
         }
