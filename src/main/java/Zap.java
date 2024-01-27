d3479c4a7dbca5a46df646a91370194885cd60fc
avrilgk <e1036297@u.nus.edu>
2024-01-27 22:40:45 +0800

    Change from Duke to Zap

---
+++ src/main/java/Zap.java
@@ -0,0 +1,10 @@
+public class Zap {
+    public static void main(String[] args) {
+        String logo = "  _____   _   _   ____\n"
+                + " |__  /  / \\ / | |  _ \\\n"
+                + "   / /  / _ \\| | | |_) |\n"
+                + "  / /_ / ___ \\ | |  __/\n"
+                + " /____/_/   \\_\\_| |_|";
+        System.out.println("Hello from\n" + logo);
+    }
+}
