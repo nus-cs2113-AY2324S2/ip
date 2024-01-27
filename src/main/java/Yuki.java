db9c31d3e1dc348a82d798b905f71585a882028e
yuki-zmstr <yukihide99@gmail.com>
2024-01-27 13:25:41 +0800

    Add horizontal lines

--- src/main/java/Yuki.java
+++ src/main/java/Yuki.java
@@ -1,7 +1,11 @@
 public class Yuki {
     public static void main(String[] args) {
+        System.out.println("---------------------------------------------");
         System.out.println("Hello! I am Yuki, your personal chat bot.");
         System.out.println("What can I do for you?");
+        System.out.println("---------------------------------------------");
+        System.out.println("---------------------------------------------");
         System.out.println("Bye. Hope to see you soon!");
+        System.out.println("---------------------------------------------");
     }
 }
