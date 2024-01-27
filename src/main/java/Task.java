7ff82f34604cee6a640a7fe2ecb7029ed2744b6c
owx0130 <weixiang0130@gmail.com>
2024-01-27 18:09:43 +0800

    add Task class to track completion status

---
+++ src/main/java/Task.java
@@ -0,0 +1,22 @@
+public class Task {
+    private String name;
+    private boolean isDone;
+
+    public Task(String name) {
+        this.name = name;
+        isDone = false;
+    }
+
+    public void changeStatus(boolean isDone) {
+        this.isDone = isDone;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : "");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
