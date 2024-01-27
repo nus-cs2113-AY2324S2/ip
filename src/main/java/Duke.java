7f789c6856835d4f1206d7936495b5a906ed233d
Hws2209 <wansing03@gmail.com>
2024-01-27 18:38:36 +0800

    Add ability to mark tasks as done and not done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -23,9 +23,21 @@ public class Duke {
                 System.out.println("     Bye. Hope to see you again soon!");
                 break;
             } else if (line.startsWith("list")) {
+                System.out.println("     Here is your list:");
                 for (int i = 0; i < numberOfTasks; i++) {
-                    System.out.println("     " + (i + 1) + ". " + tasks[i].description);
+                    System.out.println("     " + (i + 1) + ".[" +
+                            tasks[i].getStatusIcon() + "] " + tasks[i].description);
                 }
+            } else if (line.startsWith("mark")) {
+                int indexMarked = Integer.parseInt(line.substring(5)) - 1;
+                tasks[indexMarked].isDone = true;
+                System.out.println("     Nice! I've marked this task as done:");
+                System.out.println("       [X] " + tasks[indexMarked].description);
+            } else if (line.startsWith("unmark")) {
+                int indexUnmarked = Integer.parseInt(line.substring(7)) - 1;
+                tasks[indexUnmarked].isDone = false;
+                System.out.println("     OK, I've marked this task as not done yet:");
+                System.out.println("       [ ] " + tasks[indexUnmarked].description);
             } else {
                 tasks[numberOfTasks] = new Task(line);
                 numberOfTasks++;
