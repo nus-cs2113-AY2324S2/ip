8bfc2d22bbbfea3713da1cdd6ff25a131be28da9
itsmejr257 <chanjunrong2@gmail.com>
2024-01-27 17:48:14 +0800

    Add addToList() and printList() functions
    Add the ability to store whatever text entered by the user and display
    them back to the user when requested.

--- src/main/java/JunBot.java
+++ src/main/java/JunBot.java
@@ -1,26 +1,47 @@
 import java.util.Scanner;
 public class JunBot {
 
-    public static void commandHandler(){
-        String line;
-        String divider = "____________________________________________________________\n";
-        Scanner userInputScanner = new Scanner(System.in);
-        line = userInputScanner.nextLine();
+    public static void printList(String[] tasks, int tasksCount){
+        int taskNumber = 1;
+        String divider = "____________________________________________________________";
 
-        while (!line.equals("bye")){
-            System.out.println(divider + line+ "\n" + divider);
-            line = userInputScanner.nextLine();
+        System.out.println(divider);
+        for(int i = 0; i < tasksCount; i++){
+            System.out.println( taskNumber + ". " + tasks[i]);
+            taskNumber += 1;
         }
-
+        System.out.println(divider+"\n");
     }
+
+    public static void addToList(String task, String[] tasks, int listPosition){
+        String divider = "____________________________________________________________\n";
+        tasks[listPosition] = task;
+        System.out.println(divider + "added: " + task + "\n" + divider);
+    }
+
     public static void main(String[] args) {
         String divider = "____________________________________________________________\n";
         String greeting = "Hello! I'm JunBot\nWhat can I do for you?\n";
         String goodbye = "Bye. Hope to see you again soon!\n";
         String[] userInputsList = new String[100];
 
+
         System.out.println(divider + greeting + divider);
-        commandHandler();
+
+        Scanner userInputScanner = new Scanner(System.in);
+        String userInput = userInputScanner.nextLine();
+        int tasksCount = 0;
+
+        while (!userInput.equals("bye")){
+            switch (userInput) {
+            case "list" : printList(userInputsList, tasksCount);
+                break;
+            default: addToList(userInput, userInputsList, tasksCount); tasksCount++;
+                break;
+            }
+            userInput = userInputScanner.nextLine();
+        }
+
         System.out.println(divider + goodbye + divider);
     }
 }
