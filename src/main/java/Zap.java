67f4605bab2b5ef4f01cf8f09880912ad7cdadb0
avrilgk <e1036297@u.nus.edu>
2024-01-27 22:51:02 +0800

    Greet and Exit

--- src/main/java/Zap.java
+++ src/main/java/Zap.java
@@ -5,7 +5,22 @@ public class Zap {
                 + "   / /  / _ \\| | | |_) |\n"
                 + "  / /_ / ___ \\ | |  __/\n"
                 + " /____/_/   \\_\\_| |_|";
-
         System.out.println("Hello from\n" + logo);
+        greeting();
+        exit();
+    }
+    //User Greeting
+    private static void greeting() {
+        String chatbotName = "ZAP";
+        System.out.println("____________________________________________________________");
+        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+    }
+    //Exit Chatbot
+    private static void exit() {
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
