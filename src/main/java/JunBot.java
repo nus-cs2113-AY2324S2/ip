25e30ece092a81e8173e8997a1a789f572193d42
itsmejr257 <chanjunrong2@gmail.com>
2024-01-27 13:54:25 +0800

    Add greetings

--- src/main/java/JunBot.java
+++ src/main/java/JunBot.java
@@ -1,4 +1,7 @@
 public class JunBot {
     public static void main(String[] args) {
+        String line = "____________________________________________________________\n";
+        String greeting = "Hello! I'm JunBot\nWhat can I do for you?\n";
+        System.out.println(line + greeting + line);
     }
 }
