4cfe50d2bb242e8675fa3db992f0e7c8ad0ca735
soongensayo <e0959972@u.nus.edu>
2024-01-27 14:59:13 +0800

    Add Echo

--- src/main/java/Sayo.java
+++ src/main/java/Sayo.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class Sayo {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        System.out.println("Hello! I'm Sayo \n" + "What can I do for you? \n" + "\n" + "Bye. Hope to see you again soon!");
+        System.out.println("Hello! I'm Sayo and it's great to see you! \n" + "What can I do for you today? \n");
+
+        String input = " ";
+        do {
+            input = scanner.nextLine();
+            if(!input.equals("bye")){
+                System.out.println(input);
+            }
+        } while (!input.equals("bye"));
+
+        System.out.println("Bye. Hope to see you again soon!");
+        scanner.close();
     }
 }
