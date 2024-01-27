df1b2bf7c25489dbe5e950e4c0c6fbfc26d82bd5
Edmundtangg <mira3les@gmail.com>
2024-01-27 11:19:41 +0800

    W3-L2: able to store user input as a list and display to them when requested

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,46 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
 
     public static void inputChecker() {
-        String line; //declared a string object to take in user input
         Scanner in = new Scanner(System.in); //declared a scanner object
-        line = "";
+        String line; //declared a string object to take in user input
 
+        String[] list; //declare a list with string data type
+        list = new String[100]; //create a list of 100 elements
+        int counter = 0;
+        int index = 1;
+
+//start of user input
         while (true) {
             line = in.nextLine(); //takes in user input
+
+            //when user exits
             if (line.equals("bye")) {
                 System.out.println("Bye human. Come back soon !");
                 break;
             }
-            System.out.println("You entered: " + line);
+
+            //if user wants to display a list
+            else if (line.equals("list")) {
+                for (String s : list) {
+                    if (s == null){
+                        break;
+                    }
+                    System.out.println(index + ". " + s);
+                    index += 1;
+                }
+                System.out.println("____________________________________________________________");
+                index = 1;
+                continue;
+            }
+
+            //user adds in elements
+            System.out.println("added: " + line);
+            System.out.println("____________________________________________________________");
+            list[counter] = line;
+            counter += 1;
         }
     }
 
