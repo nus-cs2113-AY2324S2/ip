73b1bf424841a6d29785d9a9bcbfe92760e1db93
Justin <hoggersoh@gmail.com>
2024-01-27 13:44:49 +0800

    Add Echo
    - Added ability for bot to echo
    - bot able to say bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
-        String appName = "mimichat";
+    final static String appName = "mimichat";
+
+    public static void startupSequence(){
         System.out.println("-------------------------------------------");
         System.out.println("Hello! I'm " + appName);
         System.out.println("What can I do for you?");
         System.out.println("-------------------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void customPrint(String input){
+        System.out.println("-------------------------------------------");
+        System.out.println(input);
+        System.out.println("-------------------------------------------");
+
+    }
+    public static void main(String[] args) {
+
+        // Initial welcome message
+        startupSequence();
+
+        while(true){
+            Scanner scanner = new Scanner(System.in);
+            String input = scanner.nextLine();
+            if (input.equals("bye")){
+                customPrint("Bye. Hope to see you again soon!");
+                break;
+            }
+            customPrint(input);
+        }
+
     }
 }
