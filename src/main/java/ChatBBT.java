d4128eeb29fbd1525d075f680f3889e443c00a80
XiaoBo <xiaobo990219@gmail.com>
2024-01-27 22:35:45 +0800

    level-1: Echo added

--- src/main/java/ChatBBT.java
+++ src/main/java/ChatBBT.java
@@ -1,7 +1,32 @@
+import java.util.Scanner;
+
 public class ChatBBT {
-    public static void main(String[] args) {
+
+    public static void exitMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void greetingMessage() {
         System.out.println("Hello! I'm ChatBBT");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        greetingMessage();
+        Scanner input = new Scanner(System.in);
+        boolean isFinished = false;
+
+        while(!isFinished) {
+            String inputText = input.nextLine();
+            if (inputText.equals("bye")) {
+                exitMessage();
+                isFinished = true;
+            } else {
+                System.out.println(inputText);
+                System.out.println("------------------------------------------");
+            }
+        }
+
     }
 }
