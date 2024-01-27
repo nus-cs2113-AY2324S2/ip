55caddf431e163a94a208cce9198a9cb67e421d9
sweijie24 <e0958828@u.nus.edu>
2024-01-27 17:05:08 +0800

    Add Echo command

--- src/main/java/Quokka.java
+++ src/main/java/Quokka.java
@@ -1,7 +1,27 @@
+import java.util.Scanner;
+
 public class Quokka {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Quokka");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            // Read user input
+            String userInput = scanner.nextLine();
+
+            // Echo the user's command
+            System.out.println("    " + userInput);
+
+            // Check if the user wants to exit
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+
+        // Close the scanner
+        scanner.close();
     }
 }
