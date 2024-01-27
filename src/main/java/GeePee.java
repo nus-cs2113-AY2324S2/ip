96489095ea31db2a2ba75ff8e3938eee3cb51bd2
owx0130 <weixiang0130@gmail.com>
2024-01-27 15:55:04 +0800

    add feature to echo user input

--- src/main/java/GeePee.java
+++ src/main/java/GeePee.java
@@ -1,10 +1,32 @@
+import java.util.Scanner;
+
 public class GeePee {
+
+    public static void greetUser() {
+        System.out.println("    ________________________________________________");
+        System.out.println("    Hello! I'm GeePee, your friendly chatbot assistant!");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ________________________________________________\n");
+    }
+
+    public static void echoInput(String input) {
+        System.out.println("    ________________________________________________");
+        if (input.equals("bye")) {
+            System.out.println("    Bye! Hope to see you again soon!");
+        } else {
+            System.out.println("    " + input);
+        }
+        System.out.println("    ________________________________________________\n");
+    }
+
     public static void main(String[] args) {
-        System.out.println("________________________________________________");
-        System.out.println("Hello! I'm GeePee, your friendly chatbot assistant!");
-        System.out.println("What can I do for you?");
-        System.out.println("________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("________________________________________________");
+        greetUser();
+        String line = "";
+        Scanner in = new Scanner(System.in);
+        while (!(line.equals("bye"))) {
+            line = in.nextLine();
+            if (line.trim().equals("")) continue;
+            echoInput(line);
+        }
     }
 }
