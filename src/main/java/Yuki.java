fee04290e9d62b855575a5c477cf37daac889bb0
yuki-zmstr <yukihide99@gmail.com>
2024-01-27 13:20:07 +0800

    Rename, greet, and exit.
    Renamed bot to Yuki, added some greetings.

---
+++ src/main/java/Yuki.java
@@ -0,0 +1,7 @@
+public class Yuki {
+    public static void main(String[] args) {
+        System.out.println("Hello! I am Yuki, your personal chat bot.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you soon!");
+    }
+}
