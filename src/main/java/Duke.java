dbd93dc8fd7cadbf24a7f2997548de053ef33a1a
Leong-ZM <e0957648@u.nus.edu>
2024-01-27 09:09:25 +0800

    Add Rename, Greet, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,4 @@
-public class Duke {
+/*public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +6,14 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+    }
+}*/
+
+public class Duke {
+    public static void main(String[] args) {
+        String chatbotName = "Horizon";
+        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" What can I do for you? ");
+        System.out.println(" Bye. Hope to see you again soon! ");
     }
 }
