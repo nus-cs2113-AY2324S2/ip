a8918c397ef88952addcf638fed62596583c54b0
sweijie24 <e0958828@u.nus.edu>
2024-01-27 16:31:15 +0800

    Renamed chatbot Added introductory lines

---
+++ src/main/java/Quokka.java
@@ -0,0 +1,7 @@
+public class Quokka {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Quokka");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
