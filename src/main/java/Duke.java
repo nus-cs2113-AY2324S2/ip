37a11fd7b8288e9e80b11d773165da5b11a441d3
Edmundtangg <mira3les@gmail.com>
2024-01-27 10:51:57 +0800

    W3S1: echo commands and can exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    public static void inputChecker() {
+        String line; //declared a string object to take in user input
+        Scanner in = new Scanner(System.in); //declared a scanner object
+        line = "";
+
+        while (true) {
+            line = in.nextLine(); //takes in user input
+            if (line.equals("bye")) {
+                System.out.println("Bye human. Come back soon !");
+                break;
+            }
+            System.out.println("You entered: " + line);
+        }
+    }
+
     public static void main(String[] args) {
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________?");
+
+        inputChecker();
     }
 }
 
