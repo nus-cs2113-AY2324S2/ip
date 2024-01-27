2e77a6a03a4dc73da5dd0bb37e7f2d8dde555888
itsmejr257 <chanjunrong2@gmail.com>
2024-01-27 17:04:46 +0800

    Add command echoing and exit on 'bye' command
    This commit introduces a new feature that echoes commands entered by
    user in the command-line. Application exits when user types 'bye'

--- src/main/java/JunBot.java
+++ src/main/java/JunBot.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
 public class JunBot {
+
+    public static void echoCommands(){
+        String line;
+        String divider = "____________________________________________________________\n";
+        Scanner userInputScanner = new Scanner(System.in);
+        line = userInputScanner.nextLine();
+
+        while (!line.equals("bye")){
+            System.out.println(divider + line+ "\n" + divider);
+            line = userInputScanner.nextLine();
+        }
+
+    }
     public static void main(String[] args) {
-        String line = "____________________________________________________________\n";
+        String divider = "____________________________________________________________\n";
         String greeting = "Hello! I'm JunBot\nWhat can I do for you?\n";
         String goodbye = "Bye. Hope to see you again soon!\n";
-        System.out.println(line + greeting + line);
-        System.out.println(line + goodbye + line);
+
+        System.out.println(divider + greeting + divider);
+        echoCommands();
+        System.out.println(divider + goodbye + divider);
     }
 }
