d302fbc8394b7c7d4116870dbf07f9b9addc3349
rismm <sim.jj.ryan@u.nus.edu>
2024-01-27 23:17:17 +0800

    Add chatbot echo

--- src/main/java/Joe.java
+++ src/main/java/Joe.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class Joe {
     private static final String H_LINE = "____________________________________________________________\n";
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
         System.out.println(H_LINE + "HI I'M JOE\n" + "WHAT CAN I DO FOR YOU\n" + H_LINE);
-        System.out.println(H_LINE + "GOODBYE. PLEASE COME BACK AGAIN\n" + H_LINE);
+        
+        boolean hasExitInput = false;
+        while (!hasExitInput) {
+            String input = in.nextLine();
+            if (input.equals("bye")) {
+                hasExitInput = true;
+            } else {
+                System.out.println(H_LINE + input + '\n' + H_LINE);
+            }
+        }
+        
+        System.out.println(H_LINE + "GOODBYE. PLEASE COME BACK AGAIN :)\n" + H_LINE);
     }
 }
