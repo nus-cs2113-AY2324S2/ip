9b9443d28b9983c487fadca9b3063ce35e290007
itsmejr257 <chanjunrong2@gmail.com>
2024-01-27 18:52:43 +0800

    Add mark, unmark and print functionality
    This commit implements ability to mark and unmark tasks, along with a
    new feature to print tasks with its status icon.

--- src/main/java/JunBot.java
+++ src/main/java/JunBot.java
@@ -1,13 +1,37 @@
 import java.util.Scanner;
 public class JunBot {
 
+
+    public static void unmarkTaskInList(Task[] tasks, String command){
+        command = command.replace("unmark", "").trim();
+        int listNumber = Integer.parseInt(command) - 1;
+        tasks[listNumber].unmarkTask();
+    }
+    public static void markTaskInList(Task[] tasks, String command){
+        command = command.replace("mark", "").trim();
+        int listNumber = Integer.parseInt(command) - 1;
+        tasks[listNumber].markTask();
+    }
+
+    public static String getCommand(String userInput){
+        int i  = userInput.indexOf(' ');
+        String command;
+        if ( i != -1 ) {
+            command = userInput.substring(0, i);
+        } else {
+            command = userInput;
+        }
+        return command;
+
+    }
     public static void printList(Task[] tasks, int tasksCount){
         int taskNumber = 1;
         String divider = "____________________________________________________________";
 
         System.out.println(divider);
         for(int i = 0; i < tasksCount; i++){
-            System.out.println( taskNumber + ". " + tasks[i].description);
+            System.out.print( taskNumber + ". ");
+            tasks[i].printTask();
             taskNumber += 1;
         }
         System.out.println(divider+"\n");
@@ -15,7 +39,7 @@ public class JunBot {
 
     public static void addToList(String description, Task[] tasks, int listPosition){
         String divider = "____________________________________________________________\n";
-        Task userTask = new Task(description);
+        Task userTask = new Task(description,listPosition);
         tasks[listPosition] = userTask;
         System.out.println(divider + "added: " + userTask.description + "\n" + divider);
     }
@@ -24,7 +48,7 @@ public class JunBot {
         String divider = "____________________________________________________________\n";
         String greeting = "Hello! I'm JunBot\nWhat can I do for you?\n";
         String goodbye = "Bye. Hope to see you again soon!\n";
-        Task[] userInputsList = new Task[100];
+        Task[] tasks = new Task[100];
 
         System.out.println(divider + greeting + divider);
 
@@ -33,10 +57,16 @@ public class JunBot {
         int tasksCount = 0;
 
         while (!userInput.equals("bye")){
-            switch (userInput) {
-            case "list" : printList(userInputsList, tasksCount);
+            String command = getCommand(userInput);
+            switch (command) {
+            case "list" : printList(tasks, tasksCount);
                 break;
-            default: addToList(userInput, userInputsList, tasksCount); tasksCount++;
+
+            case "mark" : markTaskInList(tasks, userInput);
+                break;
+            case "unmark": unmarkTaskInList(tasks, userInput);
+                break;
+            default: addToList(userInput, tasks, tasksCount); tasksCount++;
                 break;
             }
             userInput = userInputScanner.nextLine();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,41 @@
 public class Task {
     protected String description;
+    protected int listNumber;
     protected boolean isDone;
 
-    public Task(String description) {
+
+    public Task(String description, int listPosition) {
         this.description = description;
         this.isDone = false;
+        this.listNumber = listPosition;
     }
 
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
+
+    public void markTask(){
+        this.isDone = true;
+        String divider = "____________________________________________________________";
+        System.out.println(divider);
+        System.out.println("Nice! I've marked this task as done:");
+        this.printTask();
+        System.out.println(divider);
+
+    }
+
+    public void unmarkTask(){
+        this.isDone = false;
+        String divider = "____________________________________________________________";
+        System.out.println(divider);
+        System.out.println("Ok, I've marked this task as not done yet:");
+        this.printTask();
+        System.out.println(divider);
+
+    }
+
+    public void printTask(){
+        System.out.print("[" + this.getStatusIcon() + "] " + this.description + "\n");
+    }
+
 }
