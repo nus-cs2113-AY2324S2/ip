f421cefb76c0103619d63e9627cb9d0af5f92490
Cryolian <tanyixin@gmail.com>
2024-01-27 21:47:07 +0800

    Add Level-1: Echo functionality

--- src/main/java/John.java
+++ src/main/java/John.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
+
 public class John {
     public static void main(String[] args) {
 
         System.out.println("Hello! I'm John Chatbot.\n" + "What can I do for you?\n");
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println(line);
+            line = in.nextLine();
+        }
 
         System.out.println("Bye. Hope to see you again soon!");
     }
