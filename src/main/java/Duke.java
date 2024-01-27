e5b48f317f20717a665849d2fa2bc9f5cc012c13
Justin <hoggersoh@gmail.com>
2024-01-27 14:11:07 +0800

    Add List
    - Change to switch statements
    - Able to add to taskList
    - Able to print out taskList

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,32 +3,54 @@ import java.util.Scanner;
 public class Duke {
     final static String appName = "mimichat";
 
-    public static void startupSequence(){
+    public static void startupSequence() {
         System.out.println("-------------------------------------------");
         System.out.println("Hello! I'm " + appName);
         System.out.println("What can I do for you?");
         System.out.println("-------------------------------------------");
     }
 
-    public static void customPrint(String input){
+    public static void customPrint(String input) {
         System.out.println("-------------------------------------------");
         System.out.println(input);
         System.out.println("-------------------------------------------");
 
     }
+
+    public static void listTasks(String[] list, int numberOfTasks){
+        System.out.println("-------------------------------------------");
+        for(int i = 0; i < numberOfTasks; i++){
+            System.out.println(Integer.toString(i+1) + ". " + list[i]);
+        }
+        System.out.println("-------------------------------------------");
+
+
+    }
+
     public static void main(String[] args) {
 
         // Initial welcome message
         startupSequence();
-
-        while(true){
+        String[] taskList = new String[100];
+        int numberOfTask = 0;
+        boolean isRunning = true;
+        while (isRunning) {
             Scanner scanner = new Scanner(System.in);
             String input = scanner.nextLine();
-            if (input.equals("bye")){
+            switch (input) {
+            case "bye":
                 customPrint("Bye. Hope to see you again soon!");
+                isRunning = false;
+                break;
+            case "list":
+                listTasks(taskList, numberOfTask);
+                break;
+            default:
+                taskList[numberOfTask] = input;
+                numberOfTask++;
+                customPrint("added: " + input);
                 break;
             }
-            customPrint(input);
         }
 
     }
