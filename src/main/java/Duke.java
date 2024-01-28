729b790d195f53ea326d5acf62ed3b58aa3f5a77
yzhanglp <yzhanglp@connect.ust.hk>
2024-01-28 23:17:58 +0800

    Finished the implementation of Level of ip

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,10 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        String name ="Altria";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello, I'm "+name);
+        System.out.println("What can I do for you?");
+        System.out.println("\n"+"Bye.Hope to see you again soon!");
     }
 }
