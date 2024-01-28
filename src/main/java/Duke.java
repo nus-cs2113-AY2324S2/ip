8c5b44b52ee242ca2504d162def2a04e31e3cbfd
E0958902 <E0958902@u.nus.edu>
2024-01-28 14:26:53 +0800

    Rename chatbot to Lotes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,6 @@
 public class Duke {
     public static void main(String[] args) {
-
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Lotes");
     }
 }
