6ab36d2ef74c288881807aa018df174dd40374b6
Luo Zhiyang <e0969568@u.nus.edu>
2024-01-29 00:20:13 +0800

    Level-1

--- src/main/java/Asuka.java
+++ src/main/java/Asuka.java
@@ -1,8 +1,22 @@
+import java.util.Scanner;
+
 public class Asuka {
     public static void main(String[] args) {
+
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Asuka\nWhat can I do for you?");
         System.out.println("____________________________________________________________");
+        Scanner myObj = new Scanner(System.in);
+        String command = myObj.nextLine();
+        while (!command.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(command);
+            System.out.println("____________________________________________________________");
+            command = myObj.nextLine();
+        };
+        myObj.close();
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
