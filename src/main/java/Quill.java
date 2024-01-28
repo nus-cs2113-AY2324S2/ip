e4b54abe8b585702364d43fbe2bce221def9cdec
nyh3 <e0970473@u.nus.edu>
2024-01-28 18:03:26 +0800

    Echo commands entered by users

--- src/main/java/Quill.java
+++ src/main/java/Quill.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class Quill {
     public static void main(String[] args) {
-        String partition = "____________________________________________________________\n";
-        System.out.println(partition + "Hello! I'm Quill.\nWhat can i do for you?");
-        System.out.println(partition + "Bye! Hope to see you again soon!\n" + partition);
+        String horizontalLine = "\n____________________________________________________________\n";
+        String name = "Quill";
+        String line;
+        Scanner in = new Scanner(System.in);
+        System.out.println(horizontalLine + "Hello! I'm " + name + ".\nWhat can i do for you?" + horizontalLine);
+
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(horizontalLine + line + horizontalLine);
+            line = in.nextLine();
+        }
+        System.out.println(horizontalLine + "Bye! Hope to see you again soon!" + horizontalLine);
     }
 }
