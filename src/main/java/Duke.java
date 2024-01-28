2d304a13461e19d6b7a80fe2d62415c5479dd31f
RyanTDL <teodeliangryan@gmail.com>
2024-01-28 17:43:23 +0800

    Completion of Increment (Level 1)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,26 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String greet = 
-        "____________________________________________________________ \n"
-        + "Hello! I'm J@RV15\n"
-        + "What can I do for you? \n"
-        + "____________________________________________________________ \n"
-        + "Bye. Hope to see you again soon! \n"
-        + "____________________________________________________________ \n";
-
+        
+        String greet = "Hello! I'm J@RV15\n" + "What can I do for you? \n" + "__________________________";
         System.out.println(greet);
+        boolean isStillChatting = true;
+        Scanner myScanner = new Scanner(System.in);
+
+        while (isStillChatting){
+            String userCommand = myScanner.nextLine();
+
+            if (userCommand.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+                isStillChatting = false;
+            } else {
+                System.out.println("Currently processing your request to do: " + userCommand);
+                System.out.println("Request completed! \n" + "_____________________________");
+
+            }
+        }
+
+        myScanner.close();
     }
 }
