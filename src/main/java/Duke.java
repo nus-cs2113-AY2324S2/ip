4d2aefe7548f33c3529710f2075d0e2e85abe7bf
FANGSIHAN <A17321429315@outlook.com>
2024-01-28 18:13:10 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,31 @@
+import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
+    public static void dividingLine(){
+        System.out.println("\t________________________________");
+    }
+    public static void communicate(){
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while(!line.equals("bye")){
+            dividingLine();
+            System.out.println("\t" + line);
+            dividingLine();
+            line = in.nextLine();
+        }
+    }
+    public static void startConversation(){
+        dividingLine();
+        System.out.println("\tHello!, I'm 'Noooob'");
+        System.out.println("\tWhat can I do for you?");
+        dividingLine();
+
+        communicate();
+        dividingLine();
+        System.out.println("\tBye. Hope to see you again soon!");
+        dividingLine();
+    }
     public static void main(String[] args) {
-        System.out.println("________________________________");
-        System.out.println("Hello!, I'm 'Noooob'");
-        System.out.println("What can I do for you?");
-        System.out.println("________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+       startConversation();
     }
 }
