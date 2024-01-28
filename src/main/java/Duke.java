903c6f2dfa33638db7cfb95d7092e68a0d8e216d
E0958902 <E0958902@u.nus.edu>
2024-01-28 14:28:08 +0800

    Add greet message to chatbot

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,5 +2,7 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Lotes");
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
     }
 }
