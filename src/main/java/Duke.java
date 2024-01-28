acc2148972784313ba902e57833dd67c5a2f0022
Tan Dexter <dextboy@outlook.com>
2024-01-28 22:50:50 +0800

    Add printGreeting() and printExitMessage() for Level-0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
+
+    private static final String NAME = "Natsu";
+
+    private static void printGreeting() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm " + NAME);
+        System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printExitMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        printGreeting();
+        printExitMessage();
     }
 }
