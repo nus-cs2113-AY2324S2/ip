ff818ab93ab031786bf5ddb92b9f5bce42bca7c2
avrilgk <e1036297@u.nus.edu>
2024-01-28 22:21:22 +0800

    Level 1: Echo

--- src/main/java/Zap.java
+++ src/main/java/Zap.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Zap {
     public static void main(String[] args) {
         String logo = "  _____   _   _   ____\n"
@@ -7,20 +9,44 @@ public class Zap {
                 + " /____/_/   \\_\\_| |_|";
         System.out.println("Hello from\n" + logo);
         greeting();
+        processCommands();
         exit();
     }
-    //User Greeting
+
+    // User Greeting
     private static void greeting() {
         String chatbotName = "ZAP";
         System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm " + chatbotName);
+        System.out.println(" 你好! Hello! Konichiwa! I'm " + chatbotName);
         System.out.println(" What can I do for you?");
         System.out.println("____________________________________________________________");
-
     }
-    //Exit Chatbot
+
+    // Process User Commands
+    private static void processCommands() {
+        Scanner scanner = new Scanner(System.in);
+        String userCommand;
+
+        do {
+            System.out.print("Enter a command: ");
+            userCommand = scanner.nextLine();
+
+            if (userCommand.equalsIgnoreCase("thank you and bye")) {
+                scanner.close();
+                break;
+            } else if (userCommand.equalsIgnoreCase("hi")) {
+                System.out.println("Hello! I am ZAP and I will just repeat everything you say!");
+            } else if (userCommand.equalsIgnoreCase("bye")) {
+                System.out.println("You should say thank you, then say bye.");
+            } else {
+                System.out.println(userCommand);
+            }
+        } while (true);
+        scanner.close();
+    }
+    // Exit Chatbot
     private static void exit() {
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("Bye! See you again next time :)");
         System.out.println("____________________________________________________________");
     }
 }
