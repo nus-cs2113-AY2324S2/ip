6b6270d8a16f62df1c1961bb3eba62e88199dc41
RyanTDL <teodeliangryan@gmail.com>
2024-01-28 20:13:17 +0800

    Completion of Increment (Level 3)

---
+++ src/main/java/Jake.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class Jake {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello! I'm Jake\n" + "What can I do for you? \n" + "__________________________");
+
+        Scanner myScanner = new Scanner(System.in);
+        Task[] commands = new Task[100];
+        int totalCommands = 0;
+        String userCommand= "";
+
+        do {
+            userCommand = myScanner.nextLine();
+
+            if (userCommand.equals("bye")){
+                System.out.println("Bye. Hope to see you again soon!");
+            } else if (userCommand.equals("list")){
+                System.out.println("Current commands being executed: ");
+                for (int i=0; i<totalCommands; i++){
+                    System.out.println(Integer.toString(i+1) + ".["+ commands[i].getStatusIcon() + "] "+ commands[i].description);
+                }
+            } else if (userCommand.contains("mark")){
+                int taskNumber = Integer.parseInt(userCommand.substring(userCommand.length()-1));
+                if (taskNumber>totalCommands){
+                    System.out.println("Task does not exist!");
+                    continue;
+                }
+                if (userCommand.startsWith("unmark")){
+                    System.out.println("Successfully unmarked");
+                    commands[taskNumber-1].markAsUndone();;
+                } else {
+                    System.out.println("Successfully marked");
+                    commands[taskNumber-1].markAsDone();;
+                }
+            } else {
+                System.out.println("Successfully added: " + userCommand + "\n" + "__________________________");
+                Task newTask = new Task(userCommand);
+                commands[totalCommands] = newTask;
+                totalCommands++;
+            }
+        } while (!userCommand.equals("bye"));
+
+        myScanner.close();
+    }
+}
