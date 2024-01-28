af9a94c903a17bfc2a7bcdbf9e72cd7fd6099a0b
Lin Htet <earthangle007@gmail.com>
2024-01-28 16:42:24 +0800

    Fix error of adding the word 'list' as part of the whole list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,14 +10,14 @@ public class Duke {
         int count = 0;
         while(true){
             Scanner userInput = new Scanner(System.in);
-            userCommands[count] = userInput.nextLine();
-            if(userCommands[count].equals("bye")){
+            String task = userInput.nextLine();
+            if(task.equals("bye")){
                 System.out.println(
                         "_________________________\n"
                         + "Bye. Hope to see you again soon!\n"
                         + "_________________________\n");
                 return;
-            } else if(userCommands[count].equals("list")){
+            } else if(task.equals("list")){
                 System.out.println("_________________________");
                 for(int i = 0; i < count; i++){
                     System.out.println("    " + (i+1) + ". "
@@ -25,13 +25,14 @@ public class Duke {
                 }
                 System.out.println("_________________________");
             }else{
+                userCommands[count] = task;
                 System.out.println(
                         "_________________________\n"
                         + "    added: " + userCommands[count]
                         + "\n"
                         + "_________________________\n");
+                count++;
             }
-            count++;
         }
 
     }
