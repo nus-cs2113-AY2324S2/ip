efa76bbbcbb474275d0cd59a9a87194413a7552c
Lin Htet <earthangle007@gmail.com>
2024-01-28 15:47:51 +0800

    Echo commands entered by the user.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String chatBot = "Hello! I'm TONY\n"
                 + "What can I do for you?\n"
-                + "_________________________\n"
-                + "Bye. Hope to see you again soon!\n"
                 + "_________________________";
         System.out.println(chatBot);
+        while(true){
+            Scanner userInput = new Scanner(System.in);
+            String names = userInput.nextLine();
+
+            System.out.println(
+                    "_________________________\n"
+                    + "    " + names
+                    + "\n"
+                    + "_________________________\n");
+        }
+
     }
 }
