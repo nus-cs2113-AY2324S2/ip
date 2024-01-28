778e8c71a6bbe8d4739c7ec611f3d04e16488bc4
nyh3 <e0970473@u.nus.edu>
2024-01-28 18:28:51 +0800

    Add ability to store texts

--- src/main/java/Quill.java
+++ src/main/java/Quill.java
@@ -4,13 +4,27 @@ public class Quill {
         String horizontalLine = "\n____________________________________________________________\n";
         String name = "Quill";
         String line;
+        String[] texts = new String[100];
+        int totalTexts = 0;
         Scanner in = new Scanner(System.in);
+
         System.out.println(horizontalLine + "Hello! I'm " + name + ".\nWhat can i do for you?" + horizontalLine);
 
         line = in.nextLine();
         while (!line.equals("bye")) {
-            System.out.println(horizontalLine + line + horizontalLine);
-            line = in.nextLine();
+            if (line.equals("list")) {
+                System.out.println((horizontalLine));
+                for (int i = 0; i < totalTexts; i++) {
+                    System.out.println(i + 1 + ". " + texts[i]);
+                }
+                System.out.println(horizontalLine);
+                line = in.nextLine();
+            } else {
+                System.out.println(horizontalLine + "Added: " + line + horizontalLine);
+                texts[totalTexts] = line;
+                totalTexts++;
+                line = in.nextLine();
+            }
         }
         System.out.println(horizontalLine + "Bye! Hope to see you again soon!" + horizontalLine);
     }
