4e2d99fd21a2c1df908ac812da49a4927b264706
fxe025 <e0968867@u.nus.edu>
2024-01-29 13:40:31 +0800

    Add increment: Level-1. Update Level-0.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
 public class Duke {
+
+    public static void printText(String textLine) {
+        System.out.println(textLine);
+    }
+
+    public static void echo() {
+        String horizon = "____________________________"
+                + "________________________________\n";
+        while (true) {
+            Scanner userInput = new Scanner(System.in);
+            String text = userInput.next();
+            if (text.equals("bye")) {
+                break;
+            }
+            printText(horizon + text);
+            printText(horizon);
+        }
+    }
     public static void main(String[] args) {
         String horizon = "____________________________"
                 + "________________________________\n";
         String botName = "Battch";
-        System.out.println(horizon + "Hello! I'm " + botName);
-        System.out.println("What can I do for you?");
-        System.out.println(horizon + "Bye. Hope to see you again soon!");
-        System.out.print(horizon);
+        printText(horizon + "Hello! I'm " + botName);
+        printText("What can I do for you?");
+        printText(horizon);
+
+        echo();
+
+        printText(horizon + "Bye. Hope to see you again soon!");
+        printText(horizon);
+
+
     }
 }
