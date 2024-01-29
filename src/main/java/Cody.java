1099ffe892e10b1b7539c54ab1f1184ffa81b37b
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-01-29 16:08:37 +0800

    Implement add to task list
    * Implement createList method that adds tasks given by the user into a list. Method prints the list when user gives the command 'list' and exits when the user gives the command 'bye'.
    * Edit greet message to prompt user to give Cody tasks to add to the list

--- src/main/java/Cody.java
+++ src/main/java/Cody.java
@@ -13,25 +13,47 @@ public class Cody {
         }
     }
 
+    private static void createList() {
+        String[] list = new String[100];
+        int listSize = 0;
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while (!input.equals("bye")) {
+            if (input.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < listSize; i++) {
+                    System.out.println(i + 1 + ". " + list[i]);
+                }
+                System.out.println("_____________________________________________________________");
+            } else {
+                list[listSize] = input;
+                listSize++;
+                System.out.println("______________________________________________________________\n" +
+                        " added: " + input + "\n" +
+                        "_____________________________________________________________");
+            }
+            input = in.nextLine();
+        }
+    }
+
     private static void greet() {
         String greet = " ____________________________________________________________\n" +
                 " Hello! I'm Cody\n" +
-                " What can I do for you?\n" +
+                " Tell me your tasks and I will create a task list for you\n" +
                 "_____________________________________________________________\n";
         System.out.println(greet);
     }
 
     private static void exit() {
-        String exit = "____________________________________________________________\n" +
+        String exit = "_____________________________________________________________\n" +
                 " Bye. Hope to see you again soon!\n" +
                 "_____________________________________________________________";
         System.out.println(exit);
     }
 
-
     public static void main(String[] args) {
         greet();
-        echo();
+        createList();
         exit();
     }
 }
