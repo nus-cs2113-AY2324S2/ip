3e7da8da5551afaac409dfbfa51c71e730088240
YHWong20 <yihao.wong@u.nus.edu>
2024-01-29 15:39:09 +0800

    Add input retrieval logic

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         Ui userInterface = new Ui();
-        userInterface.generateUi();
+        userInterface.printLogo();
+        userInterface.generateWelcome();
+
+        Scanner sc = new Scanner(System.in);
+        boolean hasMoreInput = true;
+
+        do {
+            String userCommand = sc.next();
+
+            if (userCommand.equals("bye")) {
+                hasMoreInput = false;
+            } else {
+                userInterface.echoCommand(userCommand);
+            }
+
+        } while (hasMoreInput);
+
+        userInterface.generateExit();
     }
 }
