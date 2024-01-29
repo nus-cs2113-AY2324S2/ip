9d348974b7d11ae05595326c77b11b9ebef97524
Mmaxx15 <leongxwmax@gmail.com>
2024-01-29 22:49:02 +0800

    Added ability to list tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -9,10 +9,28 @@ public class Task {
         tasklist = new String[100];
         taskcount = 0;
     }
+
+    /**
+     * Adds user input into taskList
+     */
     public void addTask(String task) {
         tasklist[taskcount] = task;
         taskcount += 1;
         System.out.println("added: " + task);
+    }
+    /**
+     * list out current tasks
+     */
+    public void listTask() {
+        if (taskcount == 0) {
+            System.out.println("Dobby has no tasks :(");
+            return;
+        }
+        System.out.println("List\n~~~~~~~~~~~~~~~~");
+        for (int i = 0 ; i < taskcount ; i += 1) {
+            System.out.println("  " + (i+1) + ". " + tasklist[i]);
+        }
+        System.out.println("~~~~~~~~~~~~~~~~");
     }
 
     public void userCommand() {
@@ -23,9 +41,11 @@ public class Task {
             if (command.toLowerCase().equals("bye")) {  //exit dobby
                 System.out.println("~~~~~~~~~~~~~~~~\nDobby say's BYE!");
                 break;
+            } else if (command.toLowerCase().equals("list")) {
+                listTask();
+            } else {
+                addTask(command);
             }
-            addTask(command);
-            //System.out.println("~~~~~~~~~~~~~~~~\n" + command);
         }
     }
 
