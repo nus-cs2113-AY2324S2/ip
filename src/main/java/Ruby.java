e03af5073e47b5c4a38c7c6863b736d98e57ee30
JiahaoGan <u3585924@connect.hku.hk>
2024-01-29 16:14:51 +0800

    Add echo function

--- src/main/java/Ruby.java
+++ src/main/java/Ruby.java
@@ -1,15 +1,39 @@
+import java.util.Scanner;
+
 public class Ruby {
     public static void main(String[] args) {
         greet();
+
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
+
+        while (!userInput.toLowerCase().equals("bye")){
+            print(userInput);
+            userInput = in.nextLine();
+        }
         exit();
     }
 
     private static void greet(){
-        System.out.println("Hi, I am here. Greets from Ruby.");
-        System.out.println("What can I do for you?");
+        print(new String[]{"Hi, I am here. Greets from Ruby.", "What can I do for you?"});
     }
 
     private static void exit(){
-        System.out.println("Bye. Always feels good work with you.");
+        print("Bye. Always feels good work with you.");
+    }
+
+    private static void print(String thingToPrint){
+        System.out.println("    " + "--------------");
+        System.out.println("    " + thingToPrint);
+        System.out.println("    " + "--------------");
+    }
+
+    private static void print(String[] thingsToPrint){
+        System.out.println("    " + "--------------");
+        for (String thing: thingsToPrint){
+            System.out.println("    " + thing);
+        }
+        System.out.println("    " + "--------------");
     }
 }
