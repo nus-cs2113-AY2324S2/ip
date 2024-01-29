16414ed9a50513b368fb1f621ee6404d669d1c27
nur-haziq <nhaziq01@gmail.com>
2024-01-30 01:57:36 +0800

    Add greet and exit methods
    The 'greet' and 'exit' output prints are now wrapped in a method
    called greet() and exit() respectively.

--- src/main/java/Mona.java
+++ src/main/java/Mona.java
@@ -1,13 +1,6 @@
 public class Mona {
-    public static void main(String[] args) {
-        String logo = "___  ___                  \n"
-                + "|  \\/  |                  \n"
-                + "| .  . | ___  _ __   __ _ \n"
-                + "| |\\/| |/ _ \\| '_ \\ / _` |\n"
-                + "| |  | | (_) | | | | (_| |\n"
-                + "\\_|  |_/\\___/|_| |_|\\__,_|\n";
-        System.out.println("Hello from\n" + logo);
 
+    public static void greet() {
         for (int i = 0; i < 59; i++) {
             System.out.print("_");
         }
@@ -20,6 +13,13 @@ public class Mona {
             System.out.print("_");
         }
         System.out.println("_");
+    }
+
+    public static void exit() {
+        for (int i = 0; i < 59; i++) {
+            System.out.print("_");
+        }
+        System.out.println("_");
 
         System.out.println("Bye. Hope to see you again soon!");
 
@@ -27,5 +27,18 @@ public class Mona {
             System.out.print("_");
         }
         System.out.println("_");
+    }
+    public static void main(String[] args) {
+        String logo = "___  ___                  \n"
+                + "|  \\/  |                  \n"
+                + "| .  . | ___  _ __   __ _ \n"
+                + "| |\\/| |/ _ \\| '_ \\ / _` |\n"
+                + "| |  | | (_) | | | | (_| |\n"
+                + "\\_|  |_/\\___/|_| |_|\\__,_|\n";
+        System.out.println("Hello from\n" + logo);
+
+        greet();
+
+        exit();
     }
 }
