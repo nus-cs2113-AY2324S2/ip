2dcb15ca36714f187ed51cf3a38ef2c3e7c44d3d
JiahaoGan <u3585924@connect.hku.hk>
2024-01-29 15:47:07 +0800

    Rename Duke.java to Ruby.java

--- src/main/java/Ruby.java
+++ src/main/java/Ruby.java
@@ -1,11 +1,5 @@
-public class Duke {
+public class Ruby {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
-//        System.out.println("Hello from\n" + logo);
         greet();
         exit();
     }
