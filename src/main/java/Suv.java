c8b5c610b045e9ffc3ab7e0bfcdb8faa7cd911a8
SuveenE <suveen.te1@gmail.com>
2024-01-29 19:11:03 +0800

    Add echo and exit functionalities

--- src/main/java/Suv.java
+++ src/main/java/Suv.java
@@ -1,11 +1,22 @@
+import java.util.Scanner;
+
 public class Suv {
     public static void main(String[] args) {
         String name = "Suv";
-        System.out.println("____________________________________________________________\n" +
-                " Hello! I'm " + name + "\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            String input = in.nextLine();
+            if(!input.equals("bye")){
+                System.out.println("____________________________________________________________\n" +
+                        input +
+                        "\n____________________________________________________________\n");
+            }else {
+                System.out.println( "____________________________________________________________\n" +
+                        " Bye. Hope to see you again soon!\n" +
+                        "____________________________________________________________\n");
+                break;
+            }
+        }
     }
 }
