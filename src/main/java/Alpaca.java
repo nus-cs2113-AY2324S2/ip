5cf7242fb5df7d52d549808cd996402562432a9b
Li_Kunjun <StargaerX0>
2024-01-29 20:58:03 +0800

    Level 1. Echo

--- src/main/java/Alpaca.java
+++ src/main/java/Alpaca.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Alpaca {
         private static final String LOGO = "           _      _____        _____          \n" +
                 "     /\\   | |    |  __ \\ /\\   / ____|   /\\    \n" +
@@ -11,13 +13,26 @@ public class Alpaca {
         private static final String HORIZONTAL_LINE =  "_____________" +
                 "_______________________________________________\n";
 
-        private static final String GREET = "Hello! I'm Alpaca\n"
-                + "What can I do for you?\n";
-        private static final String EXIT = "Bye. Hope to see you again soon!\n";
+        private static final String GREET = "Baa-baa-baa, I'm Alpaca!\n"
+                + "How can I assist you today?\n";
+        private static final String EXIT = "Bye. Hope to see you again soon, baa-baa-baa\n";
 
         public static void main(String[] args) {
                 System.out.print(LOGO);
                 System.out.print(HORIZONTAL_LINE + GREET + HORIZONTAL_LINE);
+
+                String line;
+                Scanner in = new Scanner(System.in);
+                line = in.nextLine();
+
+                while (!line.equals("bye")) {
+                        System.out.print(HORIZONTAL_LINE);
+                        System.out.println(line);
+                        System.out.print(HORIZONTAL_LINE);
+                        line = in.nextLine();
+                }
+
+                System.out.print(HORIZONTAL_LINE);
                 System.out.print(EXIT + HORIZONTAL_LINE);
         }
 }
