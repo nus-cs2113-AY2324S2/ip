5e9e07c279f2cb10abc2e16294a7b4382d084e1d
yzhanglp <yzhanglp@connect.ust.hk>
2024-01-30 00:16:04 +0800

    Finish the implementation of Level_3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -17,20 +17,49 @@ public class Duke {
         String instruction = " ";
         String[] task_list = new String[100];
         int number_of_task = 0;
+        Integer[] done_or_not = new Integer[100];
         while(!instruction.equals("bye")) //main loop of the chat bot
         {
             instruction = in.nextLine();
+            String[] split_instruction = instruction.split(" ");
+
             if(instruction.equals("list")) //list method
             {
                 int order = 1;
                 for(String element:task_list){
                     if(element==null)
                         break;
-                    System.out.println("\t"+String.valueOf(order)+". "+ element);
+                    if(done_or_not[order-1]==0)
+                        System.out.println("\t"+String.valueOf(order)+". "+ "[ ] "+element);
+                    else
+                        System.out.println("\t"+String.valueOf(order)+". "+"[X] " + element);
                     order++;
                 }
                 continue;
             }
+
+
+            if(split_instruction[0].equals("mark")) //mark method
+            {
+                int mark_number = Integer.parseInt(split_instruction[1]);
+                mark_number--; //fit with start with 1
+                done_or_not[mark_number] = 1;
+                System.out.println(prefix+"Nice! I've marked this task as done:");
+                System.out.println("\t"+"[X] "+task_list[mark_number]);
+                continue;
+            }
+
+            if(split_instruction[0].equals("unmark")) //mark method
+            {
+                int mark_number = Integer.parseInt(split_instruction[1]);
+                mark_number--;
+                done_or_not[mark_number] = 0;
+                System.out.println(prefix+"OK, I've marked this task as not done yet:");
+                System.out.println("\t"+"[ ] "+task_list[mark_number]);
+                continue;
+            }
+
+
             if(Arrays.asList(task_list).contains(instruction))  //add the task if not exist
             {
                 System.out.println(prefix+"The task "+ instruction + " already exist!");
@@ -40,6 +69,7 @@ public class Duke {
             {
                 System.out.println(prefix+"added: "+instruction+"\n");
                 task_list[number_of_task] = instruction;
+                done_or_not[number_of_task] = 0;
                 number_of_task++;
             }
 
