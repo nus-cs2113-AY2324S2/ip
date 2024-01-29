98356414fe6df256f6f2b9a3c4f81bf97f1a4d78
Ningsong Shen <ningsongshen123@outlook.com>
2024-01-29 17:16:27 +0800

    Add ability to mark tasks as done and not done via a task class

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -2,8 +2,10 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-
 public class Bob {
+    // todo: better indexing for the list of items
+    // todo: better display of statusicon
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                     + "|  _ \\_____ _|__ _\n"
@@ -17,7 +19,7 @@ public class Bob {
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
-        List<String> list = new ArrayList<>();
+        List<Task> list = new ArrayList<>();
 
         while (true) {
             String line;
@@ -27,23 +29,43 @@ public class Bob {
 
             if (line.equals("list")) {
                 System.out.println("____________________________________________________________");
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < list.size(); i++) {
-                    System.out.println((i+1) + ". " + list.get(i));
+                    System.out.println((i+1) + "." + list.get(i).getListItem());
                 }
 
                 System.out.println("____________________________________________________________");
 
-                continue;
+
+            } else if (line.startsWith("mark")) {
+                String[] lineSplit = line.split(" ");
+                list.get(Integer.parseInt(lineSplit[1]) - 1).setDone(true);
+                System.out.println("____________________________________________________________");
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println(list.get(Integer.parseInt(lineSplit[1]) - 1).getListItem());
+                System.out.println("____________________________________________________________");
+
+
+            } else if (line.startsWith("unmark")) {
+                String[] lineSplit = line.split(" ");
+                list.get(Integer.parseInt(lineSplit[1]) - 1).setDone(false);
+                System.out.println("____________________________________________________________");
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(list.get(Integer.parseInt(lineSplit[1]) - 1).getListItem());
+                System.out.println("____________________________________________________________");
 
             } else if (line.equals("bye")) {
                 break;
+            } else {
+                Task t = new Task(line);
+                list.add(t);
+
+                System.out.println("____________________________________________________________");
+                System.out.println("added: " + t.getDescription());
+                System.out.println("____________________________________________________________\n");
             }
 
-            list.add(line);
 
-            System.out.println("____________________________________________________________");
-            System.out.println("added: " + line);
-            System.out.println("____________________________________________________________\n");
         }
 
 
