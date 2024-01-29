507f53a5192ed56beb0a190bd494dbaf8573ff07
pqienso <buckslimyuan@gmail.com>
2024-01-29 14:00:41 +0800

    Add list functionality

--- src/main/java/Bean.java
+++ src/main/java/Bean.java
@@ -1,18 +1,40 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Bean {
+    public static void printList(String[] list){
+        int numItems = 1;
+
+        for(String item : list){
+            System.out.println(numItems + ": " + item);
+            numItems += 1;
+        }
+    }
+
     public static void main(String[] args) {
         String separator = "--------------------------------------";
         System.out.println("Hello! I'm Bean.\nWhat can I do for you?");
         System.out.println(separator);
+
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
+
+        String[] list = new String[100];
+        int numItems = 0;
         while(!line.equals("bye")) {
-            System.out.println("    " + line);
-            System.out.println(separator);
+            if(line.equals("list")){
+                printList(Arrays.copyOf(list, numItems));
+            }
+            else {
+                System.out.println("    added: " + line);
+                System.out.println(separator);
+                list[numItems] = line;
+                numItems += 1;
+            }
             line = in.nextLine();
         }
+
         System.out.println("Bean will take a nap now. Bye!");
     }
 }
