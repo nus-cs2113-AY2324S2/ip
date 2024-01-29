c7dc7e35bac55e095b9b158d12d9d96616df5dbd
pqienso <buckslimyuan@gmail.com>
2024-01-29 11:31:58 +0800

    Add echo functionality

--- src/main/java/Bean.java
+++ src/main/java/Bean.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Bean {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Bean.\n" +
-                "What can I do for you?\n");
-        System.out.println("Bye. Hope to see you again soon!");
+        String separator = "--------------------------------------";
+        System.out.println("Hello! I'm Bean.\nWhat can I do for you?");
+        System.out.println(separator);
+        String line;
+        Scanner in = new Scanner(System.in);
+        line = in.nextLine();
+        while(!line.equals("bye")) {
+            System.out.println("    " + line);
+            System.out.println(separator);
+            line = in.nextLine();
+        }
+        System.out.println("Bean will take a nap now. Bye!");
     }
 }
-
-
