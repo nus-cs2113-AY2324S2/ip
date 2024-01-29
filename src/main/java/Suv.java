47fc254b6e9265b449c9e4adf8705c356fb5d022
SuveenE <suveen.te1@gmail.com>
2024-01-29 19:58:17 +0800

    Add tasks list to store and retrieve text

--- src/main/java/Suv.java
+++ src/main/java/Suv.java
@@ -4,6 +4,8 @@ public class Suv {
     public static void main(String[] args) {
         String name = "Suv";
         Scanner in = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskIndex = 0;
 
         System.out.println("____________________________________________________________\n" +
                 " Hello! I'm " + name + "\n" +
@@ -12,15 +14,23 @@ public class Suv {
 
         while(true) {
             String input = in.nextLine();
-            if(!input.equals("bye")){
-                System.out.println("____________________________________________________________\n" +
-                        input +
-                        "\n____________________________________________________________\n");
-            }else {
+            if(input.equals("bye")){
                 System.out.println( "____________________________________________________________\n" +
                         " Bye. Hope to see you again soon!\n" +
                         "____________________________________________________________\n");
                 break;
+            }else if (input.equals("list")){
+                System.out.println("____________________________________________________________");
+                for(int i = 0; i < taskIndex; i++){
+                    int index = i + 1;
+                    System.out.println(" " + index + ". " + tasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                tasks[taskIndex++] = input;
+                System.out.println("____________________________________________________________\n" +
+                        " added: " + input +
+                        "\n____________________________________________________________\n");
             }
         }
     }
