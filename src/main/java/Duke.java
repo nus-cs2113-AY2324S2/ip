1ac1ee49b0fd328f599b51693354f773f95eea74
fxe025 <e0968867@u.nus.edu>
2024-01-29 14:33:07 +0800

    Add increment: Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,42 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class Duke {
 
     public static void printText(String textLine) {
         System.out.println(textLine);
     }
 
-    public static void echo() {
+    public static void printTask(String[] tasks) {
+        int index = 1;
+        for (String task : tasks) {
+            String indexPrinted = String.valueOf(index) + ". ";
+            printText(indexPrinted + task);
+            index++;
+        }
+    }
+
+    public static void addTask(String[] tasks) {
         String horizon = "____________________________"
-                + "________________________________\n";
+                + "________________________________";
+        int index = 0;
         while (true) {
             Scanner userInput = new Scanner(System.in);
-            String text = userInput.next();
+            String text = userInput.nextLine();
             if (text.equals("bye")) {
                 break;
             }
-            printText(horizon + text);
-            printText(horizon);
+            else if (text.equals("list")) {
+                printText(horizon);
+                printTask(Arrays.copyOf(tasks, index));
+                printText(horizon + "\n");
+            }
+            else {
+                printText(horizon);
+                tasks[index] = text;
+                index++;
+                printText("added: " + text);
+                printText(horizon + "\n");
+            }
         }
     }
     public static void main(String[] args) {
@@ -26,7 +47,9 @@ public class Duke {
         printText("What can I do for you?");
         printText(horizon);
 
-        echo();
+        String[] tasks = new String[100];
+
+        addTask(tasks);
 
         printText(horizon + "Bye. Hope to see you again soon!");
         printText(horizon);
