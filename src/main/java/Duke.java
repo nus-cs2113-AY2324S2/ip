31dd6792814f2b67af01c6a672d186f7ab183c90
yzhanglp <yzhanglp@connect.ust.hk>
2024-01-29 23:43:37 +0800

    Finished the implementation of Level_2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class Duke {
     public static void main(String[] args) {
@@ -8,16 +9,43 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String name ="Altria";
+        String prefix = "\n\t";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello, I'm "+name);
         System.out.println("What can I do for you?");
         Scanner in = new Scanner(System.in);
-        String instruction = in.nextLine();
+        String instruction = " ";
+        String[] task_list = new String[100];
+        int number_of_task = 0;
         while(!instruction.equals("bye")) //main loop of the chat bot
         {
-            System.out.println("\n"+"\t"+instruction+"\n");
             instruction = in.nextLine();
+            if(instruction.equals("list")) //list method
+            {
+                int order = 1;
+                for(String element:task_list){
+                    if(element==null)
+                        break;
+                    System.out.println("\t"+String.valueOf(order)+". "+ element);
+                    order++;
+                }
+                continue;
+            }
+            System.out.println(Arrays.asList(task_list).contains(instruction));
+            if(Arrays.asList(task_list).contains(instruction))  //add the task if not exist
+            {
+                System.out.println(prefix+"The task "+ instruction + " already exist!");
+                continue;
+            }
+            else
+            {
+                System.out.println(prefix+"added: "+instruction+"\n");
+                task_list[number_of_task] = instruction;
+                number_of_task++;
+            }
+
+
         }
-        System.out.println("\n"+"\t"+"Bye.Hope to see you again soon!");
+        System.out.println(prefix+"Bye.Hope to see you again soon!");
     }
 }
