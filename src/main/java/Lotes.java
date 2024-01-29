51e2441d2fc896052805f60c9e7a9691d68e402a
E0958902 <E0958902@u.nus.edu>
2024-01-29 09:46:07 +0800

    Add store and display of user input

--- src/main/java/Lotes.java
+++ src/main/java/Lotes.java
@@ -1,19 +1,43 @@
 import java.util.Scanner;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 public class Lotes {
+    public static String lineSeparator = System.lineSeparator();
+    public static String underscore = "    ____________________________________________________________";
+    public static void printUserList(List<String> list){
+        //System.out.println("List: " + list.get(0));
+        System.out.println(underscore);
+        if(list.isEmpty()){
+            System.out.println("    List is empty, please enter some text to add to list.");
+        }
+        int wordCount = 0;
+        for(String items : list){
+            wordCount++;
+            System.out.print(wordCount + ". " + items + lineSeparator);
+        }
+        System.out.println(underscore);
+    }
     public static void main(String[] args) {
-        String underscore = "    ____________________________________________________________\n";
-        System.out.println(underscore + "    Hello! I'm Lotes\n" + "    What can I do for you?\n" + underscore);
-
+        System.out.println(underscore + lineSeparator + "    Hello! I'm Lotes" + lineSeparator +
+                "    What can I do for you?" + lineSeparator + underscore);
         String userInput;
+
+        List<String> list = new ArrayList<>();
+
         while(true){
             Scanner in = new Scanner(System.in);
             userInput = in.nextLine();
             if(userInput.equals("bye")){
-                System.out.println(underscore + "    Bye. Hope to see you again soon!\n" + underscore);
+                System.out.println(underscore + lineSeparator
+                        + "    Bye. Hope to see you again soon!\n" + underscore);
                 break;
+            }else if(userInput.equals("list")){
+                printUserList(list);
+            }else{
+                list.add(userInput);
+                System.out.print(underscore + lineSeparator + "     added: " + userInput
+                        + lineSeparator + underscore + lineSeparator);
             }
-            System.out.print(underscore + "     " + userInput + "\n" + underscore);
         }
     }
 }
