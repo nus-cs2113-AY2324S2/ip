6ab706a3a5d65196225ecec93b82686951dd284c
sxfoo <fsx159@gmail.com>
2024-01-29 20:53:28 +0800

    Add functionality to receive userInput and echo
    Level-1 Echo functionality - echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = "   _____                     _   \n" +
@@ -10,6 +12,7 @@ public class Duke {
 
         // Chatbot functionality
         greetUser();
+        handleUserInput();
         sayGoodbye();
     }
 
@@ -19,6 +22,26 @@ public class Duke {
         System.out.println(messageDivider);
         System.out.println("Hello! I'm Carrot!");
         System.out.println("What can I do for you?");
+        System.out.println(messageDivider);
+    }
+
+    private static void handleUserInput() {
+        // Scanner object for userInput
+        Scanner in = new Scanner(System.in);
+        String userInput;
+
+        while (true) {
+            userInput = in.nextLine();
+            if (userInput.toLowerCase().equals("bye")) {
+                break;
+            }
+            echoUserCommand(userInput);
+        }
+    }
+
+    private static void echoUserCommand(String userInput) {
+        System.out.println(messageDivider);
+        System.out.println(userInput);
         System.out.println(messageDivider);
     }
 
