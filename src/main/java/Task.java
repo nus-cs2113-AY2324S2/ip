9ce9c7c98ce9763018d1b30104eab3c44a8b76bf
LowTL <e0957642@u.nus.edu>
2024-01-29 15:27:12 +0800

    Create Task.java

---
+++ src/main/java/Task.java
@@ -0,0 +1,9 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String desc) {
+        this.description = desc;
+        this.isDone = false;
+    }
+}
