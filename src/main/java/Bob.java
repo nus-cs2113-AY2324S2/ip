d72d78b37dfaa263248f839b18185b239ad6b8e4
Ningsong Shen <ningsongshen123@outlook.com>
2024-01-29 16:40:18 +0800

    Improve the skeletal version of Bob to echo commands and exit with 'bye'

--- src/main/java/Bob.java
+++ src/main/java/Bob.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+
+
 public class Bob {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,9 +10,32 @@ public class Bob {
                     + "|____/ \\__,_|_\\__/\n";
         System.out.println("Hello from\n" + logo);
 
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Bob");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+
+
+        while (true) {
+            String line;
+            Scanner in = new Scanner(System.in);
+
+            line = in.nextLine();
+
+            if (line.equals("bye")) {
+                break;
+            }
+
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________\n");
+        }
+
+
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
 
     }
 }
