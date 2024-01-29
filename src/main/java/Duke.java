12880cf1d802c5c2b4c545cd40d05951f8598265
B1G-SAM <54065426+B1G-SAM@users.noreply.github.com>
2024-01-30 01:00:34 +0800

    add Increment 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! My name is Jeff. What can I do for you");
-        System.out.println("Bye. Hope to see you aghain soon!");
+        System.out.println("Hello! My name is Jeff.");
+        System.out.println("What can I do for you?");
+        boolean stillUsing = true;
+        while (stillUsing){
+            String line;
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (line.equals("bye") || line.equals("Bye")){
+                stillUsing = false;
+            }
+            else{
+                System.out.println(line);
+            }
+        }
 
-        
+        System.out.println("Bye. Hope to see you again soon!");
+
+
     }
 }
