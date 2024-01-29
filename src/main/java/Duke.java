1ba8d573de19d153bb4dd5389ad44f436f2621c5
nickczh <nickchua@u.nus.edu>
2024-01-29 22:14:56 +0800

    Complete Level-2 (Add, List)
    Let's,
    * Create a fixed size array ( String[100] ) to store the items.
    * Store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,7 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
@@ -18,18 +19,29 @@ public class Duke {
         System.out.println("What can I do for you?\n");
 
         Scanner in = new Scanner(System.in);
+        String[] userCommands = new String[100];
         String command;
+        int count = 0;
 
         while (true) {
             command = in.nextLine();
             if (command.equals("bye")) {
                 break;
             }
+            if (command.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < count; i++) {
+                    System.out.println("\t" + Integer.toString(i + 1) + ". " + userCommands[i]);
+                }
+                System.out.println("____________________________________________________________");
+                continue;
+            }
+            userCommands[count] = command;
+            count++;
             System.out.println("____________________________________________________________");
-            System.out.println("\t" + command);
+            System.out.println("\t" + "added: " + command);
             System.out.println("____________________________________________________________");
         }
-
         System.out.println("____________________________________________________________");
         System.out.println("\t" + "Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
