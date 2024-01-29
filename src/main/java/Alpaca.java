6ea8a156767783e449a914ed500ba78a2ce0d211
Li_Kunjun <StargaerX0>
2024-01-29 21:31:54 +0800

    Level 2. Add, List

--- src/main/java/Alpaca.java
+++ src/main/java/Alpaca.java
@@ -15,20 +15,36 @@ public class Alpaca {
 
         private static final String GREET = "Baa-baa-baa, I'm Alpaca!\n"
                 + "How can I assist you today?\n";
-        private static final String EXIT = "Bye. Hope to see you again soon, baa-baa-baa\n";
+        private static final String EXIT = "Bye. Hope to see you again soon, baa-baa-baa!\n";
 
         public static void main(String[] args) {
                 System.out.print(LOGO);
                 System.out.print(HORIZONTAL_LINE + GREET + HORIZONTAL_LINE);
+
+                String[] list = new String[100];
+                int listCount = 0;
 
                 String line;
                 Scanner in = new Scanner(System.in);
                 line = in.nextLine();
 
                 while (!line.equals("bye")) {
-                        System.out.print(HORIZONTAL_LINE);
-                        System.out.println(line);
-                        System.out.print(HORIZONTAL_LINE);
+                        switch (line) {
+                        case "list":
+                                System.out.print(HORIZONTAL_LINE);
+                                for (int i = 0; i < listCount; i++) {
+                                        System.out.println((i + 1) + ". " + list[i]);
+                                }
+                                System.out.print(HORIZONTAL_LINE);
+                                break;
+                        default:
+                                System.out.print(HORIZONTAL_LINE);
+                                System.out.print("added: ");
+                                System.out.println(line);
+                                System.out.print(HORIZONTAL_LINE);
+                                list[listCount] = line;
+                        }
+                        listCount++;
                         line = in.nextLine();
                 }
 
