6233d62259e2400c2f1ca89e694dfd5ba2411ef1
LowTL <e0957642@u.nus.edu>
2024-01-29 15:02:40 +0800

    Add echo to Katleen.java

--- src/main/java/Katleen.java
+++ src/main/java/Katleen.java
@@ -1,14 +1,28 @@
+import java.util.Scanner;
 public class Katleen {
     public static final String line = "____________________________________________________________";
 
+public static void echo(String text) {
+    System.out.println(line);
+    System.out.println(text);
+    System.out.println(line);
+}
     public static void main(String[] args) {
-
         System.out.println(line);
         System.out.println("Hello! I'm Katleen.");
-        System.out.println("What can I do for you today?");
+        System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
+
+        Scanner in = new Scanner(System.in);
+        String text = in.nextLine();
+        while (!text.equals("bye")) {
+            echo(text);
+            text = in.nextLine();
+        }
         System.out.println(line);
+        System.out.println("Bye, see you again!");
+        System.out.println(line);
+
         return;
     }
 }
