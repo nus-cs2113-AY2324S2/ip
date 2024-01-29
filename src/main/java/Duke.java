fa645e081ab37abc5f7000dbc23daf155ddfeec8
nickczh <nickchua@u.nus.edu>
2024-01-29 21:20:49 +0800

    Complete Level-1 (Echo)
    Let's,
    
    * Add a file containing ASCII content of name to the root of the
      project directory for uniqueness and customization.
    
    * Improve skeletal version of Duke so that it echos commands entered by the user, and exits when the user types the command bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,37 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String name = "Nick";
+        try {
+            String name = new String(Files.readAllBytes(Paths.get("name.txt")));
+            System.out.print(name);
+        }
+        catch (IOException ex) {
+            System.out.println(ex);
+        }
+
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+        System.out.println("Welcome to the Ultimate Nick Bot!");
+        System.out.println("What can I do for you?\n");
+
+        Scanner in = new Scanner(System.in);
+        String command;
+
+        while (true) {
+            command = in.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println("\t" + command);
+            System.out.println("____________________________________________________________");
+        }
+
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("\t" + "Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
 }
