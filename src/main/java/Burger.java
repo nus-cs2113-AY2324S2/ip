7ba95cef0522411ed9100451e1e4c46a91487b0a
BenjoBurger <benjaminggr@gmail.com>
2024-01-31 00:12:59 +0800

    Add printLine, echo and goodbye functions

--- src/main/java/Burger.java
+++ src/main/java/Burger.java
@@ -1,10 +1,39 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class Burger {
+    static final String CHATBOT_NAME = "Burger";
+
     public static void main(String[] args) {
-        System.out.println("---------------------------------");
-        System.out.println("Hello! I'm Burger");
+        printLine();
+        System.out.println("Hello! I'm " + CHATBOT_NAME);
         System.out.println("What can I do for you?");
-        System.out.println("---------------------------------");
-        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+        Scanner input = new Scanner(System.in);
+        while (true) {
+            String text = input.nextLine();
+            if (text.equalsIgnoreCase("bye")) {
+                break;
+            }
+            echo(text);
+        }
+        goodbye();
+    }
+
+    public static void printLine() {
         System.out.println("---------------------------------");
     }
+
+    public static void echo(String text) {
+        printLine();
+        System.out.println(text);
+        printLine();
+    }
+
+    public static void goodbye() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
+    }
+
 }
