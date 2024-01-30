82a87049690f5cc79ca007b225e685ac9036eebd
Tan Dexter <dextboy@outlook.com>
2024-01-30 13:46:28 +0800

    Refactor methods in Natsu Class for A-CodingStandard increment
    * Break down the echoCommands method into smaller, more manageable functions.
    * Ensure all methods in the Natsu class adhere to the 30 lines of code limit.

--- src/main/java/Natsu.java
+++ src/main/java/Natsu.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Natsu {
 
     private static final String NAME = "Natsu";
+    private static final Task[] list = new Task[100];
+    private static int taskCount = 0;
 
     private static void printGreeting() {
         printLine();
@@ -12,44 +14,59 @@ public class Natsu {
     }
 
     private static void echoCommands() {
-        Scanner input = new Scanner(System.in);
-        boolean isExit = false;
-        Task[] list = new Task[100];
-        int index = 0;
-        while (!isExit) {
-            String userInput = input.nextLine();
-            if (userInput.equals("bye")) {
-                input.close();
-                isExit = true;
-                printExitMessage();
-            } else if (userInput.equals("list")) {
-                printLine();
-                System.out.println("     Here are the tasks in your list:");
-                for (int i = 0; i < index; i++) {
-                    System.out.println("     " + (i + 1) + "." + list[i].toString());
-                }
-                printLine();
-            } else if (userInput.startsWith("mark ")) {
-                int itemIndex = Integer.parseInt(userInput.substring(5)) - 1;
-                list[itemIndex].markAsDone();
-                printLine();
-                System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       " + list[itemIndex].toString());
-                printLine();
-            } else if (userInput.startsWith("unmark ")) {
-                int itemIndex = Integer.parseInt(userInput.substring(7)) - 1;
-                list[itemIndex].markAsUndone();
-                printLine();
-                System.out.println("     OK, I've marked this task as not done yet:");
-                System.out.println("       " + list[itemIndex].toString());
-                printLine();
-            } else {
-                list[index++] = new Task(userInput);
-                printLine();
-                System.out.println("     " + userInput);
-                printLine();
+        try (Scanner input = new Scanner(System.in)) {
+            boolean isActive = true;
+            while (isActive) {
+                String userInput = input.nextLine();
+                isActive = processInput(userInput);
             }
         }
+    }
+
+    private static boolean processInput(String userInput) {
+        switch (userInput) {
+        case "bye":
+            printExitMessage();
+            return false;
+        case "list":
+            listTasks();
+            break;
+        default:
+            handleTaskCommands(userInput);
+        }
+        return true;
+    }
+
+    private static void handleTaskCommands(String userInput) {
+        if (userInput.startsWith("mark ")) {
+            int itemIndex = Integer.parseInt(userInput.substring(5)) - 1;
+            list[itemIndex].markAsDone();
+            printLine();
+            System.out.println("     Nice! I've marked this task as done:");
+            System.out.println("       " + list[itemIndex].toString());
+            printLine();
+        } else if (userInput.startsWith("unmark ")) {
+            int itemIndex = Integer.parseInt(userInput.substring(7)) - 1;
+            list[itemIndex].markAsUndone();
+            printLine();
+            System.out.println("     OK, I've marked this task as not done yet:");
+            System.out.println("       " + list[itemIndex].toString());
+            printLine();
+        } else {
+            list[taskCount++] = new Task(userInput);
+            printLine();
+            System.out.println("     " + userInput);
+            printLine();
+        }
+    }
+
+    private static void listTasks() {
+        printLine();
+        System.out.println("     Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("     " + (i + 1) + "." + list[i].toString());
+        }
+        printLine();
     }
 
     private static void printLine() {
