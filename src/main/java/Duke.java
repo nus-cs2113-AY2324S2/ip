e04458fbfd14178e05a20cb216e9d327a7969de3
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-01-30 21:59:19 +0800

    Added Level 2 of week 3.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,19 +5,30 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println(break_line + "\nHello! I'm Jeremy.\nWhat can I do for you?\n" + break_line);
 
+        String[] tasks = new String[100];
+        int count = 0;
+
         Scanner in = new Scanner(System.in);
         String line;
 
         do {
             line = in.nextLine();
+
             if (!line.equals("bye")) {
-                System.out.println(line + "\n" + break_line);
+                if (line.equals("list")) {
+                    // Print the list of tasks
+                    for (int i = 0; i < count; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                    System.out.println(break_line);
+                } else {
+                    tasks[count] = line;
+                    System.out.println("added: " + line + "\n" + break_line);
+                    count++;
+                }
             }
-        }
-        while (!line.equals("bye"));
+        } while (!line.equals("bye"));
 
         System.out.println("Bye. Hope to see you again soon!\n" + break_line);
-
-
-        }
     }
+}
