ed0edf8df4ffa68a2a43ada4fc4f709e22774748
wong quan wei alvin <wongquan.weialvin@gmail.com>
2024-01-30 21:35:11 +0800

    Level 1. Echo

--- src/main/java/Wongster.java
+++ src/main/java/Wongster.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
+
 public class Wongster {
     public static void main(String[] args) {
         String name = "Wongster";
-        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n\n" + "Bye. Hope to see  you again soon!");
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n");
+        Echo();
+    }
+
+    public static void Echo() {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String userInput = scanner.nextLine();
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see  you again soon!");
+                break;
+            }
+            System.out.println(userInput);
+        }
+        scanner.close();
     }
 }
-
-
-
