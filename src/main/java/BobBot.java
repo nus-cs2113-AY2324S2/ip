5e4936e91eceaa70b85f9ec49081d90765b8511f
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-01-30 12:50:22 +0800

    Add echo capabilities

--- src/main/java/BobBot.java
+++ src/main/java/BobBot.java
@@ -1,20 +1,45 @@
+import java.util.Scanner;
+
 public class BobBot {
 
-    public static void drawLine() {
-        System.out.println("__________________________________________");
+    public static void echoCommand(String lineString) {
+        drawLine(true);
+        System.out.println("\t" + lineString);
+        drawLine(true);
+        System.out.println();
+    }
+
+    public static void drawLine(Boolean includeIndentation) {
+        if (includeIndentation) {
+            System.out.print("\t");
+        }
+        else {
+            System.out.print("________");
+        }
+        System.out.println("__________________________________");
     }
 
     public static void greet() {
-        drawLine();
+        drawLine(false);
         System.out.println("Hello! I'm BobBot");
-        System.out.println("What can I do for you?");
-
-        drawLine();
-        System.out.println("Bye. Hope to see you again soon!");
-
-        drawLine();
+        System.out.println("Type in anything!");
+        drawLine(false);
     }
     public static void main(String[] args) {
         greet();
+
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        line = in.nextLine();
+        
+        while (!line.contains("bye")) {
+            echoCommand(line);
+            line = in.nextLine();
+        }
+
+        drawLine(true);
+        System.out.println("\tBye. Hope to see you again soon!");
+        drawLine(true);
     }
 }
