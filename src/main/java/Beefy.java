4473d22a4b5f6732f7f5c8b525ebd55d934d3ca0
OKW32 <e0969674@u.nus.edu>
2024-01-30 12:54:24 +0800

    Feature: Add echo user command

--- src/main/java/Beefy.java
+++ src/main/java/Beefy.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
 public class Beefy {
     private static final String BOTNAME = "BEEFY";
+    private Scanner userInput = new Scanner(System.in);
 
     /**
      * Prints a separation row of n "-", where n = WIDTH
@@ -13,14 +15,24 @@ public class Beefy {
     }
 
     public void startChat() {
+        String userLine;
         printSeparation();
-        System.out.println("Hello! I'm " + BOTNAME);
-        System.out.println("What can I do for you?");
+        System.out.println(BOTNAME + ": Hello! I'm " + BOTNAME);
+        System.out.println(BOTNAME + ": What can I do for you?");
         printSeparation();
+        do {
+            System.out.print("You: ");
+            userLine = userInput.nextLine();
+            printSeparation();
+            if (!userLine.equalsIgnoreCase("bye")) {
+                System.out.println(BOTNAME + ": " + userLine);
+                printSeparation();
+            }
+        } while(!userLine.equalsIgnoreCase("bye"));
     }
 
     public void endChat() {
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(BOTNAME + ": Bye. Hope to see you again soon!");
         printSeparation();
     }
 }
