319d4046c1c1a51c8567d05faa5b787e160a4352
syj_02 <>
2024-01-31 00:58:45 +0800

    refractor: Abstrac task list into another class Created TaskList to store all the tasks in a list and keep track of the total number of tasks added

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,20 @@
+public class TaskList {
+    protected Task[] list;
+    protected int count;
+
+    public TaskList() {
+        this(new Task[100], 0);
+    }
+
+    public TaskList(Task[] list, int count) {
+        this.list = list;
+        this.count = count;
+    }
+
+    public void addTask(Task task) {
+        this.list[count] = task;
+        this.count = count + 1;
+        task.index = count;
+    }
+
+}
