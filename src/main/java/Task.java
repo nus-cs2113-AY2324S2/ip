56b1b9c03eaa48fd091a9b752670e16fceb2bcb0
E0958902 <E0958902@u.nus.edu>
2024-01-30 23:53:21 +0800

    Task.java: update Task class to represent user tasks

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,17 @@
 public class Task {
+    protected String description;
+    protected boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        setDone(false);
+    }
+    public String getDescription() {
+        return description;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+    public void setDone(boolean done) {
+        isDone = done;
+    }
 }
