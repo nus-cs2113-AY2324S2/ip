4b23bc043ee26e314e99afb2b3d78f6aee5eb148
Joshuahoky <joshuaho2002@gmail.com>
2024-01-30 13:03:29 +0800

    Add functionality to display list of commands

--- src/main/java/Baron.java
+++ src/main/java/Baron.java
@@ -1,9 +1,22 @@
 import java.util.Scanner;
 
 public class Baron {
+    public static void displayCommandsList(String[] commandsList) {
+        int listNumber = 1;
+        for (String command: commandsList) {
+            if (command == null) {
+                break;
+            }
+            System.out.println(listNumber + ". " + command);
+            listNumber += 1;
+        }
+    }
+
     public static void main(String[] args) {
+        String[] commandsList = new String[100];
+        int numberOfCommands = 0;
         System.out.println("Hello! I'm Baron");
-        System.out.print("What can I do for you?\n");
+        System.out.println("What can I do for you?\n");
         while (true) {
             Scanner userInput = new Scanner(System.in);
             String input = userInput.nextLine();
@@ -11,7 +24,14 @@ public class Baron {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
             }
-            System.out.println(input + "\n");
+            else if (input.equalsIgnoreCase("list")) {
+                displayCommandsList(commandsList);
+                System.out.println("");
+                continue;
+            }
+            System.out.println("Added: " + input + "\n");
+            commandsList[numberOfCommands] = input;
+            numberOfCommands += 1;
         }
     }
 }
