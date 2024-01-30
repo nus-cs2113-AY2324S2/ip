393289fe53d228260c2e47b462cd8844d0b62071
hikohiko <duliyan@u.nus.edu>
2024-01-30 23:05:12 +0800

    no message

--- CONTRIBUTORS.md
+++ CONTRIBUTORS.md
@@ -1,8 +1,8 @@
 # Contributors
 
-Display | Name | Github Profile | Homepage
----|:---:|:---:|:---:
-![](https://avatars0.githubusercontent.com/u/22460123?s=100) | Jeffry Lum | [Github](https://github.com/j-lum/) | [Homepage](https://se.kasugano.moe)
+Display |        Name         | Github Profile | Homepage
+---|:-------------------:|:---:|:---:
+![](https://avatars0.githubusercontent.com/u/22460123?s=100) |     Hiko LiYan      | [Github](https://github.com/HikoHikoHiko) | [Homepage](https://github.com/HikoHikoHiko)
 ![](https://avatars0.githubusercontent.com/u/1673303?s=100) | Damith C. Rajapakse | [Github](https://github.com/damithc/) | [Homepage](https://www.comp.nus.edu.sg/~damithch/)
 # I would like to join this list. How can I help the project
 
---
+++ src/main/java/Hiko.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class Hiko {
+    public static void main(String[] args) {
+        String logo =
+
+
+        "  \n" +
+                " __ __  ____  __  _   ___  \n" +
+                "|  |  ||    ||  |/ ] /   \\ \n" +
+                "|  |  | |  | |  ' / |     |\n" +
+                "|  _  | |  | |    \\ |  O  |\n" +
+                "|  |  | |  | |     \\|     |\n" +
+                "|  |  | |  | |  .  ||     |\n" +
+                "|__|__||____||__|\\_| \\___/ \n" +
+                "                           \n";
+        System.out.println("Hello from \n" + logo);
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("What can I do for you?");
+        String input = scanner.nextLine();
+        System.out.println("Thank you and goodbye " );
+        scanner.close();
+
+    }
+}
