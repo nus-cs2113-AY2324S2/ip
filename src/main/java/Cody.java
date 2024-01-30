f4173027d33fa554431c70a08199ffc2ff8a128d
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-01-30 14:46:53 +0800

    Implement mark as done
    * Implemented Task class in Task.java. isDone attribute in Task class tracks the done status of the task.
    * markDone and markUndone methods in Cody class prints to the user the task that they want to mark and unmark.

--- src/main/java/Cody.java
+++ src/main/java/Cody.java
@@ -1,53 +1,89 @@
 import java.util.Scanner;
-
 public class Cody {
 
     private static void echo() {
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
         while (!input.equals("bye")) {
-            System.out.println("____________________________________________________________\n" +
-                    " " + input + "\n" +
-                    "_____________________________________________________________");
+            System.out.println("____________________________________________________________\n"
+                    + " " + input + "\n"
+                    + "_____________________________________________________________");
             input = in.nextLine();
         }
     }
 
     private static void createList() {
-        String[] list = new String[100];
-        int listSize = 0;
+        Task[] tasks = new Task[100];
+        int taskCount = 0;
+
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
+
         while (!input.equals("bye")) {
             if (input.equals("list")) {
-                System.out.println("____________________________________________________________");
-                for (int i = 0; i < listSize; i++) {
-                    System.out.println(i + 1 + ". " + list[i]);
-                }
-                System.out.println("_____________________________________________________________");
+                printList(tasks, taskCount);
+            } else if (input.startsWith("mark")) {
+                String[] words = input.split(" ");
+                int index = Integer.parseInt(words[1]) - 1;
+                tasks[index].markDone();
+                markDone(tasks, index);
+            } else if (input.startsWith("unmark")) {
+                String[] words = input.split(" ");
+                int index = Integer.parseInt(words[1]) - 1;
+                tasks[index].markUndone();
+                markUndone(tasks, index);
             } else {
-                list[listSize] = input;
-                listSize++;
-                System.out.println("______________________________________________________________\n" +
-                        " added: " + input + "\n" +
-                        "_____________________________________________________________");
+                Task task = new Task(input);
+                addTask(tasks, task, taskCount);
+                taskCount++;
             }
             input = in.nextLine();
         }
     }
 
+    public static void printList(Task[] tasks, int taskCount) {
+        System.out.println("______________________________________________________________\n"
+                + " Here are the tasks in your list:");
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println(" " + (i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
+        }
+        System.out.println("_____________________________________________________________");
+    }
+
+    public static void markDone(Task[] tasks, int index) {
+        System.out.println("______________________________________________________________\n"
+                + " Good job! I've marked this task as done:\n"
+                + " [X] " + tasks[index].getDescription() + "\n"
+                + "_____________________________________________________________");
+    }
+
+    public static void markUndone(Task[] tasks, int index) {
+        System.out.println("______________________________________________________________\n"
+                + " Okay! I've marked this task as not done yet:\n"
+                + " [ ] " + tasks[index].getDescription() + "\n"
+                + "_____________________________________________________________");
+    }
+
+    public static void addTask(Task[] tasks, Task task, int taskCount) {
+        tasks[taskCount] = task;
+        System.out.println("______________________________________________________________\n"
+                + " Got it. I've added this task to your list:\n"
+                + "   [ ] " + task.getDescription() + "\n"
+                + "_____________________________________________________________");
+    }
+
     private static void greet() {
-        String greet = " ____________________________________________________________\n" +
-                " Hello! I'm Cody\n" +
-                " Tell me your tasks and I will create a task list for you\n" +
-                "_____________________________________________________________\n";
+        String greet = " ____________________________________________________________\n"
+                + " Hello! I'm Cody\n"
+                + " Tell me your tasks and I will create a task list for you\n"
+                + "_____________________________________________________________\n";
         System.out.println(greet);
     }
 
     private static void exit() {
-        String exit = "_____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "_____________________________________________________________";
+        String exit = "_____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "_____________________________________________________________";
         System.out.println(exit);
     }
 
