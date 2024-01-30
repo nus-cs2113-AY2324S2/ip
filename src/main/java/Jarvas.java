4b24a9e5ff486300ef6bca3ce45a21234cc3dcb3
l5_z <e0958707@u.nus.edu>
2024-01-30 19:30:01 +0800

    Add Level-0: Rename, Greet, Exit

--- src/main/java/Jarvas.java
+++ src/main/java/Jarvas.java
@@ -1,10 +1,28 @@
 public class Jarvas {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+    public static void initialiseBot(){
+        String logo = " _____                                  \n" +
+                "(___  )                                 \n" +
+                "    | |   _ _  _ __  _   _    _ _   ___ \n" +
+                " _  | | /'_` )( '__)( ) ( ) /'_` )/',__)\n" +
+                "( )_| |( (_| || |   | \\_/ |( (_| |\\__, \\\n" +
+                "`\\___/'`\\__,_)(_)   `\\___/'`\\__,_)(____/";
         System.out.println("Hello from\n" + logo);
+        String greet = "____________________________________________________________\n"
+                + " Hello! I'm Jarvas\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(greet);
+
+    }
+    public static void terminateBot(){
+        String exit = "____________________________________________________________\n"
+                + " Bye! Hope to see you again soon!\n"
+                + "____________________________________________________________";
+        System.out.println(exit);
+    }
+
+    public static void main(String[] args) {
+        initialiseBot();
+        terminateBot();
     }
 }
