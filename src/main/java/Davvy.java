5117f8f31732399d06c135db8d55457e0ae6d88a
Daviancold <110613918+Daviancold@users.noreply.github.com>
2024-01-31 02:05:59 +0800

    Add echo feature
    A new method that allows user input to be returned back to the command
    line was added to help the user ensure the correct message is being
    received

--- src/main/java/Davvy.java
+++ src/main/java/Davvy.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
 public class Davvy {
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void printStatement(String statementType) {
+        printLine();
+        if (statementType.equals("greetings")) {
+            System.out.println(" Hello! I'm Davvy\n" + " What can I do for you?");
+        } else if (statementType.equals("goodbye")) {
+            System.out.println(" Bye. Hope to see you again soon!");
+        }
+        printLine();
+    }
+
+    private static void echoUserInput(String userInput) {
+        printLine();
+        System.out.println(" " + userInput);
+        printLine();
+    }
+
     public static void main(String[] args) {
-        String greetings = "____________________________________________________________\n" +
-                " Hello! I'm Davvy\n" +
-                " What can I do for you?\n" +
-                "____________________________________________________________\n" +
-                " Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n";
-        System.out.println(greetings);
+        printStatement("greetings");
+
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+
+        while (!userInput.equals("bye")) {
+            echoUserInput(userInput);
+            userInput = in.nextLine();
+        }
+
+        printStatement("goodbye");
     }
 }
