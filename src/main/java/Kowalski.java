79d34510a4ee694d3b894646d33773d0d785badb
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-01-30 22:38:09 +0800

    Add Echo Functionality

--- src/main/java/Kowalski.java
+++ src/main/java/Kowalski.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Kowalski {
     public static void main(String[] args){
-        System.out.println("Hello, I'm Kowalski! Reporting for duty! \n" +
-                "What can I do for you today, Skipper?" );
+        String dividingline = "____________________________________________________________";
+        System.out.println(dividingline);
+        System.out.println("Hello, I'm Kowalski!" +
+                "What can I do for you?" );
+        System.out.println(dividingline);
+
+        String userInput;
+        Scanner in = new Scanner(System.in);
+
+        while (true){
+            userInput = in.nextLine();
+            System.out.println(dividingline);
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(dividingline);
+                break;
+            }
+            System.out.println(userInput);
+            System.out.println(dividingline);
+
+        }
     }
 }
