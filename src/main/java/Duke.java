3c1efc24a148733b5f3013fd5696d1c3fac99a66
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-01-30 15:50:31 +0800

    Added Level 0.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,10 @@
+import java.util.Scanner;
 public class Duke {
+    static String break_line = "____________________________________________________________";
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(break_line + "\nHello! I'm Jeremy.\nWhat can I do for you?\n" + break_line);
+
+        System.out.println("Bye. Hope to see you again soon!\n" + break_line);
+        }
     }
-}
