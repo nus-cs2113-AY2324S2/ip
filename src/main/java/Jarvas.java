4a5c3b1f8c185401a26838ff7f6ce15fe1192ca0
l5_z <e0958707@u.nus.edu>
2024-01-30 22:15:58 +0800

    Add Level-2: Add, List

--- src/main/java/Jarvas.java
+++ src/main/java/Jarvas.java
@@ -1,43 +1,54 @@
 import java.util.Scanner;
 public class Jarvas {
-    public static String partitionLine = "____________________________________________________________\n";
+    public static final String partitionLine = "____________________________________________________________";
     public static void initialiseBot(){
-        String logo = " _____                                  \n" +
-                "(___  )                                 \n" +
-                "    | |   _ _  _ __  _   _    _ _   ___ \n" +
-                " _  | | /'_` )( '__)( ) ( ) /'_` )/',__)\n" +
-                "( )_| |( (_| || |   | \\_/ |( (_| |\\__, \\\n" +
-                "`\\___/'`\\__,_)(_)   `\\___/'`\\__,_)(____/";
-        System.out.println("Hello from\n" + logo);
-        String greet = partitionLine
-                + " Hello! I'm Jarvas\n"
-                + " What can I do for you?\n"
-                + partitionLine;
-        System.out.println(greet);
+        System.out.println(" _____                                  ");
+        System.out.println("(___  )                                 ");
+        System.out.println("    | |   _ _  _ __  _   _    _ _   ___ ");
+        System.out.println(" _  | | /'_` )( '__)( ) ( ) /'_` )/',__)");
+        System.out.println("( )_| |( (_| || |   | \\_/ |( (_| |\\__, \\");
+        System.out.println("`\\___/'`\\__,_)(_)   `\\___/'`\\__,_)(____/");
 
+        System.out.println(partitionLine);
+        System.out.println("Hello! I'm Jarvas");
+        System.out.println("What can I do for you?");
+        System.out.println(partitionLine);
     }
     public static void terminateBot(){
-        String exit = partitionLine
-                + " Bye! Hope to see you again soon!\n"
-                + partitionLine;
-        System.out.println(exit);
+        System.out.println(partitionLine);
+        System.out.println("Bye, see you soon!");
+        System.out.println(partitionLine);
     }
     public static void runTime(){
         Scanner in = new Scanner(System.in);
         String userInput = in.nextLine();
+        Task[] tasks = new Task[100];
+        int taskCounter = 0;
 
-        while (!userInput.equals("bye")) {
-            echoInput(userInput);
+        while (true) {
+            switch(userInput){
+            case "bye":
+                return;
+            case "list":
+                System.out.println(partitionLine);
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println((i+1) + ". " + tasks[i].label);
+                }
+                System.out.println(partitionLine);
+                break;
+            default:
+                System.out.println(partitionLine);
+                System.out.print("added: ");
+                System.out.println(userInput);
+                System.out.println(partitionLine);
+
+                Task task = new Task(userInput);
+                tasks[taskCounter] = task;
+                taskCounter++;
+                break;
+            }
             userInput = in.nextLine();
         }
-    }
-
-    public static void echoInput(String userInput){
-        String echoOutput = partitionLine
-                + userInput
-                + "\n"
-                + partitionLine;
-        System.out.print(echoOutput);
     }
 
     public static void main(String[] args) {
