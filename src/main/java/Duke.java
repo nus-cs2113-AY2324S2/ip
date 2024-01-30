dc8eb8043c5a83dafbfcda59adf215a1071ef0f8
sxfoo <fsx159@gmail.com>
2024-01-30 11:55:22 +0800

    Tweak the code to comply with Java Coding Standard
    A-CodingStandard.
    Changed the code to fit standards as described in https://se-education.org/guides/conventions/java/basic.html

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -16,13 +16,13 @@ public class Duke {
         sayGoodbye();
     }
 
-    private static final String messageDivider = "-------------------------------------";
+    private static final String MESSAGE_DIVIDER = "-------------------------------------";
 
     private static void greetUser() {
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
         System.out.println("Hello! I'm Carrot!");
         System.out.println("What can I do for you?");
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
     }
 
     private static void handleUserInput() {
@@ -38,6 +38,11 @@ public class Duke {
         while (true) {
             userInput = in.nextLine().trim();
 
+            if (isMarkUnmarkTask(userInput)) {
+                handleMarkUnmarkTask(userInput, listOfTasks, taskCount);
+                continue;
+            }
+
             switch (userInput.toLowerCase()) {
             case "":
                 continue;
@@ -47,9 +52,6 @@ public class Duke {
                 echoListItems(listOfTasks, taskCount);
                 break;
             default:
-                if (handleMarkUnmarkTask(userInput, listOfTasks, taskCount)) {
-                    break;
-                }
                 addTaskToList(userInput, listOfTasks, taskCount);
                 taskCount++;
                 break;
@@ -57,51 +59,55 @@ public class Duke {
         }
     }
 
-    private static boolean handleMarkUnmarkTask(String userInput, Task[] listOfTasks, int taskCount) {
-        if (userInput.toLowerCase().startsWith("mark") || userInput.toLowerCase().startsWith("unmark")) {
-            String command = userInput.toLowerCase().startsWith("mark") ? "mark" : "unmark";
-            String numberPart = userInput.substring(command.length()).trim();
-            try {
-                int taskIndex = Integer.parseInt(numberPart);
-                changeTaskStatus(command.equals("mark"), taskIndex, listOfTasks);
-            } catch (NumberFormatException e) {
-                System.out.println(messageDivider);
-                System.out.println("ERROR: Invalid task index, usage \"" + command + " (taskIndex)\"");
-                System.out.println(messageDivider);
-            } catch (IndexOutOfBoundsException e) {
-                System.out.println(messageDivider);
-                System.out.println("ERROR: Index out of bounds, usage \"" + command + " (taskIndex)\"");
-                System.out.println(messageDivider);
-            }
+    private static boolean isMarkUnmarkTask(String userInput) {
+        if (userInput.toLowerCase().startsWith("mark")
+                || userInput.toLowerCase().startsWith("unmark")) {
             return true;
         }
         return false;
+    }
+
+    private static void handleMarkUnmarkTask(String userInput, Task[] listOfTasks, int taskCount) {
+        String command = userInput.toLowerCase().startsWith("mark") ? "mark" : "unmark";
+        String numberPart = userInput.substring(command.length()).trim();
+        try {
+            int taskIndex = Integer.parseInt(numberPart);
+            changeTaskStatus(command.equals("mark"), taskIndex, listOfTasks);
+        } catch (NumberFormatException e) {
+            System.out.println(MESSAGE_DIVIDER);
+            System.out.println("ERROR: Invalid task index, usage \"" + command + " (taskIndex)\"");
+            System.out.println(MESSAGE_DIVIDER);
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(MESSAGE_DIVIDER);
+            System.out.println("ERROR: Index out of bounds, usage \"" + command + " (taskIndex)\"");
+            System.out.println(MESSAGE_DIVIDER);
+        }
     }
 
     private static void changeTaskStatus(boolean checkmark, int taskIndex, Task[] listOfTasks) {
         // listOfTasks is indexed from 0
         listOfTasks[taskIndex - 1].setStatus(checkmark);
 
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
         if (checkmark == true) {
             System.out.println("Nice! I've marked this task as done:");
         } else {
             System.out.println("OK, I've marked this task as not done yet:");
         }
         listOfTasks[taskIndex - 1].printTaskInfo();
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
     }
 
     private static void addTaskToList(String userInput, Task[] listOfTasks, int index) {
         listOfTasks[index] = new Task(userInput);
 
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
         System.out.println("added: " + userInput);
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
     }
 
     private static void echoListItems(Task[] listOfTasks, int taskCount) {
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
         if (taskCount == 0) {
             System.out.println("No tasks added");
         } else {
@@ -110,12 +116,12 @@ public class Duke {
                 System.out.printf("%d. [%s] %s%n", i+1, listOfTasks[i].getStatusIcon(), listOfTasks[i].description);
             }
         }
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
     }
 
     private static void sayGoodbye() {
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(messageDivider);
+        System.out.println(MESSAGE_DIVIDER);
     }
 }
