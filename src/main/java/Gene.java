b00ca1ddc76c14e74a071ed97be280ce8fe1b006
genexus85 <arilimeelik@gmail.com>
2024-01-31 00:34:48 +0800

    Feature: Add echo functionality to repeat commands entered by user

--- src/main/java/Gene.java
+++ src/main/java/Gene.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Gene {
     private static String botName = "Gene";
+    private Scanner scanner = new Scanner(System.in);
 
     private void printLineSeparation() {
         for (int i = 0; i < 29; i++) {
@@ -13,9 +16,27 @@ public class Gene {
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
         printLineSeparation();
+
+        // Read user input in a loop
+        while (true) {
+            System.out.print("User Input: ");
+            String userInput = scanner.nextLine();
+
+            // End chat if user types "bye"
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            // Echo user input
+            printLineSeparation();
+            System.out.println(userInput);
+            printLineSeparation();
+
+        }
     }
 
     public void endChat() {
+        printLineSeparation();
         System.out.println("Bye. Hope to see you again soon!");
         printLineSeparation();
     }
