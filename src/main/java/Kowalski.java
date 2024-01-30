8a51274e677d25eed218b6044da62cb9fcf14aa8
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-01-30 23:55:58 +0800

    Add functionality to list and add tasks

--- src/main/java/Kowalski.java
+++ src/main/java/Kowalski.java
@@ -2,26 +2,42 @@ import java.util.Scanner;
 
 public class Kowalski {
     public static void main(String[] args){
-        String dividingline = "____________________________________________________________";
-        System.out.println(dividingline);
-        System.out.println("Hello, I'm Kowalski!" +
-                "What can I do for you?" );
-        System.out.println(dividingline);
-
+        String[] currentTasks = new String[100];
+        String dividingLine = "____________________________________________________________";
         String userInput;
+        int counter = 0;
+
+        // Introduction to User
+        System.out.println(dividingLine);
+        System.out.println("Hello, I'm Kowalski!\n" +
+                "What can I do for you?" );
+        System.out.println(dividingLine);
+
+        // Turn on scanner
         Scanner in = new Scanner(System.in);
 
+        //Continuous loop until break statement
         while (true){
-            userInput = in.nextLine();
-            System.out.println(dividingline);
+            //User Inputs
+            userInput = ((in.nextLine()).trim()).toLowerCase();
+            System.out.println(dividingLine);
+
+            //Cases for the userInput
             if (userInput.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(dividingline);
+                System.out.println(dividingLine);
                 break;
+            } else if (userInput.equals("list")){
+                for (int i = 1; i < counter+1;i++){
+                    System.out.println(i + ". " + currentTasks[i-1]);
+                }
+                System.out.println(dividingLine);
+            } else {
+                System.out.println("added: " + userInput);
+                currentTasks[counter] = userInput;
+                counter++;
+                System.out.println(dividingLine);
             }
-            System.out.println(userInput);
-            System.out.println(dividingline);
-
         }
     }
 }
