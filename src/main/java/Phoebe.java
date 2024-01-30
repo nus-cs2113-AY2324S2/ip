431e555b5687540604fd0717a5c9efef4f8f4b98
iscyng <justisaacyong@gmail.com>
2024-01-30 16:48:37 +0800

    Added greet and exit, changed duke to phoebe

---
+++ src/main/java/Phoebe.java
@@ -0,0 +1,9 @@
+public class Phoebe {
+    public static void main(String[] args) {
+        String greet = "Hello! I'm Phoebe! What can I do for you?";
+        String exit = "Bye. Hope to see you again soon!";
+        System.out.println(greet);
+        System.out.println(exit);
+
+    }
+}
