44fbc544a3987f01c1c2f8936aa6c0901780a264
wong quan wei alvin <wongquan.weialvin@gmail.com>
2024-01-30 23:58:19 +0800

    Level 3. Mark as Done

--- src/main/java/Wongster.java
+++ src/main/java/Wongster.java
@@ -9,20 +9,41 @@ public class Wongster {
         Echo();
     }
 
-    public static void Addlist(List<String> userList, String newItem) {
+    public static void Addlist(List<String> userList, String newItem, List<Boolean> markedList) {
         userList.add(newItem);
+        markedList.add(false);
         System.out.println("Added: " + newItem);
     }
 
-    public static void Printlist(List<String> userList) {
-        System.out.println("list \n");
+    public static void Printlist(List<String> userList, List<Boolean> markedList) {
+        System.out.println("Here are tasks in your list: \n");
         for (int i = 0; i < userList.size(); i++) {
-            System.out.println((i + 1) + ". " + userList.get(i));
+            String marking = markedList.get(i) ? "[x]" : "[ ]";
+            System.out.println((i + 1) + "." + marking + " " + userList.get(i));
         }
+    }
+
+    public static void Marklist(List<String> userList, List<Boolean> markedList, String indexStr) {
+            int index = Integer.parseInt(indexStr) - 1;
+            if (index >= 0 && index < userList.size()) {
+                markedList.set(index, true);
+                System.out.println("Nice! I've marked this task as done: \n");
+                System.out.println("[X] " + userList.get(index));
+            }
+    }
+
+    public static void Unmarklist(List<String> userList, List<Boolean> markedList, String indexStr) {
+            int index = Integer.parseInt(indexStr) - 1;
+            if (index >= 0 && index < userList.size()) {
+                markedList.set(index, false);
+                System.out.println("OK, I've marked this task as not done yet: \n");
+                System.out.println("[ ] " + userList.get(index));
+            }
     }
 
     public static void Echo() {
         List<String> userList = new ArrayList<>();
+        List<Boolean> markedList = new ArrayList<>();
         Scanner scanner = new Scanner(System.in);
         while (true) {
             String userInput = scanner.nextLine();
@@ -30,11 +51,14 @@ public class Wongster {
                 System.out.println("Bye. Hope to see  you again soon!");
                 break;
             }
-
-            if (userInput.equalsIgnoreCase("list")) {
-                Printlist(userList);
+            if (userInput.startsWith("mark") && userInput.length() > 5) {
+                Marklist(userList, markedList, userInput.substring(5));
+            } else if (userInput.startsWith("unmark") && userInput.length() > 7) {
+                Unmarklist(userList, markedList, userInput.substring(7));
+            } else if (userInput.equalsIgnoreCase("list")) {
+                Printlist(userList, markedList);
             } else {
-                Addlist(userList, userInput);
+                Addlist(userList, userInput, markedList);
             }
 
         }
