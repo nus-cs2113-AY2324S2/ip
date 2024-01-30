43562a4c8a77992934aca7236d3f110e8b2b2fac
syj_02 <>
2024-01-30 19:28:47 +0800

    style: Rename chatbot

---
+++ src/main/java/Jane.java
@@ -0,0 +1,11 @@
+public class Jane {
+    public static void main(String[] args) {
+        String logo = " _____    _____    ____ _    _____ \n" +
+                "|____ |  |     |  |    | |  | ____|\n" +
+                "    | |  |  |  |  | |  | |  | |___ \n" +
+                " _  | |  |  _  |  | |  | |  |  ___|\n" +
+                "| |_| |  | | | |  | |  | |  | |___ \n" +
+                "|_____|  |_| |_|  |_| ___|  |_____|\n";
+        System.out.println("Hello from\n" + logo);
+    }
+}
