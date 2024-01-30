e34e71c2679506f591693fc0b1e261414ffc4ec0
wong quan wei alvin <wongquan.weialvin@gmail.com>
2024-01-30 21:22:39 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Wongster.java
@@ -0,0 +1,9 @@
+public class Wongster {
+    public static void main(String[] args) {
+        String name = "Wongster";
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n\n" + "Bye. Hope to see  you again soon!");
+    }
+}
+
+
+
