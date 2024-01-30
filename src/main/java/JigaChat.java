8a307be2785f0291db0046d799558eb06edb5972
alalal47 <kerbbeqr@gmail.com>
2024-01-30 23:37:27 +0800

    Add Increment Level 1-1

--- src/main/java/JigaChat.java
+++ src/main/java/JigaChat.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
 public class JigaChat {
+    public static void readCommand() {
+        String command;
+        Scanner in = new Scanner(System.in);
+        command =in.nextLine();
+        if (command.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else {
+            System.out.println(command);
+            readCommand();
+        }
+
+    }
     public static void main(String[] args) {
         System.out.println("Hello! I'm JigaChat");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        readCommand();
+        }
     }
-}
