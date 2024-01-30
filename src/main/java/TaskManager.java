ca04493b3a32f1152e97d56e21bbfcb41f45f4b6
YHWong20 <yihao.wong@u.nus.edu>
2024-01-30 13:21:31 +0800

    Add logic to track task completion

--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -10,22 +10,46 @@ public class TaskManager {
     }
 
     String addTask(String taskName) {
-        int currentIdx = nextTaskId - 1;
+        int currentIndex = nextTaskId - 1;
         Task newTask = new Task(taskName, nextTaskId);
-        tasks[currentIdx] = newTask;
+        tasks[currentIndex] = newTask;
         nextTaskId++;
         taskCount++;
 
-        return String.format("added: %s", taskName);
+        return String.format(" added: %s", taskName);
     }
 
     String listTasks() {
-        String output = "";
+        StringBuilder output = new StringBuilder(" Here are the tasks in your list:\n");
         for (int i = 0; i < taskCount; i++) {
             Task currentTask = tasks[i];
-            output += String.format("%d. %s\n", currentTask.getTaskId(), currentTask.getTaskName());
+            if (currentTask.isCompleted()) {
+                output.append(String.format(" %d.[X] %s\n", currentTask.getTaskId(), currentTask.getTaskName()));
+            } else {
+                output.append(String.format(" %d.[ ] %s\n", currentTask.getTaskId(), currentTask.getTaskName()));
+            }
         }
 
-        return output.stripTrailing();
+        return output.toString().stripTrailing();
+    }
+
+    String updateTaskProgress(int taskId, String command) {
+        String output;
+        int currentTaskIndex = taskId - 1; // Index in array is ID - 1
+        Task currentTask = tasks[currentTaskIndex];
+        String currentTaskName = currentTask.getTaskName();
+
+        if (command.equals("mark")) {
+            output = " Nice! I've marked this task as done:\n";
+            currentTask = currentTask.completeTask();
+            output += String.format("   [X] %s", currentTaskName);
+        } else {
+            output = " OK, I've marked this task as not done yet:\n";
+            currentTask = currentTask.uncompleteTask();
+            output += String.format("   [ ] %s", currentTaskName);
+        }
+
+        tasks[currentTaskIndex] = currentTask;
+        return output;
     }
 }
