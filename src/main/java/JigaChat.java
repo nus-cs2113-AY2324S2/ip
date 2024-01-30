fcc4a18d5ccb007de47a16fbec772846ecb10ba4
alalal47 <kerbbeqr@gmail.com>
2024-01-31 01:10:59 +0800

    Add Task.java Implement Level 2

--- src/main/java/JigaChat.java
+++ src/main/java/JigaChat.java
@@ -1,21 +1,37 @@
 import java.util.Scanner;
+import java.util.Arrays;
 public class JigaChat {
-    public static void readCommand() {
+    Task[] taskList = new Task[100];
+    int taskCounter = 0;
+
+    public void addtoList() {
         String command;
         Scanner in = new Scanner(System.in);
-        command =in.nextLine();
+        command = in.nextLine();
         if (command.equals("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
+        } else if (command.equals("list")) {
+            printList();
+            addtoList();
         }
         else {
-            System.out.println(command);
-            readCommand();
+            taskList[taskCounter] = new Task(command);
+            taskCounter++;
+            System.out.println("added: " + command);
+            addtoList();
         }
+    }
 
+    public void printList() {
+        for (int i = 0; i < taskCounter; i ++) {
+            System.out.println ((i + 1) + ". " + taskList[i].description);
+        }
     }
     public static void main(String[] args) {
         System.out.println("Hello! I'm JigaChat");
         System.out.println("What can I do for you?");
-        readCommand();
-        }
+        JigaChat chat = new JigaChat();
+        chat.addtoList();
     }
+}
+
