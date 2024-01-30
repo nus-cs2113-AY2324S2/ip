19e96b408f02de3de47cf11dcced287d96d9bb61
Claribel Ho <hoclaribel@gmail.com>
2024-01-31 00:45:29 +0800

    Add scanner that reads user input for echo

--- src/main/java/Soot.java
+++ src/main/java/Soot.java
@@ -1,8 +1,19 @@
+import java.util.Scanner;
+
 public class Soot {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
         drawLine();
         System.out.println("Hello! I'm Soot, your personal chatbot companion :)");
         System.out.println("What can I help you with?");
+        drawLine();
+
+        line = in.nextLine(); //user input
+        drawLine();
+
+        System.out.println(line);
         drawLine();
 
         System.out.println("Bye! Till the next time we meet...");
