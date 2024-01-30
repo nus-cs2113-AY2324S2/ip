efe9ff6c3697ec3a4116474c6192e2d0c6553a44
syj_02 <>
2024-01-31 01:00:41 +0800

    feat: Add mark and unmark functionalities Added mark and unmark to update whether the tasks are done.

--- src/main/java/Jane.java
+++ src/main/java/Jane.java
@@ -16,22 +16,38 @@ public class Jane {
 
         Scanner in = new Scanner(System.in);
         String input = in.nextLine();
-        String[] textList = new String[100];
-        int textCount = 0;
+        Task t = new Task(input);
+        TaskList taskList = new TaskList();
 
         while (!input.equals("bye")) {
+            System.out.print(horizontalLine);
+            Task currentTask;
             if (input.equals("list")) {
-                for (int i = 1; i <= textCount; i++) {
-                    System.out.println(i + ". " + textList[i - 1]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskList.count; i++) {
+                    currentTask = taskList.list[i];
+                    System.out.println(currentTask.getIndex() + "." + currentTask.getStatusIcon() + currentTask.getDescription());
                 }
             }
+            else if (input.startsWith("mark")) {
+                input = input.replace("mark ", "");
+                currentTask = taskList.list[Integer.parseInt(input)-1];
+                currentTask.isDone("mark");
+                System.out.println("Nice! I've marked this task as done:\n" + currentTask.getStatusIcon() + currentTask.getDescription());
+            }
+            else if (input.startsWith("unmark")) {
+                input = input.replace("unmark ", "");
+                currentTask = taskList.list[Integer.parseInt(input)-1];
+                currentTask.isDone("unmark");
+                System.out.println("OK, I've marked this task as not done yet:\n" + currentTask.getStatusIcon() + currentTask.getDescription());
+            }
             else {
-                textList[textCount] = input;
-                textCount++;
-                System.out.println("Jane: " + "added: " + input);
+                taskList.addTask(t);
+                System.out.println("added: " + t.getDescription());
             }
             System.out.print(horizontalLine);
             input = in.nextLine();
+            t = new Task(input);
         }
 
         System.out.print(horizontalLine + exitMessage + horizontalLine);
