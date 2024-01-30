da64b991700b91ffb340fcf5861c14196925a4f9
l5_z <e0958707@u.nus.edu>
2024-01-30 19:29:18 +0800

    Revert "feat: Add greeting message and exit message"
    This reverts commit 2635b2e7f3cba88f7c1de6b3f8e45024433306cb.

--- src/main/java/Jarvas.java
+++ src/main/java/Jarvas.java
@@ -5,16 +5,6 @@ public class Jarvas {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-
-        String greet = "____________________________________________________________\n"
-                + " Hello! I'm Jarvas\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n";
-        String exit = "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
-        System.out.println(greet);
-        System.out.println(exit);
     }
 }
