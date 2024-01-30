ec62644971b37550fe6e72269adb4ef0fcb4ea10
Tan Dexter <dextboy@outlook.com>
2024-01-30 12:12:11 +0800

    Enhance Natsu with Task class for A-Classes extension
    Introduce the Task class with the following features:
    * A description field for storing task details.
    * A boolean isDone flag to track task completion.
    * Methods markAsDone() and markAsUndone() to update task status.
    * A custom toString method for formatted task display.
    
    Refactor echoCommands() method to utilize the Task class:
    * Replace String array with Task array for task storage.
    * Implement 'list' command to display all tasks using Task's toString method.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,7 @@ public class Duke {
     private static void echoCommands() {
         Scanner input = new Scanner(System.in);
         boolean isExit = false;
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int index = 0;
         while (!isExit) {
             String userInput = input.nextLine();
@@ -26,27 +26,25 @@ public class Duke {
                 printLine();
                 System.out.println("     Here are the tasks in your list:");
                 for (int i = 0; i < index; i++) {
-                    System.out.println("     " + (i + 1) + "." + list[i]);
+                    System.out.println("     " + (i + 1) + "." + list[i].toString());
                 }
                 printLine();
             } else if (userInput.startsWith("mark ")) {
                 int itemIndex = Integer.parseInt(userInput.substring(5)) - 1;
-                list[itemIndex] = "[X] " + list[itemIndex].substring(4);
+                list[itemIndex].markAsDone();
                 printLine();
                 System.out.println("     Nice! I've marked this task as done:");
-                System.out.println("       " + list[itemIndex]);
+                System.out.println("       " + list[itemIndex].toString());
                 printLine();
-            } else if (userInput.startsWith("unmark ")){
+            } else if (userInput.startsWith("unmark ")) {
                 int itemIndex = Integer.parseInt(userInput.substring(7)) - 1;
-                list[itemIndex] = "[ ] " + list[itemIndex].substring(4);
+                list[itemIndex].markAsUndone();
                 printLine();
                 System.out.println("     OK, I've marked this task as not done yet:");
-                System.out.println("       " + list[itemIndex]);
+                System.out.println("       " + list[itemIndex].toString());
                 printLine();
-            }
-
-            else {
-                list[index++] = "[ ] " + userInput;
+            } else {
+                list[index++] = new Task(userInput);
                 printLine();
                 System.out.println("     " + userInput);
                 printLine();
