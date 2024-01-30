888f1f8f2a6e06d5979a5b2b1aab63f654410c07
djleong01 <dengjunleong2@gmail.com>
2024-01-30 19:00:12 +0800

    Complete Level-2 Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,27 +6,55 @@ public class Duke {
         System.out.println("Hello I am " + name + ".\n");
         System.out.println("How can I help you today?\n");
         Scanner userInput = new Scanner(System.in);
-        while (true)
-        {
+        String[] messages = new String[100];
+        int messageCount = 0;
+        while (true) {
             String x = userInput.nextLine();
             boolean exit = x.equals("bye");
-            if (exit)
-            {
+            boolean list = x.equals("list");
+            if (exit) {
                 break;
-            }
-            else
-            {
-                echoPrint(x);
+            } else if (list) {
+                listMessages(messages);
+            } else {
+                addList(x, messageCount, messages);
+                messageCount++;
             }
         }
-        System.out.println("Bye. Hope to see you again!");
-
+        printSeparator();
+        System.out.println("Bye. Hope to see you again soon!");
+        printSeparator();
     }
-    private static void echoPrint(String message)
+    private static void printSeparator()
     {
         String separator = "____________________________________________________________";
         System.out.println(separator);
-        System.out.println(message);
-        System.out.println(separator + ".\n");
     }
+
+    private static void echo(String message)
+    {
+        printSeparator();
+        System.out.println(message);
+        printSeparator();
+    }
+    private static void addList(String message, int messageCount, String[] messages) {
+        printSeparator();
+        System.out.println("added: " + message + "\n");
+        printSeparator();
+        messages[messageCount] = message;
+    }
+
+
+    private static void listMessages(String[] message) {
+        printSeparator();
+        for (int i = 0; i < 100; i++) {
+            if (message[i] == null) {
+                break;
+            }
+            System.out.println(i+1 + ". " + message[i]);
+        }
+        printSeparator();
+    }
+
+
 }
