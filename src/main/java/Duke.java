6a6bd52a4390399d8a1003ed9ddbdf00ced5004f
Tan Dexter <dextboy@outlook.com>
2024-01-30 09:52:02 +0800

    Add echoCommands() for Level-1 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,41 @@
+import java.util.Scanner;
+
 public class Duke {
 
     private static final String NAME = "Natsu";
 
     private static void printGreeting() {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm " + NAME);
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm " + NAME);
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    private static void echoCommands() {
+        Scanner input = new Scanner(System.in);
+        boolean isExit = false;
+        while (!isExit) {
+            String userInput = input.nextLine();
+            if (userInput.equals("bye")) {
+                input.close();
+                isExit = true;
+                printExitMessage();
+            } else {
+            System.out.println("    ____________________________________________________________");
+            System.out.println("    " + userInput);
+            System.out.println("    ____________________________________________________________");
+            }
+        }
     }
 
     private static void printExitMessage() {
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 
     public static void main(String[] args) {
         printGreeting();
-        printExitMessage();
+        echoCommands();
     }
 }
