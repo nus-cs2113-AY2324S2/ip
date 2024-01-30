f6209a87971a39591d23491019330176f283d154
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-01-30 21:41:23 +0800

    Added Level-1 from week 3.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,19 @@ public class Duke {
     public static void main(String[] args) {
         System.out.println(break_line + "\nHello! I'm Jeremy.\nWhat can I do for you?\n" + break_line);
 
+        Scanner in = new Scanner(System.in);
+        String line;
+
+        do {
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println(line + "\n" + break_line);
+            }
+        }
+        while (!line.equals("bye"));
+
         System.out.println("Bye. Hope to see you again soon!\n" + break_line);
+
+
         }
     }
