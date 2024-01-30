c50fbdbab5d56aaaa0c6f73e115cce9f795347ca
OKW32 <e0969674@u.nus.edu>
2024-01-30 12:53:15 +0800

    Style: Create Main class and add empty line at end of Beefy.java
    Beefy.java is the entry point.
    
    Creating a Main class to be used as the entry point for our program is
    cleaner and follows the coding standards.
    
    Let's
    * Create a new class Main
    * Create class functions in Beefy e.g printSeparation(), startChat(),
    endChat() to be used in Main class

--- src/main/java/Beefy.java
+++ src/main/java/Beefy.java
@@ -1,8 +1,10 @@
 public class Beefy {
+    private static final String BOTNAME = "BEEFY";
+
     /**
      * Prints a separation row of n "-", where n = WIDTH
      */
-    public static void printSeparation() {
+    private void printSeparation() {
         int WIDTH = 59;
         for (int i = 0; i < WIDTH; i++) {
             System.out.print("_");
@@ -10,12 +12,14 @@ public class Beefy {
         System.out.println("_");
     }
 
-    public static void main(String[] args) {
-        String BOTNAME = "BEEFY";
+    public void startChat() {
         printSeparation();
         System.out.println("Hello! I'm " + BOTNAME);
         System.out.println("What can I do for you?");
         printSeparation();
+    }
+
+    public void endChat() {
         System.out.println("Bye. Hope to see you again soon!");
         printSeparation();
     }
---
+++ src/main/java/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Beefy chatBot = new Beefy();
+        chatBot.startChat();
+        chatBot.endChat();
+    }
+}
