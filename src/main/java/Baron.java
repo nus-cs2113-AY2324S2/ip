fc629eedbecff12ac3a741c9388ad12c55d3fb23
Joshuahoky <joshuaho2002@gmail.com>
2024-01-30 12:28:47 +0800

    Rename Duke class to Baron Class

---
+++ src/main/java/Baron.java
@@ -0,0 +1,7 @@
+public class Baron {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Baron");
+        System.out.println("What can I do for you?\n");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
