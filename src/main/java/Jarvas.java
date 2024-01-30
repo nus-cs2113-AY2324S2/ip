fdf3235f821f16d7371de476c337333e15d8ec2f
l5_z <e0958707@u.nus.edu>
2024-01-30 19:55:25 +0800

    Add Level-1: Echo

--- src/main/java/Jarvas.java
+++ src/main/java/Jarvas.java
@@ -1,4 +1,6 @@
+import java.util.Scanner;
 public class Jarvas {
+    public static String partitionLine = "____________________________________________________________\n";
     public static void initialiseBot(){
         String logo = " _____                                  \n" +
                 "(___  )                                 \n" +
@@ -7,22 +9,40 @@ public class Jarvas {
                 "( )_| |( (_| || |   | \\_/ |( (_| |\\__, \\\n" +
                 "`\\___/'`\\__,_)(_)   `\\___/'`\\__,_)(____/";
         System.out.println("Hello from\n" + logo);
-        String greet = "____________________________________________________________\n"
+        String greet = partitionLine
                 + " Hello! I'm Jarvas\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n";
+                + partitionLine;
         System.out.println(greet);
 
     }
     public static void terminateBot(){
-        String exit = "____________________________________________________________\n"
+        String exit = partitionLine
                 + " Bye! Hope to see you again soon!\n"
-                + "____________________________________________________________";
+                + partitionLine;
         System.out.println(exit);
+    }
+    public static void runTime(){
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine();
+
+        while (!userInput.equals("bye")) {
+            echoInput(userInput);
+            userInput = in.nextLine();
+        }
+    }
+
+    public static void echoInput(String userInput){
+        String echoOutput = partitionLine
+                + userInput
+                + "\n"
+                + partitionLine;
+        System.out.print(echoOutput);
     }
 
     public static void main(String[] args) {
         initialiseBot();
+        runTime();
         terminateBot();
     }
 }
