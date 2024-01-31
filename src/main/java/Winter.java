e4f25351d195e1ab2178de955a5f096072677099
zhengwinter <e0958239@u.nus.edu>
2024-01-31 23:32:47 +0800

    Implement add task
    Used String array to implement the task list, used continue statement to execute the list command

--- src/main/java/Winter.java
+++ src/main/java/Winter.java
@@ -10,21 +10,22 @@ public class Winter {
                 "       \\/          \\/          \\/    ";
 
         System.out.println("Hello from\n" + logo);
-        hi();
-        echo();
-        bye();
+        sayHi();
+        //echo();
+        addTasks();
+        sayBye();
 
     }
 
     /* Function for greeting and farewell messages*/
-    private static void hi() {
+    private static void sayHi() {
         String line = "-----------------------------------\n";
         String greet = "Hello! I'm Winter!\nWhat can I do for you?";
         System.out.print(line);
         System.out.println(greet);
         System.out.print(line);
     }
-    private static void bye() {
+    private static void sayBye() {
         String line = "-----------------------------------\n";
         String farewell = "Bye. Hope to see you again soon!";
         System.out.print(line);
@@ -49,5 +50,34 @@ public class Winter {
             echoLine = input.nextLine();
         }
 
+    }
+    private static void addTasks() {
+        String line = "-----------------------------------\n";
+        String indent = "   ";
+        String[] taskList = new String[100];
+        int taskIndex = 0;
+        Scanner input = new Scanner(System.in);
+
+        while (true) {
+            String command = input.nextLine();
+            if(command.equals("bye") || command.equals("Bye") || command.equals("BYE")) {
+                break;
+            }
+            else if (command.equals("list") || command.equals("List") || command.equals("LIST")) {
+                for (int i = 0; i < taskIndex; i++) {
+                    System.out.print(indent);
+                    System.out.println(taskList[i]);
+                }
+                System.out.print(line);
+                continue;
+            }
+            taskList[taskIndex] = Integer.toString(taskIndex+1) + ". " + command;
+            taskIndex++;
+            System.out.print(line);
+            System.out.print(indent);
+            System.out.println("added: " + command);
+            System.out.print(line);
+
+        }
     }
 }
