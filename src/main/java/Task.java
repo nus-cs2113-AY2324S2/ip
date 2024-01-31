39ce62f2c6434210eb1ec1abbb8c2fd2706bffba
alalal47 <kerbbeqr@gmail.com>
2024-01-31 23:04:10 +0800

    Implement mark and unmark feature

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,6 +7,16 @@ public class Task {
         this.isDone = false;
     }
 
+    public void markAsDone () {
+        this.isDone = true;
+        System.out.println("[" + getStatusIcon() + "]" + this.description);
+    }
+
+    public void markAsUndone () {
+        this.isDone = false;
+        System.out.println("[" + getStatusIcon() + "]" + this.description);
+    }
+
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
