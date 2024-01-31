d9d386ebcc793391fcde25ad8317b6e64754bea7
yeozongyao <yeozongyao@gmail.com>
2024-02-01 02:33:00 +0800

    Level-1

--- src/main/java/Yeos.java
+++ src/main/java/Yeos.java
@@ -1,9 +1,21 @@
+import java.util.Scanner;
 public class Yeos {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String greeting = "Hello! I'm Yeos, your personal chatbot!\n"
                 + "What can I do for you today?\n";
         String bye = "Bye. Hope to see you again soon!\n";
         String line = "_____________________________________________________________________\n";
-        System.out.println(line + greeting + line + bye);
+        System.out.println(line + greeting + line);
+
+        while(true) {
+            String input = scanner.nextLine();
+            if (input.equals("bye")) {  // Check if the input is "bye"
+                System.out.println(line + bye + line);
+                break;
+            }
+            System.out.println(line + input + "\n" + line);
+        }
+        scanner.close();
     }
 }
