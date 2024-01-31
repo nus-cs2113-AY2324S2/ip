ba5f5c32976f0d33c1a92fa2f6d74c2590181389
joshualeejunyi <joshua.lee.99@hotmail.com>
2024-01-31 18:40:45 +0800

    Added username and echo back for level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+    public static void printBanner() {
         String logo = "  __ _ _ __| |_ ___ _ __ ___ (_)___ \n" +
                 " / _` | '__| __/ _ \\ '_ ` _ \\| / __|\n" +
                 "| (_| | |  | ||  __/ | | | | | \\__ \\\n" +
@@ -7,8 +9,29 @@ public class Duke {
                 "                                    ";
         System.out.println("======================================\n");
         System.out.println(logo);
-        System.out.println("Hello! I'm Artemis.\n");
-        System.out.println("What can I do for you?\n");
+    }
+
+    public static void main(String[] args) {
+        printBanner();
+
+        Scanner scan = new Scanner(System.in);
+        String username;
+        String userInput;
+
+        System.out.println("Hello! I'm Artemis. What is your name?\n");
+        System.out.print("[User]: ");
+        username = scan.nextLine();
+        System.out.printf("What can I do for you, %s?\n", username);
+
+        for (;;) {
+            System.out.printf("[%s]: ", username);
+            userInput = scan.nextLine();
+
+            if (userInput.equalsIgnoreCase("bye")) {
+                break;
+            }
+            System.out.printf("[Artemis]: %s\n", userInput);
+        }
 
         System.out.println("======================================\n");
         System.out.println("Bye. Hope to see you again soon!\n");
