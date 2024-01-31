6f08701018a7ea74e6a72710975f9b68483457dd
hikohiko <duliyan@u.nus.edu>
2024-01-31 13:34:31 +0800

    no message

--- src/main/java/Echo.java
+++ src/main/java/Echo.java
@@ -1,8 +1,27 @@
 import java.util.Scanner;
 
 public class Echo {
+    static String break_line = "----------------------------------------";
+    public void startEchoing() {
+        Scanner scanner = new Scanner(System.in);
+        String input = " ";
+
+        while (!input.equalsIgnoreCase("bye")) {
+            System.out.println("What can I do for you?");
+            input = scanner.nextLine();
+
+            if (!input.equalsIgnoreCase("bye")) {
+                System.out.println(break_line);
+                System.out.println(repeat(input));
+                System.out.println(break_line);
+            }
+        }
+
+        scanner.close();
+        System.out.println("Kkkkkk thanks bye ");
+    }
+
     public String repeat(String input) {
         return input;
     }
-
 }
--- src/main/java/Hiko.java
+++ src/main/java/Hiko.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Hiko {
-    static String break_line = "----------------------------------------";
+
     public static void main(String[] args) {
         String logo =
 
@@ -15,30 +15,9 @@ public class Hiko {
                 "|__|__||____||__|\\_| \\___/ \n" +
                 "                           \n";
         System.out.println("Hello from \n" + logo);
-        Scanner scanner = new Scanner(System.in);
-        String input = " ";
-
-        while(!input.contains("bye"))
-        {
-            System.out.println("What can I do for you?");
-            input = scanner.nextLine();
-
-            //deal with the exception of whenever user inputs bye
-            //skip the repeat
-            if(input.equalsIgnoreCase("bye")){
-                scanner.close();
-                break;
-
-            }
-
-
-            Echo echo = new Echo();
-            System.out.println(echo.repeat(input));
-            System.out.println(break_line);
-        }
-
-        scanner.close();
-        System.out.println("Kkkkkk thanks bye " );
+        // create an instance of Echo and start echoing
+        Echo echo = new Echo();
+        echo.startEchoing();
 
     }
 }
