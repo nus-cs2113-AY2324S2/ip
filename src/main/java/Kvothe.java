780dbb1aabc765be831293ee0566822171b62af0
Álvaro <alvaro.zamanillo@estudiante.uam.es>
2024-01-31 16:28:58 +0800

    Add methods mark and unmark.
    Arguments for methods mark and unmark are checked.

--- src/main/java/Kvothe.java
+++ src/main/java/Kvothe.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class Kvothe {
 
-    private static String[] textStorage = new String[100];
-    private static int textStorageIndex = 0;
+    private static final int MAXTASKS = 100;
+    private static Task[] tasks = new Task[MAXTASKS];
+    private static int tasksIndex = 0;
 
     public static void main(String[] args) {
         String greeting = "Hello! I'm Kvothe.\n"
@@ -40,14 +41,54 @@ public class Kvothe {
     }
 
     private static void add(String line) {
-        textStorage[textStorageIndex] = line;
-        textStorageIndex++;
+        tasks[tasksIndex] = new Task(line);
+        tasksIndex++;
     }
 
     private static void list() {
-        for (int i = 0; i < textStorageIndex; i++) {
-            echo(i + 1 + ". " + textStorage[i], i == 0, i == textStorageIndex - 1);
+        for (int i = 0; i < tasksIndex; i++) {
+            echo(i + 1 + ". " + tasks[i], i == 0, i == tasksIndex - 1);
         }
+    }
+
+    private static void done(int index) {
+        tasks[index - 1].markAsDone();
+        echo("Nice! I've marked this task as done:\n\t\t" + tasks[index - 1]);
+    }
+
+    private static void mark(int index){
+        tasks[index - 1].setIsDone(true);
+        echo("Nice! I've marked this task as done:\n\t\t" + tasks[index - 1]);
+    }
+
+    private static void unmark(int index){
+        tasks[index - 1].setIsDone(false);
+        echo("OK, I've marked this task as not done yet:\n\t\t" + tasks[index - 1]);
+    }
+
+    private static int getTaskNumber(String[] args){
+
+        int index = 0;
+        if (args.length < 2 || args.length > 2) {
+           echo("Sorry. Please specify one task number.");
+           return -1;
+        }
+
+        try{
+            // First argument is the command
+            index = Integer.parseInt(args[1]);
+        } catch (NumberFormatException e) {
+            echo("Sorry. Please specify the task number as an integer.");
+            return -1;
+        }
+
+        if(index > tasksIndex){
+            echo("Sorry. There are just " + tasksIndex + " tasks in the list.");
+            return -1;
+        }
+
+        return index;
+
     }
 
     public static void interact() {
@@ -56,9 +97,26 @@ public class Kvothe {
         String line = in.nextLine();
 
         while (!line.equals(stopWord)) {
-            switch (line) {
+            String lineWords[] = line.split(" ");
+            String command = lineWords[0];
+
+            int taskNumber;
+
+            switch (command) {
                 case "list":
                     list();
+                    break;
+                case "mark":
+                    taskNumber = getTaskNumber(lineWords);
+                    if(taskNumber != -1){
+                        mark(taskNumber);
+                    }
+                    break;
+                case "unmark":
+                    taskNumber = getTaskNumber(lineWords);
+                    if(taskNumber != -1){
+                        unmark(taskNumber);
+                    }
                     break;
                 default:
                     add(line);
