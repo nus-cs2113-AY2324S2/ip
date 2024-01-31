85b587153b1723154a9dac1c3dda774a05d8e125
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-02-01 00:15:07 +0800

    Add store input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -13,12 +13,25 @@ public class Duke {
         System.out.println("Hello! I'm Gary");
         System.out.println("What can I do for you?");
 
+        String[] todos = new String[100];
+        int todosCount = 0;
+
         String line;
         line = in.nextLine();
         while (!(line.toUpperCase().contains("BYE"))) {
-            System.out.println(line);
+            if (line.equalsIgnoreCase("LIST")) {
+                for(int i = 0; i < todosCount; i += 1) {
+                    System.out.println((i + 1) + ". " + todos[i]);
+                }
+            } else {
+                System.out.println("added: " + line);
+                todos[todosCount] = line;
+                todosCount += 1;
+            }
+
             line = in.nextLine();
         }
+
 
         System.out.println("Bye. Hope to see you again!");
     }
