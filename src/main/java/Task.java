32742614d5cf560170027a8d7d7a6f697ef2f1dc
hikohiko <duliyan@u.nus.edu>
2024-01-31 17:33:53 +0800

    no message

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -18,4 +18,7 @@ public class Task {
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
+    public String toString() {
+        return description;
+    }
 }
