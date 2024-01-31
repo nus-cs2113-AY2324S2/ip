981d70aa3e429cf3be83c2c6a8e18dff0e6513bf
Álvaro <alvaro.zamanillo@estudiante.uam.es>
2024-01-31 15:16:10 +0800

    Rename chatbot and display greeting and bye messages.

---
+++ src/main/java/Kvothe.java
@@ -0,0 +1,11 @@
+public class Kvothe {
+    public static void main(String[] args) {
+        String greeting = "Hello! I'm Kvothe.\n"
+                + "What can I do for you?";
+
+        String bye = "Bye. Hope to see you again soon!";
+
+        System.out.println(greeting);
+        System.out.println(bye);
+    }
+}
