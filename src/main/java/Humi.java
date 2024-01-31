9439d2f6e942d14c184606004794f2bfd25dea05
edwardhumi <edward.humianto@gmail.com>
2024-01-31 15:05:34 +0800

    Add greetings

---
+++ src/main/java/Humi.java
@@ -0,0 +1,11 @@
+public class Humi {
+    public static void main(String[] args) {
+//        String logo = " ____        _        \n"
+//                + "|  _ \\ _   _| | _____ \n"
+//                + "| | | | | | | |/ / _ \\\n"
+//                + "| |_| | |_| |   <  __/\n"
+//                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello! I'm Humi");
+        System.out.println("What can I do for you?");
+    }
+}
