608316140d56d81a83475c0f129bc5cb52ed69e7
Haotian199 <110802843+Haotian199@users.noreply.github.com>
2024-01-31 20:29:27 +0800

    level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,34 @@
-public class Duke {
+import java.util.Scanner;
+
+public class Duke  {
 
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|____/ \\__\n" +
+                "    },_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         Greet greet = new Greet();
-        greet.sayGreet();
+        greet.sayHello();
+
+
+        String line;
+        Scanner in = new Scanner(System.in);
+        while(true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                break;
+            }
+            greet.printHyphen();
+            System.out.println();
+            System.out.println(line);
+            greet.printHyphen();
+            System.out.println();
+        }
+        greet.sayBye();
+
     }
 }
 
--- src/main/java/Greet.java
+++ src/main/java/Greet.java
@@ -1,21 +1,27 @@
 public class Greet {
-    public void sayGreet() {
-        for(int i = 0 ; i < 50; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println();
-        for(int i = 0 ; i < 50; i++) {
-            System.out.print("-");
-        }
-        System.out.println();
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println();
-        for(int i = 0 ; i < 50; i++) {
+
+    public void printHyphen() {
+        for(int i = 0; i < 50; i++) {
             System.out.print("-");
         }
     }
+    public void sayHello() {
+        printHyphen();
+        System.out.println();
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        printHyphen();
+        System.out.println();
+    }
+
+    public void sayBye() {
+        printHyphen();
+        System.out.println();
+        System.out.println("Bye. Hope to see you again soon!");
+        printHyphen();
+    }
+
+
 
 }
+
