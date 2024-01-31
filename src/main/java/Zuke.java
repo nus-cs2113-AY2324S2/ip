f60f727f46d1d8d8e096b0b663aca7fed61555bf
ZMinghuiZ <zmh992545@gmail.com>
2024-01-31 18:50:50 +0800

    Add store task and list task feature

--- src/main/java/Zuke.java
+++ src/main/java/Zuke.java
@@ -13,25 +13,38 @@ public class Zuke {
                 "███████╗╚██████╔╝██║  ██╗███████╗\n" +
                 "╚══════╝ ╚═════╝ ╚═╝  ╚═╝╚══════╝\n";
         String receivedMessage;
+        int numberOfTask = 0;
+        String[] tasks = new String[100];
 
         System.out.println("Hello from\n" + logo);
         System.out.println(INDENTATION_LINE +
-                " Hello! I'm Zuke\n" +
-                " What can I do for you?\n" +
+                "Hello! I'm Zuke\n" +
+                "What can I do for you?\n" +
                 INDENTATION_LINE);
 
         Scanner userInput = new Scanner(System.in);
         while (userInput.hasNextLine()) {
             receivedMessage = userInput.nextLine();
+            System.out.println(INDENTATION_LINE);
+
             if (receivedMessage.equals("bye")) {
-                System.out.println(INDENTATION_LINE +
-                        " Bye. Hope to see you again soon!\n" +
+                System.out.println("Bye. Hope to see you again soon!\n" +
                         INDENTATION_LINE);
                 break;
             }
-            System.out.println(INDENTATION_LINE +
-                    receivedMessage + "\n" +
-                    INDENTATION_LINE);
+
+            if (receivedMessage.equals("list")) {
+                for (int i = 0; i < numberOfTask; i++) {
+                    System.out.println(i + 1 + ". " + tasks[i]);
+                }
+                System.out.println(INDENTATION_LINE);
+            } else {
+                tasks[numberOfTask] = receivedMessage;
+                numberOfTask++;
+                System.out.println("added: " +
+                        receivedMessage + "\n" +
+                        INDENTATION_LINE);
+            }
         }
         userInput.close();
     }
