c8513c81d7d653f3850a4a23404777bd92a30409
Leong-ZM <e0957648@u.nus.edu>
2024-01-31 11:12:54 +0800

    Add Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,25 +10,32 @@
 }*/
 import java.util.Scanner;
 public class Duke {
-    public static boolean respond(String input) {
-        if (input.equals("bye")) {
-            System.out.println("Bye. Hope to see you again soon! ");
-            return false;
-        } else {
-            System.out.println(input);
-            return true;
-        }
-    }
     public static void main(String[] args) {
         String chatbotName = "Horizon";
         System.out.println("Hello! I'm " + chatbotName);
         System.out.println("What can I do for you? ");
 
         String input;
-        Scanner in = new Scanner((System.in));
-        do {
+        String[] list;
+        int listLength = 0;
+        list = new String[100];
+        Scanner in = new Scanner(System.in);
+        while (true) {
             input = in.nextLine();
-        } while (respond(input));
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon! ");
+                break;
+            } else if (input.equals("list")) {
+                for (int i = 0; i < listLength; i += 1) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+            } else {
+                System.out.println("added:" + input);
+                list[listLength] = input;
+                listLength += 1;
+            }
+        }
+
 
     }
 }
