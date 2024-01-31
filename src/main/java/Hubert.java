b86cbaaf5268b113c304250e4dc7d11d42f564bd
liuy1103 <e0968020@u.nus.edu>
2024-01-31 14:01:26 +0800

    Incremmet Level-1 Echo

--- src/main/java/Hubert.java
+++ src/main/java/Hubert.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Hubert {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,13 +7,29 @@ public class Hubert {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        
+
+        //greetings
         String horizontal = "____________________________________________________________";
         String botName = "Hubert";
         System.out.println("Hello! I'm " + botName);
         System.out.println("What can I do for you?");
         System.out.println(horizontal);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(horizontal);
+
+        //echo
+        Scanner in = new Scanner(System.in);
+        String line;
+        String exitWord = "bye";
+        line = in.nextLine();
+        while (!line.equalsIgnoreCase(exitWord)) {
+            System.out.println(line);
+            System.out.println(horizontal);
+            line = in.nextLine();
+        }
+
+        //exit
+        if (line.equalsIgnoreCase(exitWord)) {
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(horizontal);
+        }
     }
 }
