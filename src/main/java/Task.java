841ab8f04adf6a7f3c4daadfb0cd6d3981a2bca7
Álvaro <alvaro.zamanillo@estudiante.uam.es>
2024-01-31 15:57:16 +0800

    Create Task class.

---
+++ src/main/java/Task.java
@@ -0,0 +1,35 @@
+public class Task {
+
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setIsDone(boolean isDone){
+        this.isDone = isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public String getStatusIcon() {
+        return (this.isDone ? "X" : " "); //return tick or X symbols
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.description;
+    }
+}
