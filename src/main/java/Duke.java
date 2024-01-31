34baab0af81d32c4745fcbca571bb146752c80c4
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-01-31 14:51:44 +0800

    A-CodingStandard for week 3 added.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 public class Duke {
-    static String break_line = "____________________________________________________________";
-    static String done = "[X]";
-    static String notDone = "[ ]";
+    static String BREAK_LINE = "____________________________________________________________";
+    static String DONE = "[X]";
+    static String NOT_DONE = "[ ]";
 
     public static void main(String[] args) {
-        System.out.println(break_line + "\nHello! I'm Jeremy.\nWhat can I do for you?\n" + break_line);
+        System.out.println(BREAK_LINE + "\nHello! I'm Jeremy.\nWhat can I do for you?\n" + BREAK_LINE);
 
         String[] tasks = new String[100];
         String[] doneCheck = new String[100];
@@ -16,7 +16,7 @@ public class Duke {
 
         do {
             line = in.nextLine();
-            System.out.println(break_line);
+            System.out.println(BREAK_LINE);
 
             if (!line.equals("bye")) {
                 if (line.equals("list")) {
@@ -26,31 +26,31 @@ public class Duke {
 
                         System.out.println((i + 1) + "." + doneCheck[i] + " " + tasks[i]);
                     }
-                    System.out.println(break_line);
+                    System.out.println(BREAK_LINE);
                 } else if (line.contains("unmark")) {
 
                     String[] parts = line.split(" ");
                     int taskMarkNumber = Integer.parseInt(parts[1]) - 1;
-                    doneCheck[taskMarkNumber] = notDone;
+                    doneCheck[taskMarkNumber] = NOT_DONE;
                     System.out.println("OK, I've marked this task as not done yet:");
-                    System.out.println(doneCheck[taskMarkNumber] + " " + tasks[taskMarkNumber] + "\n" + break_line);
+                    System.out.println(doneCheck[taskMarkNumber] + " " + tasks[taskMarkNumber] + "\n" + BREAK_LINE);
                 } else if (line.contains("mark")){
 
                     String[] parts = line.split(" ");
                     int taskMarkNumber = Integer.parseInt(parts[1]) - 1;
-                    doneCheck[taskMarkNumber] = done;
+                    doneCheck[taskMarkNumber] = DONE;
                     System.out.println("Nice! I've marked this task as done:");
-                    System.out.println(doneCheck[taskMarkNumber] + " " + tasks[taskMarkNumber] + "\n" + break_line);
+                    System.out.println(doneCheck[taskMarkNumber] + " " + tasks[taskMarkNumber] + "\n" + BREAK_LINE);
                 } else {
 
                     tasks[count] = line;
-                    doneCheck[count] = notDone;
-                    System.out.println("added: " + line + "\n" + break_line);
+                    doneCheck[count] = NOT_DONE;
+                    System.out.println("added: " + line + "\n" + BREAK_LINE);
                     count++;
                 }
             }
         } while (!line.equals("bye"));
 
-        System.out.println("Bye. Hope to see you again soon!\n" + break_line);
+        System.out.println("Bye. Hope to see you again soon!\n" + BREAK_LINE);
     }
 }
