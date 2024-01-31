e5e171d1e9ddd393e33a1abf461d33146ff69683
chenxk619 <chenxk619@gmail.com>
2024-01-31 14:34:58 +0800

    Level 0 task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,21 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String chat_name = "Sigma";
+        String logo = "____________________________________________________________\n"
+                + " Hello! I'm " + chat_name + "\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n"
+                + " Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________";
         System.out.println("Hello from\n" + logo);
     }
 }
+
+/*
+____________________________________________________________
+ Hello! I'm [YOUR CHATBOT NAME]
+ What can I do for you?
+____________________________________________________________
+ Bye. Hope to see you again soon!
+____________________________________________________________
+ */
