b579245033f0414aed3e9d535c01a845731010f3
chenxk619 <chenxk619@gmail.com>
2024-01-31 15:52:02 +0800

    Level 1 task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,13 +1,31 @@
+import java.util.Scanner;
 public class Duke {
+    public static void echo(String input){
+        System.out.println("____________________________________________________________\n"
+                            + input + "\n"
+                            + "____________________________________________________________");
+    }
     public static void main(String[] args) {
         String chat_name = "Sigma";
-        String logo = "____________________________________________________________\n"
+        String output = "____________________________________________________________\n"
                 + " Hello! I'm " + chat_name + "\n"
                 + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println("Hello from\n" + logo);
+                +"____________________________________________________________\n";
+        System.out.println(output);
+        String line;
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                echo(line);
+            }
+            else {
+                break;
+            }
+        }
+        System.out.println("____________________________________________________________\n"
+                + "Bye. Hope to see you again soon!\n"
+                +"____________________________________________________________\n");
     }
 }
 
