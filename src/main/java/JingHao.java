669029e335d1e3c92e80c71eadc91e2bc40f8754
JingHaoooo <jinghaoooo17@gmail.com>
2024-02-01 00:04:06 +0800

    Add function to store texts and displpay it to the user

--- src/main/java/JingHao.java
+++ src/main/java/JingHao.java
@@ -1,34 +1,49 @@
 import java.util.Scanner;
 public class JingHao {
-    public static void main(String[] args) {
+    protected Task[] list;
+    protected int numberOfTask;
+
+    public JingHao() {
+        this.numberOfTask = 0;
+        this.list = new Task[100];
+    }
+    public void Start(){
         String divider = "____________________________________________________________";
         String name = "JingHao";
         String line;
         Scanner in = new Scanner(System.in);
 
-        System.out.println(divider);
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println(divider);
 
-        boolean loop = true;
-        while (loop) {
+        while (true) {
             // Reads user input
             line = in.nextLine();
             System.out.println(divider);
-
             // Check if the user wants to exit
-            if (!line.equalsIgnoreCase("bye")) {
-                System.out.println(line);
-                System.out.println(divider);
+            if (line.equalsIgnoreCase("bye")) {
+                break;
+            }
+            else if(line.equalsIgnoreCase("list")) {
+                if (numberOfTask == 0) {
+                    System.out.println("No task");
+                } else {
+                    for (int i = 0; i < numberOfTask; i++) {
+                        System.out.println(i+1 + ".[" +list[i].getStatusIcon()+ "] " + list[i].description);
+                    }
+                }
             }
             else{
-                loop = false;
+                Task taskDescription = new Task(line);
+                list[numberOfTask]=taskDescription;
+                numberOfTask++;
+                System.out.println("added: " + line);
+                System.out.println(divider);
             }
-
-
         }
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(divider);
     }
+
 }
---
+++ src/main/java/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args){
+        JingHao bot = new JingHao();
+        bot.Start();
+    }
+}
