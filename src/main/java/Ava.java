1ba667e40a83e586559c94df5b41df9cf016c6b0
runxinghuan <rxinghuan@gmail.com>
2024-01-31 22:43:04 +0800

    Add echo function and customize its greet and exit functions
    Echo repeats commands entered by the user
    
    Customize its greet and exit functions to make it more unique

--- src/main/java/Ava.java
+++ src/main/java/Ava.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Ava {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -5,15 +7,42 @@ public class Ava {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        printLine();
-        System.out.println(" Hello! I'm Ava");
-        System.out.println(" What can I do for you?");
-        printLine();
-        System.out.println(" Bye. Hope to see you again soon!");
-        printLine();
+
+        greet();
+        echo();
+        exit();
+    }
+
+    public static void echo() {
+        String command;
+        Scanner in = new Scanner(System.in);
+        command = in.nextLine();
+        if (command.equals("bye")) {
+            return;
+        }
+        while (!command.equals("bye")) {
+            printLine();
+            System.out.println(command);
+            printLine();
+            command = in.nextLine();
+        }
     }
 
     public static void printLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void greet() {
+        printLine();
+        System.out.println(" Hello!!! AvavaAVA!!! Here is Ava!!!");
+        System.out.println(" Let's have a relaxing and happy chat together!!!");
+        System.out.println(" What can I do for you?");
+        printLine();
+    }
+
+    public static void exit() {
+        printLine();
+        System.out.println(" Bye!!! Hope to see you again soon!!!");
+        printLine();
     }
 }
