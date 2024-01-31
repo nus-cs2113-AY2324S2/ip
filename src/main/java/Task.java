7493f23d3e12bf358ef77ed22be9e30b0a0e921e
nur-haziq <nhaziq01@gmail.com>
2024-01-31 14:28:17 +0800

    Add Task.java
    Implemented a class to represent each task.

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+    public static int noOfTasks = 0;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+        noOfTasks += 1;
+    }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); //mark done task with "X"
+    }
+    public void markAsDone() {
+        isDone = true;
+    }
+    public void markAsNotDone() {
+        isDone = false;
+    }
+
+
+}
