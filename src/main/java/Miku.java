d371de46d903272fe329c6e51fe5244731e2dd36
eliz <tan_eliz@hotmail.com>
2024-01-31 14:52:32 +0800

    changed duke.java to miku.java

---
+++ src/main/java/Miku.java
@@ -0,0 +1,9 @@
+public class Miku {
+    public static void main(String[] args) {
+        System.out.println("______________________");
+        System.out.println("Hello! I'm Miku!\n" + "What can I do for you?");
+        System.out.println("______________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("______________________");
+    }
+}
