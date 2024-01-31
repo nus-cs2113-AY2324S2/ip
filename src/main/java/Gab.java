be024fc3422a73a41e477865f3052b2e2cc74b87
leongxingyu <leongxingyu02@gmail.com>
2024-01-31 23:18:56 +0800

    Modify code to use Todo, Deadline and Event classes
    Add 3 new cases, "todo", "deadline" and "event" to make use of the respective classes created.

--- src/main/java/Gab.java
+++ src/main/java/Gab.java
@@ -1,34 +1,8 @@
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.lang.String;
 
 public class Gab {
 
-    public static class Task { //each task represent one instance of this class
-        private String description;
-        private boolean isDone;
-
-        public Task (String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public String getDescription() {
-            return description;
-        }
-
-        public void markAsDone() { //need to choose which task to mark as done
-            isDone = true;
-        }
-
-        public void markAsNotDone() { isDone = false; }
-
-        public String getStatusIcon() { //if it is done mark as [X]
-            return (isDone ? "[X]" : "[ ]");
-        }
-    }
-
-    private static ArrayList<Task> taskList = new ArrayList<>(); //array of Task instances
+    private static Task[] taskList = new Task[100]; //array of Task instances
 
     public static void getTask() {
         String taskDescription;
@@ -38,58 +12,92 @@ public class Gab {
         checkKeywords(taskDescription); //check if its un mark/mark/exit/list
     }
 
-    public static void addToList(String taskName) { //only add to list if it is a valid task
-        Task newTask = new Task(taskName);
-        taskList.add(newTask);
-    }
-
     public static void checkKeywords (String task) { //going to input task description from getTask here
-        final String exitCode = "bye";
-        final String displayCode = "list";
-        final String markCommand = "mark";
-        final String unMarkCommand = "unmark";
-        if (task.startsWith(markCommand) || task.startsWith(unMarkCommand)) {
-            markTask(task);
+        String[] taskAction = task.split(" ");
+        String action = taskAction[0];
+
+        if (action.equals("mark") || action.equals("unmark")) {
+            markTask(task); //need display as [ ][X] task name
             getTask();
         } else {
-            switch (task) {
-            case exitCode:
+            switch (action) {
+            case "bye":
                 System.out.println("I hope you complete them!");
                 System.exit(0);
                 break;
-            case displayCode:
+            case "list":
                 listTask();
-                getTask();
                 break;
-            default:
-                displayTask(task);
-                getTask();
+            case "todo": //need display [T][ ] name
+                setToDo(task);
+                break;
+            case "deadline":
+                setDeadline(task);
+                break;
+            case "event":
+                setEvent(task);
                 break;
             }
+            getTask();
         }
     }
 
     public static void listTask() { //using the array to list the tasks
-        for (int i = 0; i < taskList.size(); i++) {
-            Task task = taskList.get(i); //instance of task in the display list
-            String taskName = task.getDescription();
-            String status = task.getStatusIcon(); //getting the status from the instance
-            System.out.println((i + 1) + ". " + status + " " + taskName);
+        System.out.println("All your tasks are here");
+        for (int i = 0; i < Task.getTaskCount(); i++) {
+            //Task task = taskList[i]; //instance of task in the display list
+            //String taskName = task.getDescription();
+            //String status = task.getStatusIcon(); //getting the status from the instance
+            System.out.println((i+1) + "." + taskList[i].toString());
+            //System.out.println((i + 1) + ". " + status + " " + taskName);
         }
     }
 
-    public static void displayTask(String task) {
-        addToList(task);
-        System.out.println("_____________");
-        System.out.println("\tadded: " + task);
-        System.out.println("_____________");
+    public static void setToDo(String task) {
+        String todoName = task.substring(5);
+        Todo newToDo = new Todo(todoName);
+        taskList[Task.getTaskCount() - 1] = newToDo; //creating new class(to do) in the array
+        System.out.println("_______________________");
+        System.out.println("Oh no! One new task added...");
+        System.out.println(newToDo.toString());
+        System.out.println("Now you have " + Task.getTaskCount() + " task(s)");
+        System.out.println("_______________________");
+    }
+
+    public static void setDeadline (String task) {
+        int deadlineIndex = task.indexOf("/by"); //gives index of /by
+        String deadline = task.substring(deadlineIndex + 4);
+        String deadlineTask = task.substring(9, deadlineIndex - 1);
+        Deadline newDeadline = new Deadline(deadlineTask, deadline);
+        taskList[Task.getTaskCount() - 1] = newDeadline;
+        System.out.println("_______________________");
+        System.out.println("Ok! Watch the deadline!");
+        System.out.println(newDeadline.toString());
+        System.out.println("Now you have " + Task.getTaskCount() + " task(s)");
+        System.out.println("_______________________");
+    }
+
+    public static void setEvent (String task) {
+        int eventStartIndex = task.indexOf("/from"); //returns index of '/' of /from
+        int eventEndIndex = task.indexOf("/to");
+        String eventName = task.substring(6, eventStartIndex - 1);
+        String eventStartDate = task.substring(eventStartIndex + 6, eventEndIndex - 1);
+        String eventEndDate = task.substring(eventEndIndex + 3);
+        Event newEvent = new Event(eventName, eventStartDate, eventEndDate);
+        taskList[Task.getTaskCount() - 1] = newEvent;
+        System.out.println("_______________________");
+        System.out.println("Weehoo! Enjoy the event!");
+        System.out.println(newEvent.toString());
+        System.out.println("Now you have " + Task.getTaskCount() + " task(s)");
+        System.out.println("_______________________");
     }
 
     public static void markTask (String tasks) {
         String[] task = tasks.split(" ");
-        String taskIndex = task[1]; //getting the task to mark or un mark
+        String taskIndex = task[1]; //getting the index task to mark or un mark
         int index = Integer.parseInt(taskIndex) - 1;
-        Task taskToMark = taskList.get(index);
+
+        Task taskToMark = taskList[index];
         String taskName = taskToMark.getDescription();
         if (task[0].equals("mark")) {
             taskToMark.markAsDone();
@@ -99,7 +107,7 @@ public class Gab {
             System.out.println("Oh no! More tasks!");
         }
         String status = taskToMark.getStatusIcon();
-        System.out.println(status + " " + taskName);
+        System.out.println(taskToMark.toString());
     }
 
     public static void main(String[] args) {
@@ -116,7 +124,6 @@ public class Gab {
         System.out.println("\tAnything I can help you with?");
 
         Gab.getTask();
-        //Gab.checkKeywords(task);
     }
 }
 
