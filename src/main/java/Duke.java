820c407df25ea7e28ce2227e27b4a4e668c19ea4
PureUsagi <ngjunhancanoe@gmail.com>
2024-01-31 23:55:29 +0800

    Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,30 @@
-public class Duke {
-    public static void main(String[] args) {
-        String welcome = "--------------------------------------------------\n"
-                + "Hello! I'm Misty\n"
-                + "What can I do for you?\n";
+import java.util.Scanner;
 
-        String bye = "--------------------------------------------------\n"
-                + "Bye. Hope to see you again soon!"
-                + "\n"
-                + "--------------------------------------------------\n";
-        System.out.println(welcome);
-        System.out.println(bye);
+public class Duke {
+    public static void printReply (String input) {
+        System.out.println("\t--------------------------------------------------");
+        System.out.println("\t" + input);
+        System.out.println("\t--------------------------------------------------");
+    }
+
+    public static void main(String[] args) {
+        String welcome = "Hello! I'm Misty\n"
+                + "\tWhat can I do for you?";
+        String bye = "Bye. Hope to see you again soon!";
+
+        printReply(welcome);
+
+        String input;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            input = in.nextLine();
+
+            if (input.equals("bye")) {
+                printReply(bye);
+                break;
+            } else {
+                printReply(input);
+            }
+        }
     }
 }
