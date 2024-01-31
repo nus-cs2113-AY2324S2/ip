49f7b75b07a001c1a299e389adba383728e2a4db
Leong-ZM <e0957648@u.nus.edu>
2024-01-31 09:16:39 +0800

    Add Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -8,12 +8,27 @@
         System.out.println("Hello from\n" + logo);
     }
 }*/
-
+import java.util.Scanner;
 public class Duke {
+    public static boolean respond(String input) {
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon! ");
+            return false;
+        } else {
+            System.out.println(input);
+            return true;
+        }
+    }
     public static void main(String[] args) {
         String chatbotName = "Horizon";
-        System.out.println(" Hello! I'm " + chatbotName);
-        System.out.println(" What can I do for you? ");
-        System.out.println(" Bye. Hope to see you again soon! ");
+        System.out.println("Hello! I'm " + chatbotName);
+        System.out.println("What can I do for you? ");
+
+        String input;
+        Scanner in = new Scanner((System.in));
+        do {
+            input = in.nextLine();
+        } while (respond(input));
+
     }
 }
