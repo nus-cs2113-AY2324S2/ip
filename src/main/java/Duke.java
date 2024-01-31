54706173e36e59e586bb3d88c673d88d06b54879
Cohii <hengjunxiangg@gmail.com>
2024-01-31 23:18:07 +0800

    Greet and Exit
    Implemented greet method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,19 @@
 public class Duke {
+    public static String chatbotName = "Noriaki";
+    public static int lineUnderscore = 30;
+    public static void printLine(){
+        for(int i = 0; i < lineUnderscore; i++){
+            System.out.print("_");
+        }
+        System.out.println();
+    }
+    public static void greet(){
+        printLine();
+        System.out.println("Hello! I'm " + chatbotName + "\nWhat can I do for you?");
+        printLine();
+        System.out.println("Hope to see you again soon!");
+        printLine();
+    }
     public static void main(String[] args) {
         String logo =
                 " _______               .__        __   .__ \n" +
@@ -8,5 +23,6 @@ public class Duke {
                 "\\____|__  /\\____/|__|  |__(____  /__|_ \\__|\n" +
                 "        \\/                     \\/     \\/   \n";
         System.out.println("Hello from\n" + logo);
+        greet();
     }
 }
