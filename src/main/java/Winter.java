f06616ff116b05c4dd2014ffc75233daa79850fc
zhengwinter <e0958239@u.nus.edu>
2024-01-31 22:43:58 +0800

    Implement echo, restructure hi and bye

--- src/main/java/Winter.java
+++ src/main/java/Winter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Winter {
     public static void main(String[] args) {
         String logo = "  __      __.__        __                \n" +
@@ -6,21 +8,46 @@ public class Winter {
                 " \\        /|  |   |  \\  | \\  ___/|  | \\/\n" +
                 "  \\__/\\  / |__|___|  /__|  \\___  >__|   \n" +
                 "       \\/          \\/          \\/    ";
+
         System.out.println("Hello from\n" + logo);
-        hiandbye();
+        hi();
+        echo();
+        bye();
+
     }
 
     /* Function for greeting and farewell messages*/
-    private static void hiandbye() {
+    private static void hi() {
         String line = "-----------------------------------\n";
-        String greet = "Hello! I'm Winter!\nWhat can I do for you?\n";
-        String farewell = "Bye. Hope to see you again soon!\n";
+        String greet = "Hello! I'm Winter!\nWhat can I do for you?";
         System.out.print(line);
         System.out.println(greet);
         System.out.print(line);
+    }
+    private static void bye() {
+        String line = "-----------------------------------\n";
+        String farewell = "Bye. Hope to see you again soon!";
+        System.out.print(line);
         System.out.println(farewell);
         System.out.print(line);
-
     }
 
+    private static void echo() {
+        String line = "-----------------------------------\n";
+        String indent = "   ";
+        String echoLine;
+        Scanner input = new Scanner(System.in);
+        echoLine = input.nextLine();
+        while (true) {
+            if(echoLine.equals("bye") || echoLine.equals("Bye") || echoLine.equals("BYE")) {
+                break;
+            }
+            System.out.print(line);
+            System.out.print(indent);
+            System.out.println(echoLine);
+            System.out.print(line);
+            echoLine = input.nextLine();
+        }
+
+    }
 }
