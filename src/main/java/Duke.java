bf4726329ca0ad3b297941e6582cf3d50a8514b9
Lin Htet <earthangle007@gmail.com>
2024-01-31 23:48:21 +0800

    make chatbot interactive with marking or unmarking tasks in the list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,7 +6,7 @@ public class Duke {
                 + "What can I do for you?\n"
                 + "_________________________";
         System.out.println(chatBot);
-        String[] userCommands = new String[100];
+        Task[] toDo = new Task[100];
         int count = 0;
         while(true){
             Scanner userInput = new Scanner(System.in);
@@ -14,24 +14,47 @@ public class Duke {
             if(task.equals("bye")){
                 System.out.println(
                         "_________________________\n"
-                        + "Bye. Hope to see you again soon!\n"
-                        + "_________________________\n");
+                                + "Bye. Hope to see you again soon!\n"
+                                + "_________________________\n");
                 return;
             } else if(task.equals("list")){
                 System.out.println("_________________________");
                 System.out.println("\tYour List:");
                 for(int i = 0; i < count; i++){
-                    System.out.println("\t" + (i+1) + ". "
-                            + userCommands[i]);
+                    System.out.println("\t" + (i+1) + ". ["
+                            + toDo[i].getStatusIcon()
+                            + "] "
+                            + toDo[i].description);
                 }
                 System.out.println("_________________________");
-            }else{
-                userCommands[count] = task;
+            } else if(task.contains("mark")){
+                String[] subCommand = task.split(" ");
+                int num = Integer.parseInt(subCommand[1]);
+                if(subCommand[0].equals("mark")){
+                    toDo[num-1].markDone();
+                    System.out.println(
+                            "_________________________\n"
+                                    + "\tNice! I've marked this task as done:\n"
+                                    + "[" + toDo[num-1].getStatusIcon() + "] "
+                                    + toDo[num-1].description
+                                    + "\n_________________________");
+                } else{
+                    toDo[num-1].markNotDone();
+                    System.out.println(
+                            "_________________________\n"
+                                    + "\tOK, I've marked this task as not done yet:\n"
+                                    + "[" + toDo[num-1].getStatusIcon() + "] "
+                                    + toDo[num-1].description
+                                    + "\n_________________________");
+                }
+            } else{
+                Task tasks = new Task(task);
+                toDo[count] = tasks;
                 System.out.println(
                         "_________________________\n"
-                        + "\tadded: " + userCommands[count]
-                        + "\n"
-                        + "_________________________\n");
+                                + "\tadded: " + task
+                                + "\n"
+                                + "_________________________\n");
                 count++;
             }
         }
