ee280403b1ee2459ba3fce2a94d5607af6ea1be6
nikhil-2101 <nikhilaaditya.babu@gmail.com>
2024-01-31 21:53:31 +0800

    Add,List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,35 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
-    public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+    public static void main(String[] args) {{
+            System.out.println("____________________________________________________________");
 
-        Scanner scanner = new Scanner(System.in);
+            Scanner scanner = new Scanner(System.in);
+            ArrayList<String> tasks = new ArrayList<>();
 
-        while (true) {
-            String userInput = scanner.nextLine();
+            while (true) {
+                String userInput = scanner.nextLine();
 
-            if ("bye".equalsIgnoreCase(userInput)) {
-                break;
+                if ("bye".equalsIgnoreCase(userInput)) {
+                    break;
+                }
+
+                System.out.println("____________________________________________________________");
+                System.out.println(userInput);
+                System.out.println("____________________________________________________________");
+                if ("list".equalsIgnoreCase(userInput)) {
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < tasks.size(); i++) {
+                        System.out.println((i + 1) + ". " + tasks.get(i));
+                    }
+                    System.out.println("____________________________________________________________");
+                } else {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("added: " + userInput);
+                    System.out.println("____________________________________________________________");
+                    tasks.add(userInput);
+                }
             }
 
             System.out.println("____________________________________________________________");
-            System.out.println(userInput);
-            System.out.println("____________________________________________________________");
-        }
-
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("_____________________________________________________________");
-    }
-}
