661ab422924645a996faf1fdb72ed154f9f48db8
edwardhumi <edward.humianto@gmail.com>
2024-01-31 19:32:11 +0800

    Add Level-1 Echo

--- src/main/java/Humi.java
+++ src/main/java/Humi.java
@@ -1,12 +1,26 @@
+import java.util.Scanner;
+
 public class Humi {
+    public static final String LINE = "____________________________________________________________";
+        //    String logo = " ____        _        \n"
+        //                + "|  _ \\ _   _| | _____ \n"
+        //                + "| | | | | | | |/ / _ \\\n"
+        //                + "| |_| | |_| |   <  __/\n"
+        //                + "|____/ \\__,_|_|\\_\\___|\n";
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println(LINE);
         System.out.println("Hello! I'm Humi");
         System.out.println("What can I do for you?");
+        System.out.println(LINE);
+
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(LINE);
+            System.out.println(input);
+            System.out.println(LINE);
+            input = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
