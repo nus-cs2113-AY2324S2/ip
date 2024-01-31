7e3eb8d783f397e778c48ce414e878ba84e25082
MaJiajun <majiajun1106@gmail.com>
2024-01-31 22:17:14 +0800

    Level-0

---
+++ src/main/java/Ma.java
@@ -0,0 +1,17 @@
+public class Ma {
+    private static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        String logo = "🐎  __  __    _    🐎\n"
+                + "   |  \\/  |  / \\   \n"
+                + "   | |\\/| | / _ \\  \n"
+                + "   | |  | |/ ___ \\ \n"
+                + "🐎 |_|  |_/_/   \\_\\🐎\n"
+                + "What can I help you with？";
+        System.out.println("Hello from\n" + logo);
+        printLine();
+        System.out.println("See you again! Goodbye!\n");
+    }
+}
