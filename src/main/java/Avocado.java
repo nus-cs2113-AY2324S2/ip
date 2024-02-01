9d0e2b5083e40065e2a596c48fcbe9a35671c319
cayennegoh <cayennegoh@gmail.com>
2024-02-01 23:46:37 +0800

    Level 0

---
+++ src/main/java/Avocado.java
@@ -0,0 +1,7 @@
+public class Avocado {
+    public static void main(String[] args) {
+        System.out.println("Hello from avocado");
+        System.out.println("How can I help you ?");
+        System.out.println("See you!");
+    }
+}
