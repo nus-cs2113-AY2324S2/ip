8c07a1ade3c435c8128e39a3eb6e1550e07bddcb
seandooa <seandooa@yahoo.com.sg>
2024-02-02 00:57:52 +0800

    Add echo functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+        String line;
+        Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Bobby\n" + "What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        do {
+            line = in.nextLine();
+            if (!line.equals("bye")) {
+                System.out.println(line);
+            }
+        } while (!line.equals("bye"));
+
+        System.out.println("Bye. Hope to see you again soon!\n");
     }
 }
