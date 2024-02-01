6d647b67eee9f59223539cc5675a440a4a6f663b
Jason Lienardi <lienardijason@gmail.com>
2024-02-01 23:44:56 +0800

    Add Add,List

--- src/main/java/Xavier.java
+++ src/main/java/Xavier.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Xavier {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        String[] itemList = new String[100];
+        int numberOfItems = 0;
+
         System.out.println("---------------------------------------------------------------------");
         System.out.println("Hello! I'm: " + "Xavier");
         System.out.println("What can I do for you?");
@@ -16,9 +19,18 @@ public class Xavier {
                 System.out.println("---------------------------------------------------------------------");
                 return;
             }
-            else {
+            else if (command.equals("list")) {
                 System.out.println("---------------------------------------------------------------------");
-                System.out.println(command);
+                for (int i = 0; i < numberOfItems; i++) {
+                    System.out.println(i+1 + ". " + itemList[i]);
+                }
+                System.out.println("---------------------------------------------------------------------");
+            }
+            else {
+                itemList[numberOfItems] = command;
+                numberOfItems += 1;
+                System.out.println("---------------------------------------------------------------------");
+                System.out.println("added: " + command);
                 System.out.println("---------------------------------------------------------------------");
             }
         }
