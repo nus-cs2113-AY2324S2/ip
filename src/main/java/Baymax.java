70cca0a434470d3cfcf4debf25669bf81a1927f0
isaaceng7 <>
2024-02-01 23:22:58 +0800

    Create Task class

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -5,7 +5,7 @@ public class Baymax {
         String name = "Baymax";
         System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n\n");
 
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int num = 0;
 
         while(true) {
@@ -17,18 +17,33 @@ public class Baymax {
             if (text.equalsIgnoreCase("bye")) {
                 break;
             }
+            else if (text.startsWith("mark")) {
+                int position = Integer.parseInt(text.substring(5)) - 1;
+                list[position].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("[X] " + list[position].description);
+            }
+
+            else if (text.startsWith("unmark")) {
+                int position = Integer.parseInt(text.substring(7)) - 1;
+                list[position].isDone = false;
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("[ ] " + list[position].description);
+            }
+
             else if (text.equalsIgnoreCase("list")) {
                 for (int i = 0; i < 100; i++) {
 
                     if(list[i] == null) {
                         break;
                     }
-                    System.out.println(i+1 + ". " + list[i]);
+                    System.out.println(i+1 + ". " + "[" + list[i].getStatusIcon() + "] " + list[i].description);
                 }
             }
             else {
                 System.out.println("added: " + text);
-                list[num] = text;
+
+                list[num] = new Task(text);
                 num++;
             }
         }
