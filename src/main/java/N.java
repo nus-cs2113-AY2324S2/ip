0023ac55a56c977d6411c3395487f9e6ea2e2065
anneleong <anneleongsongen@gmail.com>
2024-02-01 15:23:33 +0800

    Modify echo function to create and display tasklist

--- src/main/java/N.java
+++ src/main/java/N.java
@@ -11,14 +11,32 @@ public class N {
         printLine();
     }
 
-    public static void echo() {
+    public static void printAsList(String[] taskList) {
+        if (taskList[0] == null) {
+            printMessage("no tasks to complete");
+        } else {
+            printLine();
+            for(int i = 0; taskList[i] != null; i++) {
+                System.out.println(i+1 + ". " +taskList[i]);
+            }
+            printLine();
+        }
+    }
+
+    public static void makeTaskList(String[] taskList, int nextIndex) {
         Scanner in = new Scanner(System.in);
         String message = in.nextLine();
         if (message.equalsIgnoreCase("bye")) {
             printMessage("Bye. Hope to see you again soon!");
-        } else {
-            printMessage(message);
-            echo();
+        }
+        else if (message.equalsIgnoreCase("list")) {
+            printAsList(taskList);
+            makeTaskList(taskList, nextIndex);
+        }
+        else {
+            printMessage("added: " +message.trim());
+            taskList[nextIndex] = message.trim();
+            makeTaskList(taskList, nextIndex + 1);
         }
     }
 
@@ -34,6 +52,7 @@ public class N {
         System.out.println("    Hello! I'm N :) \n" + "    What can I do for you? \n");
         printLine();
 
-        echo();
+        String[] taskList = new String[100];
+        makeTaskList(taskList, 0);
     }
 }
