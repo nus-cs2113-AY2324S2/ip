edd348dbf4a063872de89e1e0e755ed3f5e1307c
kirangeofran <kirangeofran123@gmail.com>
2024-02-01 10:43:42 +0800

    Level-0 increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,14 @@
-public class Duke {
+public class MyChatBot {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+
+        String ChatBotName = "Rose" ;
+        System.out.println("__________________________________________");
+        System.out.println("Hello! I'm " + ChatBotName);
+        System.out.println("What can I do for you?") ;
+        System.out.println("__________________________________________");
+
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("__________________________________________");
     }
 }
+
