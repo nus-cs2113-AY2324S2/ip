e03e8a7c59e98cb9cd3b5dd4fb637bbd29dc1580
ChinYanXu <yanxu6267@gmail.com>
2024-02-02 00:23:39 +0800

    Add markAsDone and markAsUndone functionality and Task class

--- src/main/java/Serf.java
+++ src/main/java/Serf.java
@@ -1,19 +1,22 @@
 import java.util.Scanner;
-
 public class Serf {
     public static void main(String[] args) {
         String VERTICALLINES = "    ____________________________________________________________";
-        String[] taskList = new String[100];
+        String FIVEWHITESPACES = "     ";
+        Task[] taskList = new Task[100];
         int taskCounter = 0;
         String chatBotName = "Serf";
         String GREETINGMESSAGE = VERTICALLINES
                 + "\n"
-                + "     Good day my lord! I'm " + chatBotName + "\n"
-                + "     How shall I serve thee?\n"
+                + FIVEWHITESPACES
+                + "Good day my lord! I'm " + chatBotName + "\n"
+                + FIVEWHITESPACES
+                + "How shall I serve thee?\n"
                 + VERTICALLINES;
         String FAREWELLMESSAGE = VERTICALLINES
                 + "\n"
-                + "     Farewell my lord. \n"
+                + FIVEWHITESPACES
+                + "Farewell, my lord. \n"
                 + VERTICALLINES;
         System.out.println(GREETINGMESSAGE);
         Scanner requestedMessage = new Scanner(System.in);
@@ -22,23 +25,65 @@ public class Serf {
         while(!receivedMessage.equals("bye")) {
             if (receivedMessage.equals("list") && taskCounter > 0) {
                 System.out.println(VERTICALLINES);
+                System.out.println(FIVEWHITESPACES + "My lord, here are the tasks as recorded in thy list");
                 for (int i = 0; i < taskCounter; i += 1) {
-                    System.out.println("     " + (i + 1) + ". " + taskList[i] + " ");
+                    System.out.println(FIVEWHITESPACES + (i + 1) + "." + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].description + " ");
                 }
                 System.out.println(VERTICALLINES);
             }
             else if (receivedMessage.equals("list") && taskCounter == 0){
                 System.out.println(VERTICALLINES);
-                System.out.println("     " + "list is empty");
+                System.out.println(FIVEWHITESPACES + "list is empty");
                 System.out.println(VERTICALLINES);
                 receivedMessage = requestedMessage.nextLine();
                 continue;
             }
+            else if (receivedMessage.contains("mark") && !receivedMessage.contains("unmark")) {
+                String number = "";
+                for (int j = 0; j < receivedMessage.length(); j += 1) {
+                    if (Character.isDigit(receivedMessage.charAt(j))) {
+                        number += receivedMessage.charAt(j);
+                    }
+                }
+                if (number.isEmpty()) {
+                    System.out.println(FIVEWHITESPACES + "Error");
+                    continue;
+                }
+                else {
+                    taskList[Integer.parseInt(number) - 1].markAsDone();
+                }
+                System.out.println(VERTICALLINES);
+                System.out.println(FIVEWHITESPACES + "Acknowledged sire, I've marked this task as complete");
+                System.out.println(FIVEWHITESPACES + "  " + "[" + taskList[Integer.parseInt(number) - 1].getStatusIcon() + "] " + taskList[Integer.parseInt(number) - 1].getDescription());
+                System.out.println(VERTICALLINES);
+
+            }
+
+            else if (receivedMessage.contains("unmark")) {
+                String number = "";
+                for (int j = 0; j < receivedMessage.length(); j += 1) {
+                    if (Character.isDigit(receivedMessage.charAt(j))) {
+                        number += receivedMessage.charAt(j);
+                    }
+                }
+                if (number.isEmpty()) {
+                    System.out.println(FIVEWHITESPACES + "Error");
+                    continue;
+                }
+                else {
+                    taskList[Integer.parseInt(number) - 1].markAsUndone();
+                }
+                System.out.println(VERTICALLINES);
+                System.out.println(FIVEWHITESPACES + "Acknowledged sire, I've marked this task as incomplete");
+                System.out.println(FIVEWHITESPACES + "  " + "[" + taskList[Integer.parseInt(number) - 1].getStatusIcon() + "] " + taskList[Integer.parseInt(number) - 1].getDescription());
+                System.out.println(VERTICALLINES);
+            }
+
             else {
-                taskList[taskCounter] = receivedMessage;
+                taskList[taskCounter] = new Task(receivedMessage);
                 taskCounter += 1;
                 System.out.println(VERTICALLINES);
-                System.out.println("     " + "added: " + receivedMessage);
+                System.out.println(FIVEWHITESPACES + "added: " + receivedMessage);
                 System.out.println(VERTICALLINES);
             }
             receivedMessage = requestedMessage.nextLine();
