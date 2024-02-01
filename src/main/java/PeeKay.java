7f5bf604a918825d089b630fd523aa0851a89268
paturikarthik <paturi.karthik1@gmail.com>
2024-02-02 03:20:06 +0800

    Level-2

--- src/main/java/PeeKay.java
+++ src/main/java/PeeKay.java
@@ -1,21 +1,32 @@
 import java.util.Scanner;
 public class PeeKay {
-    static String line = "____________________________________________________________";
+    static String indent = "  ";
+    static String line = "  ____________________________________________________________";
     public static void echo(String input){
         if(input.equals("bye")){
-            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(indent + "Bye. Hope to see you again soon!");
         }else {
-            System.out.println("  " + input);
+            System.out.println(indent + "added: " +input);
 
         }
     }
 
     public static void chat(){
         String input;
+        String[] list = new String[100];
+        int count = 0;
         do {
             input = new Scanner(System.in).nextLine();
             System.out.println(line);
-            echo(input);
+            if (input.equals("list")){
+                for(int x = 0; x < count;x++){
+                    System.out.println(indent + (x+ 1)+"."+" "+list[x]);
+                }
+            }else {
+                list[count] = input;
+                count++;
+                echo(input);
+            }
             System.out.println(line);
         } while(!input.equals("bye"));
     }
@@ -23,8 +34,8 @@ public class PeeKay {
     public static void main(String[] args) {
 
         System.out.println(line);
-        System.out.println("Hello! I'm PeeKay");
-        System.out.println("What can I do for you?");
+        System.out.println(indent + "Hello! I'm PeeKay");
+        System.out.println(indent + "What can I do for you?");
         System.out.println(line);
         chat();
     }
