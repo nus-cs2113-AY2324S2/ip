ed4723bce4d9d19bc90f4cc2e334646835c05e99
nkotaa <e0957774@u.nus.edu>
2024-02-01 21:15:01 +0800

    Add Task Adding & Listing Behaviour

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,22 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Duke {
+    // assume no more than 100 tasks for now
+    private static String[] tasks = new String[100];
+    private static int numTasks = 0;
     public static void respond(String query) {
-        // Echo query for now
-        System.out.println(query);
+        switch (query) {
+        case "list":
+            for (int i = 0; i < numTasks; i += 1) {
+                System.out.println(Integer.toString(i + 1) + ". " + tasks[i]);
+            }
+            break;
+        default:
+            tasks[numTasks % 100] = query;
+            numTasks += 1;
+            System.out.println("added: " + query);
+        }
     }
     public static void main(String[] args) {
         System.out.println("Greetings! I am Blue, your personal chatbot assistant. How may I help you?");
