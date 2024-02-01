27a62e78cd685d0f9b0801fab0d49a0dbdd95314
fungg0 <fungfunwenwu@gmail.com>
2024-02-01 23:35:06 +0800

    Able to store and display text

--- src/main/java/Allez.java
+++ src/main/java/Allez.java
@@ -2,6 +2,18 @@ import java.util.Scanner;
 
 
 public class Allez {
+
+    public static void printList(String[] list){
+        int count = 0;
+        while(list[count]!=null){
+            System.out.println((count+1) + ". " + list[count]);
+            count+=1;
+        }
+    }
+
+//    public static void storeInList(String[] list, int count, String line) {
+//        list[count] = line;
+//    }
     public static void main(String[] args) {
 
         System.out.println("_________________________");
@@ -10,8 +22,22 @@ public class Allez {
         System.out.println("_________________________");
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
+        String[] list = new String[100];
+        int itemsInList = 0;
+
+
         while (!line.equals("bye")){
-            System.out.println(line);
+            switch(line){
+                case "list":
+                    //method to list out items
+                    printList(list);
+                    break;
+                default:
+                    //method to store items;
+                    list[itemsInList] = line;
+                    System.out.println("added: " + list[itemsInList]);
+                    itemsInList+=1;
+            }
             line = in.nextLine();
         }
         System.out.println("Bye. Hope to see you again soon!");
