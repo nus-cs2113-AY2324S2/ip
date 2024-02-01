07258dc361a16d8accb11487ce9b780981d8ecb9
kirangeofran <kirangeofran123@gmail.com>
2024-02-01 10:58:09 +0800

    Level-1 Increment

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,14 +1,34 @@
+import java.util.Scanner;
+
 public class MyChatBot {
     public static void main(String[] args) {
+        String chatBotName = "Rose";
+        Scanner scanner = new Scanner(System.in);
 
-        String ChatBotName = "Rose" ;
         System.out.println("__________________________________________");
-        System.out.println("Hello! I'm " + ChatBotName);
-        System.out.println("What can I do for you?") ;
+        System.out.println("Hello! I'm " + chatBotName);
+        System.out.println("What can I do for you?");
         System.out.println("__________________________________________");
 
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("__________________________________________");
+        // Input loop
+        while (true) {
+            String userInput = scanner.nextLine().trim(); 
+
+            // Echo user input
+            System.out.println("__________________________________________");
+            System.out.println(" " + userInput);
+            System.out.println("__________________________________________");
+
+            // Check for the bye command to exit
+            if (userInput.equalsIgnoreCase("bye")) {
+                System.out.println("__________________________________________");
+                System.out.println(" Byeeee. Hope to see you again soon!");
+                System.out.println("__________________________________________");
+                break; // Exit the loop
+            }
+        }
+
+
+        scanner.close();
     }
 }
-
