dff214c5a1eacbed9f34f4895e62a9798116b74c
anneleong <anneleongsongen@gmail.com>
2024-02-01 14:26:07 +0800

    Add basic chatbot greeting

---
+++ src/main/java/N.java
@@ -0,0 +1,20 @@
+public class N {
+
+    public static void printLine() {
+        System.out.println("____________________________________________________________ \n");
+    }
+    public static void main(String[] args) {
+        String logo = " ____        _        \n"
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
+        System.out.println("Hello from\n" + logo);
+
+        printLine();
+        System.out.println("Hello! I'm N :) \n" + "What can I do for you? \n");
+        printLine();
+        System.out.println("Bye. Hope to see you again soon! \n");
+        printLine();
+    }
+}
