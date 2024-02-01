901fc3df08dcf19891023cf657eaeb81ef6637d5
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-02-01 17:04:13 +0800

    Add echo functionality

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Spike {
     public static void main(String[] args) {
         String chatbot = "Spike";
         System.out.println("Hello! I'm " + chatbot);
         System.out.println("What can I do for you?\n");
+
+        while(true){
+            Scanner in = new Scanner(System.in);
+            String input = in.nextLine();
+            if(input.contentEquals("bye")){
+                break;
+            }
+
+            System.out.println(input);
+
+        }
+
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
