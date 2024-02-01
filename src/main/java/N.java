3750028284a7577d482206b53990801908a09612
anneleong <anneleongsongen@gmail.com>
2024-02-01 14:48:15 +0800

    Add echo function

--- src/main/java/N.java
+++ src/main/java/N.java
@@ -1,8 +1,27 @@
+import java.util.Scanner;
 public class N {
 
     public static void printLine() {
-        System.out.println("____________________________________________________________ \n");
+        System.out.println("    ____________________________________________________________ \n");
     }
+
+    public static void printMessage(String message) {
+        printLine();
+        System.out.println("    " +message);
+        printLine();
+    }
+
+    public static void echo() {
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();
+        if (message.equalsIgnoreCase("bye")) {
+            printMessage("Bye. Hope to see you again soon!");
+        } else {
+            printMessage(message);
+            echo();
+        }
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -12,9 +31,9 @@ public class N {
         System.out.println("Hello from\n" + logo);
 
         printLine();
-        System.out.println("Hello! I'm N :) \n" + "What can I do for you? \n");
+        System.out.println("    Hello! I'm N :) \n" + "    What can I do for you? \n");
         printLine();
-        System.out.println("Bye. Hope to see you again soon! \n");
-        printLine();
+
+        echo();
     }
 }
