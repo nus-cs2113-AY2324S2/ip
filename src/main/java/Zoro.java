abdcb8d291c5700fa150cc7ef2d2c2de62257a24
Vavinan <vavithamarai2000@gmail.com>
2024-02-02 01:24:45 +0800

    Add Task Management Feature

--- src/main/java/Zoro.java
+++ src/main/java/Zoro.java
@@ -9,47 +9,80 @@ public class Zoro {
         String name = "Zoro";
 
         System.out.println(LINE);
-        System.out.println(TAB_SPACE + "Hello I'm "+ name);
+        System.out.println(TAB_SPACE + "Hello I'm " + name);
         System.out.println(TAB_SPACE + "What can I do for you?");
         System.out.println(LINE);
 
-        String[] taskLists = new String[MAX_TASKS];
+        Task[] taskLists = new Task[MAX_TASKS];
         int index = 0;
         Scanner in = new Scanner(System.in);
 
-        while(true){
+        while (true){
             String input;
-            input = in.nextLine();
-            if(input.equalsIgnoreCase("bye")){
+            input = in.nextLine().trim();
+
+            if (input.equalsIgnoreCase("bye")) {
                 break;
             }
 
-            if(input.equalsIgnoreCase("list" )) {
+            if (input.equalsIgnoreCase("list")) {
                 int count = 0;
                 System.out.println(LINE);
                 System.out.println(TAB_SPACE + "Here are the tasks in your list:");
-                while(count < MAX_TASKS && taskLists[count] != null){
-                    System.out.println(TAB_SPACE + (count + 1) +". " + taskLists[count]);
+
+                while (count < MAX_TASKS && taskLists[count] != null) {
+                    System.out.println(TAB_SPACE + (count + 1) + "." + taskLists[count].getStatusIcon() + " " + taskLists[count].description);
                     count++;
                 }
                 System.out.println(LINE);
-                continue;
-            }
-            if(index<MAX_TASKS) {
 
-                taskLists[index++] = input;
+            } else if (input.startsWith("mark ")) {
+
+                int taskIndex = Integer.parseInt(input.substring(5).trim()) - 1;
+
+                if (taskIndex >= index) {
+                    System.out.println(LINE);
+                    System.out.println(TAB_SPACE + "Index out of bound for marking. Check your index properly");
+                    System.out.println(LINE);
+                    continue;
+                }
+
+                taskLists[taskIndex].setAsDone();
                 System.out.println(LINE);
-                System.out.println(TAB_SPACE + "added: "+input);
+                System.out.println(TAB_SPACE + "Nice! I've marked this task as done:");
+                System.out.println(TAB_SPACE + taskLists[taskIndex].getStatusIcon() + " " + taskLists[taskIndex].description);
+                System.out.println(LINE);
+
+            } else if (input.startsWith("unmark ")) {
+
+                int taskIndex = Integer.parseInt(input.substring(7).trim()) - 1;
+                if (taskIndex >= index) {
+                    System.out.println(LINE);
+                    System.out.println(TAB_SPACE + "Index out of bound for to unmark. Check your index properly");
+                    System.out.println(LINE);
+                    continue;
+                }
+                taskLists[taskIndex].setAsNotDone();
+
+                System.out.println(LINE);
+                System.out.println(TAB_SPACE + "OK, I've marked this task as not done yet:");
+                System.out.println(TAB_SPACE + taskLists[taskIndex].getStatusIcon() + " " + taskLists[taskIndex].description);
                 System.out.println(LINE);
             } else {
-                System.out.println(LINE);
-                System.out.println(TAB_SPACE + "Reached maximum limit for adding tasks");
-                System.out.println(LINE);
+                if (index<MAX_TASKS){
+                    taskLists[index++] = new Task(input);
+                    System.out.println(LINE);
+                    System.out.println(TAB_SPACE + "added: " + input);
+                    System.out.println(LINE);
+                } else{
+                    System.out.println(LINE);
+                    System.out.println(TAB_SPACE + "Reached maximum limit for adding tasks");
+                    System.out.println(LINE);
+                }
             }
-
         }
-        in.close();
 
+        in.close();
         System.out.println(LINE);
         System.out.println(TAB_SPACE + "Bye. Hope to see you again soon!");
         System.out.println(LINE);
