6a1c87df7f0e43cd5ee09dc8496dcc1f54c1e784
Incy <85068563+IncyBot@users.noreply.github.com>
2024-02-01 20:04:32 +0800

    Added Echo

--- src/main/java/Incy.java
+++ src/main/java/Incy.java
@@ -1,9 +1,13 @@
+import java.util.Scanner;
+
 public class Incy {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String input;
         String logo = " ____  _  _  ___  _  _ \n"
-                + "(_  _)( \\( )/ __)( \\/ )\n"
-                + " _)(_  )  (( (__  \\  / \n"
-                + "(____)(_)\\_)\\___) (__) \n";
+                    + "(_  _)( \\( )/ __)( \\/ )\n"
+                    + " _)(_  )  (( (__  \\  / \n"
+                    + "(____)(_)\\_)\\___) (__) \n";
         
         System.out.println("____________________________________________________________\n"
                             + "Hello! I'm\n" + logo
@@ -11,7 +15,21 @@ public class Incy {
                             + "____________________________________________________________"
         );
         
-        System.out.println("Bye. Hope to see you again soon!"
-                            + "\n____________________________________________________________");
+        while (true) {
+            input = scanner.nextLine();
+            if (input.equals("bye")) {
+                break;
+            }
+            System.out.println("____________________________________________________________\n"
+                                + input
+                                + "\n____________________________________________________________"
+            );
+        }
+        
+        System.out.println("____________________________________________________________\n"
+                            + "Bye. Hope to see you again soon!"
+                            + "\n____________________________________________________________"
+        );
+        scanner.close();
     }
 }
