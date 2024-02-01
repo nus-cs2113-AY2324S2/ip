961d8925ce42a07ca6019fe3537df455cbd43cee
Zhang Yangda <zhang.yangda16@gmail.com>
2024-02-02 01:10:55 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____  _    _  ____ _____ ___  ____  \n"
@@ -5,12 +7,26 @@ public class Duke {
                     + "| | | | |  | | |     | || | | | | | |\n"
                     + "| |_| | |__| | |___  | || |_| | |_| |\n"
                     + "|____/ \\____/ \\____| |_| \\___/|____/ \n";
-        System.out.println("Hello from Doctod\r\n" + //
-                " Hello! I'm Ductod\r\n" + //
-                " What can I do for you?\r\n" + //
-                "____________________________________________________________\r\n" + //
-                " Bye. Hope to see you again soon!\r\n" + //
-                "____________________________________________________________\r\n" + //
+        System.out.println("Hello from Duke\n" + 
+                " Hello! I'm Duke\n" + 
+                " What can I do for you?\n" + 
+                "____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        String line;
+
+        do {
+            line = in.nextLine();
+            System.out.println("____________________________________________________________");
+            if (!line.equals("bye")) {
+                System.out.println(line);
+                System.out.println("____________________________________________________________");
+            }
+        } while (!line.equals("bye"));
+
+        System.out.println(" Bye. Hope to see you again soon!\n" + 
+                "____________________________________________________________\n" + 
                 "\n" + logo);
+        in.close();
     }
 }
