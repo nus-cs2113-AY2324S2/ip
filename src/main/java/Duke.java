623f216128f148077478b6bebbea3f4b140124c8
shawnpong <110764881+shawnpong@users.noreply.github.com>
2024-02-01 22:45:12 +0800

    Added echo functionality for George

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
 //        String logo = " ____        _        \n"
@@ -7,7 +9,14 @@ public class Duke {
 //                + "|____/ \\__,_|_|\\_\\___|\n";
         String name = "George";
         System.out.println("Hello! I'm " + name);
-        System.out.println("What can I do for you?");
+//        System.out.println("What can I do for you?");
+
+        Scanner scanner = new Scanner(System.in);
+        String line;
+        System.out.println("Say anything and I shall repeat it. Type 'bye' to exit.");
+        while (!"bye".equals((line = scanner.nextLine()))) {
+            System.out.println(line);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
