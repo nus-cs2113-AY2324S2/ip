416d3213224138b4a357616beffa1888aca7734c
isaaceng7 <>
2024-02-01 22:53:25 +0800

    Add, List

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -5,19 +5,35 @@ public class Baymax {
         String name = "Baymax";
         System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n\n");
 
+        String[] list = new String[100];
+        int num = 0;
+
         while(true) {
             String text;
+
             Scanner in = new Scanner(System.in);
             text = in.nextLine();
 
-            if(text.equalsIgnoreCase("bye")) {
+            if (text.equalsIgnoreCase("bye")) {
                 break;
             }
+            else if (text.equalsIgnoreCase("list")) {
+                for (int i = 0; i < 100; i++) {
 
-            System.out.println(text);
+                    if(list[i] == null) {
+                        break;
+                    }
+                    System.out.println(i+1 + ". " + list[i]);
+                }
+            }
+            else {
+                System.out.println("added: " + text);
+                list[num] = text;
+                num++;
+            }
         }
 
-        System.out.println("Bye. Hope to see  you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
     }
 
 }
