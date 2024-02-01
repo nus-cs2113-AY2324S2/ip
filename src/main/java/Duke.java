e93348392ebe744d001aa7914baa6ef42baf01fe
Ahmed <shaiyan.alam16@gmail.com>
2024-02-01 23:13:39 +0800

    Level 1 changes made

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,17 +1,37 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = 
-              "   ____ ____  \n"
-            + " / ___/ ___| \n"
-            + "| |   \\___ \\ \n"
-            + "| |___ ___) |\n"
-            + " \\____|____/ \n";
-        System.out.println("Hello from Shaiyan\r\n" +
-                " Hello! I'm CS\r\n" + 
-                " What can I do for you?\r\n" +
-                "____________________________________________________________\r\n" +
-                " Bye. Hope to see you again soon!\r\n" +
-                "____________________________________________________________\r\n" +
-                "\n" + logo);
+        "   J     A    SSSS    OOO   N   N \n"
+        + "   J    A A   S      O   O  NN  N \n"
+        + "   J   AAAAA   SSS   O   O  N N N \n"
+        + "J  J  A     A     S  O   O  N  NN \n"
+        + " JJJ  A     A  SSSS   OOO   N   N \n";
+        System.out.println("Hello from Shaivan\r\n"
+        + " Eyy wassup I'm Jason\r\n" 
+        + " What can I do for you?\r\n"
+        + logo);
+      
+
+        Scanner scanner = new Scanner(System.in);
+        String input;
+
+        while (true) {
+            input = scanner.nextLine(); // Read user input
+
+            if (input.equalsIgnoreCase("bye")) { // User wants to exit
+                System.out.println("Bye. See ya laterr!\r\n"
+                        + "____________________________________________________________\r\n"
+                        + "\n" + logo);
+                break; // Exit the loop
+            }
+
+            // Echo the input back to the user with decoration
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+        }
+
+        scanner.close(); // Close the scanner when done
     }
 }
