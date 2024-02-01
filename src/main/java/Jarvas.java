84b3a0441ad39ad9dd24aeb6e4b899f7d6112cea
l5_z <e0958707@u.nus.edu>
2024-02-01 18:21:31 +0800

    Add A-CodingStandard

--- src/main/java/Jarvas.java
+++ src/main/java/Jarvas.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 public class Jarvas {
-    public static final String partitionLine = "____________________________________________________________";
+    public static final String PARTITION_LINE = "____________________________________________________________";
     public static void initialiseBot(){
         System.out.println(" _____                                  ");
         System.out.println("(___  )                                 ");
@@ -9,15 +9,15 @@ public class Jarvas {
         System.out.println("( )_| |( (_| || |   | \\_/ |( (_| |\\__, \\");
         System.out.println("`\\___/'`\\__,_)(_)   `\\___/'`\\__,_)(____/");
 
-        System.out.println(partitionLine);
+        System.out.println(PARTITION_LINE);
         System.out.println("Hello! I'm Jarvas");
         System.out.println("What can I do for you?");
-        System.out.println(partitionLine);
+        System.out.println(PARTITION_LINE);
     }
     public static void terminateBot(){
-        System.out.println(partitionLine);
+        System.out.println(PARTITION_LINE);
         System.out.println("Bye, see you soon!");
-        System.out.println(partitionLine);
+        System.out.println(PARTITION_LINE);
     }
     public static void runTime(){
         Scanner in = new Scanner(System.in);
@@ -29,38 +29,36 @@ public class Jarvas {
             if (userInput.equals("bye")) {
                 return;
             } else if (userInput.startsWith("mark")) {
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
                 String index = userInput.substring(5); // obtain value after mark
-
                 int i = Integer.parseInt(index) - 1; // convert to int
                 tasks[i].setCompletedTrue();
 
                 System.out.println("Nice! I've marked this task as done:");
                 tasks[i].printTask();
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
             } else if (userInput.startsWith("unmark")) {
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
                 String index = userInput.substring(7); // obtain value after unmark
-
                 int i = Integer.parseInt(index) - 1; // convert to int
                 tasks[i].setCompletedFalse();
 
                 System.out.println("Okay, I've marked this task as not done yet:");
                 tasks[i].printTask();
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
             } else if (userInput.equals("list")) {
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
                 System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCounter; i++) {
                     System.out.print((i + 1) + ".");
                     tasks[i].printTask();
                 }
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
             } else {
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
                 System.out.print("added: ");
                 System.out.println(userInput);
-                System.out.println(partitionLine);
+                System.out.println(PARTITION_LINE);
 
                 Task task = new Task(userInput);
                 tasks[taskCounter] = task;
