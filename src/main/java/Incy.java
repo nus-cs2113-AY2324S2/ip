b4c4501c5bbdff040bf61d95cf5f4bfab5bb0c36
Incy <85068563+IncyBot@users.noreply.github.com>
2024-02-01 20:44:40 +0800

    Level 3
    Added the ability to mark tasks as done and the ability to change the status back to not done.

--- src/main/java/Incy.java
+++ src/main/java/Incy.java
@@ -4,19 +4,19 @@ public class Incy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         String logo = " ____  _  _  ___  _  _ \n"
                     + "(_  _)( \\( )/ __)( \\/ )\n"
                     + " _)(_  )  (( (__  \\  / \n"
                     + "(____)(_)\\_)\\___) (__) \n";
-        
+
         System.out.println("____________________________________________________________\n"
                             + "Oi bruv! I'm\n" + logo
                             + "Wotcha need from me today?\n"
                             + "____________________________________________________________"
         );
-        
+
         while (true) {
             input = scanner.nextLine();
             if (input.equalsIgnoreCase("bye")) {
@@ -31,6 +31,26 @@ public class Incy {
                     }
                 }
                 System.out.println("____________________________________________________________");
+            } else if (input.startsWith("mark ")) {
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsDone();
+                    System.out.println("____________________________________________________________\n"
+                                        + "Buzzin'! This one's sorted now:\n  "
+                                        + tasks[index]
+                                        + "\n____________________________________________________________"
+                    );
+                }
+            } else if (input.startsWith("unmark ")) {
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                if (index >= 0 && index < taskCount) {
+                    tasks[index].markAsNotDone();
+                    System.out.println("____________________________________________________________\n"
+                                        + "No prob, flipped it back to not done, innit:\n  "
+                                        + tasks[index]
+                                        + "\n____________________________________________________________"
+                    );
+                }
             } else {
                 if (taskCount >= tasks.length) {
                     System.out.println("____________________________________________________________\n"
@@ -38,7 +58,7 @@ public class Incy {
                                         + "\n____________________________________________________________"
                     );
                 } else {
-                    tasks[taskCount] = input;
+                    tasks[taskCount] = new Task(input);
                     taskCount++;
                     System.out.println("____________________________________________________________\n"
                                         + "Sorted! Added: " + input
@@ -47,7 +67,7 @@ public class Incy {
                 }
             }
         }
-        
+
         System.out.println("____________________________________________________________\n"
                             + "Cya later mate!"
                             + "\n____________________________________________________________"
