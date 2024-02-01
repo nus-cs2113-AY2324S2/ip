92fd0e03d02919d422f6a2d61aa83341a9738dc6
ChinYanXu <yanxu6267@gmail.com>
2024-02-01 22:09:48 +0800

    Add echo functionality

--- src/main/java/Serf.java
+++ src/main/java/Serf.java
@@ -1,12 +1,35 @@
+import java.util.Scanner;
+
 public class Serf {
     public static void main(String[] args) {
         String chatBotName = "Serf";
-        String message = "____________________________________________________________\n"
-                + " Hello! I'm " + chatBotName + "\n"
-                + " What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + " Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________\n";
-        System.out.println(message);
+        String greetingMessage = "    ____________________________________________________________\n"
+                + "     Good day my lord! I'm " + chatBotName + "\n"
+                + "     How shall I serve thee?\n"
+                + "    ____________________________________________________________\n";
+        String farewellMessage = "    ____________________________________________________________\n"
+                + "     Farewell my lord. \n"
+                + "    ____________________________________________________________\n";
+        System.out.println(greetingMessage);
+        Scanner requestedMessage = new Scanner(System.in);
+        String receivedMessage = requestedMessage.nextLine();
+        //adds vertical lines on top and below word
+        String paddedReceivedMessage = "    ____________________________________________________________\n"
+                + "    "
+                + " "
+                + receivedMessage
+                + "\n"
+                + "    ____________________________________________________________\n";
+        while(!receivedMessage.equals("bye")) {
+            System.out.println(paddedReceivedMessage);
+            receivedMessage = requestedMessage.nextLine();
+            paddedReceivedMessage = "    ____________________________________________________________\n"
+                    + "    "
+                    + " "
+                    + receivedMessage
+                    + "\n"
+                    + "    ____________________________________________________________\n";
+        }
+        System.out.println(farewellMessage);
     }
 }
