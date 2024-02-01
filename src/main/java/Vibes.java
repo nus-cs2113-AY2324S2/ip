f1911a6127439820ff4a948e89ef0db4b66a28d8
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-02 00:05:35 +0800

    Add echo feature
    The echo feature results in the text entered by the user to be printed back to the user by the ChatBot.

--- src/main/java/Vibes.java
+++ src/main/java/Vibes.java
@@ -1,12 +1,28 @@
+import java.util.Scanner;
+
 public class Vibes {
     public static void main(String[] args) {
         String name = "Vibes";
-        String line = "---------------------------------------------------------------------------------------";
+        String line = "\t---------------------------------------------------------------------------------------";
+        String userInput;
+        Scanner in = new Scanner(System.in);
 
         System.out.println(line);
-        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n");
+        System.out.println("\t Hello! I'm " + name + "\n\t What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!\n");
+
+        while(true){
+            userInput = in.nextLine().trim();
+            if(userInput.equalsIgnoreCase("bye")){
+                break;
+            }
+            System.out.println(line);
+            System.out.println("\t " + userInput);
+            System.out.println(line);
+        }
+
+        System.out.println(line);
+        System.out.println("\t Bye. Hope to see you again soon!");
         System.out.println(line);
     }
 }
