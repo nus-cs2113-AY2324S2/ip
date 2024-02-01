8926737b6be9907d31324a85a9945d694ce764ea
seandooa <seandooa@yahoo.com.sg>
2024-02-02 01:29:39 +0800

    Add ability for users to store text and display it upon request

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,16 +2,27 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
-        String line;
+        String[] line = new String[100];
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Bobby\n" + "What can I do for you?");
 
-        do {
-            line = in.nextLine();
-            if (!line.equals("bye")) {
-                System.out.println(line);
+        for (int i = 1; i < 101; i += 1) {
+            line[i] = in.nextLine();
+            if (!line[i].equals("bye") && !line[i].equals("list")) {
+                System.out.println("added: " + line[i]);
+            } else {
+                if (line[i].equals("list")) {
+                    for (int j = 1; j < i; j += 1) {
+                        System.out.println(j + ". " + line[j]);
+                    }
+                    i -= 1;
+                } else {
+                    if (line[i].equals("bye")) {
+                        break;
+                    }
+                }
             }
-        } while (!line.equals("bye"));
+        }
 
         System.out.println("Bye. Hope to see you again soon!\n");
     }
