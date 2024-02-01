0a0caecec11ec82a95c30d63524abeb91252aa9e
ChinYanXu <yanxu6267@gmail.com>
2024-02-01 22:24:41 +0800

    Add text store functionality and list functionality

--- src/main/java/Serf.java
+++ src/main/java/Serf.java
@@ -2,34 +2,47 @@ import java.util.Scanner;
 
 public class Serf {
     public static void main(String[] args) {
+        String VERTICALLINES = "    ____________________________________________________________";
+        String[] taskList = new String[100];
+        int taskCounter = 0;
         String chatBotName = "Serf";
-        String greetingMessage = "    ____________________________________________________________\n"
+        String GREETINGMESSAGE = VERTICALLINES
+                + "\n"
                 + "     Good day my lord! I'm " + chatBotName + "\n"
                 + "     How shall I serve thee?\n"
-                + "    ____________________________________________________________\n";
-        String farewellMessage = "    ____________________________________________________________\n"
+                + VERTICALLINES;
+        String FAREWELLMESSAGE = VERTICALLINES
+                + "\n"
                 + "     Farewell my lord. \n"
-                + "    ____________________________________________________________\n";
-        System.out.println(greetingMessage);
+                + VERTICALLINES;
+        System.out.println(GREETINGMESSAGE);
         Scanner requestedMessage = new Scanner(System.in);
         String receivedMessage = requestedMessage.nextLine();
-        //adds vertical lines on top and below word
-        String paddedReceivedMessage = "    ____________________________________________________________\n"
-                + "    "
-                + " "
-                + receivedMessage
-                + "\n"
-                + "    ____________________________________________________________\n";
+
         while(!receivedMessage.equals("bye")) {
-            System.out.println(paddedReceivedMessage);
+            if (receivedMessage.equals("list") && taskCounter > 0) {
+                System.out.println(VERTICALLINES);
+                for (int i = 0; i < taskCounter; i += 1) {
+                    System.out.println("     " + (i + 1) + ". " + taskList[i] + " ");
+                }
+                System.out.println(VERTICALLINES);
+            }
+            else if (receivedMessage.equals("list") && taskCounter == 0){
+                System.out.println(VERTICALLINES);
+                System.out.println("     " + "list is empty");
+                System.out.println(VERTICALLINES);
+                receivedMessage = requestedMessage.nextLine();
+                continue;
+            }
+            else {
+                taskList[taskCounter] = receivedMessage;
+                taskCounter += 1;
+                System.out.println(VERTICALLINES);
+                System.out.println("     " + "added: " + receivedMessage);
+                System.out.println(VERTICALLINES);
+            }
             receivedMessage = requestedMessage.nextLine();
-            paddedReceivedMessage = "    ____________________________________________________________\n"
-                    + "    "
-                    + " "
-                    + receivedMessage
-                    + "\n"
-                    + "    ____________________________________________________________\n";
         }
-        System.out.println(farewellMessage);
+        System.out.println(FAREWELLMESSAGE);
     }
 }
