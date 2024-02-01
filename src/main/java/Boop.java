9c07628f762629e490331701aeaa5ee714e0c48e
annnniexu <annie.xu2025@berkeley.edu>
2024-02-01 22:00:23 +0800

    finished level1

--- src/main/java/Boop.java
+++ src/main/java/Boop.java
@@ -1,7 +1,26 @@
+import java.util.Scanner;
+
 public class Boop {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm BOOP");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+        Scanner in = new Scanner(System.in);
+        String curCommand;
+        while(true) {
+            curCommand = in.nextLine();
+            System.out.println("____________________________________________________________");
+            if(curCommand.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            } else {
+                System.out.println(curCommand);
+                System.out.println("____________________________________________________________");
+
+            }
+        }
+        //System.out.println("Bye. Hope to see you again soon!");
     }
 }
