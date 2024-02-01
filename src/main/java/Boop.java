c74352896014242b06e510ec21f68f5acf006340
annnniexu <annie.xu2025@berkeley.edu>
2024-02-01 22:14:17 +0800

    finished level2

--- src/main/java/Boop.java
+++ src/main/java/Boop.java
@@ -1,6 +1,9 @@
 import java.util.Scanner;
 
 public class Boop {
+    public static String[] lst = new String[100];
+    public static int count = 0;
+
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm BOOP");
@@ -12,15 +15,26 @@ public class Boop {
             curCommand = in.nextLine();
             System.out.println("____________________________________________________________");
             if(curCommand.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("____________________________________________________________");
                 break;
+            } else if (curCommand.equals("list")){
+                printLst();
             } else {
-                System.out.println(curCommand);
-                System.out.println("____________________________________________________________");
+                lst[count] = curCommand;
+                count += 1;
+                System.out.println("added: " + curCommand);
 
             }
+            System.out.println("____________________________________________________________");
         }
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
         //System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    public static void printLst() {
+        for(int i = 0; i < count; i+= 1) {
+            System.out.println("" + (i+1) + ". " + lst[i]);
+        }
+
     }
 }
