af702a8549ee2a9c58f8a9a44c242fccc42461a0
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-02 00:48:01 +0800

    Remove taskCount from Task class

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,13 +1,8 @@
 public class Task {
     private String description;
-    private static int taskCount = 0;
 
     public Task(String description) {
         this.description = description;
-    }
-
-    public static int getTaskCount() {
-        return taskCount;
     }
 
     public String getDescription() {
