5dba68967a32b3bb02dc1a52b1112c9f3ac76594
fungg0 <fungfunwenwu@gmail.com>
2024-02-01 21:20:19 +0800

    add echo to bot

--- src/main/java/Allez.java
+++ src/main/java/Allez.java
@@ -1,12 +1,19 @@
+import java.util.Scanner;
+
+
 public class Allez {
     public static void main(String[] args) {
-//        String logo = " ____        _        \n"
-//                + "|  _ \\ _   _| | _____ \n"
-//                + "| | | | | | | |/ / _ \\\n"
-//                + "| |_| | |_| |   <  __/\n"
-//                + "|____/ \\__,_|_|\\_\\___|\n";
+
+        System.out.println("_________________________");
         System.out.println("Hello! I'm Allez");
         System.out.println("What can I do for you?");
+        System.out.println("_________________________");
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+        while (!line.equals("bye")){
+            System.out.println(line);
+            line = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
