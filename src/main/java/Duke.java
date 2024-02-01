12db21fa02a345dd280669e6146005be66f9d363
Thaw Tun Zan <110762796+ThawTunZan@users.noreply.github.com>
2024-02-02 00:34:21 +0800

    Add Echo for level 1 Increment This commit does the following: *Adds a feature where the program repeat the user's input *Adds a feature that will allow the user to manually exit the program

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,34 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String logo = "Hello! I'm ThawBot!\nWhat can I do for you?\n" +
-                "Bye. Hope to see you again soon!";
-        System.out.println(logo);
+        printsGreeting();
+        mimicMessage();
+
+    }
+    private static void printsGreeting() {
+        String greetingMessage = "Hello! I'm ThawBot!\nWhat can I do for you?\n";
+        System.out.println(greetingMessage);
+    }
+
+    private static void printGoodByeMessage() {
+        String goodbyeMessage = "Bye. Hope to see you again soon!";
+        System.out.println(goodbyeMessage);
+    }
+
+    private static void mimicMessage(){
+        Scanner input = new Scanner(System.in);
+        boolean canExit = false;
+        while (!canExit) {
+            String usersInput = input.nextLine();
+            if (usersInput.equals("bye")) {
+                canExit = true;
+                printGoodByeMessage();
+            }
+            else {
+                System.out.println(usersInput);
+            }
+
+        }
     }
 }
