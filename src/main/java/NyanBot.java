748668bcd9e888367bf603ef89d92e0ef2935d73
blackmirag3 <shuihon@gmail.com>
2024-02-01 17:45:49 +0800

    Add echo function

--- src/main/java/NyanBot.java
+++ src/main/java/NyanBot.java
@@ -1,15 +1,29 @@
+import java.util.Scanner;
 public class NyanBot {
+    private static String line = "____________";
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
         String greet = "お帰りなさいませ、ご主人様。ニャンー♡♡";
         String prompt = "なにをしたいの？";
         String bye = "じゃー、またね〜！！";
 
         System.out.println(line);
-        System.out.println(greet);
-        System.out.println(prompt);
+        System.out.println(greet + "\n" + prompt);
+
+        echo();
+
         System.out.println(line);
         System.out.println(bye);
+    }
+
+    private static void echo() {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+        if (input.equals("bye")) {
+            return;
+        }
         System.out.println(line);
+        System.out.println(input);
+        echo();
     }
 }
