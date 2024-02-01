6df61f04cad5f4ec917da8c578d38c04c032f12a
B1G-SAM <54065426+B1G-SAM@users.noreply.github.com>
2024-02-02 00:55:00 +0800

    add increment 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,35 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        Task[] tasks = new Task[100];
+        int itemsCount = 0;
         System.out.println("Hello! My name is Jeff.");
         System.out.println("What can I do for you?");
-        boolean stillUsing = true;
-        while (stillUsing){
+        boolean isChatting = true;
+        while (isChatting){
             String line;
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            if (line.equals("bye") || line.equals("Bye")){
-                stillUsing = false;
+
+            switch (line){
+            case "list":
+                for (int i = 0; i < itemsCount; i += 1){
+                    System.out.println((i +1) + ". " + tasks[i].getDescription());
+                }
+                break;
+
+            case "bye":
+                isChatting = false;
+                break;
+
+
+            default:
+                tasks[itemsCount] = new Task(line);
+                System.out.println("added: " + line);
+                itemsCount += 1;
+
             }
-            else{
-                System.out.println(line);
-            }
+
         }
 
         System.out.println("Bye. Hope to see you again soon!");
