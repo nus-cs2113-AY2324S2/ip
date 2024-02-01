04fc8aae8e572496231c2f7ffb428d89049a1871
Kishen271828 <kg240904kg@gmail.com>
2024-02-01 23:37:36 +0800

    Level 1: Add Echo feature

--- src/main/java/Nehsik.java
+++ src/main/java/Nehsik.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Nehsik {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,6 +9,18 @@ public class Nehsik {
         printLine();
         System.out.println("Hello! I'm Nehsik");
         System.out.println("What can I do for you?");
+        printLine();
+        String command;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            command = in.nextLine();
+            if (command.equals("bye")) {
+                break;
+            }
+            printLine();
+            System.out.println(command);
+            printLine();
+        }
         printLine();
         System.out.println("Bye. Hope to see you again soon!");
         printLine();
