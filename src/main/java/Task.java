d7b535485eb28290e9f97d483e01d44c313e52d5
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-02 01:20:00 +0800

    Task class: add isDone attribute

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,8 +1,18 @@
 public class Task {
     private String description;
+    private boolean isDone;
 
     public Task(String description) {
         this.description = description;
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public void setDone(boolean done) {
+        isDone = done;
     }
 
     public String getDescription() {
