0c9770b5c1edd411e1fcf367d4e7f24c004f024b
Bryvo <bryancastorius@gmail.com>
2024-02-02 00:32:28 +0800

    Add level 3

--- src/main/java/Gandalf.java
+++ src/main/java/Gandalf.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class Gandalf {
     public static void makeList() {
+        //Array of class Task to store all in to do lists
+        Task[] listTasks = new Task[100];
         Scanner in = new Scanner(System.in);
-        String[] listActivities = new String[100];
-        String byeStatement = "bye";
         final String line = "____________________________________________________________";
 
         System.out.println(line);
@@ -12,28 +12,59 @@ public class Gandalf {
         System.out.println(line);
         String text = in.nextLine();
 
-        int printIndex = 1;
         int insertIndex = 0;
 
         while (true) {
-            printIndex = 1;
-            if (text.equals(byeStatement)) {
+            if (text.equals("bye")) {
                 System.out.println(line);
                 return;
             } else if (text.equals("list")) {
-                for (String activity : listActivities) {
-                    if (activity != null) {
-                        System.out.println(printIndex + "." + " " + activity);
-                        printIndex += 1;
+                System.out.println(line);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < listTasks.length; i++) {
+                    if (listTasks[i] != null) {
+                        System.out.println((i + 1) + "." + " " + listTasks[i].getStatusIcon() + " " + listTasks[i].getDescription());
                     }
                 }
                 System.out.println(line);
+            } else if (text.startsWith("mark ")) {
+                int indexToMark = Integer.parseInt(text.substring(5).trim());
+                if (indexToMark >= 1 && indexToMark <= listTasks.length && listTasks[indexToMark - 1] != null) {
+                    listTasks[indexToMark - 1].markAsDone();
+                    System.out.println(line);
+                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println(listTasks[indexToMark - 1].getStatusIcon() + " " + listTasks[indexToMark - 1].getDescription());
+                    System.out.println(line);
+                } else {
+                    System.out.println(line);
+                    System.out.println("Invalid task index. Please try again.");
+                    System.out.println(line);
+                }
+            } else if (text.startsWith("unmark ")) {
+                int indexToUnmark = Integer.parseInt(text.substring(7).trim());
+                if (indexToUnmark >= 1 && indexToUnmark <= listTasks.length && listTasks[indexToUnmark - 1] != null) {
+                    listTasks[indexToUnmark - 1].unmarkAsDone();
+                    System.out.println(line);
+                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println(listTasks[indexToUnmark - 1].getStatusIcon() + " " + listTasks[indexToUnmark - 1].getDescription());
+                    System.out.println(line);
+                } else {
+                    System.out.println(line);
+                    System.out.println("Invalid task index. Please try again.");
+                    System.out.println(line);
+                }
             } else {
-                listActivities[insertIndex] = text;
-                insertIndex += 1;
-                System.out.println(line);
-                System.out.println("added: " + text);
-                System.out.println(line);
+                if (insertIndex < listTasks.length) {
+                    listTasks[insertIndex] = new Task(text);
+                    insertIndex += 1;
+                    System.out.println(line);
+                    System.out.println("added: " + text);
+                    System.out.println(line);
+                } else {
+                    System.out.println(line);
+                    System.out.println("List is full. Cannot add more items.");
+                    System.out.println(line);
+                }
             }
             text = in.nextLine();
         }
@@ -63,16 +94,15 @@ public class Gandalf {
 
         while (!hasSaidBye) {
             text = in.nextLine();
-            System.out.println(line); //not being printed
+            System.out.println(line);
             if (text.equals(byeStatement)) {
                 hasSaidBye = true;
             } else if (text.equals(makeListStatement)) {
                 makeList();
-                return;
             } else {
                 System.out.println(text);
-                System.out.println(line);
             }
+            System.out.println(line);
         }
     }
 
