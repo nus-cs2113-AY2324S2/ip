518bac33eb8690068c669bae3f19d90608259a0e
paturikarthik <paturi.karthik1@gmail.com>
2024-02-02 02:59:31 +0800

    Level-1

--- src/main/java/PeeKay.java
+++ src/main/java/PeeKay.java
@@ -1,11 +1,31 @@
+import java.util.Scanner;
 public class PeeKay {
+    static String line = "____________________________________________________________";
+    public static void echo(String input){
+        if(input.equals("bye")){
+            System.out.println("Bye. Hope to see you again soon!");
+        }else {
+            System.out.println("  " + input);
+
+        }
+    }
+
+    public static void chat(){
+        String input;
+        do {
+            input = new Scanner(System.in).nextLine();
+            System.out.println(line);
+            echo(input);
+            System.out.println(line);
+        } while(!input.equals("bye"));
+    }
+
     public static void main(String[] args) {
-        String line = "____________________________________________________________";
+
         System.out.println(line);
         System.out.println("Hello! I'm PeeKay");
         System.out.println("What can I do for you?");
         System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
+        chat();
     }
 }
