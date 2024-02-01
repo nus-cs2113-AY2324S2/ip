df4c70b67c140be60df521c51e8dba0865cdc5a4
Shu <e0957502@u.nus.edu>
2024-02-01 23:54:39 +0800

    Add RoleyPoley.java and Implement Level 0

---
+++ src/main/java/RoleyPoley.java
@@ -0,0 +1,6 @@
+public class RoleyPoley {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm RoleyPoley \nWhat can I do for you today?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
