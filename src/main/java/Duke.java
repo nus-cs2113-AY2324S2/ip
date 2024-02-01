550f897970668ea8474280514a288091fe6aa676
Ijaaz01 <ijaaz2308@gmail.com>
2024-02-01 22:57:27 +0800

    Duke.java: Increment level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         String lineBreak = "----------------------------------------------------------";
@@ -18,7 +20,14 @@ public class Duke {
                     + "   ██                 ██\n"
                     + "    ███             ███ \n"
                     + "      ███████████████   \n\n";
+
+        String input = "Start";
+        Scanner in = new Scanner(System.in);
         System.out.println(idle + "Hello, Im Pythia, how may I help you today?\n"+lineBreak);
+        while (!input.equals("bye")) {
+            input = in.nextLine();
+            System.out.println(input+"\n"+lineBreak);
+        }
         System.out.println(happy+"Happy to help, have a great day.\n"+lineBreak);
     }
 }
