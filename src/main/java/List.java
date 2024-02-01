aa90fdc6a557b5957f3e31fd644f5b590d103e37
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-02 00:53:43 +0800

    List class: add addTask method

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,4 +1,9 @@
 public class List {
     Task[] tasks = new Task[100];
     private int tasksCount = 0;
+
+    public void addTask(String description) {
+        tasks[tasksCount] = new Task(description);
+        tasksCount++;
+    }
 }
