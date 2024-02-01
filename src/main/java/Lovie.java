2ec69e787a9e086e615d0235db1d35d69d3d5cf6
Nidhi Nayak <nidhirnayak@gmail.com>
2024-02-02 01:54:03 +0800

    Level-3

--- src/main/java/Lovie.java
+++ src/main/java/Lovie.java
@@ -5,7 +5,7 @@ public class Lovie {
     public static void main(String[] args) {
         Scanner inputGetter = new Scanner(System.in);   
         String input = new String(); 
-        ArrayList<String> taskList = new ArrayList<String>();   
+        ArrayList<Task> taskList = new ArrayList<Task>();   
 
         String logo = 
         "       ██╗░░░░░░█████╗░██╗░░░██╗██╗███████╗ \n" +
@@ -28,18 +28,63 @@ public class Lovie {
                     System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
                     int counter = 0; 
                     while (counter < taskList.size()) {
-                        String listNumber = Integer.toString(counter+1); 
-                        System.out.println("        " + listNumber + ". " + taskList.get(counter) + "\n\n");
+                        String listNumber = Integer.toString(counter + 1); 
+                        System.out.println("        " + listNumber + ". " + "[" + taskList.get(counter).getStatusIcon() + "] " + taskList.get(counter).description + "\n\n");
                         counter += 1; 
                     }
                     System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
                 }
                 else {
-                    taskList.add(input);
-                    System.out.println("\n\n");
-                    System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
-                    System.out.println("        added: " + input + "\n\n"); 
-                    System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"); 
+                    if (input.contains("unmark")) {
+                        int taskNumber = Integer.parseInt(input.split(" ")[1].toString()) - 1; 
+                        if (taskNumber >= taskList.size()) {
+                            System.out.println("\n\n");
+                            System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+                            System.out.println("        Sorry, there is no record of a task number " + input.split(" ")[1].toString() + "\n");
+                            System.out.println("        Can I help you with anything else?\n\n"); 
+                            System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"); 
+                        }
+                        else {
+                            Task selectedTask = taskList.get(taskNumber); 
+                            selectedTask.markAsUndone();
+                            System.out.println("\n\n");
+                            System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+                            System.out.println("        Okay, no worries. I've unmarked this task for you: \n"); 
+                            System.out.println("            [" + selectedTask.getStatusIcon() + "] " + selectedTask.description + "\n"); 
+                            System.out.println("        What else can I do for you today?\n\n"); 
+                            System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"); 
+                        }
+                    }
+                    else {
+                        if (input.contains("mark")) {
+                            int taskNumber = Integer.parseInt(input.split(" ")[1].toString()) - 1; 
+                            if (taskNumber >= taskList.size()) {
+                                System.out.println("\n\n");
+                                System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+                                System.out.println("        Sorry, there is no record of a task number " + input.split(" ")[1].toString() + "\n");
+                                System.out.println("        Can I help you with anything else?\n\n"); 
+                                System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"); 
+                            }
+                            else {
+                                Task selectedTask = taskList.get(taskNumber); 
+                                selectedTask.markAsDone();
+                                System.out.println("\n\n");
+                                System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+                                System.out.println("        Woo hoo! Way to go. I've marked this task as done:\n"); 
+                                System.out.println("            [" + selectedTask.getStatusIcon() + "] " + selectedTask.description + "\n"); 
+                                System.out.println("        What else can I do for you today?\n\n"); 
+                                System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+                            } 
+                        }
+                        else {
+                            Task newTask = new Task(input);
+                            taskList.add(newTask);
+                            System.out.println("\n\n");
+                            System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+                            System.out.println("        added: " + newTask.description + "\n\n"); 
+                            System.out.println("        ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n"); 
+                        }
+                    }
                 }
             }
         } 
