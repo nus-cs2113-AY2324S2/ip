91f8700c1c0eb2c9a479076e6dd879544d204810
Incy <85068563+IncyBot@users.noreply.github.com>
2024-02-01 20:22:29 +0800

    Level-2
    Added Personality and the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Incy.java
+++ src/main/java/Incy.java
@@ -4,30 +4,52 @@ public class Incy {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String input;
+        String[] tasks = new String[100];
+        int taskCount = 0;
         String logo = " ____  _  _  ___  _  _ \n"
                     + "(_  _)( \\( )/ __)( \\/ )\n"
                     + " _)(_  )  (( (__  \\  / \n"
                     + "(____)(_)\\_)\\___) (__) \n";
         
         System.out.println("____________________________________________________________\n"
-                            + "Hello! I'm\n" + logo
-                            + "What can I do for you?\n"
+                            + "Oi bruv! I'm\n" + logo
+                            + "Wotcha need from me today?\n"
                             + "____________________________________________________________"
         );
         
         while (true) {
             input = scanner.nextLine();
-            if (input.equals("bye")) {
+            if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                System.out.println("____________________________________________________________");
+                if (taskCount == 0) {
+                    System.out.println("Blimey, your list is empty, innit?");
+                } else {
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                if (taskCount >= tasks.length) {
+                    System.out.println("____________________________________________________________\n"
+                                        + "Cor blimey! The list is full to the brim. Can't add more, sorry!"
+                                        + "\n____________________________________________________________"
+                    );
+                } else {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("____________________________________________________________\n"
+                                        + "Sorted! Added: " + input
+                                        + "\n____________________________________________________________"
+                    );
+                }
             }
-            System.out.println("____________________________________________________________\n"
-                                + input
-                                + "\n____________________________________________________________"
-            );
         }
         
         System.out.println("____________________________________________________________\n"
-                            + "Bye. Hope to see you again soon!"
+                            + "Cya later mate!"
                             + "\n____________________________________________________________"
         );
         scanner.close();
