92ed5459d93b86fca13496fb2233d6dc1baada52
HUNGPHAN <hungpd1975@gmail.com>
2024-02-01 22:41:45 +0800

    Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -6,16 +6,28 @@ public class Duke {
         System.out.println("Hello! I'm " + name);
         System.out.println("What can I do for you?");
         System.out.println("-----------------------------------");
-
+        String[] list = new String[100];
+        int index = 0;
         Scanner in = new Scanner(System.in);
         String line = in.nextLine();
         while (!line.equals("bye")){
-            System.out.println("-----------------------------------");
-            System.out.println(line);
-            System.out.println("-----------------------------------");
+            if (!line.equals("list")) {
+                System.out.println("-----------------------------------");
+                System.out.println("added: " + line);
+                list[index] = line;
+                index++;
+                System.out.println("-----------------------------------");
+            } else {
+                System.out.println("-----------------------------------");
+                for (int i = 0; i < index; i++){
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+                System.out.println("-----------------------------------");
+            }
             line = in.nextLine();
         }
-
+        System.out.println("-----------------------------------");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("-----------------------------------");
     }
 }
