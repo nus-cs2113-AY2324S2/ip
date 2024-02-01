437531aed8bedf966b31bfc3b2f047f9debd8dab
chenxk619 <chenxk619@gmail.com>
2024-02-01 13:14:52 +0800

    Level 2 task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,20 @@
 import java.util.Scanner;
 public class Duke {
+    public static String[] list = new String[100];
+    public static int listLength = 0;
+    /*
     public static void echo(String input){
         System.out.println("____________________________________________________________\n"
                             + input + "\n"
                             + "____________________________________________________________");
+    }
+    */
+    public static void add(String input){
+        System.out.println("____________________________________________________________\n"
+                + "added: " + input + "\n"
+                + "____________________________________________________________");
+        list[listLength] = input;
+        listLength++;
     }
     public static void main(String[] args) {
         String chat_name = "Sigma";
@@ -16,11 +27,19 @@ public class Duke {
         while (true) {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            if (!line.equals("bye")) {
-                echo(line);
+            if (line.equals("bye")) {
+                break;
+            }
+            if (line.equals("list")) {
+                System.out.println("____________________________________________________________");
+                for (int i = 0; i < listLength; i++){
+                    System.out.print(i + ". ");
+                    System.out.println(list[i] + '.');
+                }
+                System.out.println("____________________________________________________________");
             }
             else {
-                break;
+                add(line);
             }
         }
         System.out.println("____________________________________________________________\n"
@@ -29,11 +48,3 @@ public class Duke {
     }
 }
 
-/*
-____________________________________________________________
- Hello! I'm [YOUR CHATBOT NAME]
- What can I do for you?
-____________________________________________________________
- Bye. Hope to see you again soon!
-____________________________________________________________
- */
