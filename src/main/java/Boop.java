16a079b1341573cf3eaf2dcd0f3737f69c2c86e1
annnniexu <annie.xu2025@berkeley.edu>
2024-02-01 21:49:56 +0800

    finished level-0

---
+++ src/main/java/Boop.java
@@ -0,0 +1,7 @@
+public class Boop {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm BOOP");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
