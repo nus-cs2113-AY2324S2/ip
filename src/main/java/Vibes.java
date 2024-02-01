f2155cf237ed958df5dffb13bc7c24262e6186c5
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-02 00:28:52 +0800

    Update input to use switch for cases
    Changed the code to use switch rather than if statements to look for the different user input cases

--- src/main/java/Vibes.java
+++ src/main/java/Vibes.java
@@ -5,24 +5,29 @@ public class Vibes {
         String name = "Vibes";
         String line = "\t---------------------------------------------------------------------------------------";
         String userInput;
+        String vibesOutput = "";
+        boolean isExit = true;
         Scanner in = new Scanner(System.in);
 
         System.out.println(line);
         System.out.println("\t Hello! I'm " + name + "\n\t What can I do for you?");
         System.out.println(line);
 
-        while(true){
+        while(isExit){
             userInput = in.nextLine().trim();
-            if(userInput.equalsIgnoreCase("bye")){
+
+            switch (userInput){
+            case "bye":
+                vibesOutput = "Bye. Hope to see you again soon!";
+                isExit = false;
+                break;
+            default:
+                vibesOutput = userInput;
                 break;
             }
             System.out.println(line);
-            System.out.println("\t " + userInput);
+            System.out.println("\t " + vibesOutput);
             System.out.println(line);
         }
-
-        System.out.println(line);
-        System.out.println("\t Bye. Hope to see you again soon!");
-        System.out.println(line);
     }
 }
