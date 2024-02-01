5bdc4d9afa312ceb4efe01a70cff3660b19e5c7a
webster <webster.tan01@gmail.com>
2024-02-01 13:27:47 +0800

    Add echo command feature, update greeting and exit message

--- src/main/java/Helpy.java
+++ src/main/java/Helpy.java
@@ -1,20 +1,38 @@
+import java.util.Scanner;
 public class Helpy {
     public static void greetUser() {
-        String horizontalLine = "______________________";
+        String horizontalLine = "______________________\n";
         String name = "░▒█░▒█░▒█▀▀▀░▒█░░░░▒█▀▀█░▒█░░▒█\n" +
                 "░▒█▀▀█░▒█▀▀▀░▒█░░░░▒█▄▄█░▒▀▄▄▄▀\n" +
                 "░▒█░▒█░▒█▄▄▄░▒█▄▄█░▒█░░░░░░▒█░░\n";
         System.out.println(horizontalLine
-                + "\nGreetings, I am\n" + name);
+                + "Greetings, I am\n" + name);
         System.out.println("How can I help you?\n" + horizontalLine);
+    }
+    public static void echoCommand(String command) {
+        String horizontalLine = "______________________\n";
+        System.out.print(horizontalLine);
+        System.out.println(command);
+        System.out.println(horizontalLine);
     }
     public static void goodbyeUser() {
         String horizontalLine = "______________________\n";
-        System.out.println("Goodbye, see you next time!");
         System.out.print(horizontalLine);
+        System.out.println("Goodbye, see you next time!");
+        System.out.println(horizontalLine);
     }
     public static void main(String[] args) {
         greetUser();
-        goodbyeUser();
+
+        Scanner in = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")) {
+            command = in.nextLine();
+            if (command.equals("bye")) {
+                goodbyeUser();
+            } else {
+                echoCommand(command);
+            }
+        }
     }
 }
