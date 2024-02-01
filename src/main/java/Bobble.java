274f704910f7c34a241527d230dec3759f11b267
kyuichyi <chongkaitlyn@u.nus.edu>
2024-02-01 23:08:42 +0800

    Level 2 (Add, List): Added the ability to store whatever text entered by the user and display them back to the user when requested.

--- src/main/java/Bobble.java
+++ src/main/java/Bobble.java
@@ -1,25 +1,44 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bobble {
     public static void main(String[] args) {
         start();
         Scanner input = new Scanner(System.in);
-        String userCommand = input.nextLine();
-        while (!userCommand.equals("bye")) {
-            echo(userCommand);
-            userCommand = input.nextLine();
+        String userInput = input.nextLine();
+        String userList[] = new String[100];
+        int taskCount = 0;
+
+        while (!userInput.equals("bye")) {
+            switch (userInput) {
+                case "list":
+                    System.out.println("____________________________________________________________");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + userList[i]);
+                    }
+                    System.out.println("____________________________________________________________\n");
+                    break;
+                default:
+                    userList[taskCount++] = userInput;
+                    System.out.println("____________________________________________________________\n" +
+                            "added: " + userInput +
+                            "\n____________________________________________________________\n");
+            }
+            userInput = input.nextLine();
         }
         goodbye();
     }
 
     //Greets user
-    public static void start(){
+
+    public static void start() {
         System.out.println("____________________________________________________________\n" +
                 "Hello! I'm Bobble.\n" +
-                "What can I do for you?");
+                "What can I do for you?" +
+                "\n____________________________________________________________\n");
     }
-
     //Exits
+
     public static void goodbye() {
         System.out.println("____________________________________________________________\n" +
                 "Bye. Hope to see you again soon!\n" +
