443d3a746fe25207c69324560de61d44d906c86b
Kishen271828 <kg240904kg@gmail.com>
2024-02-02 00:54:31 +0800

    Add list functionality

--- src/main/java/Nehsik.java
+++ src/main/java/Nehsik.java
@@ -1,32 +1,46 @@
 import java.util.Scanner;
 public class Nehsik {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        printLine();
-        System.out.println("Hello! I'm Nehsik");
-        System.out.println("What can I do for you?");
-        printLine();
+        displayGreetings();
         String command;
         Scanner in = new Scanner(System.in);
+        String[] commandList = new String[100];
+        int commandIndex = 0;
         while (true) {
             command = in.nextLine();
-            if (command.equals("bye")) {
+            if (command.equals("list")) {
+                printLine();
+                for (int i = 0; i < commandIndex; i++) {
+                    System.out.println((i + 1) + ". " + commandList[i]);
+                }
+                printLine();
+            } else if (command.equals("bye")) {
                 break;
+            } else {
+                commandList[commandIndex] = command;
+                printLine();
+                System.out.println("added: " + command);
+                printLine();
+                commandIndex++;
             }
-            printLine();
-            System.out.println(command);
-            printLine();
         }
-        printLine();
-        System.out.println("Bye. Hope to see you again soon!");
-        printLine();
+        displayExitMessage();
     }
 
     public static void printLine() {
         System.out.println("____________________________________________________________");
+    }
+
+    public static void displayGreetings() {
+        printLine();
+        System.out.println("Hello! I'm Nehsik");
+        System.out.println("What can I do for you?");
+        printLine();
+    }
+
+    public static void displayExitMessage() {
+        printLine();
+        System.out.println("Bye. Hope to see you again soon!");
+        printLine();
     }
 }
