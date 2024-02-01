5daf65adfa4704fbe1b3aae5278e3d853896d947
Shu <e0957502@u.nus.edu>
2024-02-02 00:06:15 +0800

    Update RoleyPoley.java : Implement Level 1, createLine(), echo() and greet() functions

--- src/main/java/RoleyPoley.java
+++ src/main/java/RoleyPoley.java
@@ -1,6 +1,36 @@
+import java.util.Scanner;
+
 public class RoleyPoley {
     public static void main(String[] args) {
+        greet();
+        echo();
+    }
+
+    public static void createLine() {
+        for (int i = 0; i < 20; i++) {
+            System.out.print('-');
+        }
+        System.out.print('\n');
+    }
+
+    public static void greet() {
         System.out.println("Hello! I'm RoleyPoley \nWhat can I do for you today?");
-        System.out.println("Bye. Hope to see you again soon!");
+        createLine();
+    }
+
+    public static void echo() {
+        String line;
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            line = in.nextLine();
+            if (line.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                createLine();
+                break;
+            } else {
+                System.out.println('\t' + line);
+                createLine();
+            }
+        }
     }
 }
