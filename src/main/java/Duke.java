6c2a44c66a1d97dc552f99259b19948f11118d04
Ijaaz01 <ijaaz2308@gmail.com>
2024-02-02 00:47:50 +0800

    Duke.java: increment level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,12 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 public class Duke {
+    public static void printList(List<String> list) {
+        for (int i = 0; i < list.size(); i++) {
+            System.out.println((i+1)+"."+list.get(i));
+        }
+    }
     public static void main(String[] args) {
         String lineBreak = "----------------------------------------------------------";
         String idle = "       █████████████    \n"
@@ -22,11 +28,18 @@ public class Duke {
                     + "      ███████████████   \n\n";
 
         String input = "Start";
+        List<String> list = new ArrayList<String>(10);
         Scanner in = new Scanner(System.in);
         System.out.println(idle + "Hello, Im Pythia, how may I help you today?\n"+lineBreak);
         while (!input.equals("bye")) {
             input = in.nextLine();
-            System.out.println(input+"\n"+lineBreak);
+            if (input.equals("list")) {
+                printList(list);
+            }
+            else {
+                System.out.println("added: "+input+"\n"+lineBreak);
+                list.add(input);
+            }
         }
         System.out.println(happy+"Happy to help, have a great day.\n"+lineBreak);
     }
