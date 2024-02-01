eda5dcc34d669add99e6fef0b23e3a7873c824fc
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-02-01 17:28:23 +0800

    Add list

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -6,15 +6,25 @@ public class Spike {
         System.out.println("Hello! I'm " + chatbot);
         System.out.println("What can I do for you?\n");
 
-        while(true){
+        String[] inputList = new String[100];
+        int iter = 0;
+        while (true) {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
-            if(input.contentEquals("bye")){
+            if (input.contentEquals("list")) {
+                for (String value : inputList) {
+                    if (value == null) {
+                        break;
+                    }
+                    System.out.println(value);
+                }
+            } else if (input.contentEquals("bye")) {
                 break;
+            } else {
+                System.out.println("added: " + input);
+                inputList[iter] = input;
             }
-
-            System.out.println(input);
-
+            iter += 1;
         }
 
 
