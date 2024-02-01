2226ce57c18c4df0e23c5b4a098d51a0e579f4fd
Aak242 <110832731+Aak242@users.noreply.github.com>
2024-02-02 02:26:39 +0800

    Add unique ASCII art for logo,and Level-1 functionality (take input and echo text)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,41 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+        String logo = "         z$$$$P\n" +
+                "        d$$$$\"\n" +
+                "      .$$$$$\"\n" +
+                "     z$$$$$\"\n" +
+                "    z$$$$P\n" +
+                "   d$$$$$$$$$$\"\n" +
+                "  *******$$$\"\n" +
+                "       .$$$\"\n" +
+                "      .$$\"\n" +
+                "     4$P\"\n" +
+                "    z$\"\n" +
+                "   zP\n" +
+                "  z\"\n" +
+                " /    M A V I S'\n" +
+                "^";
         System.out.println("Hello from\n" + logo);
-
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Mavis!");
         System.out.println("What can I do for you?\n");
+
+        String inputToEcho;
+        Scanner in = new Scanner(System.in);
+
+        while(true) {
+            inputToEcho = in.nextLine();
+
+            if (inputToEcho.equals("bye")) {
+                break;
+            }
+
+            System.out.println(" ____________________________________________________________");
+            System.out.println(inputToEcho);
+            System.out.println(        "    ____________________________________________________________");
+        }
+
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
