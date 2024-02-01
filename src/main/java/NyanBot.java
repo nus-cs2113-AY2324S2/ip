f9193e3659e5d8e94259a746ecd2e1b550515e46
Shui Hon Siew <shuihon@gmail.com>
2024-02-01 18:12:43 +0800

    Add user text storage ability

--- src/main/java/NyanBot.java
+++ src/main/java/NyanBot.java
@@ -1,7 +1,30 @@
 import java.util.Scanner;
 public class NyanBot {
     private static String line = "____________";
+    private static String[] userInput = new String[100];
+    private static int userInputCount = 0;
 
+    private static void echo() {
+        Scanner in = new Scanner(System.in);
+        String input = in.nextLine();
+
+        switch(input) {
+        case "bye":
+            return;
+        case "list":
+            System.out.println(line);
+            for (int i = 0; i < userInputCount; i++) {
+                System.out.println(i + 1 + ". " + userInput[i]);
+            }
+            break;
+        default:
+            System.out.println(line);
+            userInput[userInputCount] = input;
+            userInputCount++;
+            System.out.println("Added: " + input);
+        }
+        echo();
+    }
     public static void main(String[] args) {
         String greet = "お帰りなさいませ、ご主人様。ニャンー♡♡";
         String prompt = "なにをしたいの？";
@@ -14,16 +37,5 @@ public class NyanBot {
 
         System.out.println(line);
         System.out.println(bye);
-    }
-
-    private static void echo() {
-        Scanner in = new Scanner(System.in);
-        String input = in.nextLine();
-        if (input.equals("bye")) {
-            return;
-        }
-        System.out.println(line);
-        System.out.println(input);
-        echo();
     }
 }
