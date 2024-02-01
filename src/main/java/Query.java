ed11ba92799f5fa2158773045924dcd8dc2c0a06
IanFH <101397272+IanFH@users.noreply.github.com>
2024-02-01 15:29:20 +0800

    add new class Query.java, modified SalmonSan.java

---
+++ src/main/java/Query.java
@@ -0,0 +1,14 @@
+public class Query {
+    // attributes of query
+    public String input;
+
+    // create constructor
+    public Query() {
+        this("");
+    }
+
+    public Query(String s){
+        this.input = s;
+    }
+
+}
--- src/main/java/SalmonSan.java
+++ src/main/java/SalmonSan.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class SalmonSan {
     public static void main(String[] args) {
         System.out.println("Salmon-San desu! Yoroshikuonegaishimasu (^.^)/");
+
+        Query query = new Query();
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("How can I assist you today? ");
+        query.input = in.nextLine();
+
+        System.out.println("Can I double check that you said: " + query.input);
     }
 }
