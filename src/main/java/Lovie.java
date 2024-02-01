72bef8b4eb7a1a46b9681b9d5e582432f25ba30e
Nidhi Nayak <nidhirnayak@gmail.com>
2024-02-01 21:02:35 +0800

    Level-0

---
+++ src/main/java/Lovie.java
@@ -0,0 +1,15 @@
+public class Lovie {
+    public static void main(String[] args) {
+        String logo = "██╗░░░░░░█████╗░██╗░░░██╗██╗███████╗ \n" +
+        "██║░░░░░██╔══██╗██║░░░██║██║██╔════╝ \n" +
+        "██║░░░░░██║░░██║╚██╗░██╔╝██║█████╗░░ \n" +
+        "██║░░░░░██║░░██║░╚████╔╝░██║██╔══╝░░ \n" +
+        "███████╗╚█████╔╝░░╚██╔╝░░██║███████╗ \n" +
+        "╚══════╝░╚════╝░░░░╚═╝░░░╚═╝╚══════╝ \n\n"; 
+        System.out.println("Hey hey! My name is \n\n" + logo);
+        System.out.println("How can I help you today?\n");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+        System.out.println("Thanks for using me! See you next time <3\n");
+        System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n");
+    }
+}
