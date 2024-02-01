18e7c4e29c28d843c53ac4a9515494ef3a3edc91
annoy-o-mus <55198236+annoy-o-mus@users.noreply.github.com>
2024-02-01 23:41:09 +0800

    refactor: Make logo and section bar text final

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,21 @@
 public class Duke {
-    public static void main(String[] args) {
-        // ASCII Art Generated from: https://patorjk.com/software/taag/#p=display&f=Big&t=anonBot
-        String logo = "                          ____        _   \n"
-                + "                         |  _ \\      | |  \n"
-                + "   __ _ _ __   ___  _ __ | |_) | ___ | |_ \n"
-                + "  / _` | '_ \\ / _ \\| '_ \\|  _ < / _ \\| __|\n"
-                + " | (_| | | | | (_) | | | | |_) | (_) | |_ \n"
-                + "  \\__,_|_| |_|\\___/|_| |_|____/ \\___/ \\__|\n";
-        String sectionBar = "____________________________________________________________";
+    // ASCII Art Generated from: https://patorjk.com/software/taag/#p=display&f=Big&t=anonBot
+    public static final String LOGO = "                          ____        _   \n"
+            + "                         |  _ \\      | |  \n"
+            + "   __ _ _ __   ___  _ __ | |_) | ___ | |_ \n"
+            + "  / _` | '_ \\ / _ \\| '_ \\|  _ < / _ \\| __|\n"
+            + " | (_| | | | | (_) | | | | |_) | (_) | |_ \n"
+            + "  \\__,_|_| |_|\\___/|_| |_|____/ \\___/ \\__|\n";
+    public static final String SECTION_BAR = "____________________________________________________________";
 
-        System.out.println(logo);
-        System.out.println(sectionBar);
+    public static void main(String[] args) {
+
+        System.out.println(LOGO);
+        System.out.println(SECTION_BAR);
         System.out.println("Hello! I'm anonBot");
         System.out.println("What can I do for you?");
-        System.out.println(sectionBar);
+        System.out.println(SECTION_BAR);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(sectionBar);
+        System.out.println(SECTION_BAR);
     }
 }
