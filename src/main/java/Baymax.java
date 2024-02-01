a246c048e2fa04687a772e90db7e9faa95506003
isaaceng7 <>
2024-02-01 22:08:47 +0800

    Echo

--- src/main/java/Baymax.java
+++ src/main/java/Baymax.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Baymax {
     public static void main(String[] args) {
         String name = "Baymax";
-        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n\n" + "Bye. Hope to see  you again soon!");
+        System.out.println("Hello! I'm " + name + "\nWhat can I do for you?\n\n");
+
+        while(true) {
+            String text;
+            Scanner in = new Scanner(System.in);
+            text = in.nextLine();
+
+            if(text.equalsIgnoreCase("bye")) {
+                break;
+            }
+
+            System.out.println(text);
+        }
+
+        System.out.println("Bye. Hope to see  you again soon!");
     }
+
 }
