942f13fbce9e9827fc16599701958423a9a3d8a9
V4vern <28131050+V4Vern@users.noreply.github.com>
2024-02-01 12:08:11 +0800

    Implement task completion status toggle

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Duke {
     private static final int MAX_TASKS = 100;
-    private static String[] tasks = new String[MAX_TASKS];
+    private static Task[] tasks = new Task[MAX_TASKS];
     private static int taskCount = 0;
 
 
@@ -11,50 +11,81 @@ public class Duke {
 
         printWelcomeMessage();
 
-        while (true){
+        while (true) {
             String userInput = scanner.nextLine();
             printLineSeparator();
 
-            if (userInput.equals("bye")){
+            if (userInput.equals("bye")) {
                 printGoodbyeMessage();
                 break;
-            } else if(userInput.equals("list")){
+            } else if (userInput.startsWith("mark ")) {
+                markTask(userInput, true);
+            } else if (userInput.startsWith("unmark ")) {
+                markTask(userInput, false);
+            } else if (userInput.equals("list")) {
                 listTasks();
-            }else{
-                addTasks(userInput);
+            } else {
+                addTask(userInput);
             }
         }
 
         scanner.close();
     }
-    private static void printWelcomeMessage(){
+
+    private static void printWelcomeMessage() {
         printLineSeparator();
         System.out.println("Hello! I'm Byte, your friendly chat assistant!");
         System.out.println("What can I do for you?");
         printLineSeparator();
     }
-    private static void printGoodbyeMessage(){
+
+    private static void printGoodbyeMessage() {
         System.out.println("Bye. Hope to see you again soon!");
         printLineSeparator();
     }
-    private static void addTasks(String task){
-        if (taskCount < MAX_TASKS){
-            tasks[taskCount] = task;
-            taskCount ++;
-            System.out.println("added: " + task);
-        }else{
+
+    private static void addTask(String taskDescription) {
+        if (taskCount < MAX_TASKS) {
+            Task newTask = new Task(taskDescription);
+            tasks[taskCount] = newTask;
+            taskCount++;
+            System.out.println("added: " + taskDescription);
+        } else {
             System.out.println("Sorry, I can only handle up to " + MAX_TASKS + " tasks!");
         }
         printLineSeparator();
     }
-    private static void listTasks(){
-        for (int i=0; i<taskCount; i++){
-            System.out.println((i+1) + ". " + tasks[i]);
+
+    private static void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
         }
         printLineSeparator();
     }
 
-    private static void printLineSeparator(){
+    private static void markTask(String userInput, boolean isDone) {
+        int taskNumber;
+        try {
+            taskNumber = Integer.parseInt(userInput.split(" ")[1]) - 1;
+            if (taskNumber < 0 || taskNumber >= taskCount) {
+                throw new IndexOutOfBoundsException("Task number is out of range");
+            }
+            if (isDone) {
+                tasks[taskNumber].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n  " + tasks[taskNumber]);
+            } else {
+                tasks[taskNumber].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:\n  " + tasks[taskNumber]);
+            }
+        } catch (NumberFormatException e) {
+            System.out.println("Please enter a valid task number.");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println(e.getMessage());
+        }
+        printLineSeparator();
+    }
+
+    private static void printLineSeparator() {
         System.out.println("____________________________________________________________");
     }
 }
