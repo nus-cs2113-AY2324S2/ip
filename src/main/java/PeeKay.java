77a1db0a0bc7930adcf48aa621c4ff2f643c4934
paturikarthik <paturi.karthik1@gmail.com>
2024-02-02 04:25:52 +0800

    A-CodingStandard

--- src/main/java/PeeKay.java
+++ src/main/java/PeeKay.java
@@ -1,42 +1,45 @@
 import java.util.Scanner;
+
 public class PeeKay {
     static String indent = "     ";
     static String line = "    ____________________________________________________________";
-    public static void echo(String input){
-        if(input.equals("bye")){
+
+    public static void echo(String input) {
+        if (input.equals("bye")) {
             System.out.println(indent + "Bye. Hope to see you again soon!");
-        }else {
-            System.out.println(indent + "added: " +input);
+        } else {
+            System.out.println(indent + "added: " + input);
         }
     }
 
-    public static int mark(String input){
-        int idx = Integer.parseInt(input.substring(input.indexOf(" ")+1));
-        return idx -1;
+    public static int mark(String input) {
+        int idx = Integer.parseInt(input.substring(input.indexOf(" ") + 1));
+        return idx - 1;
     }
 
-    public static void chat(){
+    public static void chat() {
         String input;
         Task[] list = new Task[100];
         int count = 0;
         do {
             input = new Scanner(System.in).nextLine();
             System.out.println(line);
-            if (input.equals("list")){
+            if (input.equals("list")) {
                 System.out.println(indent + "Here are the tasks in your list:");
-                for(int x = 0; x < count;x++){
-                    System.out.println(indent + " " + (x+ 1)+".[" + (list[x].getStatusIcon())+ "] "+list[x].getDescription());
+                for (int x = 0; x < count; x++) {
+                    System.out.println(indent + " " + (x + 1) + ".[" + (list[x].getStatusIcon()) + "] "
+                            + list[x].getDescription());
                 }
-            } else if (input.contains("unmark")){
+            } else if (input.contains("unmark")) {
                 int idx = mark(input);
                 list[idx].setDone(false);
                 System.out.println(indent + "OK, I've marked this task as not done yet:");
-                System.out.println(indent +"["+ list[idx].getStatusIcon() + "] " + list[idx].getDescription());
+                System.out.println(indent + "[" + list[idx].getStatusIcon() + "] " + list[idx].getDescription());
             } else if (input.contains("mark")) {
                 int idx = mark(input);
                 list[idx].setDone(true);
                 System.out.println(indent + "Nice! I've marked this task as done:");
-                System.out.println(indent +"["+ list[idx].getStatusIcon() + "] " + list[idx].getDescription());
+                System.out.println(indent + "[" + list[idx].getStatusIcon() + "] " + list[idx].getDescription());
             } else {
                 Task t = new Task(input);
                 list[count] = t;
@@ -44,7 +47,7 @@ public class PeeKay {
                 echo(input);
             }
             System.out.println(line);
-        } while(!input.equals("bye"));
+        } while (!input.equals("bye"));
     }
 
     public static void main(String[] args) {
