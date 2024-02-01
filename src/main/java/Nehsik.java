31a7fe16317d67c34d1cb5cb802651723c557e0d
Kishen271828 <kg240904kg@gmail.com>
2024-02-02 02:26:13 +0800

    Add mark/unmark task feature

--- src/main/java/Nehsik.java
+++ src/main/java/Nehsik.java
@@ -4,24 +4,39 @@ public class Nehsik {
         displayGreetings();
         String command;
         Scanner in = new Scanner(System.in);
-        String[] commandList = new String[100];
-        int commandIndex = 0;
+        Task[] taskList = new Task[100];
+        int taskIndex = 0;
         while (true) {
             command = in.nextLine();
             if (command.equals("list")) {
                 printLine();
-                for (int i = 0; i < commandIndex; i++) {
-                    System.out.println((i + 1) + ". " + commandList[i]);
+                System.out.println("Here are the tasks in your list:");
+                for (int i = 0; i < taskIndex; i++) {
+                    System.out.println((i + 1) + ".[" + taskList[i].getStatusIcon() + "] " + taskList[i].getDescription());
                 }
                 printLine();
             } else if (command.equals("bye")) {
                 break;
-            } else {
-                commandList[commandIndex] = command;
+            } else if (command.startsWith("mark")){
+                printLine();
+                System.out.println("Nice! I've marked this task as done:");
+                int taskNum = Integer.parseInt(command.substring(5)) - 1;
+                taskList[taskNum].markAsDone();
+                System.out.println("[X] " + taskList[taskNum].getDescription());
+                printLine();
+            } else if (command.startsWith("unmark")) {
+                printLine();
+                System.out.println("OK, I've marked this task as not done yet:");
+                int taskNum = Integer.parseInt(command.substring(7)) - 1;
+                taskList[taskNum].markAsUndone();
+                System.out.println("[ ] " + taskList[taskNum].getDescription());
+                printLine();
+            }  else {
+                taskList[taskIndex] = new Task(command);
                 printLine();
                 System.out.println("added: " + command);
                 printLine();
-                commandIndex++;
+                taskIndex++;
             }
         }
         displayExitMessage();
