82ad6b5b0040dd1fed1d214bd8573bd318461a7e
hwc0419 <hwc0419.dev@gmail.com>
2024-02-02 14:52:33 +0800

    Implement echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
 public class Duke {
 
     public static void main(String[] args) {
         String name = "Hirofumi";
-        System.out.println("____________________________________________________________");
+        System.out.println("************************************************************");
         System.out.println(" Hello! I'm " + name);
         System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println("************************************************************");
+        String userInput = null;
+        Scanner in = new Scanner(System.in);
+        while (userInput == null || !(userInput.equals("bye"))) {
+            userInput = in.nextLine();
+            System.out.println(userInput);
+        }
+        System.out.println("************************************************************");
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("************************************************************");
     }
 }
