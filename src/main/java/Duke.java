2a7d953565d5747e35c22af60ecedd56438d48aa
Aak242 <110832731+Aak242@users.noreply.github.com>
2024-02-02 14:38:22 +0800

    Add list function and ability to store tasks (Level 2)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
@@ -16,12 +17,21 @@ public class Duke {
                 "  z\"\n" +
                 " /    M A V I S'\n" +
                 "^";
+        //Store added tasks to an array
+        String[] listOfTasks = new String[100];
+
         System.out.println("Hello from\n" + logo);
         System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Mavis!");
         System.out.println("What can I do for you?\n");
 
+        //String to store the input entered by the user
         String inputToEcho;
+
+        //This variable stores the current number of tasks added
+        // and also uses it to place a new task in the next available spot.
+        int listOfTasksSize = 0;
+
         Scanner in = new Scanner(System.in);
 
         while(true) {
@@ -31,9 +41,21 @@ public class Duke {
                 break;
             }
 
-            System.out.println(" ____________________________________________________________");
-            System.out.println(inputToEcho);
-            System.out.println(        "    ____________________________________________________________");
+            else if (inputToEcho.equals("list")) {
+
+                for (int i = 0; (listOfTasks[i] != null); i++) {
+
+                    listTask(i, listOfTasks[i]);
+                }
+
+            }
+            else {
+
+                showNewlyAddedTask(inputToEcho);
+
+                listOfTasks[listOfTasksSize] = inputToEcho;
+                listOfTasksSize += 1;
+            }
         }
 
         System.out.println("____________________________________________________________");
@@ -41,4 +63,19 @@ public class Duke {
         System.out.println("____________________________________________________________");
 
     }
+
+    public static void listTask(int currentTaskIndex, String currentTask) {
+        System.out.println("____________________________________________________________");
+        System.out. println((currentTaskIndex + 1) + ". " + currentTask);
+        System.out.println("____________________________________________________________");
+    }
+
+    public static void showNewlyAddedTask(String input) {
+        System.out.println("____________________________________________________________");
+        System.out.println("Added Task: " + input);
+        System.out.println("____________________________________________________________");
+    }
 }
+
+
+
