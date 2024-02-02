6471a59f152306ebee90c97c4361f6fbfcea63ce
Shu <e0957502@u.nus.edu>
2024-02-02 13:54:37 +0800

    Update RoleyPoley.java and Task.java, Implement Level 3

--- src/main/java/RoleyPoley.java
+++ src/main/java/RoleyPoley.java
@@ -2,9 +2,18 @@ import java.util.Scanner;
 
 public class RoleyPoley {
     public static void main(String[] args) {
-        String[] list = new String[100];
+        Task[] taskList = new Task[100];
         greet();
-        echo(list);
+        echo(taskList);
+    }
+
+    public static void displayList(Task[] taskList) {
+        for ( int i = 1; i < taskList.length; i++) {
+            if (taskList[i] == null) {
+                break;
+            }
+            System.out.println("\t" + i + ".[" + taskList[i].getStatusIcon() + "]" + taskList[i].description);
+        }
     }
 
     public static void createLine() {
@@ -19,29 +28,66 @@ public class RoleyPoley {
         createLine();
     }
 
-    public static void echo(String[] list) {
+    public static void echo(Task[] taskList) {
         String line;
-        Scanner in = new Scanner(System.in);
+
         int counter = 1;
         while (true) {
+            Scanner in = new Scanner(System.in);
             line = in.nextLine();
             if (line.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 createLine();
                 break;
-            }
-            else if (line.equals("list")) {
-                for (int j = 1; j < counter; j++) {
-                    System.out.println("\t" + j + ". " + list[j]);
+            } else if (line.equals("list")) {
+                displayList(taskList);
+                createLine();
+            } else if (line.startsWith("mark")) {
+                String[] words = line.split(" ");
+                if (words.length == 2) {
+                    try {
+                        int taskNum = Integer.parseInt(words[1]);
+                        if (taskList[taskNum] == null) {
+                            System.out.println("Error! Out of Bounds!");
+                        }
+                        else {
+                            taskList[taskNum].markAsDone();
+                        }
+                        createLine();
+                    } catch (NumberFormatException e) {
+                        System.out.println("\tadded : " + line);
+                        createLine();
+                        taskList[counter] = new Task(line);
+                        counter++;
+                    }
                 }
+
+            } else if (line.startsWith("unmark")) {
+                String[] words = line.split(" ");
+                if (words.length == 2) {
+                    try {
+                        int taskNum = Integer.parseInt(words[1]);
+                        if (taskList[taskNum] == null) {
+                            System.out.println("Error! Out of Bounds!");
+                        }
+                        else {
+                            taskList[taskNum].markAsUndone();
+                        }
+                        createLine();
+                    } catch (NumberFormatException e) {
+                        System.out.println("\tadded : " + line);
+                        createLine();
+                        taskList[counter] = new Task(line);
+                        counter++;
+                    }
+                }
+            } else {
+                System.out.println("\tadded : " + line);
                 createLine();
-            }
-            else {
-                System.out.println('\t' + line);
-                createLine();
-                list[counter] = line;
+                taskList[counter] = new Task(line);
                 counter++;
             }
         }
     }
 }
+
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class Task {
+
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("\t [" + this.getStatusIcon() + "] " + this.description);
+
+    }
+
+    public void markAsUndone() {
+        this.isDone = false;
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("\t [" + this.getStatusIcon() + "] " + this.description);
+    }
+
+
+
 }
