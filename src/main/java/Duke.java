4935dceea1d06b9c2f02f83faa2d8f6c3fd1695a
FeathersRe <e0969003@u.nus.edu>
2024-02-02 17:48:15 +0800

    Increment: Level-0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,13 @@
 public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    public static void main(String[] a  rgs) {
+        String INTRO_PROMPT = "Hello! I'm Daisy.\nWhat can I do for you?";
+        String EXIT_PROMPT = "Bye. Hope to see you again soon!";
+        String LINE_BREAK = "\n____________________________________";
+
+        System.out.println(LINE_BREAK);
+        System.out.println(INTRO_PROMPT);
+        System.out.println(LINE_BREAK);
+        System.out.println(EXIT_PROMPT);
+        System.out.println(LINE_BREAK);
     }
 }
