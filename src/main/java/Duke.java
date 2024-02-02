bcf0e7c1483ac517ead00647a0ddb3a8c0e66861
Ahmed <shaiyan.alam16@gmail.com>
2024-02-02 16:47:47 +0800

    Tweak code to comply with a coding standard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,64 +1,70 @@
 import java.util.Scanner;
 
+/**
+ * Represents the main class for the application Duke.
+ */
 public class Duke {
-    public static void main(String[] args) {
-        String logo = 
-        "   J     A    SSSS    OOO   N   N \n"
-        + "   J    A A   S      O   O  NN  N \n"
-        + "   J   AAAAA   SSS   O   O  N N N \n"
-        + "J  J  A     A     S  O   O  N  NN \n"
-        + " JJJ  A     A  SSSS   OOO   N   N \n" +
-                "                 \n";
-        System.out.println(logo + " Eyy wassup I'm Jason\r\n"
 
-        + " What can I do for you?\r\n"
+    /**
+     * Main entry point of the Duke application.
+     * Initializes the application and starts the interaction with the user.
+     *
+     * @param args Command line arguments.
+     */
+    public static void main(String[] args) {
+        // Initialize greeting logo and print welcome message
+        String logo =
+                "   J     A    SSSS    OOO   N   N \n"
+                        + "   J    A A   S      O   O  NN  N \n"
+                        + "   J   AAAAA   SSS   O   O  N N N \n"
+                        + "J  J  A     A     S  O   O  N  NN \n"
+                        + " JJJ  A     A  SSSS   OOO   N   N \n" +
+                        "                 \n";
+        System.out.println(logo + " Eyy wassup I'm Jason\r\n"
+                + " What can I do for you?\r\n"
         );
-      
 
         Scanner scanner = new Scanner(System.in);
         String input;
         Task[] list = new Task[100];
         int num = 0;
 
-
         while (true) {
             input = scanner.nextLine();
 
-
-            if (input.equalsIgnoreCase("bye")) { // User wants to exit
+            if (input.equalsIgnoreCase("bye")) {
+                // User wants to exit
                 System.out.println("Bye. See ya laterr!\r\n"
                         + "____________________________________________________________\r\n"
                         + "\n" + logo);
                 break;
             }
 
-            //split the input
+            // Split the input and check for commands
             String[] parts = input.split(" ");
-            //Check whether input contains "mark" or "unmark"
             if (parts.length > 1 && parts[0].equalsIgnoreCase("mark")) {
+                // Mark task as done
                 int taskNumber = Integer.parseInt(parts[1]) - 1;
                 list[taskNumber].markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
                 System.out.println(list[taskNumber]);
             } else if (parts.length > 1 && parts[0].equalsIgnoreCase("unmark")) {
+                // Mark task as not done
                 int taskNumber = Integer.parseInt(parts[1]) - 1;
                 list[taskNumber].markAsNotDone();
                 System.out.println("OK, I've marked this task as not done yet:");
                 System.out.println(list[taskNumber]);
-            }
-
-            else if (input.equalsIgnoreCase("list")) {
-
+            } else if (input.equalsIgnoreCase("list")) {
+                // List all tasks
                 for (int i = 0; i < num; i++) {
-                    System.out.println(i+1 + ":" + list[i]);
+                    System.out.println(i + 1 + ":" + list[i]);
                 }
             } else {
+                // Add new task
                 System.out.println("added: " + input);
                 list[num] = new Task(input);
                 num++;
-
             }
-
 
             System.out.println("____________________________________________________________");
         }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,25 +1,51 @@
+/**
+ * Represents a task with a description and a status.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
 
+    /**
+     * Creates a new Task with the specified description.
+     *
+     * @param description The description of the task.
+     */
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    /**
+     * Retrieves the status icon depending on whether the task is done.
+     *
+     * @return The status icon.
+     */
     public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+        // Mark done task with X, otherwise a space
+        return (isDone ? "[X]" : "[ ]");
     }
 
+    /**
+     * Marks the task as done.
+     */
     public void markAsDone() {
         isDone = true;
     }
 
+    /**
+     * Marks the task as not done.
+     */
     public void markAsNotDone() {
         isDone = false;
     }
+
+    /**
+     * Returns the string representation of the task, including its status and description.
+     *
+     * @return The string representation of the task.
+     */
     @Override
     public String toString() {
-        return (isDone ? "[X] " : "[ ] ") + description;
+        return getStatusIcon() + " " + description;
     }
 }
