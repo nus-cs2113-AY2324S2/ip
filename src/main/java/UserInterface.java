b8b8b324aef92d7912628caabc54fa79490678f1
STeng618 <shantengyap@gmail.com>
2024-02-02 23:18:52 +0800

    Abstract UI features into UserInterface

---
+++ src/main/java/UserInterface.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class UserInterface {
+    private final Scanner scanner = new Scanner(System.in);
+    private final LogicManager logicManager;
+    private String currentInput;
+
+    public UserInterface(LogicManager logicManager) {
+        this.logicManager = logicManager;
+    }
+
+    private void readNextLine() {
+        do {
+            currentInput = scanner.nextLine();
+        } while (currentInput.isEmpty());
+    }
+
+    // Assume currentInput is updated;
+    private boolean isExitCommandGiven() {
+        return currentInput.startsWith("bye");
+    }
+
+    private void printCurrentInput() {
+        HorizontalGenerator.printHorizontal();
+        System.out.println(currentInput);
+        HorizontalGenerator.printHorizontal();
+    }
+
+    public void echo() {
+        while (true) {
+            readNextLine();
+            if (isExitCommandGiven()) {
+                return;
+            }
+            printCurrentInput();
+        }
+    }
+
+    public void manageList() {
+        while (true) {
+            readNextLine();
+            if (isExitCommandGiven()) {
+                return;
+            }
+            logicManager.processCommand(currentInput);
+            HorizontalGenerator.printHorizontal();
+        }
+    }
+
+}
