3ae20c21674aa6516b80e7f3cdfc3e1926b77cf4
TimothyLKM <>
2024-02-02 11:49:37 +0800

    Add a toDoList to track task Add additional methods to print list

--- src/main/java/TimL.java
+++ src/main/java/TimL.java
@@ -1,13 +1,36 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.lang.String;
 
 public class TimL {
-    public static void respondToRandomText(String text){
+    public static void respondToCommand(String text, List<String> list){
+        if (text.equals("list")){
+            printToDoList(list);
+        }else {
+            addToList(text, list);
+        }
+    }
+    public static void printToDoList(List<String> list){
+        int i = 1;
+        System.out.println("    ____________________________________________________________\n");
+        for (String lists : list){
+            System.out.println("    " + i + ": " + lists + "\n");
+            i++;
+        }
+        System.out.println("    ____________________________________________________________\n");
+    }
+
+
+    public static void addToList(String item, List<String> list){
+        list.add(item);
         System.out.println("    ____________________________________________________________\n" +
-                "     " + text + "\n" +
+                "    added: " + item + "\n" +
                 "    ____________________________________________________________\n");
     }
+
     public static void main(String[] args) {
+        List<String> todolist = new ArrayList<>(  100);
         System.out.println("    ____________________________________________________________\n" +
                 "     Hello! I'm TimL\n" +
                 "     What can I do for you?\n" +
@@ -16,7 +39,7 @@ public class TimL {
         Scanner in = new Scanner(System.in);
         response = in.nextLine();
         while (!response.equals("bye")){
-            respondToRandomText(response);
+            respondToCommand(response, todolist);
             in = new Scanner(System.in);
             response = in.nextLine();
         }
