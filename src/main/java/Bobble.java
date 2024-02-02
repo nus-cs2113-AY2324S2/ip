f10726d8f183d5968faa790271295bdf6bd240b1
kyuichyi <chongkaitlyn@u.nus.edu>
2024-02-02 13:01:11 +0800

    A-CodingStandard: modified case to fit coding standards.

--- src/main/java/Bobble.java
+++ src/main/java/Bobble.java
@@ -11,33 +11,34 @@ public class Bobble {
 
         while (!userInput.equals("bye")) {
             switch (userInput) {
-                case "list":
+            case "list":
+                System.out.println("____________________________________________________________\n" +
+                        "Here are the tasks in your list:");
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ".[" + tasklist[i].getStatusIcon() + "] " + tasklist[i].getDescription());
+                }
+                System.out.println("____________________________________________________________\n");
+                break;
+            default:
+                if (userInput.startsWith("unmark")) {
+                    int taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
+                    tasklist[taskNumber].setDone(false);
+                    System.out.println("OK, I've marked this task as not done yet:\n" +
+                            "  [ ] " + tasklist[taskNumber].getDescription() +
+                            "\n____________________________________________________________\n");
+                } else if (userInput.startsWith("mark")) {
+                    int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
+                    tasklist[taskNumber].setDone(true);
+                    System.out.println("Nice! I've marked this task as done:\n" +
+                            "  [X] " + tasklist[taskNumber].getDescription() +
+                            "\n____________________________________________________________\n");
+                } else {
+                    // add task to list
+                    tasklist[taskCount++] = new Task(userInput);
                     System.out.println("____________________________________________________________\n" +
-                            "Here are the tasks in your list:");
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.println((i + 1) + ".[" + tasklist[i].getStatusIcon() + "] " + tasklist[i].getDescription());
-                    }
-                    System.out.println("____________________________________________________________\n");
-                    break;
-                default:
-                    if (userInput.startsWith("unmark")) {
-                        int taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
-                        tasklist[taskNumber].setDone(false);
-                        System.out.println("OK, I've marked this task as not done yet:\n" +
-                                "  [ ] " + tasklist[taskNumber].getDescription() +
-                                "\n____________________________________________________________\n");
-                    } else if (userInput.startsWith("mark")) {
-                        int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
-                        tasklist[taskNumber].setDone(true);
-                        System.out.println("Nice! I've marked this task as done:\n" +
-                                "  [X] " + tasklist[taskNumber].getDescription() +
-                                "\n____________________________________________________________\n");
-                    } else {
-                        tasklist[taskCount++] = new Task(userInput);
-                        System.out.println("____________________________________________________________\n" +
-                                "added: " + userInput +
-                                "\n____________________________________________________________\n");
-                    }
+                            "added: " + userInput +
+                            "\n____________________________________________________________\n");
+                }
             }
             userInput = input.nextLine();
         }
