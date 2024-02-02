24b5a49537b189759fcac3c293a10d648113c778
cyhjason29 <jackplok29@gmail.com>
2024-02-02 17:09:34 +0800

    Add ability to echo commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
         System.out.println("Hello! I'm FredBot.\nWhat can I do for you?\n");
+
+        line = in.nextLine();
+        while (!line.equals("bye")) {
+            System.out.println(line+"\n");
+            line = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
