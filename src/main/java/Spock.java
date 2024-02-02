d9c93b04ff800ab112c9a697ce67c8ebc08f508d
Jeffinson Darmawan <jeffinsondarmawan200702@gmail.com>
2024-02-02 17:27:07 +0800

    Level 2

--- src/main/java/Spock.java
+++ src/main/java/Spock.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Spock {
     public static void main(String[] args) {
@@ -7,17 +9,39 @@ public class Spock {
         Scanner myObj = new Scanner(System.in);
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
+
         String input = myObj.nextLine();
+        List<String> list = new ArrayList<String>();
+        int no_of_items_in_list = 0;
 
         for(String reply = input; !reply.equals("bye"); reply = myObj.nextLine()) {
+
+
+
             System.out.println("____________________________________________________________");
-            System.out.println(reply);
+
+            if (reply.equals("list")) {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(list.get(i));
+                }
+            }
+            else {
+                list.add(no_of_items_in_list + ". " + input);
+                no_of_items_in_list++;
+                System.out.println("added: " + reply);
+            }
             System.out.println("____________________________________________________________");
         }
-
 
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
-}   
+}
+
+
+
+
+
+
+
