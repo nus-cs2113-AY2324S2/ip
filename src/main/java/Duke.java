703da627439499aca34ad4cdfff7089c6fe723c1
cyhjason29 <jackplok29@gmail.com>
2024-02-02 22:28:29 +0800

    Add the ability to store input and display list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,34 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void printList(String[] list) {
+        for (int j = 0; j < list.length; j++) {
+            if (list[j] == null) {
+                System.out.println();
+                return;
+            }
+            System.out.println(Integer.toString(j+1) + ": " + list[j]);
+        }
+    }
+
     public static void main(String[] args) {
         String line;
         Scanner in = new Scanner(System.in);
+        String[] list = new String[100];
 
         System.out.println("Hello! I'm FredBot.\nWhat can I do for you?\n");
 
         line = in.nextLine();
+        int i = 0;
         while (!line.equals("bye")) {
-            System.out.println(line+"\n");
+            if (line.equals("list")) {
+                printList(list);
+            }
+            else {
+                list[i] = line;
+                i++;
+                System.out.println("added: " + line + "\n");
+            }
             line = in.nextLine();
         }
 
