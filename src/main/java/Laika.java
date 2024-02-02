74e5d142385bda25bcfa438682545f3ff6647917
cirelesna <anseleric29@gmail.com>
2024-02-02 16:52:09 +0800

    Added ability to Echo

--- src/main/java/Laika.java
+++ src/main/java/Laika.java
@@ -1,10 +1,20 @@
+import java.util.Scanner;
+
 public class Laika {
     public static void main(String[] args) {
+        String line = "";
+        Scanner in = new Scanner(System.in);
         String logo = " ^..^      /\n"
                 + " /_/\\_____/\n"
                 + "    /\\   /\\\n"
                 + "   /  \\ /  \\\n";
-        System.out.println("Hi! My name is Laika!\n\n" + logo + "How can I help you?");
-        System.out.println("Bye! Have a nice day!");
+        System.out.println("Laika: Hi! My name is Laika!\n\n" + logo + "Laika: How can I help you?");
+        while(!line.equalsIgnoreCase("bye")){
+            System.out.print("User: ");
+            line = in.nextLine();
+            System.out.println("Laika: " + line);
+        }
+
+        System.out.println("Laika: Bye! Have a nice day!");
     }
 }
