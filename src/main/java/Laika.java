302d1a504d2ae1226f8a816e1c763c733aed0021
cirelesna <anseleric29@gmail.com>
2024-02-02 16:21:00 +0800

    Update Laika.java
    Renamed chatbot and added greet and exit

---
+++ src/main/java/Laika.java
@@ -0,0 +1,10 @@
+public class Laika {
+    public static void main(String[] args) {
+        String logo = " ^..^      /\n"
+                + " /_/\\_____/\n"
+                + "    /\\   /\\\n"
+                + "   /  \\ /  \\\n";
+        System.out.println("Hi! My name is Laika!\n\n" + logo + "How can I help you?");
+        System.out.println("Bye! Have a nice day!");
+    }
+}
