6e3daab019a124c17d6110205cba1f887609b78e
YoengKokLeong <e0968894@u.nus.edu>
2024-02-02 16:15:09 +0800

    added level 3, added Task class

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,38 +2,30 @@ import java.util.Scanner;
 import java.util.Arrays;
 
 public class Duke {
-    public static void echo() {
-        boolean ifExit = false;
-        Scanner in = new Scanner(System.in);
-        String[] list = new String[100];
-        int printCounter;
-        int listIndex = 0;
-        while (!ifExit) {
-            String line = in.nextLine();
-            if (line.equals("bye")) {
-                System.out.println("--------------------------------------");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println("--------------------------------------");
-                ifExit = true;
-            } else if (line.equals("list")) {
-                printCounter = 1;
-                System.out.println("--------------------------------------");
-                for (String item: Arrays.copyOf(list, listIndex)) {
-                    System.out.println(printCounter + ". " + item);
-                    printCounter ++;
-                }
-                System.out.println("--------------------------------------");
-            } else {
-                System.out.println("--------------------------------------");
-                System.out.println("added: " + line);
-                System.out.println("--------------------------------------");
-                list[listIndex] = line;
-                listIndex ++;
-            }
+    public static void addList(String line) {
+        System.out.println("--------------------------------------");
+        System.out.println("added: " + line);
+        System.out.println("--------------------------------------");
+    }
+
+    public static void printBye() {
+        System.out.println("--------------------------------------");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("--------------------------------------");
+    }
+
+    public static void printList(Task[] list, int listIndex) {
+        int printCounter = 1;
+        System.out.println("--------------------------------------");
+        System.out.println("Here are the tasks in your lists:");
+        for (Task item : Arrays.copyOf(list, listIndex)) {
+            System.out.println(printCounter + "." + item.getStatusIcon() + " " + item.description);
+            printCounter++;
         }
     }
-    public static void main(String[] args) {
-        String logo 
+
+    public static void introStart() {
+        String logo
                 = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
@@ -44,6 +36,34 @@ public class Duke {
         System.out.println("Hello! I'm Steffy");
         System.out.println("What can I do for you?");
         System.out.println("--------------------------------------");
-        echo();
+    }
+
+    public static void main(String[] args) {
+
+        introStart(); //Prints starting screen
+
+        boolean ifExit = false; //exits program if true
+        Scanner in = new Scanner(System.in);
+        Task[] list = new Task[100];
+        int listIndex = 0;
+
+        while (!ifExit) {
+            String line = in.nextLine();
+            if (line.equals("bye")) { //checks for bye interaction and sets ifExit to true
+                printBye();
+                ifExit = true;
+            }
+            if (line.equals("list")) { //calls printList and shows list of tasks
+                printList(list, listIndex);
+            } else if (line.contains("unmark")) { //checks for "unmark" to unmark a task
+                list[Integer.parseInt(line.substring(7)) - 1].unmarkDone();
+            } else if (line.contains("mark")) { //else checks for "mark" to mark a task
+                list[Integer.parseInt(line.substring(5)) - 1].markAsDone();
+            } else { //else adds a new task and echos task once
+                addList(line);
+                list[listIndex] = new Task(line);
+                listIndex++;
+            }
+        }
     }
 }
