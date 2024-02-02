708a5848d14a27989ecfd80b8c7424728a70ef9f
Geinzit <heinz03huang@gmail.com>
2024-02-02 17:15:59 +0800

    Add feature: bot echoes all input commands, exits when received 'bye' command

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,20 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String botName = "Huan";
         System.out.println("Hello! I'm " + botName + ", a chat bot");
-        System.out.println("Bye! See ya!");
+        while(true) {
+            System.out.println("-------------------------");
+            String userInput = scanner.nextLine();
+
+            if(userInput.equalsIgnoreCase("bye")) {
+                System.out.println("Bye! See ya!");
+                break;
+            }
+
+            System.out.println(userInput);
+        }
     }
 }
