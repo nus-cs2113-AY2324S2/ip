6db6312c21e81df3090c2ce2ff93a42098389c52
yuhengr <e0957964@u.nus.edu>
2024-02-02 15:56:45 +0800

    Add level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -7,6 +9,16 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("I'm Ekud! What can I do for you?");
+
+        String userInput;
+        Scanner in = new Scanner(System.in);
+        userInput = in.nextLine();
+
+        while(!userInput.equals("bye")){
+            System.out.println(userInput);
+            userInput = in.nextLine();
+        }
+
         System.out.println("Bye. Hope to see you again soon.");
     }
 }
