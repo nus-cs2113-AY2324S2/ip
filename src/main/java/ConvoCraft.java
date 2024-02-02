b8fef84f7b88d913e62e54c8f61472d6e8b26942
STeng618 <shantengyap@gmail.com>
2024-02-02 23:21:47 +0800

    Refactor main

--- src/main/java/ConvoCraft.java
+++ src/main/java/ConvoCraft.java
@@ -1,14 +1,23 @@
 public class ConvoCraft {
-    private static int MAX_NUM_OF_TASKS = 100;
-    public static void main(String[] args) {
-        ListKeeper listKeeper = new ListKeeper(MAX_NUM_OF_TASKS);
+    private static void printWelcomeMessage() {
         HorizontalGenerator.printHorizontal();
         System.out.println("Hello! I'm ConvoCraft");
         System.out.println("What can I do for you?");
         HorizontalGenerator.printHorizontal();
-        listKeeper.manageList();
+    }
+
+    private static void printExitMessage() {
         HorizontalGenerator.printHorizontal();
         System.out.println("Bye. Hope to see you again soon!");
         HorizontalGenerator.printHorizontal();
+    }
+
+    public static void main(String[] args) {
+        ListKeeper listKeeper = new ListKeeper();
+        LogicManager logicManager = new LogicManager(listKeeper);
+        UserInterface userInterface = new UserInterface(logicManager);
+        printWelcomeMessage();
+        userInterface.manageList();
+        printExitMessage();
     }
 }
