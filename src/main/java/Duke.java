e02efae67737c4df9c472f011fbcc2b8de8ac87f
Hws2209 <wansing03@gmail.com>
2024-02-02 13:26:51 +0800

    refactor: Extract method readInputAndExecute()

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,27 +1,6 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static void printTaskList(Task[] tasks, int numberOfTasks) {
-        System.out.println("     Here is your list:");
-        for (int i = 0; i < numberOfTasks; i++) {
-            System.out.println("     " + (i + 1) + "." + tasks[i].getDetails());
-        }
-    }
-
-    private static void modifyTaskList(Task[] tasks, String line) {
-        if (line.startsWith("mark")) {
-            int indexMarked = Integer.parseInt(line.substring(5)) - 1;
-            tasks[indexMarked].isDone = true;
-            System.out.println("     Nice! I've marked this task as done:");
-            System.out.println("       " + tasks[indexMarked].getDetails());
-        } else if (line.startsWith("unmark")) {
-            int indexUnmarked = Integer.parseInt(line.substring(7)) - 1;
-            tasks[indexUnmarked].isDone = false;
-            System.out.println("     OK, I've marked this task as not done yet:");
-            System.out.println("       " + tasks[indexUnmarked].getDetails());
-        }
-    }
-
     public static void main(String[] args) {
         String logo = "  ____   _   _      __     ______    _       _  _____\n" +
                 " / ___| | | | |    /  \\    |  _  \\  | |     | ||  ___|\n" +
@@ -33,13 +12,16 @@ public class Duke {
         System.out.println("Hello! I'm Charlie!\n" + logo);
         System.out.println("What can I do for you?");
 
-        Scanner in = new Scanner(System.in);
-        String line;
-
         final int MAX_SIZE = 100;
         Task[] tasks = new Task[MAX_SIZE];
         int numberOfTasks = 0;
 
+        readInputAndExecute(tasks, numberOfTasks);
+    }
+
+    private static void readInputAndExecute(Task[] tasks, int numberOfTasks) {
+        Scanner in = new Scanner(System.in);
+        String line;
         while (true) {
             line = in.nextLine();
             if (line.contains("bye")) {
@@ -53,13 +35,34 @@ public class Duke {
             }
 
             if (line.startsWith("mark") || line.startsWith("unmark")) {
-                modifyTaskList(tasks, line);
+                modifyTaskStatus(tasks, line);
                 continue;
             }
 
             tasks[numberOfTasks] = new Task(line);
             numberOfTasks++;
             System.out.println("     added: " + line);
+        }
+    }
+
+    private static void printTaskList(Task[] tasks, int numberOfTasks) {
+        System.out.println("     Here is your list:");
+        for (int i = 0; i < numberOfTasks; i++) {
+            System.out.println("     " + (i + 1) + "." + tasks[i].getDetails());
+        }
+    }
+
+    private static void modifyTaskStatus(Task[] tasks, String line) {
+        if (line.startsWith("mark")) {
+            int indexMarked = Integer.parseInt(line.substring(5)) - 1;
+            tasks[indexMarked].isDone = true;
+            System.out.println("     Nice! I've marked this task as done:");
+            System.out.println("       " + tasks[indexMarked].getDetails());
+        } else if (line.startsWith("unmark")) {
+            int indexUnmarked = Integer.parseInt(line.substring(7)) - 1;
+            tasks[indexUnmarked].isDone = false;
+            System.out.println("     OK, I've marked this task as not done yet:");
+            System.out.println("       " + tasks[indexUnmarked].getDetails());
         }
     }
 }
