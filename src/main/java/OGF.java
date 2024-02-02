18525993a15b9fb40ec8ea637f45d6e39a6f2aa1
samuelory <samuelong7@gmail.com>
2024-02-02 11:23:27 +0800

    increment level-1. Echo

--- src/main/java/OGF.java
+++ src/main/java/OGF.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class OGF {
     public static void main(String[] args) {
         String logo = "\n" +
@@ -10,5 +11,21 @@ public class OGF {
                 "                                              \n" +
                 "                                              \n";
         System.out.println("Welcome! I'm your \n" + logo + "Nice to meet you!");
+        System.out.println("What can I do for you?");
+
+        String input;
+        while (true) {
+            Scanner in = new Scanner(System.in);
+            input = in.nextLine();
+            if (!input.equals("bye")) {
+                System.out.println("You said: " + input);
+                System.out.println("____________________________________________________________");
+                System.out.println("What can I do for you?");
+            } else {
+                System.out.println("Bye bye now!");
+                break;
+            }
+        }
+
     }
 }
