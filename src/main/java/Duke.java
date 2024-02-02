46afd35bb8df35072f13d865bbc3b6e4a465e21d
shawnpong <110764881+shawnpong@users.noreply.github.com>
2024-02-02 10:37:03 +0800

    Added list tracking functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,12 +10,23 @@ public class Duke {
         String name = "George";
         System.out.println("Hello! I'm " + name);
 //        System.out.println("What can I do for you?");
+        String[] list = new String[100];
+        int taskCounter = 0;
 
         Scanner scanner = new Scanner(System.in);
         String line;
-        System.out.println("Say anything and I shall repeat it. Type 'bye' to exit.");
+        System.out.println("I can keep track of a to-do list for you! Just type what you want to add to the list.\nType 'list' to see what is in the list so far.\nType 'bye' to exit.");
         while (!"bye".equals((line = scanner.nextLine()))) {
-            System.out.println(line);
+            if ("list".equals(line)) {
+                System.out.println("List so far: ");
+                for (int i = 0; i < taskCounter; i++) {
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+            } else {
+                list[taskCounter] = line;
+                taskCounter++;
+                System.out.println("Added: " + line);
+            }
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
