e0e8fbbbf71fdccd82530a32ac648d4163d8debf
jthibault03 <98941496+jthibault03@users.noreply.github.com>
2024-02-01 23:53:01 -0800

    Level-2

--- src/main/java/Tickles.java
+++ src/main/java/Tickles.java
@@ -1,8 +1,11 @@
 
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class Tickles {
 
     private static boolean stillGoing = true;
+    private static List<String> list = new ArrayList<>();
     public static void main(String[] args) {
         String logo =
             "╱╭╮╱╱╱╱╭╮╱╭╮\n" +
@@ -24,10 +27,26 @@ public class Tickles {
         if (prompt.equals("bye")) {
             stillGoing = false;
             printThis("Bye. Hope to see you again soon! Mr. Tickles will miss you.");
-        } else {
-            printThis(prompt);
+        } else if (prompt.equals("list")) {
+            printList();
+        }
+        else {
+            addToList(prompt);
+            printThis("added: " + prompt);
         }
 
+    }
+
+    public static void printList() {
+        int counter = 1;
+        for (String s : list) {
+            System.out.println(counter + ". " + s);
+            counter += 1;
+        }
+    }
+
+    public static void addToList(String str) {
+        list.add(str);
     }
 
     public static void printThis(String str) {
