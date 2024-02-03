c8a830e1379763f60e39d849ad6f8d7487599054
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-02-03 15:46:01 +0800

    Add "markTask" method

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,49 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void printItems(String[] items, int itemCount) {
+    public static void printItems(Task[] tasks, int itemCount) {
+        System.out.println("Here are the tasks in your list:");
         int itemNumber = 1;
         for (int i = 0; i < itemCount; i++) {
-            System.out.println(itemNumber + ". " + items[i]);
+            System.out.print(itemNumber + ".");
+            tasks[i].printTask();
             itemNumber++;
         }
     }
+
+    public static void markTask(Task[] tasks, int itemNumber) {
+        System.out.println("Nice! I've marked this task as done:");
+        tasks[itemNumber].markAsDone();
+        System.out.print(" ");
+        tasks[itemNumber].printTask();
+    }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Ms Chatty :)");
         System.out.println("What can I do for you?");
 
         String userCommand;
-        String[] items = new String[100];
+        Task[] tasks = new Task[100];
         int itemCount = 0;
         Scanner in = new Scanner(System.in);
         userCommand = in.nextLine();
 
         while (!(userCommand.equals("bye"))) {
             if (userCommand.equals("list")) {
-                printItems(items, itemCount);
+                printItems(tasks, itemCount);
                 userCommand = in.nextLine();
                 continue;
             }
 
-            items[itemCount] = userCommand;
+            if (userCommand.contains("mark ")) {
+                String number = userCommand.replaceAll("[^0-9]", "");
+                int itemNumber = Integer.parseInt(number);
+                markTask(tasks,itemNumber-1);
+                userCommand = in.nextLine();
+                continue;
+            }
+
+            tasks[itemCount] = new Task(userCommand);
             itemCount++;
 
             System.out.println("added: " + userCommand);
