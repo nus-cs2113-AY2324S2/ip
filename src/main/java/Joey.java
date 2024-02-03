7fa5a45661474fc9a13c30d7391462f13029d8fe
jasraa <e0968320@u.nus.edu>
2024-02-03 21:48:59 +0800

    Added Level 1 functionality: Echo

--- src/main/java/Joey.java
+++ src/main/java/Joey.java
@@ -1,10 +1,26 @@
+import java.util.Scanner;
+
 public class Joey {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Joey");
+        System.out.println("Hey! I'm Joey");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            String userCommand = scanner.nextLine();
+            if (userCommand.equalsIgnoreCase("bye")) {
+                System.out.println("____________________________________________________________");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+            System.out.println("____________________________________________________________");
+            System.out.println(" " + userCommand);
+            System.out.println("____________________________________________________________");
+        }
+        scanner.close();
     }
 }
+
