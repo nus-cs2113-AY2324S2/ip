5eb755396a1a9ed419b89b4e22bc910fd655474e
Jeffinson Darmawan <jeffinsondarmawan200702@gmail.com>
2024-02-04 02:15:33 +0800

    Level 3
    with some future TODO for improvements

--- src/main/java/Spock.java
+++ src/main/java/Spock.java
@@ -47,9 +47,7 @@ public class Spock {
             }
 
             System.out.println("____________________________________________________________");
-
         }
-
         System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
