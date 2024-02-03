81c6f12df062aa17d46e719bdf4abb24f00c7a3f
LuoZhiyang <e0969568@u.nus.edu>
2024-02-04 01:17:35 +0800

    Level-4 wip

--- src/main/java/Asuka.java
+++ src/main/java/Asuka.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 
 class Task {
-    private String description;
-    private boolean isDone;
-    public static int count = 0;
+    protected String description;
+    protected boolean isDone;
 
     public Task(String description) {
         this.description = description;
@@ -18,10 +17,6 @@ class Task {
         this.isDone = false;
     }
 
-    public String getDescription() {
-        return this.description;
-    }
-
     public void printTask() {
         if (this.isDone) {
             System.out.println("[X] " + this.description);
@@ -31,105 +26,231 @@ class Task {
     }
 }
 
+class Todo extends Task {
+
+    public Todo(String description) {
+        super(description);
+    }
+
+    public void printTask() {
+        if (this.isDone) {
+            System.out.println("[T][X] " + this.description);
+        } else {
+            System.out.println("[T][ ] " + this.description);
+        }
+    }
+}
+
+class Deadline extends Task {
+
+    public Deadline(String description) {
+        super(description);
+    }
+
+    public void printTask() {
+        if (this.isDone) {
+            System.out.println("[D][X] " + this.description);
+        } else {
+            System.out.println("[D][ ] " + this.description);
+        }
+    }
+}
+
+class Event extends Task {
+
+    public Event(String description) {
+        super(description);
+    }
+
+    public void printTask() {
+        if (this.isDone) {
+            System.out.println("[E][X] " + this.description);
+        } else {
+            System.out.println("[E][ ] " + this.description);
+        }
+    }
+}
 
 enum Commands {
     list, mark, unmark, add, bye
 }
 
-
 public class Asuka {
+
+    public static final int MAX_TASKS = 100;
+
     public static void main(String[] args) {
         // Welcome message
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm Asuka\nWhat can I do for you?");
-        System.out.println("____________________________________________________________");
-        // Read input
+        printWelcomeMessage();
+
+        //Initialise
+        Commands command = null;
+        int taskCount = 0;
+        Task[] tasks = new Task[MAX_TASKS];
         Scanner myObj = new Scanner(System.in);
-        String input = myObj.nextLine();
-        Task[] tasks = new Task[100];
-        int count = 0;
-        int i;
-        boolean toDo = false;
-        boolean toUndo = false;
-        //Mark which task to do or undo
-        while (myObj != null) {
-            for (i = 1; i <= count; i++) {
-                if (input.equals("mark " + i)) {
-                    toDo = true;
-                    break;
-                } else if (input.equals("unmark " + i)) {
-                    toUndo = true;
-                    break;
-                }
-            }
+
+        //Loop through different commands, until "bye" command
+        while (command != Commands.bye) {
+
+            // Read input
+            String input = myObj.nextLine();
+            String[] inputs = input.split(" ");
+
             // Determine command
-            Commands command;
-            if (input.equals("bye")) {
-                command = Commands.bye;
-            } else if (input.equals("list")) {
-                command = Commands.list;
-            } else if (toDo) {
-                command = Commands.mark;
-            } else if (toUndo) {
-                command = Commands.unmark;
-            } else {
-                command = Commands.add;
+            command = getCommands(input, inputs);
+            if (command == null) {
+                printInvalidCommand();
+                continue;
             }
+
             // Execute command
             switch (command) {
             case mark:
-                System.out.println(
-                        "____________________________________________________________");
-                System.out.println("Nice! I've marked this task as done: ");
-                tasks[i - 1].markAsDone();
-                tasks[i - 1].printTask();
-                System.out.println(
-                        "____________________________________________________________");
-                toDo = false;
-                input = myObj.nextLine();
+                mark(inputs, taskCount, tasks);
                 break;
             case unmark:
-                System.out.println(
-                        "____________________________________________________________");
-                System.out.println("Nice! I've marked this task as undone: ");
-                tasks[i - 1].markAsUndone();
-                tasks[i - 1].printTask();
-                System.out.println(
-                        "____________________________________________________________");
-                toUndo = false;
-                input = myObj.nextLine();
+                unmark(inputs, taskCount, tasks);
                 break;
             case list:
-                System.out.println(
-                        "____________________________________________________________");
-                for (int j = 0; j < count; j++) {
-                    System.out.print(j + 1 + ". ");
-                    tasks[j].printTask();
-                }
-                System.out.println(
-                        "____________________________________________________________");
-                input = myObj.nextLine();
+                list(taskCount, tasks);
                 break;
             case add:
-                System.out.println(
-                        "____________________________________________________________");
-                System.out.println("added: " + input);
-                System.out.println(
-                        "____________________________________________________________");
-                tasks[count] = new Task(input);
-                count++;
-                input = myObj.nextLine();
+                taskCount = updateTaskCount(input, tasks, taskCount);
                 break;
             case bye:
-                System.out.println(
-                        "____________________________________________________________");
-                System.out.println("Bye. Hope to see you again soon!");
-                System.out.println(
-                        "____________________________________________________________");
+                bye();
                 myObj.close();
                 myObj = null;
                 break;
+            default:
+                break;
             }
         }
+    }
+
+    private static int updateTaskCount(String input, Task[] tasks, int taskCount) {
+        String[] inputs;
+        System.out.println("____________________________________________________________");
+        inputs = input.split(" ", 2);
+        if (inputs[0].equalsIgnoreCase("todo")) {
+            tasks[taskCount] = new Todo(inputs[1]);
+        } else if (inputs[0].equalsIgnoreCase("deadline")) {
+            inputs = inputs[1].split("/", 2);
+            if (inputs.length != 2)
+            {
+                System.out.println("Incorrect format of time specification.");
+                System.out.println("____________________________________________________________");
+                return taskCount;
+            }
+            tasks[taskCount] = new Deadline(inputs[0] + "(" + inputs[1] + ")");
+        } else {
+            inputs = inputs[1].split("/", 3);
+            if (inputs.length != 3)
+            {
+                System.out.println("Incorrect format of time specification.");
+                System.out.println("____________________________________________________________");
+                return taskCount;
+            }
+            tasks[taskCount] = new Event(inputs[0] + "(" + inputs[1] + inputs[2] + ")");
+        }
+        taskCount++;
+        System.out.println("Got it. I've added this task:");
+        tasks[taskCount - 1].printTask();
+        if (taskCount == 1) {
+            System.out.println("Now you got " + taskCount + " task in the list.");
+        } else {
+            System.out.println("Now you got " + taskCount + " tasks in the list.");
+        }
+        System.out.println("____________________________________________________________");
+        return taskCount;
+    }
+
+    private static void printInvalidCommand() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Invalid command");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void bye() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void list(int taskCount, Task[] tasks) {
+        System.out.println("____________________________________________________________");
+        for (int j = 0; j < taskCount; j++) {
+            System.out.print(j + 1 + ". ");
+            tasks[j].printTask();
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void unmark(String[] inputs, int taskCount, Task[] tasks) {
+        int taskNumber;
+        taskNumber = Integer.parseInt(inputs[1]);
+        System.out.println("____________________________________________________________");
+        if (taskNumber > 0 && taskNumber <= taskCount) {
+
+            System.out.println("Nice! I've marked this task as undone: ");
+            tasks[taskNumber - 1].markAsUndone();
+            tasks[taskNumber - 1].printTask();
+        }
+        else {
+            System.out.println("Task index out of bound, the total number of task(s) you have now is: " + taskCount);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    private static void mark(String[] inputs, int taskCount, Task[] tasks) {
+        int taskNumber;
+        taskNumber = Integer.parseInt(inputs[1]);
+        System.out.println("____________________________________________________________");
+        if (taskNumber > 0 && taskNumber <= taskCount) {
+            System.out.println("Nice! I've marked this task as done: ");
+            tasks[taskNumber - 1].markAsDone();
+            tasks[taskNumber - 1].printTask();
+        }
+        else {
+            System.out.println("Task index out of bound, the total number of task(s) you have now is: " + taskCount);
+        }
+        System.out.println("____________________________________________________________");
+    }
+
+    private static Commands getCommands(String input, String[] inputs) {
+        if ((inputs[0].equalsIgnoreCase("todo")) || (inputs[0].equalsIgnoreCase("deadline")) || (inputs[0].equalsIgnoreCase("event"))){
+            if (inputs.length == 1)
+            {
+                return null;
+            }
+            return Commands.add;
+        }
+        if (inputs.length > 2) {
+            return null;
+        } else if (inputs.length == 2 && !inputs[0].equalsIgnoreCase("mark") && !inputs[0].equalsIgnoreCase("unmark")) {
+            return null;
+        }
+        if (inputs.length == 2)
+        {
+            if (inputs[0].equalsIgnoreCase("mark") && Integer.parseInt(inputs[1]) <= MAX_TASKS && Integer.parseInt(inputs[1]) > 0) {
+                return Commands.mark;
+            } else if (inputs[0].equalsIgnoreCase("unmark") && Integer.parseInt(inputs[1]) <= MAX_TASKS && Integer.parseInt(inputs[1]) > 0) {
+                return Commands.unmark;
+            } else {
+                return null;
+            }
+        }
+        if (input.equalsIgnoreCase("bye")) {
+            return Commands.bye;
+        } else if (input.equalsIgnoreCase("list")) {
+            return Commands.list;
+        }
+        return null;
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("____________________________________________________________");
+        System.out.println("Hello! I'm Asuka\nWhat can I do for you?");
+        System.out.println("____________________________________________________________");
     }
 }
