dd9a20d7ebae6022f0f935c5316ee2884fe86ef1
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-02-03 15:31:20 +0800

    Edit chatbot to echo all commands except bye which causes an exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("Hello! I'm Ms Chatty :)");
         System.out.println("What can I do for you?");
+
+        String userCommand;
+        Scanner in = new Scanner(System.in);
+        userCommand = in.nextLine();
+        while (!(userCommand.equals("bye"))) {
+            System.out.println(userCommand);
+            userCommand = in.nextLine();
+        }
+
         System.out.println("Bye! Hope to see you again :)");
     }
 }
