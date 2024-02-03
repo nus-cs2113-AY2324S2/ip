d167a76b27a30a22d7cec3dace69df43168b7991
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-02-03 15:38:41 +0800

    Add ability to store items and print item list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,34 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static void printItems(String[] items, int itemCount) {
+        int itemNumber = 1;
+        for (int i = 0; i < itemCount; i++) {
+            System.out.println(itemNumber + ". " + items[i]);
+            itemNumber++;
+        }
+    }
     public static void main(String[] args) {
         System.out.println("Hello! I'm Ms Chatty :)");
         System.out.println("What can I do for you?");
 
         String userCommand;
+        String[] items = new String[100];
+        int itemCount = 0;
         Scanner in = new Scanner(System.in);
         userCommand = in.nextLine();
+
         while (!(userCommand.equals("bye"))) {
-            System.out.println(userCommand);
+            if (userCommand.equals("list")) {
+                printItems(items, itemCount);
+                userCommand = in.nextLine();
+                continue;
+            }
+
+            items[itemCount] = userCommand;
+            itemCount++;
+
+            System.out.println("added: " + userCommand);
             userCommand = in.nextLine();
         }
 
