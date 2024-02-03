e677158d2c5a22d1c8f68c4450be513fd87453e5
JianJiaT <tjjtjn@gmail.com>
2024-02-03 13:56:08 +0800

    Remove unused Task constructor

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -11,12 +11,7 @@ public class Task {
         this.name = name;
         this.isDone = false;
     }
-
-    public Task(String name, boolean isDone) {
-        this.name = name;
-        this.isDone = isDone;
-    }
-
+    
     public String getName() {
         return name;
     }
