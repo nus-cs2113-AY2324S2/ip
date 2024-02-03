79124a6727796596c4a33fe0bca07a6e5fc822c5
Dheekshitha2 <shreedhee0212@gmail.com>
2024-02-03 21:42:53 +0800

    Add level-1 functionality: Echo

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -1,10 +1,26 @@
 public class Alice {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
+
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
         System.out.println("Hello! I'm Alice");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println(line);
+
+        System.out.println(args[0]);
+
+        String x = "bye";
+        boolean isSame = x.equals(args[0]);
+
+        if (isSame) {
+            System.out.println(line);
+            System.out.println("Bye. Hope to see you again soon!");
+            System.out.println(line);
+        } else {
+            System.out.println(line);
+            System.out.println(args[0]);
+            System.out.println(line);
+        }
     }
 }
