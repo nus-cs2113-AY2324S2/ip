ab9e20bc4408393ec77a4bbe025bd5071c9ef819
jasraa <e0968320@u.nus.edu>
2024-02-03 21:57:34 +0800

    Added level 2 functionality: Add, List. Change the message printed for the "bye" scan to add more personality for joey!

--- src/main/java/Joey.java
+++ src/main/java/Joey.java
@@ -2,25 +2,35 @@ import java.util.Scanner;
 
 public class Joey {
     public static void main(String[] args) {
-        System.out.println("Hey! I'm Joey");
+        System.out.println("Hello! I'm Joey");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
 
         Scanner scanner = new Scanner(System.in);
+        String[] newTasks = new String[100];
+        int taskCount = 0;
 
         while (true) {
             String userCommand = scanner.nextLine();
+            System.out.println("____________________________________________________________");
+
             if (userCommand.equalsIgnoreCase("bye")) {
-                System.out.println("____________________________________________________________");
-                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println(" bye bye, take care:)!");
                 System.out.println("____________________________________________________________");
                 break;
+            } else if (userCommand.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(" " + (i + 1) + ". " + newTasks[i]);
+                }
+                System.out.println("____________________________________________________________");
+            } else {
+                newTasks[taskCount] = userCommand;
+                taskCount++;
+                System.out.println(" added: " + userCommand);
+                System.out.println("____________________________________________________________");
             }
-            System.out.println("____________________________________________________________");
-            System.out.println(" " + userCommand);
-            System.out.println("____________________________________________________________");
         }
+
         scanner.close();
     }
 }
-
