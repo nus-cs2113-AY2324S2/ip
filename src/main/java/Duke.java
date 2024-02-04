bba7c6b6c1bbf5f14fd01daf206675f1e5fc6a65
hwc0419 <hwc0419.dev@gmail.com>
2024-02-04 15:09:17 +0800

    Implement list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,20 +1,43 @@
 import java.util.Scanner;
+
 public class Duke {
+    public Duke() {
+    }
 
     public static void main(String[] args) {
-        String name = "Hirofumi";
+        String botName = "Hirofumi";
         System.out.println("************************************************************");
-        System.out.println(" Hello! I'm " + name);
+        System.out.println(" Hello! I'm " + botName);
         System.out.println(" What can I do for you?");
         System.out.println("************************************************************");
         String userInput = null;
         Scanner in = new Scanner(System.in);
-        while (userInput == null || !(userInput.equals("bye"))) {
+
+        int MAX_NUMBERED_LIST_LENGTH = 100;
+        String[] numberedList = new String[MAX_NUMBERED_LIST_LENGTH];
+        int numberedListIndex = 0;
+
+        while(true) {
             userInput = in.nextLine();
-            System.out.println(userInput);
+            if (userInput.equals("bye")) {
+                System.out.println("************************************************************");
+                System.out.println(" Bye. Hope to see you again soon!");
+                System.out.println("************************************************************");
+                break;
+            }
+            else if (!userInput.equals("list")) {
+                numberedList[numberedListIndex] = userInput;
+                System.out.println("added: " + userInput);
+                numberedListIndex++;
+            }
+            else if (numberedListIndex == 0) {
+                System.out.println("List is empty");
+            }
+            else {
+                for (int i = 0; i < numberedListIndex; i++) {
+                    System.out.println((i+1) + ". " + numberedList[i]);
+                }
+            }
         }
-        System.out.println("************************************************************");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("************************************************************");
     }
 }
