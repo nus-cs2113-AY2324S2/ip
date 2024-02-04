8326ec76b943e16ba20077c7402f1a131b6e9e74
JackieNeoCEG <e0959140@u.nus.edu>
2024-02-04 23:23:52 +0800

    Add echo function for Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,29 @@
+import java.util.Scanner;
 public class Duke {
+    static String LINE = "____________________________________________________________";
     public static void main(String[] args) {
-        /* String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
-        System.out.println("____________________________________________________________");
+        greetUser();
+        echoUser();
+        byeUser();
+    }
+    private static void greetUser() {
+        System.out.println(LINE);
         System.out.println("Hello! I'm Bartholomew, but you can call me Bart for short :)");
         System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        System.out.println(LINE);
+    }
+    private static void echoUser() {
+        Scanner in = new Scanner(System.in);
+        String command = "";
+        while (!command.equals("bye")) {
+            command = in.nextLine();
+            System.out.println(LINE);
+            System.out.println(command);
+            System.out.println(LINE);
+        }
+    }
+    private static void byeUser() {
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
-
+        System.out.println(LINE);
     }
 }
