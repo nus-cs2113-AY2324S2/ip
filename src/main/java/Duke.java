dcd11c1c6086a21c846719ea01299482d88edab6
CerealMiller <chongzhenkang@gmail.com>
2024-02-04 23:28:00 +0800

    Create echo chat functionality

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,30 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Duke\n" + "What can I do for you?\n" + "Bye. Hope to see you again!");
+        // Printing the logo and greeting message
+        String logo = "  _________   _____      _____   \n" +
+                " /   _____/  /  _  \\    /     \\  \n" +
+                " \\_____  \\  /  /_\\  \\  /  \\ /  \\ \n" +
+                " /        \\/    |    \\/    Y    \\\n" +
+                "/_______  /\\____|__  /\\____|__  /\n" +
+                "        \\/         \\/         \\/ ";
+        System.out.println("Hello! I'm SAM\n" + logo + "\n" + "What can I do for you?\n");
+
+        // Initializing scanner to read user input
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        // Main loop
+        while (!line.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+            line = in.nextLine();
+        }
+        // Exiting message
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
