bd275243b722b6fb1911aa783fd9b33017296d9a
seandooa <seandooa@yahoo.com.sg>
2024-02-04 17:48:14 +0800

    Add Deadline.java, Event.java and Todo.java as subclasses of Task.java

---
+++ src/main/java/Event.java
@@ -0,0 +1,10 @@
+public class Event extends Task {
+    protected String by;
+    protected String from;
+
+    public Event(String description, String by, String from) {
+        super(description);
+        this.by = by;
+        this.from = from;
+    }
+}
