502b812888c1d72376f4bf6d4fd4d1495901fe8e
XavierLiau34 <xavyliau@gmail.com>
2024-02-05 04:41:27 +0800

    Add Increments (+ commit, tag, push): Level-1

--- src/main/java/Dul.java
+++ src/main/java/Dul.java
@@ -1,4 +1,5 @@
-public class Duke {
+import java.util.Scanner;
+public class Dul {
     public static void main(String[] args) {
         String logo = " ____        _\n"
                 + "|  _ \\ _   _| |\n"
@@ -7,6 +8,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\n";
         System.out.println("Hello I'm\n" + logo);
         System.out.println("What can I do for you?");
+        String guyInput = "";
+        Scanner in = new Scanner(System.in);
+        while (!guyInput.equals("bye")) {
+            guyInput = in.nextLine();
+            System.out.println(guyInput);
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
