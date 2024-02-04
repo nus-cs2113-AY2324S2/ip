03fc6fdc5e5f5ad0b5c395840a4b63f47064da2b
Joellimjr <joellimjr12@gmail.com>
2024-02-04 16:38:43 +0800

    Add list command

--- src/main/java/Chelle.java
+++ src/main/java/Chelle.java
@@ -2,21 +2,43 @@ import java.util.Scanner;
 
 public class Chelle {
     public static void main(String[] args) {
-        Scanner Sc = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
+        String[] messages = new String[100];
+        int messageCount = 0;
+
         System.out.println("Hello! I'm Chelle.\nI like to talkity talkity talk!");
 
         while (true) {
             System.out.print("You: ");
-            String userInput = Sc.nextLine();
+            String userInput = scanner.nextLine();
 
             if (userInput.equals("bye")) {
-                System.out.println("Chelle: Bye. Hope to see you again soon!");
+                System.out.println("Chelle: Bye! Hope to see you again soon!");
                 break;
+            } else if (userInput.equals("list")) {
+                System.out.println("Chelle: ");
+                displayMessages(messages, messageCount);
+            } else {
+                if (messageCount < messages.length) {
+                    messages[messageCount] = userInput;
+                    messageCount++;
+                    System.out.println("Chelle: added: " + userInput);
+                } else {
+                    System.out.println("Chelle: Message limit reached. Cannot add more messages.");
+                }
             }
-
-            System.out.println("Chelle: " + userInput);
         }
 
-        Sc.close();
+        scanner.close();
+    }
+
+    private static void displayMessages(String[] messages, int count) {
+        if (count == 0) {
+            System.out.println("No messages to display.");
+        } else {
+            for (int i = 0; i < count; i++) {
+                System.out.println((i + 1) + ". " + messages[i]);
+            }
+        }
     }
 }
