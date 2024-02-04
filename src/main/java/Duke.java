e2d12b175564ff23f1fcb6659637304ba510c572
JackieNeoCEG <e0959140@u.nus.edu>
2024-02-04 23:58:58 +0800

    Add list function for Level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
 public class Duke {
     static String LINE = "____________________________________________________________";
+    static String[] tasks = new String[100];
+    static int taskCount = 0;
     public static void main(String[] args) {
         greetUser();
-        echoUser();
+        addTask();
         byeUser();
     }
     private static void greetUser() {
@@ -12,16 +14,27 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(LINE);
     }
-    private static void echoUser() {
+    private static void addTask() {
         Scanner in = new Scanner(System.in);
         String command = "";
         while (!command.equals("bye")) {
             command = in.nextLine();
-            System.out.println(LINE);
-            System.out.println(command);
-            System.out.println(LINE);
+            if (command.equals("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println(i + 1 + "."  + tasks[i]);
+                }
+                System.out.println(LINE);
+            } else {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println(LINE);
+                System.out.println("added: " + command);
+                System.out.println(LINE);
+            }
         }
     }
+
     private static void byeUser() {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println(LINE);
