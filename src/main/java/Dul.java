b0a5c4379dd4b981fafe0997fa7402a67b91325c
XavierLiau34 <xavyliau@gmail.com>
2024-02-05 23:50:15 +0800

    Level 2. Add, List

--- src/main/java/Dul.java
+++ src/main/java/Dul.java
@@ -1,5 +1,9 @@
 import java.util.Scanner;
 public class Dul {
+    public static final int maxtasks = 100;
+    public static String[] tasks = new String[maxtasks];
+    public static int taskCount = 0;
+
     public static void main(String[] args) {
         String logo = " ____        _\n"
                 + "|  _ \\ _   _| |\n"
@@ -12,8 +16,30 @@ public class Dul {
         Scanner in = new Scanner(System.in);
         while (!guyInput.equals("bye")) {
             guyInput = in.nextLine();
-            System.out.println(guyInput);
+            processInput(guyInput);
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
+
+    public static void processInput(String input) {
+        if (input.equals("list")) {
+            listTasks();
+        } else if (!input.equals("bye")) {
+            addTask(input);
+        }
+    }
+
+    public static void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println((i + 1) + ". " + tasks[i]);
+        }
+    }
+
+    public static void addTask(String task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        System.out.println("added: " + task);
+    }
 }
+
+
