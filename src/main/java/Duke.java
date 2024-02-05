07eb5ee0310a7f1c039203d97c99ef7546cea435
Sim Justin <1sim.justin@gmail.com>
2024-02-06 00:50:55 +0800

    Add init and exit functions

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,34 +1,40 @@
 public class Duke {
-    public static void displayLogo() {
-        String logo = 
-                " _    _                          _          \n"
-                + "| |  | |                        | |         \n"
-                + "| |  | |_      ___   _ _ __  ___| |__   ___ \n"
-                + "| |  | \\ \\ /\\ / / | | | '_ \\|_  / '_ \\ / _ \\\n"
-                + "| |__| |\\ V  V /| |_| | | | |/ /| | | |  __/\n"
-                + " \\____/  \\_/\\_/  \\__,_|_| |_/___|_| |_|\\___|\n";
+    static String botName = "Uwunzhe";
+    static String logo = " _    _                          _          \n"
+            + "| |  | |                        | |         \n"
+            + "| |  | |_      ___   _ _ __  ___| |__   ___ \n"
+            + "| |  | \\ \\ /\\ / / | | | '_ \\|_  / '_ \\ / _ \\\n"
+            + "| |__| |\\ V  V /| |_| | | | |/ /| | | |  __/\n"
+            + " \\____/  \\_/\\_/  \\__,_|_| |_/___|_| |_|\\___|\n";
 
+    static String lineString = "____________________________________________________________";
+
+    public static void displayLogo() {
         System.out.println(logo);
     }
 
     public static void addLineBreak() {
-        String lineString = "____________________________________________________________";
         System.out.println(lineString);
     }
 
-    public static void main(String[] args) {
-        String botName = "Uwunzhe";
+    public static void initialize() {
         displayLogo();
-        
         addLineBreak();
 
         System.out.println("HELLO MY POSITIVE MENTALITY FLEN!! MY NAME IS " + botName.toUpperCase() + "!!!");
         System.out.println("Actually uh... What even do you want me to do?");
 
         addLineBreak();
+    }
 
+    public static void exit() {
         System.out.println("Good night my positive mentality flen, "
                 + "it is time for me to take my happy pills. Bye Bye!");
         addLineBreak();
+    }
+
+    public static void main(String[] args) {
+        initialize();
+        exit();
     }
 }
