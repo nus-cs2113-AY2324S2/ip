fc085a858f2a465f2a99f8f333438979affc5a3a
lordgareth10 <51813599+lordgareth10@users.noreply.github.com>
2024-02-06 02:17:13 +0800

    Make Dross echo

--- src/main/java/Dross.java
+++ src/main/java/Dross.java
@@ -1,7 +1,24 @@
+import java.util.Scanner;
+
 public class Dross {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         System.out.println("Hello! I'm Dross");
         System.out.println("What can I do for you?");
+        System.out.println("____________________________________________________________");
+
+        Scanner in = new Scanner(System.in);
+        String line = in.nextLine();
+
+        while (!line.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(line);
+            System.out.println("____________________________________________________________");
+            line = in.nextLine();
+        }
+
+        System.out.println("____________________________________________________________");
         System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("____________________________________________________________");
     }
 }
