75e5af5bbfc9152b3d514e72f65939b66361a238
Dheekshitha2 <shreedhee0212@gmail.com>
2024-02-05 20:48:43 +0800

    Level 2: Add, List

--- src/main/java/Alice.java
+++ src/main/java/Alice.java
@@ -5,6 +5,10 @@ public class Alice {
 
         // Create scanner object to read input
         Scanner scanner = new Scanner(System.in);
+        // Create an array to store the tasks entered by the user
+        String[] tasks = new String[100];
+        // Create a variable to store the number of tasks so far
+        int taskNum = 0;
         // Create string to store line separating responses
         String line = "____________________________________________________________";
 
@@ -14,25 +18,32 @@ public class Alice {
         System.out.println("What can I do for you?");
         System.out.println(line);
 
-        // Declare the input variable
-        String input;
         // Use a condition that directly checks input
         while (true) {
             // Read the user input here
-            input = scanner.nextLine();
+            String input = scanner.nextLine();
+
             if (input.equals("bye")) {
-                // Exit loop if input is "bye"
+                System.out.println(line);
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
                 break;
+            } else if (input.equals("list")){
+                System.out.println(line);
+
+                // Print out the whole list of tasks numbered
+                for (int i = 0; i < taskNum; i++){
+                    System.out.println((i + 1) + ". "+ tasks[i]);
+                }
+                System.out.println(line);
+            } else {
+                tasks[taskNum] = input;
+                System.out.println(line);
+                System.out.println("added: " + input);
+                System.out.println(line);
+                taskNum++;
             }
-            System.out.println(line);
-            System.out.println(input); // Echo user input after reading it
-            System.out.println(line);
         }
-
-        System.out.println(line);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(line);
-
         // Close the scanner after exiting the loop
         scanner.close();
     }
