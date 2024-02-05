de2f4e02465f336e8e81b5a56a974e3ff7c053c5
SharlynLui <sharlynluitm@gmail.com>
2024-02-05 22:20:42 +0800

    Level 2 - add, list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,44 @@
+import java.util.Scanner;
+public class Duke {
+    public static void printLine() {
+        System.out.println("____________________________________________________________");
+    }
+    public static void main(String[] args) {
+        String[] list = new String[100];
+        for (int i = 0; i < 100; i++) {
+            list[i] = " ";
+        }
+        int index = 0;
+        Scanner in = new Scanner(System.in);
+        printLine();
+        System.out.println("Hello! I'm Doraemon!");
+        System.out.println("What can I do for you?");
+        printLine();
+        while (!list[index].equals("bye")) {
+            list[index] = in.nextLine();
+            if (list[index].equals("bye")) {
+                break;
+            }
+            if (list[index].equals("list")) {
+                int counter = 1;
+                for (int j = 0; j < index; j++) {
+                    System.out.print(counter + ". ");
+                    System.out.println(list[j]);
+                    counter++;
+                }
+                index--;
+            } else {
+                System.out.print("Added: ");
+                System.out.println(list[index]);
+            }
+            printLine();
+            index++;
+        }
+        System.out.println("Bye. Have a great day!");
+        printLine();
+    }
+}
+
 /*
 public class Duke {
     public static void main(String[] args) {
@@ -12,24 +53,4 @@ public class Duke {
 
  */
 
-import java.util.Scanner;
-public class Duke {
-    public static void printLine() {
-        System.out.println("____________________________________________________________");
-    }
-    public static void main(String[] args) {
-        String line = " ";
-        Scanner in = new Scanner(System.in);
-        printLine();
-        System.out.println("Hello! I'm Doraemon!");
-        System.out.println("What can I do for you?");
-        printLine();
-        while (!line.equals("bye")) {
-            line = in.nextLine();
-            System.out.println(line);
-            printLine();
-        }
-        System.out.println("Bye. Have a great day!");
-        printLine();
-    }
-}
+
