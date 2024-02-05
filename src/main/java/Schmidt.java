e08c7a8b79aa5bb1e33463f1fafc7eb6b0d843d4
mihirheda02 <87570498+mihirheda02@users.noreply.github.com>
2024-02-05 14:12:59 +0800

    Split tasks into todos, events, and deadlines

--- src/main/java/Schmidt.java
+++ src/main/java/Schmidt.java
@@ -16,14 +16,15 @@ public class Schmidt {
                 "░╚═══██╗██║░░██╗██╔══██║██║╚██╔╝██║██║██║░░██║░░░██║░░░\n" +
                 "██████╔╝╚█████╔╝██║░░██║██║░╚═╝░██║██║██████╔╝░░░██║░░░\n" +
                 "╚═════╝░░╚════╝░╚═╝░░╚═╝╚═╝░░░░░╚═╝╚═╝╚═════╝░░░░╚═╝░░░";
+        String HELLO_MESSAGE = "------------------------------------------------------------\n" +
+                " Hello! I'm Schmidt\n" +
+                " What can I do for you?\n" +
+                "------------------------------------------------------------";
         ArrayList<Task> tasks = new ArrayList<Task>();
 
         System.out.println("Hello from\n" + LOGO);
 
-        System.out.println("------------------------------------------------------------\n" +
-                " Hello! I'm Schmidt\n" +
-                " What can I do for you?\n" +
-                "------------------------------------------------------------");
+        System.out.println(HELLO_MESSAGE);
 
         Scanner sc = new Scanner(System.in);
 
@@ -31,42 +32,148 @@ public class Schmidt {
             System.out.print("\t-> ");
 
             String input = sc.nextLine();
+            String command = input.split(" ")[0];
 
-            if (input.equals("bye")) {
-                System.out.println("------------------------------------------------------------\n" +
-                        " Bye. Hope to see you again soon!\n" +
-                        "------------------------------------------------------------");
-                break;
-            } else if (input.equals("list")) {
-                System.out.println("------------------------------------------------------------\n" +
-                        " Here are the tasks in your list:");
-
-                for (int i = 0; i < tasks.size(); i++) {
-                    System.out.println(" " + (i + 1) + ". " + tasks.get(i).toString());
-                }
-
-                System.out.println("------------------------------------------------------------");
-            } else if (input.startsWith("mark")) {
+            // Switch statement to handle different commands
+            switch (command) {
+            case "bye":
+                printByeMessage();
+                return;
+            case "list":
+                printListHelper(tasks);
+                continue;
+            case "mark":
                 markTaskHelper(tasks, input, true);
-            } else if (input.startsWith("unmark")) {
+                continue;
+            case "unmark":
                 markTaskHelper(tasks, input, false);
-            } else {
-                tasks.add(new Task(input));
-
-                System.out.println("------------------------------------------------------------\n" +
-                        "added " + input + "\n" +
-                        "------------------------------------------------------------");
+                continue;
+            case "todo":
+                addTodoHelper(tasks, input);
+                continue;
+            case "deadline":
+                addDeadlineHelper(tasks, input);
+                continue;
+            case "event":
+                addEventHelper(tasks, input);
+                continue;
+            default:
+                printValidCommands();
+                continue;
             }
         }
+    }
+
+    /**
+     * This is a function to print the valid commands
+     */
+    public static void printValidCommands() {
+        System.out.println("------------------------------------------------------------\n" +
+                " I'm sorry, but the valid commands are:\n" +
+                "\tbye\n" +
+                "\tlist\n" +
+                "\ttodo <description>\n" +
+                "\tdeadline <description> /by <date>\n" +
+                "\tevent <description> /from <date> /to <date>\n" +
+                "\tmark <task number>\n" +
+                "\tunmark <task number>\n" +
+                "------------------------------------------------------------");
+    }
+
+    /**
+     * This is a function to print the bye message
+     */
+    public static void printByeMessage() {
+        System.out.println("------------------------------------------------------------\n" +
+                " Bye. Hope to see you again soon!\n" +
+                "------------------------------------------------------------");
+    }
+
+    /**
+     * This is a helper method to print the list of tasks
+     * @param tasks The list of tasks
+     */
+    public static void printListHelper(ArrayList<Task> tasks) {
+        System.out.println("------------------------------------------------------------\n" +
+                " Here are the tasks in your list:");
+
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("\t" + (i + 1) + "." + tasks.get(i).toString());
+        }
+
+        System.out.println("------------------------------------------------------------");
+    }
+
+    /**
+     * This is a helper method to print the message after adding a task
+     * @param tasks The list of tasks
+     */
+    public static void printAddedTaskMessage(ArrayList<Task> tasks) {
+        System.out.println("------------------------------------------------------------\n" +
+                "Got it. I've added this task:\n" +
+                "\t" + tasks.get(tasks.size() - 1).toString() + "\n" +
+                " Now you have " + tasks.size() + " tasks in the list.\n" +
+                "------------------------------------------------------------");
+    }
+
+    /**
+     * This is a helper method to add a todo task
+     * @param tasks The list of tasks
+     * @param input The user input
+     */
+    public static void addTodoHelper(ArrayList<Task> tasks, String input) {
+        try {
+            String description = input.substring(5);
+            tasks.add(new Todo(description));
+        } catch (StringIndexOutOfBoundsException e) {
+            printValidCommands();
+        }
+
+        printAddedTaskMessage(tasks);
+    }
+
+    /**
+     * This is a helper method to add a deadline task
+     * @param tasks The list of tasks
+     * @param input The user input
+     */
+    public static void addDeadlineHelper(ArrayList<Task> tasks, String input) {
+        try {
+            String description = input.substring(9, input.indexOf("/by") - 1);
+            String by = input.substring(input.indexOf("/by") + 4);
+            tasks.add(new Deadline(description, by));
+        } catch (StringIndexOutOfBoundsException e) {
+            printValidCommands();
+        }
+
+        printAddedTaskMessage(tasks);
+    }
+
+    /**
+     * This is a helper method to add an event task
+     * @param tasks The list of tasks
+     * @param input The user input
+     */
+    public static void addEventHelper(ArrayList<Task> tasks, String input) {
+        try {
+            String description = input.substring(6, input.indexOf("/from") - 1);
+            String from = input.substring(input.indexOf("/from") + 6, input.indexOf("/to") - 1);
+            String to = input.substring(input.indexOf("/to") + 4);
+            tasks.add(new Event(description, from, to));
+        } catch (StringIndexOutOfBoundsException e) {
+            printValidCommands();
+        }
+
+        printAddedTaskMessage(tasks);
     }
 
     /**
      * This is a helper method to mark or unmark a task as done
      * @param tasks The list of tasks
      * @param input The user input
-     * @param isDone A boolean to indicate if the task is done
+     * @param shouldMark A boolean to indicate if the task needs to be marked or unmarked as done
      */
-    public static void markTaskHelper(ArrayList<Task> tasks, String input, boolean isDone) {
+    public static void markTaskHelper(ArrayList<Task> tasks, String input, boolean shouldMark) {
         String[] tokens = input.split(" ");
 
         if (tokens.length != 2) {
@@ -93,7 +200,7 @@ public class Schmidt {
             return;
         }
 
-        if (isDone) {
+        if (shouldMark) {
             tasks.get(index).markAsDone();
 
             System.out.println("------------------------------------------------------------\n" +
