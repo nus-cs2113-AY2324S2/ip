1b73a90b84facec93e4e14fe8c5f79fc95fd83b2
SharlynLui <sharlynluitm@gmail.com>
2024-02-05 22:50:28 +0800

    Level 3 - Mark

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,8 +5,11 @@ public class Duke {
     }
     public static void main(String[] args) {
         String[] list = new String[100];
+        String[] status = new String[100];
+        //initializing the array
         for (int i = 0; i < 100; i++) {
             list[i] = " ";
+            status[i] = " ";
         }
         int index = 0;
         Scanner in = new Scanner(System.in);
@@ -20,12 +23,18 @@ public class Duke {
                 break;
             }
             if (list[index].equals("list")) {
-                int counter = 1;
+                int counter = 1; //the numbering in the list
                 for (int j = 0; j < index; j++) {
-                    System.out.print(counter + ". ");
+                    System.out.print(counter + ".[" + status[j] + "] ");
                     System.out.println(list[j]);
                     counter++;
                 }
+                index--;
+            } else if (list[index].contains("mark")) {
+                list[index] = list[index].substring(5);
+                int numForStatus = Integer.parseInt(list[index]) - 1;
+                status[numForStatus] = "X";
+                System.out.println("Done and marked! Good job!");
                 index--;
             } else {
                 System.out.print("Added: ");
