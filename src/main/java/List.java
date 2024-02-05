cc0239a7cd3587eaab6d43e678f77cb51c9ad5cd
Sim Justin <1sim.justin@gmail.com>
2024-02-06 00:55:01 +0800

    Add List class

---
+++ src/main/java/List.java
@@ -0,0 +1,29 @@
+import java.util.ArrayList;
+
+public class List {
+    private ArrayList<String> list = new ArrayList<String>();
+    private int size = 0;
+
+    /**
+     * Prints the list.
+     * 
+     * @param None
+     * @return None
+     */
+    public void printList() {
+        for (int i = 0; i < size; i++) {
+            System.out.println(i + 1 + ". " + list.get(i));
+        }
+    }
+
+    /**
+     * Adds an item to the list.
+     * 
+     * @param item The item to be added.
+     * @return None
+     */
+    public void addItem(String item) {
+        list.add(item);
+        size++;
+    }
+}
