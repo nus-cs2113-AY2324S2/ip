a26396e444374a6fdac3adea5ee619b32aa84c04
sxfoo <fsx159@gmail.com>
2024-02-05 23:58:03 +0800

    Add new class CommandManager and enum CommandType
    To handle userinput commands on a separate class

---
+++ src/main/java/CommandManager.java
@@ -0,0 +1,100 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommandManager {
+    private static final Pattern MARK_PATTERN =
+            Pattern.compile("mark (?<taskIndex>\\d+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern UNMARK_PATTERN =
+            Pattern.compile("unmark (?<taskIndex>\\d+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern LIST_PATTERN =
+            Pattern.compile("list", Pattern.CASE_INSENSITIVE);
+    private static final Pattern TODO_PATTERN =
+            Pattern.compile("todo (?<taskDescription>.+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern DEADLINE_PATTERN =
+            Pattern.compile("deadline (?<taskDescription>.+) /(?<by>.+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern EVENT_PATTERN =
+            Pattern.compile("event (?<taskDescription>.+) /(?<from>.+) /(?<to>.+)", Pattern.CASE_INSENSITIVE);
+    private static final Pattern EXIT_PATTERN =
+            Pattern.compile("bye", Pattern.CASE_INSENSITIVE);
+
+    private static Map<CommandType, Pattern> commandPatternMap = initCommandTypes();
+
+    private static Map<CommandType, Pattern> initCommandTypes() {
+        Map<CommandType, Pattern> commandPatternMap = new HashMap<>();
+
+        commandPatternMap.put(CommandType.EXIT, EXIT_PATTERN);
+        commandPatternMap.put(CommandType.MARK, MARK_PATTERN);
+        commandPatternMap.put(CommandType.UNMARK, UNMARK_PATTERN);
+        commandPatternMap.put(CommandType.LIST, LIST_PATTERN);
+        commandPatternMap.put(CommandType.TODO, TODO_PATTERN);
+        commandPatternMap.put(CommandType.DEADLINE, DEADLINE_PATTERN);
+        commandPatternMap.put(CommandType.EVENT, EVENT_PATTERN);
+
+        return commandPatternMap;
+    }
+
+    protected static CommandType getCommandType(String userInput) {
+        for (Map.Entry<CommandType, Pattern> entry : commandPatternMap.entrySet()) {
+            // Retrieve the Pattern associated with the command in the entry
+            Pattern commandPattern = entry.getValue();
+
+            // Create a Matcher object using the current Pattern and the user input
+            Matcher matcher = commandPattern.matcher(userInput);
+
+            // Check if the entire input sequence matches the pattern
+            if (matcher.matches()) {
+                // If there is a match, return the corresponding CommandType
+                return entry.getKey();
+            }
+        }
+        return CommandType.INVALID;
+    }
+
+    protected static String[] getCommandArguments(CommandType command, String userInput) {
+        // Check if command-pattern key-value exists in hashmap
+        // Get the pattern associated with the command
+        Pattern commandPattern = commandPatternMap.get(command);
+        if (commandPattern == null) {
+            return null;
+        }
+
+        // Check if userinput matches the associated pattern
+        Matcher matcher = commandPattern.matcher(userInput);
+        if (!matcher.matches()) {
+            return null;
+        }
+
+        // Return in String[], the user arguments associated with the command
+        // eg. <taskIndex> for mark, <taskDescription> && <by> for deadline,
+        String[] arguments = null;
+
+        switch (command) {
+        case MARK:
+        case UNMARK:
+            arguments = new String[1];
+            arguments[0] = matcher.group("taskIndex");
+            break;
+        case TODO:
+            arguments = new String[1];
+            arguments[0] = matcher.group("taskDescription");
+            break;
+        case DEADLINE:
+            arguments = new String[2];
+            arguments[0] = matcher.group("taskDescription");
+            arguments[1] = matcher.group("by");
+            break;
+        case EVENT:
+            arguments = new String[3];
+            arguments[0] = matcher.group("taskDescription");
+            arguments[1] = matcher.group("from");
+            arguments[2] = matcher.group("to");
+            break;
+        default:
+            return null;
+        }
+
+        return arguments;
+    }
+}
---
+++ src/main/java/CommandType.java
@@ -0,0 +1,10 @@
+public enum CommandType {
+    MARK,
+    UNMARK,
+    LIST,
+    TODO,
+    DEADLINE,
+    EVENT,
+    EXIT,
+    INVALID
+}
