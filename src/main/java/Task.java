4af211c64fb380c01b9fe961c4f28db408246467
Bryvo <bryancastorius@gmail.com>
2024-02-05 19:49:45 +0800

    Update Task.java
    Indentation fix

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,4 +1,5 @@
 public class Task {
+<<<<<<< HEAD
 				protected String description;
 				protected boolean isDone;
 
@@ -22,5 +23,30 @@ public class Task {
 				public String getDescription() {
 								return description;
 				}
+=======
+	protected String description;
+	protected boolean isDone;
+
+	public Task(String description) {
+					this.description = description;
+					this.isDone = false;
+	}
+
+	public String getStatusIcon() {
+					return (isDone ? "[X]" : "[ ]");
+	}
+
+	public void markAsDone() {
+					this.isDone = true;
+	}
+
+	public void unmarkAsDone() {
+					this.isDone = false;
+	}
+
+	public String getDescription() {
+					return description;
+	}
+>>>>>>> parent of 0daec96 (Task Indentation Error)
 }
 
