11eb6117ac6717ccd3b50d6bb30414bf6e678bfb
Mmaxx15 <leongxwmax@gmail.com>
2024-02-06 22:00:12 +0800

    Revert "moved functions related to marking and unmarking tasks into new class called Todo.java"
    This reverts commit 41feeeb284787f778310638f58f937007d841820.

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,58 +1,15 @@
 import java.util.Scanner;
 public class TaskList {
-    private final Task[] taskList;
-    private static final int MAX_TASKS = 100;
-    private int taskCount;
+        private final Task[] taskList;
+        private static final int MAX_TASKS = 100;
+        //protected boolean[] isMarked;
+        private int taskCount;
 
-    public TaskList() {
-        this.taskList = new Task[MAX_TASKS];
-        this.taskCount = 0;
-    }
 
-    public void addTask(Task task) {
-        taskList[taskCount] = task;
-        taskCount += 1;
-        System.out.println("added: " + task);
-    }
-
-    public void listTasks() {
-        if (taskCount == 0) {
-            System.out.println("Dobby has no tasks :(");
-            return;
+        public TaskList() {
+            this.taskList = new Task[MAX_TASKS];
+            this.taskCount = 0;
         }
-        System.out.println("List\n~~~~~~~~~~~~~~~~");
-        for (int i = 0 ; i < taskCount ; i += 1) {
-            System.out.println("  " + (i+1) + ". " + taskList[i]);
-        }
-        System.out.println("~~~~~~~~~~~~~~~~");
-    }
-
-    public void markTask(int taskIndex) {
-        Todo.markTask(taskList, taskIndex);
-    }
-
-    public void unmarkTask(int taskIndex) {
-        Todo.unmarkTask(taskList, taskIndex);
-    }
-
-    public void userCommand() {
-        Scanner scanner = new Scanner(System.in);
-        while(true) {
-            String command = scanner.nextLine().toLowerCase();
-
-            switch (command) {
-            case "bye":
-                System.out.println("~~~~~~~~~~~~~~~~\nDobby says BYE!");
-                return;
-            case "list":
-                listTasks();
-                break;
-            default:
-                processTaskCommand(command);
-                break;
-            }
-        }
-    }
 
     private void processTaskCommand(String command) {
         String[] commandParts = command.split(" ", 2);
@@ -77,10 +34,112 @@ public class TaskList {
             }
             break;
         default:
-            addTask(new Task(command)); // Default to addTask if not recognized
+            addTask(new Task(command)); // Default to addTask if not recognised
             break;
         }
     }
+
+        /**
+         * Adds user input into taskList
+         *
+         * @param task input given by user
+         */
+        public void addTask(Task task) {
+            taskList[taskCount] = task;
+            //isMarked[taskCount] = false;
+            taskCount += 1;
+            System.out.println("added: " + task);
+        }
+
+        /**
+         * list out current tasks and displays task status
+         */
+        public void listTasks() {
+            if (taskCount == 0) {
+                System.out.println("Dobby has no tasks :(");
+                return;
+            }
+            System.out.println("List\n~~~~~~~~~~~~~~~~");
+            for (int i = 0 ; i < taskCount ; i += 1) {
+                System.out.println("  " + (i+1) + ". " + taskList[i]);
+            }
+            System.out.println("~~~~~~~~~~~~~~~~");
+        }
+
+        private Boolean isValidIndex(int index) {
+            return index >= 0 && index < taskCount;
+        }
+
+        /**
+         * 1. Checks if user inputs a valid index if valid
+         * marks the stated task and shows which task is marked
+         *
+         * @param taskIndex index of task stored in array
+         */
+        public void markTask(int taskIndex) {
+            if (!isValidIndex(taskIndex)) {
+                System.out.println("Invalid number! Please try again");
+            } else if (taskList[taskIndex].isDone()) {
+                System.out.println("ERROR: task is already marked");
+            } else {
+                taskList[taskIndex].markTask();
+                System.out.println("OK, Dobby has marked this task as done:");
+                System.out.println("  " + taskList[taskIndex]);
+                System.out.println("~~~~~~~~~~~~~~~~");
+            }
+        }
+
+        /**
+         * Checks if user inputs a valid index
+         * if valid, unmarks the stated task and shows the unmarked task
+         *
+         * @param taskIndex index of task stored in array
+         */
+        public void unmarkTask(int taskIndex) {
+            if (!isValidIndex(taskIndex)) {
+                System.out.println("Invalid number> Please try again");
+            } else if (!taskList[taskIndex].isDone()) {
+                System.out.println("The task is already unmarked");
+            } else {
+                taskList[taskIndex].unmarkTask();
+                System.out.println("OK, Dobby marked this task as not done:");
+                System.out.println("  " + taskList[taskIndex]);
+                System.out.println("~~~~~~~~~~~~~~~~");
+            }
+        }
+
+        public void userCommand() {
+            while(true) {
+                String command;
+                Scanner in = new Scanner(System.in);
+                command = in.nextLine().toLowerCase();
+
+                switch (command) {
+                case "bye":
+                    System.out.println("~~~~~~~~~~~~~~~~\nDobby say's BYE!");
+                    return;
+                case "list":
+                    listTasks();
+                    break;
+                default:
+                    processTaskCommand(command);
+                    break;
+//                if (command.startsWith("add")) {
+//                    addTask(command.substring(4).trim());
+//                } else if (command.startsWith("mark")) {
+//                    int taskIndex = Integer.parseInt(command.split(" ")[1]);
+//                    markTask(taskIndex);
+//                } else if (command.startsWith("unmark")) {
+//                    int taskIndex = Integer.parseInt(command.split(" ")[1]);
+//                    unmarkTask(taskIndex);
+//                } else {
+//                    addTask(command);
+//                }
+//                break;
+                }
+
+            }
+        }
 
 
 
