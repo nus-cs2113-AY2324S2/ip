9109f7b9119b96faff23d0fa672b6b5bf109f41f
nicknamenic <ranjnicoli1@gmail.com>
2024-02-07 00:38:23 +0800

    Level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,52 @@
+import java.util.Scanner;
 public class Duke {
+    private static String[] tasks = new String[100];
+    private static int taskCount = 0;
+
+    public static void addTask(String task){
+        tasks[taskCount] = task;
+        taskCount++;
+    }
+
+    public static void printTasks() {
+        int originalCount = taskCount;
+        for (String s: tasks){
+            if (taskCount > 0) {
+                System.out.println(s);
+                taskCount--;
+            } else {
+                break;
+            }
+        }
+        taskCount = originalCount;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Nocturne!");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+        String input;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Good evening. I'm Nocturne.");
+        System.out.println("What ails you on this fine day?");
+        input = in.nextLine();
+
+        while (!input.equals("bye")) {
+            if (input.equals("enter task mode")) {
+                System.out.println("Entered task mode.");
+                while (!input.equals("exit task mode")) {
+                    input = in.nextLine();
+                    if (input.equals("list")) {
+                        printTasks();
+                        continue;
+                    }
+                    addTask(input);
+                    System.out.println("added: " + input);
+                }
+                System.out.println("Exited task mode.");
+            } else {
+                System.out.println(input);
+                input = in.nextLine();
+            }
+        }
+        System.out.println("Farewell, and may the fortunes be ever in your favour.");
     }
 }
