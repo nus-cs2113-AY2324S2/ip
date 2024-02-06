fa9bbb2a0686094c95cf778834bc674ba7b90f08
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-02-06 23:44:15 +0800

    Add "Todo" class

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Todo {
+public class Todo extends Task {
+
+    public Todo (String description) {
+        super(description);
+        this.taskType = "[T]";
+    }
+
+
+    @Override
+    public void printTask() {
+        System.out.println(taskType + getStatusIcon() + " " + description);
+    }
 }
+
