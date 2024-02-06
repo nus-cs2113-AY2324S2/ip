bb59e11ae742ced604597031aa5bd23d901067f0
eliz <tan_eliz@hotmail.com>
2024-02-06 12:51:02 +0800

    Edited the code to fit the coding standard

--- src/main/java/Miku.java
+++ src/main/java/Miku.java
@@ -18,31 +18,32 @@ public class Miku {
             System.out.println("______________________");
 
             if (line.equals("list")) {
-                System.out.println("Here are your list items!");
+                System.out.println("Here are ycxdfqwert1e123our list items!");
                 for (int i = 0; i < numberOfListItems; i++) {
-                    System.out.println(i+1 + ". [" + storedList[i].getStatusIcon() + "] " + storedList[i].description + "\n");
+                    System.out.println(i + 1 + ". [" + storedList[i].getStatusIcon()
+                            + "] " + storedList[i].description + "\n");
                 }
-            } else if (line.contains("mark")){
+            } else if (line.contains("mark")) {
                 boolean isUnmarking = line.contains("unmark");
-                String printedLine = (isUnmarking ? "Aww... I've marked it as undone." : "Good job~! I've marked it as done");
+                String printedLine = (isUnmarking ? "Aww... I've marked it as undone."
+                        : "Good job~! I've marked it as done");
                 System.out.println(printedLine);
                 String[] markList = line.split(" ");
                 String listNumberString = markList[(markList.length - 1)];
                 int listNumberInt = Integer.parseInt(listNumberString);
                 storedList[listNumberInt-1].isDone = (!isUnmarking);
-                System.out.println("[" + storedList[listNumberInt-1].getStatusIcon() + "] " + storedList[listNumberInt-1].description + "\n");
+                System.out.println("[" + storedList[listNumberInt-1].getStatusIcon()
+                        + "] " + storedList[listNumberInt-1].description + "\n");
             } else {
                     System.out.println("added: " + newItem);
                     storedList[numberOfListItems] = new Task(newItem);
                     storedList[numberOfListItems].description = newItem;
                     numberOfListItems++;
             }
-
             System.out.println("______________________");
             line = in.nextLine();
             newItem = line;
         }
-
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("______________________");
     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,22 +1,11 @@
 public class Task {
     protected String description;
     protected boolean isDone;
-
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
-
     public String getStatusIcon() {
         return (isDone ? "X" : " "); // mark done task with X
     }
-
-    public void setDescription(String inputDescription) {
-        this.description = inputDescription;
-    }
-
-    public String getDescription() {
-        return this.description;
-    }
-
 }
