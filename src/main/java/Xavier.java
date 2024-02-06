5146c6f65b61eff94a76f3e9a8e090c612d8986f
Jason Lienardi <lienardijason@gmail.com>
2024-02-06 17:14:23 +0800

    Automated Text UI Testing

--- src/main/java/Xavier.java
+++ src/main/java/Xavier.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Xavier {
     public static final String LINE = "_________________________________________________________________";
-
+    static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+
         TaskManager taskManager = new TaskManager();
         printWelcomeMessage();
-
         while (true) {
             String command = input.nextLine();
             System.out.println(LINE);
             if (command.equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 System.out.println(LINE);
+                exit();
                 return;
             }
             else {
@@ -28,6 +28,10 @@ public class Xavier {
         System.out.println("Hello! I'm " + "Xavier");
         System.out.println("What can I do for you?");
         System.out.println(LINE);
+    }
+
+    private static void exit() {
+        input.close();
     }
 }
 
