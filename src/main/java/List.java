a215635622d18bcca3a29feabb8ed2291ab2849d
PureUsagi <ngjunhancanoe@gmail.com>
2024-02-06 20:16:32 +0800

    Improve Code Quality

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -10,42 +10,48 @@ public class List {
     public void printTaskCount() {
         System.out.println(String.format("\tYou now have %d tasks in the list.", itemCount));
     }
-    public void addTodo(String description) {
-        Task newTask = new Todo(description);
-        list[itemCount] = newTask;
-        itemCount++;
-        System.out.println("\tGot it. I've added this task:");
-        System.out.println(String.format("\t\t %s", newTask));
-        printTaskCount();
-    }
 
-    public void addDeadline(String description, String by) {
-        Task newTask = new Deadline(description, by);
-        list[itemCount] = newTask;
-        itemCount++;
-        System.out.println("\tGot it. I've added this task:");
-        System.out.println(String.format("\t\t %s", newTask));
-        printTaskCount();
-    }
-
-    public void addEvent(String description, String from, String to) {
-        Task newTask = new Event(description, from, to);
-        list[itemCount] = newTask;
-        itemCount++;
+    private void printAddTaskMessage(Task newTask) {
         System.out.println("\tGot it. I've added this task:");
         System.out.println(String.format("\t\t%s", newTask));
         printTaskCount();
     }
+
+    private void addTask(Task newTask) {
+        list[itemCount] = newTask;
+        itemCount++;
+    }
+
+    public void addTodo(String description) {
+        Task newTask = new Todo(description);
+        addTask(newTask);
+        printAddTaskMessage(newTask);
+    }
+
+    public void addDeadline(String description, String by) {
+        Task newTask = new Deadline(description, by);
+        addTask(newTask);
+        printAddTaskMessage(newTask);
+    }
+
+    public void addEvent(String description, String from, String to) {
+        Task newTask = new Event(description, from, to);
+        addTask(newTask);
+        printAddTaskMessage(newTask);
+    }
+
     public void markTask(int index) {
         list[index - 1].setTaskAsDone();
         System.out.println("\tNice! I've marked this task as done:");
         System.out.println(String.format("\t%s",list[index-1]));
     }
+
     public void unmarkTask(int index) {
         list[index - 1].setTaskAsNotDone();
         System.out.println("\tOK, I've marked this task as not done yet:");
         System.out.println(String.format("\t%s",list[index-1]));
     }
+
     public void listAll() {
         System.out.println("\tHere are the tasks in your list:");
         for (int i = 0 ; i < itemCount; i++) {
--- src/main/java/Misty.java
+++ src/main/java/Misty.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class Misty {
-    public static void printLineOutput() {
+    public static void printMessageBorder() {
         System.out.println("\t--------------------------------------------------");
     }
 
@@ -9,20 +9,20 @@ public class Misty {
         String welcome = "Hello! I'm Misty\n"
                 + "\tWhat can I do for you?";
 
-        printLineOutput();
+        printMessageBorder();
         System.out.println("\t" + welcome);
-        printLineOutput();
+        printMessageBorder();
     }
 
     public static void printByeMessage() {
         String bye = "Bye! Hope to see you again soon!";
 
-        printLineOutput();
+        printMessageBorder();
         System.out.println("\t" + bye);
-        printLineOutput();
+        printMessageBorder();
     }
 
-    public static void printUnknownCommand() {
+    public static void printUnknownCommandMessage() {
         String bye = "Sorry, unknown command entered!";
         System.out.println("\t" + bye);
     }
@@ -38,40 +38,43 @@ public class Misty {
             input = in.nextLine();
             switch (input) {
             case "list":
-                printLineOutput();
+                printMessageBorder();
                 itemList.listAll();
-                printLineOutput();
+                printMessageBorder();
                 break;
             case "bye":
                 break;
             default:
-                printLineOutput();
+                printMessageBorder();
                 if (input.startsWith("unmark")) {
                     int index;
                     index = Integer.parseInt(input.substring(7));
+
                     itemList.unmarkTask(index);
                 } else if (input.startsWith("mark")) {
                     int index;
                     index = Integer.parseInt(input.substring(5));
+
                     itemList.markTask(index);
                 } else if (input.startsWith("todo")) {
                     String description = input.substring(input.indexOf(" ")).trim();
+
                     itemList.addTodo(description);
-                    printLineOutput();
-                    break;
                 } else if (input.startsWith("deadline")) {
                     String description = input.substring(input.indexOf(" "), input.indexOf("/by")).trim();
                     String by = input.substring(input.indexOf("/by") + 4);
+
                     itemList.addDeadline(description, by);
                 } else if (input.startsWith("event")) {
                     String description = input.substring(input.indexOf(" "), input.indexOf("/from")).trim();
                     String from = input.substring(input.indexOf("/from") + 6, input.indexOf("/to")).trim();
                     String to = input.substring(input.indexOf("/to") + 3).trim();
+
                     itemList.addEvent(description, from, to);
                 } else {
-                    printUnknownCommand();
+                    printUnknownCommandMessage();
                 }
-                printLineOutput();
+                printMessageBorder();
             }
 
             if (input.equals("bye")) {
