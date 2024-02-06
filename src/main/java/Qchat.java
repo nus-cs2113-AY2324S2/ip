53b69a623032fc246796e12b3e725c3a41c6ba25
晨珖 姚 <1014717807@QQ.com>
2024-02-06 16:55:47 +0800

    changed name of Duke

---
+++ src/main/java/Qchat.java
@@ -0,0 +1,19 @@
+public class Qchat {
+
+   static final String  WELCOME_GREETING = new String(
+           "Qchat ,A truly humanized intelligent voice assisstant \n"
+                   +"knows better about life and better about you\n"
+                   +"What can I do for you?\n"
+                   +"----------------------------------------------------------------\n") ;
+   static final String GOODBYE_GREETING = new String(
+           "goodbye\n"+
+                   "Qchat, your life-long trusted companion\n");
+
+   public static void main(String[] args) {
+
+        System.out.print(WELCOME_GREETING);
+        System.out.print(GOODBYE_GREETING);
+
+   }
+
+}
