125183c7b1255a15426e239bf3290462958f04ca
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-02-07 00:54:09 +0800

    Add Todo, Deadline, Event Class

---
+++ src/main/java/Event.java
@@ -0,0 +1,18 @@
+public class Event extends Task {
+    protected String startDayAndTime;
+    protected String endDayAndTime;
+
+    public Event (String description, String startDayAndTime, String endDayAndTime) {
+        super(description);
+        this.startDayAndTime = startDayAndTime;
+        this.endDayAndTime = endDayAndTime;
+    }
+    @Override
+    public String getDescription(){
+        return description + " (from: " + startDayAndTime + " to: " + endDayAndTime;
+    }
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (from: " + startDayAndTime + " to: " + endDayAndTime + ")";
+    }
+}
