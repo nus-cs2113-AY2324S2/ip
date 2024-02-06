7b7546b75f93deac53f1f3f722caa61f5c506378
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-02-06 23:44:27 +0800

    Add "Deadline" class

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Deadline {
+public class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+        this.taskType = "[D]";
+    }
+    public String getBy() {
+        return by;
+    }
+
+    public void setBy(String by) {
+        this.by = by;
+    }
+
+    @Override
+    public void printTask() {
+        System.out.println(taskType + getStatusIcon() + " " + description + "(by:" + by + ")");
+    }
 }
+
