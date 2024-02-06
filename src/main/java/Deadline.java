27048ac788dae16471c868e046b189f903f56840
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-02-07 01:42:02 +0800

    Create Deadline subclass

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,24 @@
+public class Deadline extends Task{
+    protected String by;
+    public Deadline(String taskDescription, String by) {
+        super(taskDescription);
+        this.taskType = TaskType.DEADLINE;
+        this.by = by;
+    }
+
+    public String getBy() {
+        return this.by;
+    }
+
+
+    @Override
+    public void printAdd(int todosCount) {
+        System.out.println("Got it! I've added this task: ");
+        System.out.println("  [D][ ] "
+                + this.getTaskDescription()
+                + " (by: "
+                + this.by
+                + ")");
+        System.out.println("Now you have " + todosCount + " tasks in your list.");
+    }
+}
