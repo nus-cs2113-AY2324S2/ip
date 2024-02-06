e2eef964ae90224f95da0cda333ff6985991f735
Bryvo <bryancastorius@gmail.com>
2024-02-06 12:01:25 +0800

    Updated Task.java indentation

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,31 +1,29 @@
 public class Task {
-	protected String description;
-	protected boolean isDone;
+    protected String description;
+    protected boolean isDone;
 
-	public Task(String description) {
-		this.description = description;
-		this.isDone = false;
-	}
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
 
-	public String getStatusIcon() {
-		return (isDone ? "[X]" : "[ ]");
-	}
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]");
+    }
 
-	public void markAsDone() {
-		this.isDone = true;
-	}
+    public void markAsDone() {
+        this.isDone = true;
+    }
 
-	public void unmarkAsDone() {
-		this.isDone = false;
-	}
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public String toString() {
-		return getStatusIcon() + " " + getDescription();
-	}
+    public String toString() {
+        return getStatusIcon() + " " + getDescription();
+    }
 }
-
-
