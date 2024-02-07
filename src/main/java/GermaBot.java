37892d1d0c8ac738ee37e3ca2d4d062fabcf40ff
Fureimi <e0957998@u.nus.edu>
2024-02-07 21:57:32 +0800

    Level 1-1

--- src/main/java/GermaBot.java
+++ src/main/java/GermaBot.java
@@ -1,13 +1,24 @@
+import java.util.Scanner;
+
 public class GermaBot {
     public static void main(String[] args) {
         String WelcomeMessage = "____________________ \n"
                 + "Hello! GermaBot here! \n"
                 + "What may I do for you this fine day? \n"
                 + "____________________";
+        System.out.println(WelcomeMessage);
+        while (true) {
+            String echo;
+            Scanner in = new Scanner(System.in);
+            echo = in.nextLine();
+            if (echo.equals("bye")) {
+                break;
+            }
+            System.out.println(echo);
+        }
         String GoodbyeMessage = "____________________ \n"
                 + "Thanks for using me! Hope you again soon~! \n"
                 + "____________________";
-        System.out.println(WelcomeMessage);
         System.out.println(GoodbyeMessage);
     }
 }
