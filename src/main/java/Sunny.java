5067e11925d54bf76ec8358238163219d7f8a890
tiffanyliu0220 <tiffanyliu0220@gmail.com>
2024-02-07 23:28:13 +0800

    Week 2 Level-0: Rename, Greet, Exit

---
+++ src/main/java/Sunny.java
@@ -0,0 +1,13 @@
+public class Sunny {
+    public static void main(String[] args) {
+        String chatbotname = "Sunny";
+
+        //Greets the user
+        System.out.println("Hello! I'm " + chatbotname);
+        System.out.println("What can I do for you?");
+        System.out.println(" ");
+
+        //Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
