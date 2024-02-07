8d473bdbc41680ca00ab7ccc71ec14d36cd78b05
Fureimi <e0957998@u.nus.edu>
2024-02-07 21:35:21 +0800

    Increment Level 1

---
+++ src/main/java/GermaBot.java
@@ -0,0 +1,13 @@
+public class GermaBot {
+    public static void main(String[] args) {
+        String WelcomeMessage = "____________________ \n"
+                + "Hello! GermaBot here! \n"
+                + "What may I do for you this fine day? \n"
+                + "____________________";
+        String GoodbyeMessage = "____________________ \n"
+                + "Thanks for using me! Hope you again soon~! \n"
+                + "____________________";
+        System.out.println(WelcomeMessage);
+        System.out.println(GoodbyeMessage);
+    }
+}
