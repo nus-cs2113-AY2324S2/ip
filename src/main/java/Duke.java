2626335baa6ada06e62552fbe7b046aa8a8b46e5
R M Raajamani <>
2024-02-07 21:22:09 +0800

    Level 4. ToDos, Events, Deadlines

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.ArrayList;
 
 class Task {
     protected String description;
@@ -15,12 +15,8 @@ class Task {
         isDone = true;
     }
 
-    public void unmark() {
-        isDone = false;
-    }
-
     public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]"); // Mark done task with X
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
     }
 
     @Override
@@ -29,9 +25,47 @@ class Task {
     }
 }
 
+class Todo extends Task {
+    public Todo(String description) {
+        super(description);
+    }
+
+    @Override
+    public String toString() {
+        return "[T]" + super.toString();
+    }
+}
+
+class Deadline extends Task {
+    protected String by;
+
+    public Deadline(String description, String by) {
+        super(description);
+        this.by = by;
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+}
+
+class Event extends Task {
+    protected String at;
+
+    public Event(String description, String at) {
+        super(description);
+        this.at = at;
+    }
+
+    @Override
+    public String toString() {
+        return "[E]" + super.toString() + " (at: " + at + ")";
+    }
+}
+
 public class Duke {
-    private static final int MAX_TASKS = 100;
-    private static List<Task> tasks = new ArrayList<>();
+    private static final List<Task> tasks = new ArrayList<>();
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
@@ -53,20 +87,37 @@ public class Duke {
                 for (int i = 0; i < tasks.size(); i++) {
                     System.out.println((i + 1) + "." + tasks.get(i));
                 }
+            } else if (input.startsWith("todo")) {
+                String taskDescription = input.substring(5);
+                Todo newTask = new Todo(taskDescription);
+                tasks.add(newTask);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + newTask);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else if (input.startsWith("deadline")) {
+                String[] parts = input.substring(9).split(" /by ");
+                Deadline newTask = new Deadline(parts[0], parts[1]);
+                tasks.add(newTask);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + newTask);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
+            } else if (input.startsWith("event")) {
+                String[] parts = input.substring(6).split(" /from ");
+                Event newTask = new Event(parts[0], parts[1]);
+                tasks.add(newTask);
+                System.out.println("Got it. I've added this task:");
+                System.out.println("  " + newTask);
+                System.out.println("Now you have " + tasks.size() + " tasks in the list.");
             } else if (input.startsWith("mark")) {
-                int taskIndex = Integer.parseInt(input.substring(5)) - 1;
-                Task task = tasks.get(taskIndex);
-                task.markAsDone();
+                int index = Integer.parseInt(input.substring(5)) - 1;
+                tasks.get(index).markAsDone();
                 System.out.println("Nice! I've marked this task as done:");
-                System.out.println(task);
+                System.out.println("  " + tasks.get(index));
             } else if (input.startsWith("unmark")) {
-                int taskIndex = Integer.parseInt(input.substring(7)) - 1;
-                Task task = tasks.get(taskIndex);
-                task.unmark();
+                int index = Integer.parseInt(input.substring(7)) - 1;
+                tasks.get(index).isDone = false;
                 System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println(task);
-            } else {
-                addTask(input);
+                System.out.println("  " + tasks.get(index));
             }
 
             System.out.println(line);
@@ -76,15 +127,5 @@ public class Duke {
         System.out.println(line);
 
         scanner.close();
-    }
-
-    private static void addTask(String description) {
-        if (tasks.size() < MAX_TASKS) {
-            Task newTask = new Task(description);
-            tasks.add(newTask);
-            System.out.println("added: " + description);
-        } else {
-            System.out.println("Task list is full!");
-        }
     }
 }
