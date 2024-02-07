d84981e37a5f5475eaf7f891770ae626d0342d80
YHWong20 <yihao.wong@u.nus.edu>
2024-02-07 16:29:13 +0800

    Reorganize code structure and add comments

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -8,38 +8,51 @@ public class Parser {
         this.inputReader = new Scanner(System.in);
     }
 
-    public String parseCommand() {
-        return inputReader.next();
+    /**
+     * Parse user command (e.g., list, deadline, bye)
+     */
+    public Command parseCommand() {
+        return Command.valueOf(inputReader.next().toUpperCase());
     }
 
-    public void clearInput() {
-        inputReader.nextLine();
-    }
+    /**
+     * Parse arguments provided (e.g., /from, /by, /to)
+     */
+    public String[] parseArguments(Command userCommand) {
+        int expectedArgumentCount;
 
-    public String[] parseArguments(int expectedArgumentCount) {
-        String[] arguments = new String[expectedArgumentCount];
-        MatchResult argumentMatches;
-
-        switch (expectedArgumentCount) {
-        case 2:
-            // Deadline command
+        switch (userCommand) {
+        case DEADLINE:
             inputReader.findInLine("(.+) /by (.+)"); // Set regex format
+            expectedArgumentCount = 2;
             break;
-        case 3:
-            // Event command
+        case EVENT:
             inputReader.findInLine("(.+) /from (.+) /to (.+)"); // Set regex format
+            expectedArgumentCount = 3;
             break;
         default:
             inputReader.findInLine("(.+)"); // Set regex format
+            expectedArgumentCount = 1;
             break;
         }
 
-        argumentMatches = inputReader.match();
+        MatchResult argumentMatches = inputReader.match();
+        String[] arguments = new String[expectedArgumentCount]; // Return arguments in String array
 
         for (int i = 1; i <= expectedArgumentCount; i++) {
+            // Extract provided arguments
             arguments[i - 1] = argumentMatches.group(i).strip();
         }
 
         return arguments;
+    }
+
+    public boolean isDone(Command userCommand) {
+        return userCommand.equals(Command.BYE);
+    }
+
+    public void clearInput() {
+        // Force Scanner to jump to next line
+        inputReader.nextLine();
     }
 }
