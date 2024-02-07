9f26f1c34c88408e2815f4910464dea4fea722ad
nkotaa <e0957774@u.nus.edu>
2024-02-07 22:27:07 +0800

    Improve Program Robustness and Code Readability

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,38 +1,60 @@
 import java.util.Scanner;
 
 public class Duke {
-    private static void handleLine(String line, TaskManager tmMaster) {
+    private static final String WELCOME_MESSAGE = "Greetings! I'm Blue, your personal chatbot assistant. How may I be of service?";
+    private static final String GOODBYE_MESSAGE = "Till we meet again.";
+    public static void talk(String line) {
+        // no formatting for now
+        System.out.println(line);
+    }
+    private static void warnInvalidInput() {
+        talk("I'm sorry. I'm afraid I can't do that.");
+    }
+    private static void handleLine(String line) {
+        TaskManager tmMaster = new TaskManager();
         String request = line.split(" ")[0];
         switch (request) {
         case "list":
             tmMaster.listTasks();
             break;
         case "mark":
-            tmMaster.markTask(Integer.parseInt(line.substring(5)) - 1);
+            int taskIndex = Integer.parseInt(line.substring(5)) - 1;
+            if (taskIndex < 0 || taskIndex >= tmMaster.getNumTasks()) {
+                warnInvalidInput();
+                break;
+            }
+            tmMaster.markTask(taskIndex);
             break;
         case "todo":
             tmMaster.addTask(line.substring(5));
             break;
         case "deadline":
             int byStringIndex = line.indexOf("/by");
+            if (byStringIndex < 0) {
+                warnInvalidInput();
+                break;
+            }
             tmMaster.addTask(line.substring(9, byStringIndex - 1), line.substring(byStringIndex + 4));
             break;
         case "event":
             int fromStringIndex = line.indexOf("/from");
             int toStringIndex = line.indexOf("/to");
+            if (fromStringIndex < 0 || toStringIndex < 0) {
+                warnInvalidInput();
+                break;
+            }
             tmMaster.addTask(line.substring(6, fromStringIndex - 1), line.substring(fromStringIndex + 6, toStringIndex - 1), line.substring(toStringIndex + 4));
             break;
         default:
-            System.out.println("Invalid action");
+            warnInvalidInput();
         }
     }
     public static void main(String[] args) {
-        System.out.println("Greetings! I'm Blue, your personal chatbot assistant. How may I be of service?");
+        talk(WELCOME_MESSAGE);
         Scanner in = new Scanner(System.in);
-        TaskManager tmMaster = new TaskManager();
         for (String line = in.nextLine(); !line.equals("bye"); line = in.nextLine()) {
-            handleLine(line, tmMaster);
+            handleLine(line);
         }
-        System.out.println("Glad to help. Bye!");
+        talk(GOODBYE_MESSAGE);
     }
 }
--- src/main/java/TaskManager.java
+++ src/main/java/TaskManager.java
@@ -1,36 +1,34 @@
-public class TaskManager {
+public class TaskManager extends Duke {
     // assume no more than 100 tasks for now
     public static final int MAX_TASKS = 100;
-    private Task[] tasks;
-    private int numTasks;
+    private static Task[] tasks = new Task[MAX_TASKS];
+    private static int numTasks = 0;
 
-    public TaskManager() {
-        tasks = new Task[MAX_TASKS];
-        numTasks = 0;
+    public int getNumTasks() {
+        return numTasks;
     }
-
     public void listTasks() {
-        for (int i = 0; i < this.numTasks; i += 1) {
-            System.out.println(Integer.toString(i + 1) + ". " + tasks[i]);
+        for (int i = 0; i < numTasks; i += 1) {
+            talk(Integer.toString(i + 1) + ". " + tasks[i]);
         }
     }
     public void markTask(int taskIndex) {
         tasks[taskIndex].setDone();
-        System.out.println("Task " + tasks[taskIndex].getDescription() + " marked as done.");
+        talk("Task " + tasks[taskIndex].getDescription() + " marked as done.");
     }
     public void addTask(String description) {
         tasks[numTasks] = new Task(description);
         numTasks += 1;
-        System.out.println("added: " + description);
+        talk("added: " + description);
     }
     public void addTask(String description, String by) {
         tasks[numTasks] = new Deadline(description, by);
         numTasks += 1;
-        System.out.println("added: " + description);
+        talk("added: " + description);
     }
     public void addTask(String description, String from, String to) {
         tasks[numTasks] = new Event(description, from, to);
         numTasks += 1;
-        System.out.println("added: " + description);
+        talk("added: " + description);
     }
 }
