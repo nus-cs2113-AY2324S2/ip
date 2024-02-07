47f57d63509dfac3be63fd63cf436e623813e5ec
R M Raajamani <>
2024-02-07 20:39:28 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,35 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        // Border line
+        Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________";
         
-        // Greeting message
-        String greeting = " Hello! I'm Orion\n What can I do for you?";
+        // Start-up message
+        System.out.println(line);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+        
+        // Command echo loop
+        while (true) {
+            String input = scanner.nextLine();
+            
+            // Check for the 'bye' command to terminate the program
+            if (input.equalsIgnoreCase("bye")) {
+                break;
+            }
+            
+            // Echo the input back to the user
+            System.out.println(line);
+            System.out.println(input);
+            System.out.println(line);
+        }
         
         // Farewell message
-        String farewell = " Bye. Hope to see you again soon!";
-        
-        // Print greeting section
-        System.out.println(line);
-        System.out.println(greeting);
+        System.out.println("Bye. Hope to see you again soon!");
         System.out.println(line);
         
-        // Print farewell section when the program is ending or the user decides to exit
-        System.out.println(farewell);
-        System.out.println(line);
+        scanner.close();
     }
 }
