3d189e2b8e7fe11ff288e0a80513a00a03a6ce1f
R M Raajamani <>
2024-02-07 20:43:45 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,6 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static final int MAX_TASKS = 100; // Maximum number of tasks
+    private static String[] tasks = new String[MAX_TASKS]; // Array to store tasks
+    private static int taskCount = 0; // Counter for the number of tasks
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = "____________________________________________________________";
@@ -11,18 +15,27 @@ public class Duke {
         System.out.println("What can I do for you?");
         System.out.println(line);
         
-        // Command echo loop
+        // Processing commands
         while (true) {
             String input = scanner.nextLine();
+            System.out.println(line);
             
-            // Check for the 'bye' command to terminate the program
             if (input.equalsIgnoreCase("bye")) {
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else {
+                if (taskCount < MAX_TASKS) {
+                    tasks[taskCount] = input;
+                    taskCount++;
+                    System.out.println("added: " + input);
+                } else {
+                    System.out.println("Task list is full!");
+                }
             }
             
-            // Echo the input back to the user
-            System.out.println(line);
-            System.out.println(input);
             System.out.println(line);
         }
         
