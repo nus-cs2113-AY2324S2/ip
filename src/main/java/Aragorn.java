0eb2de183d6635fe453fe34e1703a1da0131cfc9
vimalapugazhan <p.vimalapugazhan@gmail.com>
2024-02-07 17:01:23 +0800

    Implemented basic functionality for level 3

--- src/main/java/Aragorn.java
+++ src/main/java/Aragorn.java
@@ -1,5 +1,6 @@
+
 import java.util.Scanner;
-import java.util.Arrays;
+//import java.util.Arrays;
 
 public class Aragorn {
 
@@ -10,11 +11,10 @@ public class Aragorn {
                 "    What can I do for you?\n";
         String EXIT = "    Bye. Hope to see you again soon!\n";
         String TAB = "    ";
-        String[] list;
-        list = new String[100];
+        Task[] list = new Task[100];
         int listLength = 0;
         System.out.println(LINE + GREET + LINE);
-
+        int index;
         String echo;
         Scanner in = new Scanner(System.in);
         while(true) {
@@ -27,14 +27,27 @@ public class Aragorn {
 
             if (userInput.equals("list")) {
                 System.out.println(LINE);
+                System.out.println("Here are the tasks in your list: ");
                 for (int i = 0; i < listLength; i += 1) {
-                    System.out.println(TAB + (i + 1) + ". " + list[i]);
+                    System.out.println(TAB + "[" + list[i].getStatusIcon() + "] " + (i + 1) + ". " + list[i].getDescription());
                 }
                 System.out.println(LINE);
                 continue;
             }
 
-            list[listLength] = userInput;
+            if (userInput.contains("unmark")) {
+                index = Integer.parseInt(userInput.substring(7)) - 1;
+                list[index].markAsUndone();
+                continue;
+            }
+
+            else if (userInput.contains("mark")) {
+                index = Integer.parseInt(userInput.substring(5)) - 1;
+                list[index].markAsDone();
+                continue;
+            }
+
+            list[listLength] = new Task(userInput);
             listLength += 1;
             echo = userInput;
             System.out.println(LINE + TAB + "added: " + echo + "\n" + LINE);
