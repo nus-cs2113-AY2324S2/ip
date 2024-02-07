b46ca3d4824984ac60aca4f163c583c27fb6e3a5
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-02-07 12:54:25 +0800

    Improve Code Quality

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 
 public class Spike {
-    private static final String chatbot = "Spike";
+    private static final String CHATBOT = "Spike";
+    public static final String DIVIDER = "_________________________________________________";
+
     public static void main(String[] args) {
-        String divider = "_________________________________________________";
-        System.out.println(divider);
-        System.out.println("Hello! I'm " + chatbot);
-        System.out.println("What can I do for you?\n");
-        System.out.println(divider);
+        displayWelcomeMsg();
 
         Task[] inputList = new Task[100];
         int iter = 0;
@@ -16,47 +14,69 @@ public class Spike {
         while (true) {
             String input = in.nextLine();
             if (input.contentEquals("list")) {
-                System.out.println(divider);
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < inputList.length; i++) {
-                    Task value = inputList[i];
-                    if (value == null) {
-                        break;
-                    }
-                    System.out.println((i + 1) + ".[" + value.getStatusIcon() + "] " + value.description);
-                }
-                System.out.println(divider);
-            }
-            else if (input.startsWith("mark")) {
+                displayList(inputList);
+            } else if (input.startsWith("mark")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 inputList[index].setDone(true);
-                System.out.println(divider);
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println((index + 1) + ".[X] " + inputList[index].description);
-                System.out.println(divider);
-            }
-            else if (input.startsWith("unmark")) {
+                displayMarkMsg(index, inputList);
+            } else if (input.startsWith("unmark")) {
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 inputList[index].setDone(false);
-                System.out.println(divider);
-                System.out.println("OK, I've marked this task as not done yet:");
-                System.out.println((index + 1) + ".[ ] " + inputList[index].description);
-                System.out.println(divider);
-            }
-            else if (input.contentEquals("bye")) {
+                displayUnmarkMsg(index, inputList);
+            } else if (input.contentEquals("bye")) {
                 break;
-            }
-            else {
-                System.out.println(divider);
-                System.out.println("added: " + input);
-                System.out.println(divider);
+            } else {
+                displayAddedMsg(input);
                 inputList[iter] = new Task(input);
             }
             iter += 1;
         }
+        displayByeMsg();
+    }
 
-        System.out.println(divider);
+    private static void displayAddedMsg(String input) {
+        System.out.println(DIVIDER);
+        System.out.println("added: " + input);
+        System.out.println(DIVIDER);
+    }
+
+    private static void displayList(Task[] inputList) {
+        System.out.println(DIVIDER);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < inputList.length; i++) {
+            Task value = inputList[i];
+            if (value == null) {
+                break;
+            }
+            System.out.println((i + 1) + ".[" + value.getStatusIcon() + "] " + value.description);
+        }
+        System.out.println(DIVIDER);
+    }
+
+    private static void displayUnmarkMsg(int index, Task[] inputList) {
+        System.out.println(DIVIDER);
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println((index + 1) + ".[ ] " + inputList[index].description);
+        System.out.println(DIVIDER);
+    }
+
+    private static void displayMarkMsg(int index, Task[] inputList) {
+        System.out.println(DIVIDER);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println((index + 1) + ".[X] " + inputList[index].description);
+        System.out.println(DIVIDER);
+    }
+
+    private static void displayByeMsg() {
+        System.out.println(DIVIDER);
         System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(divider);
+        System.out.println(DIVIDER);
+    }
+
+    private static void displayWelcomeMsg() {
+        System.out.println(DIVIDER);
+        System.out.println("Hello! I'm " + CHATBOT);
+        System.out.println("What can I do for you?\n");
+        System.out.println(DIVIDER);
     }
 }
