39e141db888087dce1147c401815dfba02dcabab
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-02-07 18:30:39 +0800

    Add sub-task functionality

--- src/main/java/Spike.java
+++ src/main/java/Spike.java
@@ -3,18 +3,21 @@ import java.util.Scanner;
 public class Spike {
     private static final String CHATBOT = "Spike";
     public static final String DIVIDER = "_________________________________________________";
+    public static final int MAX_TASK = 100;
 
     public static void main(String[] args) {
         displayWelcomeMsg();
 
-        Task[] inputList = new Task[100];
+        Task[] inputList = new Task[MAX_TASK];
         int iter = 0;
+        int taskCount = 0;
         Scanner in = new Scanner(System.in);
 
         while (true) {
             String input = in.nextLine();
             if (input.contentEquals("list")) {
                 displayList(inputList);
+                iter -= 1;
             } else if (input.startsWith("mark")) {
                 int index = Integer.parseInt(input.substring(5)) - 1;
                 inputList[index].setDone(true);
@@ -23,15 +26,50 @@ public class Spike {
                 int index = Integer.parseInt(input.substring(7)) - 1;
                 inputList[index].setDone(false);
                 displayUnmarkMsg(index, inputList);
+            } else if (input.startsWith("todo")) {
+                inputList[iter] = new Todo(processTodo(input));
+                displayAcknowledgement(inputList, iter);
+            } else if (input.startsWith("deadline")) {
+                inputList[iter] = new Deadline(processDeadline(input));
+                displayAcknowledgement(inputList, iter);
+            } else if (input.startsWith("event")) {
+                inputList[iter] = new Event(processEvent(input));
+                displayAcknowledgement(inputList, iter);
             } else if (input.contentEquals("bye")) {
                 break;
             } else {
-                displayAddedMsg(input);
-                inputList[iter] = new Task(input);
+                System.out.println("Not valid");
+                iter -= 1;
             }
             iter += 1;
         }
         displayByeMsg();
+    }
+
+    private static void displayAcknowledgement(Task[] inputList, int iter) {
+        char Badge = getBadge(inputList[iter]);
+        System.out.println(DIVIDER);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(" [" + Badge + "]" + "[ ] " + inputList[iter].description);
+        System.out.println("Now you have " + (iter + 1) + " tasks in the list.");
+        System.out.println(DIVIDER);
+    }
+
+    private static String processEvent(String input) {
+        String event = input.substring(6);
+        String[] parts = event.split(" /from ");
+        String[] time = parts[1].split(" /to ");
+        return parts[0] + " (from: " + time[0] + " to: " + time[1] + ")";
+    }
+
+    private static String processDeadline(String input) {
+        String deadline = input.substring(9);
+        String[] parts = deadline.split(" /by ");
+        return parts[0] + " (by: " + parts[1] + ")";
+    }
+
+    private static String processTodo(String input) {
+        return input.substring(5);
     }
 
     private static void displayAddedMsg(String input) {
@@ -48,22 +86,37 @@ public class Spike {
             if (value == null) {
                 break;
             }
-            System.out.println((i + 1) + ".[" + value.getStatusIcon() + "] " + value.description);
+            char Badge = getBadge(value);
+            System.out.println((i + 1) + ".[" + Badge + "]" + "[" + value.getStatusIcon() + "] " + value.description);
         }
         System.out.println(DIVIDER);
     }
 
+    private static char getBadge(Task value) {
+        char Badge = ' ';
+        if (value instanceof Todo) {
+            Badge = ((Todo) value).getBadge();
+        } else if (value instanceof Deadline) {
+            Badge = ((Deadline) value).getBadge();
+        } else if (value instanceof Event) {
+            Badge = ((Event) value).getBadge();
+        }
+        return Badge;
+    }
+
     private static void displayUnmarkMsg(int index, Task[] inputList) {
+        char Badge = getBadge(inputList[index]);
         System.out.println(DIVIDER);
         System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println((index + 1) + ".[ ] " + inputList[index].description);
+        System.out.println((index + 1) + ".[" + Badge + "]" + "[ ] " + inputList[index].description);
         System.out.println(DIVIDER);
     }
 
     private static void displayMarkMsg(int index, Task[] inputList) {
+        char Badge = getBadge(inputList[index]);
         System.out.println(DIVIDER);
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println((index + 1) + ".[X] " + inputList[index].description);
+        System.out.println((index + 1) + ".[" + Badge + "]" + "[X] " + inputList[index].description);
         System.out.println(DIVIDER);
     }
 
