6d935abd8563d56f9666393212f5ecb4fbc4f2a1
XavierLiau34 <xavyliau@gmail.com>
2024-02-07 22:14:49 +0800

    Level 3. Mark as Done

--- src/main/java/Dul.java
+++ src/main/java/Dul.java
@@ -1,7 +1,8 @@
 import java.util.Scanner;
+
 public class Dul {
-    public static final int maxtasks = 100;
-    public static String[] tasks = new String[maxtasks];
+    public static final int maximumTASKS = 100;
+    public static Task[] tasks = new Task[maximumTASKS];
     public static int taskCount = 0;
 
     public static void main(String[] args) {
@@ -16,30 +17,82 @@ public class Dul {
         Scanner in = new Scanner(System.in);
         while (!guyInput.equals("bye")) {
             guyInput = in.nextLine();
-            processInput(guyInput);
+            listInput(guyInput);
         }
         System.out.println("Bye. Hope to see you again soon!");
+        in.close();
     }
 
-    public static void processInput(String input) {
-        if (input.equals("list")) {
-            listTasks();
-        } else if (!input.equals("bye")) {
-            addTask(input);
+    public static void listInput(String input) {
+        String[] command = input.split(" ");
+        switch (command[0]) {
+            case "list":
+                listTasks();
+                break;
+            case "mark":
+                markTaskAsDone(Integer.parseInt(command[1]) - 1);
+                break;
+            case "unmark":
+                markTaskAsNotDone(Integer.parseInt(command[1]) - 1);
+                break;
+            case "bye":
+                break;
+            default:
+                addTask(input);
+                break;
         }
     }
 
     public static void listTasks() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < taskCount; i++) {
-            System.out.println((i + 1) + ". " + tasks[i]);
+            System.out.println((i + 1) + "." + tasks[i].toString());
         }
     }
 
-    public static void addTask(String task) {
-        tasks[taskCount] = task;
-        taskCount++;
-        System.out.println("added: " + task);
+    public static void addTask(String taskDescription) {
+            tasks[taskCount] = new Task(taskDescription);
+            taskCount++;
+            System.out.println("added: " + taskDescription);
+    }
+
+    public static void markTaskAsDone(int index) {
+        tasks[index].markAsDone();
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println("  " + tasks[index].toString());
+    }
+
+    public static void markTaskAsNotDone(int index) {
+        tasks[index].markAsNotDone();
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println("  " + tasks[index].toString());
     }
 }
 
+class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    @Override
+    public String toString() {
+        return getStatusIcon() + " " + description;
+    }
+}
 
