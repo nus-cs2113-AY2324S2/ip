8c29b77a76419b37f33b84262965d5ba702e1b98
Fureimi <e0957998@u.nus.edu>
2024-02-07 22:53:39 +0800

    Level-2

--- src/main/java/GermaBot.java
+++ src/main/java/GermaBot.java
@@ -7,6 +7,8 @@ public class GermaBot {
                 + "What may I do for you this fine day? \n"
                 + "____________________";
         System.out.println(WelcomeMessage);
+        String[] toDoList = new String[100];
+        int counter = 0;
         while (true) {
             String echo;
             Scanner in = new Scanner(System.in);
@@ -14,11 +16,25 @@ public class GermaBot {
             if (echo.equals("bye")) {
                 break;
             }
-            System.out.println(echo);
+            if (echo.equals("list")) {
+                int i = 1;
+                for (String s : toDoList) {
+                    if (s == null) {
+                        break;
+                    }
+                    System.out.println(i + ". " + s);
+                    i++;
+                }
+            }
+            else {
+                toDoList[counter] = echo;
+                System.out.println("Added: " + echo + " to the list!");
+                counter += 1;
+            }
         }
         String GoodbyeMessage = "____________________ \n"
                 + "Thanks for using me! Hope you again soon~! \n"
                 + "____________________";
         System.out.println(GoodbyeMessage);
     }
-}
+    }
