b04698741e014d56b11b164a375380b3b94a8be5
YHWong20 <yihao.wong@u.nus.edu>
2024-02-07 12:08:11 +0800

    Add Deadline class

---
+++ src/main/java/Deadline.java
@@ -0,0 +1,25 @@
+public class Deadline extends Task {
+    private final String dueDate;
+
+    private Deadline(String taskName, int taskId, boolean isCompleted, String dueDate) {
+        super(taskName, taskId, isCompleted);
+        this.dueDate = dueDate;
+    }
+    public Deadline(String taskName, int taskId, String dueDate) {
+        super(taskName, taskId, false);
+        this.dueDate = dueDate;
+    }
+
+    public Task completeTask() {
+        return new Deadline(this.taskName, this.taskId, true, this.dueDate);
+    }
+
+    public Task uncompleteTask() {
+        return new Deadline(this.taskName, this.taskId, false, this.dueDate);
+    }
+
+    @Override
+    public String toString() {
+        return "[D]" + super.toString() + String.format(" (by: %s)", this.dueDate);
+    }
+}
