35ec609da73aa0b0b920aba737871704d1ace3f1
hongyijie06 <hongyijie06@gmail.com>
2024-02-07 15:26:12 +0800

    add Add,list

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,11 +2,12 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        String list[] = new String[100];
 
         System.out.println("Hello! I'm apple");
         System.out.println("What can I do for you?");
 
-       
+       int index = 0;
 
         String line = "";
 
@@ -15,13 +16,27 @@ public class Duke {
         while (!line.equals("bye")){
             Scanner input = new Scanner(System.in);
             line = input.nextLine();
-            
 
             if (line.equals("bye")){
                 break;
             }
 
-            System.out.println(line);
+            if (line.equals("list")){
+                for (int i = 0; i < index; i ++) {
+
+                    System.out.println((i + 1) + ". " + list[i]);
+                }
+            }else {
+                list[index] = line;
+                System.out.println("added: " + line);
+            }
+
+            index ++;
+            
+
+
+
+
         }
         System.out.println("Bye. Hope to see you again soon!");
         
