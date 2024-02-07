3878c8d5db08b7ef755b4ee4168bca43a39bdc87
ChenKangg <cckcyc@gmail.com>
2024-02-07 20:54:33 +0800

    Update classes

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,72 +1,5 @@
 import java.util.Scanner;
 
-class Task {
-    protected String description;
-    protected boolean isDone;
-
-    public Task(String description) {
-        this.description = description;
-        this.isDone = false;
-    }
-
-    public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]");
-    }
-
-    public void markAsDone() {
-        this.isDone = true;
-    }
-
-    public void markAsNotDone() {
-        this.isDone = false;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-}
-
-class ToDo extends Task {
-    public ToDo(String description) {
-        super(description);
-    }
-
-    @Override
-    public String toString() {
-        return "[T]" + super.getStatusIcon() + " " + super.getDescription();
-    }
-}
-
-class Deadline extends Task {
-    protected String by;
-
-    public Deadline(String description, String by) {
-        super(description);
-        this.by = by;
-    }
-
-    @Override
-    public String toString() {
-        return "[D]" + super.getStatusIcon() + " " + super.getDescription() + " (by: " + by + ")";
-    }
-}
-
-class Event extends Task {
-    protected String from;
-    protected String to;
-
-    public Event(String description, String from, String to) {
-        super(description);
-        this.from = from;
-        this.to = to;
-    }
-
-    @Override
-    public String toString() {
-        return "[E]" + super.getStatusIcon() + " " + super.getDescription() + " (from: " + from + " to: " + to + ")";
-    }
-}
-
 public class Duke {
     public static void main(String[] args) {
         //Display initial greeting message
@@ -150,7 +83,7 @@ public class Duke {
                 System.out.println("Now you have " + taskCount + " tasks in the list.");
 
             } else {
-                //Handles the case if invalid input 
+                //Handles the case if invalid input
                 System.out.println("Invalid command.");
             }
             System.out.println("____________________________________________________________");
