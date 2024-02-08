801b310335a40bdd90f054b6876ed32cb39dc1d1
Andy123qq4 <1394568964@qq.com>
2024-02-08 21:36:34 +0800

    Level 2. Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,9 +1,13 @@
 import java.util.Scanner;
 public class Duke {
     private String name;
+    private String[] tasks;
+    private int taskCount;
 
     public Duke(String name) {
         this.name = name;
+        this.tasks = new String[100];
+        this.taskCount = 0;
     }
 
     public void greet() {
@@ -12,15 +16,41 @@ public class Duke {
         System.out.println("What can I do for you?");
 //        System.out.println("=========================");
     }
+
+    public void addTask(String task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        System.out.println("added: " + task);
+    }
+
+    public void listTasks() {
+        if (taskCount == 0) {
+            System.out.println("No tasks added yet.");
+        } else {
+            System.out.println("Here are the tasks in your list:");
+            for (int i = 0; i < taskCount; i++) {
+                System.out.println((i + 1) + ". " + tasks[i]);
+            }
+        }
+    }
+
     public void echoCommands() {
         Scanner scanner = new Scanner(System.in);
         String command;
 
         do {
             command = scanner.nextLine();
-            System.out.println("=========================");
-            System.out.println(command);
-            System.out.println("=========================");
+            // Level 1
+//            System.out.println("=========================");
+//            System.out.println(command);
+//            System.out.println("=========================");
+
+            // Level 2
+            if (command.equals("list")) {
+                listTasks();
+            } else if (!command.equals("bye")) {
+                addTask(command);
+            }
         } while (!command.equals("bye"));
 
         scanner.close();
