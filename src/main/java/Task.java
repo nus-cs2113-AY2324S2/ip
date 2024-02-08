a479ac03026d0f363c4b236e12c7acedacd3c80a
fungg0 <fungfunwenwu@gmail.com>
2024-02-09 02:50:59 +0800

    Add todo, deadline, event classes.

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,14 +1,21 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    private static int numberOfTasks = 0;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
+        numberOfTasks +=1;
     }
 
     public String getStatusIcon() {
         return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    public String toString() {
+        return this.getStatusIcon() +
+                " " + this.getDescription();
     }
 
     public void markDone() {
