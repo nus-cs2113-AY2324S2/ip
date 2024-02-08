245a800664814af5ef3178a2cde13f17f0dea179
FeathersRe <e0969003@u.nus.edu>
2024-02-08 12:26:02 +0800

    Level-1 Commits

--- src/main/java/Daisy.java
+++ src/main/java/Daisy.java
@@ -1,12 +1,25 @@
+import java.util.Scanner;
+
 public class Daisy {
     public static void main(String[] args) {
-        String INTRO_PROMPT = "Hello! I'm Daisy.\nWhat can I do for you?";
-        String EXIT_PROMPT = "Bye. Hope to see you again soon!";
-        String LINE_BREAK = "\n____________________________________";
+        String INTRO_PROMPT = "Good day! This is Daisy.\nAny task for today?";
+        String EXIT_PROMPT = "Ending prompt received. Remember to keep to the deadlines!";
+        String LINE_BREAK = "____________________________________";
 
         System.out.println(LINE_BREAK);
         System.out.println(INTRO_PROMPT);
         System.out.println(LINE_BREAK);
+
+        String command = "";
+        Scanner in = new Scanner(System.in);
+
+        while(!command.equals("bye")) {
+            command = in.nextLine();
+            System.out.println(LINE_BREAK);
+            System.out.println(command);
+            System.out.println(LINE_BREAK);
+        }
+
         System.out.println(EXIT_PROMPT);
         System.out.println(LINE_BREAK);
     }
