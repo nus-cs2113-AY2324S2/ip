db873197cb4cf7f746aad31096a7494d52b0dd41
Shu <e0957502@u.nus.edu>
2024-02-09 02:34:02 +0800

    Update method to support the tracking of three types of tasks, Implement Level 4

--- src/main/java/RoleyPoley.java
+++ src/main/java/RoleyPoley.java
@@ -9,12 +9,19 @@ public class RoleyPoley {
         echo(taskList);
     }
 
+    private static void printReply(Task[] taskList, int counter) {
+        System.out.println("\t Got it. I've added this task:");
+        System.out.println("\t  [" + taskList[counter].getTaskTypeIcon() + "][ ]" + taskList[counter].getDescription());
+        System.out.println("\t Now you have " + counter + " tasks in the list.");
+    }
+
     public static void displayList(Task[] taskList) {
-        for ( int i = 1; i < taskList.length; i++) {
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 1; i < taskList.length; i++) {
             if (taskList[i] == null) {
                 break;
             }
-            System.out.println("\t" + i + ".[" + taskList[i].getStatusIcon() + "]" + taskList[i].description);
+            System.out.println("\t" + i + ".[" + taskList[i].getTaskTypeIcon() + "][" + taskList[i].getStatusIcon() + "]" + taskList[i].getDescription());
         }
     }
 
@@ -32,61 +39,89 @@ public class RoleyPoley {
 
     public static void echo(Task[] taskList) {
         String line;
-
         int counter = 1;
         while (true) {
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
-            if (line.equals("bye")) {
+            String[] splitString = line.split(" ");
+            if (splitString[0].equals("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 createLine();
                 break;
             } else if (line.equals("list")) {
                 displayList(taskList);
                 createLine();
-            } else if (line.startsWith("mark")) {
+            } else if (splitString[0].equals("mark")) {
                 String[] words = line.split(" ");
                 if (words.length == 2) {
-                    try {
-                        int taskNum = Integer.parseInt(words[1]);
-                        if (taskList[taskNum] == null) {
-                            System.out.println("Error! Out of Bounds!");
-                        } else {
-                            taskList[taskNum].markAsDone();
-                        }
-                        createLine();
-                    } catch (NumberFormatException e) {
-                        System.out.println("\tadded : " + line);
-                        createLine();
-                        taskList[counter] = new Task(line);
-                        counter++;
+                    int taskNum = Integer.parseInt(words[1]);
+                    if (taskList[taskNum] == null) {
+                        System.out.println("Error! Task " + taskNum + " cannot be found. Enter command list to view task list.");
+                    } else {
+                        taskList[taskNum].markAsDone();
                     }
+                    createLine();
                 }
-            } else if (line.startsWith("unmark")) {
+            } else if (splitString[0].equals("unmark")) {
                 String[] words = line.split(" ");
                 if (words.length == 2) {
-                    try {
-                        int taskNum = Integer.parseInt(words[1]);
-                        if (taskList[taskNum] == null) {
-                            System.out.println("Error! Out of Bounds!");
-                        } else {
-                            taskList[taskNum].markAsUndone();
-                        }
+                    int taskNum = Integer.parseInt(words[1]);
+                    if (taskList[taskNum] == null) {
+                        System.out.println("Error! Task " + taskNum + " cannot be found. Enter command list to view task list.");
+                    } else {
+                        taskList[taskNum].markAsUndone();
+                    }
+                    createLine();
+                }
+            } else if (splitString[0].equals("todo")) {
+                String description = line.substring("todo".length());
+                taskList[counter] = new Todo(description);
+                printReply(taskList, counter);
+                createLine();
+                counter++;
+            } else if (splitString[0].equals("deadline")) {
+                String[] words = line.split("/by");
+                if (words.length == 1) {
+                    System.out.println("Invalid entry. Please enter input in the following format:");
+                    System.out.println("deadline <Task Description> /by <Due Date>");
+                } else {
+                    String description = words[0].substring("deadline".length());
+                    String dueDate = words[1];
+                    taskList[counter] = new Deadline(description, dueDate);
+                    printReply(taskList, counter);
+                    createLine();
+                    counter++;
+                }
+            } else if (splitString[0].equals("event")) {
+                String[] words = line.split("/from");
+                if (words.length == 1) {
+                    System.out.println("Invalid entry. Please enter input in the following format:");
+                    System.out.println("event <Task Description> /from <Start Time> /to <End Time");
+                } else {
+                    String description = words[0].substring("event".length());
+                    int indexOfEndTime = words[1].indexOf("/to");
+                    if (indexOfEndTime == -1) {
+                        System.out.println("Invalid entry. Please enter input in the following format:");
+                        System.out.println("event <Task Description> /from <Start Time> /to <End Time");
+                    } else {
+                        String startTime = words[1].substring(0, indexOfEndTime - 1);
+                        String endTime = words[1].substring(indexOfEndTime + "/to".length());
+                        taskList[counter] = new Event(description, startTime, endTime);
+                        printReply(taskList, counter);
                         createLine();
-                    } catch (NumberFormatException e) {
-                        System.out.println("\tadded : " + line);
-                        createLine();
-                        taskList[counter] = new Task(line);
                         counter++;
                     }
                 }
             } else {
-                System.out.println("\tadded : " + line);
+                taskList[counter] = new Todo(" " + line);
+                System.out.println("Task type not defined. Task is automatically assigned to todo task type.");
+                printReply(taskList, counter);
                 createLine();
-                taskList[counter] = new Task(line);
                 counter++;
             }
         }
     }
 }
+
+
 
