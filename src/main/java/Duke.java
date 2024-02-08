19f083fa5d6f1ed7e8926bdda56681dfe8d41c53
Geinzit <heinz03huang@gmail.com>
2024-02-08 13:23:28 +0800

    Add feature to store past commands and list stored commands

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,21 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 public class Duke {
+    private static List<String> commandHistory = new ArrayList<>();
+
+    public static void storeCommand(String command) {
+        commandHistory.add(command);
+    }
+
+    public static void printHistory() {
+        int cnt = 0;
+        for (String command: commandHistory) {
+            cnt += 1;
+            System.out.printf(cnt + ". " + command + "%n");
+        }
+    }
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
@@ -9,12 +25,15 @@ public class Duke {
             System.out.println("-------------------------");
             String userInput = scanner.nextLine();
 
-            if(userInput.equalsIgnoreCase("bye")) {
+            if (userInput.equalsIgnoreCase("bye")) {
                 System.out.println("Bye! See ya!");
                 break;
+            } else if (userInput.equalsIgnoreCase("list")) {
+                printHistory();
+            } else {
+                storeCommand(userInput);
+                System.out.println("added: " + userInput);
             }
-
-            System.out.println(userInput);
         }
     }
 }
