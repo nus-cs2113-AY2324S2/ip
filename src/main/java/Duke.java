da5bbbb46ceaf9020a364645ac8915c35a55a1a4
liuzehui03 <liuzehui03@gmail.com>
2024-02-08 12:23:39 +0800

    added level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,41 @@
-public class Duke {
-    public static void main(String[] args) {
-        /* String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-         */
+import java.util.Scanner;
+//so scanner class can be used
 
+public class Duke {
+    public static void printLine() {
+        System.out.println("________________________________________");
+    }
+    public static void printShortLine() {
+        System.out.println("_____________");
+    }
+
+    public static void printHello() {
+        String logo =
+                "__    __   ___    ____ \n"
+                        + "\\ \\ / /  / _ \\  | ___ |\n"
+                        + " \\ Y /  | | | |     | | \n"
+                        + "  \\ /   | | | |     | | \n"
+                        + "  | |   | |_| |  ___| | \n"
+                        + "  |_|    \\___/  |____/          \n";
+        printLine();
+        System.out.println("Hello from\n" + logo);
         System.out.println("Hello! I'm YOJ");
         System.out.println("What can I do for you?");
+        printLine();
+    }
+    public static void main(String[] args) {
+        printHello();
+        // get user input
+        Scanner in = new Scanner(System.in);
+        String userInput;
+        userInput = in.nextLine();
+
+        while(!userInput.equals("bye")) {
+            printShortLine();
+            System.out.println(userInput);
+            printLine();
+            userInput = in.nextLine();
+        }
         System.out.println("Bye. Hope to see you again soon!");
     }
 }
