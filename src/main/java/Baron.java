796705cefc3bb07c0c4ab45b227b00577f694a5d
Joshuahoky <joshuaho2002@gmail.com>
2024-02-08 23:04:45 +0800

    Refactor code in Baron class and edit code to comply with coding standards

--- src/main/java/Baron.java
+++ src/main/java/Baron.java
@@ -15,28 +15,34 @@ public class Baron {
     }
 
     public static void main(String[] args) {
-        Task[] tasks = new Task[100];
-        int numberOfTasks = 0;
-        System.out.println("Hello! I'm Baron");
-        System.out.println("What can I do for you?\n");
-        getUserInput(tasks, numberOfTasks);
+        greetUser();
+        getUserInput();
+        printExitMessage();
     }
 
-    private static void getUserInput(Task[] tasks, int numberOfTasks) {
+    private static void greetUser() {
+        System.out.println("Hello! I'm Baron");
+        System.out.println("What can I do for you?\n");
+    }
+
+    private static void printExitMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void getUserInput() {
+        Task[] tasks = new Task[100];
+        int numberOfTasks = 0;
         while (true) {
             Scanner userInput = new Scanner(System.in);
             String input = userInput.nextLine();
             input = input.toLowerCase();
             String[] inputArray = input.split(" ", 2);
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
                 break;
-            }
-            else if (input.equals("list")) {
+            } else if (input.equals("list")) {
                 displayCommandsList(tasks);
                 continue;
-            }
-            else if (input.startsWith("todo")) {
+            } else if (input.startsWith("todo")) {
                 tasks[numberOfTasks] = new ToDo(inputArray[1]);
                 numberOfTasks += 1;
                 System.out.println("\tGot it. I've added this task:");
@@ -44,8 +50,7 @@ public class Baron {
                 System.out.println("\tNow you have " + numberOfTasks
                         + (numberOfTasks == 1 ? " task" :" tasks") +" in the list.\n");
                 continue;
-            }
-            else if (input.startsWith("deadline")) {
+            } else if (input.startsWith("deadline")) {
                 int dividerPosition = inputArray[1].indexOf('/');
                 String description = inputArray[1].substring(0, dividerPosition);
                 String deadline = inputArray[1].substring(dividerPosition + 4);
@@ -56,8 +61,7 @@ public class Baron {
                 System.out.println("\tNow you have " + numberOfTasks +
                         (numberOfTasks == 1 ? " task" :" tasks") +" in the list.\n");
                 continue;
-            }
-            else if (input.startsWith("event")) {
+            } else if (input.startsWith("event")) {
                 int firstDividerPosition = inputArray[1].indexOf('/');
                 int secondDividerPosition = inputArray[1].indexOf("to");
                 String description = inputArray[1].substring(0, firstDividerPosition);
@@ -70,20 +74,17 @@ public class Baron {
                 System.out.println("\tNow you have " + numberOfTasks +
                         (numberOfTasks == 1 ? " task" :" tasks") +" in the list.\n");
                 continue;
-            }
-            else if (input.startsWith("mark")) {
+            } else if (input.startsWith("mark")) {
                 String taskNumber = inputArray[1];
                 int commandIndex = Integer.parseInt(taskNumber);
                 tasks[commandIndex - 1].markAsDone(numberOfTasks);
                 continue;
-            }
-            else if (input.startsWith("unmark")) {
+            } else if (input.startsWith("unmark")) {
                 String taskNumber = inputArray[1];
                 int commandIndex = Integer.parseInt(taskNumber);
                 tasks[commandIndex - 1].unmarkAsDone(numberOfTasks);
                 continue;
-            }
-            else {
+            } else {
                 System.out.println("Sorry but that is not a valid command. Please enter a valid command");
             }
         }
