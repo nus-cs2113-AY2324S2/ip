4bd5dfaf1d07e88c4c48ed716e63bfeb750ca3d7
chenhowy <>
2024-02-09 03:14:04 +0800

    Revert "Revert "Chris Intro""
    This reverts commit fc5a68e9e00e67f1e34c5668d667e753aa91a245.

---
+++ src/main/java/Task.java
@@ -0,0 +1,21 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markTask() {
+        isDone = !isDone;
+    }
+}
