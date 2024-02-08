0b44030707bd46a7e991fbcd809c686fe643ebed
luozihui2003 <e0969601@u.nus.edu>
2024-02-08 16:23:17 +0800

    Level 3. Mark as Done

--- src/main/java/Loopy.java
+++ src/main/java/Loopy.java
@@ -1,8 +1,8 @@
-import java.util.Scanner;  // Import the Scanner class
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Loopy {
-    private static ArrayList<String> tasks = new ArrayList<>();
+    private static ArrayList<Task> tasks = new ArrayList<>(); // Create task list array
 
     public static void main(String[] args) {
         String logo = " __                \n"
@@ -15,45 +15,11 @@ public class Loopy {
 
         Scanner taskScanner = new Scanner(System.in);  // Create a Scanner object
 
-
-        //create taskList array
-
         while (true) {
             String task = taskScanner.nextLine();
             processTask(task);
             if (task.equals("bye")) {
                 System.out.println("Bye! Hope to see you again.\n");
-                System.out.println(".                       \n" +
-                        "                                 --                      \n" +
-                        "                                 =..                     \n" +
-                        "                                .=.::                    \n" +
-                        "                                :=:.                     \n" +
-                        "                 .::.. ....::::::::..                    \n" +
-                        "                .-+++-:::::::::::::::::..                \n" +
-                        "                 :+=-:::::----------::::::....           \n" +
-                        "                 .------------------------:=++-.         \n" +
-                        "                .---------------------------=+=.         \n" +
-                        "                :---------===-------=-------=:.          \n" +
-                        "               .---------===================-            \n" +
-                        "              .--------=#%#*+===============-            \n" +
-                        "             .----------*%%+======++===----.           \n" +
-                        "             :=------------------=+@@#=------.           \n" +
-                        "             -======---==-=%@@@%*---==-------.           \n" +
-                        "             -+===========*@@@%*##====-------.           \n" +
-                        "             .+++++++++++++#%@@@#++==========.           \n" +
-                        "              :+++++++*********+++=========.           \n" +
-                        "               .=********##=-=*#**++++++++++:            \n" +
-                        "                  -+#******++******++++++:             \n" +
-                        "            .:-=-.:**###****************+.               \n" +
-                        "          .=++***=++****#########+=--:..                 \n" +
-                        "         .=+****===+++++*********+:                      \n" +
-                        "         .++***===+*+++++++++++++++-.                    \n" +
-                        "          =+++-===+*++=====----==+*+=.                   \n" +
-                        "          .++---=+*+===-:::::::---++:                  \n" +
-                        "           -----=+*++=::::::::::-:=*+++=.                \n" +
-                        "            :-=-##*+=-::::::::::::-+++=.               \n" +
-                        "            :%#*#+=--:::::::::::-.:=*%#+               \n" +
-                        "             -++++++----:::::::::--.  .:-.");
                 break;
             }
         }
@@ -62,34 +28,37 @@ public class Loopy {
     private static void processTask(String task) {
         if (task.equals("list")) {
             displayTaskList();
-        } /*else if (task.startsWith("mark ")) {
+        } else if (task.startsWith("mark ")) {
             markTaskAsDone(task);
         } else if (task.startsWith("unmark ")) {
             markTaskAsUndone(task);
-        }*/ else {
-            addTask(task);
+        } else {
+            addTask(new Task(task)); // Create Task object and add it to the list
         }
     }
 
-    private static void addTask(String task) {
-        if (!task.equals("bye")) {
+    private static void addTask(Task task) {
+        if (!task.getDescription().equals("bye")) {
             tasks.add(task);
-            System.out.println("added: " + task);
+            System.out.println("added: " + task.getDescription());
         }
     }
-
+//NOT DISPLAYING LIST PROPERLY
     private static void displayTaskList() {
+        System.out.println("Here are the tasks in your list:");
         for (int i = 0; i < tasks.size(); i++) {
-            System.out.println((i + 1) + ". " + tasks.get(i));
+            Task currentTask = tasks.get(i);
+            System.out.println((i + 1) + ". " + currentTask);
         }
     }
 
-   /* private static void markTaskAsDone(String task) {
+    private static void markTaskAsDone(String task) {
         int taskIndex = Integer.parseInt(task.substring(5)) - 1;
         if (taskIndex >= 0 && taskIndex < tasks.size()) {
-            tasks.get(taskIndex).markAsDone();
+            Task currentTask = tasks.get(taskIndex);
+            currentTask.markAsDone();
             System.out.println("Nice! I've marked this task as done:");
-            System.out.println("  " + tasks.get(taskIndex).toString());
+            System.out.println(" " + currentTask);
         } else {
             System.out.println("Invalid task index. Please provide a valid index.");
         }
@@ -98,36 +67,42 @@ public class Loopy {
     private static void markTaskAsUndone(String task) {
         int taskIndex = Integer.parseInt(task.substring(7)) - 1;
         if (taskIndex >= 0 && taskIndex < tasks.size()) {
-            tasks.get(taskIndex).markAsNotDone();
+            Task currentTask = tasks.get(taskIndex);
+            currentTask.markAsNotDone();
             System.out.println("OK, I've marked this task as not done yet:");
-            System.out.println("  " + tasks.get(taskIndex).toString());
+            System.out.println(" " + currentTask);
         } else {
             System.out.println("Invalid task index. Please provide a valid index.");
         }
     }
-
-    static class Task {
-        private String description;
-        private boolean isDone;
-
-        public Task(String description) {
-            this.description = description;
-            this.isDone = false;
-        }
-
-        public void markAsDone() {
-            this.isDone = true;
-        }
-
-        public void markAsNotDone() {
-            this.isDone = false;
-        }
-
-        @Override
-        public String toString() {
-            return "[" + (isDone ? "X" : " ") + "] " + description;
-        }
-    }*/
 }
 
+class Task {
+    private String description;
+    private boolean isDone;
 
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone = false;
+    }
+
+    public boolean isDone() {
+        return isDone;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    @Override
+    public String toString() {
+        return "[" + (isDone ? "X" : " ") + "] " + description;
+    }
+}
