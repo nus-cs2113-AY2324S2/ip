84ff5ede14911e4a99b7c348412e0bd70cf9bc18
Andy123qq4 <tsugumi@Tsugumis-MacBook-Air.local>
2024-02-08 20:47:57 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Duke {
     private String name;
 
@@ -11,9 +12,21 @@ public class Duke {
         System.out.println("What can I do for you?");
 //        System.out.println("=========================");
     }
+    public void echoCommands() {
+        Scanner scanner = new Scanner(System.in);
+        String command;
 
+        do {
+            command = scanner.nextLine();
+            System.out.println("=========================");
+            System.out.println(command);
+            System.out.println("=========================");
+        } while (!command.equals("bye"));
+
+        scanner.close();
+    }
     public void exit() {
-        System.out.println("=========================");
+//        System.out.println("=========================");
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("=========================");
     }
@@ -26,6 +39,7 @@ public class Duke {
 //        System.out.println("Hello from\n" + logo);
         Duke chatbot = new Duke("aoliba");
         chatbot.greet();
+        chatbot.echoCommands();
         chatbot.exit();
     }
 }
