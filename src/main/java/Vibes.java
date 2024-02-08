a8854a1c151a6152290304fb91317fd674ab91d7
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-08 22:14:00 +0800

    Vibes class: Add extractCommand method

--- src/main/java/Vibes.java
+++ src/main/java/Vibes.java
@@ -1,22 +1,36 @@
 import java.util.Scanner;
 
 public class Vibes {
+    private static final String CHATBOT_NAME = "Vibes";
+    private static final String DASHED_LINE = "\t---------------------------------------------------------------------------------------";
+
+    public static String extractCommand(String userInput){
+        if (userInput.contentEquals("list")){
+            return "list";
+        } else if (userInput.contentEquals("bye")){
+            return "bye";
+        } else {
+            return userInput.substring(0,userInput.indexOf(" "));
+        }
+    }
+
     public static void main(String[] args) {
-        String name = "Vibes";
-        String line = "\t---------------------------------------------------------------------------------------";
         String userInput;
         boolean isExit = true;
         Scanner in = new Scanner(System.in);
         List taskList = new List();
 
-        System.out.println(line);
-        System.out.println("\t Hello! I'm " + name + "\n\t What can I do for you?");
-        System.out.println(line);
+        System.out.println(DASHED_LINE);
+        System.out.println("\t Hello! I'm " + CHATBOT_NAME + "\n\t What can I do for you?");
+        System.out.println(DASHED_LINE);
 
         while(isExit){
+            int taskNumber;
             userInput = in.nextLine().trim();
-            System.out.println(line);
-            switch (userInput.toLowerCase()){
+            String commandToExecute = extractCommand(userInput.toLowerCase());
+
+            System.out.println(DASHED_LINE);
+            switch (commandToExecute){
             case "bye":
                 System.out.println("\t Bye. Hope to see you again soon!");
                 isExit = false;
@@ -24,19 +38,19 @@ public class Vibes {
             case "list":
                 taskList.listTasks();
                 break;
+            case "mark":
+                taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
+                taskList.setAsDone(taskNumber);
+                break;
+            case "unmark":
+                taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
+                taskList.setAsNotDone(taskNumber);
+                break;
             default:
-                if (userInput.toLowerCase().startsWith("mark")){
-                    int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
-                    taskList.setAsDone(taskNumber);
-                } else if (userInput.toLowerCase().startsWith("unmark")) {
-                    int taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
-                    taskList.setAsNotDone(taskNumber);
-                } else {
-                    taskList.addTask(userInput);
-                }
+                taskList.addTask(userInput);
                 break;
             }
-            System.out.println(line);
+            System.out.println(DASHED_LINE);
         }
     }
 }
