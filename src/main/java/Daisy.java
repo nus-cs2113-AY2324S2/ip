c40a40cec7f91fab53858ad7bf3dc76360872ff3
FeathersRe <liaoyuzhou123@gmail.com>
2024-02-08 13:29:13 +0800

    Level-2 commits

--- src/main/java/Daisy.java
+++ src/main/java/Daisy.java
@@ -5,6 +5,8 @@ public class Daisy {
         String INTRO_PROMPT = "Good day! This is Daisy.\nAny task for today?";
         String EXIT_PROMPT = "Ending prompt received. Remember to keep to the deadlines!";
         String LINE_BREAK = "____________________________________";
+        String[] tasks = new String[100];
+        int task_no = 0;
 
         System.out.println(LINE_BREAK);
         System.out.println(INTRO_PROMPT);
@@ -13,10 +15,19 @@ public class Daisy {
         String command = "";
         Scanner in = new Scanner(System.in);
 
-        while(!command.equals("bye")) {
+        while (!command.equals("bye")) {
             command = in.nextLine();
             System.out.println(LINE_BREAK);
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i<task_no; i++) {
+                    System.out.println((i+1) + ". " + tasks[i]);
+                }
+            }
+            else {
+                tasks[task_no] = command;
+                task_no++;
+                System.out.println("added: " + command);
+            }
             System.out.println(LINE_BREAK);
         }
 
