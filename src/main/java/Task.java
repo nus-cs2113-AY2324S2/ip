0f9a833dfde4c17a4c6945f4e700c4179cc97ccf
ChinYanXu <yanxu6267@gmail.com>
2024-02-09 03:24:50 +0800

    Add Todo, Event and Deadline functionality

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,6 +1,10 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+
+    protected String startDate = "";
+    protected String endDate = "";
+    protected String taskStatus = "";
     public Task(String description) {
         this.description = description;
         this.isDone = false;
@@ -16,5 +20,37 @@ public class Task {
     }
     public void markAsUndone() {
         this.isDone = false;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+    public String getStartDate() {
+        return this.startDate;
+    }
+
+    public void setEndDate(String endDate) {
+        this.endDate = endDate;
+    }
+    public String getEndDate() {
+        return this.endDate;
+    }
+    public void setTaskStatus(String status) {
+        switch(status) {
+            case "todo":
+                this.taskStatus = "T";
+                break;
+            case "deadline":
+                this.taskStatus = "D";
+                break;
+            case "event":
+                this.taskStatus = "E";
+                break;
+            default:
+                break;
+        }
+    }
+    public String getTaskStatus() {
+        return this.taskStatus;
     }
 }
