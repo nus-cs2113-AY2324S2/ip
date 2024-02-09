f7da7fd248245266b254900d484729a414cad03a
R M Raajamani <>
2024-02-09 14:52:02 +0800

    Level 2 - Add, List (New)

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,31 +1,65 @@
 import java.util.Scanner;
 
 public class Duke {
+    // Array to store tasks with a maximum of 100 entries.
+    private static final String[] tasks = new String[100];
+    // Counter for the number of tasks currently stored.
+    private static int taskCount = 0;
+
     public static void main(String[] args) {
+        // Print the welcome message with two spaces for indentation.
+        System.out.println("  " + Constants.LOGO);
+        System.out.println("  " + Constants.LINE);
+        System.out.println("  Hello! I'm Duke\n  What can I do for you?");
+        System.out.println("  " + Constants.LINE);
 
-        // Greeting Message
-        System.out.println(Constants.LOGO);
-        System.out.println(Constants.LINE);
-        System.out.println("Hello! I'm Orion!\n" + "What can I do for you?");
-        System.out.println(Constants.LINE);
-
+        // Scanner to read user input.
         Scanner scanner = new Scanner(System.in);
         String input;
 
-        // Echo loop
-        do {
+        // Main command processing loop.
+        while (true) {
             input = scanner.nextLine();
-            System.out.println(Constants.LINE);
-            
-            if (!input.equalsIgnoreCase("bye")) {
-                System.out.println(input);
-            } else {
-                System.out.println("Bye. Hope to see you again soon!");
-            }
 
-            System.out.println(Constants.LINE);
-        } while (!input.equalsIgnoreCase("bye"));
+            // 'bye' command to exit the program.
+            if ("bye".equalsIgnoreCase(input)) {
+                System.out.println("  " + Constants.LINE);
+                System.out.println("  Bye. Hope to see you again soon!");
+                System.out.println("  " + Constants.LINE);
+                break;
+            }
+            // 'list' command to display stored tasks.
+            else if ("list".equalsIgnoreCase(input)) {
+                System.out.println("  " + Constants.LINE);
+                listTasks();
+                System.out.println("  " + Constants.LINE);
+            }
+            // Any other input is considered a task and added to the list.
+            else {
+                System.out.println("  " + Constants.LINE);
+                addTask(input);
+                System.out.println("  " + Constants.LINE);
+            }
+        }
 
         scanner.close();
+    }
+
+    // Add a task to the array and increment task counter.
+    private static void addTask(String task) {
+        if (taskCount < tasks.length) {
+            tasks[taskCount] = task;
+            taskCount++;
+            System.out.println("  added: " + task);
+        } else {
+            System.out.println("  Task list is full!");
+        }
+    }
+
+    // Print all tasks with numbering.
+    private static void listTasks() {
+        for (int i = 0; i < taskCount; i++) {
+            System.out.println("  " + (i + 1) + ". " + tasks[i]);
+        }
     }
 }
