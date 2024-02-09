0e843e2da5d23accacccf99ee7c1a63b34db9b56
cayennegoh <cayennegoh@gmail.com>
2024-02-09 14:50:17 +0800

    Level 3

--- src/main/java/Avocado.java
+++ src/main/java/Avocado.java
@@ -1,10 +1,11 @@
+
 import java.util.Scanner;
 public class Avocado {
     public static void main(String[] args) {
         System.out.println("Hello from avocado");
         System.out.println("How can I help you ?");
         System.out.println("See you!");
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int i = 0;
         int n = 0;
         while (true) {
@@ -15,13 +16,24 @@ public class Avocado {
             if (line.equals("bye")) {
                 return;
             }
-
-            if (line.equals("list")) {
+            String[] array;
+            Task t = new Task(line);
+            array = line.split(" ");
+            if (array[0].startsWith("mark")) {
+                list[Integer.parseInt(array[1]) - 1].setDone();
+                System.out.print("[" + list[Integer.parseInt(array[1]) - 1].getStatusIcon() + "] " + list[Integer.parseInt(array[1]) -1].taskDescription());
+            }
+            else if (array[0].startsWith("unmark")) {
+                list[Integer.parseInt(array[1]) - 1].setNotDone();
+                System.out.print("[" + list[Integer.parseInt(array[1]) -1].getStatusIcon() + "]" + list[Integer.parseInt(array[1]) -1 ].taskDescription());
+            }
+            else if (line.equals("list")) {
                 for (i = 0; i < n; i++) {
-                    System.out.println((i + 1) + "." + list[i]);
+                    System.out.println((i + 1) + "." + "[" + list[i].getStatusIcon() + "]" + list[i].taskDescription());
                 }
-            } else {
-                list[i] = line;
+            }
+            else{
+                list[i] = new Task(line);
                 System.out.println("added: " + line);
                 i++;
                 n++;
