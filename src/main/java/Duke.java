1b6ca15d9aaa88aa5a7a834645c3e6f233cda887
hongyijie06 <hongyijie06@gmail.com>
2024-02-09 16:49:51 +0800

    add A-CodingStandard

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,50 +1,48 @@
 import java.util.Scanner;
 
 public class Duke {
-  public static void main(String[] args) {
-    Task[] list = new Task[100];
+    public static void main(String[] args) {
+        Task[] list = new Task[100];
 
+        //greeting
+        System.out.println("Hello! I'm Apple");
+        System.out.println("What can I do for you?");
 
+        int index = 0;//number of items in the list
+        String line = " ";
 
-    //greeting
-    System.out.println("Hello! I'm Apple");
-    System.out.println("What can I do for you?");
+        while (!line.equals("bye")) {
+            Scanner input = new Scanner(System.in);
+            line = input.nextLine();
 
-    int index = 0;//number of items in the list
-    String line =" ";
+            Task t = new Task(line);
+            String[] inputs = line.split(" ");
 
-    while (!line.equals("bye")) {
-      Scanner input = new Scanner(System.in);
-      line = input.nextLine();
+            if (inputs[0].equals("mark")) {//mark as done
+                int idx = Integer.parseInt(inputs[1]);
+                list[idx - 1].markAsDone();
+                System.out.println("Nice! I've marked this task as done: ");
+                System.out.println("[" + list[idx - 1].getStatusIcon() + "]" + list[idx - 1].description);
+            } else if (inputs[0].equals("unmark")) {//unmark done
+                int idx = Integer.parseInt(inputs[1]);
+                list[idx - 1].unmarkDone();
+                System.out.println("OK, I've marked this task as not done yet: ");
+                System.out.println("[" + list[idx - 1].getStatusIcon() + "]" + list[idx - 1].description);
+            } else if (line.equals("list")) {//lists items
+                System.out.println("Here are the tasks in your list: ");
+                for (int i = 0; i < index; i++) {
+                    System.out.println((i + 1) + ". [" + list[i].getStatusIcon() + "]" + list[i].description);
+                }
+            } else if (line.equals("bye")) {//exit chat
+                break;
+            } else {//add items
+                list[index] = t;
+                System.out.println("added: " + t.description);
+                index++;
+            }
 
-      Task t = new Task(line);
-      String[] inputs = line.split(" ");
-
-      if (inputs[0].equals("mark")){//mark as done
-        int idx = Integer.parseInt(inputs[1]);
-        list[idx - 1].markAsDone();
-        System.out.println("Nice! I've marked this task as done: ");
-        System.out.println("[" + list[idx - 1].getStatusIcon()+ "]" + list[idx - 1].description);
-      } else if(inputs[0].equals("unmark")){//unmark done
-        int idx = Integer.parseInt(inputs[1]);
-        list[idx - 1].unmarkDone();
-        System.out.println("OK, I've marked this task as not done yet: ");
-        System.out.println("[" + list[idx - 1].getStatusIcon()+ "]" + list[idx - 1].description);
-      }else if (line.equals("list")) {//lists items
-        System.out.println("Here are the tasks in your list: ");
-        for (int i = 0; i < index; i++) {
-          System.out.println((i + 1) + ". [" + list[i].getStatusIcon()+ "]" + list[i].description);
         }
-      }else if (line.equals("bye")) {//exit chat
-        break;
-      }else {//add items
-        list[index] = t;
-        System.out.println("added: " + t.description);
-        index ++;
-      }
-
+        System.out.println("Bye. Hope to see you again soon!");
     }
-    System.out.println("Bye. Hope to see you again soon!");
-  }
 
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,24 +1,21 @@
 public class Task {
-  protected String description;
-  protected int taskIndex;
-  protected boolean isDone;
+    protected String description;
+    protected boolean isDone;
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
 
-  public Task(String description) {
-    this.description = description;
-    this.isDone = false;
-  }
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
 
-  public String getStatusIcon() {
-    return (isDone ? "X" : " "); // mark done task with X
-  }
+    public void markAsDone() {
+        this.isDone = true;
+    }
 
-  public void markAsDone(){
-    this.isDone = true;
-
-  }
-
-  public void unmarkDone(){
-    this.isDone = false;
-  }
+    public void unmarkDone() {
+        this.isDone = false;
+    }
 }
 
