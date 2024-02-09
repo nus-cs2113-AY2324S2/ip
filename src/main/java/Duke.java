3440622a8966a0a8b2eea8281ce4584b1f80d864
yuhengr <e0957964@u.nus.edu>
2024-02-09 14:37:13 +0800

    Add level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -12,18 +12,32 @@ public class Duke {
 
         String userInput;
         Scanner in = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;
         userInput = in.nextLine();
 
         while(!userInput.equals("bye")){
+            String[] words = userInput.split(" ");
             if(userInput.equals("list")){
+                System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < taskCount; i++){
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks[i]);
                 }
             }
+            else if(words[0].equals("mark")){
+                int taskIndex = Integer.parseInt(words[1]) - 1;
+                tasks[taskIndex].markAsDone();
+                System.out.println("Nice! I've marked this task as done:");
+                System.out.println(tasks[taskIndex]);
+            }
+            else if(words[0].equals("unmark")){
+                int taskIndex = Integer.parseInt(words[1]) - 1;
+                tasks[taskIndex].markAsNotDone();
+                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println(tasks[taskIndex]);
+            }
             else{
-                tasks[taskCount] = userInput;
+                tasks[taskCount] = new Task(userInput);
                 System.out.println("added: " + tasks[taskCount]);
                 taskCount++;
             }
