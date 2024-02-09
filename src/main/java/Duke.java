17119f62749b3f38faa74274d1eee6aec328d4c8
LWachtel1 <lwachtelproper@gmail.com>
2024-02-09 15:02:05 +0000

    Create getUserCommands() & echoUsercommand() to echo input

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,42 @@
+import java.util.Scanner;
+
 public class Duke {
 
     public static void greet(){
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm CHAT-MAN");
-        System.out.println(" What can I do for you?");
-
+        System.out.printf("%s%n", "    ____________________________________________________________");
+        System.out.printf("%s%n", "     Hello! I'm CHAT-MAN");
+        System.out.printf("%s%n", "     What can I do for you?");
 
     }
     public static void exit(){
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.printf("%s%n","    ____________________________________________________________");
+        System.out.printf("%s%n","     Bye. Hope to see you again soon!");
+        System.out.printf("%s%n","    ____________________________________________________________");
+    }
+
+    public static void getUserCommands(){
+        boolean shouldExit=false;
+        Scanner commandReader= new Scanner(System.in);
+        String userCommand;
+
+        while(!shouldExit){
+            System.out.printf("%s%n%n","    ____________________________________________________________");
+            userCommand=commandReader.nextLine();
+            shouldExit=echoUserCommand(userCommand);
+        }
+
+
+    }
+    public static boolean echoUserCommand(String userCommand){
+        if(userCommand.equalsIgnoreCase("bye")){
+            exit();
+            return true;
+        }
+        else{
+            System.out.printf("%s%n","    ____________________________________________________________");
+            System.out.printf("     %s%n",userCommand);
+            return false;
+        }
 
     }
     public static void main(String[] args) {
@@ -22,7 +48,7 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
 
         greet();
-        exit();
+        getUserCommands();
 
     }
 }
