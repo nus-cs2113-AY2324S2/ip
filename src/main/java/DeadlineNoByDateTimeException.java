a5e7e02dd9673610bc6d7a3b5567d347f115f028
JianJiaT <tjjtjn@gmail.com>
2024-02-10 18:55:37 +0800

    Add exceptions for handling task-adding errors

---
+++ src/main/java/DeadlineNoByDateTimeException.java
@@ -0,0 +1,5 @@
+public class DeadlineNoByDateTimeException extends Exception {
+    public DeadlineNoByDateTimeException() {
+        super("ERROR: 'Deadline'-type task is missing due date!");
+    }
+}
