5bcc60f011f66937383d72fb86de9071488fe652
LWachtel1 <lwachtelproper@gmail.com>
2024-02-10 08:48:17 +0000

    Create Task class; defined getter & setter methods, including for isDone

---
+++ src/main/java/Task.java
@@ -0,0 +1,34 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        setDescription(description);
+        markAsNotDone();
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " "); // mark done task with X
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public void setDescription(String description) {
+        this.description=description;
+    }
+
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void markAsDone() {
+        this.isDone=true;
+    }
+
+    public void markAsNotDone() {
+        this.isDone=false;
+    }
+}
