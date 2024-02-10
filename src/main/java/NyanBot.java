2588b4467153d8fc01de1c1d491904202cc58133
Shui Hon Siew <shuihon@gmail.com>
2024-02-11 00:40:12 +0800

    Add printer class

--- src/main/java/NyanBot.java
+++ src/main/java/NyanBot.java
@@ -1,33 +1,33 @@
 import java.util.Scanner;
 public class NyanBot {
-    static final String LIST_COMMAND = "LIST";
-    static final String MARK_COMMAND = "MARK";
-    static final String UNMARK_COMMAND = "UNMARK";
-    static final String TODO_COMMAND = "TODO";
-    static final String DEADLINE_COMMAND = "DEADLINE";
-    static final String EVENT_COMMAND = "EVENT";
-    static final String BYE_COMMAND = "BYE";
+    private static final String LIST_COMMAND = "LIST";
+    private static final String MARK_COMMAND = "MARK";
+    private static final String UNMARK_COMMAND = "UNMARK";
+    private static final String TODO_COMMAND = "TODO";
+    private static final String DEADLINE_COMMAND = "DEADLINE";
+    private static final String EVENT_COMMAND = "EVENT";
+    private static final String BYE_COMMAND = "BYE";
     private static String LINE = "____________";
     private static Task[] tasks = new Task[100];
     private static int taskCount = 0;
+
+
+    public static void addTask(Task task) {
+        tasks[taskCount] = task;
+        taskCount++;
+        Printer.printAddTaskSuccess(task);
+    }
 
     private static void markTask(String input) {
         try {
             String[] splitInputs = input.split(" ");
             int index = Integer.parseInt(splitInputs[1]) - 1;
             tasks[index].markAsDone();
-            System.out.println("はい、markしました！");
-        } catch (NullPointerException e) {
-            System.out.println("item no exist...");
-            System.out.println("またして見よう！");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("puriisu use correct number!...");
-            System.out.println("またして見よう！");
+            Printer.printMarkSuccess();
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
+            Printer.printNullError();
         } catch (NumberFormatException e) {
-            System.out.println("puriisu use number...");
-            System.out.println("またして見よう！");
-        } finally {
-            System.out.println(LINE);
+            Printer.printMarkUsage();
         }
     }
 
@@ -36,18 +36,11 @@ public class NyanBot {
             String[] splitInputs = input.split(" ");
             int index = Integer.parseInt(splitInputs[1]) - 1;
             tasks[index].markAsUndone();
-            System.out.println("はい、unmarkしました！");
-        } catch (NullPointerException e) {
-            System.out.println("item no exist...");
-            System.out.println("またして見よう！");
-        } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("puriisu use correct number!...");
-            System.out.println("またして見よう！");
+            Printer.printUnmarkSuccess();
+        } catch (ArrayIndexOutOfBoundsException | NullPointerException e) {
+            Printer.printNullError();
         } catch (NumberFormatException e) {
-            System.out.println("puriisu use number...");
-            System.out.println("またして見よう！");
-        } finally {
-            System.out.println(LINE);
+            Printer.printUnmarkUsage();
         }
     }
 
@@ -57,10 +50,7 @@ public class NyanBot {
             Todo todo = new Todo(description);
             addTask(todo);
         } catch (StringIndexOutOfBoundsException e) {
-            System.out.println("puriisu add description!...");
-            System.out.println("またして見よう！");
-        } finally {
-            System.out.println(LINE);
+            Printer.printTodoUsage();
         }
     }
 
@@ -72,13 +62,11 @@ public class NyanBot {
             Deadline deadline = new Deadline(description, date);
             addTask(deadline);
         } catch (StringIndexOutOfBoundsException e) {
-            System.out.println("puriisu add description!...");
-            System.out.println("使い方：deadline [description] /[date]");
+            Printer.printMissingDescription();
+            Printer.printDeadlineUsage();
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("puriisu add date!");
-            System.out.println("使い方：deadline [description] /[date]");
-        } finally {
-            System.out.println(LINE);
+            Printer.printMissingDate();
+            Printer.printDeadlineUsage();
         }
     }
 
@@ -91,13 +79,11 @@ public class NyanBot {
             Event event = new Event(description, start, end);
             addTask(event);
         } catch (StringIndexOutOfBoundsException e) {
-            System.out.println("puriisu add description!...");
-            System.out.println("使い方：event [description] /[date]");
+            Printer.printMissingDescription();
+            Printer.printEventUsage();
         } catch (ArrayIndexOutOfBoundsException e) {
-            System.out.println("puriisu add start & end!");
-            System.out.println("使い方：event [description] /[start] /[end]");
-        } finally {
-            System.out.println(LINE);
+            Printer.printMissingStartEnd();
+            Printer.printEventUsage();
         }
     }
 
@@ -120,11 +106,7 @@ public class NyanBot {
                     return;
                 case LIST_COMMAND:
                     System.out.println(LINE);
-
-                    for (int i = 0; i < taskCount; i++) {
-                        System.out.print(i + 1 + ". " + tasks[i].getStatusIcon() + " ");
-                        System.out.println(tasks[i]);
-                    }
+                    Printer.printTasks(tasks, taskCount);
                     break;
                 case MARK_COMMAND:
                     int markIndex;
@@ -143,31 +125,16 @@ public class NyanBot {
                     addEvent(input);
                     break;
                 default:
-                    System.out.println("あの。。。puriisu taipu valid input ne \uD83D\uDC31\uD83C\uDF08");
-                    System.out.println("try \"help\" desu？");
+                    Printer.printInvalidInput();
                     break;
             }
         }
     }
 
-    public static void addTask(Task task) {
-        tasks[taskCount] = task;
-        taskCount++;
-        System.out.println(LINE);
-        System.out.println("Added " + task);
-    }
-
     public static void main(String[] args) {
-        String GREET = "お帰りなさいませ、ご主人様。ニャンー♡♡";
-        String PROMPT = "なにをしたいの？";
-        String BYE = "じゃー、またね〜！！";
 
-        System.out.println(LINE);
-        System.out.println(GREET + "\n" + PROMPT);
-
+        Printer.printGreet();
         getInput();
-
-        System.out.println(LINE);
-        System.out.println(BYE);
+        Printer.printBye();
     }
 }
