d2836b63c2de7a1d8149b7775dcd5f9fe2244247
LWachtel1 <lwachtelproper@gmail.com>
2024-02-11 09:57:10 +0000

    Change code to comply with Google Java Style guide; use switch statement

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,90 +1,140 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
+// I am using the Google Java Style Guide https://google.github.io/styleguide/javaguide.html
+
 public class Duke {
 
-    static ArrayList<Task> enteredCommands=new ArrayList<Task>();
+  static ArrayList<Task> enteredCommands=new ArrayList<Task>();
 
-    public static void greet(){
-        System.out.printf("%s%n", "    ____________________________________________________________");
-        System.out.printf("%s%n", "     Hello! I'm CHAT-MAN");
-        System.out.printf("%s%n", "     What can I do for you?");
+  public static void greet() {
+    System.out.printf("%s%n", "    ____________________________________________________________");
+    System.out.printf("%s%n", "     Hello! I'm CHAT-MAN");
+    System.out.printf("%s%n", "     What can I do for you?");
+  }
 
+  public static void exit() {
+    System.out.printf("%s%n","    ____________________________________________________________");
+    System.out.printf("%s%n","     Bye. Hope to see you again soon!");
+    System.out.printf("%s%n","    ____________________________________________________________");
+  }
+
+  public static void getUserCommands() {
+    Scanner commandReader= new Scanner(System.in);
+    String userCommand;
+    boolean shouldExit=false;
+
+    while (!shouldExit) {
+      if (enteredCommands.size()<100) {
+        System.out.printf("%s%n%n", "    ____________________________________________________________");
+        userCommand = commandReader.nextLine();
+        shouldExit = executesCommand(userCommand);
+      } else {
+        exit();
+        shouldExit=true;
+      }
     }
-    public static void exit(){
+  }
+
+  public static boolean executesCommand(String userCommand) {
+
+    String[] separatedCommand=userCommand.split(" ");
+    int taskToMark;
+
+    switch(separatedCommand[0].toUpperCase()){
+
+      case "BYE":
+        exit();
+        return true;
+
+      case "LIST":
         System.out.printf("%s%n","    ____________________________________________________________");
-        System.out.printf("%s%n","     Bye. Hope to see you again soon!");
+        for(int i=0;i< enteredCommands.size();i++) {
+          System.out.printf("     %d.%s %s%n",(i+1),enteredCommands.get(i).getStatusIcon(),enteredCommands.get(i).getDescription());
+        }
+        return false;
+
+
+      case "MARK":
+        try{
+          taskToMark=Integer.parseInt(userCommand.replaceAll("[^0-9]", ""))-1;
+          enteredCommands.get(taskToMark).markAsDone();
+          System.out.printf("%s%n","    ____________________________________________________________");
+          System.out.printf("     Nice! I've marked this task as done:%n       %s %s%n",
+              enteredCommands.get(taskToMark).getStatusIcon(),enteredCommands.get(taskToMark).getDescription());
+          return false;
+        }
+        catch(Exception e){
+          System.out.printf("%s%n","    ____________________________________________________________");
+          System.out.printf("     No tasks added yet to mark.%n");
+          return false;
+        }
+
+      case "UNMARK":
+        try{
+          taskToMark=Integer.parseInt(userCommand.replaceAll("[^0-9]", ""))-1;
+          enteredCommands.get(taskToMark).markAsNotDone();
+          System.out.printf("%s%n","    ____________________________________________________________");
+          System.out.printf("     OK, I've marked this task as not done yet:%n       %s %s%n",
+            enteredCommands.get(taskToMark).getStatusIcon(),enteredCommands.get(taskToMark).getDescription());
+          return false;
+        }
+        catch(Exception E){
+          System.out.printf("%s%n","    ____________________________________________________________");
+          System.out.printf("     No tasks added yet to mark.%n");
+          return false;
+        }
+
+      default:
         System.out.printf("%s%n","    ____________________________________________________________");
-    }
-
-    public static void getUserCommands(){
-        boolean shouldExit=false;
-        Scanner commandReader= new Scanner(System.in);
-        String userCommand;
-
-        while(!shouldExit){
-            if(enteredCommands.size()<100) {
-                System.out.printf("%s%n%n", "    ____________________________________________________________");
-                userCommand = commandReader.nextLine();
-                shouldExit = executesCommand(userCommand);
-            }
-            else{
-                exit();
-                shouldExit=true;
-            }
-        }
-
-
+        System.out.printf("     added: %s%n",userCommand);
+        enteredCommands.add(new Task(userCommand));
+        return false;
 
     }
-    public static boolean executesCommand(String userCommand){
-        if(userCommand.equalsIgnoreCase("bye")){
-            exit();
-            return true;
-        }
 
-        else if(userCommand.equalsIgnoreCase("list")){
-            System.out.printf("%s%n","    ____________________________________________________________");
-            for(int i=0;i< enteredCommands.size();i++){
-                System.out.printf("     %d.%s %s%n",(i+1),enteredCommands.get(i).getStatusIcon(),enteredCommands.get(i).getDescription());
-            }
-            return false;
-        }
-        else if(userCommand.toUpperCase().contains("MARK") && !userCommand.toUpperCase().contains("UNMARK")){
-            int taskToMark=Integer.parseInt(userCommand.replaceAll("[^0-9]", ""))-1;
-            enteredCommands.get(taskToMark).markAsDone();
-            System.out.printf("%s%n","    ____________________________________________________________");
-            System.out.printf("     Nice! I've marked this task as done:%n       %s %s%n",
-                    enteredCommands.get(taskToMark).getStatusIcon(),enteredCommands.get(taskToMark).getDescription());
-            return false;
-        }
-        else if(userCommand.toUpperCase().contains("UNMARK")){
-            int taskToMark=Integer.parseInt(userCommand.replaceAll("[^0-9]", ""))-1;
-            enteredCommands.get(taskToMark).markAsNotDone();
-            System.out.printf("%s%n","    ____________________________________________________________");
-            System.out.printf("     OK, I've marked this task as not done yet:%n       %s %s%n",
-                    enteredCommands.get(taskToMark).getStatusIcon(),enteredCommands.get(taskToMark).getDescription());
-            return false;
-        }
+    /*if (userCommand.equalsIgnoreCase("bye")) {
+      exit();
+      return true;
+    } else if (userCommand.equalsIgnoreCase("list")) {
+      System.out.printf("%s%n","    ____________________________________________________________");
+      for(int i=0;i< enteredCommands.size();i++) {
+        System.out.printf("     %d.%s %s%n",(i+1),enteredCommands.get(i).getStatusIcon(),enteredCommands.get(i).getDescription());
+      }
+      return false;
+    } else if (userCommand.toUpperCase().contains("MARK") && !userCommand.toUpperCase().contains("UNMARK")) {
+      int taskToMark=Integer.parseInt(userCommand.replaceAll("[^0-9]", ""))-1;
+      enteredCommands.get(taskToMark).markAsDone();
+      System.out.printf("%s%n","    ____________________________________________________________");
+      System.out.printf("     Nice! I've marked this task as done:%n       %s %s%n",
+          enteredCommands.get(taskToMark).getStatusIcon(),enteredCommands.get(taskToMark).getDescription());
+      return false;
+    } else if (userCommand.toUpperCase().contains("UNMARK")) {
+      int taskToMark=Integer.parseInt(userCommand.replaceAll("[^0-9]", ""))-1;
+      enteredCommands.get(taskToMark).markAsNotDone();
+      System.out.printf("%s%n","    ____________________________________________________________");
+      System.out.printf("     OK, I've marked this task as not done yet:%n       %s %s%n",
+          enteredCommands.get(taskToMark).getStatusIcon(),enteredCommands.get(taskToMark).getDescription());
+      return false;
+    } else {
+      System.out.printf("%s%n","    ____________________________________________________________");
+      System.out.printf("     added: %s%n",userCommand);
+      enteredCommands.add(new Task(userCommand));
+      return false;
+    }*/
 
-        else{
-            System.out.printf("%s%n","    ____________________________________________________________");
-            System.out.printf("     added: %s%n",userCommand);
-            enteredCommands.add(new Task(userCommand));
-            return false;
-        }
+  }
 
-    }
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
+  public static void main(String[] args) {
+    String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+    System.out.println("Hello from\n" + logo);
 
-        greet();
-        getUserCommands();
+    greet();
+    getUserCommands();
 
-    }
+  }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,34 +1,35 @@
 public class Task {
-    protected String description;
-    protected boolean isDone;
 
-    public Task(String description) {
+  protected String description;
+  protected boolean isDone;
+
+  public Task(String description) {
         setDescription(description);
         markAsNotDone();
-    }
+  }
 
-    public String getStatusIcon() {
-        return (isDone ? "[X]" : "[ ]"); // mark done task with X
-    }
+  public String getDescription() {
+    return this.description;
+  }
 
-    public String getDescription() {
-        return this.description;
-    }
+  public void setDescription(String description) {
+    this.description=description;
+  }
 
-    public void setDescription(String description) {
-        this.description=description;
-    }
-
-
-    public boolean getIsDone() {
+  public boolean getIsDone() {
         return this.isDone;
-    }
+  }
 
-    public void markAsDone() {
-        this.isDone=true;
-    }
+  public void markAsDone() {
+    this.isDone=true;
+  }
 
-    public void markAsNotDone() {
-        this.isDone=false;
-    }
+  public void markAsNotDone() {
+    this.isDone=false;
+  }
+
+  public String getStatusIcon() {
+    return (isDone ? "[X]" : "[ ]"); // mark done task with X
+  }
+
 }
