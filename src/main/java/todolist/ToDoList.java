8f8372cf920f99a9b829ceb964d7a2f5227e9775
XiaoBo <xiaobo990219@gmail.com>
2024-02-11 15:57:29 +0800

    Split Todolist to further subclass; Add ErrorMessage class, Keyword calss and KeywordPatternMatcher

--- src/main/java/todolist/ToDoList.java
+++ src/main/java/todolist/ToDoList.java
@@ -1,22 +1,10 @@
 package todolist;
 import java.util.Scanner;
 import java.util.ArrayList;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ToDoList {
     private final ArrayList<Task> toDoListArray;
     //Keywords
-    private static final String MARK_FEATURE_PATTERN = "^mark \\d+$"; //mark
-    private static final String UNMARK_FEATURE_PATTERN = "^unmark \\d+$"; //unmark
-    private static final String TODOTASK_FEATURE_PATTERN = "^todo .*"; //todoItem
-    private static final String DEADLINESTASK_FEATURE_PATTERN = "^deadline .*"; //todoItem
-    private static final String EVENTSTASK_PATTERN = "^event .*"; //todoItem
-
-    private static final int SPACE_OFFSET = 1; //keyword " " offset
-    private static final int BY_OFFSET = 4; //keyword "/by" offset
-    private static final int FROM_OFFSET = 6; //keyword "/from" offset
-    private static final int TO_OFFSET = 4; //keyword "/to" offset
 
     /**
      * Constructor for ToDoList
@@ -30,13 +18,13 @@ public class ToDoList {
      * @param index the index of the task to be marked
      */
     private void markTask(int index) {
-        if (index > toDoListArray.size()) {
-            System.out.println("Task does not exist");
-            return;
+        try {
+            this.toDoListArray.get(index - 1).mark();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  " + this.toDoListArray.get(index - 1).toString());
+        } catch (IndexOutOfBoundsException e) {
+            ErrorMessages.taskDoesNotExistErrorMessage();
         }
-        this.toDoListArray.get(index - 1).mark();
-        System.out.println("Nice! I've marked this task as done:");
-        System.out.println("  " + this.toDoListArray.get(index -1).toString());
     }
 
     /**
@@ -44,13 +32,14 @@ public class ToDoList {
      * @param index the index of the task to be unmarked
      */
     private void unMarkTask(int index) {
-        if (index > toDoListArray.size()) {
-            System.out.println("Task does not exist");
-            return;
+        try {
+            this.toDoListArray.get(index - 1).unmark();
+            System.out.println("OK, I've marked this task as not done yet:");
+            System.out.println("  " + this.toDoListArray.get(index - 1).toString());
+        } catch (IndexOutOfBoundsException e) {
+            ErrorMessages.taskDoesNotExistErrorMessage();
         }
-        this.toDoListArray.get(index - 1).unmark();
-        System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("  " + this.toDoListArray.get(index -1).toString());
+
     }
 
     /**
@@ -59,34 +48,47 @@ public class ToDoList {
      */
     public void execute(Scanner input) {
         featureIntroMessage();
-        boolean isFinished = false;
         while (true) {
             String inputText = input.nextLine();
-            try {
-                if (!processKeywordInput(inputText)) {
-                    //add a specific todoTask based on the keyword input
-                    switch (inputText) {
-                    case "list":
-                        printToDoList();
-                        break;
-                
-                    case "help":
-                        featureIntroMessage();
-                        break;
-                
-                    case "quit":
-                        return;
-                
-                    default:
-                        System.out.println("invalid command, use [help] to show all the commands");
-                        break;
-                    }
+            KeywordPatternMatcher keywordFinder = new KeywordPatternMatcher(inputText);
+            if (keywordFinder.getKeywordType() == Keyword.none) {
+                switch (inputText) {
+                case "list":
+                    printToDoList();
+                    break;
+
+                case "help":
+                    featureIntroMessage();
+                    break;
+
+                case "quit":
+                    return;
+
+                default:
+                    ErrorMessages.invalidCommandErrorMessage();
+                    break;
                 }
-                messageDivider();
-            } catch (StringIndexOutOfBoundsException e) {
-                System.out.println("invalid command, use [help] to show all the commands");
-                messageDivider();
+            } else {
+                switch (keywordFinder.getKeywordType()) {
+                case mark:
+                    markTask(keywordFinder.findMarkOrUnmarkIndex());
+                    break;
+                case unmark:
+                    unMarkTask(keywordFinder.findMarkOrUnmarkIndex());
+                    break;
+                case todoError:
+                    ErrorMessages.todoTaskFormatIncorrectErrorMessage();
+                    break;
+                default:
+                    if (keywordFinder.processKeywordInput() == null) {
+                        ErrorMessages.taskFormatIncorrectErrorMessage();
+                    } else {
+                        addTask(keywordFinder.processKeywordInput());
+                    }
+                    break;
+                }
             }
+            messageDivider();
         }
     }
 
@@ -100,11 +102,11 @@ public class ToDoList {
 
     /**
      * Add a Task to the ToDoList
-     * @param item the ToDo item to be added
+     * @param task the Task to be added
      */
-    public void addTask(Task item) {
-        this.toDoListArray.add(item);
-        System.out.println("added: " + item.getName());
+    public void addTask(Task task) {
+        this.toDoListArray.add(task);
+        System.out.println("added: " + task.getName());
         System.out.println("Now you have " + this.toDoListArray.size() + " task(s) in the list");
     }
 
@@ -138,6 +140,7 @@ public class ToDoList {
         System.out.println("[event XXX /from XXX /to XXX] Add a Event task");
         System.out.println("[help] Show commands");
         System.out.println("[quit] Return to main menu");
+        System.out.println("Where XXX Stands for any string (except space character)");
         messageDivider();
         System.out.println("Please enter a command:");
     }
@@ -148,58 +151,4 @@ public class ToDoList {
     public static void messageDivider() {
         System.out.println("------------------------------------------");
     }
-    
-    /**
-     * Get the position of the first space in the input text
-     * @param inputText the input text
-     * @return the position of the first space in the input text
-     */
-    private int getNumberPosition(String inputText) {
-        return inputText.indexOf(" ");
-    }
-    
-    /**
-     * Process the keyword input and add the corresponding task to the list
-     * @param inputText the input text
-     * @return true if the input is a keyword, false otherwise
-     */
-    private boolean processKeywordInput(String inputText) {
-        int numberPosition = getNumberPosition(inputText);
-        if (matchesPattern(inputText, MARK_FEATURE_PATTERN)) {
-            markTask(Integer.parseInt(inputText.substring(numberPosition + SPACE_OFFSET)));
-            return true;
-        } else if (matchesPattern(inputText, UNMARK_FEATURE_PATTERN)) {
-            unMarkTask(Integer.parseInt(inputText.substring(numberPosition + SPACE_OFFSET)));
-            return true;
-        } else if (matchesPattern(inputText, TODOTASK_FEATURE_PATTERN)) {
-            addTask(new ToDoTask(inputText.substring(numberPosition + SPACE_OFFSET)));
-            return true;
-        } else if (matchesPattern(inputText, DEADLINESTASK_FEATURE_PATTERN)) {
-            int keywordPosition = inputText.indexOf("/by");
-            addTask(new DeadLinesTask(inputText.substring(numberPosition + SPACE_OFFSET, keywordPosition - 1),
-                    inputText.substring(keywordPosition + BY_OFFSET)));
-            return true;
-        } else if (matchesPattern(inputText, EVENTSTASK_PATTERN)) {
-            int keywordPosition1 = inputText.indexOf("/from"); // check /from keyword position
-            int keywordPosition2 = inputText.indexOf("/to"); // check /to keyword position
-            addTask(new EventsTask(inputText.substring(numberPosition + SPACE_OFFSET, keywordPosition1 - 1),
-                    inputText.substring(keywordPosition1 + FROM_OFFSET, keywordPosition2 - 1),
-                    inputText.substring(keywordPosition2 + TO_OFFSET)));
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * Check if the input matches the pattern
-     * @param input the input text
-     * @param pattern the pattern to be matched
-     * @return true if the input matches the pattern, false otherwise
-     */
-    private static boolean matchesPattern(String input, String pattern) {
-        Pattern regex = Pattern.compile(pattern);
-        Matcher matcher = regex.matcher(input);
-        return matcher.matches();
-    }
-
 }
