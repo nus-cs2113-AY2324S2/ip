3c715e3334848a4207d21a19d8d3a90a668dfdcf
Joellimjr <joellimjr12@gmail.com>
2024-02-12 23:25:56 +0800

    add error handling

--- src/main/java/Chelle.java
+++ src/main/java/Chelle.java
@@ -18,28 +18,54 @@ public class Chelle {
                 System.out.println("Chelle: Bye! Hope to see you again soon!");
                 scanner.close();
                 return;
+
             case "list":
                 System.out.println("Chelle: ");
                 displayTasks(tasks, taskCount);
                 break;
+
             case "mark":
-                markTask(userInput, tasks, taskCount);
+                if (userInput.length() > 5) {
+                    markTask(userInput.substring(5), tasks, taskCount);
+                } else {
+                    System.out.println("Chelle: Invalid format. Use 'mark ___'.");
+                }
                 break;
+
             case "unmark":
-                unmarkTask(userInput, tasks, taskCount);
+                if (userInput.length() > 7) {
+                    unmarkTask(userInput.substring(7), tasks, taskCount);
+                } else {
+                    System.out.println("Chelle: Invalid format. Use 'unmark ___'.");
+                }
                 break;
+
             case "todo":
-                taskCount = createTask(userInput.substring(5), tasks, taskCount, TaskType.TODO);
+                if (userInput.length() > 5) {
+                    taskCount = createTask(userInput.substring(5), tasks, taskCount, TaskType.TODO);
+                } else {
+                    System.out.println("Chelle: Invalid format. Use 'todo ___'.");
+                }
                 break;
+
             case "deadline":
-                taskCount = createTask(userInput.substring(9), tasks, taskCount, TaskType.DEADLINE);
+                if (userInput.length() > 9 && userInput.contains("/by")) {
+                    taskCount = createTask(userInput.substring(9), tasks, taskCount, TaskType.DEADLINE);
+                } else {
+                    System.out.println("Chelle: Invalid format. Use 'deadline ___ /by ___'.");
+                }
                 break;
+
             case "event":
-                taskCount = createTask(userInput.substring(6), tasks, taskCount, TaskType.EVENT);
+                if (userInput.length() > 6 && (userInput.indexOf("/from") < userInput.indexOf("/to"))) {
+                    taskCount = createTask(userInput.substring(6), tasks, taskCount, TaskType.EVENT);
+                } else {
+                    System.out.println("Chelle: Invalid format. Use 'event ___ /from ___ /to ___'.");
+                }
                 break;
             default:
                 System.out.println("Chelle: Invalid command. Please start your command with one of the following commands: \n" +
-                        "        'list', 'mark', 'unmark', 'todo', deadline' or 'event'.");
+                        "'list', 'mark', 'unmark', 'todo', 'deadline' or 'event'.");
                 break;
             }
         }
@@ -56,68 +82,55 @@ public class Chelle {
     }
 
     private static int createTask(String userInput, Task[] tasks, int taskCount, TaskType taskType) {
-        if (taskCount < tasks.length) {
-            switch (taskType){
-                case TODO:
-                tasks[taskCount] = new ToDo(userInput);
-                break;
-            case DEADLINE:
-                tasks[taskCount] = new Deadline(userInput);
-                break;
-            case EVENT:
-                tasks[taskCount] = new Event(userInput);
-                break;
-            default:
-                System.out.println("Error");
-                break;
-            }
-            taskCount++;
-            System.out.println("Chelle: Got it. I've added this task:\n        " +
-                    tasks[taskCount - 1].toString() +
-                    "\n     Now you have " + taskCount + " tasks in the list.");
-        } else {
-            System.out.println("Chelle: Task limit reached. Cannot add more tasks.");
+        switch (taskType) {
+        case TODO:
+            tasks[taskCount] = new ToDo(userInput);
+            break;
+        case DEADLINE:
+            tasks[taskCount] = new Deadline(userInput);
+            break;
+        case EVENT:
+            tasks[taskCount] = new Event(userInput);
+            break;
+        default:
+            System.out.println("Chelle: Error");
+            return taskCount;
         }
+
+        taskCount++;
+        System.out.println("Chelle: Got it. I've added this task:\n        " +
+                tasks[taskCount - 1].toString() +
+                "\n     Now you have " + taskCount + " tasks in the list.");
         return taskCount;
     }
 
     private static void markTask(String userInput, Task[] tasks, int taskCount) {
-        String[] parts = userInput.split(" ");
-        if (parts.length == 2) {
-            try {
-                int taskIndex = Integer.parseInt(parts[1]) - 1;
-                if (taskIndex >= 0 && taskIndex < taskCount) {
-                    tasks[taskIndex].markDone();
-                    System.out.println("Chelle: Nice! I've marked this task as done:\n        " +
-                            tasks[taskIndex].toString());
-                } else {
-                    System.out.println("Chelle: Invalid task index.");
-                }
-            } catch (NumberFormatException e) {
+        try {
+            int taskIndex = Integer.parseInt(userInput) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markDone();
+                System.out.println("Chelle: Nice! I've marked this task as done:\n        " +
+                        tasks[taskIndex].toString());
+            } else {
                 System.out.println("Chelle: Invalid task index.");
             }
-        } else {
-            System.out.println("Chelle: Invalid command format.");
+        } catch (NumberFormatException e) {
+            System.out.println("Chelle: Invalid task index.");
         }
     }
 
     private static void unmarkTask(String userInput, Task[] tasks, int taskCount) {
-        String[] parts = userInput.split(" ");
-        if (parts.length == 2) {
-            try {
-                int taskIndex = Integer.parseInt(parts[1]) - 1;
-                if (taskIndex >= 0 && taskIndex < taskCount) {
-                    tasks[taskIndex].markUndone();
-                    System.out.println("Chelle: OK, I've marked this task as not done yet:\n        " +
-                            tasks[taskIndex].toString());
-                } else {
-                    System.out.println("Chelle: Invalid task index.");
-                }
-            } catch (NumberFormatException e) {
+        try {
+            int taskIndex = Integer.parseInt(userInput) - 1;
+            if (taskIndex >= 0 && taskIndex < taskCount) {
+                tasks[taskIndex].markUndone();
+                System.out.println("Chelle: OK, I've marked this task as not done yet:\n        " +
+                        tasks[taskIndex].toString());
+            } else {
                 System.out.println("Chelle: Invalid task index.");
             }
-        } else {
-            System.out.println("Chelle: Invalid command format.");
+        } catch (NumberFormatException e) {
+            System.out.println("Chelle: Invalid task index.");
         }
     }
 }
