3b008f83ccfa27e8aa5ad6d3782783d89ee70876
EugeneChanJiajun <e0969415@u.nus.edu>
2024-02-12 22:14:51 +0800

    Added error messages to deal with incorrect inputs

--- src/main/java/Binks.java
+++ src/main/java/Binks.java
@@ -5,7 +5,7 @@ public class Binks {
     /**
      * Prints out the greeting message from the chatbot "Binks".
      */
-    public static void greetUser(){
+    public static void greetUser() {
         createLineSpacing();
         System.out.println("Hello! I'm Binks.");
         System.out.println("What can I do for you?");
@@ -15,66 +15,119 @@ public class Binks {
     /**
      * Prints out the line spacing between inputs and outputs in the console.
      */
-    public static void createLineSpacing(){
+    public static void createLineSpacing() {
         System.out.println("____________________________________________________________");
     }
 
     /**
      * Prints out the exit message from the chatbot "Binks" when user inputs "Bye".mark
      */
-    public static void exitChatbot(){
+    public static void exitChatbot() {
         createLineSpacing();
         System.out.println("Bye. Hope to see you again soon!");
         createLineSpacing();
     }
-    public static void main(String[] arg){
+
+    public static void main(String[] arg) {
         greetUser();
         List list = new List();
-        while(true){
+        while (true) {
             String line;
             Scanner in = new Scanner(System.in);
             line = in.nextLine();
             String[] command = line.split(" ");
-            if (line.equalsIgnoreCase("list")){
+
+            switch (command[0].toLowerCase()) {
+            case "list":
                 list.getList();
-            }
-            //when the mark command is given
-            else if(command.length == 2 && command[0].equalsIgnoreCase("mark")){
-                int taskNumber = Integer.parseInt(command[1]);
-                list.markAsDone(taskNumber);
-            }
-            //when the unmark command is given
-            else if(command.length == 2 && command[0].equalsIgnoreCase("unmark")){
-                int taskNumber = Integer.parseInt(command[1]);
-                list.unmarkAsDone(taskNumber);
-            }
-            //when the bye command is given
-            else if (line.equalsIgnoreCase("bye")) {
-                exitChatbot();
                 break;
-            }
+
+            //when the mark command is given
+            case "mark":
+                if (command.length == 2) {
+                    int taskNumber = Integer.parseInt(command[1]);
+                    list.markAsDone(taskNumber);
+                } else {
+                    createLineSpacing();
+                    System.out.println("Please specify which task to mark.");
+                    createLineSpacing();
+                }
+                break;
+
+            //when the unmark command is given
+            case "unmark":
+                if (command.length == 2) {
+                    int taskNumber = Integer.parseInt(command[1]);
+                    list.unmarkAsDone(taskNumber);
+                } else {
+                    createLineSpacing();
+                    System.out.println("Please specify which task to unmark.");
+                    createLineSpacing();
+                }
+                break;
+
+            //when the bye command is given
+            case "bye":
+                exitChatbot();
+                return;
+
             //when there is a new todo to add
-            else if (command.length >= 2 && command[0].equalsIgnoreCase("todo")){
-                list.addTodo(line.substring(5));
-            }
+            case "todo":
+                if (command.length >= 2) {
+                    list.addTodo(line.substring(5));
+                } else {
+                    createLineSpacing();
+                    System.out.println("WARNING! The description of todo cannot be empty!");
+                    createLineSpacing();
+                }
+                break;
+
             //when there is a new deadline to add
-            else if (command.length >= 2 && command[0].equalsIgnoreCase("deadline")) {
-                int bySeparator = line.indexOf("/by ");
-                String deadline = line.substring(bySeparator + 4);
-                String task = line.substring(9, bySeparator);
-                list.addDeadline(task + " (by: " + deadline + ")");
-            }
+            case "deadline":
+                if (command.length >= 2) {
+                    int bySeparator = line.indexOf("/by ");
+                    if (bySeparator != -1) {
+                        String deadline = line.substring(bySeparator + 4);
+                        String task = line.substring(9, bySeparator);
+                        list.addDeadline(task + " (by: " + deadline + ")");
+                    } else {
+                        createLineSpacing();
+                        System.out.println("WARNING! Please indicate a deadline using /by.");
+                        createLineSpacing();
+                    }
+                } else {
+                    createLineSpacing();
+                    System.out.println("Invalid deadline command.");
+                    createLineSpacing();
+                }
+                break;
+
             //when there is a new event to add
-            else if (command.length >= 2 && command[0].equalsIgnoreCase("event")) {
-                int fromSeparator = line.indexOf("/from ");
-                int toSeparator = line.indexOf("/to ");
-                String startTime = line.substring(fromSeparator + 6, toSeparator);
-                String endTime = line.substring(toSeparator + 4);
-                String task = line.substring(6, fromSeparator);
-                list.addEvent(task + " (from: " + startTime + "to: " + endTime + ")");
-            }
-            else {
-                System.out.println("This is not a valid command.");
+            case "event":
+                if (command.length >= 2) {
+                    int fromSeparator = line.indexOf("/from ");
+                    int toSeparator = line.indexOf("/to ");
+                    if (fromSeparator != -1 && toSeparator != -1) {
+                        String startTime = line.substring(fromSeparator + 6, toSeparator);
+                        String endTime = line.substring(toSeparator + 4);
+                        String task = line.substring(6, fromSeparator);
+                        list.addEvent(task + " (from: " + startTime + "to: " + endTime + ")");
+                    } else {
+                        createLineSpacing();
+                        System.out.println("OOPS!!! Please specify start and end times using /from and /to.");
+                        createLineSpacing();
+                    }
+                } else {
+                    createLineSpacing();
+                    System.out.println("Invalid event command.");
+                    createLineSpacing();
+                }
+                break;
+
+            default:
+                createLineSpacing();
+                System.out.println("This is not a valid command!");
+                createLineSpacing();
             }
         }
     }
