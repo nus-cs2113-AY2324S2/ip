be5594c83166b02037d97fc3ecd1212040c956f6
Lin Htet <earthangle007@gmail.com>
2024-02-13 15:00:48 +0800

    Add try/catch to handle exception for empty command

--- src/main/java/Tony.java
+++ src/main/java/Tony.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Tony {
@@ -16,16 +17,24 @@ public class Tony {
             } else if(line.equals("list")) {
                 listTasks(tasks);
             } else if(line.contains("mark")) {
-                String[] subCommand = line.split(" ");
-                int num = Integer.parseInt(subCommand[1]);
-                if(num > taskCounter) {
-                    System.out.println("Suggest a number available in the list!");
-                } else {
-                    markTasks(subCommand, tasks, num);
+                try {
+                    String[] subCommand = line.split(" ");
+                    int num = Integer.parseInt(subCommand[1]);
+                    checkNumberWithinRange(num);
+                    markTasks(subCommand, num, tasks);
+                } catch (NumberFormatException nfe) {
+                    System.out.println("Suggest only number after 'mark'!");
+                } catch (TonyException e) {
+                    System.out.println("To mark a task, suggest a number available in the list!");
                 }
             } else if(line.startsWith("todo") || line.startsWith("deadline")
                     || line.startsWith("event")) {
-                addTasks(line, tasks);
+                try {
+                    addATask(line, tasks);
+                } catch (TonyException e) {
+                    System.out.println(LINE_BREAKER + "OOPS!! The description of " + line
+                            + " cannot be empty." + System.lineSeparator() + LINE_BREAKER);
+                }
             } else {
                 System.out.println("Please begin input with the following words: "
                         + System.lineSeparator()
@@ -33,7 +42,6 @@ public class Tony {
             }
         }
     }
-
     private static void listTasks(Task[] tasks) {
         System.out.println(LINE_BREAKER);
         System.out.println("\tHere are the tasks in your list:");
@@ -45,7 +53,13 @@ public class Tony {
         System.out.println(LINE_BREAKER);
     }
 
-    private static void markTasks(String[] subCommand, Task[] tasks, int num) {
+    private static void checkNumberWithinRange(int num) throws TonyException {
+        if(num > taskCounter) {
+            throw new TonyException();
+        }
+    }
+    private static void markTasks(String[] subCommand, int num, Task[] tasks) {
+
         if(subCommand[0].equals("mark")) {
             tasks[num -1].markDone();
             System.out.println(
@@ -75,25 +89,42 @@ public class Tony {
                 + LINE_BREAKER;
         System.out.println(chatBot);
     }
-    private static void addTasks(String userInput, Task[] allTasks) {
+    private static void addATask(String userInput, Task[] allTasks) throws TonyException {
         if(userInput.startsWith("todo")) {
             String[] toDoTask = userInput.split("todo");
-            Todo todo = new Todo(toDoTask[1]);
-            allTasks[taskCounter] = todo;
-            printAddNewTask(allTasks);
+            checkArrayLength(userInput, toDoTask);
+            addTodoTask(allTasks, toDoTask);
         } else if(userInput.startsWith("deadline")) {
             String[] deadlineTask = userInput.split("deadline");
-            String[] description = deadlineTask[1].split("/by");
-            Deadline deadline = new Deadline(description[0], description[1]);
-            allTasks[taskCounter] = deadline;
-            printAddNewTask(allTasks);
+            checkArrayLength(userInput, deadlineTask);
+            addDeadlineTask(allTasks, deadlineTask);
         } else if(userInput.startsWith("event")) {
             String[] eventTask = userInput.split("event");
-            String[] description = eventTask[1].split("/from | /to");
-            Event event = new Event(description[0], description[1], description[2]);
-            allTasks[taskCounter] = event;
-            printAddNewTask(allTasks);
+            checkArrayLength(userInput, eventTask);
+            addEventTask(allTasks, eventTask);
         }
+    }
+    private static void checkArrayLength(String userInput, String[] toDoTask) throws TonyException {
+        if(toDoTask.length != 2) {
+            throw new TonyException();
+        }
+    }
+    private static void addEventTask(Task[] allTasks, String[] eventTask) {
+        String[] description = eventTask[1].split("/from | /to");
+        Event event = new Event(description[0], description[1], description[2]);
+        allTasks[taskCounter] = event;
+        printAddNewTask(allTasks);
+    }
+    private static void addDeadlineTask(Task[] allTasks, String[] deadlineTask) {
+        String[] description = deadlineTask[1].split("/by");
+        Deadline deadline = new Deadline(description[0], description[1]);
+        allTasks[taskCounter] = deadline;
+        printAddNewTask(allTasks);
+    }
+    private static void addTodoTask(Task[] allTasks, String[] toDoTask) {
+        Todo todo = new Todo(toDoTask[1]);
+        allTasks[taskCounter] = todo;
+        printAddNewTask(allTasks);
     }
     private static void printAddNewTask(Task[] allTasks) {
         taskCounter++;
