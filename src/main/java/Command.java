765422fe81e2ca2d3b69c7ae462b52a62f32d7b0
imanamirshah <iman.amirshah@gmail.com>
2024-02-13 12:45:25 +0800

    feat: Add Command enum
    • facilitates switch case based on command
    • improves scalability when more commands are added

---
+++ src/main/java/Command.java
@@ -0,0 +1,39 @@
+public enum Command {
+    LIST,
+    BYE,
+    MARK,
+    UNMARK,
+    TODO,
+    DEADLINE,
+    EVENT;
+
+    public static Command getCommand(String type) {
+        return Command.valueOf(type.toUpperCase());
+    }
+
+    public static Command getFirstWord(String input) {
+        int firstSpace = input.indexOf(" ");
+        String firstWord;
+        // Check if there is no space in the input string
+        if (firstSpace == -1) {
+            // If no space is found, the entire input string is considered the first word
+            firstWord = input;
+        } else {
+            // If a space is found, extract the substring up to the first space
+            firstWord = input.substring(0, firstSpace);
+        }
+        return Command.getCommand(firstWord);
+    }
+
+
+    public static String getDescription(String input) {
+        int firstSpace = input.indexOf(" ");
+        // Check if there is no space in the input string
+        if (firstSpace == -1) {
+            return ""; // Return an empty string if no description is present
+        }
+        // Otherwise, return the substring after the first space
+        return input.substring(firstSpace + 1);
+    }
+
+}
--- src/main/java/TaskType.java
+++ src/main/java/TaskType.java
@@ -2,9 +2,5 @@ public enum TaskType {
     TODO,
     EVENT,
     DEADLINE;
-
-    public static TaskType getTaskType(String type) {
-            return TaskType.valueOf(type.toUpperCase());
-        }
 }
 
