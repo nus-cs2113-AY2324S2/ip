4dc482a98b3861b06de09c88669b08bccf694cda
KRISHNAAYAGARI\kak36 <mukund1403@yahoo.com>
2024-02-13 17:20:31 +0800

    Add exception handling functionalities to todo, deadline, and event

---
+++ src/main/java/emptyEvent.java
@@ -0,0 +1,5 @@
+public class emptyEvent extends EventException{
+    public void errorMessage(){
+        System.out.println("Ma G! You got no description for event. Please re-add event with description.");
+    }
+}
