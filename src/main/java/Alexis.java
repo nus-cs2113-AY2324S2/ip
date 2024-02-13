5fea8379535d6877fd48be94f9d386345cdb9106
imanamirshah <iman.amirshah@gmail.com>
2024-02-13 12:50:05 +0800

    refactor: Implement switch statement to replace conditionals
    • Abstracted the processing of user inputs to Console class
    • Utilised switch statements for improved readability
    • Extracted methods within switch statements to improve readability

--- src/main/java/Alexis.java
+++ src/main/java/Alexis.java
@@ -6,43 +6,7 @@ public class Alexis {
         Scanner in = new Scanner(System.in);
 
         Console.printWelcomeMessage();
-
-        while (true) {
-            String line = in.nextLine();
-
-            if (line.equals("bye")) {
-                break;
-            } else if (line.equals("list")) {
-                System.out.println(Console.LINEBREAK);
-                tasks.printTasks();
-                System.out.println(Console.LINEBREAK);
-            } else {
-                System.out.println(Console.LINEBREAK);
-                if (line.startsWith("mark")) {
-                    tasks.markTask(line);
-                } else if (line.startsWith("unmark")) {
-                    tasks.unmarkTask(line);
-                } else {
-                    int firstSpace = line.indexOf(" ");
-                    String firstWord = line.substring(0, firstSpace);
-                    String description = line.substring(firstSpace);
-                    TaskType taskType = TaskType.getTaskType(firstWord);
-                    switch (taskType) {
-                    case TODO:
-                    default:
-                        tasks.addTask(TaskType.TODO, description);
-                        break;
-                    case DEADLINE:
-                        tasks.addTask(TaskType.DEADLINE, description);
-                        break;
-                    case EVENT:
-                        tasks.addTask(TaskType.EVENT, description);
-                        break;
-                    }
-                }
-                System.out.println(Console.LINEBREAK);
-            }
-        }
+        Console.processUserInput(tasks, in);
         Console.printGoodbyeMessage();
     }
 }
--- src/main/java/Console.java
+++ src/main/java/Console.java
@@ -1,3 +1,6 @@
+import javax.swing.undo.UndoManager;
+import java.util.Scanner;
+
 public class Console {
     private final static String NAME = "   ('-.                 ('-.  ) (`-.               .-')    \n"
             + "  ( OO ).-.           _(  OO)  ( OO ).            ( OO ).  \n"
@@ -18,6 +21,61 @@ public class Console {
         System.out.println(LINEBREAK);
         System.out.println(GREETING);
         System.out.println(LINEBREAK);
+    }
+
+    public static void processUserInput(TaskList tasks, Scanner in) {
+        while (true) {
+            String line = in.nextLine();
+            Command command = Command.getFirstWord(line);
+            String description = Command.getDescription(line);
+
+            switch (command) {
+            case BYE:
+                return;
+            case LIST:
+                printTasksToConsole(tasks);
+                break;
+            case MARK:
+                printMarkedItemToConsole(tasks, line);
+                break;
+            case UNMARK:
+                printUnmarkedItemToConsole(tasks, line);
+                break;
+            case TODO:
+                printTaskToConsole(tasks, TaskType.TODO, description);
+                break;
+            case DEADLINE:
+                printTaskToConsole(tasks, TaskType.DEADLINE, description);
+                break;
+            case EVENT:
+                printTaskToConsole(tasks, TaskType.EVENT, description);
+                break;
+            }
+        }
+    }
+
+    private static void printTasksToConsole(TaskList tasks) {
+        System.out.println(Console.LINEBREAK);
+        tasks.printTasks();
+        System.out.println(Console.LINEBREAK);
+    }
+
+    private static void printMarkedItemToConsole(TaskList tasks, String line) {
+        System.out.println(Console.LINEBREAK);
+        tasks.markTask(line);
+        System.out.println(Console.LINEBREAK);
+    }
+
+    private static void printUnmarkedItemToConsole(TaskList tasks, String line) {
+        System.out.println(Console.LINEBREAK);
+        tasks.unmarkTask(line);
+        System.out.println(Console.LINEBREAK);
+    }
+
+    private static void printTaskToConsole(TaskList tasks, TaskType todo, String description) {
+        System.out.println(Console.LINEBREAK);
+        tasks.addTask(todo, description);
+        System.out.println(Console.LINEBREAK);
     }
 
     public static void printGoodbyeMessage() {
