a6bdb0aa1ba1d8b98e49ae750924b82a572c6896
nikhil-2101 <nikhilaaditya.babu@gmail.com>
2024-02-13 16:32:02 +0800

    Level 5: Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import java.util.ArrayList;
+
 public class Duke {
     public static void main(String[] args) {
         System.out.println("____________________________________________________________");
@@ -11,100 +11,101 @@ public class Duke {
         Task[] tasks = new Task[100];
         int taskCount = 0;
 
-
         while (true) {
-            String userInput = scanner.nextLine();
-            if ("bye".equalsIgnoreCase(userInput)) {
-                break; // Exit the loop if the user types "bye"
-            }
+            try {
+                String userInput = scanner.nextLine();
 
-            if ("list".equalsIgnoreCase(userInput)) {
-                System.out.println("____________________________________________________________");
-                System.out.println("Here are the tasks in your list:");
-                for (int i = 0; i < taskCount; i++) {
-                    Task task = tasks[i];
-                    System.out.println((i + 1) + "." + task);
-                }
-                System.out.println("____________________________________________________________");
-            } else if (userInput.startsWith("mark ")) {
-                processTask(userInput, tasks, true, taskCount);
-            } else if (userInput.startsWith("unmark ")) {
-                processTask(userInput, tasks, false, taskCount);
-            }else if (userInput.startsWith("todo ")) {
-                if (taskCount >= 100) {
-                    System.out.println("You have reached the task limit. Cannot add more tasks!");
-                    continue;
+                if ("bye".equalsIgnoreCase(userInput)) {
+                    break;
+
                 }
 
-                String description = userInput.substring(5).trim();
-                Todo newTodo = new Todo(description);
-                tasks[taskCount] = newTodo;
-                taskCount++;
+                if ("list".equalsIgnoreCase(userInput)) {
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Here are the tasks in your list:");
+                    for (int i = 0; i < taskCount; i++) {
+                        Task task = tasks[i];
+                        System.out.println((i + 1) + "." + task);
+                    }
+                    System.out.println("____________________________________________________________");
+                } else if (userInput.startsWith("mark")) {
+                    processTask(userInput, tasks, true, taskCount);
+                } else if (userInput.startsWith("unmark")) {
+                    processTask(userInput, tasks, false, taskCount);
+                } else if (userInput.startsWith("todo")) {
+                    if (taskCount >= 100) {
+                        throw new DukeException("You have reached the task limit. Cannot add more tasks!");
+                    }
 
-                System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + newTodo);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
-                System.out.println("____________________________________________________________");
-            } else if (userInput.startsWith("deadline ")) {
-                if (taskCount >= 100) {
-                    System.out.println("You have reached the task limit. Cannot add more tasks!");
-                    continue;
-                }
+                    String description = userInput.substring(4).trim();
 
-                String content = userInput.substring(9).trim();
-                int index = content.indexOf("/by");
+                    if (description.isEmpty()) {
+                        throw new DukeException("OOPSSSSSSS!!! The task's description is empty! Please do specify!");
+                    }
 
-                if (index != -1) {
-                    System.out.println("Please use '/by' to specify the deadline time.");
-                    String description = content.substring(0, index).trim();
-                    String by = content.substring(index + 4).trim();
-                    Deadline newDeadline = new Deadline(description, by);
-                    tasks[taskCount] = newDeadline;
+                    Todo newTodo = new Todo(description);
+                    tasks[taskCount] = newTodo;
                     taskCount++;
+
                     System.out.println("____________________________________________________________");
                     System.out.println("Got it. I've added this task:");
-                    System.out.println("  " + newDeadline);
+                    System.out.println("  " + newTodo);
                     System.out.println("Now you have " + taskCount + " tasks in the list.");
                     System.out.println("____________________________________________________________");
+                } else if (userInput.startsWith("deadline")) {
+                    if (taskCount >= 100) {
+                        throw new DukeException("You have reached the task limit. Cannot add more tasks!");
+                    }
 
+                    String content = userInput.substring(8).trim();
+                    int index = content.indexOf("/by");
+                    if (index == -1) {
+                        throw new DukeException("Please use '/by' to specify the deadline time.");
+                    } else {
+                        String description = content.substring(0, index).trim();
+                        String by = content.substring(index + 4).trim();
+                        Deadline newDeadline = new Deadline(description, by);
+                        tasks[taskCount] = newDeadline;
+                        taskCount++;
+                        System.out.println("____________________________________________________________");
+                        System.out.println("Got it. I've added this task:");
+                        System.out.println("  " + newDeadline);
+                        System.out.println("Now you have " + taskCount + " tasks in the list.");
+                        System.out.println("____________________________________________________________");
+                    }
+                } else if (userInput.startsWith("event")) {
+                    if (taskCount >= 100) {
+                        throw new DukeException("You have reached the task limit. Cannot add more tasks!");
+                    }
+
+                    String content = userInput.substring(5).trim();
+
+                    String[] parts = content.split("/from | /to ");
+
+                    if (parts.length < 3) {
+                        throw new DukeException("Please use the format: event [description] /from [start time] /to [end time]");
+                    }
+
+                    String description = parts[0].trim();
+                    String from = parts[1].trim();
+                    String to = parts[2].trim();
+
+                    Event newEvent = new Event(description, from, to);
+                    tasks[taskCount] = newEvent;
+                    taskCount++;
+
+                    System.out.println("____________________________________________________________");
+                    System.out.println("Got it. I've added this task:");
+                    System.out.println("  " + newEvent);
+                    System.out.println("Now you have " + taskCount + " tasks in the list.");
+                    System.out.println("____________________________________________________________");
                 } else {
-                    System.out.println("Please use '/by' to specify the deadline time.");
+                    throw new DukeException("OOPSSSSSSS!!! What is that? Please type in ? :(");
                 }
-            } else if (userInput.startsWith("event ")) {
-                if (taskCount >= 100) {
-                    System.out.println("You have reached the task limit. Cannot add more tasks!");
-                    continue;
-                }
-
-                String content = userInput.substring(6).trim();
-
-                String[] parts = content.split("/from | /to ");
-
-                if (parts.length < 3) {
-                    System.out.println("Please use the format: event [description] /from [start time] /to [end time]");
-                    continue;
-                }
-
-                String description = parts[0].trim();
-                String from = parts[1].trim();
-                String to = parts[2].trim();
-
-                Event newEvent = new Event(description, from, to);
-                tasks[taskCount] = newEvent;
-                taskCount++;
-
+            } catch (DukeException de) {
                 System.out.println("____________________________________________________________");
-                System.out.println("Got it. I've added this task:");
-                System.out.println("  " + newEvent);
-                System.out.println("Now you have " + taskCount + " tasks in the list.");
+                System.out.println(de.getMessage());
                 System.out.println("____________________________________________________________");
-            }else {
-                if (taskCount >= 100) {
-                    System.out.println("You have reached the task limit. Cannot add more tasks!");
-                    continue;
-                }
-
             }
         }
 
@@ -112,15 +113,13 @@ public class Duke {
         System.out.println("Bye. Hope to see you again soon!");
         System.out.println("____________________________________________________________");
     }
-
     public static void processTask(String userInput, Task[] tasks, boolean mark, int taskCount) {
         int taskNumber;
         try {
             taskNumber = Integer.parseInt(userInput.split(" ")[1]);
-            if (taskNumber > taskCount || taskNumber <= 0) {
+            if (taskNumber <= 0 || taskNumber > taskCount) {
                 throw new IndexOutOfBoundsException();
             }
-
             Task task = tasks[taskNumber - 1];
             if (mark) {
                 task.markAsDone();
