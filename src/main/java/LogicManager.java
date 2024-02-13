b050fa588a8ef3d31370f69941966b36321fe30e
STeng618 <shantengyap@gmail.com>
2024-02-13 22:53:04 +0800

    Update LogicManager

--- src/main/java/LogicManager.java
+++ src/main/java/LogicManager.java
@@ -5,168 +5,160 @@ public class LogicManager {
         this.listKeeper = listKeeper;
     }
 
-    private Command getInvalidCommand() {
-        return new Command(CommandType.INVALID_COMMAND);
+    private void executeList(String[] words) throws IllegalNumberOfArguments{
+        if (words.length != 1) {
+            throw new IllegalNumberOfArguments();
+        }
+        this.listKeeper.printList();
     }
 
-    private Command getListCommand(String[] words) {
-        boolean hasUnnecessaryArguments = words.length > 1;
-        if (hasUnnecessaryArguments) {
-            return getInvalidCommand();
-        }
-        return new Command(CommandType.LIST);
-    }
+    private void executeMark (String[] words)
+        throws IllegalNumberOfArguments, InvalidTaskIndex {
 
-    private boolean canStringBeParsedAsInt(String taskIndexString) {
-        try {
-            Integer.parseInt(taskIndexString);
-            return true;
-        } catch (Exception exception) {
-            return false;
-        }
-    }
-    private boolean isValidArgument(String taskIntegerString) {
-        if (!canStringBeParsedAsInt(taskIntegerString)) {
-            return false;
-        }
-        int taskIndex = Integer.parseInt(taskIntegerString);
-        return this.listKeeper.isValidTaskIndex(taskIndex);
-    }
-    private Command getMarkUnmarkCommand(String[] words) {
         if (words.length != 2) {
-            return getInvalidCommand();
+            throw new IllegalNumberOfArguments();
         }
         String taskIndexString = words[1];
-        if (!isValidArgument(taskIndexString)) {
-            return getInvalidCommand();
-        }
 
-        int taskIndex = Integer.parseInt(taskIndexString);
-        MarkUnmarkArguments markUnmarkArguments = new MarkUnmarkArguments(taskIndex);
-        if (words[0].equals("mark")) {
-            return new Command(CommandType.MARK, markUnmarkArguments);
-        }
-        return new Command(CommandType.UNMARK, markUnmarkArguments);
-    }
-
-    private Command getToDoCommand(String currentInput) {
-        // Extract after _todo_, which is 4 characters
-        String taskName = currentInput.substring(4);
-        taskName = taskName.trim();
-        if (taskName.isEmpty()) {
-            return getInvalidCommand();
-        }
-        return new Command(CommandType.TODO, new ToDoArguments(taskName));
-    }
-    private Command getDeadlineCommand(String currentInput) {
+        int taskIndex;
         try {
-            int idxOfDeadline = currentInput.indexOf("/");
+            taskIndex = Integer.parseInt(taskIndexString);
+        } catch (NumberFormatException e) {
+            throw new InvalidTaskIndex();
+        }
+
+        if (!this.listKeeper.isValidTaskIndex(taskIndex)) {
+            throw new InvalidTaskIndex();
+        }
+
+        boolean isCompleted = words[0].equals("mark");
+        this.listKeeper.processMark(taskIndex, isCompleted);
+    }
+
+    private void executeToDo(String currentInput)
+        throws EmptyTaskDescription, InvalidTaskArguments {
+        try {
+            // Extract after _todo_, which is 4 characters long
+            String taskName = currentInput.substring(4);
+            taskName = taskName.trim();
+            if (taskName.isEmpty()) {
+                throw new EmptyTaskDescription();
+            }
+            this.listKeeper.addToList(new ToDo(taskName));
+
+        } catch (IndexOutOfBoundsException e) {
+            throw new InvalidTaskArguments();
+        }
+
+    }
+
+    private void executeDeadline(String currentInput)
+        throws EmptyTaskDescription, InvalidTaskArguments {
+        try {
+            int idxOfDeadline = currentInput.indexOf(" /by ");
+            if (idxOfDeadline == -1) {
+                throw new InvalidTaskArguments();
+            }
 
             // Extract after _deadline_, which is 8 characters long
             String taskName = currentInput.substring(8, idxOfDeadline);
             taskName = taskName.trim();
             if (taskName.isEmpty()) {
-                return getInvalidCommand();
+                throw new EmptyTaskDescription();
             }
 
-            // We extract the deadline after _/by _, which is 4 characters long
-            String by = currentInput.substring(idxOfDeadline + 4);
+            // Extract after _ /by _, which is 5 characters long
+            String by = currentInput.substring(idxOfDeadline + 5);
             by = by.trim();
 
-            DeadLineArguments deadLineArguments = new DeadLineArguments(taskName, by);
-            return new Command(CommandType.DEADLINE, deadLineArguments);
-        } catch (Exception exception) {
-            return getInvalidCommand();
+            this.listKeeper.addToList(new Deadline(taskName, by));
+
+        } catch (IndexOutOfBoundsException e) {
+            throw new InvalidTaskArguments();
         }
+
     }
-    private Command getEventCommand(String currentInput) {
+
+    private void executeEvent(String currentInput)
+        throws EmptyTaskDescription, InvalidTaskArguments {
         try {
-            int idxOfStart = currentInput.indexOf("/");
+            int idxOfStart = currentInput.indexOf(" /from ");
+            int idxOfEnd = currentInput.indexOf(" /to ");
+
+            if (idxOfStart == -1 || idxOfEnd == -1) {
+                throw new InvalidTaskArguments();
+            }
+
             // Extract after _event_, which is 5 characters long
             String taskName = currentInput.substring(5, idxOfStart);
             taskName = taskName.trim();
             if (taskName.isEmpty()) {
-                return getInvalidCommand();
+                throw new EmptyTaskDescription();
             }
 
-            int idxOfEnd = currentInput.indexOf("/", idxOfStart + 1);
-            // Extract start after _/from_, which is 5 characters long, and before idxOfEnd
-            String start = currentInput.substring(idxOfStart + 5, idxOfEnd);
+            // Extract start after _ /from _, which is 7 characters long, and before idxOfEnd
+            String start = currentInput.substring(idxOfStart + 7, idxOfEnd);
             start = start.trim();
 
-            // Extract end after _/to_, which is 3 characters long
-            String end = currentInput.substring(idxOfEnd + 3);
+            // Extract end after _ /to _, which is 5 characters long
+            String end = currentInput.substring(idxOfEnd + 5);
             end = end.trim();
 
-            EventArguments eventArguments = new EventArguments(taskName, start, end);
-            return new Command(CommandType.EVENT, eventArguments);
-        } catch (Exception exception) {
-            return getInvalidCommand();
+            this.listKeeper.addToList(new Event(taskName, start, end));
+
+        } catch (IndexOutOfBoundsException e) {
+            throw new InvalidTaskArguments();
         }
+
     }
 
-    private Command getCommand(String currentInput) {
+    private void executeCommand (String currentInput)
+        throws IllegalNumberOfArguments, InvalidTaskIndex,
+            EmptyTaskDescription, InvalidTaskArguments, Confusion{
+
         String[] words = currentInput.split(" ");
         String commandType = words[0];
-        if (commandType.equals("list")) {
-            return getListCommand(words);
-        }
-        if (commandType.equals("mark") || commandType.equals("unmark")) {
-            return getMarkUnmarkCommand(words);
-        }
-        if (commandType.equals("todo")) {
-            return getToDoCommand(currentInput);
-        }
-        if (commandType.equals("deadline")) {
-            return getDeadlineCommand(currentInput);
-        }
-        if (commandType.equals("event")) {
-            return getEventCommand(currentInput);
-        }
-        return getInvalidCommand();
-    }
 
-    private void printErrorMessage() {
-        System.out.println("I'm very confused~~~~");
+        switch (commandType) {
+        case "list":
+            executeList(words);
+            break;
+
+        case "mark":
+        case "unmark":
+            executeMark(words);
+            break;
+
+        case "todo":
+            executeToDo(currentInput);
+            break;
+
+        case "deadline":
+            executeDeadline(currentInput);
+            break;
+
+        case "event":
+            executeEvent(currentInput);
+            break;
+
+        default:
+            throw new Confusion();
+        }
     }
 
     public void processCommand(String currentInput) {
-        Command command = getCommand(currentInput);
-        switch (command.commandType) {
-        case LIST:
-            listKeeper.printList();
-            break;
-
-        case MARK:
-        case UNMARK:
-            MarkUnmarkArguments markUnmarkArguments = (MarkUnmarkArguments)(command.commandArguments);
-            int taskIndex = markUnmarkArguments.index;
-            if (command.commandType == CommandType.MARK) {
-                listKeeper.processMark(taskIndex);
-            } else {
-                listKeeper.processUnmark(taskIndex);
-            }
-            break;
-
-        case TODO:
-            ToDoArguments toDoArguments = (ToDoArguments)(command.commandArguments);
-            listKeeper.addToList(new ToDo(toDoArguments));
-            break;
-
-        case DEADLINE:
-            DeadLineArguments deadLineArguments = (DeadLineArguments)(command.commandArguments);
-            listKeeper.addToList(new Deadline(deadLineArguments));
-            break;
-
-        case EVENT:
-            EventArguments eventArguments = (EventArguments)(command.commandArguments);
-            listKeeper.addToList(new Event(eventArguments));
-            break;
-
-        case INVALID_COMMAND:
-        default:
-            printErrorMessage();
-            break;
+        try {
+            executeCommand(currentInput);
+        } catch (IllegalNumberOfArguments e) {
+            System.out.println("You provided too few or too many arguments");
+        } catch (InvalidTaskIndex e) {
+            System.out.println("Task specified does not exist");
+        } catch (EmptyTaskDescription e) {
+            System.out.println("Task description cannot be empty");
+        } catch (InvalidTaskArguments e) {
+            System.out.println("Not a valid task");
+        } catch (Confusion e) {
+            System.out.println("I'm very very confused~~~");
         }
     }
 }
