ad3e0577b379480753a43c38200dfd538e273add
Joshuahoky <joshuaho2002@gmail.com>
2024-02-13 20:42:29 +0800

    Refactor Parser class code for easier readability

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -7,25 +7,16 @@ public class Parser {
     public static final String EVENT_COMMAND = "event";
     public static final String MARK_COMMAND = "mark";
     public static final String UNMARK_COMMAND = "unmark";
-
     public static void parseInput(String input, TaskList tasks) {
         String[] inputArray = input.split(" ", 2);
         if (input.equals(LIST_COMMAND)) {
             tasks.displayTasksList();
         } else if (input.startsWith(TODO_COMMAND)) {
-            tasks.addTask(new ToDo(inputArray[1]));
+            tasks.addTask(TODO_COMMAND, inputArray[1]);
         } else if (input.startsWith(DEADLINE_COMMAND)) {
-            int dividerPosition = inputArray[1].indexOf('/');
-            String description = inputArray[1].substring(0, dividerPosition);
-            String deadline = inputArray[1].substring(dividerPosition + 4);
-            tasks.addTask(new Deadline(description, deadline));
+            tasks.addTask(DEADLINE_COMMAND, inputArray[1]);
         } else if (input.startsWith(EVENT_COMMAND)) {
-            int firstDividerPosition = inputArray[1].indexOf('/');
-            int secondDividerPosition = inputArray[1].indexOf("to");
-            String description = inputArray[1].substring(0, firstDividerPosition);
-            String eventStart = inputArray[1].substring(firstDividerPosition + 6, secondDividerPosition - 1);
-            String eventEnd = inputArray[1].substring(secondDividerPosition + 3);
-            tasks.addTask(new Event(description, eventStart, eventEnd));
+            tasks.addTask(EVENT_COMMAND, inputArray[1]);
         } else if (input.startsWith(MARK_COMMAND)) {
             int taskIndex = Integer.parseInt(inputArray[1]) - 1;
             tasks.markTask(taskIndex);
@@ -33,7 +24,7 @@ public class Parser {
             int taskIndex = Integer.parseInt(inputArray[1]) - 1;
             tasks.unmarkTask(taskIndex);
         } else {
-            System.out.println("Sorry but that is not a valid command. Please enter a valid command");
+            System.out.println("Sorry but that is not a valid command. Please enter a valid command.");
         }
     }
 }
--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -24,19 +24,53 @@ public class TaskList {
     }
 
     public void markTask(int taskIndex) {
-        this.tasks.get(taskIndex).markAsDone();
+        if (taskIndex < 0 || taskIndex > taskCount) {
+            System.out.println("Invalid task number. Please enter a valid task number");
+        } else {
+            this.tasks.get(taskIndex).markAsDone();
+        }
     }
 
     public void unmarkTask(int taskIndex) {
-        this.tasks.get(taskIndex).unmarkAsDone();
+        if (taskIndex < 0 || taskIndex > taskCount) {
+            System.out.println("Invalid task number. Please enter a valid task number");
+        } else {
+            this.tasks.get(taskIndex).unmarkAsDone();
+        }
     }
 
-    public void addTask(Task task) {
-        tasks.add(task);
+    public void addTask(String taskType, String taskDetails) {
+        if (taskType.equals(Parser.TODO_COMMAND)) {
+            handleToDo(taskDetails);
+        } else if (taskType.equals(Parser.DEADLINE_COMMAND)) {
+            handleDeadline(taskDetails);
+        } else {
+            handleEvent(taskDetails);
+        }
         taskCount += 1;
         System.out.println("\tGot it. I've added this task:");
         System.out.println("\t\t" + tasks.get(taskCount - 1).getTaskDetails());
         System.out.println("\tNow you have " + taskCount
                 + (taskCount == 1 ? " task" : " tasks") + " in the list.\n");
+    }
+
+    public void handleToDo(String taskDetails) {
+        tasks.add(new ToDo(taskDetails));
+    }
+
+    public void handleDeadline(String taskDetails) {
+        int dividerPosition = taskDetails.indexOf('/');
+        String description = taskDetails.substring(0, dividerPosition);
+        String deadline = taskDetails.substring(dividerPosition + 4);
+        tasks.add(new Deadline(description, deadline));
+    }
+
+    public void handleEvent(String taskDetails) {
+        int firstDividerPosition = taskDetails.indexOf('/');
+        int secondDividerPosition = taskDetails.indexOf("to");
+        String description = taskDetails.substring(0, firstDividerPosition);
+        String eventStart = taskDetails.substring(firstDividerPosition + 6, secondDividerPosition - 1);
+        String eventEnd = taskDetails.substring(secondDividerPosition + 3);
+        tasks.add(new Event(description, eventStart, eventEnd));
     }
 }
