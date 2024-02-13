e2d3dac1336d8b178995158fc22943b787b74ab9
共和七十三年壬寅七月廿三制\Si Zhe <shaosizhe@hotmail.com>
2024-02-13 21:50:02 +0800

    Added exceptions handling for Week 5.

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -3,7 +3,7 @@ public class Deadline extends Task {
     public Deadline(String input) {
         super(input);
         String[] parts = input.split(" /by ", 2);
-        this.description = parts[0].substring(9).trim(); // Remove "deadline" command and trim spaces
+        this.description = parts[0].substring(9).trim(); //Remove deadline command and trim spaces.
         this.by = parts[1];
         this.eventType = "[D]";
     }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,44 +1,45 @@
 import java.util.Scanner;
+
 public class Duke {
     static String BREAK_LINE = "____________________________________________________________";
 
     public static void main(String[] args) {
 
-        System.out.println(BREAK_LINE + "\nHello! I'm Jeremy.\nWhat can I do for you?\n" + BREAK_LINE);
-        Task[] tasks = new Task[100]; //To store the list of tasks inputted.
+        System.out.println(BREAK_LINE + "\nHello! I'm 550W.\nWhat can I do for you?\n" + BREAK_LINE);
+        Task[] tasks = new Task[100];
         int count = 0;
         Scanner in = new Scanner(System.in);
-        String line;
+        String line = "";
 
-        do {
+        while (true) {
+            try {
+                line = in.nextLine();
+                System.out.println(BREAK_LINE);
 
-            line = in.nextLine(); //String Line that reads in the next line of input.
-            System.out.println(BREAK_LINE);
-
-            if (!line.equals("bye")) {
+                if (line.equals("bye")) {
+                    break; //Break out of the loop immediately when bye command is entered, quitting the programme.
+                }
 
                 if (line.equals("list")) {
 
-                    System.out.println("Here are the tasks in your list:");
+                    System.out.println("Abracadabra! \uD83C\uDF1F Here are the tasks in your list:");
                     for (int i = 0; i < count; i++) {
-
                         System.out.println((i + 1) + "." + tasks[i].toString());
                     }
-
                     System.out.println(BREAK_LINE);
 
                 } else if (line.startsWith("unmark")) {
 
                     int taskMarkNumber = Integer.parseInt(line.split(" ")[1]) - 1;
                     tasks[taskMarkNumber].markAsNotDone();
-                    System.out.println("OK, I've marked this task as not done yet:");
+                    System.out.println("All good! We've hit the rewind button and unmarked this task \uD83D\uDD04:");
                     System.out.println("  " + tasks[taskMarkNumber] + "\n" + BREAK_LINE);
 
                 } else if (line.startsWith("mark")) {
 
                     int taskMarkNumber = Integer.parseInt(line.split(" ")[1]) - 1;
                     tasks[taskMarkNumber].markAsDone();
-                    System.out.println("Nice! I've marked this task as done:");
+                    System.out.println("X marks the spot! \uD83C\uDF89 I've marked this task as done:");
                     System.out.println("  " + tasks[taskMarkNumber] + "\n" + BREAK_LINE);
 
                 } else if (line.startsWith("todo")) {
@@ -58,11 +59,18 @@ public class Duke {
                     tasks[count] = new Event(line);
                     tasks[count].print(count);
                     count++;
+
+                } else {
+
+                    System.out.println("Oops! I've no clue what that means. Could you enlighten me, please? \uD83E\uDD16\uD83D\uDCA1");
+                    System.out.println(BREAK_LINE);
                 }
+            } catch (Exception error) {
+
+                DukeException.handleException(error, line);
             }
-        } while (!line.equals("bye"));
+        }
 
-        System.out.println("Bye. Hope to see you again soon!\n" + BREAK_LINE);
-
+        System.out.println("Bye. Hope to see you again soon! \uD83D\uDE0A\n" + BREAK_LINE);
     }
 }
