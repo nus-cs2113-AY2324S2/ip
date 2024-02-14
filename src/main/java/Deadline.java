23e6356cdc24186596f0b840cfd43e88d4dc369b
LIN Siyan <slinbh@connect.ust.hk>
2024-02-15 03:06:37 +0800

    level 5: Handle error

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -2,7 +2,7 @@ public class Deadline extends Task{
 
     protected String by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by){
         super(description);
         this.by = by.split(" ", 2)[1];
     }
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -38,14 +38,38 @@ public class Duke {
                 }
             } else {
                 if (userInput.contains("deadline")) {
-                    String descriptionAndBy = userInput.split(" ", 2)[1];
-                    String Description = descriptionAndBy.split("/")[0];
-                    String By = descriptionAndBy.split("/")[1];
-                    tasks[Task.numOfTask] = new Deadline(Description, By);
+                    try {
+                        String descriptionAndBy = userInput.split(" ", 2)[1];
+                        try {
+                            String Description = descriptionAndBy.split("/")[0];
+                            String By = descriptionAndBy.split("/")[1];
+                            tasks[Task.numOfTask] = new Deadline(Description, By);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("OOPS!!! The by of a deadline cannot be empty.");
+                            System.out.println("____________________________________________________________\n");
+                            userInput = scanner.nextLine();
+                            userInput = userInput.toLowerCase();
+                            continue;
+                        }
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("OOPS!!! The description and by of a deadline cannot be empty.");
+                        System.out.println("____________________________________________________________\n");
+                        userInput = scanner.nextLine();
+                        userInput = userInput.toLowerCase();
+                        continue;
+                    }
                 }
                 else if (userInput.contains("todo")) {
-                    String Description = userInput.split(" ", 2)[1];
-                    tasks[Task.numOfTask] = new ToDo(Description);
+                    try {
+                        String Description = userInput.split(" ", 2)[1];
+                        tasks[Task.numOfTask] = new ToDo(Description);
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        System.out.println("OOPS!!! The description of a todo cannot be empty.");
+                        System.out.println("____________________________________________________________\n");
+                        userInput = scanner.nextLine();
+                        userInput = userInput.toLowerCase();
+                        continue;
+                    }
                 }
                 else if (userInput.contains("event")) {
                     String descriptionAndRange = userInput.split(" ", 2)[1];
@@ -53,6 +77,14 @@ public class Duke {
                     String from = descriptionAndRange.split("/",3)[1];
                     String to = descriptionAndRange.split("/", 3)[2];
                     tasks[Task.numOfTask] = new Event(Description, from, to);
+                }
+                else {
+                    System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
+                    System.out.println("____________________________________________________________\n");
+                    userInput = scanner.nextLine();
+                    userInput = userInput.toLowerCase();
+                    continue;
+
                 }
                 System.out.println("Got it. I've added this task:");
                 System.out.println(tasks[Task.numOfTask - 1]);
