56965a30937b3440de29fd1de3aa5dc1a01b00b6
leongxingyu <leongxingyu02@gmail.com>
2024-02-14 16:12:45 +0800

    Remove command and exception packages

--- src/main/java/ByeCommand.java
+++
@@ -1,8 +0,0 @@
-public class ByeCommand implements Command {
-
-    @Override
-    public void execute (String input, Task[] taskList) {
-        Gab.isExit = true;
-        System.out.println("All the best!");
-    }
-}
