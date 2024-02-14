2dd8f69cf689bec92796227889ade281a17b45d9
kyuichyi <chongkaitlyn@u.nus.edu>
2024-02-15 00:21:34 +0800

    Level-5: chatbot deal with errors such as incorrect inputs entered by the user

--- src/main/java/Bobble.java
+++ src/main/java/Bobble.java
@@ -1,7 +1,9 @@
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class Bobble {
+
+    public static final String LINE_WRAP = "____________________________________________________________\n";
+
     public static void main(String[] args) {
         start();
         Scanner input = new Scanner(System.in);
@@ -11,41 +13,52 @@ public class Bobble {
         String command;
 
         while (!userInput.equals("bye")) {
-            String[] UserInputs = splitUserInput(userInput);
+            String[] UserInputs = getCommandAndDesc(userInput);
             command = UserInputs[0];
-            switch (command) {
-            case "list":
-                listResponse(tasklist, taskCount);
-                break;
-            case "todo":
-                tasklist[taskCount] = new ToDo(UserInputs[1]);
-                addTaskResponse(tasklist[taskCount], taskCount);
-                taskCount++;
-                break;
-            case "deadline":
-                String[] parts = UserInputs[1].split("/by");
-                tasklist[taskCount] = new Deadline(parts[0], parts[1]);
-                addTaskResponse(tasklist[taskCount],taskCount);
-                taskCount++;
-                break;
-            case "event":
-                parts = UserInputs[1].split("/from");
-                String[] duration = parts[1].split("/to");
-                tasklist[taskCount] = new Event(parts[0], duration[0], duration[1]);
-                addTaskResponse(tasklist[taskCount], taskCount);
-                taskCount++;
-                break;
-            case "mark":
-                int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
-                tasklist[taskNumber].setDone(true);
-                markResponse(tasklist[taskNumber]);
-                break;
-            case "unmark":
-                taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
-                tasklist[taskNumber].setDone(false);
-                unmarkResponse(tasklist[taskNumber]);
-                break;
-            default:
+            try {
+                switch (command) {
+                case "list":
+                    listResponse(tasklist, taskCount);
+                    break;
+                case "todo":
+                    tasklist[taskCount] = new ToDo(UserInputs[1]);
+                    addTaskResponse(tasklist[taskCount], taskCount);
+                    taskCount++;
+                    break;
+                case "deadline":
+                    String[] parts = UserInputs[1].split("/by");
+                    tasklist[taskCount] = new Deadline(parts[0], parts[1]);
+                    addTaskResponse(tasklist[taskCount], taskCount);
+                    taskCount++;
+                    break;
+                case "event":
+                    parts = UserInputs[1].split("/from");
+                    String[] duration = parts[1].split("/to");
+                    tasklist[taskCount] = new Event(parts[0], duration[0], duration[1]);
+                    addTaskResponse(tasklist[taskCount], taskCount);
+                    taskCount++;
+                    break;
+                case "mark":
+                    int taskNumber = Integer.parseInt(userInput.substring(5)) - 1;
+                    tasklist[taskNumber].setDone(true);
+                    markResponse(tasklist[taskNumber]);
+                    break;
+                case "unmark":
+                    taskNumber = Integer.parseInt(userInput.substring(7)) - 1;
+                    tasklist[taskNumber].setDone(false);
+                    unmarkResponse(tasklist[taskNumber]);
+                    break;
+                default:
+                    throw new DukeExceptionCommand();
+                }
+            } catch (DukeExceptionCommand e) {
+                System.out.println(LINE_WRAP +
+                        "OOPS!! I'm sorry, but I don't know what that means :-(\n" +
+                        LINE_WRAP);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(LINE_WRAP +
+                        "OOPS!! One or more fields of a(n) " + command + " cannot be empty.\n" +
+                        LINE_WRAP);
             }
             userInput = input.nextLine();
         }
@@ -53,52 +66,48 @@ public class Bobble {
     }
 
     private static void listResponse(Task[] tasklist,int taskCount) {
-        System.out.println("____________________________________________________________\n" +
-                "Here are the tasks in your list:");
+        System.out.println(LINE_WRAP + "Here are the tasks in your list:");
         for (int i = 0; i < taskCount; i++) {
             System.out.println((i + 1) + "." + tasklist[i].toString());
         }
-        System.out.println("____________________________________________________________\n");
+        System.out.println(LINE_WRAP);
     }
 
     private static void markResponse(Task tasklist) {
         System.out.println("Nice! I've marked this task as done:\n" +
-                tasklist.toString() +
-                "\n____________________________________________________________\n");
+                tasklist.toString() + "\n" + LINE_WRAP);
     }
 
     private static void unmarkResponse(Task task) {
         System.out.println("OK, I've marked this task as not done yet:\n" +
-                task.toString() +
-        "\n____________________________________________________________\n");
+                task.toString() + "\n" + LINE_WRAP);
     }
 
-    public static String[] splitUserInput(String input) {
+    public static String[] getCommandAndDesc(String input) {
         String[] userInput = input.split(" ", 2);
         return userInput;
     }
 
     public static void addTaskResponse(Task task, int taskCount) {
-        System.out.println("____________________________________________________________\n" +
+        System.out.println(LINE_WRAP +
                 "Got it. I've added this task: \n  " +
                 task.toString() +
-                "\nNow you have " + (taskCount + 1) + " task(s) in the list." +
-                "\n____________________________________________________________\n");
+                "\nNow you have " + (taskCount + 1) + " task(s) in the list.\n" +
+                LINE_WRAP);
     }
 
     //Greets user
     public static void start() {
-        System.out.println("____________________________________________________________\n" +
+        System.out.println(LINE_WRAP +
                 "Hello! I'm Bobble.\n" +
-                "What can I do for you?" +
-                "\n____________________________________________________________\n");
+                "What can I do for you?\n" +
+                LINE_WRAP);
     }
 
     //Exits
     public static void goodbye() {
-        System.out.println("____________________________________________________________\n" +
-                "Bye. Hope to see you again soon!\n" +
-                "____________________________________________________________\n");
+        System.out.println(LINE_WRAP +
+                "Bye. Hope to see you again soon!\n" + LINE_WRAP);
     }
 
 }
