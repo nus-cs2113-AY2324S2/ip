b8325175200aaa011be398891eaf486faf4e1cd9
Fureimi <e0957998@u.nus.edu>
2024-02-14 20:25:16 +0800

    Level 4

--- src/main/java/GermaBot.java
+++ src/main/java/GermaBot.java
@@ -1,8 +1,61 @@
 import java.util.Scanner;
 
 public class GermaBot {
+    static int counter = 0;
+    static Task[] toDoList = new Task[100];
     public static int getIdx(String input) {
         return Integer.parseInt(input.substring(input.indexOf(" ") + 1)) - 1;
+    }
+
+    public static void echo(String input) {
+        if (input.contains("todo")) {
+            System.out.println("Gotcha! Added '" + input + "' to your To Do List!");
+        }
+        else if (input.contains("deadline")) {
+            System.out.println("Gotcha! Added '" + input + "' to your To Do List!" +
+                    " You have to finish this by a certain time, so be reminded!");
+        }
+        else if (input.contains("event")) {
+            System.out.println("Gotcha! Added '" + input + "' to your To Do List!" +
+                    " This happens and ends at a specific time, so remember to check your calender! (Or ask me!)");
+        }
+        else {
+            System.out.println("Uhh.. I'm sorry but I'm not quite sure what '" + input + "' means..." +
+                    " Remember to include the Task Type in front of the input!!");
+        }
+    }
+
+    public static void createTask(String input) {
+        if (input.contains("todo")) {
+            toDoList[counter] = new ToDo(input.substring(input.indexOf("todo ") + 5));
+            counter++;
+            echo(input);
+        }
+        else if (input.contains("deadline")) {
+            String description = input.replaceFirst("deadline ", "");
+            int idxOfEndDate = description.indexOf("/");
+            String Date = description.substring( idxOfEndDate + 4);
+            toDoList[counter] = new Deadline(description.substring(0, idxOfEndDate - 1), Date);
+            counter++;
+            echo(input);
+        }
+        else if(input.contains("event")) {
+            String description = input.replaceFirst("event ", "");
+            int idxOfFrom = description.indexOf("/from");
+            int idxOfBy = description.indexOf("/to");
+            String startDate = description.substring(idxOfFrom + 6, idxOfBy - 1);
+            String endDate = description.substring(idxOfBy + 4);
+            toDoList[counter] = new Event(description.substring(0, idxOfFrom - 1), startDate, endDate);
+            counter++;
+            echo(input);
+        } else {
+            echo(input);
+        }
+        /*Task t = new Task(input);
+        toDoList[counter] = t;
+        toDoList[counter].setDescription(input);
+        System.out.println("Added '" + input + "' to your To Do List!");
+        counter++;*/
     }
 
     public static void main(String[] args) {
@@ -11,41 +64,36 @@ public class GermaBot {
                 + "What may I do for you this fine day? \n"
                 + "____________________";
         System.out.println(WelcomeMessage);
-        Task[] toDoList = new Task[100];
-        int counter = 0;
         while (true) {
-            String echo;
+            String input;
             Scanner in = new Scanner(System.in);
-            echo = in.nextLine();
-            if (echo.equals("bye")) {
+            input = in.nextLine();
+            if (input.equals("bye")) {
                 break;
             }
-            if (echo.equals("list")) {
+            if (input.equals("list")) {
                 int printCounter = 1;
                 System.out.println("Gotcha! Here are your tasks:");
-                for (int j = 0; j < counter; j++) {
-                    if (toDoList[j] == null) {
+                for (int i = 0; i < counter; i++) {
+                    if (toDoList[i] == null) {
                         break;
                     }
-                    System.out.println(printCounter + ". [" + toDoList[j].getStatusIcon() + "] " + toDoList[j].getDescription());
+                    /*System.out.println(printCounter + ". [" + toDoList[j].getStatusIcon() + "] " + toDoList[j].getDescription());*/
+                    System.out.println(printCounter + ". " + toDoList[i].toString());
                     printCounter++;
                 }
-            } else if (echo.contains("unmark")) {
-                int idx = getIdx(echo);
+            } else if (input.contains("unmark")) {
+                int idx = getIdx(input);
                 toDoList[idx].setDone(false);
                 System.out.println("Aww, not done? Okay, I'll mark this task as undone: "
                         + "[" + toDoList[idx].getStatusIcon() + "] " + toDoList[idx].getDescription());
-            } else if (echo.contains("mark")) {
-                int idx = getIdx(echo);
+            } else if (input.contains("mark")) {
+                int idx = getIdx(input);
                 toDoList[idx].setDone(true);
                 System.out.println("Good job! I'll mark this task as done: "
                         + "[" + toDoList[idx].getStatusIcon() + "] " + toDoList[idx].getDescription());
             } else {
-                Task t = new Task(echo);
-                toDoList[counter] = t;
-                toDoList[counter].setDescription(echo);
-                System.out.println("Added '" + echo + "' to the list!");
-                counter++;
+                createTask(input);
             }
         }
         String GoodbyeMessage = "____________________ \n"
