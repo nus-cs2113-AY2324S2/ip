19458cace718f5153305c0b97dc156672b1f149b
PureUsagi <ngjunhancanoe@gmail.com>
2024-02-15 00:45:22 +0800

    Move message printing related methods to the parser class and replace printing methods in list class with method calls to parser class

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -8,12 +8,11 @@ public class List {
     }
 
     public void printTaskCount() {
-        System.out.println(String.format("\tYou now have %d tasks in the list.", itemCount));
+        Parser.printTaskCount(itemCount);
     }
 
     private void printAddTaskMessage(Task newTask) {
-        System.out.println("\tGot it. I've added this task:");
-        System.out.println(String.format("\t\t%s", newTask));
+        Parser.printAddTaskMessage(newTask);
         printTaskCount();
     }
 
@@ -42,20 +41,15 @@ public class List {
 
     public void markTask(int index) {
         list[index - 1].setTaskAsDone();
-        System.out.println("\tNice! I've marked this task as done:");
-        System.out.println(String.format("\t%s",list[index-1]));
+        Parser.printTaskMarkAsDone(list[index-1]);
     }
 
     public void unmarkTask(int index) {
         list[index - 1].setTaskAsNotDone();
-        System.out.println("\tOK, I've marked this task as not done yet:");
-        System.out.println(String.format("\t%s",list[index-1]));
+        Parser.printTaskUnmarkAsNotDone(list[index-1]);
     }
 
     public void listAll() {
-        System.out.println("\tHere are the tasks in your list:");
-        for (int i = 0 ; i < itemCount; i++) {
-            System.out.println(String.format("\t%d.%s", i + 1, list[i]));
-        }
+        Parser.printList(list, itemCount);
     }
 }
