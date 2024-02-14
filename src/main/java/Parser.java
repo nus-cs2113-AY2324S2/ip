63923954e560cbeb0a4b91fa21e3cba28fa1dfaa
DavidTay <davidtaywd@gmail.com>
2024-02-14 17:12:06 +0800

    Add error checking to tasks
    There are now task specific exceptions in the package exceptions. There are also checks for if the task description/duedate/to/from are empty. A corresponding error message would be printed out.

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,22 +1,32 @@
+import Exceptions.DeadlineLackInputsException;
+import Exceptions.EventLackInputsException;
+import Exceptions.TodoLackInputsException;
+
 public class Parser {
     protected String userInput;
     
     public Parser () {}
     
     // no error checking for processing task information has been implemented (e.g. deadline but no /by)
-    public String[] processTaskInformation(String userInput) {
+    public String[] processTaskInformation(String userInput) throws TodoLackInputsException, DeadlineLackInputsException, EventLackInputsException {
         userInput = userInput.toLowerCase();
         String[] wordArray = userInput.split(" ");
         String[] output = new String[4];
         if (wordArray[0].equals("todo")) {
             output[0] = "todo";
             output[1] = userInput.substring(4);
+            if (output[1].isEmpty()) {
+                throw new TodoLackInputsException();
+            }
             return output;
         } else if (wordArray[0].equals("deadline")) {
             output[0] = "deadline";
             int byIndex = userInput.indexOf("/by");
             output[1] = userInput.substring(8, byIndex);
             output[2] = userInput.substring(byIndex + 4);
+            if (output[1].isEmpty() || output[2].isEmpty()) {
+                throw new DeadlineLackInputsException();
+            }
             return output;
         } else if (wordArray[0].equals("event")) {
             output[0] = "event";
@@ -25,6 +35,9 @@ public class Parser {
             int toIndex = userInput.indexOf("/to");
             output[2] = userInput.substring(fromIndex + 6, toIndex);
             output[3] = userInput.substring(toIndex + 4);
+            if (output[1].isEmpty() || output[2].isEmpty() || output[3].isEmpty()) {
+                throw new EventLackInputsException();
+            }
             return output;
         }
         else {
