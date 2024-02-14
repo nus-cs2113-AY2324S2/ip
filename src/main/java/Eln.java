8e38f98010cf362f9c19029470daf18c0757bcc1
nigelheng <e0970002@u.nus.edu>
2024-02-14 12:22:24 +0800

    Add increment Level-3
    implement taskManager
    add todo and task classes
    add method to mark and unmark tasks

--- src/main/java/Eln.java
+++ src/main/java/Eln.java
@@ -3,8 +3,99 @@ import java.util.Scanner;
 
 
 public class Eln {
-    static String LINE = "____________________________________________________________";
-    private static void echoUser() {
+    private static final String LINE = "____________________________________________________________";
+    private static Task[] tasksList = new Task[100]; // max number of tasks set to 100
+    private static int numOfTasks = 0;
+
+    private static void greeting() {
+        System.out.println(LINE);
+        System.out.println("Hello! I'm Eln");
+        System.out.println("What can I do for you?");
+        System.out.println(LINE);
+    }
+
+    private static void farewell() {
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(LINE);
+    }
+
+    private static void showList() {
+        System.out.println(LINE);
+        if(numOfTasks == 0) {
+            System.out.println("there are no pending tasks");
+        }
+        for(int i = 0; i < numOfTasks; i++) {
+            System.out.println((i+1) + ". " + tasksList[i].toString());
+        }
+        System.out.println(LINE);
+    }
+
+    private static void addTask(String taskToAdd) {
+
+        Task newTask = new Todo(taskToAdd);
+
+        tasksList[numOfTasks] = newTask;
+        numOfTasks++;
+
+        //addTaskResponse
+        System.out.println(LINE);
+        System.out.println("The following task has been added as task number " + numOfTasks);
+        System.out.println(newTask.toString());
+        System.out.println(LINE);
+    }
+
+    private static void mark(String taskToMark) {
+        int taskNumToMark = Integer.parseInt(taskToMark.substring("mark".length()).trim()) - 1;
+        tasksList[taskNumToMark].markAsDone();
+
+        //markAsDone response
+        System.out.println(LINE);
+        System.out.println("The following task was marked");
+        System.out.println(tasksList[taskNumToMark].toString());
+        System.out.println(LINE);
+    }
+
+    private static void unmark(String taskToUnmark) {
+        int taskNumToUnmark = Integer.parseInt(taskToUnmark.substring("unmark".length()).trim()) - 1;
+        tasksList[taskNumToUnmark].markAsUndone();
+
+        //markAsDone response
+        System.out.println(LINE);
+        System.out.println("The following task was unmarked");
+        System.out.println(tasksList[taskNumToUnmark].toString());
+        System.out.println(LINE);
+    }
+
+
+
+    private static void taskManager() {
+        Scanner in = new Scanner(System.in);
+        String userInput = in.nextLine(); // read user input
+
+        while (!userInput.equalsIgnoreCase("bye")){
+            if (userInput.equalsIgnoreCase("list")) {
+                showList();
+            } else if (userInput.startsWith("mark")) {
+                mark(userInput);
+            } else if (userInput.startsWith("unmark")) {
+                unmark(userInput);
+            } else {
+                addTask(userInput);
+            }
+            userInput = in.nextLine();
+        }
+
+    }
+    public static void main(String[] args) {
+        greeting();
+        taskManager(); // taskManager ends when user inputs "bye"
+        farewell();
+    }
+}
+
+
+
+    /*private static void echoUser() {
         Scanner scan = new Scanner(System.in);
         String input = "";
         String[] list = new String[20];
@@ -30,18 +121,4 @@ public class Eln {
                 index++;
             }
         }
-    }
-    public static void main(String[] args) {
-
-        System.out.println(LINE);
-        System.out.println("Hello! I'm Eln");
-        System.out.println("What can I do for you?");
-        System.out.println(LINE);
-
-        echoUser();
-
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(LINE);
-
-    }
-}
+    }*/
---
+++ src/main/java/Todo.java
@@ -0,0 +1,14 @@
+public class Todo extends Task {
+
+    protected String toDo;
+
+    public Todo(String description) {
+        super(description);
+        this.toDo = this.description.substring("todo".length()).trim();
+    }
+
+    @Override
+    public String toString() {
+        return "[" + this.getStatusIcon() + "] " + this.toDo;
+    }
+}
