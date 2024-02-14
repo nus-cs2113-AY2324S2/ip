8e0a49f6d1d68de446879efbb4d38416dbb95f31
samuelory <samuelong7@gmail.com>
2024-02-14 23:31:25 +0800

    Reduced nesting

--- src/main/java/OGF.java
+++ src/main/java/OGF.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 public class OGF {
+
+    private static int numItem;
+    private static final Task[] tasks = new Task[100];
+
+    private static boolean isRunning = true;
     private static void printBreakLine(){
         System.out.println(("____________________________________________________________"));
     }
@@ -9,6 +14,64 @@ public class OGF {
         System.out.println(task);
         System.out.printf("You have %d tasks in the list.%n", taskIndex+1);
         printBreakLine();
+    }
+
+    private static boolean handleInput (String input) {
+        switch (input.split(" ")[0]) {
+            case ("bye"):
+                System.out.println("Bye bye now!");
+                return (false);
+            case ("list"):
+                System.out.println("Here are your tasks for today: ");
+                for (int i = 0; i < numItem; i++) {
+                    System.out.print(i + 1 + ". ");
+                    System.out.println(tasks[i]);
+                }
+                printBreakLine();
+                break;
+            case ("mark"):
+                //Fallthrough
+            case ("unmark"):
+                int taskNo = Integer.parseInt(input.split(" ")[1]) - 1;
+
+                if (input.split(" ")[0].equals("mark")) {
+                    System.out.println("Good Job! I'm setting this task as done: ");
+                    tasks[taskNo].setDone(true);
+                } else {
+                    System.out.println("Oop! I'm setting this task as undone: ");
+                    tasks[taskNo].setDone(false);
+                }
+                System.out.println(tasks[taskNo]);
+                printBreakLine();
+                break;
+            case ("todo"):
+                String newTodoDesc = input.substring(input.indexOf(" "));
+                tasks[numItem] = new Todo(newTodoDesc);
+                printTaskAdded(tasks[numItem], numItem);
+                numItem++;
+                break;
+            case ("deadline"):
+                String newDeadlineDesc = input.substring(input.indexOf(" "), input.indexOf(" /by"));
+                String newDeadlineTime = input.substring(input.indexOf("/by") + 4);
+                tasks[numItem] = new Deadline(newDeadlineDesc, newDeadlineTime);
+                printTaskAdded(tasks[numItem], numItem);
+                numItem++;
+                break;
+            case ("event"):
+                String newEventDesc = input.substring(input.indexOf(" "), input.indexOf("/from"));
+                String newEventStart = input.substring(input.indexOf("/from") + 6, input.indexOf(" /to"));
+                String newEventEnd = input.substring(input.indexOf("/to") + 4);
+                tasks[numItem] = new Event(newEventDesc, newEventStart, newEventEnd);
+                printTaskAdded(tasks[numItem], numItem);
+                numItem++;
+                break;
+
+            default:
+                System.out.println("unrecognised input");
+                printBreakLine();
+                break;
+        }
+        return true;
     }
     public static void main(String[] args) {
         String logo = "\n" +
@@ -23,68 +86,12 @@ public class OGF {
         System.out.println("Welcome! I'm your \n" + logo + "Nice to meet you!");
         System.out.println("What can I do for you?");
 
-
-        Task[] tasks = new Task[100];
-        int numItem = 0;
         Scanner scanner = new Scanner(System.in);
-        int taskNo;
-        while (true) {
+        while (isRunning) {
             String input = scanner.nextLine();
-            switch (input.split(" ")[0]) {
-                case ("bye"):
-                    System.out.println("Bye bye now!");
-                    return;
-                case ("list"):
-                    System.out.println("Here are your tasks for today: ");
-                    for (int i = 0; i < numItem; i++){
-                        System.out.print(i+1 + ". ");
-                        System.out.println(tasks[i]);
-                    }
-                    printBreakLine();
-                    break;
-                case ("mark"):
-                //Fallthrough
-                case ("unmark"):
-                    taskNo = Integer.parseInt(input.split(" ")[1])-1;
-
-                    if (input.split(" ")[0].equals("mark")){
-                        System.out.println("Good Job! I'm setting this task as done: ");
-                        tasks[taskNo].setDone(true);
-                    } else{
-                        System.out.println("Oop! I'm setting this task as undone: ");
-                        tasks[taskNo].setDone(false);
-                    }
-                    System.out.println(tasks[taskNo]);
-                    printBreakLine();
-                    break;
-                case ("todo"):
-                    String newTodoDesc = input.substring(input.indexOf(" "));
-                    tasks[numItem] = new Todo(newTodoDesc);
-                    printTaskAdded(tasks[numItem], numItem);
-                    numItem++;
-                    break;
-                case ("deadline"):
-                    String newDeadlineDesc = input.substring(input.indexOf(" "), input.indexOf(" /by"));
-                    String newDeadlineTime = input.substring(input.indexOf("/by")+4);
-                    tasks[numItem] = new Deadline(newDeadlineDesc, newDeadlineTime);
-                    printTaskAdded(tasks[numItem], numItem);
-                    numItem++;
-                    break;
-                case ("event"):
-                    String newEventDesc = input.substring(input.indexOf(" "), input.indexOf("/from"));
-                    String newEventStart = input.substring(input.indexOf("/from")+6, input.indexOf(" /to"));
-                    String newEventEnd = input.substring(input.indexOf("/to")+4);
-                    tasks[numItem] = new Event(newEventDesc, newEventStart, newEventEnd);
-                    printTaskAdded(tasks[numItem], numItem);
-                    numItem++;
-                    break;
-
-                default:
-                    System.out.println("unrecognised input");
-                    printBreakLine();
-                    break;
+            isRunning = handleInput(input);
             }
         }
 
     }
-}
+
