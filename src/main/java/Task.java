d3c82d612ed865aafaa7d8058f187253c68d8e35
Shu <e0957502@u.nus.edu>
2024-02-15 12:01:43 +0800

    Update print statement of Task.java

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -15,13 +15,13 @@ public class Task {
     public void markAsDone() {
         this.isDone = true;
         System.out.println("Nice! I've marked this task as done:");
-        System.out.println("\t [" + this.getStatusIcon() + "] " + this.description);
+        System.out.println("\t[" + this.getTaskTypeIcon() + "][" + this.getStatusIcon() + "] " + this.description);
     }
 
     public void markAsUndone() {
         this.isDone = false;
         System.out.println("OK, I've marked this task as not done yet:");
-        System.out.println("\t [" + this.getStatusIcon() + "] " + this.description);
+        System.out.println("\t[" + this.getTaskTypeIcon() + "][" + this.getStatusIcon() + "]" + this.description);
     }
 
     public String getDescription() {
