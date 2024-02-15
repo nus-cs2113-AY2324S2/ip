5fd17b865bba639d31f56e6b166f8abb9a40728b
nicknamenic <ranjnicoli1@gmail.com>
2024-02-16 00:15:52 +0800

    Added extra exceptions

--- src/main/java/Nocturne.java
+++ src/main/java/Nocturne.java
@@ -67,7 +67,6 @@ public class Nocturne {
                     System.out.println("  " + trueTodo);
                     break;
 
-
                 case "event":
                     String[] eventSeparated = input.split("/");
                     if (eventSeparated.length != 3) {
