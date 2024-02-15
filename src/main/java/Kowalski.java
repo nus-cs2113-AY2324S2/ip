0daa13c6a3db91cfc2abea0ea6ac2ceaec934f94
Parth Sanjay Gandhi <E0957991@u.nus.edu>
2024-02-15 11:23:25 +0800

    Add error handling using KowalskiException

--- src/main/java/Kowalski.java
+++ src/main/java/Kowalski.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.InputMismatchException;
 
 
 public class Kowalski {
@@ -12,10 +13,8 @@ public class Kowalski {
 
     /**
      * Prints out the message introducing the functionalities of Kowalski Bot
-     *
      */
     public static void printIntro(){
-        // Introduction to User
         System.out.println(DIVIDING_LINE);
         System.out.println("Welcome Skipper! I'm Kowalski, reporting for Duty!" + System.lineSeparator() +
                 "What can I do for you today?" );
@@ -23,8 +22,29 @@ public class Kowalski {
     }
 
     /**
+     * Used to check if the user has accurately input the deadline by stating the "/by"
+     * @param deadlineDetails : Contains the details of the deadline task
+     * @throws KowalskiException In the event that the input has no "/by"
+     */
+    public static void checkDeadlineInput(String deadlineDetails) throws KowalskiException{
+        if (!(deadlineDetails.contains("/by"))) {
+            throw new KowalskiException();
+        }
+    }
+
+    /**
+     * Used to check if the user has accurately input the deadline by stating the "/from" and "/to"
+     * @param eventDetails : Contains the details of the event task
+     * @throws KowalskiException In the event that the input has no "/from" or "/to" or both
+     */
+    public static void checkEventInput(String eventDetails) throws KowalskiException{
+        if (!((eventDetails.contains("/from")) && (eventDetails.contains("/to")))) {
+            throw new KowalskiException();
+        }
+    }
+
+    /**
      * Used to process the different variations of the users inputs
-     *
      * @param userInput : String which the user inputs
      * @return String which is in lowercase and clear of any unnecessary whitespace
      */
@@ -32,6 +52,10 @@ public class Kowalski {
         return (userInput.trim()).toLowerCase();
     }
 
+    /**
+     * Prints out an accurate message for the number of tasks in the list.
+     * @param number : represents the total current task count
+     */
     public static void printCurrentTaskMessage(int number){
         switch (number){
         case 0:
@@ -45,12 +69,50 @@ public class Kowalski {
         }
     }
 
+    /**
+     * Used in the "list" command to print all the Current Tasks in the proper format
+     */
     public static void printCurrentTaskItems(){
         for (int i = 1; i <= currentTask.size(); i++){
             System.out.println(i + "." + currentTask.get(i-1));
         }
     }
 
+    /**
+     * Cleans up the user input and forms new Deadline Task
+     * @param deadlineDetails : User input for details of the deadline task
+     * @return new deadline task created
+     */
+    private static Task getNewDeadlineTask(String deadlineDetails) {
+        String[] deadlineArray = deadlineDetails.split("/by");
+        for (int i = 0; i < deadlineArray.length; i++) {
+            deadlineArray[i] = deadlineArray[i].trim();
+        }
+
+
+        return new Deadline(deadlineArray[0], deadlineArray[1]);
+    }
+
+
+    /**
+     * Cleans up the user input and forms new event Task
+     * @param eventDetails : User input for details of the event task
+     * @return new event task created
+     */
+    private static Task getNewEventTask(String eventDetails) {
+        String[] eventArray = eventDetails.split("/from");
+        String eventInformation = eventArray[0].trim();
+        String [] fromAndTo = eventArray[1].split("/to");
+        String eventFrom = fromAndTo[0].trim();
+        String eventTo = fromAndTo[1].trim();
+
+        return new Event(eventInformation, eventFrom, eventTo);
+    }
+
+    /**
+     * Processes all the inputs from the user and categorises the User Commands
+     * @param UserCommand: The first word input by the user
+     */
     public static void parseUserCommand(String UserCommand) {
 
         int taskNumber;
@@ -68,7 +130,12 @@ public class Kowalski {
             break;
 
         case "unmark":
-            taskNumber = in.nextInt();
+            try {
+                taskNumber = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Skipper input a god damn number! I am now gonna add the text which you inputted into our list!");
+                break;
+            }
 
             try{
                 currentTask.get(taskNumber - 1).markAsNotDone();
@@ -81,7 +148,12 @@ public class Kowalski {
             break;
 
         case "mark":
-            taskNumber = in.nextInt();
+            try {
+                taskNumber = in.nextInt();
+            } catch (InputMismatchException e) {
+                System.out.println("Skipper input a god damn number! I am now gonna add the text which you inputted into our list!");
+                break;
+            }
 
             try{
                 currentTask.get(taskNumber - 1).markAsDone();
@@ -107,49 +179,46 @@ public class Kowalski {
             break;
 
         case "deadline":
-            //Processing
             String deadlineDetails = in.nextLine();
 
-            if (deadlineDetails.contains("/by")) {
-                String[] deadlineArray = deadlineDetails.split("/by");
-                for (int i = 0; i < deadlineArray.length; i++) {
-                    deadlineArray[i] = deadlineArray[i].trim();
-                }
+            try{
+                checkDeadlineInput(deadlineDetails);
 
-                Task newDeadlineTask = new Deadline(deadlineArray[0], deadlineArray[1]);
+                //Adding the new deadline task into currentTask List after processing and cleaning inputs
+                Task newDeadlineTask = getNewDeadlineTask(deadlineDetails);
                 currentTask.add(newDeadlineTask);
                 lastTaskIndex = currentTask.size() - 1;
 
+                //Printing the appropriate information for the User
                 System.out.println("Skipper, I have recorded this deadline:");
                 System.out.println("  " + currentTask.get( lastTaskIndex));
                 printCurrentTaskMessage(currentTask.size());
                 System.out.println(DIVIDING_LINE);
-            } else {
-                System.out.println("Skipper your inputs are wrong! Don't be sorry, be better!");
-                //throw new KowalskiException();
+            } catch (KowalskiException e){
+                System.out.println("Skipper your inputs are wrong! Try again!");
+                System.out.println(DIVIDING_LINE);
             }
             break;
 
         case "event":
             String eventDetails = in.nextLine();
-            if ((eventDetails.contains("/from")) && (eventDetails.contains("/to"))) {
-                String[] eventArray = eventDetails.split("/from");
-                String eventInformation = eventArray[0].trim();
-                String [] fromAndTo = eventArray[1].split("/to");
-                String eventFrom = fromAndTo[0].trim();
-                String eventTo = fromAndTo[1].trim();
 
-                Task newEventTask = new Event(eventInformation, eventFrom, eventTo);
+            try{
+                checkEventInput(eventDetails);
+
+                //Adding the new event task into currentTask List after processing and cleaning inputs
+                Task newEventTask = getNewEventTask(eventDetails);
                 currentTask.add(newEventTask);
                 lastTaskIndex = currentTask.size() - 1;
 
+                //Printing the appropriate information for the User
                 System.out.println("Skipper I've noted this event in my calendar:");
                 System.out.println("  " + currentTask.get(lastTaskIndex));
                 printCurrentTaskMessage(currentTask.size());
                 System.out.println(DIVIDING_LINE);
-            } else {
-                System.out.println("Skipper your inputs are wrong! Don't be sorry, be better!");
-                //throw new KowalskiException();
+            } catch (KowalskiException e) {
+                System.out.println("Skipper your inputs are wrong! Try again!");
+                System.out.println(DIVIDING_LINE);
             }
             break;
 
@@ -166,7 +235,6 @@ public class Kowalski {
 
     /**
      * Prints out the message to end conversation with the user
-     *
      */
     public static void printEndConversation(){
         System.out.println("Bye Skipper! Hope to serve you again for your next mission!");
