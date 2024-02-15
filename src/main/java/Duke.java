a4717359cb6e35c500e3e592dcbe7d92bd4e725d
nicknamenic <ranjnicoli1@gmail.com>
2024-02-15 21:42:53 +0800

    Adhering to Coding Standards

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
         Task[] tasks = new Task[100];
-        int COUNT = 0;
+        int taskCount = 0;
 
         String input;
         Scanner in = new Scanner(System.in);
@@ -14,28 +14,28 @@ public class Duke {
         while (!input.equals("bye")) {
             String[] commandCheck = input.split(" ");
             if (input.equals("list")) {
-                for (int i = 0; i < COUNT; i++){
+                for (int i = 0; i < taskCount; i++){
                     System.out.println((i + 1) + ".[" + tasks[i].getStatusIcon() + "] " + tasks[i].getDescription());
                 }
             }
             else if(commandCheck[0].equals("mark")) {
                 int listIndex = Integer.parseInt(commandCheck[1]);
-                System.out.println("Nice! I've marked this task as done:");
+                System.out.println("Congratulations. I have marked this task as finished:");
                 System.out.println("[X] " + tasks[listIndex - 1].getDescription());
                 tasks[listIndex - 1].isDone = true;
             }
 
             else if(commandCheck[0].equals("unmark")) {
                 int listIndex = Integer.parseInt(commandCheck[1]);
-                System.out.println("OK, I've marked this task as not done yet:");
+                System.out.println("Do not neglect your duties. I have marked this task as unfinished:");
                 System.out.println("[ ] " + tasks[listIndex - 1].getDescription());
                 tasks[listIndex - 1].isDone = false;
             }
 
             else {
                 System.out.println("added: " + input);
-                tasks[COUNT] = new Task(input);
-                COUNT++;
+                tasks[taskCount] = new Task(input);
+                taskCount++;
             }
             input = in.nextLine();
         }
