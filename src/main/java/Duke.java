2ac814f377cc8e97f431510214aeb78afee3b74b
nicknamenic <ranjnicoli1@gmail.com>
2024-02-15 20:34:40 +0800

    Increment Level

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,15 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        String input;
+        Scanner in = new Scanner(System.in);
         System.out.println("Good evening. I'm Nocturne.");
         System.out.println("What ails you on this fine day?");
+        input = in.nextLine();
+        while (!input.equals("bye")) {
+            System.out.println(input);
+            input = in.nextLine();
+        }
         System.out.println("Farewell, and may the fortunes be ever in your favour.");
     }
 }
