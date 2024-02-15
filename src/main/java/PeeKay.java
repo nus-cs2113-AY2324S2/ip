410bc60120193b4c16c5c9b7f12d9686fc117abb
paturikarthik <paturi.karthik1@gmail.com>
2024-02-15 23:12:02 +0800

    included changes from peer review and tidied up code

--- src/main/java/PeeKay.java
+++ src/main/java/PeeKay.java
@@ -1,45 +1,57 @@
 import java.util.Scanner;
 
 public class PeeKay {
-    static String indent = "     ";
-    static String line = "    ____________________________________________________________";
-    static Task[] list = new Task[100];
+
+    static String line = "\t____________________________________________________________";
+    static Task[] tasks = new Task[100];
     static int count = 0;
 
     public static void echo(String input) {
-        if (input.equals("bye")) {
-            System.out.println(indent + "Bye. Hope to see you again soon!");
-        } else {
-            System.out.println(indent + "Got it. I've added this task: ");
-            System.out.println((indent + "  " + list[count - 1].toString()));
-            String task = count == 1 ? "task " : "tasks ";
-            System.out.println(indent + "Now you have " + count + " " + task + "in the list.");
-        }
+        System.out.println("\t Got it. I've added this task: ");
+        System.out.println(("\t   " + tasks[count - 1].toString()));
+        String task = (count == 1) ? "task " : "tasks ";
+        System.out.println("\t Now you have " + count + " " + task + "in the tasks.");
     }
 
-    public static void createTask(String input) {
+    public static void sayBye() {
+        System.out.println("\t Bye. Hope to see you again soon!");
+    }
+
+    public static void createTodo(String input){
+        tasks[count] = new ToDo(input.substring(input.indexOf("todo ") + 5));
+        count++;
+        echo(input);
+    }
+
+    public static void createDeadline(String input){
+        String description = input.replaceFirst("deadline ", "");
+        int by = description.indexOf("/");
+        String Date = description.substring(by + 4);
+        tasks[count] = new Deadline(description.substring(0, by - 1), Date);
+        count++;
+        echo(input);
+    }
+
+    public static void createEvent(String input){
+        String description = input.replaceFirst("event ", "");
+        int from = description.indexOf("/from");
+        int by = description.indexOf("/to");
+        String startDate = description.substring(from + 6, by - 1);
+        String endDate = description.substring(by + 4);
+        tasks[count] = new Event(description.substring(0, from - 1), startDate, endDate);
+        count++;
+        echo(input);
+    }
+
+    public static void handleInput(String input) {
         if (input.contains("todo")) {
-            list[count] = new ToDo(input.substring(input.indexOf("todo ") + 5));
-            count ++;
-            echo(input);
+            createTodo(input);
         } else if (input.contains("deadline")) {
-            String description = input.replaceFirst("deadline ", "");
-            int by = description.indexOf("/");
-            String Date = description.substring(by + 4);
-            list[count] = new Deadline(description.substring(0, by - 1), Date);
-            count++;
-            echo(input);
-        } else if (input.contains("event")){
-            String description = input.replaceFirst("event ", "");
-            int from = description.indexOf("/from");
-            int by = description.indexOf("/to");
-            String startDate = description.substring(from + 6, by - 1);
-            String endDate = description.substring(by + 4);
-            list[count] = new Event(description.substring(0, from - 1), startDate, endDate);
-            count++;
-            echo(input);
-        }else {
-            echo(input);
+            createDeadline(input);
+        } else if (input.contains("event")) {
+            createEvent(input);
+        } else {
+            sayBye();
         }
     }
 
@@ -48,28 +60,40 @@ public class PeeKay {
         return idx - 1;
     }
 
+    public static void listItems() {
+        System.out.println("\t Here are the tasks in your list:");
+        for (int x = 0; x < count; x++) {
+            System.out.println("\t  " + (x + 1) + "." + tasks[x].toString());
+        }
+    }
+
+    public static void unmarkItem(String input) {
+        int idx = getIndexToMark(input);
+        tasks[idx].setDone(false);
+        System.out.println("\t OK, I've marked this task as not done yet:");
+        System.out.println("\t " + tasks[idx].toString());
+    }
+
+    public static void markItem(String input) {
+        int idx = getIndexToMark(input);
+        tasks[idx].setDone(true);
+        System.out.println("\t Nice! I've marked this task as done:");
+        System.out.println("\t " + tasks[idx].toString());
+    }
+
     public static void chat() {
         String input;
         do {
             input = new Scanner(System.in).nextLine();
             System.out.println(line);
             if (input.equals("list")) {
-                System.out.println(indent + "Here are the tasks in your list:");
-                for (int x = 0; x < count; x++) {
-                    System.out.println(indent + " " + (x + 1) + "." + list[x].toString());
-                }
+                listItems();
             } else if (input.contains("unmark")) {
-                int idx = getIndexToMark(input);
-                list[idx].setDone(false);
-                System.out.println(indent + "OK, I've getIndexToMed this task as not done yet:");
-                System.out.println(indent + list[idx].toString());
+                unmarkItem(input);
             } else if (input.contains("mark")) {
-                int idx = getIndexToMark(input);
-                list[idx].setDone(true);
-                System.out.println(indent + "Nice! I've marked this task as done:");
-                System.out.println(indent + list[idx].toString());
+                markItem(input);
             } else {
-                createTask(input);
+                handleInput(input);
             }
             System.out.println(line);
         } while (!input.equals("bye"));
@@ -78,8 +102,8 @@ public class PeeKay {
     public static void main(String[] args) {
 
         System.out.println(line);
-        System.out.println(indent + "Hello! I'm PeeKay");
-        System.out.println(indent + "What can I do for you?");
+        System.out.println("\t Hello! I'm PeeKay");
+        System.out.println("\t What can I do for you?");
         System.out.println(line);
         chat();
     }
