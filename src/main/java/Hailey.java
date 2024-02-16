acec1f9bec6fe8c8a197c035a433b9443cec8c63
hailey-jung <hyojhj0319@gmail.com>
2024-02-16 16:38:22 +0800

    Level 0. Rename, Greet, Exit

--- src/main/java/Hailey.java
+++ src/main/java/Hailey.java
@@ -1,8 +1,23 @@
+import java.util.Scanner;
+
 public class Hailey {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         System.out.println("Hello! I'm Hailey");
         System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+
+        while (true) {
+            String input = scanner.nextLine();
+            System.out.println(input);
+
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
 
