3fe680b14fa5c1f4458948df2116997384dafb37
hailey-jung <hyojhj0319@gmail.com>
2024-02-16 16:54:24 +0800

    A-CodingStandard

--- src/main/java/Hailey.java
+++ src/main/java/Hailey.java
@@ -13,17 +13,17 @@ public class Hailey {
         while (true) {
             String input = scanner.nextLine();
 
-            if (input.equalsIgnoreCase("bye")) {
+            if ("bye".equalsIgnoreCase(input)) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
-            } else if (input.equalsIgnoreCase("list")) {
+            } else if ("list".equalsIgnoreCase(input)) {
                 if (tasks.isEmpty()) {
                     System.out.println("No tasks added yet.");
                 } else {
                     System.out.println("Here are the tasks in your list:");
                     for (int i = 0; i < tasks.size(); i++) {
                         Task task = tasks.get(i);
-                        System.out.println(" " + (i + 1) + "." + task.getStatusIcon() + " " + task.getDescription());
+                        System.out.printf(" %d.%s %s%n", i + 1, task.getStatusIcon(), task.getDescription());
                     }
                 }
             } else if (input.startsWith("mark")) {
@@ -34,7 +34,7 @@ public class Hailey {
                         Task task = tasks.get(index);
                         task.markAsDone();
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+                        System.out.printf("  %s %s%n", task.getStatusIcon(), task.getDescription());
                     } else {
                         System.out.println("Invalid task index.");
                     }
@@ -49,7 +49,7 @@ public class Hailey {
                         Task task = tasks.get(index);
                         task.markAsUndone();
                         System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println("  " + task.getStatusIcon() + " " + task.getDescription());
+                        System.out.printf("  %s %s%n", task.getStatusIcon(), task.getDescription());
                     } else {
                         System.out.println("Invalid task index.");
                     }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -27,3 +27,4 @@ public class Task {
         return (isDone ? "[X]" : "[ ]");
     }
 }
+
