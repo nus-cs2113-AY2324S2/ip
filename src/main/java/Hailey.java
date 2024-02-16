53e2584ce82bfbec76f9d4c05fc8b2f90c26b128
hailey-jung <hyojhj0319@gmail.com>
2024-02-16 16:42:38 +0800

    Level 2. Add, List

--- src/main/java/Hailey.java
+++ src/main/java/Hailey.java
@@ -3,22 +3,35 @@ import java.util.Scanner;
 public class Hailey {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int taskCount = 0;
 
         System.out.println("Hello! I'm Hailey");
         System.out.println("What can I do for you?");
 
         while (true) {
             String input = scanner.nextLine();
-            System.out.println(input);
 
             if (input.equalsIgnoreCase("bye")) {
                 System.out.println("Bye. Hope to see you again soon!");
                 break;
+            } else if (input.equalsIgnoreCase("list")) {
+                if (taskCount == 0) {
+                    System.out.println("No tasks added yet.");
+                } else {
+                    System.out.println("Tasks:");
+                    for (int i = 0; i < taskCount; i++) {
+                        System.out.println((i + 1) + ". " + tasks[i]);
+                    }
+                }
+            } else {
+                tasks[taskCount] = input;
+                taskCount++;
+                System.out.println("added: " + input);
             }
         }
 
         scanner.close();
     }
 }
-
 
