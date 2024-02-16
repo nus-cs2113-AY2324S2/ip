91b967d8c3d7f1e3f24e21d485e9d84bda386e6e
hailey-jung <hyojhj0319@gmail.com>
2024-02-16 16:33:06 +0800

    Level 0. Rename, Greet, Exit

---
+++ src/main/java/Hailey.java
@@ -0,0 +1,8 @@
+public class Hailey {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Hailey");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
+
