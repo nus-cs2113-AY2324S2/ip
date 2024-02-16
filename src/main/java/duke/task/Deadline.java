47302d92f21cab85d893ba31075fe499a5a898a6
NgYaoDong <e0957740@u.nus.edu>
2024-02-16 23:42:08 +0800

    Add dates and times
    Teach the chatbot how to understand dates and times.

--- src/main/java/duke/task/Deadline.java
+++ src/main/java/duke/task/Deadline.java
@@ -1,21 +1,50 @@
 package duke.task;
 
+import duke.DukeException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.DateTimeException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected LocalDateTime by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by) throws DukeException {
         super(description);
-        this.by = by;
+        this.by = convertBy(by);
+    }
+
+    private LocalDateTime convertBy(String by) throws DukeException {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm");
+        int ISOLength = 10;
+        if (by.length() == ISOLength) {
+            by += " 2359";
+        }
+        LocalDateTime parsedBy;
+        try {
+            parsedBy = LocalDateTime.parse(by, formatter);
+        } catch (DateTimeException e) {
+            throw new DukeException("Time Out....\n\t " +
+                    "OOPS!!! The format of a deadline task is wrong!\n\t " +
+                    "TASK /by DEADLINE (in date: yyyy-mm-dd time: HHmm format)\n\t " +
+                    "Example: deadline return book /by 2024-05-01 1800");
+        }
+        if (parsedBy.isBefore(LocalDateTime.now())) {
+            throw new DukeException("Time Out....\n\t " +
+                    "OOPS!!! Your deadline is long due!");
+        }
+        return parsedBy;
     }
 
     @Override
     public String toDisk() {
-        return "D" + super.toDisk() + " | " + this.by + System.lineSeparator();
+        String byToDisk = this.by.toString().replace("T", " ").replace(":", "");
+        return "D" + super.toDisk() + " | " + byToDisk + System.lineSeparator();
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + "(by: " + this.by + ")";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMM dd yyyy HHmm");
+        return "[D]" + super.toString() + " (by: " + this.by.format(formatter) + ")";
     }
 }
