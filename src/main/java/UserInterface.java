f6b9fb24c93ea0decb6c78f88b472eeab0fb5454
DavidTay <davidtaywd@gmail.com>
2024-02-17 23:26:12 +0800

    Refactored userinterface
    Refactored userinterface to not recursively call when getting user inputs and to use a while loop instead

--- src/main/java/UserInterface.java
+++ src/main/java/UserInterface.java
@@ -3,32 +3,34 @@ public class UserInterface {
     Scanner myScanner = new Scanner(System.in);
     TaskManager taskManager = new TaskManager();
 
+    private boolean continueReceivingUserInputs = true;
+
     public UserInterface() {
         String userInput = myScanner.nextLine();
         processUserCommand(userInput);
     }
 
     public void processUserCommand(String userInput) {
-        userInput = userInput.toLowerCase();
-        if (userInput.equals("bye")) {
-            System.out.println("Bye. I will be awaiting your next order! ☺");
-        } else if (userInput.equals("list")) {
-            taskManager.showListContents();
-            userInput = myScanner.nextLine();
-            processUserCommand(userInput);
-        } else if (userInput.contains("mark")) {
-            taskManager.changeTaskStatus(userInput);
-            userInput = myScanner.nextLine();
-            processUserCommand(userInput);
-        } else if (userInput.contains("todo") || userInput.contains("event") || userInput.contains("deadline")){
-            taskManager.addListContents(userInput);
-            userInput = myScanner.nextLine();
-            processUserCommand(userInput);
-        }
-        else {
-            System.out.println("Please enter a valid command. Here have a cup of tea as you think of your next command. ☕");
-            userInput = myScanner.nextLine();
-            processUserCommand(userInput);
+        while (continueReceivingUserInputs) {
+            userInput = userInput.toLowerCase();
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. I will be awaiting your next order! ☺");
+                continueReceivingUserInputs = false;
+            } else if (userInput.equals("list")) {
+                taskManager.showListContents();
+                userInput = myScanner.nextLine();
+            } else if (userInput.contains("mark")) {
+                taskManager.changeTaskStatus(userInput);
+                userInput = myScanner.nextLine();
+            } else if (userInput.contains("todo") || userInput.contains("event") || userInput.contains("deadline")){
+                taskManager.addListContents(userInput);
+                userInput = myScanner.nextLine();
+            }
+            else {
+                System.out.println("Please enter a valid command. Here have a cup of tea as you think of your next command. ☕");
+                userInput = myScanner.nextLine();
+                processUserCommand(userInput);
+            }
         }
     }
 }
