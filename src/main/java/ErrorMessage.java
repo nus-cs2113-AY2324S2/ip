22c37862e70b3250dd787d1171b635c245788bca
Songyue Wang <me@songyue.wang>
2024-02-17 19:49:27 +0800

    Exception handling 2

---
+++ src/main/java/ErrorMessage.java
@@ -0,0 +1,7 @@
+public class ErrorMessage {
+    public String INVALID_ID = "Task ID should be an integer. Please try again.";
+    public String INVALID_COMMAND = "Command does not exist. Please try again.";
+    public String ID_NOT_EXIST = "Task ID does not exist. Please try again";
+    public String INVALID_EVENT_FORMAT = "Invalid event format. Please follow 'event <NAME> /from <START_TIME> /to <END_TIME>' format";
+    public String INVALID_DEADLINE_FORMAT = "Invalid deadline format. Please follow 'deadline <NAME> /by <DUE_TIME>' format";
+}
