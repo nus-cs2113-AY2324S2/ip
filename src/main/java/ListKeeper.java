550ae1580b16b04e37351ee783a5b9578b22e1d9
STeng618 <shantengyap@gmail.com>
2024-02-17 21:28:08 +0800

    Use ArrayList to store tasks

--- src/main/java/ListKeeper.java
+++ src/main/java/ListKeeper.java
@@ -1,40 +1,31 @@
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class ListKeeper {
-    private Task[] tasks;
-    private int numTaskAdded = 0;
-    private static final int MAX_NUM_OF_TASKS = 100;
+    private ArrayList<Task> tasks;
 
-    public ListKeeper(int numOfTasks) {
-        this.tasks = new Task[numOfTasks];
-    }
     public ListKeeper() {
-        this(MAX_NUM_OF_TASKS);
+        this.tasks = new ArrayList<>();
     }
 
     public void addToList(Task task) {
-        if (this.numTaskAdded == this.tasks.length) {
-            System.out.println("No more tasks can be added");
-            return;
-        }
-        this.tasks[this.numTaskAdded] = task;
-        this.numTaskAdded++;
+        this.tasks.add(task);
         // Provide feedback to user
         System.out.println("I have added this task:");
         System.out.println(task);
     }
 
     public void printList() {
-        for (int i = 0; i < this.numTaskAdded; i++) {
-            System.out.println(i + 1 + ". " + this.tasks[i]);
+        for (int i = 0; i < this.tasks.size(); i++) {
+            System.out.println(i + 1 + ". " + this.tasks.get(i));
         }
     }
 
     public boolean isValidTaskIndex(int inputIndex) {
-        return inputIndex >= 1 && inputIndex <= this.numTaskAdded;
+        return inputIndex >= 1 && inputIndex <= this.tasks.size();
     }
 
     public void processMark(int inputIndex, boolean isCompleted) {
-        tasks[inputIndex - 1].mark(isCompleted);
+        Task task = this.tasks.get(inputIndex - 1);
+        task.mark(isCompleted);
     }
 }
