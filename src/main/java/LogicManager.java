61e286750a17805b5cd07d6c70991375baa51ba5
STeng618 <shantengyap@gmail.com>
2024-02-18 01:27:03 +0800

    Parse blank lines

--- src/main/java/LogicManager.java
+++ src/main/java/LogicManager.java
@@ -88,6 +88,10 @@ public class LogicManager {
     }
 
     public void processCommand(String currentInput) {
+        currentInput = currentInput.trim();
+        if (currentInput.isEmpty()) {
+            return;
+        }
         try {
             executeCommand(currentInput);
         } catch (IllegalNumberOfArguments e) {
