a289853ec94254380ba88f795dcfbc20585ca08b
leongxingyu <leongxingyu02@gmail.com>
2024-02-18 16:05:22 +0800

    Implement ArrayList

---
+++ src/main/java/TaskList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+public class TaskList {
+    public ArrayList<Task> taskList;
+    public TaskList() {
+        taskList = new ArrayList<>();
+    }
+    public void addToList (Task task) {
+        taskList.add(task);
+    }
+    public int getTaskCount () {
+        return taskList.size();
+    }
+}
