2dca5ab40df8273b1933aa073901712c627ef139
yuki-zmstr <yukihide99@gmail.com>
2024-02-18 15:24:39 +0800

    Add read and write functionality

--- src/main/java/yuki/Yuki.java
+++ src/main/java/yuki/Yuki.java
@@ -6,7 +6,11 @@ import yuki.task.Event;
 import yuki.task.Task;
 import yuki.task.Todo;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileWriter;
 public class Yuki {
 
     private static final int MAX_NUM_TASKS = 100;
@@ -20,6 +24,49 @@ public class Yuki {
 
     private static String[] data;
     private static String description;
+
+    private static final String PATH_TO_FILE = "data/tasks.txt";
+
+    public static void readFile() throws FileNotFoundException {
+        File f = new File(PATH_TO_FILE);
+        Scanner s = new Scanner(f);
+        String line;
+        while (s.hasNext()) {
+            line = s.nextLine();
+            if (line.charAt(7) == 'T') {
+                Task t = new Todo(line.substring(10));
+                tasks[taskCount] = t;
+            } else if (line.charAt(7) == 'D') {
+                Task t = new Deadline(line.substring(10));
+                tasks[taskCount] = t;
+            } else if (line.charAt(7) == 'E') {
+                Task t = new Event(line.substring(10));
+                tasks[taskCount] = t;
+            } else {
+                System.out.println("error in input file");
+            }
+            taskCount++;
+        }
+    }
+
+    public static void writeFile() throws IOException {
+        File dataDirectory = new File("data");
+        if (!dataDirectory.exists()) {
+            boolean directoryCreated = dataDirectory.mkdirs(); // create directory to save in.
+            if (!directoryCreated) {
+                // Handle the case where directory creation fails
+                throw new IOException("Failed to create the 'data' directory.");
+            }
+        }
+
+        FileWriter fw = new FileWriter(PATH_TO_FILE);
+
+        for (int i = 0; i < taskCount; i++) {
+            fw.write((i + 1) + ".[" + tasks[i].getStatusIcon() + "] "
+                    + tasks[i].taskType + " " + tasks[i].description + "\n");
+        }
+        fw.close();
+    }
 
     public static void listTasks() {
         System.out.println("Wake up your idea and do these tasks:");
@@ -74,6 +121,12 @@ public class Yuki {
     }
 
     public static void main(String[] args) {
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("Txt file of tasks not found, will create one");
+        }
+
         Utils.printWelcomeMessage();
 
         String line;
@@ -131,6 +184,11 @@ public class Yuki {
 
             Utils.printLine();
             line = in.nextLine();
+        }
+        try {
+            writeFile();
+        } catch (IOException e) {
+            System.out.println("Something went wrong: " + e.getMessage());
         }
         Utils.printExitMessage();
     }
