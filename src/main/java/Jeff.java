d87260b66da9278829941cce6b2296e967696f01
B1G-SAM <54065426+B1G-SAM@users.noreply.github.com>
2024-02-18 16:29:22 +0800

    fix buggy main file

--- src/main/java/Jeff.java
+++ src/main/java/Jeff.java
@@ -1,6 +1,6 @@
 
 public class Jeff {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws JeffException.InvalidKeywordException {
         List list = new List();
         list.generateSizeOfList(100);
         int taskIndex;
@@ -11,52 +11,61 @@ public class Jeff {
 
 
         boolean isChatting = true;
-        while (isChatting){
-            String userInput = Parser.getUserInput();
-            String userFirstWord = Parser.getFirstWord(userInput);
-
-            switch (userFirstWord){
-            case "list":
-                list.listTasks();
-                break;
-
-            case "bye":
-                isChatting = false;
-                break;
-
-            case "mark":
-                taskIndex = Parser.getFirstInt(userInput);
-                list.markIndex(taskIndex);
-                break;
-
-            case "unmark":
-                taskIndex = Parser.getFirstInt(userInput);
-                list.unmarkIndex(taskIndex);
-                break;
-
-            case "deadline":
-                description = Parser.extractDescription(userInput);
-                String deadLine = Parser.extractStartTime(userInput);
-                list.insertTask(new Deadline(description, deadLine));
-                break;
+            while (isChatting) {
+                try{
+                String userInput = Parser.getUserInput();
+                String userFirstWord = Parser.getFirstWord(userInput);
 
 
-            case "event":
-                description = Parser.extractDescription(userInput);
-                String start = Parser.extractStartTime(userInput);
-                String end = Parser.extractEndTime(userInput);
-                list.insertTask(new Event(description, start, end));
-                break;
+                switch (userFirstWord) {
+                    case "list":
+                        list.listTasks();
+                        break;
+
+                    case "bye":
+                        isChatting = false;
+                        break;
+
+                    case "mark":
+                        taskIndex = Parser.getFirstInt(userInput);
+                        list.markIndex(taskIndex);
+                        break;
+
+                    case "unmark":
+                        taskIndex = Parser.getFirstInt(userInput);
+                        list.unmarkIndex(taskIndex);
+                        break;
+
+                    case "deadline":
+                        description = Parser.extractDescription(userInput);
+                        String deadLine = Parser.extractStartTime(userInput);
+                        list.insertTask(new Deadline(description, deadLine));
+                        break;
 
 
-            case "todo":
-                list.insertTask(new Todo(Parser.extractDescription(userInput)));
-                break;
+                    case "event":
+                        description = Parser.extractDescription(userInput);
+                        String start = Parser.extractStartTime(userInput);
+                        String end = Parser.extractEndTime(userInput);
+                        list.insertTask(new Event(description, start, end));
+                        break;
 
-            default:
-                System.out.println("Hi, please use a correct keyword (todo, event, deadline) to add an item to the " +
-                        "list");
-                break;
+
+                    case "todo":
+                        list.insertTask(new Todo(Parser.extractDescription(userInput)));
+                        break;
+
+                    case "remove":
+                        taskIndex = Parser.getFirstInt(userInput);
+                        list.deleteIndex(taskIndex);
+                        break;
+
+                    default:
+                        throw new JeffException.InvalidKeywordException("");
+                }
+            } catch (JeffException.InvalidKeywordException e) {
+                    throw new JeffException.InvalidKeywordException("SORRY I DONT UNDERSTAND THAT KEYWORD PLEASE" +
+                            " USE todo, deadline or event only. THANKS");
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
