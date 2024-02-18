40dd120342c5fcf69258b3f7a6d9c1b06b87778f
Claribel Ho <hoclaribel@gmail.com>
2024-02-18 18:13:32 +0800

    Add Exceptions to catch incorrect inputs by user

--- src/main/java/CommandManager.java
+++ src/main/java/CommandManager.java
@@ -7,114 +7,93 @@ public class CommandManager {
         taskList = new Task[LIST_SIZE];
         listCounter = 0;
     }
-    public static String identifyCommand(String input) {
-        String inputCommand = input.trim().toLowerCase();
-        String inputTask = input;
-        if (inputCommand.startsWith("done")) {
-            inputCommand = "done";
-            inputTask = input.substring(5);
-        } else if (inputCommand.startsWith("unmark")) {
-            inputCommand = "unmark";
-            inputTask = input.substring(7);
-        } else if (inputCommand.startsWith("todo")) {
-            inputCommand = "todo";
-            inputTask = input.substring(5);
-        } else if (inputCommand.startsWith("deadline")) {
-            inputCommand = "deadline";
-            inputTask = input.substring(9);
-        } else if (inputCommand.startsWith("event")) {
-            inputCommand = "event";
-            inputTask = input.substring(6);
+
+    public static String getCommand(String input) throws EmptyTaskException {
+        if (input.equals("bye") || input.equals("list")) {
+            return input;
         }
-        return inputCommand;
+
+        int spaceIndex = input.indexOf(" ");
+        if (spaceIndex == -1) {
+            throw new EmptyTaskException();
+        }
+        return input.substring(0, spaceIndex);
     }
+    public static boolean isInputBye(String input) {
+        String inputCommand = input;
+        try {
+            inputCommand = getCommand(input);
+            String inputTask;
+            switch (inputCommand) {
+            case "bye":
+                return true;
+            case "list":
+                System.out.println("tasks to be done:");
+                if (listCounter == 0) {
+                    System.out.println("  >> nothing so far :)");
+                    drawLine();
+                    break;
+                }
+                for (int i = 0; i < listCounter; i++)
+                    taskList[i].printTask();
+                drawLine();
+                break;
+            case "done":
+                inputTask = input.substring(5);
+                int listIndex = Integer.parseInt(inputTask) - 1;
+                taskList[listIndex].markDone();
+                drawLine();
+                break;
+            case "unmark":
+                inputTask = input.substring(7);
+                listIndex = Integer.parseInt(inputTask) - 1;
+                taskList[listIndex].markUndone();
+                drawLine();
+                break;
+            case "todo":
+                inputTask = input.substring(5);
+                taskList[listCounter] = new Todo(inputTask, listCounter);
+                taskList[listCounter].printRespond();
+                drawLine();
+                listCounter++;
+                break;
+            case "deadline":
+                inputTask = input.substring(9);
+                //split taskName and dueDate
+                int deadlineIndex = inputTask.indexOf('/');
+                String taskName = inputTask.substring(0, deadlineIndex - 1);
+                String dueDate = inputTask.substring(deadlineIndex + 4);
 
-    public static boolean verifyInput(String input) {
-        String inputCommand = identifyCommand(input);
-        String inputTask = input;
-        if (inputCommand.startsWith("done")) {
-//            inputCommand = "done";
-            inputTask = input.substring(5);
-        } else if (inputCommand.startsWith("unmark")) {
-//            inputCommand = "unmark";
-            inputTask = input.substring(7);
-        } else if (inputCommand.startsWith("todo")) {
-//            inputCommand = "todo";
-            inputTask = input.substring(5);
-        } else if (inputCommand.startsWith("deadline")) {
-//            inputCommand = "deadline";
-            inputTask = input.substring(9);
-        } else if (inputCommand.startsWith("event")) {
-//            inputCommand = "event";
-            inputTask = input.substring(6);
-        }
+                taskList[listCounter] = new Deadline(taskName, listCounter, dueDate);
+                taskList[listCounter].printRespond();
+                drawLine();
+                listCounter++;
+                break;
+            case "event":
+                inputTask = input.substring(6);
+                //split taskName and time frames
+                int startIndex = inputTask.indexOf('/');
+                taskName = inputTask.substring(0, startIndex - 1);
 
-        switch (inputCommand) {
-        case "bye":
-            return true;
-        case "list":
-            System.out.println("tasks to be done:");
-            if (listCounter == 0) {
-                System.out.println("  >> nothing so far :)");
+                String timeLine = inputTask.substring(startIndex + 6);
+                int endIndex = timeLine.indexOf('/');
+
+                String startDate = timeLine.substring(0, endIndex - 1);
+                String endDate = timeLine.substring(endIndex + 4);
+
+                taskList[listCounter] = new Event(taskName, listCounter, startDate, endDate);
+                taskList[listCounter].printRespond();
+                drawLine();
+                listCounter++;
+                break;
+            default:
+                System.out.println("  !! this isn't a command i recognise...\n  sorry, pls try again");
                 drawLine();
                 break;
             }
-            for (int i = 0; i < listCounter; i++)
-                taskList[i].printTask();
+        } catch (EmptyTaskException e) {
+            System.out.println("  !! hmmm, no task was specified.");
             drawLine();
-            break;
-        case "done":
-            int listIndex = Integer.parseInt(inputTask) - 1;
-            taskList[listIndex].markDone();
-            drawLine();
-            break;
-        case "unmark":
-            listIndex = Integer.parseInt(inputTask) - 1;
-            taskList[listIndex].markUndone();
-            drawLine();
-            break;
-        case "todo":
-            taskList[listCounter] = new Todo(inputTask, listCounter);
-            taskList[listCounter].printRespond();
-            drawLine();
-            listCounter++;
-            break;
-        case "deadline":
-            //split taskName and dueDate
-            int deadlineIndex = inputTask.indexOf('/');
-            String taskName = inputTask.substring(0, deadlineIndex - 1);
-            String dueDate = inputTask.substring(deadlineIndex + 4);
-
-            taskList[listCounter] = new Deadline(taskName, listCounter, dueDate);
-            taskList[listCounter].printRespond();
-            drawLine();
-            listCounter++;
-            break;
-        case "event":
-            //split taskName and time frames
-            int startIndex = inputTask.indexOf('/');
-            taskName = inputTask.substring(0, startIndex - 1);
-
-            String timeLine = inputTask.substring(startIndex + 6);
-            int endIndex = timeLine.indexOf('/');
-
-            String startDate = timeLine.substring(0, endIndex - 1);
-            String endDate = timeLine.substring(endIndex + 4);
-
-            taskList[listCounter] = new Event(taskName, listCounter, startDate, endDate);
-            taskList[listCounter].printRespond();
-            drawLine();
-            listCounter++;
-            break;
-        default:
-//            System.out.println("added: " + input);
-//            drawLine();
-//
-//            taskList[listCounter] = new Task(input, listCounter);
-//            listCounter++;
-            System.out.println("  this isn't a command i recognise...\n  sorry, pls try again");
-            drawLine();
-            break;
         }
         return false;
     }
@@ -127,4 +106,5 @@ public class CommandManager {
         }
         System.out.println("");
     }
+
 }
