8d54e69d27a9d30a9107955506cecdc2c62efd57
Mmaxx15 <leongxwmax@gmail.com>
2024-02-18 12:20:43 +0800

    removed todo class

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,12 +1,1 @@
 //seems useless can probably just delete
-public class Todo extends Task {
-    public Todo(String description) {
-        super(description);
-    }
-
-    @Override
-    public  String getType() {
-        return "T";
-    }
-
-}
