499b7938f24011c49337fb13d3ed3c5ae7ef2368
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-02-20 23:06:24 +0800

    Edit try/catch

--- src/main/java/MsChatty.java
+++ src/main/java/MsChatty.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Chatty {
+public class MsChatty {
     public static void printTasks(Task[] tasks, int taskCount) {
         System.out.println("Here are the tasks in your list:");
         int taskNumber = 1;
@@ -10,6 +10,7 @@ public class Chatty {
             taskNumber++;
         }
     }
+
     public static void markAndUnmarkTask(Task[] tasks, int taskNumber, String[] arrayOfCommand) {
         if (arrayOfCommand[0].equals("mark")) {
             System.out.println("Nice! I've marked this task as done:");
@@ -21,45 +22,45 @@ public class Chatty {
         System.out.print(" ");
         tasks[taskNumber].printTask();
     }
-    public static void handleMarkAndUnmarkRequest(String userCommand,String[] arrayOfCommand,Task[] tasks) {
-        arrayOfCommand = userCommand.split(" ",2);
-        if (arrayOfCommand.length < 2 || arrayOfCommand[1].isEmpty()) {
-            System.out.print(Guide.MARK_AND_UNMARK_REQUEST_FORMAT);
-            throw new StringIndexOutOfBoundsException();
-        }
-        int taskNumber = Integer.parseInt(arrayOfCommand[1]);
-        markAndUnmarkTask(tasks, taskNumber - 1, arrayOfCommand);
-    }
-    public static void handleTodoDeadlineAndEvent(String userCommand,int taskCount,String[] arrayOfCommand,Task[] tasks){
-        if (userCommand.startsWith("todo")) {
-            arrayOfCommand = userCommand.split(" ", 2);
+
+    public static void handleMarkAndUnmarkRequest(String userCommand, String[] arrayOfCommand, Task[] tasks, int taskCount) {
+        arrayOfCommand = userCommand.split(" ", 2);
+        try {
             if (arrayOfCommand.length < 2 || arrayOfCommand[1].isEmpty()) {
-                System.out.println(Guide.TODO_REQUEST_FORMAT);
                 throw new StringIndexOutOfBoundsException();
             }
+            int taskNumber = Integer.parseInt(arrayOfCommand[1]);
+            if (taskNumber > taskCount) {
+                throw new NullPointerException();
+            }
+            markAndUnmarkTask(tasks, taskNumber - 1, arrayOfCommand);
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.print(Guide.MARK_AND_UNMARK_REQUEST_FORMAT);
+        } catch (NullPointerException e) {
+            System.out.print(Guide.OUT_OF_BOUND);
+        }
+    }
+
+    public static void handleTodoDeadlineAndEvent(String userCommand, int taskCount, String[] arrayOfCommand, Task[] tasks) {
+        if (userCommand.startsWith("todo")) {
+            arrayOfCommand = userCommand.split(" ", 2);
             tasks[taskCount] = new Todo(arrayOfCommand[1]);
         }
 
         if (userCommand.startsWith("deadline")) {
             arrayOfCommand = userCommand.split("deadline | /by");
-            if (arrayOfCommand.length < 2 || arrayOfCommand[1].isEmpty()) {
-                System.out.println(Guide.DEADLINE_REQUEST_FORMAT);
-                throw new StringIndexOutOfBoundsException();
-            }
             tasks[taskCount] = new Deadline(arrayOfCommand[1], arrayOfCommand[2]);
         }
 
         if (userCommand.startsWith("event")) {
             arrayOfCommand = userCommand.split("event | /from | /to ");
-            if (arrayOfCommand.length < 3 || arrayOfCommand[1].isEmpty() || arrayOfCommand[2].isEmpty()) {
-                System.out.println(Guide.EVENT_REQUEST_FORMAT);
-                throw new StringIndexOutOfBoundsException();
-            }
             tasks[taskCount] = new Event(arrayOfCommand[1], arrayOfCommand[2], arrayOfCommand[3]);
         }
 
         System.out.println("Got it. I've added this task:");
+        tasks[taskCount].printTask();
     }
+
     public static void handleCommand(String userCommand) {
         final int MAX_SIZE = 100;
         Task[] tasks = new Task[MAX_SIZE];
@@ -68,33 +69,65 @@ public class Chatty {
         Scanner in = new Scanner(System.in);
 
         while (!userCommand.equals("bye")) {
-            if (userCommand.equals("list")) {
-                printTasks(tasks, taskCount);
-                userCommand = in.nextLine();
-                continue;
-            } else if (userCommand.contains("unmark")) {
-                handleMarkAndUnmarkRequest(userCommand, arrayOfCommand, tasks);
-                userCommand = in.nextLine();
-                continue;
-            } else if (userCommand.contains("mark")) {
-                handleMarkAndUnmarkRequest(userCommand, arrayOfCommand, tasks);
-                userCommand = in.nextLine();
-                continue;
-            } else if (userCommand.contains("todo") || userCommand.contains("deadline") || userCommand.contains("event")) {
-                handleTodoDeadlineAndEvent(userCommand, taskCount, arrayOfCommand, tasks);
-            } else {
-                System.out.println(Guide.REQUESTS_FORMAT);
-                throw new IllegalArgumentException();
+            String[] words = userCommand.split(" ");
+            try {
+                switch (words[0]) {
+                case "list":
+                    printTasks(tasks, taskCount);
+                    userCommand = in.nextLine();
+                    break;
+                case "unmark":
+                    handleMarkAndUnmarkRequest(userCommand, arrayOfCommand, tasks, taskCount);
+                    userCommand = in.nextLine();
+                    break;
+                case "mark":
+                    handleMarkAndUnmarkRequest(userCommand, arrayOfCommand, tasks, taskCount);
+                    userCommand = in.nextLine();
+                    break;
+                case "todo":
+                    handleTodoDeadlineAndEvent(userCommand, taskCount, arrayOfCommand, tasks);
+                    taskCount++;
+                    System.out.println("Now you have " + taskCount + " task(s) in the list.");
+                    userCommand = in.nextLine();
+                    break;
+                case "deadline":
+                    handleTodoDeadlineAndEvent(userCommand, taskCount, arrayOfCommand, tasks);
+                    taskCount++;
+                    System.out.println("Now you have " + taskCount + " task(s) in the list.");
+                    userCommand = in.nextLine();
+                    break;
+                case "event":
+                    handleTodoDeadlineAndEvent(userCommand, taskCount, arrayOfCommand, tasks);
+                    taskCount++;
+                    System.out.println("Now you have " + taskCount + " task(s) in the list.");
+                    userCommand = in.nextLine();
+                    break;
+                default:
+                    System.out.println(Guide.REQUESTS_FORMAT);
+                    userCommand = in.nextLine();
+                }
+            } catch (IndexOutOfBoundsException e) {
+                switch (words[0]) {
+                case "todo":
+                    System.out.println(Guide.TODO_REQUEST_FORMAT);
+                    userCommand = in.nextLine();
+                    break;
+                case "deadline":
+                    System.out.println(Guide.DEADLINE_REQUEST_FORMAT);
+                    userCommand = in.nextLine();
+                    break;
+                case "event":
+                    System.out.println(Guide.EVENT_REQUEST_FORMAT);
+                    userCommand = in.nextLine();
+                    break;
+                default:
+                    break;
+                }
             }
-
-            tasks[taskCount].printTask();
-            taskCount++;
-            System.out.println("Now you have " + taskCount + " task(s) in the list.");
-            userCommand = in.nextLine();
         }
-
         System.out.println("Bye! Hope to see you again :)");
     }
+
     public static void main(String[] args) {
         System.out.println("Hello! I'm Ms Chatty :)");
         System.out.println("What can I do for you?");
