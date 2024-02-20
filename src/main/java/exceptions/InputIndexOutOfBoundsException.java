7c84ed152dde165320688b3853222afe49144211
JianJiaT <tjjtjn@gmail.com>
2024-02-20 22:11:12 +0800

    Add general exception for out-of-bounds index

---
+++ src/main/java/exceptions/InputIndexOutOfBoundsException.java
@@ -0,0 +1,7 @@
+package exceptions;
+
+public class InputIndexOutOfBoundsException extends Exception {
+    public InputIndexOutOfBoundsException() {
+        super("ERROR: Index out of bounds!");
+    }
+}
