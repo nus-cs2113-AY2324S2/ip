6b9dbb79b9320b3ca2de687127c849221df0ef95
Hafizuddin Bin Aminuddin <hafizuddinaminuddin1@gmail.com>
2024-02-20 14:05:36 +0800

    Implement regression testing
    * .bat test file could not run after putting Cody in package. Updated file path to include /Cody

--- src/main/java/cody/Cody.java
+++ src/main/java/cody/Cody.java
@@ -4,7 +4,7 @@ public class Cody {
 
     // Constants for repeated strings
     private static final String BORDER = "______________________________________________________________\n";
-    private static final String GREET_MESSAGE = " Hey there! I'm cody.Cody, your personal task manager\n"
+    private static final String GREET_MESSAGE = " Hey there! I'm Cody, your personal task manager\n"
             + " Tell me your tasks and I will create a task list for you\n";
     private static final String EXIT_MESSAGE = " Bye. Hope to see you again soon!\n";
 
