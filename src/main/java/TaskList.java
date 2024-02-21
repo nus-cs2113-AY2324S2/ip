d68cae463b703f89f000b73145bea44de7b0a7eb
syj02 <>
2024-02-21 18:45:59 +0800

    feat: Add delete method using collections Modify code to use ArrayList<> for taskList Add delete method in taskList to remove task

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,42 +1,36 @@
+import java.util.ArrayList;
 public class TaskList {
-    protected Task[] list;
+    protected ArrayList<Task> list;
     protected int count;
 
     public TaskList() {
-        this(new Task[100], 0);
-    }
-
-    public TaskList(Task[] list, int count) {
-        this.list = list;
-        this.count = count;
+        this.list = new ArrayList<>();
     }
 
     public void addTask(Task task) {
-        this.list[count] = task;
-        this.count = count + 1;
-        task.sequence = count;
+        this.list.add(task);
     }
 
     public int getCount() {
-        return count;
+        return list.size();
     }
 
     public void printList() {
         System.out.println("Here are the tasks in your list:");
-        for (int i = 0; i < this.count; i++) {
-            System.out.println(this.list[i].getSequence() + "." + this.list[i]);
+        for (int i = 0; i < this.getCount(); i++) {
+            System.out.println(i+1 + "." + this.list.get(i));
         }
     }
 
     public void markAsDone(int sequence) {
-        Task task = this.list[sequence];
+        Task task = this.list.get(sequence);
         task.isDone(true);
         System.out.println("Nice! I've marked this task as done:\n" + task);
 
     }
 
     public void markAsUndone(int sequence) {
-        Task task = this.list[sequence];
+        Task task = this.list.get(sequence);
         task.isDone(false);
         System.out.println("OK, I've marked this task as not done yet:\n" + task);
     }
@@ -85,6 +79,17 @@ public class TaskList {
                 + "\n"
                 + "Now you have "
                 + this.getCount()
+                + " tasks in the list.");
+    }
+
+    public void removeTask(int sequence) {
+        Task task = this.list.get(sequence);
+        this.list.remove(sequence);
+        System.out.println("Noted. I've removed this task:\n"
+                + task
+                + "\n"
+                + "Now you have "
+                + getCount()
                 + " tasks in the list.");
     }
 }
