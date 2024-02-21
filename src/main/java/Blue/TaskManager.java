4fe13dbba7685b15ded86496a961e3458239caff
nkotaa <e0957774@u.nus.edu>
2024-02-21 21:53:11 +0800

    Add Skeleton Ability To Load Tasks From File

--- src/main/java/Blue/TaskManager.java
+++ src/main/java/Blue/TaskManager.java
@@ -2,14 +2,16 @@ package Blue;
 
 import java.io.File;
 import java.io.FileWriter;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class TaskManager extends Blue {
     // assume no more than 100 tasks for now
     public static final String DATA_DIR_PATH = "data";
     public static final String TASK_FILE_PATH = DATA_DIR_PATH + "/tasks.txt";
     public static final int MAX_TASKS = 100;
-    private static Task[] tasks = new Task[MAX_TASKS];
+    private static Task[] tasks = loadTasks();
     private static int numTasks = 0;
     private Input request;
 
@@ -55,6 +57,50 @@ public class TaskManager extends Blue {
         tasks[numTasks] = task;
         numTasks += 1;
         talk("added: " + task.getDescription());
+    }
+
+    //assume taskFile is in the proper format for now
+    private static Task[] loadTasks() {
+        Task[] restoredTasks = new Task[MAX_TASKS];
+        File taskFile = new File(TASK_FILE_PATH);
+        //if (!taskFile.isFile()) {
+        //    return tasks;
+        //}
+        try {
+            Scanner s = new Scanner(taskFile);
+            while (s.hasNext()) {
+                String[] savedTaskDetails = s.nextLine().split("\\|");
+                Task savedTask = restoreTask(savedTaskDetails);
+                restoredTasks[numTasks] = savedTask;
+                numTasks += 1;
+            }
+            talk("Restored " + numTasks + " tasks.");
+        } catch (FileNotFoundException e) {
+            talk("No saved tasks found, starting from scratch.");
+        } finally {
+            return restoredTasks;
+        }
+    }
+
+    private static Task restoreTask(String[] savedDetails) {
+        Task restoredTask;
+        switch (savedDetails[0]) {
+        case "T":
+            restoredTask = new Task(savedDetails[2]);
+            break;
+        case "D":
+            restoredTask = new Deadline(savedDetails[2], savedDetails[3]);
+            break;
+        case "E":
+            restoredTask = new Event(savedDetails[2], savedDetails[4], savedDetails[3]);
+            break;
+        default:
+            restoredTask = new Task();
+        }
+        if (savedDetails[1].equals("1")) {
+            restoredTask.setDone();
+        }
+        return restoredTask;
     }
 
     private void saveTasks() {
