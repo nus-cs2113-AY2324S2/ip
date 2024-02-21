a4274e6b39fa1e0f5f1376081bc7e82b7fedd6d1
tayponghee <tayponghee@gmail.com>
2024-02-21 16:38:09 +0800

    Revert "Refactored code to fit java convention"
    This reverts commit b7067df4c72002d2ab2029db723e06ff799af45d.

--- src/main/java/DavinciBot.java
+++ src/main/java/DavinciBot.java
@@ -320,9 +320,7 @@ public class DavinciBot {
             String command = tokens[0].toUpperCase();
             boolean isDone = tokens[tokens.length - 1].equals("1");
             Task newTask = commandCases(command, tokens);
-            if (newTask == null) {
-                return null;
-            }
+            if (newTask == null) return null;
             if (isDone) {
                 newTask.markAsDone();
             }
