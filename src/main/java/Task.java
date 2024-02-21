2c0a5666fbc5ef0049370e493266535276d3ca0b
LowTL <e0957642@u.nus.edu>
2024-02-21 15:05:12 +0800

    Add exception handling

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -7,10 +7,22 @@ public class Task {
         this.description = desc;
         this.isDone = false;
         taskCount++;
+        System.out.println("Added: ");
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 
     public void setDone(boolean done) {
         this.isDone = done;
+        if (done) {
+            System.out.println("Well done! This task is marked as done:");
+            this.printTask();
+        } else {
+            System.out.println("This task is marked as undone:");
+        }
+        this.printTask();
     }
 
     public void printTask() {
