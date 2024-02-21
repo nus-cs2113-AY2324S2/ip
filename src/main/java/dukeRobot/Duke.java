95c3697c7778f89e574098f894888f0ecc7b4099
LIN Siyan <slinbh@connect.ust.hk>
2024-02-22 00:44:53 +0800

    Updated according to the reviews

--- src/main/java/dukeRobot/Duke.java
+++ src/main/java/dukeRobot/Duke.java
@@ -10,26 +10,34 @@ public class Duke {
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        final String line = "____________________________________________________________\n";
         String name = "Duck";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________\n" + "Hello! I'm " + name
+        System.out.println(line + "Hello! I'm " + name
                 + "\n" + "What can I do for you?\n" + "\n"
-                + "____________________________________________________________\n");
+                + line);
         Scanner scanner = new Scanner(System.in);
         String userInput = scanner.nextLine();
         userInput = userInput.toLowerCase();
         Task[] tasks = new Task[100];
-        while (!userInput.contains("bye")) {
+        boolean InputBYE = userInput.contains("bye");
+        while (!InputBYE) {
 
-            System.out.println("____________________________________________________________\n");
+            System.out.println(line);
             int indexOfMark;
-            if (userInput.contains("list")) {
+            boolean InputLIST = userInput.contains("list");
+            boolean InputMARK = userInput.contains("mark");
+            boolean InputUNMARK = userInput.contains("unmark");
+            boolean InputDEADLINE = userInput.contains("deadline");
+            boolean InputTODO = userInput.contains("todo");
+            boolean InputEVENT = userInput.contains("event");
+            if (InputLIST) {
                 for (int i = 0; i < Task.numOfTask; i++) {
                     System.out.println((i+1) + "." + tasks[i]);
                 }
 
-            } else if (userInput.contains("mark")) {
-                if (userInput.contains("unmark")) {
+            } else if (InputMARK) {
+                if (InputUNMARK) {
 
                     indexOfMark = Integer.parseInt(userInput.split(" ")[1]) - 1;
                     tasks[indexOfMark].markAsUndone();
@@ -39,7 +47,7 @@ public class Duke {
                     tasks[indexOfMark].markAsDone();
                 }
             } else {
-                if (userInput.contains("deadline")) {
+                if (InputDEADLINE) {
                     try {
                         String descriptionAndBy = userInput.split(" ", 2)[1];
                         try {
@@ -48,32 +56,35 @@ public class Duke {
                             tasks[Task.numOfTask] = new Deadline(Description, By);
                         } catch (ArrayIndexOutOfBoundsException e) {
                             System.out.println("OOPS!!! The by of a deadline cannot be empty.");
-                            System.out.println("____________________________________________________________\n");
+                            System.out.println(line);
                             userInput = scanner.nextLine();
                             userInput = userInput.toLowerCase();
+                            InputBYE = userInput.contains("bye");
                             continue;
                         }
                     } catch (ArrayIndexOutOfBoundsException e) {
                         System.out.println("OOPS!!! The description and by of a deadline cannot be empty.");
-                        System.out.println("____________________________________________________________\n");
+                        System.out.println(line);
                         userInput = scanner.nextLine();
                         userInput = userInput.toLowerCase();
+                        InputBYE = userInput.contains("bye");
                         continue;
                     }
                 }
-                else if (userInput.contains("todo")) {
+                else if (InputTODO) {
                     try {
                         String Description = userInput.split(" ", 2)[1];
                         tasks[Task.numOfTask] = new ToDo(Description);
                     } catch (ArrayIndexOutOfBoundsException e) {
                         System.out.println("OOPS!!! The description of a todo cannot be empty.");
-                        System.out.println("____________________________________________________________\n");
+                        System.out.println(line);
                         userInput = scanner.nextLine();
                         userInput = userInput.toLowerCase();
+                        InputBYE = userInput.contains("bye");
                         continue;
                     }
                 }
-                else if (userInput.contains("event")) {
+                else if (InputEVENT) {
                     String descriptionAndRange = userInput.split(" ", 2)[1];
                     String Description = descriptionAndRange.split("/",3)[0];
                     String from = descriptionAndRange.split("/",3)[1];
@@ -82,9 +93,10 @@ public class Duke {
                 }
                 else {
                     System.out.println("OOPS!!! I'm sorry, but I don't know what that means :-(");
-                    System.out.println("____________________________________________________________\n");
+                    System.out.println(line);
                     userInput = scanner.nextLine();
                     userInput = userInput.toLowerCase();
+                    InputBYE = userInput.contains("bye");
                     continue;
 
                 }
@@ -93,12 +105,13 @@ public class Duke {
                 System.out.println("Now you have " + Task.numOfTask + " tasks in the list.");
             }
 
-            System.out.println("____________________________________________________________\n");
+            System.out.println(line);
             userInput = scanner.nextLine();
             userInput = userInput.toLowerCase();
+            InputBYE = userInput.contains("bye");
         }
         System.out.println("Bye. Hope to see you again soon!\n"
-                + "\n" + "____________________________________________________________");
+                + "\n" + line);
 
         }
 }
--- src/main/java/dukeRobot/Tasks/ToDo.java
+++ src/main/java/dukeRobot/Tasks/ToDo.java
@@ -4,7 +4,7 @@ package Tasks;
 public class ToDo extends Task {
     public ToDo(String description) throws ArrayIndexOutOfBoundsException{
         super(description);
-        //System.out.println(description + "yes");
+
     }
     @Override
     public String toString() {
