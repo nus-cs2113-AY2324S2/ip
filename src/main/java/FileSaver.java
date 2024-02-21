bdfbd779d67d83d55261e428d08eca4cc3814786
Lin Htet <earthangle007@gmail.com>
2024-02-21 10:10:25 +0800

    Create new file to save data to a File

--- src/main/java/FileSaver.java
+++ src/main/java/FileSaver.java
@@ -5,27 +5,24 @@ import java.util.Scanner;
 
 public class FileSaver {
     protected static final String DATA_PATH = "./data/tonytask.txt";
-
+    protected static File file = new File(DATA_PATH);
     public static void checkFileExists() {
 
     }
-    public static void saveData() {
+
+    public static void saveTodo(Task[] tasks) {
 
     }
 
-    public static void saveTodo(ArrayList<Task> tasks) {
+    public static void saveDeadline(Task[] tasks) {
 
     }
 
-    public static void saveDeadline(ArrayList<Task> tasks) {
+    public static void saveEvent(Task[] tasks) {
 
     }
 
-    public static void saveEvent(ArrayList<Task> tasks) {
-
-    }
-
-    public static void saveMark(ArrayList<Task> tasks) {
+    public static void saveMark(Task[] tasks) {
 
     }
 }
