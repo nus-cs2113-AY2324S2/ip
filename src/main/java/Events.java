46d6cc1a2144256dc43f43305362c8ad3de19595
shawnpong <110764881+shawnpong@users.noreply.github.com>
2024-02-21 17:17:30 +0800

    Add save functionality, gitignore list data

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -3,11 +3,21 @@ public class Events extends Task {
     protected String from;
     protected String to;
 
-    public Events(String description, String from, String to) {
+    public Events(String description, String from, String to, Boolean isDone) {
         super(description);
         this.from = from;
         this.to = to;
+        this.isDone = isDone;
     }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
 
     @Override
     public String toString() {
