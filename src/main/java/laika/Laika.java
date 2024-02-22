37904c1d2bc50cff301c2c8708c30cdb6750742d
cirelesna <anseleric29@gmail.com>
2024-02-22 19:38:16 +0800

    Resolved Conflicts

--- src/main/java/laika/Laika.java
+++ src/main/java/laika/Laika.java
@@ -1,6 +1,10 @@
 package laika;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
 
 public class Laika {
 
@@ -16,11 +20,12 @@ public class Laika {
         System.out.println(taskList.get(taskNumber-1));
     }
 
-    public static void deleteTask(ArrayList<Task> taskList, String[] words){
+    public static void deleteTask(ArrayList<Task> taskList,ArrayList<String> lines, String[] words){
         int taskNumber = Integer.parseInt(words[1]);
         System.out.println("Laika: Gotcha! I've dealt with this task:" + System.lineSeparator()
                             + taskList.get(taskNumber-1));
         taskList.remove(taskNumber-1);
+        lines.remove(taskNumber-1);
     }
 
     public static void addTask(ArrayList<Task> taskList, String line) throws LaikaException {
@@ -55,12 +60,46 @@ public class Laika {
         }
     }
 
-    public static void main(String[] args) {
+
+    public static void save(String[] lines,int count) throws IOException{
+        FileWriter myWriter = new FileWriter("laika.txt");
+        for (int i = 0; i<count;i++){
+            myWriter.write(lines[i] + System.lineSeparator());
+
+        }
+        myWriter.close();
+
+    }
+
+    public static void main(String[] args) throws IOException {
         String line;
         Scanner in = new Scanner(System.in);
         ArrayList<Task> taskList = new ArrayList<>();
+        ArrayList<String> lines = new ArrayList<>();
+        File f = new File("laika.txt");
         int count = 0;
         boolean isConvoOngoing = true;
+
+        if (f.createNewFile()){
+            System.out.println("Laika: Save file created!");
+        }
+
+        try {
+            Scanner reader = new Scanner(f);
+            while (reader.hasNext()){
+                String command = reader.nextLine();
+                addTask(taskList,command,count);
+                lines.add(command);
+                count++;
+            }
+        }
+        catch (FileNotFoundException e){
+            System.out.println("Laika: Error while reading save data.");
+        } catch (LaikaException e) {
+            throw new RuntimeException(e);
+        }
+
+
         String logo = " ^..^      /\n"
                 + " /_/\\_____/\n"
                 + "    /\\   /\\\n"
@@ -88,7 +127,7 @@ public class Laika {
                     break;
                 case "delete":
                     try {
-                        deleteTask(taskList, words);
+                        deleteTask(taskList, lines, words);
                     }
                     catch (IndexOutOfBoundsException e) {
                         System.out.println("Laika: You dont have so many tasks!");
@@ -105,6 +144,7 @@ public class Laika {
                         System.out.println("Laika: Hmmmm, I dont understand you?");
                         break;
                     }
+                    lines.add(line);
 
                     System.out.println("Laika: Gotcha! I've added the task for you\n  "
                             + taskList.get(count) + System.lineSeparator()
@@ -113,6 +153,12 @@ public class Laika {
 
 
 
+            }
+            try{
+                save(lines,count);
+            }
+            catch (IOException e){
+                System.out.println(e.getMessage());
             }
         }
 
