d3a280117573aadeefa2dd079344f48b5c223685
nickczh <nickchua@u.nus.edu>
2024-02-22 20:50:21 +0800

    Complete Level 6 (Delete) with A-Collections
    Let's,
    * Use the Java Collections class ArrayList<Task> to store our tasks.

--- src/main/java/Nick.java
+++ src/main/java/Nick.java
@@ -3,6 +3,7 @@ import command.Event;
 import command.Task;
 import command.Todo;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.logging.Logger;
 
@@ -21,7 +22,8 @@ public class Nick {
 
         boolean hasQuit = false;
         String command, commandAction;
-        Task[] userTasks = new Task[TOTAL_TASKS];
+        //Task[] userTasks = new Task[TOTAL_TASKS];
+        ArrayList<Task> userTasks = new ArrayList<>();
         Scanner input = new Scanner(System.in);
 
         while (input.hasNextLine()) {
@@ -36,6 +38,8 @@ public class Nick {
             case "mark":
                 // Fallthrough
             case "unmark":
+                // Fallthrough
+            case "delete":
                 boolean isContinueProgram = continueExecution(command, taskCount, userTasks, ui);
                 if (isContinueProgram) {
                     continue;
@@ -50,7 +54,7 @@ public class Nick {
         }
     }
 
-    public static void taskExecution(String command, String taskType, Task[] userTasks, UserInterface ui) throws NickException {
+    public static void taskExecution(String command, String taskType, ArrayList<Task> userTasks, UserInterface ui) throws NickException {
         Task task;
         String taskName;
         int taskDescriptionIndex = taskType.length() + 1;
@@ -61,8 +65,8 @@ public class Nick {
             try {
                 taskName = command.substring(taskDescriptionIndex);
                 task = new Todo(taskName);
-                userTasks[taskCount] = task;
-                ui.printAddTaskMsg(userTasks[taskCount].toString(), taskCount);
+                userTasks.add(taskCount, task);
+                ui.printAddTaskMsg(userTasks.get(taskCount).toString(), taskCount);
                 taskCount++;
                 break;
             }
@@ -77,8 +81,8 @@ public class Nick {
             taskName = command.substring(taskDescriptionIndex, taskDescriptionEndIndex);
             String deadline = command.substring(deadlineIndex);
             task = new Deadline(taskName, deadline);
-            userTasks[taskCount] = task;
-            ui.printAddTaskMsg(userTasks[taskCount].toString(), taskCount);
+            userTasks.add(taskCount, task);
+            ui.printAddTaskMsg(userTasks.get(taskCount).toString(), taskCount);
             taskCount++;
             break;
         case "event":
@@ -88,8 +92,8 @@ public class Nick {
             String from = command.substring(fromIndex + FROM_OFFSET_IDX, toIndex - 1);
             String to = command.substring(toIndex + TO_OFFSET_IDX);
             task = new Event(taskName, from, to);
-            userTasks[taskCount] = task;
-            ui.printAddTaskMsg(userTasks[taskCount].toString(), taskCount);
+            userTasks.add(taskCount, task);
+            ui.printAddTaskMsg(userTasks.get(taskCount).toString(), taskCount);
             taskCount++;
             break;
         default:
@@ -104,7 +108,7 @@ public class Nick {
 
     }
 
-    public static boolean continueExecution(String command, int taskCount, Task[] userTasks, UserInterface ui) {
+    public static boolean continueExecution(String command, int taskCount, ArrayList<Task> userTasks, UserInterface ui) {
         String commandAction = command.split(" ")[0];
 
         switch (commandAction) {
@@ -115,31 +119,41 @@ public class Nick {
             System.out.println(FORMAT_LINES);
             System.out.println("Here are the tasks in your list:");
             for (int i = 0; i < taskCount; i++) {
-                System.out.println("\t" + Integer.toString(i + 1) + "." + userTasks[i]);
+                System.out.println("\t" + Integer.toString(i + 1) + "." + userTasks.get(i));
             }
             System.out.println(FORMAT_LINES);
             break;
         case ("mark"):
-            userTasks[Integer.parseInt(command.split(" ")[1]) - 1].markAsDone();
+            userTasks.get(Integer.parseInt(command.split(" ")[1]) - 1).markAsDone();
             System.out.println(FORMAT_LINES);
             System.out.println("Nice! I've marked this task as done:");
             System.out.println("\t" +
                     "[" +
-                    userTasks[Integer.parseInt(command.split(" ")[1]) - 1].getStatusIcon() +
+                    userTasks.get(Integer.parseInt(command.split(" ")[1]) - 1).getStatusIcon() +
                     "] " +
-                    userTasks[Integer.parseInt(command.split(" ")[1]) - 1].description);
+                    userTasks.get(Integer.parseInt(command.split(" ")[1]) - 1).description);
             System.out.println(FORMAT_LINES);
             break;
         case ("unmark"):
-            userTasks[Integer.parseInt(command.split(" ")[1]) - 1].markAsUndone();
+            userTasks.get(Integer.parseInt(command.split(" ")[1]) - 1).markAsUndone();
             System.out.println(FORMAT_LINES);
             System.out.println("OK, I've marked this task as not done yet:");
             System.out.println("\t" +
                     "[" +
-                    userTasks[Integer.parseInt(command.split(" ")[1]) - 1].getStatusIcon() +
+                    userTasks.get(Integer.parseInt(command.split(" ")[1]) - 1).getStatusIcon() +
                     "] " +
-                    userTasks[Integer.parseInt(command.split(" ")[1]) - 1].description);
+                    userTasks.get(Integer.parseInt(command.split(" ")[1]) - 1).description);
             System.out.println(FORMAT_LINES);
+            break;
+        case ("delete"):
+            Task taskToDelete = userTasks.get(Integer.parseInt(command.split(" ")[1]) - 1);
+            Nick.taskCount--;
+            System.out.println(FORMAT_LINES);
+            System.out.println("Sure thing! I've removed this task:");
+            System.out.println("\t" + taskToDelete);
+            System.out.println("Now you have " + Nick.taskCount + " tasks in the list.");
+            System.out.println(FORMAT_LINES);
+            userTasks.remove(taskToDelete);
             break;
         }
 
