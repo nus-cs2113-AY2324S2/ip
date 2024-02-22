17b2bacb6a5dad593cc3c6b8a8033986f4e25626
yuhengr <e0957964@u.nus.edu>
2024-02-23 05:42:56 +0800

    Add delete feature

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 import exception.EkudException;
 
 public class Duke {
@@ -13,7 +14,8 @@ public class Duke {
 
         String userInput;
         Scanner in = new Scanner(System.in);
-        Task[] tasks = new Task[100];
+        ArrayList<Task> tasks = new ArrayList<>();
+        // Task[] tasks = new Task[100];
         int taskCount = 0;
         userInput = in.nextLine();
 
@@ -23,7 +25,7 @@ public class Duke {
             if(userInput.equals("list")){
                 System.out.println("Here are the tasks in your list:");
                 for(int i = 0; i < taskCount; i++){
-                    System.out.println((i + 1) + "." + tasks[i]);
+                    System.out.println((i + 1) + "." + tasks.get(i));
                 }
             }
             else if(userInputWords[0].equals("mark")){
@@ -33,9 +35,9 @@ public class Duke {
                     }
                     else {
                         int taskIndex = Integer.parseInt(userInputWords[1]) - 1;
-                        tasks[taskIndex].markAsDone();
+                        tasks.get(taskIndex).markAsDone();
                         System.out.println("Nice! I've marked this task as done:");
-                        System.out.println(tasks[taskIndex]);
+                        System.out.println(tasks.get(taskIndex));
                     }
                 }
                 catch (EkudException error) {
@@ -49,9 +51,9 @@ public class Duke {
                     }
                     else {
                         int taskIndex = Integer.parseInt(userInputWords[1]) - 1;
-                        tasks[taskIndex].markAsNotDone();
+                        tasks.get(taskIndex).markAsNotDone();
                         System.out.println("OK, I've marked this task as not done yet:");
-                        System.out.println(tasks[taskIndex]);
+                        System.out.println(tasks.get(taskIndex));
                     }
                 }
                 catch (EkudException error) {
@@ -65,9 +67,9 @@ public class Duke {
                         throw new EkudException();
                     }
                     else {
-                        tasks[taskCount] = new Todo(userInput.substring(dividerPosition + 1));
+                        tasks.add(new Todo(userInput.substring(dividerPosition + 1)));
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(tasks[taskCount]);
+                        System.out.println(tasks.get(taskCount));
                         taskCount++;
                         System.out.println("Now you have " + taskCount + " tasks in the list.");
                     }
@@ -84,9 +86,9 @@ public class Duke {
                     }
                     else {
                         int slashPosition = userInput.indexOf("/by");
-                        tasks[taskCount] = new Deadline(userInput.substring(dividerPosition + 1, slashPosition - 1), userInput.substring(slashPosition + 4));
+                        tasks.add(new Deadline(userInput.substring(dividerPosition + 1, slashPosition - 1), userInput.substring(slashPosition + 4)));
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(tasks[taskCount]);
+                        System.out.println(tasks.get(taskCount));
                         taskCount++;
                         System.out.println("Now you have " + taskCount + " tasks in the list.");
                     }
@@ -105,9 +107,9 @@ public class Duke {
                         int fromPosition = userInput.indexOf("/from");
                         int toPosition = userInput.indexOf("/to");
 
-                        tasks[taskCount] = new Event(userInput.substring(dividerPosition + 1, fromPosition - 1), userInput.substring(fromPosition + 6, toPosition - 1), userInput.substring(toPosition + 4));
+                        tasks.add(new Event(userInput.substring(dividerPosition + 1, fromPosition - 1), userInput.substring(fromPosition + 6, toPosition - 1), userInput.substring(toPosition + 4)));
                         System.out.println("Got it. I've added this task:");
-                        System.out.println(tasks[taskCount]);
+                        System.out.println(tasks.get(taskCount));
                         taskCount++;
                         System.out.println("Now you have " + taskCount + " tasks in the list.");
                     }
@@ -117,14 +119,17 @@ public class Duke {
                 }
             }
             else if(userInputWords[0].equals("delete")){
-                System.out.println("In delete block.");
                 try {
                     if(userInputWords.length == 1 || Integer.parseInt(userInputWords[1]) > taskCount){
                         throw new EkudException();
                     }
                     else{
-                        int taskIndex = Integer.parseInt(userInputWords[1]);
-                        System.out.println("Task to delete: " + taskIndex);
+                        int taskIndex = Integer.parseInt(userInputWords[1]) - 1;
+                        System.out.println("Noted. I've removed this task:");
+                        System.out.println(tasks.get(taskIndex));
+                        tasks.remove(taskIndex);
+                        taskCount--;
+                        System.out.println("Now you have " + taskCount + " tasks in the list.");
                     }
                 }
                 catch(EkudException error){
