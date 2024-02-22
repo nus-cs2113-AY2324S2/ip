eb7ebe655a39165ec22e7df2912d8dde526e688e
yuhengr <e0957964@u.nus.edu>
2024-02-23 07:10:21 +0800

    Disable opening logo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,12 +3,14 @@ import exception.EkudException;
 
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        */
         System.out.println("I'm Ekud! What can I do for you?");
 
         String userInput;
