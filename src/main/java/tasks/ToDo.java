f42dc655547cddb90275b092e2dac0030cd2fb4d
JianJiaT <tjjtjn@gmail.com>
2024-02-22 22:24:28 +0800

    Add space

--- src/main/java/tasks/ToDo.java
+++ src/main/java/tasks/ToDo.java
@@ -12,7 +12,7 @@ public class ToDo extends Task {
     public ToDo(String name, boolean isDone) {
         super(name, isDone);
     }
-    
+
     @Override
     public String getType() {
         return "T";
