be877c264cde90485938bc3d8d81305b1f0992a7
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-22 14:03:10 +0800

    List class: Add deleteTask method

--- src/main/java/List.java
+++ src/main/java/List.java
@@ -1,23 +1,22 @@
 import java.util.ArrayList;
 
 public class List {
-    //Task[] tasks = new Task[100];
     ArrayList<Task> tasks = new ArrayList<>();
     private int taskCount = 0;
 
     public void addTask(String userInput) throws InvalidArgumentException {
         String taskType = "";
-        if (userInput.startsWith("todo")){
+        if (userInput.startsWith("todo")) {
             taskType = "todo";
-        } else if(userInput.startsWith("deadline")){
+        } else if (userInput.startsWith("deadline")) {
             taskType = "deadline";
         } else if (userInput.startsWith("event")) {
             taskType = "event";
         }
         String description;
-        switch (taskType){
+        switch (taskType) {
         case "todo":
-            if(5 > userInput.length()){
+            if (5 > userInput.length()) {
                 throw new InvalidArgumentException();
             }
             description = userInput.substring(userInput.indexOf(" ") + 1);
@@ -30,7 +29,7 @@ public class List {
             break;
         case "event":
             description = userInput.substring(userInput.indexOf(" ") + 1, userInput.indexOf("/from") - 1);
-            String from = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to") -1);
+            String from = userInput.substring(userInput.indexOf("/from") + 6, userInput.indexOf("/to") - 1);
             String to = userInput.substring(userInput.indexOf("/to") + 4);
             tasks.add(new Event(description, from, to));
             break;
@@ -54,9 +53,17 @@ public class List {
         System.out.println("\t   " + tasks.get(taskNumber));
     }
 
-    public void setAsNotDone(int taskNumber){
+    public void setAsNotDone(int taskNumber) {
         tasks.get(taskNumber).setDone(false);
         System.out.println("\t OK, I've marked this task as not done yet:");
         System.out.println("\t   " + tasks.get(taskNumber));
+    }
+
+    public void deleteTask(int taskNumber) {
+        System.out.println("\t Noted. I've removed this task:");
+        System.out.println("\t   " + tasks.get(taskNumber));
+        tasks.remove(taskNumber);
+        taskCount--;
+        System.out.println("\t Now you have " + taskCount + " tasks in the list.");
     }
 }
