697bfb8265d0541b7b55a0d35dee3dbdfed3aa88
liuy1103 <e0968020@u.nus.edu>
2024-02-22 20:03:39 +0800

    Adjustments to Save events

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -14,6 +14,6 @@ public class Event extends Task {
 
     @Override
     public String toFileFormat() {
-        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + start + " to " + end;
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + start + " | " + end;
     }
 }
