b88163517643b3d8883f22873181cf5e51a529d7
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-02-23 15:29:25 +0800

    Edit taskInFileFormat method

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -4,7 +4,13 @@ public class Todo extends Task {
         super(description);
         this.taskType = "[T]";
     }
-
+    public String taskInFileFormat() {
+        boolean isDone = false;
+        if (getStatusIcon().equals("[X]")) {
+            isDone = true;
+        }
+        return "T | " + (isDone ? "1" : "0") + " | " + this.description + "\n";
+    }
     @Override
     public void printTask() {
         System.out.println(taskType + getStatusIcon() + " " + description);
