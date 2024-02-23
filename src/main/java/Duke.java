9ddae9023beba55ee5c4e28f2bd0fdc77d81e475
chenxk619 <chenxk619@gmail.com>
2024-02-24 02:30:51 +0800

    Level 6 / 7 task + A-Jar

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,19 +1,50 @@
+import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.BufferedWriter;
 
 public class Duke {
-    public static Task[] list = new Task[100];
+    public static ArrayList<Task> list = new ArrayList<>();
+
     public static int taskLength = 0;
     public static final String NEW_LINE = "____________________________________________________________\n";
+    public static final String FILE_PATH = "duke.txt";
 
     //Create a new task
-    public static void createTask(String task) {
-        list[taskLength] = new Deadline(task);
+    public static void createTask(String taskType, String task, boolean quietLoad) {
+        switch (taskType) {
+        case "todo":
+            list.add(new Todo(task));
+            break;
+        case "deadline":
+            list.add(new Deadline(task));
+            break;
+        default:
+            list.add(new Event(task));
+        }
         taskLength++;
-        System.out.println(NEW_LINE + "Okay, I've added: " + task + "\n" + NEW_LINE);
+        if (!quietLoad) {
+            System.out.println(NEW_LINE + "Okay, I've added: " + task + "\n" + NEW_LINE);
+        }
+    }
+
+    public static void deleteTask(int taskNumber) {
+        Task task = list.get(taskNumber);
+        list.remove(taskNumber);
+        taskLength--;
+        try {
+            saveFile();
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+        System.out.println(NEW_LINE + "Okay, I've removed: " + task.description + "\n" + NEW_LINE);
     }
 
     //Handle different types of tasks
-    public static void handleTasks(String input) throws EmptyTaskException{
+    public static void handleTasks(String input) throws EmptyTaskException {
         int index = input.indexOf(" ");
         String taskType = input.substring(0, index);
         String taskContent = input.substring(index);
@@ -26,16 +57,19 @@ public class Duke {
                 String deadlineContent = taskContent.split("/by ")[0];
                 if (deadlineContent.trim().isEmpty()) {
                     throw new EmptyTaskException();
-                }
-                else {
+                } else {
                     String deadlineTiming = taskContent.split("/by ")[1];
-                    taskFinal = taskContent.split("/")[0] + " (by:" + deadlineTiming + ")";
-                    createTask(taskFinal);
+                    taskFinal = taskContent.split("/")[0] + " (by: " + deadlineTiming + ")";
+                    createTask(taskType, taskFinal, false);
+                    saveFile();
                 }
             }
             catch (IndexOutOfBoundsException e) {
                 System.out.println("Cannot parse deadline start date!\n"
                 + "Format: deadline [task] /by [time]");
+            }
+            catch (IOException e) {
+                System.out.println("Error saving file" + e.getMessage());
             }
             break;
 
@@ -45,27 +79,33 @@ public class Duke {
                 String eventContent = taskContent.split("/from ")[0];
                 if (eventContent.trim().isEmpty()) {
                     throw new EmptyTaskException();
-                }
-                else {
+                } else {
                     String eventTiming = taskContent.split("/from ")[1];
                     String eventFrom = eventTiming.split("/to ")[0];
                     String eventTo = eventTiming.split("/to ")[1];
-                    taskFinal = taskContent.split("/")[0] + " (from: " + eventFrom + "to: " + eventTo + ")";
-                    createTask(taskFinal);
+                    taskFinal = taskContent.split("/")[0] + " (from: " + eventFrom + " to: " + eventTo + ")";
+                    createTask(taskType, taskFinal, false);
+                    saveFile();
                 }
-            }
-            catch (IndexOutOfBoundsException e) {
+            } catch (IndexOutOfBoundsException e) {
                 System.out.println("Cannot parse event start/end date!\n"
                 +  "Format: event [task] /from [start time] /to [end time]");
+            } catch (IOException e) {
+                System.out.println("Error saving file" + e.getMessage());
             }
             break;
 
+        //case for tod0
         default:
             if (taskContent.trim().isEmpty()) {
                 throw new EmptyTaskException();
-            }
-            else {
-                createTask(taskContent);
+            } else {
+                try {
+                    createTask(taskType, taskContent, false);
+                    saveFile();
+                } catch (IOException e) {
+                    System.out.println("Error saving file" + e.getMessage());
+                }
             }
         }
     }
@@ -76,44 +116,100 @@ public class Duke {
         System.out.println("Your current tasks");
         for (int i = 0; i < taskLength; i++){
             System.out.print((i + 1) + ". ");
-            list[i].showTask();
-            //System.out.println(list[i].classSpecifics());
+            System.out.println(list.get(i).getTask());
         }
         System.out.print(NEW_LINE);
     }
 
     public static void mark(int taskNumber) throws DuplicateMarkException {
-        if (list[taskNumber - 1].isDone) {
+        if (list.get(taskNumber - 1).isDone) {
             throw new DuplicateMarkException();
-        }
-        else {
-            list[taskNumber - 1].markAsDone();
+        } else {
+            list.get(taskNumber - 1).markAsDone();
             System.out.println("Good job! I've marked task " + taskNumber + " as done");
         }
     }
 
     public static void unMark(int taskNumber){
         System.out.println("I've marked task " + taskNumber + " as not done");
-        list[taskNumber - 1].markNotDone();
+        list.get(taskNumber - 1).markNotDone();
     }
+
+    // Save file when creating/deleting tasks
+    public static void saveFile() throws IOException {
+        File myFile = new File(FILE_PATH);
+        FileWriter fw = new FileWriter(myFile, false);
+        BufferedWriter bw = new BufferedWriter(fw);
+        String postFormat = "";
+        for (Task item : list) {
+            String preFormat = item.getTask();
+            String taskType = preFormat.split("]")[0];
+            String isDone = item.isDone ? "1" : "0";
+
+            if (taskType.equals("[T")) {
+                postFormat = "T | " + isDone + " | " + item.description;
+            } else if (taskType.equals("[D")) {
+                postFormat = "D | " + isDone + " | " + item.description.split("\\(")[0] + " | " + item.getTime();
+            } else {
+                postFormat = "E | " + isDone + " | " + item.description.split("\\(")[0] + " | " + item.getTime();
+            }
+            bw.write(postFormat);
+            bw.newLine();
+        }
+        bw.close();
+    }
+
+    // Load text file to local list
+    private static void loadFile() throws FileNotFoundException {
+        File f = new File(Duke.FILE_PATH); // create a File for the given file path
+        Scanner s = new Scanner(f); // create a Scanner using the File as the source
+        while (s.hasNext()) {
+            String line = s.nextLine();
+            String taskType = line.split(" \\| ")[0];
+            String taskDone = line.split(" \\| ")[1];
+            String taskContent = line.split(" \\| ")[2];
+            //type of task
+            if (taskType.equals("T")) {
+                createTask("todo", taskContent, true);
+            } else if (taskType.equals("D")) {
+                String taskTiming = line.split(" \\| ")[3];
+                createTask("deadline", taskContent + "(by: " + taskTiming + ")", true);
+            } else {
+                String taskTiming = line.split(" \\| ")[3];
+                String taskFrom = taskTiming.split("to")[0];
+                String taskTo = taskTiming.split("to")[1];
+                createTask("event", taskContent + "(from: " + taskFrom + "to" + taskTo + ")", true);
+            }
+        }
+    }
+
     public static void main(String[] args) {
+        //Load file at the start
+        System.out.println("Welcome...loading previous data...");
+        try {
+            loadFile();
+            System.out.println("Done :D");
+        } catch (FileNotFoundException e) {
+            System.out.println("No file found to load!");
+        }
+
         String chat_name = "Sigma";
-        String output = NEW_LINE + " Hello! I'm " + chat_name + "\n"
-                + " What can I do for you?\n" + NEW_LINE;
+        String output = NEW_LINE + "Hello! I'm " + chat_name + "\n"
+                + "What can I do for you?\n" + NEW_LINE;
         System.out.println(output);
+
+        //Takes user inputs
         while (true) {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
             if (input.equals("bye")) {
                 break;
-            }
-            else if (input.equals("list")) {
+            } else if (input.equals("list")) {
                 showTasks();
-            }
-            else if (input.startsWith("mark ")) {
+            } else if (input.startsWith("mark ")) {
                 try {
                     int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                        mark(taskNumber);
+                    mark(taskNumber);
                 }
                 catch (IndexOutOfBoundsException | NullPointerException | NumberFormatException e) {
                     System.out.println("That task does not exist!");
@@ -121,35 +217,43 @@ public class Duke {
                 catch (DuplicateMarkException e) {
                     System.out.println("Task is already marked!");
                 }
-            }
-
-            else if (input.startsWith("unmark ")) {
+                catch (Exception e){
+                    System.out.println("Error marking task :/");
+                }
+            } else if (input.startsWith("unmark ")) {
                 try {
                     int taskNumber = Integer.parseInt(input.split(" ")[1]);
-                    if (taskNumber > 0 && taskNumber <= taskLength ) {
-                        unMark(taskNumber);
-                    }
-                    else {
-                        System.out.println("Out of bounds!");
-                    }
+                    unMark(taskNumber);
+                }
+                catch (IndexOutOfBoundsException | NullPointerException | NumberFormatException e) {
+                    System.out.println("That task does not exist!");
                 }
                 catch (Exception e){
                     System.out.println("Error marking task :/");
                 }
-            }
-
-            else if (input.startsWith("todo ") || input.startsWith("deadline ") || input.startsWith("event ")) {
+            } else if (input.startsWith("todo ") || input.startsWith("deadline ") || input.startsWith("event ")) {
                 try {
                     handleTasks(input);
                 }
                 catch (EmptyTaskException e) {
                     System.out.println("Task cannot be empty!");
                 }
-            }
-
-            else {
+            } else if (input.startsWith("remove ")) {
+                int taskNumber = Integer.parseInt(input.split(" ")[1]);
+                try{
+                    deleteTask(taskNumber - 1);
+                }
+                //catch (IndexOutOfBoundsException | NullPointerException | NumberFormatException e) {
+                //    System.out.println("That task does not exist!");
+                //}
+                catch (Exception e){
+                    System.out.println("Error removing task");
+                }
+            } else {
                 System.out.println(NEW_LINE + "Sorry, I don't recognise that input\n"
-                                    + "Hint: Use todo/event/deadline [task] to list tasks\n" + NEW_LINE );
+                                    + "Hint: Use todo/event/deadline [task] to list tasks\n"
+                                    + "OR Use mark/unmark/remove [task number] to edit tasks\n"
+                                    + NEW_LINE );
             }
         }
         System.out.println(NEW_LINE + "Bye. Hope to see you again soon!\n" + NEW_LINE);
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,11 +1,13 @@
 public class Task {
     protected String description;
     protected boolean isDone;
+    protected String taskType;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
+
 
     public void markAsDone() {
         this.isDone = true;
@@ -15,17 +17,34 @@ public class Task {
         this.isDone = false;
     }
 
-    public void showTask() {
+    public String getTask() {
+        String taskType;
         if (this.getClass().toString().equals("class Todo")) {
-            System.out.print("[T]");
+            taskType = "[T]";
+        } else if (this.getClass().toString().equals("class Deadline")) {
+            taskType = "[D]";
+        } else {
+            taskType = "[E]";
         }
-        else if (this.getClass().toString().equals("class Deadline")) {
-            System.out.print("[D]");
+        this.taskType = taskType;
+        return (taskType + "[" + (isDone ? "X" : " ") + "]" + this.description);
+    }
+
+    public String getTime() {
+        String output = "";
+        try {
+        if (this.taskType.equals("[D]")) {
+            String initial = this.getTask().split("by:")[1];
+            output = initial.split("\\)")[0];
+        } else if (this.taskType.equals("[E]")) {
+            String initialFrom = (this.getTask().split("from:")[1]).split("to:")[0];
+            String initialTo = (this.getTask().split("to:")[1]).split("\\)")[0];
+            output = initialFrom.trim() + " to: " + initialTo.trim();
         }
-        else {
-            System.out.print("[E]");
+        } catch (Exception e) {
+            System.out.println("Error loading data:" + e.getMessage());
         }
-        System.out.println("[" + (isDone ? "X" : " ") + "]" + this.description);
+        return output;
     }
 
 }
