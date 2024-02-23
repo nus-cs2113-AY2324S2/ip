a62eb75079927fa41346f85d6c7b411a8854ce6d
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-02-23 17:22:59 +0800

    Create writeToFile method

--- src/main/java/DataHandler.java
+++ src/main/java/DataHandler.java
@@ -1,2 +1,8 @@
+import java.io.*;
 public class DataHandler {
+    private static void writeToFile(String filePath, String textToAdd) throws IOException {
+        FileWriter fw = new FileWriter(filePath);
+        fw.write(textToAdd);
+        fw.close();
+    }
 }
