b1426262c6fb448cb4214e97a6028de0485a93e2
leongxingyu <leongxingyu02@gmail.com>
2024-02-23 11:12:20 +0800

    Add Find Command and print found tasks

--- src/main/java/gab/command/FindCommand.java
+++ src/main/java/gab/command/FindCommand.java
@@ -1,2 +1,33 @@
-package gab.command;public class FindCommand {
+package gab.command;
+
+import gab.task.Task;
+import gab.task.TaskList;
+import gab.ui.Ui;
+
+public class FindCommand extends Command {
+    public String taskToFind;
+    public FindCommand(String taskToFind) {
+        this.taskToFind = taskToFind;
+    }
+
+    @Override
+    public void execute (String taskName, TaskList taskList) {
+        int taskCount = 0;
+        boolean isFound;
+
+        for (Task task : taskList.taskList) {
+            String[] words = task.getDescription().split("\\s+");
+            for (String word : words) {
+                if (word.trim().equalsIgnoreCase(taskToFind.trim())) {
+                    taskCount++;
+                    isFound = true;
+                    Ui.printFoundTask(task, taskCount, isFound);
+                    break;
+                }
+            }
+        }
+        if (taskCount == 0) {
+            Ui.printFoundTask(null, taskCount, false);
+        }
+    }
 }
