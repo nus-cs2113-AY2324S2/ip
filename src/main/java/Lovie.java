9bc4cf8147b570af87bb7599ca7b4eea7190ec31
nidhi-nayak <nidhirnayak@gmail.com>
2024-02-23 11:38:52 +0800

    Add delete helper function and printer

--- src/main/java/Lovie.java
+++ src/main/java/Lovie.java
@@ -34,6 +34,8 @@ public class Lovie {
                 case "mark":
                     markTaskHelper(input, tasksList);
                     break;
+                case "delete":
+                    deleteTask(input, tasksList);
                 default:
                     String taskType = processedInput.split(" ")[0];
                     Task newTask;
@@ -87,13 +89,32 @@ public class Lovie {
         System.out.println("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
     }
 
+    public static void deleteTask(String input, ArrayList<Task> tasksList) { //create error handling for if string
+        int taskNumber = Integer.parseInt(input.split(" ")[1]) - 1;
+        boolean output = true;
+        if (taskNumber >= tasksList.size() || taskNumber < 0) {
+            print("Sorry, there is no record of a task number " + input.split(" ")[1] +
+                    "\n Can I help you with anything else?");
+        } else {
+            tasksList.remove(taskNumber);
+            Task selectedTask = tasksList.get(taskNumber);
+            deleteTaskPrinter(selectedTask, tasksList);
+        }
+    }
+
+    public static void deleteTaskPrinter(Task selectedTask, ArrayList<Task> tasksList) {
+        print("Okay, no worries. I've deleted this task for you: \n" + "[" + selectedTask.getTaskIcon() + "] [" +
+                selectedTask.getStatusIcon() + "] " + selectedTask.getDescription() +
+                selectedTask.getTimespan() + "\n" + "You now have " + tasksList.size() + "tasks in the list.\n" +
+                "What else can I do for you today?");
+    }
     public static void invalidCommandPrinter() {
         print("Sorry, I can't understand what you are saying.\n" +
                 "Please try again! Your command options are as follows: \n\n" +
                 "(1) To add a todo: todo **description** \n(2) To add an event: event **description** /from **start**" +
                 "/to **end**\n(3) To add a deadline: deadline **description** /by **end**\n(4) To list all tasks: list\n"
                 + "(5) To mark a task as done: mark **task number**\n(6) To unmark a task as not done: unmark " +
-                "**task number**\n(7) To leave our session: bye"
+                "**task number**\n(7) To delete a task: delete **task number**\n(8) To leave our session: bye"
         );
     }
 
