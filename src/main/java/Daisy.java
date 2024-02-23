1819bac64240295fa751c1b42fafcd3646e80c4e
FeathersRe <e0969003@u.nus.edu>
2024-02-23 15:04:41 +0800

    Level-7 Commits

--- src/main/java/Daisy.java
+++ src/main/java/Daisy.java
@@ -6,17 +6,26 @@ import daisy.task.Event;
 import daisy.task.Task;
 import daisy.task.Todo;
 
+import java.io.IOException;
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
 
 public class Daisy {
 
     protected static Task[] tasks = new Task[100];
     protected static int task_no = 0;
 
+    protected static String defaultFileLocation = System.getProperty("user.dir") + "\\src\\main\\java\\daisy\\data\\Daisy.txt";
+
+    protected static String INTRO_PROMPT = "Good day! This is Daisy.\nAny task for today?";
+    protected final static String EXIT_PROMPT = "Ending prompt received. Remember to keep to the deadlines!";
+    protected final static String LINE_BREAK = "____________________________________";
+
     public static void main(String[] args) {
-        final String INTRO_PROMPT = "Good day! This is Daisy.\nAny task for today?";
-        final String EXIT_PROMPT = "Ending prompt received. Remember to keep to the deadlines!";
-        final String LINE_BREAK = "____________________________________";
+
+        loadData();
 
         System.out.println(LINE_BREAK);
         System.out.println(INTRO_PROMPT);
@@ -46,8 +55,7 @@ public class Daisy {
                     break;
                 case "todo":
                     try {
-                        Todo newTodo = new Todo(separate_commands[1]);
-                        addItem(newTodo);
+                        createTodo(separate_commands[1], true, false);
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("Error! No event detected for todo. Try again!");
                     }
@@ -58,8 +66,7 @@ public class Daisy {
                         if (separate_deadlines.length < 2) {
                             throw new IllegalDeadlineFormatException();
                         }
-                        Deadline newDeadline = new Deadline(separate_deadlines[0],separate_deadlines[1]);
-                        addItem(newDeadline);
+                        createDeadline(separate_deadlines[0],separate_deadlines[1], true, false);
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("Error! No event detected for deadline. Try again!");
                     } catch (IllegalDeadlineFormatException e) {
@@ -74,8 +81,7 @@ public class Daisy {
                         if (from == -1 || to == -1 ) {
                             throw new IllegalEventFormatException();
                         }
-                        Event newEvent = new Event(eventLine.substring(0, from),eventLine.substring(from + " /from ".length(), to), eventLine.substring(to+" /to ".length()));
-                        addItem(newEvent);
+                        createEvent(eventLine.substring(0, from),eventLine.substring(from + " /from ".length(), to), eventLine.substring(to+" /to ".length()), true, false);
                     } catch (IndexOutOfBoundsException e) {
                         System.out.println("Error! No event detected for event. Try again!");
                     } catch (IllegalEventFormatException e){
@@ -93,14 +99,93 @@ public class Daisy {
             System.out.println(LINE_BREAK);
             command = in.nextLine();
         }
+
         System.out.println(EXIT_PROMPT);
         System.out.println(LINE_BREAK);
+
+        saveData();
+
     }
 
-    public static void addItem(Task item) {
+    public static void addItem(Task item, boolean printOut) {
         tasks[task_no] = item;
-        System.out.println("daisy.task.Task received! The following has been added to your list of todos:\n" + item);
-        System.out.println(String.format("Now you have %d tasks in your todo list.",task_no+1));
         task_no++;
+
+        if (printOut) {
+            System.out.println("daisy.task.Task received! The following has been added to your list of todos:\n" + item);
+            System.out.println(String.format("Now you have %d tasks in your todo list.", task_no));
+        }
+    }
+
+    public static void createTodo(String taskName, boolean printOut, boolean setDone) {
+        Todo newTodo = new Todo(taskName);
+        if (setDone) {
+            newTodo.setDone();
+        }
+        addItem(newTodo, printOut);
+    }
+
+    public static void createDeadline(String taskName, String taskDeadline, boolean printOut, boolean setDone) {
+        Deadline newDeadline = new Deadline(taskName,taskDeadline);
+        if (setDone) {
+            newDeadline.setDone();
+        }
+        addItem(newDeadline, printOut);
+    }
+
+    public static void createEvent(String taskName, String fromDate, String toDate, boolean printOut, boolean setDone) {
+        Event newEvent = new Event(taskName, fromDate, toDate);
+        if (setDone) {
+            newEvent.setDone();
+        }
+        addItem(newEvent, printOut);
+    }
+
+    public static void loadData() {
+        File taskFile = new File(defaultFileLocation);
+        System.out.println("Please wait while Daisy loads your previous data!");
+        try {
+            Scanner entryReader = new Scanner(taskFile);
+            while (entryReader.hasNextLine()) {
+                String[] entryData = entryReader.nextLine().split(",");
+                boolean setDone = "1".equals(entryData[1]);
+                switch(entryData[0]) {
+                    case "T":
+                        createTodo(entryData[2], false, setDone);
+                        break;
+                    case "D":
+                        createDeadline(entryData[2], entryData[3], false, setDone);
+                        break;
+                    case "E":
+                        createEvent(entryData[2], entryData[3], entryData[4], false, setDone);
+                        break;
+                }
+            }
+            entryReader.close();
+            System.out.println("Data is successfully loaded! Program will now begin.");
+        } catch (FileNotFoundException e) {
+            System.out.println("No previous data found! Daisy will create a new one for this entry!");
+        }
+    }
+
+    public static void saveData() {
+        System.out.println("Daisy will begin saving the data for this entry!");
+        try {
+            FileWriter entryWriter = new FileWriter(defaultFileLocation);
+            for (Task task : tasks) {
+                if (task != null) {
+                    String entryInput = task.save() + "\n";
+                    entryWriter.write(entryInput);
+                }
+                else {
+                    break;
+                }
+            }
+            entryWriter.close();
+            System.out.println("Successfully saved file. Program will now exit.");
+        } catch (IOException e) {
+            System.out.println("Error has occurred when saving. This entry will not be saved!");
+        }
+
     }
 }
--- src/main/java/daisy/task/Task.java
+++ src/main/java/daisy/task/Task.java
@@ -18,4 +18,8 @@ public class Task {
     public String toString() {
         return String.format("[%s] %s", (this.isDone)? "X":" ", this.taskName);
     }
+
+    public String save() {
+        return String.format("%s,%s", (this.isDone)? "1":"0", this.taskName);
+    }
 }
