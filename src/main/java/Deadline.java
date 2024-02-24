f051067d6a807c22d3ef63395320c1232ce28a0f
jasraa <e0968320@u.nus.edu>
2024-02-24 23:39:35 +0800

    Level 7: Added save and load functionality

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,12 +1,21 @@
 class Deadline extends Task {
     private String by;
 
-    public Deadline(String description, String by) {
+    public Deadline(String description, String by, boolean isDone) {
         super(description);
         this.by = by;
+        this.isDone = isDone;
     }
 
     public String getType() {
         return "[D]";
+    }
+    public String toString() {
+        return super.toString() + " (by: " + by + ")";
+    }
+
+    @Override
+    public String toSaveFormat() {
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
     }
 }
--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,11 +1,12 @@
-class Event extends Task {
+public class Event extends Task {
     private String from;
     private String to;
 
-    public Event(String description, String from, String to) {
+    public Event(String description, String from, String to, boolean isDone) {
         super(description);
         this.from = from;
         this.to = to;
+        this.isDone = isDone;
     }
 
     public String getType() {
@@ -14,5 +15,9 @@ class Event extends Task {
 
     public String toString() {
         return super.toString() + " (from: " + from + " to: " + to + ")";
+    }
+    @Override
+    public String toSaveFormat() {
+        return "E | " + (isDone ? "1" : "0") + " | " + description + " | " + from + " | " + to;
     }
 }
