3cffeff2118f5732cd73232872b6e3dba3e58456
Chew Jing Xiang <chewjingxiangjumps@gmail.com>
2024-02-24 22:56:16 +0800

    Update Deadline.java read date & time

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,26 +1,49 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
 
-    protected String by;
+    protected String byString;
+    protected LocalDateTime byDateTime;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            this.byDateTime = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("d/M/yyyy HHmm"));
+        } catch (DateTimeParseException e) {
+            this.byString = by;
+        }
     }
 
     public Deadline(String description, String by, boolean isDone) {
         super(description);
-        this.by = by;
+        try {
+            this.byDateTime = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("d/M/yyyy HHmm"));
+        } catch (DateTimeParseException e) {
+            this.byString = by;
+        }
         if (isDone) {
             markAsDone();
         }
     }
-    // Returns the status icon, description and by of the task
+
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + by + ")";
+        if (byDateTime != null) {
+            return "[D]" + super.toString() + " (by: " + byDateTime.format(DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm a")) + ")";
+        } else {
+            return "[D]" + super.toString() + " (by: " + byString + ")";
+        }
     }
+
     @Override
     public String toFileString() {
-        return "D | " + super.toFileString() + " | " + by;
+        if (byDateTime != null) {
+            return "D | " + super.toFileString() + " | " + byDateTime.format(DateTimeFormatter.ofPattern("d/M/yyyy HHmm"));
+        } else {
+            return "D | " + super.toFileString() + " | " + byString;
+        }
     }
+
 }
