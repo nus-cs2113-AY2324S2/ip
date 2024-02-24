5ee7207214369e348ae200f6382b3361094b666e
vibes-863 <vaibhavdpillai863@outlook.com>
2024-02-24 19:12:55 +0800

    Todo class: Add TASK_TYPE to class

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,7 +1,12 @@
 public class Todo extends Task{
+    private final static char TASK_TYPE = 'T';
 
     public Todo (String description){
         super(description);
+    }
+
+    public char getTaskType(){
+        return TASK_TYPE;
     }
 
     @Override
