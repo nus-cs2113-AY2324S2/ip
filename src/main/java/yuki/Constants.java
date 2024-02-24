e99a18791d9484b947100096d37bd39c1f217266
yuki-zmstr <yukihide99@gmail.com>
2024-02-24 20:42:35 +0800

    Set constants to public

--- src/main/java/yuki/Constants.java
+++ src/main/java/yuki/Constants.java
@@ -1,19 +1,19 @@
 package yuki;
 
 public class Constants {
-    protected static final int LENGTH_TODO_COMMAND = 4;
-    protected static final int LENGTH_DEADLINE_COMMAND = 8;
-    protected static final int LENGTH_EVENT_COMMAND = 5;
+    public static final int LENGTH_TODO_COMMAND = 4;
+    public static final int LENGTH_DEADLINE_COMMAND = 8;
+    public static final int LENGTH_EVENT_COMMAND = 5;
 
-    protected static final String LIST_COMMAND = "list";
-    protected static final String MARK_COMMAND = "mark";
-    protected static final String UNMARK_COMMAND = "unmark";
-    protected static final String DELETE_COMMAND = "delete";
-    protected static final String TODO_COMMAND = "todo";
-    protected static final String DEADLINE_COMMAND = "deadline";
-    protected static final String EVENT_COMMAND = "event";
-    protected static final String HELP_COMMAND = "help";
-    protected static final String EXIT_COMMAND = "exit";
+    public static final String LIST_COMMAND = "list";
+    public static final String MARK_COMMAND = "mark";
+    public static final String UNMARK_COMMAND = "unmark";
+    public static final String DELETE_COMMAND = "delete";
+    public static final String TODO_COMMAND = "todo";
+    public static final String DEADLINE_COMMAND = "deadline";
+    public static final String EVENT_COMMAND = "event";
+    public static final String HELP_COMMAND = "help";
+    public static final String EXIT_COMMAND = "exit";
 
     public static final char TODO_INDICATOR = 'T';
     public static final char DEADLINE_INDICATOR = 'D';
