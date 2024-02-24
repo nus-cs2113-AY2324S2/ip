47954515c8a92929e5f122463cb1c8f58afe822a
yuki-zmstr <yukihide99@gmail.com>
2024-02-24 20:42:21 +0800

    Refactor Input Parser

--- src/main/java/yuki/InputParser.java
+++ src/main/java/yuki/InputParser.java
@@ -2,16 +2,18 @@ package yuki;
 
 public class InputParser {
 
-    // Parses user input
-
-    // These are the prefix strings to define the data type of a command parameter
+    // Prefix strings to define the data type of a command parameter
     private static final String TASK_DATA_PREFIX_DEADLINE = "/by ";
     private static final String TASK_DATA_PREFIX_EVENT_START = "/from ";
     private static final String TASK_DATA_PREFIX_EVENT_END = "/to ";
+    static final String regexMatcher = TASK_DATA_PREFIX_DEADLINE + "|"
+            + TASK_DATA_PREFIX_EVENT_START + "|" + TASK_DATA_PREFIX_EVENT_END;
+
+    public static String parseCommand(String userInput) {
+        return userInput.trim().split(" ")[0];
+    }
 
     public static String[] parseInput(String userInput){
-        final String regexMatcher = TASK_DATA_PREFIX_DEADLINE + "|"
-                + TASK_DATA_PREFIX_EVENT_START + "|" + TASK_DATA_PREFIX_EVENT_END;
         return userInput.trim().split(regexMatcher);
     }
 }
