e5f18add6c1c52eb18cffcb4f5c8b303538f6826
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-02-24 10:29:34 +0800

    Add create file functionality on start

--- src/main/java/DataHandler.java
+++ src/main/java/DataHandler.java
@@ -9,6 +9,9 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class DataHandler {
+
+    public static final String FILE_PATH = "src/main/java/data/data.txt";
+
     private static void writeToFile(String filePath, String textToAdd) throws IOException {
         FileWriter fw = new FileWriter(filePath, true);
         fw.write(textToAdd);
@@ -16,16 +19,21 @@ public class DataHandler {
     }
 
     public static void tasksToFile(Task[] taskList) throws IOException {
+        File f = new File(FILE_PATH);
+        FileWriter fw = new FileWriter(FILE_PATH);
         for(Task task : taskList){
             if (task == null) break;
             String stringToWrite = task.getBadge() + "," + (task.getStatusIcon().equals("X") ? 1 : 0)
-                    + "," + task.description;
-            writeToFile("data/data.txt", stringToWrite);
+                    + "," + task.description + "\n";
+            writeToFile(FILE_PATH, stringToWrite);
         }
     }
 
-    public static Task[] readFileContents(String filePath) throws FileNotFoundException {
+    public static Task[] readFileContents(String filePath) throws IOException {
         File f = new File(filePath);
+        if (!f.exists()){
+            f.createNewFile();
+        }
         Scanner s = new Scanner(f);
         Task[] taskList = new Task[Spike.MAX_TASK];
         for (int i = 0; s.hasNext(); i++) {
