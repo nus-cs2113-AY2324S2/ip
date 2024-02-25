b6bd528d5b0a2356d137307e669085746c526fb7
YHWong20 <yihao.wong@u.nus.edu>
2024-02-25 19:57:45 +0800

    Update Ui to read user input from CLI

--- src/main/java/bob/utils/Ui.java
+++ src/main/java/bob/utils/Ui.java
@@ -1,9 +1,41 @@
 package bob.utils;
 
+import java.util.Scanner;
+import java.util.regex.MatchResult;
+
 public class Ui {
     private static final String WELCOME = " Hello! I'm Bob\n What can I do for you?";
     private static final String EXIT = " Bye. Hope to see you again soon!";
     private static final String SEPARATOR = "____________________________________________________________";
+    private final Scanner inputReader;
+
+    public Ui() {
+        this.inputReader = new Scanner(System.in);
+    }
+
+    /**
+     * Read user command (e.g., list, deadline, bye)
+     */
+    public String readCommand() {
+        return inputReader.next().toUpperCase();
+    }
+
+    public void clearInput() {
+        // Force Scanner to jump to next line
+        inputReader.nextLine();
+    }
+
+    public boolean hasMoreInput() {
+        return inputReader.hasNext();
+    }
+
+    public void findInLine(String regex) {
+        inputReader.findInLine(regex);
+    }
+
+    public MatchResult match() {
+        return inputReader.match();
+    }
 
     public void printLogo() {
         String bobAsciiLogo = " ____        _\n"
