37cc39f742da05e8df77e82b3ffe6db8e6b7fded
yuki-zmstr <yukihide99@gmail.com>
2024-02-26 06:41:14 +0800

    Add method to get description

--- src/main/java/yuki/InputParser.java
+++ src/main/java/yuki/InputParser.java
@@ -9,9 +9,14 @@ public class InputParser {
     static final String regexMatcher = TASK_DATA_PREFIX_DEADLINE + "|"
             + TASK_DATA_PREFIX_EVENT_START + "|" + TASK_DATA_PREFIX_EVENT_END;
     private static final int INDEX_COMMAND = 0;
+    private static final int INDEX_DESCRIPTION= 1;
 
     public static String parseCommand(String userInput) {
         return userInput.trim().split(" ")[INDEX_COMMAND];
+    }
+
+    public static String parseDescription(String userInput) {
+        return userInput.trim().split(" ")[INDEX_DESCRIPTION];
     }
 
     public static String[] parseInput(String userInput){
