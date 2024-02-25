2ee3086c5b8c823e062094bcdd45183a701482a3
PureUsagi <ngjunhancanoe@gmail.com>
2024-02-25 12:55:47 +0800

    Change class name from Misty to Main

--- src/main/java/Misty/Main.java
+++ src/main/java/Misty/Main.java
@@ -5,7 +5,7 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Misty {
+public class Main {
     public static void main(String[] args) {
         try {
             SaveFile.createFiles();
