8ec25cd158b1b9700a44193f5f997db60d708675
ChenKangg <cckcyc@gmail.com>
2024-02-26 20:31:13 +0800

    add JavaDoc comments

--- src/main/java/DukeException.java
+++ src/main/java/DukeException.java
@@ -4,6 +4,16 @@ import java.util.ArrayList;
 
 public class DukeException extends Exception {
 
+    /**
+     * Exception for mark and unmark functions
+     * Check if task index is out of bounds
+     *
+     * @param userInput Text inputted by use in the bot
+     * @param tasks list of tasks
+     * @return returns the index of current task
+     * @throws DukeException if index is out of bounds
+     * @throws ArrayIndexOutOfBoundsException if no index is specified
+     */
     public static int getTaskIndex(String userInput, ArrayList<Task> tasks) throws DukeException {
         try {
             int taskIndex = Integer.parseInt(userInput) - 1;
@@ -19,6 +29,13 @@ public class DukeException extends Exception {
         }
     }
 
+    /**
+     * Exception for todo, deadline and event functions
+     * Check if user input is in correct format
+     *
+     * @param line line of text inputted by use in the bot
+     * @throws DukeException if user input is in the wrong format
+     */
     public static void checkDescription (String line) throws DukeException {
         String[] userInput = line.split(" ");
         if (userInput.length == 1) {
@@ -27,6 +44,13 @@ public class DukeException extends Exception {
         }
     }
 
+    /**
+     * Exception for find function
+     * Check if keyword for find is included
+     *
+     * @param line line of text inputted by use in the bot
+     * @throws DukeException if no keyword is included
+     */
     public static void checkKeyword (String line) throws DukeException {
         String[] userInput = line.split(" ");
         if (userInput.length == 1) {
