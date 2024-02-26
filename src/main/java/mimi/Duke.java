5fa9c7c6900b73e35970e52d11db81ca74e5e53d
justin <justinsohjh@gmail.com>
2024-02-26 17:30:43 +0800

    added ui
    created ui package for dealing with user interface methods

--- src/main/java/mimi/Duke.java
+++ src/main/java/mimi/Duke.java
@@ -1,22 +1,27 @@
 package mimi;
+
 import mimi.classes.Deadline;
 import mimi.classes.Event;
 import mimi.classes.Task;
 import mimi.classes.ToDo;
+
 import mimi.exceptions.MimiException;
 import mimi.exceptions.MimiException.TaskNotFound;
 import mimi.exceptions.MimiException.InsufficientParameters;
 import mimi.exceptions.MimiException.IncorrectFormat;
-import java.util.ArrayList;
 import mimi.exceptions.MimiException.FileCorrupted;
+
+import mimi.helper.Ui;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
+import java.util.ArrayList;
 import java.util.Scanner;
 
+
 public class Duke {
-    final static String APP_NAME = "mimichat";
-    final static int INPUT_LIMIT = 2;
     final static String FILE_PATH = "data/mimi.logs";
 
     // DELIMITERS
@@ -33,26 +38,25 @@ public class Duke {
 
     static Scanner scanner;
 
+    private static Ui my_ui;
 
-    // METHODS TO GET INPUT
-    private static String[] getInput() {
-        String input = scanner.nextLine();
-        return input.split(" ", INPUT_LIMIT);
-    }
+
+
 
     private static void addToDo(String[] inputs) {
-
         try {
+
             if (inputs.length != 2) {
                 throw new InsufficientParameters(MimiException.INSUFFICIENT_TODO_PARAMETERS_MSG);
             }
+
             if (inputs[1].isBlank()) {
                 throw new IncorrectFormat(MimiException.INCORRECT_TODO_FORMAT_MSG);
             }
 
             ToDo toDo = new ToDo(inputs[1]);
             appendIntoTaskList(toDo);
-            printSuccessMessage(toDo);
+            my_ui.printSuccessMessage(toDo, taskList);
         } catch (InsufficientParameters | IncorrectFormat e) {
             System.out.println(e.getMessage());
         }
@@ -69,7 +73,7 @@ public class Duke {
             String[] splitInputs = inputs[1].split(DEADLINE_DELIMITER, 2);
             Deadline deadline = Deadline.processDeadline(splitInputs);
             appendIntoTaskList(deadline);
-            printSuccessMessage(deadline);
+            my_ui.printSuccessMessage(deadline, taskList);
         } catch (InsufficientParameters | IncorrectFormat e) {
             System.out.println(e.getMessage());
         }
@@ -83,7 +87,7 @@ public class Duke {
 
             Event event = Event.processEvent(inputs[1]);
             appendIntoTaskList(event);
-            printSuccessMessage(event);
+            my_ui.printSuccessMessage(event, taskList);
         } catch (InsufficientParameters | IncorrectFormat e) {
             System.out.println(e.getMessage());
         }
@@ -97,30 +101,27 @@ public class Duke {
     public static void listTasks(ArrayList<Task> list) {
         System.out.println("-------------------------------------------");
         System.out.println("Here are the tasks in your list:");
-        for (Task t: list) {
-            System.out.println(formatTask(t, list.indexOf(t)));
-        }
+        my_ui.printTasks(taskList);
         System.out.println("-------------------------------------------");
     }
 
-    public static void deleteTask(ArrayList<Task> list, String[] inputs){
+    public static void deleteTask(ArrayList<Task> list, String[] inputs) {
         try {
             int index = checkValidityOfIndex(inputs);
             Task removedTask = list.get(index);
             list.remove(index);
-            printDeleteMessage(removedTask);
-            
+            my_ui.printDeleteMessage(removedTask, list);
+
         } catch (TaskNotFound | InsufficientParameters | IncorrectFormat e) {
             System.out.println(e.getMessage());
         }
     }
 
     public static void markTask(ArrayList<Task> list, String[] inputs) {
-
         try {
             int index = checkValidityOfIndex(inputs);
             list.get(index).markAsDone();
-            printMarkTask(list, index);
+            my_ui.printMarkTask(list, index);
         } catch (TaskNotFound | InsufficientParameters | IncorrectFormat e) {
             System.out.println(e.getMessage());
         }
@@ -150,7 +151,7 @@ public class Duke {
         try {
             int index = checkValidityOfIndex(inputs);
             list.get(index).markAsUndone();
-            printUnmarkTask(list, index);
+            my_ui.printUnmarkTask(list, index);
         } catch (TaskNotFound |
                  InsufficientParameters |
                  IncorrectFormat e) {
@@ -161,73 +162,17 @@ public class Duke {
 
 
 
-    // METHOD REGARDING PRINT FORMATTING
-    private static void printMarkTask(ArrayList<Task> list, int index) {
-        System.out.println("-------------------------------------------");
-        System.out.println("OK, I've marked this task as done");
-        System.out.println(formatTask(list.get(index), index));
-        System.out.println("-------------------------------------------");
-    }
 
-    private static void printUnmarkTask(ArrayList<Task> list, int index) {
-        System.out.println("-------------------------------------------");
-        System.out.println("OK, I've marked this task as not done yet");
-        System.out.println(formatTask(list.get(index), index));
-        System.out.println("-------------------------------------------");
-    }
 
-    public static void printDescription(String input) {
-        System.out.println("-------------------------------------------");
-        System.out.println(input);
-        System.out.println("-------------------------------------------");
-    }
 
-    private static void printSuccessMessage(Task task) {
-        System.out.println("-------------------------------------------");
-        System.out.println("Got it. I've added this task:");
-        System.out.println("\t" + formatTask(task));
-        System.out.println("Now you have " + Integer.toString(taskList.size()) + " tasks in the list");
-        System.out.println("-------------------------------------------");
-    }
 
-    private static void printDeleteMessage(Task task) {
-        System.out.println("-------------------------------------------");
-        System.out.println("Noted. I've removed this task:");
-        System.out.println("\t" + formatTask(task));
-        System.out.println("Now you have " + Integer.toString(taskList.size()) + " tasks in the list");
-        System.out.println("-------------------------------------------");
-    }
 
-    public static String formatTask(Task task) {
-        return "\t" + task;
-    }
 
-    public static String formatTask(Task task, int index) {
-        String indexNumber = Integer.toString(index + 1);
-        return indexNumber + ". " + task;
-    }
-
-    // STARTUP AND SHUTDOWN SEQUENCES
-    public static void startupSequence() {
-        System.out.println("-------------------------------------------");
-        System.out.println("Hello! I'm " + APP_NAME);
-        System.out.println("What can I do for you?");
-        System.out.println("-------------------------------------------");
-        isRunning = true;
-        scanner = new Scanner(System.in);
-    }
-
-    private static void shutdownSequence() {
-        printDescription("Bye. Hope to see you again soon!");
-        isRunning = false;
-        scanner.close();
-    }
-
-    private static void saveFile(ArrayList<Task> taskList , String filePath){
+    private static void saveFile(ArrayList<Task> taskList, String filePath) {
         try {
             File file = new File(filePath);
             FileWriter writer = new FileWriter(file);
-            for (Task t: taskList) {
+            for (Task t : taskList) {
                 writer.write(t.toFileString() + "\n");
             }
             writer.close();
@@ -242,9 +187,9 @@ public class Duke {
             File file = new File(filePath);
             Scanner fileScanner = new Scanner(file);
             fileScanner.useDelimiter("\n");
-            while(fileScanner.hasNext()){
+            while (fileScanner.hasNext()) {
                 String[] task = fileScanner.next().split("\\" + FILE_DELIMINITER);
-                if(task.length < 3) {
+                if (task.length < 3) {
                     throw new FileCorrupted(MimiException.FILE_CORRUPTED_MSG);
                 }
 
@@ -280,19 +225,18 @@ public class Duke {
                     throw new FileCorrupted(MimiException.FILE_CORRUPTED_MSG);
                 }
 
-                if(taskStatus.equals("true")) {
-                    taskList.get(taskList.size()-1).markAsDone();
+                if (taskStatus.equals("true")) {
+                    taskList.get(taskList.size() - 1).markAsDone();
                 }
             }
             saveFile(taskList, FILE_PATH);
 
         } catch (IOException e) {
             System.out.println("\u001B[31mError:Unable to load file as " + filePath + " does not exists\u001B[0m");
-        } catch(IncorrectFormat | InsufficientParameters | ArrayIndexOutOfBoundsException e) {
+        } catch (IncorrectFormat | InsufficientParameters | ArrayIndexOutOfBoundsException e) {
             throw new FileCorrupted(MimiException.FILE_CORRUPTED_MSG);
         }
     }
-
 
 
     private static ToDo loadToDo(String taskName, String taskStatus) throws FileCorrupted {
@@ -301,11 +245,9 @@ public class Duke {
         if (taskStatus.equals("true")) {
             // mark as done
             toDo.markAsDone();
-        }
-        else if (taskStatus.equals("false")){
+        } else if (taskStatus.equals("false")) {
             toDo.markAsUndone();
-        }
-        else{
+        } else {
             throw new FileCorrupted(MimiException.FILE_CORRUPTED_MSG);
         }
 
@@ -313,22 +255,24 @@ public class Duke {
     }
 
 
+
+
     public static void main(String[] args) {
 
         // Initial welcome message
-        startupSequence();
+        my_ui = new Ui();
 
-        try{
+        try {
             loadFile(FILE_PATH);
-        } catch (FileCorrupted e){
+        } catch (FileCorrupted e) {
             System.out.println(e.getMessage());
         }
 
-        while (isRunning) {
-            String[] inputs = getInput();
+        while (my_ui.isRunning()) {
+            String[] inputs = my_ui.getInput();
             switch (inputs[0]) {
             case "bye":
-                shutdownSequence();
+                my_ui.shutdownSequence();
                 break;
             case "list":
                 listTasks(taskList);
@@ -360,7 +304,6 @@ public class Duke {
                 }
                 break;
             }
-
             saveFile(taskList, FILE_PATH);
         }
     }
---
+++ src/main/java/mimi/helper/Ui.java
@@ -0,0 +1,113 @@
+package mimi.helper;
+
+import mimi.classes.Task;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ui {
+
+    private Boolean isRunning;
+    private Scanner scanner;
+    final int INPUT_LIMIT = 2;
+    final String INPUT_DELIMITER = " ";
+
+    public Ui() {
+        this.isRunning = true;
+        this.scanner = new Scanner(System.in);
+        printWelcomeMessage();
+    }
+
+    private void printWelcomeMessage() {
+        System.out.println("-------------------------------------------");
+        System.out.println("Hello! I'm mimichat");
+        System.out.println("What can I do for you?");
+        System.out.println("-------------------------------------------");
+    }
+
+    public void shutdownSequence() {
+        printDescription("Bye. Hope to see you again soon!");
+        this.setRunning(false);
+        this.scanner.close();
+    }
+
+    public String[] getInput() {
+        String input = this.scanner.nextLine();
+        return input.split(INPUT_DELIMITER, INPUT_LIMIT);
+    }
+
+    public Boolean isRunning() {
+        return isRunning;
+    }
+
+    public void setRunning(Boolean running) {
+        isRunning = running;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    private static String formatTask(Task task) {
+        return "\t" + task;
+    }
+
+    private static String formatTask(Task task, int index) {
+        String indexNumber = Integer.toString(index + 1);
+        return indexNumber + ". " + task;
+    }
+
+    private void printMessage(Task task, ArrayList<Task> taskList){
+        System.out.println("\t" + formatTask(task));
+        System.out.println("Now you have " + Integer.toString(taskList.size()) + " tasks in the list");
+    }
+
+
+    // PUBLIC ACCESSIBLE METHODS FOR PRINTING
+    public void printTasks(ArrayList<Task> taskList){
+        for (Task t : taskList) {
+            System.out.println(formatTask(t, taskList.indexOf(t)));
+        }
+    }
+
+    public void printMarkTask(ArrayList<Task> list, int index) {
+        System.out.println("-------------------------------------------");
+        System.out.println("OK, I've marked this task as done");
+        System.out.println(formatTask(list.get(index), index));
+        System.out.println("-------------------------------------------");
+    }
+
+    public void printUnmarkTask(ArrayList<Task> list, int index) {
+        System.out.println("-------------------------------------------");
+        System.out.println("OK, I've marked this task as not done yet");
+        System.out.println(formatTask(list.get(index), index));
+        System.out.println("-------------------------------------------");
+    }
+
+    public static void printDescription(String input) {
+        System.out.println("-------------------------------------------");
+        System.out.println(input);
+        System.out.println("-------------------------------------------");
+    }
+
+
+    public void printSuccessMessage(Task task, ArrayList<Task> taskList) {
+        System.out.println("-------------------------------------------");
+        System.out.println("Got it. I've added this task:");
+        printMessage(task, taskList);
+        System.out.println("-------------------------------------------");
+    }
+
+    public void printDeleteMessage(Task task, ArrayList<Task> taskList) {
+        System.out.println("-------------------------------------------");
+        System.out.println("Noted. I've removed this task:");
+        printMessage(task, taskList);
+        System.out.println("-------------------------------------------");
+    }
+
+
+}
