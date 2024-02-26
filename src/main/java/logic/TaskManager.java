d50ed827a189552d957233a90b6ba1f0ef77251e
JianJiaT <tjjtjn@gmail.com>
2024-02-27 00:35:45 +0800

    Refactor TaskManager to use Parser

--- src/main/java/logic/TaskManager.java
+++ src/main/java/logic/TaskManager.java
@@ -9,17 +9,9 @@ import exceptions.InputIndexOutOfBoundsException;
 import exceptions.InvalidInputException;
 import exceptions.TaskNoNameException;
 import tasks.Task;
-import tasks.ToDo;
-import tasks.Deadline;
-import tasks.Event;
 
 public class TaskManager {
-    public static final int TODO_LENGTH= 5;
-    public static final int DEADLINE_LENGTH = 9;
-    public static final int EVENT_LENGTH = 6;
-    public static final int BY_LENGTH = 4;
-    public static final int FROM_LENGTH = 6;
-    public static final int TO_LENGTH = 4;
+
     protected int currIndex;
     protected ArrayList<Task> tasks = new ArrayList<>();
 
@@ -27,26 +19,26 @@ public class TaskManager {
         this.currIndex = 0;
     }
 
-    public void addTask(String taskToAdd) throws Exception {
-        String[] taskAsArray = taskToAdd.split(" ");
-        if (taskAsArray.length == 1) {
+    public void addTask(String input) throws Exception {
+        String[] inputAsArray = input.split(" ");
+        if (inputAsArray.length == 1) {
             throw new TaskNoNameException();
         }
-        String taskType = taskAsArray[0];
+        String taskType = inputAsArray[0];
         switch (taskType) {
         case "todo":
-            processToDo(taskToAdd);
+            tasks.add(Parser.processToDo(input));
             break;
         case "deadline":
             try {
-                processDeadline(taskToAdd);
+                tasks.add(Parser.processDeadline(input));
             } catch (DeadlineNoByDateTimeException e) {
                 throw new DeadlineNoByDateTimeException();
             }
             break;
         case "event":
             try {
-                processEvent(taskToAdd);
+                tasks.add(Parser.processEvent(input));
             } catch (EventNoFromDateTimeException e) {
                 throw new EventNoFromDateTimeException();
             } catch (EventNoToDateTimeException e) {
@@ -60,54 +52,7 @@ public class TaskManager {
         }
         printAndIncrementAfterAddTask();
     }
-    
-    private void processToDo(String taskToAdd) {
-        String taskName;
-        taskName = taskToAdd.substring(TODO_LENGTH);
-        tasks.add(new ToDo(taskName));
-    }
 
-    private void processDeadline(String taskToAdd) throws Exception {
-        if (!(taskToAdd.contains("/by "))) {
-            throw new DeadlineNoByDateTimeException();
-        }
-        String taskName;
-        int firstBackslashIndex = taskToAdd.indexOf("/");
-        if (firstBackslashIndex == DEADLINE_LENGTH) {
-            // occurs when task is not given a name
-            throw new TaskNoNameException();
-        }
-        taskName = taskToAdd.substring(DEADLINE_LENGTH, firstBackslashIndex - 1);
-        int byWhenIndex = firstBackslashIndex + BY_LENGTH;
-        String byWhen = taskToAdd.substring(byWhenIndex);
-        tasks.add(new Deadline(taskName, byWhen));
-    }
-
-    private void processEvent(String taskToAdd) throws Exception {
-        if (!(taskToAdd.contains("/from "))) {
-            throw new EventNoFromDateTimeException();
-        }
-        if (!(taskToAdd.contains("/to "))) {
-            throw new EventNoToDateTimeException();
-        }
-        if (taskToAdd.indexOf("/to ") < taskToAdd.indexOf("/from ")) {
-            throw new EventToBeforeFromException();
-        }
-        String taskName;
-        int firstBackslashIndex = taskToAdd.indexOf("/");
-        if (firstBackslashIndex == EVENT_LENGTH) {
-            // occurs when task is not given a name
-            throw new TaskNoNameException();
-        }
-        taskName = taskToAdd.substring(EVENT_LENGTH, firstBackslashIndex - 1);
-        int fromWhenIndex = firstBackslashIndex + FROM_LENGTH;
-        int secondBackslashIndex = taskToAdd.indexOf("/", fromWhenIndex + 1);
-        int toWhenIndex = secondBackslashIndex + TO_LENGTH;
-        String fromWhen = taskToAdd.substring(fromWhenIndex, secondBackslashIndex - 1);
-        String toWhen = taskToAdd.substring(toWhenIndex);
-        tasks.add(new Event(taskName, fromWhen, toWhen));
-    }
-    
     private void printAndIncrementAfterAddTask() {
         System.out.println("Got it. I've added this task:");
         System.out.println(tasks.get(currIndex));
