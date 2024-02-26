a5b38784cb0f9e4cd12e9919777df7e52ab123d7
Tan Dexter <dextboy@outlook.com>
2024-02-26 12:12:56 +0800

    Store deadline dates as LocalDateTime format for Level-8 increment

--- src/main/java/natsu/task/Deadline.java
+++ src/main/java/natsu/task/Deadline.java
@@ -1,11 +1,22 @@
 package natsu.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
+import static natsu.util.Ui.printLine;
+
 public class Deadline extends Task {
     protected String by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        try {
+            LocalDateTime date = LocalDateTime.parse(by.trim(), DateTimeFormatter.ofPattern("dd-MM-yyyy HHmm"));
+            this.by = date.format(DateTimeFormatter.ofPattern("MMM d yyyy hh:mm a"));
+        } catch (DateTimeParseException e) {
+            this.by = by;
+        }
     }
 
     @Override
