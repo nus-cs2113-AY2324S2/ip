61ffef36b310512b91b3f9a32cf23dcb311a2a54
JianJiaT <tjjtjn@gmail.com>
2024-02-27 00:48:16 +0800

    Refactor Dor

--- src/main/java/main/Dor.java
+++ src/main/java/main/Dor.java
@@ -6,35 +6,59 @@ import java.io.IOException;
 import logic.UserInterface;
 import logic.TaskManager;
 import logic.Storage;
+import tasks.Task;
 
 public class Dor {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Dor");
-        System.out.println("What can I do for you?");
+        printWelcomeMessage();
+        Storage storage = null;
         try {
-            start();
+            storage = initialiseStorage();
         } catch (IOException e) {
-            System.out.println("ERROR: Could not create or find dor.txt!");
-            System.out.println("Now exiting Dor...");
+            printErrorExitMessage();
             return;
         }
-        System.out.println("Bye. Hope to see you again soon!");
+        TaskManager taskManager = initialiseTaskManager(storage);
+        start(storage, taskManager);
+        printByeMessage();
     }
 
-    public static void start() throws IOException {
+    public static void start(Storage storage, TaskManager taskManager) {
+        UserInterface UI = new UserInterface(storage, taskManager);
+        UI.processInput();
+    }
+
+    private static Storage initialiseStorage() throws IOException {
         Storage storage = null;
         try {
             storage = new Storage("./data/dor.txt");
         } catch (IOException e) {
             throw new IOException();
         }
+        return storage;
+    }
+
+    private static TaskManager initialiseTaskManager(Storage storage) {
         TaskManager taskManager;
         try {
             taskManager = storage.loadDataFromTextFile();
         } catch (FileNotFoundException e) {
             taskManager = new TaskManager();
         }
-        UserInterface UI = new UserInterface(storage, taskManager);
-        UI.processInput();
+        return taskManager;
+    }
+
+    private static void printWelcomeMessage() {
+        System.out.println("Hello! I'm Dor");
+        System.out.println("What can I do for you?");
+    }
+
+    private static void printByeMessage() {
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void printErrorExitMessage() {
+        System.out.println("ERROR: Could not create or find dor.txt!");
+        System.out.println("Now exiting Dor...");
     }
 }
