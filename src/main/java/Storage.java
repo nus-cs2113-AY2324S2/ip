8147b27234e2fa06b18d1302114c1f0608e34c3e
B1G-SAM <54065426+B1G-SAM@users.noreply.github.com>
2024-02-26 15:10:31 +0800

    Hardcode file path

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Storage {
-    private static String FILE_PATH = "Data/Jeff.txt";
+    private static final String FILE_PATH = "Data/Jeff.txt";
     private static ArrayList<Task> savedList;
     protected static Ui userInterface;
 
@@ -78,7 +78,7 @@ public class Storage {
         return null;
     }
 
-    
+
 
 
     public static void deserializeTasks() {
