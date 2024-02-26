bc1c8031a3e8727fc83636e2b3225e6a82382b45
pqienso <buckslimyuan@gmail.com>
2024-02-26 14:03:21 +0800

    Add date functionality

--- src/main/java/bean/task/Deadline.java
+++ src/main/java/bean/task/Deadline.java
@@ -2,24 +2,36 @@ package bean.task;
 
 import bean.command.exception.NoValueException;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
-    private String by;
+    private LocalDate byDate;
+    private String byString;
 
     public Deadline(String description, String by) throws NoValueException {
         super(description);
         if (by == null) {
             throw new NoValueException();
         }
-        this.by = by;
+
+        try{
+            this.byDate = LocalDate.parse(by);
+            this.byString = this.byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        } catch (DateTimeParseException e) {
+            this.byString = by;
+        }
+
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.toString() + " (by: " + this.by + ")";
+        return "[D]" + super.toString() + " (by: " + this.byString + ")";
     }
 
     public String toCommand() {
-        return "deadline " + description + " /by " + by + " /isDone " + isDone;
+        return "deadline " + description + " /by " + byString + " /isDone " + isDone;
     }
 
 }
