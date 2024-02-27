35facc76fa305d2b1fcfe51cffe4021684336b3a
JianJiaT <tjjtjn@gmail.com>
2024-02-27 22:57:36 +0800

    Add JavaDoc comments for Dor

--- src/main/java/main/Dor.java
+++ src/main/java/main/Dor.java
@@ -6,9 +6,18 @@ import java.io.IOException;
 import logic.UserInterface;
 import logic.TaskManager;
 import logic.Storage;
-import tasks.Task;
 
+/**
+ * Implementation of a chatbot that can track and manage user tasks named Dor
+ */
 public class Dor {
+
+    /**
+     * Main method that starts up the chatbot. Prints welcome message upon start-up and farewell message upon
+     * exit. Quits the program upon failure to create or find the data file located at "./data/dor.txt"
+     *
+     * @param args Unused
+     */
     public static void main(String[] args) {
         printWelcomeMessage();
         Storage storage = null;
@@ -23,11 +32,23 @@ public class Dor {
         printByeMessage();
     }
 
+    /**
+     * Makes the chatbot start reading user input after instantiating an UserInterface object
+     *
+     * @param storage The Storage object containing the data file to save to
+     * @param taskManager The TaskManager object within which tasks will be managed
+     */
     public static void start(Storage storage, TaskManager taskManager) {
         UserInterface UI = new UserInterface(storage, taskManager);
         UI.processInput();
     }
 
+    /**
+     * Returns a Storage object containing the data file located at "./data/dor.txt"
+     *
+     * @return The Storage object containing the data file
+     * @throws IOException On failure to create or find the data file
+     */
     private static Storage initialiseStorage() throws IOException {
         Storage storage = null;
         try {
@@ -38,6 +59,13 @@ public class Dor {
         return storage;
     }
 
+    /**
+     * Returns a task manager containing the tasks loaded from the data file, or an empty task manager if the
+     * data file cannot be loaded
+     *
+     * @param storage The Storage object containing the data file
+     * @return A TaskManager object
+     */
     private static TaskManager initialiseTaskManager(Storage storage) {
         TaskManager taskManager;
         try {
@@ -48,15 +76,24 @@ public class Dor {
         return taskManager;
     }
 
+    /**
+     * Prints welcome message
+     */
     private static void printWelcomeMessage() {
         System.out.println("Hello! I'm Dor");
         System.out.println("What can I do for you?");
     }
 
+    /**
+     * Prints farewell message
+     */
     private static void printByeMessage() {
         System.out.println("Bye. Hope to see you again soon!");
     }
 
+    /**
+     * Prints message prior to quitting the program due to failure to create or find the data file
+     */
     private static void printErrorExitMessage() {
         System.out.println("ERROR: Could not create or find dor.txt!");
         System.out.println("Now exiting Dor...");
