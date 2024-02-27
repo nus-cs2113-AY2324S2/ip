625db473587e49691b8142b5dcd986a84039b4f5
PureUsagi <ngjunhancanoe@gmail.com>
2024-02-27 20:08:58 +0800

    Add date and time formatting to Deadline class.

--- src/main/java/Misty/data/task/Deadline.java
+++ src/main/java/Misty/data/task/Deadline.java
@@ -1,19 +1,34 @@
 package misty.data.task;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
+
 public class Deadline extends Task {
     private String by;
+    private boolean isByDateFormat;
+    private LocalDate byDate;
 
     public Deadline(String taskName, String by) {
         super(taskName);
         this.by = by;
+        try {
+            byDate = LocalDate.parse(by);
+            isByDateFormat = true;
+        } catch (DateTimeParseException e) {
+            isByDateFormat = false;
+        }
     }
 
     public String getBy() {
         return by;
     }
+    public String getByDate() {
+        return byDate.getDayOfWeek() + ", " + byDate.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
 
     @Override
     public String toString() {
-        return "[D]" + getStatus() + " " + getTaskName() + " (by: " + getBy() + ")";
+        return "[D]" + getStatus() + " " + getTaskName() + " (by: " + (isByDateFormat ? getByDate() : getBy()) + ")";
     }
 }
