0022a25cebf34cf73f2acb8722ca544505e80029
itsmejr257 <chanjunrong2@gmail.com>
2024-02-27 21:04:56 +0800

    Add JavaDoc comments
    Add JavaDoc comments to the code

--- src/main/java/junbot/common/TaskList.java
+++ src/main/java/junbot/common/TaskList.java
@@ -27,6 +27,14 @@ public class TaskList {
         return getTasksList().size();
     }
 
+    /**
+     * Adds a deadline task to the task list. This method parses the full user input and obtains the name,
+     * and end date of the given task, and returns the newly added deadline task
+     *
+     * @param description The full deadline command inputted by user
+     * @return The deadline task added to the list.
+     * @throws InvalidInputException If the input is invalid.
+     */
     public Task addDeadline(String description) throws InvalidInputException {
 
         description = description.replace("deadline", "").trim();
@@ -43,6 +51,14 @@ public class TaskList {
         return userTask;
     }
 
+    /**
+     * Adds an event task to the task list. This method parses the full user input and obtains the name,
+     * start date and end date of the given task, and returns the newly added event task
+     *
+     * @param description The full event command inputted by user
+     * @return The event task added to the list.
+     * @throws InvalidInputException If the input does not include a /from and /to
+     */
     public Task addEvent(String description) throws InvalidInputException {
         description = description.replace("event", "").trim();
         if (!description.contains("/from") || !description.contains("/to")) {
@@ -57,6 +73,14 @@ public class TaskList {
         return userTask;
     }
 
+    /**
+     * Adds a todo task to the task list. This method parses the entire full user input and obtains the name
+     * of the todo task, and returns the newly added todo task
+     *
+     * @param description The full todo command inputted by user
+     * @return The todo task added to the list.
+     * @throws InvalidInputException If the input is does not have format todo <task>.
+     */
     public Task addTodo(String description) throws InvalidInputException {
         description = description.replace("todo", "").trim();
         if(description.isEmpty()){
@@ -68,6 +92,12 @@ public class TaskList {
         return userTask;
     }
 
+    /**
+     * Checks if a given list position is valid.
+     *
+     * @param command The input command.
+     * @return True if the list position < size of tasks list and > 0, false otherwise.
+     */
     public boolean isValidListPosition(String command) {
         if (command == null) {
             return false;
@@ -80,12 +110,24 @@ public class TaskList {
         }
     }
 
+    /**
+     * Marks a task as done in the task list.
+     *
+     * @param listPosition The position of the task in the list.
+     * @return The task marked as done.
+     */
     public Task markTaskInList(int listPosition) {
         Task task = this.getTasksList().get(listPosition);
         task.markTask();
         return task;
     }
 
+    /**
+     * Unmarks a previously marked task in the task list.
+     *
+     * @param listPosition The position of the task in the list.
+     * @return The task unmarked.
+     */
     public Task unmarkTaskInList(int listPosition) {
         Task task = this.getTasksList().get(listPosition);
         task.unmarkTask();
--- src/main/java/junbot/parser/Parser.java
+++ src/main/java/junbot/parser/Parser.java
@@ -7,17 +7,37 @@ import java.io.IOException;
 public class Parser {
 
 
+    /**
+     * Converts a list number (as string) to an array index. A task with a displayed list number n would have
+     * a corresponding array index of n-1
+     *
+     * @param listNumberAsString The list number as a string.
+     * @return The corresponding array index.
+     */
     public int convertToArrayIndex(String listNumberAsString) {
         int listNumberAsInt = Integer.parseInt(listNumberAsString);
         int arrayIndex = listNumberAsInt - 1;
         return arrayIndex;
     }
 
+    /**
+     * Removes the command indicator from the user input and returns the remaining user input
+     *
+     * @param command The full command input by the user.
+     * @param commandIndicator The indicator of the command (e.g., "mark", "unmark").
+     * @return The user input without the command indicator.
+     */
     public String removeCommandIndicator(String command, String commandIndicator) {
         String userInput = command.replace(commandIndicator, "").trim();
         return userInput;
     }
 
+    /**
+     * Extracts the command indicator from the full user command.
+     *
+     * @param command The full command input by the user.
+     * @return The command indicator (e.g., "mark", "unmark").
+     */
     public String getCommandIndicator(String command) {
         int i = command.indexOf(' ');
         String commandIndicator;
