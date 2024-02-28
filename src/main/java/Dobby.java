2e2bb551a57e0ecf32ee42f65f279cd38623dad9
Mmaxx15 <leongxwmax@gmail.com>
2024-02-28 14:39:00 +0800

    removeed commented code

--- src/main/java/Dobby.java
+++ src/main/java/Dobby.java
@@ -1,18 +1,9 @@
 public class Dobby {
 
     public static void main(String[] args) {
-//        String logo = " _____        _     _           \n"
-//                + "|  __ \\      | |   | |\n"
-//                + "| |  | | ___ | |__ | |__  _   _ \n"
-//                + "| |  | |/ _ \\| '_ \\| '_ \\| | | |\n"
-//                + "| |__| | |_| | |_) | |_) | |_| |\n"
-//                + "|_____/ \\___/|_.__/|_.__/ \\__, |\n"
-//                + "                           __/ |\n"
-//                + "                          |___/ \n";
-//        System.out.println(logo);
+
         Ui.showLogo();
         Ui.greetUser();
-        //System.out.println("Dobby say's Hello!\nHow can Dobby help?\n");
 
         TaskList taskmanager = new TaskList();
         FileManager fileManager = new FileManager();
