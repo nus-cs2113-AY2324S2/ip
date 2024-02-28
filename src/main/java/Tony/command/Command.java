d4a37b5ccc276e5167d66ee1c59fd23a664cdea9
Lin Htet <earthangle007@gmail.com>
2024-02-28 12:39:38 +0800

    Create new Command interface

---
+++ src/main/java/Tony/command/Command.java
@@ -0,0 +1,11 @@
+package Tony.command;
+
+import Tony.FileManager.FileSaver;
+import Tony.task.Task;
+import Tony.utility.Ui;
+
+import java.util.ArrayList;
+
+public interface Command {
+    void execute(ArrayList<Task> tasks, Ui ui, FileSaver fileSaver);
+}
