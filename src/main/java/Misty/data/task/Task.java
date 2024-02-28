51b1c932860d3a0695c3c256935f62602c76513b
PureUsagi <ngjunhancanoe@gmail.com>
2024-02-28 16:06:34 +0800

    Added JavaDocs to task classes.

--- src/main/java/Misty/data/task/Task.java
+++ src/main/java/Misty/data/task/Task.java
@@ -1,30 +1,59 @@
 package misty.data.task;
 
-public class Task {
+/**
+ * Serves as a template for all tasks.
+ */
+public abstract class Task {
     private String taskName;
     private boolean isDone;
 
+    /**
+     * Constructs Task object - Not used due to class being abstract.
+     *
+     * @param taskName Name of task.
+     */
     public Task(String taskName) {
         this.taskName = taskName;
         isDone = false;
     }
 
+    /**
+     * Returns name of task.
+     *
+     * @return Name of task.
+     */
     public String getTaskName() {
         return taskName;
     }
 
+    /**
+     * Returns whether task is marked as done or not as a string.
+     *
+     * @return Status of task as string.
+     */
     public String getStatus() {
         return (isDone ? "[X]" : "[ ]");
     }
 
+    /**
+     * Returns whether task is marked as done.
+     *
+     * @return True if task is marked as done, else false.
+     */
     public boolean getIsDone() {
         return isDone;
     }
 
+    /**
+     * Sets the task as done.
+     */
     public void setTaskAsDone() {
         isDone = true;
     }
 
+    /**
+     * Sets the task as not done.
+     */
     public void setTaskAsNotDone() {
         isDone = false;
     }
--- src/main/java/Misty/data/task/Todo.java
+++ src/main/java/Misty/data/task/Todo.java
@@ -1,10 +1,23 @@
 package misty.data.task;
 
+/**
+ * Create todo tasks.
+ */
 public class Todo extends Task {
+    /**
+     * Constructs todo object.
+     *
+     * @param taskName Name of todo task.
+     */
     public Todo (String taskName) {
         super(taskName);
     }
 
+    /**
+     * Returns string of how todo task should be printed to screen.
+     *
+     * @return String to be printed to screen.
+     */
     @Override
     public String toString() {
         return "[T]" + getStatus() + " " + getTaskName();
