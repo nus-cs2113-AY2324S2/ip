f296937c78f02616138790026fc543c56038f490
sxfoo <fsx159@gmail.com>
2024-02-28 15:52:59 +0800

    Extract out Ui class

--- src/main/java/Carrot.java
+++ src/main/java/Carrot.java
@@ -2,41 +2,18 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Carrot {
-    public static void main(String[] args) {
-        String logo = "   _____                     _   \n" +
-                        "  / ____|                   | |  \n" +
-                        " | |     __ _ _ __ _ __ ___ | |_ \n" +
-                        " | |    / _` | '__| '__/ _ \\| __|\n" +
-                        " | |___| (_| | |  | | | (_) | |_ \n" +
-                        "  \\_____\\__,_|_|  |_|  \\___/ \\__|\n";
-        System.out.println("Hello from\n" + logo);
-
-        // Chatbot functionality
-        greetUser();
-        printHelpCommand();
-        handleUserInput();
-    }
-
     private static ArrayList<Task> listOfTasks = Storage.loadListOfTasks();
 
-    private static final String MESSAGE_DIVIDER = "-------------------------------------";
-    private static final String COMMAND_LIST =
-            "Available Commands:\n" +
-                    "1. todo <taskdescription> - Add a new todo task\n" +
-                    "2. deadline <taskdescription> /<by> - Add a new task with a deadline <by>\n" +
-                    "3. event <taskdescription> /<from> /<to> - Add a new task that starts <from> and ends <to>\n" +
-                    "4. mark <taskindex> - Mark a task as done\n" +
-                    "5. unmark <taskindex> - Mark a task as not done\n" +
-                    "6. delete <taskindex> - delete a task from the list\n" +
-                    "7. list - List all tasks recorded\n" +
-                    "8. bye - Exit the program\n" +
-                    "9. help - Show available commands";
+    public static void main(String[] args) {
+        Ui.greetUser();
+        Ui.printHelpCommand();
 
-    private static void greetUser() {
-        System.out.println(MESSAGE_DIVIDER);
-        System.out.println("Hello! I'm Carrot!");
-        System.out.println("What can I do for you?");
-        System.out.println(MESSAGE_DIVIDER);
+        while (true) {
+            String userInput = Ui.readUserInput();
+
+            CommandType userCommand = CommandManager.getCommandType(userInput);
+            processUserCommand(userCommand, userInput, Ui.scanner);
+        }
     }
 
     private static void handleUserInput() {
@@ -46,17 +23,17 @@ public class Carrot {
             String userInput = scanner.nextLine().trim();
             CommandType userCommand = CommandManager.getCommandType(userInput);
 
-            processUserCommand(userCommand, userInput, scanner);
+            processUserCommand(userCommand, userInput, Ui.scanner);
         }
     }
 
     private static void processUserCommand(CommandType userCommand, String userInput, Scanner scanner) {
         switch (userCommand) {
         case HELP:
-            printHelpCommand();
+            Ui.printHelpCommand();
             break;
         case LIST:
-            printListItems();
+            Ui.printListItems(listOfTasks);
             break;
         case TODO:
         case DEADLINE:
@@ -72,12 +49,12 @@ public class Carrot {
             break;
         case EXIT:
             scanner.close();
-            sayGoodbye();
+            Ui.sayGoodbye();
             System.exit(0);
             return;
         default:
         case INVALID:
-            printInvalidCommand();
+            Ui.printInvalidCommand();
             break;
         }
     }
@@ -103,7 +80,7 @@ public class Carrot {
 
         listOfTasks.add(task);
         Storage.writeAllTasksToStorage(listOfTasks);
-        printAddedTask(task);
+        Ui.printAddedTask(task, listOfTasks);
     }
 
     private static void deleteTaskFromList(String userInput) {
@@ -117,9 +94,9 @@ public class Carrot {
             listOfTasks.remove(taskIndexInt);
             Storage.writeAllTasksToStorage(listOfTasks);
 
-            printDeletedTask(taskToRemove);
+            Ui.printDeletedTask(taskToRemove, listOfTasks);
         } catch (Exception e) {
-            printInvalidTaskIndexError();
+            Ui.printInvalidTaskIndexError();
         }
     }
 
@@ -134,7 +111,7 @@ public class Carrot {
         try {
             changeTaskStatus(isDone, taskIndexInt);
         } catch (Exception e) {
-            printInvalidTaskIndexError();
+            Ui.printInvalidTaskIndexError();
         }
     }
 
@@ -145,75 +122,6 @@ public class Carrot {
 
         task.setStatus(isDone);
         Storage.writeAllTasksToStorage(listOfTasks);
-        printChangedTaskStatus(isDone, task);
-    }
-
-    private static void printHelpCommand() {
-        System.out.println(MESSAGE_DIVIDER);
-        System.out.println(COMMAND_LIST);
-        System.out.println(MESSAGE_DIVIDER);
-    }
-
-    private static void printInvalidCommand() {
-        System.out.println(MESSAGE_DIVIDER);
-        System.out.println("Invalid command");
-        System.out.println("Type \"help\" to view available commands and syntax");
-        System.out.println(MESSAGE_DIVIDER);
-    }
-
-    private static void printInvalidTaskIndexError() {
-        System.out.println(MESSAGE_DIVIDER);
-        System.out.println("ERROR: Invalid task index. " +
-                "Type \"list\" to see available task indices\n" +
-                "Usage: \"delete/mark/unmark <taskIndex>\"");
-        System.out.println(MESSAGE_DIVIDER);
-    }
-
-    private static void printChangedTaskStatus(boolean isDone, Task task) {
-        System.out.println(MESSAGE_DIVIDER);
-        if (isDone == true) {
-            System.out.println("Nice! I've marked this task as done:");
-        } else {
-            System.out.println("OK, I've marked this task as not done yet:");
-        }
-        System.out.println("\t" + task);
-        System.out.println(MESSAGE_DIVIDER);
-    }
-
-    private static void printAddedTask(Task task) {
-        System.out.println(MESSAGE_DIVIDER);
-        System.out.println("Got it. I've added this task:");
-        System.out.println("\t" + task);
-        System.out.printf("Now you have %d task(s) in the list.%n", listOfTasks.size());
-        System.out.println(MESSAGE_DIVIDER);
-    }
-
-    private static void printDeletedTask(Task task) {
-        System.out.println(MESSAGE_DIVIDER);
-        System.out.println("Noted. I've removed this task:");
-        System.out.println("\t" + task);
-        System.out.printf("Now you have %d task(s) in the list.%n", listOfTasks.size());
-        System.out.println(MESSAGE_DIVIDER);
-    }
-
-    private static void printListItems() {
-        int numberOfTasks = listOfTasks.size();
-
-        System.out.println(MESSAGE_DIVIDER);
-        if (numberOfTasks == 0) {
-            System.out.println("No tasks added");
-        } else {
-            System.out.println("Here are the task(s) in your list:");
-            for (int i = 1; i <= numberOfTasks; i++) {
-                System.out.println(i + "." + listOfTasks.get(i - 1));
-            }
-        }
-        System.out.println(MESSAGE_DIVIDER);
-    }
-
-    private static void sayGoodbye() {
-        System.out.println(MESSAGE_DIVIDER);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(MESSAGE_DIVIDER);
+        Ui.printChangedTaskStatus(isDone, task);
     }
 }
---
+++ src/main/java/Ui.java
@@ -0,0 +1,108 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Ui {
+    private static final String logo =
+            "   _____                     _   \n" +
+            "  / ____|                   | |  \n" +
+            " | |     __ _ _ __ _ __ ___ | |_ \n" +
+            " | |    / _` | '__| '__/ _ \\| __|\n" +
+            " | |___| (_| | |  | | | (_) | |_ \n" +
+            "  \\_____\\__,_|_|  |_|  \\___/ \\__|\n";
+
+    private static final String MESSAGE_DIVIDER = "-------------------------------------";
+    private static final String COMMAND_LIST =
+            "Available Commands:\n" +
+                    "1. todo <taskdescription> - Add a new todo task\n" +
+                    "2. deadline <taskdescription> /<by> - Add a new task with a deadline <by>\n" +
+                    "3. event <taskdescription> /<from> /<to> - Add a new task that starts <from> and ends <to>\n" +
+                    "4. mark <taskindex> - Mark a task as done\n" +
+                    "5. unmark <taskindex> - Mark a task as not done\n" +
+                    "6. delete <taskindex> - delete a task from the list\n" +
+                    "7. list - List all tasks recorded\n" +
+                    "8. bye - Exit the program\n" +
+                    "9. help - Show available commands";
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void greetUser() {
+        System.out.println("Hello from\n" + logo);
+        System.out.println(MESSAGE_DIVIDER);
+        System.out.println("Hello! I'm Carrot!");
+        System.out.println("What can I do for you?");
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static String readUserInput() {
+        return scanner.nextLine().trim();
+    }
+
+    public static void printHelpCommand() {
+        System.out.println(MESSAGE_DIVIDER);
+        System.out.println(COMMAND_LIST);
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static void printInvalidCommand() {
+        System.out.println(MESSAGE_DIVIDER);
+        System.out.println("Invalid command");
+        System.out.println("Type \"help\" to view available commands and syntax");
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static void printInvalidTaskIndexError() {
+        System.out.println(MESSAGE_DIVIDER);
+        System.out.println("ERROR: Invalid task index. " +
+                "Type \"list\" to see available task indices\n" +
+                "Usage: \"delete/mark/unmark <taskIndex>\"");
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static void printChangedTaskStatus(boolean isDone, Task task) {
+        System.out.println(MESSAGE_DIVIDER);
+        if (isDone == true) {
+            System.out.println("Nice! I've marked this task as done:");
+        } else {
+            System.out.println("OK, I've marked this task as not done yet:");
+        }
+        System.out.println("\t" + task);
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static void printAddedTask(Task task, ArrayList<Task> listOfTasks) {
+        System.out.println(MESSAGE_DIVIDER);
+        System.out.println("Got it. I've added this task:");
+        System.out.println("\t" + task);
+        System.out.printf("Now you have %d task(s) in the list.%n", listOfTasks.size());
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static void printDeletedTask(Task task, ArrayList<Task> listOfTasks) {
+        System.out.println(MESSAGE_DIVIDER);
+        System.out.println("Noted. I've removed this task:");
+        System.out.println("\t" + task);
+        System.out.printf("Now you have %d task(s) in the list.%n", listOfTasks.size());
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static void printListItems(ArrayList<Task> listOfTasks) {
+        int numberOfTasks = listOfTasks.size();
+
+        System.out.println(MESSAGE_DIVIDER);
+        if (numberOfTasks == 0) {
+            System.out.println("No tasks added");
+        } else {
+            System.out.println("Here are the task(s) in your list:");
+            for (int i = 1; i <= numberOfTasks; i++) {
+                System.out.println(i + "." + listOfTasks.get(i - 1));
+            }
+        }
+        System.out.println(MESSAGE_DIVIDER);
+    }
+
+    public static void sayGoodbye() {
+        System.out.println(MESSAGE_DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(MESSAGE_DIVIDER);
+    }
+}
