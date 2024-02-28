ff652aa5ad67f7a4bd8e699323c75d89e88720ae
RexYong <rexyyong@gmail.com>
2024-02-29 00:17:14 +0800

    Extract method exyracyTaskAndDueDate from Class Deadline to Class Parser

--- src/main/java/omoh/Parser.java
+++ src/main/java/omoh/Parser.java
@@ -1,6 +1,8 @@
 package omoh;
 
 import omoh.customexceptions.EmptyTaskNumberException;
+import omoh.customexceptions.IllegalDeadlineInput;
+import omoh.tasktypes.Deadline;
 
 public class Parser {
 
@@ -20,6 +22,23 @@ public class Parser {
         }
         int taskNumber = Integer.parseInt(numberString);
         return taskNumber;
+    }
+
+    //returns the Task and due date for Deadline
+    public static Deadline extractTaskAndDueDate (String input) throws IllegalDeadlineInput {
+        //splits string according to /by keyword
+        String[] parts = input.split("/by");
+        //if array is not size 2, means that we are missing inputs
+        if (parts.length != 2) {
+            throw new IllegalDeadlineInput();
+        }
+        String deadlineDescription = parts[0].substring("deadline".length()).trim();
+        String deadlineDueBy = parts[1].trim();
+        Deadline taskAndDeadlineString = new Deadline(deadlineDescription, deadlineDueBy);
+        if (deadlineDescription.isEmpty() || deadlineDueBy.isEmpty()) {
+            throw new IllegalDeadlineInput();
+        }
+        return taskAndDeadlineString;
     }
 
 
--- src/main/java/omoh/tasktypes/Deadline.java
+++ src/main/java/omoh/tasktypes/Deadline.java
@@ -2,6 +2,7 @@ package omoh.tasktypes;
 
 import omoh.Omoh;
 
+import omoh.Parser;
 import omoh.customexceptions.IllegalDeadlineInput;
 
 public class Deadline extends Task {
@@ -19,23 +20,23 @@ public class Deadline extends Task {
         return "[D]" + super.toString() + " (by: " + by + ")";
     }
 
-    public static Deadline extractTaskAndDueDate (String input) throws IllegalDeadlineInput {
-        Deadline taskAndDeadlineString = new Deadline("random","random");
-        //splits string according to /by keyword
-        String[] parts = input.split("/by");
-        //if array is not size 2, means that we are missing inputs
-        if (parts.length != 2) {
-            throw new IllegalDeadlineInput();
-        }
-        //extracts task portion from input, after deadline keyword
-        taskAndDeadlineString.description = parts[0].substring("deadline".length()).trim();
-        //extracts deadline portion from input
-        taskAndDeadlineString.by = parts[1].trim();
-        if (taskAndDeadlineString.description.isEmpty() || taskAndDeadlineString.by.isEmpty()) {
-            throw new IllegalDeadlineInput();
-        }
-        return taskAndDeadlineString;
-    }
+//    public static Deadline extractTaskAndDueDate (String input) throws IllegalDeadlineInput {
+//        Deadline taskAndDeadlineString = new Deadline("random","random");
+//        //splits string according to /by keyword
+//        String[] parts = input.split("/by");
+//        //if array is not size 2, means that we are missing inputs
+//        if (parts.length != 2) {
+//            throw new IllegalDeadlineInput();
+//        }
+//        //extracts task portion from input, after deadline keyword
+//        taskAndDeadlineString.description = parts[0].substring("deadline".length()).trim();
+//        //extracts deadline portion from input
+//        taskAndDeadlineString.by = parts[1].trim();
+//        if (taskAndDeadlineString.description.isEmpty() || taskAndDeadlineString.by.isEmpty()) {
+//            throw new IllegalDeadlineInput();
+//        }
+//        return taskAndDeadlineString;
+//    }
 
     public String getBy() {
         return by;
@@ -52,7 +53,8 @@ public class Deadline extends Task {
 
     public static void addDeadline (String input) {
         try {
-            Deadline extractedInfo = Deadline.extractTaskAndDueDate(input);
+//            Deadline extractedInfo = Parser.extractTaskAndDueDate(input);
+            Deadline extractedInfo = Parser.extractTaskAndDueDate(input);
             Task.tasks.add(new Deadline(extractedInfo.description, extractedInfo.by));
             Task.totalTasks++;
             Deadline.printDeadline(extractedInfo);
