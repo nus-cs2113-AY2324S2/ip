834ecff471cfa071807bd6e41d63daf6057c434b
cheeseong2001 <cheeseong2001@gmail.com>
2024-02-29 18:00:36 +0800

    Package Exceptions together

--- src/main/java/InvalidInputExceptions/InvalidInputException.java
+++ src/main/java/InvalidInputExceptions/InvalidInputException.java
@@ -1,3 +1,5 @@
+package InvalidInputExceptions;
+
 public class InvalidInputException extends Exception {
     public InvalidInputException(String errorMessage) {
         System.out.println(errorMessage);
