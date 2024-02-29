ee06e9eeb6cba235a8df42e0c7ad9be7c9ba32c1
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-02-29 20:01:02 +0800

    Add javadoc for AddCommand

--- src/main/java/gary/command/AddCommand.java
+++ src/main/java/gary/command/AddCommand.java
@@ -8,6 +8,10 @@ import gary.task.TaskList;
 import java.io.File;
 import java.util.ArrayList;
 
+/**
+ * AddCommand class is used to handle user commands to add Tasks. Tasks can
+ * be Todo, Deadline, or Event
+ */
 public class AddCommand extends Command {
 
     private String line;
@@ -15,6 +19,14 @@ public class AddCommand extends Command {
     private ArrayList<Task> todos;
     private String command;
 
+    /**
+     * Constructor for AddCommand class, taking in line, file, todos, and command as parameter.
+     *
+     * @param line user input to the terminal.
+     * @param file txt file to store the tasks.
+     * @param todos array list that stores and manages the task while programme is running.
+     * @param command type of task user wants to add.
+     */
     public AddCommand(String line, File file, ArrayList<Task> todos, String command) {
         this.line = line;
         this.file = file;
@@ -22,6 +34,10 @@ public class AddCommand extends Command {
         this.command = command;
     }
 
+    /**
+     * Handles users' command to add task. If user command is unknown or missing details,
+     * error is handled and message is printed out.
+     */
     @Override
     public void handleCommand() {
         try {
