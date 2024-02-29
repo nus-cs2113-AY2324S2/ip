01945b0d8f7b8a25b5d507b4afaaf9a6ca75b367
Hws2209 <wansing03@gmail.com>
2024-03-01 00:15:01 +0800

    refactor: Extract Parser class

--- src/main/java/ip/main/Duke.java
+++ src/main/java/ip/main/Duke.java
@@ -1,15 +1,12 @@
 package ip.main;
 
-import ip.task.Todo;
-import ip.task.Deadline;
-import ip.task.Event;
-
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class Duke {
     private static Storage storage;
     private static Ui ui = new Ui();
+    private static Parser parser = new Parser();
     private static TaskList tasks = new TaskList(ui);
 
     public static void main(String[] args) {
@@ -35,57 +32,14 @@ public class Duke {
         String line;
         while (true) {
             line = ui.getInput();
-            if (line.equals("bye")) {
-                ui.print("Bye. Hope to see you again soon!");
-                break;
-            }
-
-            if (line.equals("list")) {
-                tasks.printTaskList();
-                continue;
-            }
-
-            if (line.startsWith("mark")) {
-                boolean shouldUpdate = tasks.markTask(line);
-                if (shouldUpdate) {
-                    storage.updateStoredData(tasks);
-                }
-                continue;
-            }
-
-            if (line.startsWith("unmark")) {
-                boolean shouldUpdate = tasks.unmarkTask(line);
-                if (shouldUpdate) {
-                    storage.updateStoredData(tasks);
-                }
-                continue;
-            }
-
-            if (line.startsWith("delete")) {
-                boolean shouldUpdate = tasks.deleteTask(line);
-                if (shouldUpdate) {
-                    storage.updateStoredData(tasks);
-                }
-                continue;
-            }
-
-            boolean shouldUpdate = addTask(line);
+            boolean shouldUpdate = parser.parseInput(line, ui, tasks);
             if (shouldUpdate) {
                 storage.updateStoredData(tasks);
             }
-        }
-    }
-
-    private static boolean addTask(String line) {
-        if (line.startsWith("todo")) {
-            return tasks.addTodo(line);
-        } else if (line.startsWith("deadline")) {
-            return tasks.addDeadline(line);
-        } else if (line.startsWith("event")) {
-            return tasks.addEvent(line);
-        } else {
-            ui.print("Possible commands: bye, list, mark, unmark, todo, deadline, event");
-            return false;
+            if (parser.getEnded()) {
+                ui.printWithoutLeadingSpace("Bye. Hope to see you again soon!");
+                break;
+            }
         }
     }
 }
---
+++ src/main/java/ip/main/Parser.java
@@ -0,0 +1,40 @@
+package ip.main;
+
+public class Parser {
+    private boolean hasEnded = false;
+
+    public boolean parseInput(String line, Ui ui, TaskList tasks) {
+        if (line.equals("bye")) {
+            hasEnded = true;
+            return false;
+        }
+        if (line.equals("list")) {
+            tasks.printTaskList();
+            return false;
+        }
+        if (line.startsWith("mark")) {
+            return tasks.markTask(line);
+        }
+        if (line.startsWith("unmark")) {
+            return tasks.unmarkTask(line);
+        }
+        if (line.startsWith("delete")) {
+            return tasks.deleteTask(line);
+        }
+        if (line.startsWith("todo")) {
+            return tasks.addTodo(line);
+        }
+        if (line.startsWith("deadline")) {
+            return tasks.addDeadline(line);
+        }
+        if (line.startsWith("event")) {
+            return tasks.addEvent(line);
+        }
+        ui.print("Possible commands: bye, list, mark, unmark, todo, deadline, event");
+        return false;
+    }
+
+    public boolean getEnded() {
+        return hasEnded;
+    }
+}
