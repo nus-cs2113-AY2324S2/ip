93c202d9b87fb6395095a080a7f5e802bb553892
Lin Htet <earthangle007@gmail.com>
2024-02-29 16:37:30 +0800

    Add JavaDoc for ByeCommand class

--- src/main/java/Tony/command/ByeCommand.java
+++ src/main/java/Tony/command/ByeCommand.java
@@ -8,10 +8,21 @@ import java.util.ArrayList;
 
 public class ByeCommand implements Command {
 
+    /**
+     * Executes the <code>printByeMessage</code> command.
+     * @param tasks is the current list of <code>tasks</code> to save from the file.
+     * @param ui is the user interface of that prints texts on program.
+     * @param fileSaver is object used to save data into the file.
+     */
     @Override
     public void execute(ArrayList<Task> tasks, Ui ui, FileSaver fileSaver) {
         ui.printByeMessage();
     }
+
+    /**
+     * Returns <code>true</code> if command entered <code>bye</code>
+     * @return <code>true</code> and exit program.
+     */
     @Override
     public boolean isExit() {
         return true;
