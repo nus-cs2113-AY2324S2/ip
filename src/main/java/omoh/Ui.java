d955abacb1d501632658660f0ae76ecb252243cc
RexYong <rexyyong@gmail.com>
2024-02-29 16:08:37 +0800

    Add message to prompt users to type in valid command

--- src/main/java/omoh/Ui.java
+++ src/main/java/omoh/Ui.java
@@ -59,6 +59,9 @@ public class Ui {
                 }
             } else if (line.startsWith("find")) {
                 Task.findMatchingTasks(line);
+            } else {
+                System.out.println("Please enter a valid user command!\n" +
+                        "You may refer to the user guide for more information about valid user commands :)");
             }
             line = in.nextLine();
         }
