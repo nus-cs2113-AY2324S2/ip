1e0e3dce67aac7e7eb3c2b606f22fa6f1eb36921
LAPTOP-D31L41D6\elyovs <elyovsun@gmail.com>
2024-02-29 20:02:11 +0800

    Add javadoc for Parser

--- src/main/java/gary/parser/Parser.java
+++ src/main/java/gary/parser/Parser.java
@@ -13,7 +13,20 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Parser class deals with making sense of user commands, such that the programme can
+ * process the command and output accordingly.
+ */
 public class Parser {
+    /**
+     * Takes in users command in a loop until the users choose to exit the programme.
+     * Users' input is processed according to what command is given.
+     *
+     * @param line user input to the terminal.
+     * @param file txt file to store the tasks.
+     * @param in terminal scanner to take in user input.
+     * @param todos array list that stores and manages the task while programme is running.
+     */
     public static void runCommand(String line, File file, Scanner in, ArrayList<Task> todos) {
         String[] lineWords;
         String command;
