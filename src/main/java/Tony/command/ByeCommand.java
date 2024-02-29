b5eabb726ffeb889a7f53072fc4d8eb479fb09e5
Lin Htet <earthangle007@gmail.com>
2024-02-29 16:49:33 +0800

    Add JavaDoc for CommandChecker class

--- src/main/java/Tony/command/ByeCommand.java
+++ src/main/java/Tony/command/ByeCommand.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class ByeCommand implements Command {
 
     /**
-     * Executes the <code>printByeMessage</code> command.
+     * Executes the <code>printByeMessage</code> method.
      * @param tasks is the current list of <code>tasks</code> to save from the file.
      * @param ui is the user interface of that prints texts on program.
      * @param fileSaver is object used to save data into the file.
