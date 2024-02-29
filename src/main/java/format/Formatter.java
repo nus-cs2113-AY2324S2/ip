1164cd5bdef08589941cacb01c3912d3220bd3a0
Fang Sihan <sihan_fang@u.nus.edu>
2024-02-29 17:36:44 +0800

    small fix

--- src/main/java/format/Formatter.java
+++ src/main/java/format/Formatter.java
@@ -10,16 +10,16 @@ public class Formatter {
 
     public void printFunctionality() {
         System.out.println("\t\t1. Type \"todo\" + \"TASKCONTENT\" to record a 'todo' task.");
-        System.out.println("\t\t2. Type \"deadline\" + \"TASKCONTENT\" + \"/by\" + \"DATE/TIMES\" to" +
+        System.out.println("\t\t2. Type \"deadline\" + \"TASKCONTENT\" + \"/by\" + \"DATE + TIMES\" to" +
                 " record a 'deadline' task.");
-        System.out.println("\t\t3. Type \"event\" + \"TASKCONTENT\" + \"/from\" + \"DATE/TIMES\" + " +
-                "\"/to\" + \"dates/times\" to record a 'event' task.");
+        System.out.println("\t\t3. Type \"event\" + \"TASKCONTENT\" + \"/from\" + \"DATE + TIMES\" + " +
+                "\"/to\" + \"DATE + TIMES\" to record a 'event' task.");
         System.out.println("\t\t4. Type \"list\" to see what I have recorded.");
         System.out.println("\t\t5. Type \"mark\" + \"NUMBER\" to mark tasks as done.");
         System.out.println("\t\t6. Type \"unmark\" + \"NUMBER\" to mark tasks as not done.");
         System.out.println("\t\t7. Type \"delete\" + \"NUMBER\" to delete tasks.");
         System.out.println("\t\t8. Type \"find\" + \"TASKCONTENT\" to find tasks");
-        System.out.println("\t\t9. Type \"find\" + \"DATE/TIMES\" to find tasks");
+        System.out.println("\t\t9. Type \"find\" + \"/at\" + \"DATE\" to find tasks");
         System.out.println("\t\t10. Type \"bye\" to say goodbye to me.");
     }
 
