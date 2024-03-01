37c49f3fc2a0be3db14de882a0d2bf7fb5af3b10
STeng618 <shantengyap@gmail.com>
2024-03-01 15:05:29 +0800

    Add lock UI feature

--- src/main/java/ui/UserInterface.java
+++ src/main/java/ui/UserInterface.java
@@ -6,41 +6,47 @@ import util.HorizontalGenerator;
 
 public class UserInterface {
     private final Scanner scanner = new Scanner(System.in);
-    private final LogicManager logicManager;
     private String currentInput;
+    private boolean isUILocked;
 
-    public UserInterface(LogicManager logicManager) {
-        this.logicManager = logicManager;
+    public UserInterface() {
+        this.isUILocked = false;
+    }
+
+    public void lockUI(boolean isUILocked) {
+        this.isUILocked = isUILocked;
+    }
+
+    public String getCurrentInput() {
+        return this.currentInput;
+    }
+
+    public void startAtomicSection() {
+        HorizontalGenerator.printHorizontal();
+    }
+
+    public void print(String message) {
+        if (this.isUILocked) {
+            return;
+        }
+        System.out.println(message);
+    }
+
+    public void endAtomicSection() {
+        HorizontalGenerator.printHorizontal();
     }
 
     private void formatCurrentInput() {
         this.currentInput = this.currentInput.trim();
     }
-    private void readNextLine() {
+    public void readNextLine() {
         do {
             this.currentInput = scanner.nextLine();
             this.formatCurrentInput();
         } while (this.currentInput.isEmpty());
     }
     
-    private boolean isExitCommandGiven() {
+    public boolean isExitCommandGiven() {
         return this.currentInput.startsWith(Keywords.BYE);
     }
-
-    /**
-     * Runs the main loop of the program.
-     * Reads the user input and passes it to the LogicManager for processing.
-     * Exits when the user inputs the exit command.
-     */
-    public void manageList() {
-        while (true) {
-            readNextLine();
-            if (isExitCommandGiven()) {
-                return;
-            }
-            this.logicManager.processCommand(currentInput);
-            HorizontalGenerator.printHorizontal();
-        }
-    }
-
 }
