3bf650d3795002c1b4901e23e12852d20553fa01
nur-haziq <nhaziq01@gmail.com>
2024-03-01 16:09:25 +0800

    Ui.java: Add JavaDoc comments

--- src/main/java/mona/input/Ui.java
+++ src/main/java/mona/input/Ui.java
@@ -4,14 +4,23 @@ import mona.output.ConsolePrint;
 
 import java.util.Scanner;
 
+/**
+ * Handles user interface interactions, mainly input operations.
+ */
 public class Ui {
     protected Scanner in;
-
+    /**
+     * Constructor for Ui. Initializes the Scanner and displays a greeting message.
+     */
     public Ui() {
         this.in = new Scanner(System.in);
         ConsolePrint.greet();
     }
-
+    /**
+     * Reads and returns the user's input from the console, trimming any leading and trailing whitespace.
+     *
+     * @return A trimmed string of the user's input.
+     */
     public String getUserInput() {
         return in.nextLine().trim();
     }
