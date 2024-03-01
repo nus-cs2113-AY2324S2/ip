373f587aef93b05ea51bdec31ff0d92ba00b39ad
nur-haziq <nhaziq01@gmail.com>
2024-03-01 22:32:24 +0800

    Task.java: Add JavaDoc comments

--- src/main/java/mona/task/Task.java
+++ src/main/java/mona/task/Task.java
@@ -1,8 +1,18 @@
 package mona.task;
 
+/**
+ * Represents a task with a description and a completion status. The Task class is a superclass of the
+ * Deadline, Event and Todo classes.
+ */
 public class Task {
     protected String description;
     protected boolean isDone;
+    /**
+     * Constructor for Task. Initializes the task with a description and sets its completion
+     * status to not done by default.
+     *
+     * @param description The description of the task.
+     */
 
     public Task(String description) {
         this.description = description;
@@ -16,11 +26,20 @@ public class Task {
     public boolean isDone() {
         return isDone;
     }
-
+    /**
+     * Returns a string representation of the task, including its completion status and description.
+     *
+     * @return A string representation of the task.
+     */
     @Override
     public String toString() {
         return "[" + this.getStatusIcon() + "] " + this.description;
     }
+    /**
+     * Returns a symbol representing the completion status of the task.
+     *
+     * @return "X" if the task is done, " " otherwise.
+     */
     public String getStatusIcon() {
         return (isDone ? "X" : " "); //mark done task with "X"
     }
