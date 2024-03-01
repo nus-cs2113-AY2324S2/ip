6f1b7fea3e7d2c92996d0d773a1ebc7348c510b6
nur-haziq <nhaziq01@gmail.com>
2024-03-01 22:33:02 +0800

    Todo.java: Add JavaDoc comments

--- src/main/java/mona/task/Todo.java
+++ src/main/java/mona/task/Todo.java
@@ -1,9 +1,22 @@
 package mona.task;
-
+/**
+ * Represents a Todo task with a description. The Todo class
+ * is a subclass of the Task class.
+ */
 public class Todo extends Task {
+    /**
+     * Constructor for Todo. Initializes the Todo task with a description.
+     *
+     * @param description The description of the Todo task.
+     */
     public Todo(String description) {
         super(description);
     }
+    /**
+     * Returns a string representation of the Todo task, including its type and description.
+     *
+     * @return A string representation of the Todo task.
+     */
     @Override
     public String toString() {
         return "[T]" + super.toString();
