b1fd82dd74ba227a2a9e43ddc187b8c067735cc4
hongyijie06 <hongyijie06@gmail.com>
2024-03-02 12:04:09 +0800

    fix formatting when listing tasks

--- src/main/java/ManageInputs.java
+++ src/main/java/ManageInputs.java
@@ -16,38 +16,47 @@ public class ManageInputs {
         fw.close();
     }
 
-    private static int fillFileContents(ArrayList<Task> tasks, String filePath, int index) throws IOException, UnexpectedCommandException {//updates index
+
+
+    private static int fillFileContents(ArrayList<Task> tasks, String filePath, int index, boolean isInTxt) throws IOException, UnexpectedCommandException {//updates index
         File f = new File(filePath); // create a File for the given file path
         Scanner s = new Scanner(f); // create a Scanner using the File as the source
 
         while (s.hasNext()) {
+
             String sLine = s.nextLine();
             if (sLine.contains("[E]")) {
-                dealWithEvent(tasks, index, sLine);
+                dealWithEvent(tasks, index, sLine, true);
             } else if (sLine.contains("[D]")) {
-                dealWithDeadline(tasks, index, sLine);
+                dealWithDeadline(tasks, index, sLine, true);
             } else if (sLine.contains("[T]")) {
-                dealWithTodo(tasks, index, sLine);
+                dealWithTodo(tasks, index, sLine, true);
             }
             index++;
         }
+        isInTxt = false;
         return index;
     }
 
-    public static void dealWithEvent(ArrayList<Task> tasks, int index, String line) throws UnexpectedCommandException, IOException {
-        int indexTo = line.indexOf("to");
+
+
+    public static void dealWithEvent(ArrayList<Task> tasks, int index, String line, boolean isInTxt) throws UnexpectedCommandException, IOException {
+        int indexTo = line.lastIndexOf("to");
         int indexFrom = line.indexOf("from");
+        String from;
+        String to;
+        String description;
 
         if ((indexTo == -1) || (indexFrom == -1)) { //invalid format
             System.out.println("Invalid format! Enter event in the format: event (description) from (start) to (end)");
             throw new UnexpectedCommandException();
         }
         try {//timeline not specified/ both not specified
-            String from = line.substring(indexFrom + 5, indexTo - 1);
-            String to = line.substring(indexTo + 2);
+            from = line.substring(indexFrom + 5, indexTo - 1);
+            to = line.substring(indexTo + 2);
         } catch (IndexOutOfBoundsException e) {
             try {
-                String description = line.substring(6, indexFrom - 1);
+                description = line.substring(5, indexFrom - 1);
             } catch (IndexOutOfBoundsException f) {
                 System.out.println("event description and timeline not specified");
                 throw new UnexpectedCommandException();
@@ -56,30 +65,40 @@ public class ManageInputs {
             throw new UnexpectedCommandException();
         }
         try {//description not specified
-            String description = line.substring(6, indexFrom - 1);
+            description = line.substring(5, indexFrom - 1);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("event description not specified");
             throw new UnexpectedCommandException();
         }
-        String from = line.substring(indexFrom + 5, indexTo - 1);
-        String to = line.substring(indexTo + 2);
-        String description = line.substring(5, indexFrom - 1);
+        if(!isInTxt) {
+            from = line.substring(indexFrom + 5, indexTo - 1);
+            to = line.substring(indexTo + 3);
+            description = line.substring(5, indexFrom - 1);
+        } else {
+            from = line.substring(indexFrom + 6, indexTo - 1);
+            to = line.substring(indexTo + 4, line.length() - 1);
+            description = line.substring(5, indexFrom - 1);
+        } //when uncomment this, cannot add but reads file contents properly
+        //when commented, can add but cannot read file contents properly
+        //System.out.println(isInTxt);
 
         tasks.add(index, new Event(description, from, to));
     }
 
-    public static void dealWithDeadline(ArrayList<Task> tasks, int index, String line) throws UnexpectedCommandException, IOException {
+    public static void dealWithDeadline(ArrayList<Task> tasks, int index, String line, boolean isInTxt) throws UnexpectedCommandException, IOException {
         int indexBy = line.indexOf("by");
         int space = line.indexOf(" ");
+        String by;
+        String description;
         if (indexBy == -1) {//invalid format
             System.out.println("Invalid format! Enter deadline in the format: deadline (description) by (deadline)");
             throw new UnexpectedCommandException();
         }
         try {//deadline / both not specified
-            String by = line.substring(indexBy + 2);
+            by = line.substring(indexBy + 2);
         } catch (IndexOutOfBoundsException e) {
             try {
-                String description = line.substring(space, indexBy - 1);
+                description = line.substring(space, indexBy - 1);
             } catch (IndexOutOfBoundsException f) {
                 System.out.println("deadline description and deadline not specified");
                 throw new UnexpectedCommandException();
@@ -88,18 +107,23 @@ public class ManageInputs {
             throw new UnexpectedCommandException();
         }
         try {//deadline not specified
-            String description = line.substring(space, indexBy - 1);
+            description = line.substring(space, indexBy - 1);
         } catch (IndexOutOfBoundsException e) {
             System.out.println("deadline description not specified");
             throw new UnexpectedCommandException();
         }
-        String description = line.substring(space, indexBy - 1);
-        String by = line.substring(indexBy + 2);
+        if (!isInTxt) {
+            description = line.substring(space, indexBy - 1);
+            by = line.substring(indexBy + 3);
+        } else{
+            description = line.substring(space, indexBy - 1);
+            by = line.substring(indexBy + 4, line.length() - 1);
+        }
 
         tasks.add(index, new Deadline(description, by));
     }
 
-    public static void dealWithTodo(ArrayList<Task> tasks, int index, String line) throws UnexpectedCommandException, IOException {
+    public static void dealWithTodo(ArrayList<Task> tasks, int index, String line, boolean isInTxt) throws UnexpectedCommandException, IOException {
         int indexSpace = line.indexOf(" ");
         if (indexSpace == -1) {
             System.out.println("todo description not specified");
@@ -125,7 +149,9 @@ public class ManageInputs {
     }
 
     public ManageInputs(ArrayList<Task> tasks, int index, String line) throws IOException, UnexpectedCommandException {
-        index = fillFileContents(tasks, "TaskList.txt", index);
+        boolean isInTxt = false;
+        index = fillFileContents(tasks, "TaskList.txt", index, isInTxt);
+
         while (!line.equals("bye")) {
             Boolean isValidCommand = false;
             Scanner input = new Scanner(System.in);
@@ -155,26 +181,30 @@ public class ManageInputs {
                 }
             } else if (line.equals("bye")) {//exit chat
                 isValidCommand = true;
+
+
+
+
                 break;
             } else if (inputs[0].equals("event") || inputs[0].equals("todo") || inputs[0].equals("deadline")) {//add items
                 try {
                     if (inputs[0].equals("event")) {
                         isValidCommand = true;
-                        dealWithEvent(tasks, index, line);
+                        dealWithEvent(tasks, index, line, isInTxt);
                         writeToFile("TaskList.txt", tasks.get(index));
                         System.out.println("Got it. I've added this task: ");
                         System.out.println(tasks.get(index));
                         index++;
                     } else if (inputs[0].equals("deadline")) {
                         isValidCommand = true;
-                        dealWithDeadline(tasks, index, line);
+                        dealWithDeadline(tasks, index, line, isInTxt);
                         writeToFile("TaskList.txt", tasks.get(index));
                         System.out.println("Got it. I've added this task: ");
                         System.out.println(tasks.get(index));
                         index++;
                     } else {
                         isValidCommand = true;
-                        dealWithTodo(tasks, index, line);
+                        dealWithTodo(tasks, index, line, isInTxt);
                         writeToFile("TaskList.txt", tasks.get(index));
                         System.out.println("Got it. I've added this task: ");
                         System.out.println(tasks.get(index));
