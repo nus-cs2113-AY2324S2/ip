af3ef67c157ec097ac380740b1163f012bfa53ee
Lin Htet <earthangle007@gmail.com>
2024-03-02 10:59:11 +0800

    Add JavaDoc for Deadline class

--- src/main/java/Tony/task/Deadline.java
+++ src/main/java/Tony/task/Deadline.java
@@ -4,14 +4,30 @@ public class Deadline extends Task {
 
     public String by;
 
+    /**
+     * Represents a {@code Deadline} object that saves task type <code>deadline</code>
+     * @param description is the String <code>description</code> for deadline tasks
+     * @param by is the String to specify deadline due <code>by</code> time/date
+     */
+
     public Deadline(String description, String by) {
         super(description);
         this.by = by.trim();
     }
 
+    /**
+     * Returns the type of tasks to identify deadline.
+     * @return String <code>[D]</code> as task type.
+     */
+
     public String type() {
         return "[D]";
     }
+
+    /**
+     * Returns the String to save in the current task list
+     * @return String formatted to be saved in task list.
+     */
 
     @Override
     public String toString() {
