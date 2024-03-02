45d3312c65bfb667d681d20ca1bbd90ed7f53d3b
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-02 19:55:09 +0800

    Extract Ui methods from class main to class Ui

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,2 +1,86 @@
+import Tasks.Deadline;
+import Tasks.Event;
+import Tasks.Task;
+import Tasks.Todo;
+
+import java.util.ArrayList;
+
 public class Ui {
+    private static final String CHATBOT = "Spike";
+    public static final String DIVIDER = "_________________________________________________";
+
+    protected static void displayList(ArrayList<Task> inputList) {
+        System.out.println(DIVIDER);
+        System.out.println("Here are the tasks in your list:");
+        for (int i = 0; i < inputList.size(); i++) {
+            Task value = inputList.get(i);
+            if (value == null) {
+                break;
+            }
+            char Badge = getBadge(value);
+            System.out.println((i + 1) + ".[" + Badge + "]"
+                    + "[" + value.getStatusIcon() + "] " + value.description);
+        }
+        System.out.println(DIVIDER);
+    }
+
+    protected static char getBadge(Task value) {
+        char Badge = ' ';
+        if (value instanceof Todo) {
+            Badge = ((Todo) value).getBadge();
+        } else if (value instanceof Deadline) {
+            Badge = ((Deadline) value).getBadge();
+        } else if (value instanceof Event) {
+            Badge = ((Event) value).getBadge();
+        }
+        return Badge;
+    }
+    protected static void displayUnmarkMsg(int index, ArrayList<Task> inputList) {
+        char Badge = getBadge(inputList.get(index));
+        System.out.println(DIVIDER);
+        System.out.println("OK, I've marked this task as not done yet:");
+        System.out.println((index + 1) + ".[" + Badge + "]" + "[ ] "
+                + inputList.get(index).description);
+        System.out.println(DIVIDER);
+    }
+
+    protected static void displayMarkMsg(int index, ArrayList<Task> inputList) {
+        char Badge = getBadge(inputList.get(index));
+        System.out.println(DIVIDER);
+        System.out.println("Nice! I've marked this task as done:");
+        System.out.println((index + 1) + ".[" + Badge + "]" + "[X] "
+                + inputList.get(index).description);
+        System.out.println(DIVIDER);
+    }
+
+    protected static void displayByeMsg() {
+        System.out.println(DIVIDER);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(DIVIDER);
+    }
+
+    protected static void displayWelcomeMsg() {
+        System.out.println(DIVIDER);
+        System.out.println("Hello! I'm " + CHATBOT);
+        System.out.println("What can I do for you?\n");
+        System.out.println(DIVIDER);
+    }
+
+    protected static void displayDeleteMsg(Task inputObj, int arrayLength){
+        char Badge = getBadge(inputObj);
+        System.out.println(DIVIDER);
+        System.out.println("I've removed this task:");
+        System.out.println(" [" + Badge + "]" + "["+ inputObj.getStatusIcon() +"] " + inputObj.description);
+        System.out.println("Now you have " + (arrayLength - 1) + " tasks in the list.");
+        System.out.println(DIVIDER);
+    }
+
+    protected static void displayAcknowledgement(Task inputObj, int arrayLength) {
+        char Badge = getBadge(inputObj);
+        System.out.println(DIVIDER);
+        System.out.println("Got it. I've added this task:");
+        System.out.println(" [" + Badge + "]" + "[ ] " + inputObj.description);
+        System.out.println("Now you have " + arrayLength + " tasks in the list.");
+        System.out.println(DIVIDER);
+    }
 }
