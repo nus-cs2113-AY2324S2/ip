270a2d53f0acc63351fe558d7244000e06dfb071
RyanTDL <teodeliangryan@gmail.com>
2024-03-03 21:35:57 +0800

    Implemented Parser Class

--- src/main/java/jake/Jake.java
+++ src/main/java/jake/Jake.java
@@ -1,106 +1,20 @@
 package jake;
-import java.util.Scanner;
-import java.io.File; 
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
 
+import jake.parser.Parser;
 import jake.storage.Storage;
-// import jake.storage.Storage;
 import jake.task.TaskList;
 import jake.ui.Ui;
 
-
-
 public class Jake {
-
     static final String savedTaskFilePath = "./src/main/java/jake/data/tasks.txt";
-    
     private static Ui ui = new Ui();
     private static TaskList tasks = new TaskList();
-
-    ///////////////////////////////////////////////////////////////////
     private static Storage storage = new Storage(savedTaskFilePath);
-
-    // private static Storage storage = new Storage(savedTaskFilePath);
-
-    // private static void saveTasks() {
-    //     try {
-    //         FileWriter writer = new FileWriter(savedTaskFilePath);
-    //         for (int i = 0; i < commands.size(); i++){
-    //             writer.write(commands.get(i) + System.lineSeparator());
-    //         }
-    //         writer.close();
-    //     } catch (IOException e) {
-    //         System.out.printf(MESSAGE_TASK_ERROR_ENCOUNTERED, e.getMessage());
-    //     }
-    // }
-
-    // private static void loadTasks(String filePath, TaskList tasks) {
-    //     try {
-    //         File savedFile = new File(savedTaskFilePath);
-    //         Scanner savedFileScanner = new Scanner(savedFile);
-    //         while (savedFileScanner.hasNext()) {
-    //             String userInput = savedFileScanner.nextLine();
-    //             char taskType = userInput.charAt(1);
-    //             // addSavedTask(userInput, taskType);                
-    //         }
-    //         savedFileScanner.close();
-    //     } catch (FileNotFoundException e) {
-    //         System.out.println(MESSAGE_INVALID_FILEPATH);
-    //     }
-    // }
-
-
-    ///////////////////////////////////////////////////////////////////
-
+    private static Parser parser = new Parser(storage, tasks);
 
     public static void main(String[] args) throws JakeException {
         storage.loadTasks(tasks);
         ui.showGreeting();
-        Scanner myScanner = new Scanner(System.in);
-        String userInput= "";
-
-        do {
-            userInput = myScanner.nextLine();
-            String taskType; 
-            // Check if the userInput contains a space, to handle inputs like "list" and "bye";
-            if (userInput.contains(" ")) {
-                taskType = userInput.substring(0, userInput.indexOf(" "));
-            } else {
-                taskType = userInput;
-            }
-
-            switch (taskType) {
-            case "bye":
-                ui.showGoodbye();
-                break;
-            case "list":
-                tasks.listTasks();
-                break;
-            case "mark":
-            case "unmark":
-                tasks.toggleTask(userInput, taskType);
-                storage.saveTasks(tasks);
-                break;
-            case "delete":
-                tasks.deleteTask(userInput);
-            case "todo":
-            case "deadline":
-            case "event":
-                try {
-                    tasks.addInputtedTask(userInput, taskType);
-                    storage.saveTasks(tasks);
-                    break;
-                } catch (ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e) {
-                    ui.showInvalidCommand();
-                    break;
-                } 
-            default:
-                ui.showInvalidCommand();
-            }
-        } while (!userInput.equals("bye"));
-
-        myScanner.close();
+        parser.readInput();
     }
 }
---
+++ src/main/java/jake/parser/Parser.java
@@ -0,0 +1,65 @@
+package jake.parser;
+
+import java.util.Scanner;
+
+import jake.storage.Storage;
+import jake.task.TaskList;
+import jake.ui.Ui;
+
+public class Parser {
+    private static Ui ui = new Ui();
+    private Storage storage;
+    private TaskList tasks;
+
+    public Parser(Storage storage, TaskList tasks) {
+        this.storage = storage;
+        this.tasks = tasks;
+    }
+
+    public void readInput() {
+        Scanner myScanner = new Scanner(System.in);
+        String userInput= "";
+    
+        do {
+            userInput = myScanner.nextLine();
+            String taskType; 
+            // Check if the userInput contains a space, to handle inputs like "list" and "bye";
+            if (userInput.contains(" ")) {
+                taskType = userInput.substring(0, userInput.indexOf(" "));
+            } else {
+                taskType = userInput;
+            }
+    
+            switch (taskType) {
+            case "bye":
+                ui.showGoodbye();
+                break;
+            case "list":
+                tasks.listTasks();
+                break;
+            case "mark":
+            case "unmark":
+                tasks.toggleTask(userInput, taskType);
+                storage.saveTasks(tasks);
+                break;
+            case "delete":
+                tasks.deleteTask(userInput);
+            case "todo":
+            case "deadline":
+            case "event":
+                try {
+                    tasks.addInputtedTask(userInput, taskType);
+                    storage.saveTasks(tasks);
+                    break;
+                } catch (ArrayIndexOutOfBoundsException | StringIndexOutOfBoundsException e) {
+                    ui.showInvalidCommand();
+                    break;
+                } 
+            default:
+                ui.showInvalidCommand();
+            }
+        } while (!userInput.equals("bye"));
+    
+        myScanner.close();
+    }
+}
