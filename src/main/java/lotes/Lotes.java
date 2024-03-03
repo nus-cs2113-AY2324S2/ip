375750be4617bdae5e3d99307d831e3b2edc8179
E0958902 <E0958902@u.nus.edu>
2024-03-03 19:39:34 +0800

    Remove redundant comments in Lotes

--- src/main/java/lotes/Lotes.java
+++ src/main/java/lotes/Lotes.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
 public class Lotes {
 
-    public static final Scanner inputCommand = new Scanner(System.in); // Setting the Scanner
+    public static final Scanner inputCommand = new Scanner(System.in);
 
     public static void main(String[] args) {
         System.out.println(TaskList.greetingsMessage); // Print greetings message
