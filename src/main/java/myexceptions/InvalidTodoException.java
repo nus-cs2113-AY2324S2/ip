1f91dee759bd40e8ed358dc5c577845ad1e04d42
lordgareth10 <51813599+lordgareth10@users.noreply.github.com>
2024-03-04 23:15:08 +0800

    Add comments

--- src/main/java/myexceptions/InvalidTodoException.java
+++ src/main/java/myexceptions/InvalidTodoException.java
@@ -1,4 +1,5 @@
 package myexceptions;
 
 public class InvalidTodoException extends Exception{
+    //Error is thrown when task description of part of Todo object is left empty, returns nothing and prints error message
 }
