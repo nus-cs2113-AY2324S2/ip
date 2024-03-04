1b1137e1aa0925f8dec0058dcbde6f17d68c6929
nkotaa <e0957774@u.nus.edu>
2024-03-04 23:48:48 +0800

    Update Test Scripts & Cases

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -19,21 +19,21 @@ then
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/Blue/Blue.java
+if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/blue/Blue.java
 then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
 # run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -cp ../bin Blue.Blue < input.txt > ACTUAL.TXT
+java -cp ../bin blue.Blue < input.txt > ACTUAL.TXT
 
 # convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+#cp EXPECTED.TXT EXPECTED-UNIX.TXT
+#dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
 
 # compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
+diff ACTUAL.TXT EXPECTED.TXT
 if [ $? -eq 0 ]
 then
     echo "Test result: PASSED"
