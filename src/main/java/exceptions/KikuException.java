ab9e973bde0d9bfc3d1b054c9c27efd732c759bc
liuy1103 <e0968020@u.nus.edu>
2024-03-05 11:54:06 +0800

    Add JavaDoc for KikuExceptions

--- src/main/java/exceptions/KikuException.java
+++ src/main/java/exceptions/KikuException.java
@@ -1,6 +1,12 @@
 package exceptions;
 
 public class KikuException extends Exception {
+    /**
+     * Custom exception class for KikuBot used to represent various errors that can occur during execution,
+     * including command parsing, task manipulation and storage operations.
+     *
+     * @param message A custom error message that explains the reason for the exception.
+     */
     public KikuException(String message) {
         super(message);
     }
