df7826d9a63796fcf7173d2c3d7e2007157dc0b9
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-06 00:44:33 +0800

    Add TaskTypes class

---
+++ src/main/java/vibes/common/TaskTypes.java
@@ -0,0 +1,7 @@
+package vibes.common;
+
+public class TaskTypes {
+    public static final String TODO_TYPE = "T";
+    public static final String DEADLINE_TYPE = "D";
+    public static final String EVENT_TYPE = "E";
+}
