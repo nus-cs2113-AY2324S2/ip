12aa50e9d2108577ec282636cdc4df4d31475f78
liuy1103 <e0968020@u.nus.edu>
2024-03-05 12:02:06 +0800

    Add JavaDoc for Storage

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -10,13 +10,30 @@ import java.util.Scanner;
 import tasks.Task;
 import parsers.Parser;
 
+/**
+ * Handles the loading and saving of task list data to a file.
+ * This class encapsulates all file operations, ensuring tasks are stored
+ * and can be retrieved after restarting sessions.
+ */
 public class Storage {
     private String filePath;
 
+    /**
+     * Constructs a Storage object with a specified file path for data storage.
+     *
+     * @param filePath The path of the file where tasks are stored and retrieved from.
+     */
     public Storage(String filePath) {
         this.filePath = filePath;
     }
 
+    /**
+     * Loads tasks from the specified file into an ArrayList of Task objects.
+     * If the file does not exist, initialise an empty task list.
+     *
+     * @return An ArrayList of Task objects loaded from the file.
+     * @throws FileNotFoundException If the file specified by filePath does not exist.
+     */
     public ArrayList<Task> loadTasks() throws FileNotFoundException {
         ArrayList<Task> loadedTasks = new ArrayList<>();
         File file = new File(filePath);
@@ -39,6 +56,13 @@ public class Storage {
         return loadedTasks;
     }
 
+    /**
+     * Saves the current list of tasks to the file specified by filePath.
+     * If the file or directory does not exist, it will be created.
+     *
+     * @param tasks The ArrayList of Task objects to be saved.
+     * @throws IOException If an error occurs during writing to the file.
+     */
     public void saveTasks(ArrayList<Task> tasks) throws IOException {
         try {
             File file = new File(filePath);
