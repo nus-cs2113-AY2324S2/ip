af82ec7d0c5610577a3bd506db268ae3d2925a58
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-05 22:24:29 +0800

    Update TaskList class to use TextUi instance

--- src/main/java/vibes/task/TaskList.java
+++ src/main/java/vibes/task/TaskList.java
@@ -1,49 +1,22 @@
 package vibes.task;
 
-import vibes.exception.CommandNotFoundException;
-import vibes.exception.InvalidArgumentException;
-import vibes.parser.Parser;
-import vibes.storage.Storage;
 import vibes.task.type.Deadline;
 import vibes.task.type.Event;
 import vibes.task.type.Task;
 import vibes.task.type.Todo;
 import vibes.ui.TextUi;
+
 import java.util.ArrayList;
 
 public class TaskList {
-    public static ArrayList<Task> tasks = new ArrayList<>();
+    public ArrayList<Task> tasks;
 
-    public void executeCommand(String commandToExecute, String userInput) throws CommandNotFoundException, InvalidArgumentException {
-        switch (commandToExecute) {
-        case "list":
-            TextUi.listTasks();
-            break;
-        case "mark":
-            setAsDone(Parser.parseTaskNumber(userInput));
-            break;
-        case "unmark":
-            setAsNotDone(Parser.parseTaskNumber(userInput));
-            break;
-        case "todo":
-            addTodo(Parser.parseTodo(userInput));
-            TextUi.showTaskAddedMessage();
-            break;
-        case "event":
-            addEvent(Parser.parseEvent(userInput));
-            TextUi.showTaskAddedMessage();
-            break;
-        case "deadline":
-            addDeadline(Parser.parseDeadline(userInput));
-            TextUi.showTaskAddedMessage();
-            break;
-        case "delete":
-            deleteTask(Parser.parseTaskNumber(userInput));
-            break;
-        default:
-            throw new CommandNotFoundException();
-        }
-        Storage.saveTask(this);
+    public TaskList() {
+        this.tasks = new ArrayList<>();
+    }
+
+    public TaskList(ArrayList<Task> loadTasks) {
+        this.tasks = loadTasks;
     }
 
     public void addEvent(String[] parsedInput) {
@@ -58,19 +31,19 @@ public class TaskList {
         tasks.add(new Todo(description));
     }
 
-    public void setAsDone(int taskNumber) {
+    public void setAsDone(int taskNumber, TextUi ui) {
         tasks.get(taskNumber).setDone(true);
-        TextUi.showMarkedMessage(taskNumber);
+        ui.showMarkedMessage(tasks, taskNumber);
     }
 
-    public void setAsNotDone(int taskNumber) {
+    public void setAsNotDone(int taskNumber, TextUi ui) {
         tasks.get(taskNumber).setDone(false);
-        TextUi.showUnmarkedMessage(taskNumber);
+        ui.showUnmarkedMessage(tasks, taskNumber);
     }
 
-    public void deleteTask(int taskNumber) {
+    public void deleteTask(int taskNumber, TextUi ui) {
         Task taskToDelete = tasks.get(taskNumber);
         tasks.remove(taskNumber);
-        TextUi.showDeletedMessage(taskToDelete);
+        ui.showDeletedMessage(tasks, taskToDelete);
     }
 }
