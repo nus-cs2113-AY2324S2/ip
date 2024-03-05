6f0f9e7198d112018e103b7be9def12e18ddec3f
shawnpong <110764881+shawnpong@users.noreply.github.com>
2024-03-05 14:55:46 +0800

    Testing README

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -4,24 +4,13 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-/**
- * Class responsible for handling file storage operations.
- */
 public class Storage {
     private static String FILE_PATH;
 
-    /**
-     * Constructs a Storage object with the specified file path.
-     *
-     * @param filePath The file path where tasks will be stored.
-     */
     public Storage(String filePath) {
         FILE_PATH = filePath;
     }
 
-    /**
-     * Saves tasks to the file.
-     */
     public static void saveToFile() {
         try {
             FileWriter fw = new FileWriter(FILE_PATH);
@@ -35,12 +24,6 @@ public class Storage {
         }
     }
 
-    /**
-     * Loads tasks from the file.
-     *
-     * @throws FileNotFoundException    If the file is not found.
-     * @throws InvalidInputException    If there is an invalid input in the file.
-     */
     public void loadTasks() throws FileNotFoundException, InvalidInputException {
         File file = new File(FILE_PATH);
         if (!file.exists()) {
@@ -53,21 +36,18 @@ public class Storage {
         }
         scanner.close();
     }
-
-    /**
-     * Converts a task object to a string representation for writing to the file.
-     *
-     * @param task The task object to convert.
-     * @return A string representing the task for writing to the file.
-     */
     private static String taskToLine(Task task) {
-        if (task instanceof ToDo todo) {
+        if (task instanceof ToDo) {
+            ToDo todo = (ToDo) task;
             return "T | " + (todo.isDone() ? "1" : "0") + " | " + todo.getDescription();
-        } else if (task instanceof Deadline deadline) {
+        } else if (task instanceof Deadline) {
+            Deadline deadline = (Deadline) task;
             return "D | " + (deadline.isDone() ? "1" : "0") + " | " + deadline.getDescription() + " | " + deadline.getBy();
-        } else if (task instanceof Events event) {
+        } else if (task instanceof Events) {
+            Events event = (Events) task;
             return "E | " + (event.isDone() ? "1" : "0") + " | " + event.getDescription() + " | " + event.getFrom() + " | " + event.getTo();
         }
         return "";
     }
+
 }
