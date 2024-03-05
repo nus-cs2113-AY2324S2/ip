d121327d95a27294121047e8f7f9db43db58b048
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-05 23:20:36 +0800

    Add Messages class

---
+++ src/main/java/vibes/common/Messages.java
@@ -0,0 +1,6 @@
+package vibes.common;
+
+public class Messages {
+    public static final String INVALID_COMMAND_MESSAGE = "\t Invalid Command. Please choose between: todo, deadline, event, mark, unmark, delete, and bye";
+    public static final String FILE_NOT_FOUND_MESSAGE = "File not Found!";
+}
