f2fa391877839bf32124f8db7da758bf34a2f23e
liuy1103 <e0968020@u.nus.edu>
2024-03-05 12:46:22 +0800

    Add JavaDoc for TaskList

--- src/main/java/taskList/TaskList.java
+++ src/main/java/taskList/TaskList.java
@@ -5,53 +5,95 @@ import ui.Ui;
 
 import java.util.ArrayList;
 
+/**
+ * Manage a list of tasks.
+ * This class is responsible for adding, deleting, marking tasks as done or not done,
+ * and searching tasks by keyword.
+ */
 public class TaskList {
     private ArrayList<Task> tasks;
 
+    /**
+     * Initializes an empty TaskList.
+     */
     public TaskList() {
         this.tasks = new ArrayList<>();
     }
 
+    /**
+     * Initializes a TaskList with a pre-existing list of tasks.
+     *
+     * @param tasks An ArrayList of Task objects to be added to the TaskList.
+     */
     public TaskList(ArrayList<Task> tasks) {
         this.tasks = tasks;
     }
 
+    /**
+     * Adds a task to the TaskList.
+     *
+     * @param task The Task to be added.
+     */
     public void addTask(Task task) {
         tasks.add(task);
     }
 
+    /**
+     * Deletes a task from the TaskList based on its index.
+     *
+     * @param index The index of the task to be deleted.
+     */
     public void deleteTask(int index) {
         if (index >= 0 && index < tasks.size()) {
             tasks.remove(index);
         }
     }
 
+    /**
+     * Marks a task as done based on its index.
+     *
+     * @param index The index of the task to be marked as done.
+     */
     public void markTaskAsDone(int index) {
         if (index >= 0 && index < tasks.size()) {
             tasks.get(index).markAsDone();
         }
     }
 
+    /**
+     * Marks a task as not done based on its index.
+     *
+     * @param index The index of the task to be marked as not done.
+     */
     public void markTaskAsNotDone(int index) {
         if (index >= 0 && index < tasks.size()) {
             tasks.get(index).markAsNotDone();
         }
     }
 
+    /**
+     * Retrieves the current list of tasks.
+     *
+     * @return An ArrayList containing the current tasks.
+     */
     public ArrayList<Task> getTasks() {
         return tasks;
     }
 
-    public void listTasks(Ui ui) {
-        for (int i = 0; i < tasks.size(); i++) {
-            ui.showTask((i + 1) + ". " + tasks.get(i).toString());
-        }
-    }
-
+    /**
+     * Checks if the TaskList is empty.
+     *
+     * @return true if the TaskList is empty, otherwise false.
+     */
     public boolean isEmpty() {
         return tasks.isEmpty();
     }
 
+    /**
+     * Formats the tasks in the TaskList into a string representation.
+     *
+     * @return A string representation of all tasks in the TaskList.
+     */
     public String getFormattedTasks() {
         if(tasks.isEmpty()) {
             return "OPPS! Your task list is empty :<";
@@ -64,6 +106,12 @@ public class TaskList {
         }
     }
 
+    /**
+     * Finds and returns tasks that contain the specified keyword in their description.
+     *
+     * @param keyword The keyword to search for within task descriptions.
+     * @return An ArrayList of tasks that contain the keyword.
+     */
     public ArrayList<Task> findTasks(String keyword) {
         ArrayList<Task> matchingTasks = new ArrayList<>();
         for(Task task: tasks) {
