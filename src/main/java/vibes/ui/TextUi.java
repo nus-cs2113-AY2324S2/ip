d4bf3c1e4c4c4410b8a0f7596b623c7c4d0621f5
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-05 22:25:03 +0800

    TextUi class: Add showWelcomeMessage method

--- src/main/java/vibes/ui/TextUi.java
+++ src/main/java/vibes/ui/TextUi.java
@@ -1,35 +1,59 @@
 package vibes.ui;
 
-import vibes.task.TaskList;
 import vibes.task.type.Task;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class TextUi {
-    public static void showTaskAddedMessage() {
-        System.out.println("\t Got it. I've added this task:");
-        System.out.println("\t   " + TaskList.tasks.get(TaskList.tasks.size() - 1));
-        System.out.println("\t Now you have " + TaskList.tasks.size() + " tasks in the list.");
+    private static final String CHATBOT_NAME = "Vibes";
+    public static final String DASHED_LINE = "\t---------------------------------------------------------------------------------------";
+
+    public void showLine() {
+        System.out.println(DASHED_LINE);
     }
 
-    public static void listTasks() {
+    public void showTaskAddedMessage(ArrayList<Task> tasks) {
+        System.out.println("\t Got it. I've added this task:");
+        System.out.println("\t   " + tasks.get(tasks.size() - 1));
+        System.out.println("\t Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    public void listTasks(ArrayList<Task> tasks) {
         System.out.println("\t Here are the tasks in your list:");
-        for (int i = 0; i < TaskList.tasks.size(); i++) {
-            System.out.println("\t " + (i + 1) + "." + TaskList.tasks.get(i));
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("\t " + (i + 1) + "." + tasks.get(i));
         }
     }
 
-    public static void showMarkedMessage(int taskNumber) {
+    public void showMarkedMessage(ArrayList<Task> tasks, int taskNumber) {
         System.out.println("\t Nice! I've marked this task as done:");
-        System.out.println("\t   " + TaskList.tasks.get(taskNumber));
+        System.out.println("\t   " + tasks.get(taskNumber));
     }
 
-    public static void showUnmarkedMessage(int taskNumber) {
+    public void showUnmarkedMessage(ArrayList<Task> tasks, int taskNumber) {
         System.out.println("\t OK, I've marked this task as not done yet:");
-        System.out.println("\t   " + TaskList.tasks.get(taskNumber));
+        System.out.println("\t   " + tasks.get(taskNumber));
     }
 
-    public static void showDeletedMessage(Task taskToDelete) {
+    public void showDeletedMessage(ArrayList<Task> tasks, Task taskToDelete) {
         System.out.println("\t Noted. I've removed this task:");
         System.out.println("\t   " + taskToDelete);
-        System.out.println("\t Now you have " + TaskList.tasks.size() + " tasks in the list.");
+        System.out.println("\t Now you have " + tasks.size() + " tasks in the list.");
+    }
+
+    public void showWelcomeMessage() {
+        System.out.println(DASHED_LINE);
+        System.out.println("\t Hello! I'm " + CHATBOT_NAME + "\n\t What can I do for you?");
+        System.out.println(DASHED_LINE);
+    }
+
+    public String readCommand() {
+        Scanner in = new Scanner(System.in);
+        return in.nextLine().trim();
+    }
+
+    public void showError(String message)   {
+        System.out.println(message);
     }
 }
