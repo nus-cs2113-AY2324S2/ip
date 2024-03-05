476dccea89553c96fdad3088675165639e99dc3c
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-05 23:21:08 +0800

    Update Vibes class to use storage as instance

--- src/main/java/vibes/Vibes.java
+++ src/main/java/vibes/Vibes.java
@@ -1,37 +1,31 @@
 package vibes;
 
+import vibes.common.CommandTypes;
+import vibes.common.Messages;
 import vibes.exception.CommandNotFoundException;
 import vibes.exception.InvalidArgumentException;
 import vibes.parser.Parser;
 import vibes.storage.Storage;
 import vibes.task.TaskList;
 import vibes.ui.TextUi;
-
 import java.io.FileNotFoundException;
 
 public class Vibes {
 
-    public static final String INVALID_COMMAND_MESSAGE = "\t Invalid Command. Please choose between: todo, deadline, event, mark, unmark, delete, and bye";
-    //    private Storage storage;
-    private final TaskList taskList;
+    private final Storage storage;
+    private TaskList taskList;
     private final TextUi ui;
     private boolean isExit;
 
-    public Vibes(String filePath){
+    public Vibes(){
         this.ui = new TextUi();
-//        storage = new Storage(filePath);
-//        try{
-//            taskList = new TaskList(storage.load());
-//        } catch (VibesException e){
-//            ui.showLoadingError();
-//            taskList = new TaskList();
-//        }
-        this.taskList = new TaskList();
+        this.storage = new Storage();
         this.isExit = false;
-        try {
-            Storage.loadTasks(taskList);
-        } catch (FileNotFoundException e) {
-            System.out.println("File not Found!");
+        try{
+            this.taskList = new TaskList(storage.load());
+        } catch (FileNotFoundException e){
+            ui.showError(Messages.FILE_NOT_FOUND_MESSAGE);
+            this.taskList = new TaskList();
         }
     }
 
@@ -44,7 +38,7 @@ public class Vibes {
                 String commandToExecute = Parser.parseCommand(userInput);
                 executeCommand(commandToExecute, userInput);
             } catch (CommandNotFoundException e) {
-                ui.showError(INVALID_COMMAND_MESSAGE);
+                ui.showError(Messages.INVALID_COMMAND_MESSAGE);
             } catch (InvalidArgumentException e) {
                 ui.showError(e.getMessage());
             } finally {
@@ -55,41 +49,41 @@ public class Vibes {
 
     public void executeCommand(String commandToExecute, String userInput) throws CommandNotFoundException, InvalidArgumentException {
         switch (commandToExecute) {
-        case "bye":
-            System.out.println("\t Bye. Hope to see you again soon!");
+        case CommandTypes.COMMAND_BYE:
+            ui.showByeMessage();
             isExit = true;
             break;
-        case "list":
+        case CommandTypes.COMMAND_LIST:
             ui.listTasks(taskList.tasks);
             break;
-        case "mark":
+        case CommandTypes.COMMAND_MARK:
             taskList.setAsDone(Parser.parseTaskNumber(userInput), ui);
             break;
-        case "unmark":
+        case CommandTypes.COMMAND_UNMARK:
             taskList.setAsNotDone(Parser.parseTaskNumber(userInput), ui);
             break;
-        case "todo":
+        case CommandTypes.COMMAND_TODO:
             taskList.addTodo(Parser.parseTodo(userInput));
             ui.showTaskAddedMessage(taskList.tasks);
             break;
-        case "event":
+        case CommandTypes.COMMAND_EVENT:
             taskList.addEvent(Parser.parseEvent(userInput));
             ui.showTaskAddedMessage(taskList.tasks);
             break;
-        case "deadline":
+        case CommandTypes.COMMAND_DEADLINE:
             taskList.addDeadline(Parser.parseDeadline(userInput));
             ui.showTaskAddedMessage(taskList.tasks);
             break;
-        case "delete":
+        case CommandTypes.COMMAND_DELETE:
             taskList.deleteTask(Parser.parseTaskNumber(userInput), ui);
             break;
         default:
             throw new CommandNotFoundException();
         }
-        Storage.saveTask(taskList);
+        storage.saveTask(taskList);
     }
 
     public static void main(String[] args) {
-        new Vibes ("").run();
+        new Vibes().run();
     }
 }
