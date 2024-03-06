f38ee1030ad0430b073f1a355b1743aee0c60916
seandooa <seandooa@yahoo.com.sg>
2024-03-06 11:03:00 +0800

    Refactor UI messages into UI class

--- src/main/java/bobby/Bobby.java
+++ src/main/java/bobby/Bobby.java
@@ -70,20 +70,21 @@ public class Bobby {
     }
 
     public static void main(String[] args) {
+        Ui ui = new Ui();
         boolean isExit = false;
         ArrayList<Task> tasks = new ArrayList<>(); // Using ArrayList instead of array
         Scanner in = new Scanner(System.in);
-        System.out.println("Hello I'm Bobby\n" + "What can I do for you?");
+        ui.showWelcomeMessage();
         File f = new File(FILE_PATH);
         try {
             f.createNewFile();
         } catch (IOException e) {
-            System.out.println("Something went wrong :(");
+            ui.showTextFileError();
         }
         try {
            loadFile(tasks);
         } catch (FileNotFoundException e) {
-            System.out.println("Error loading saved tasks, file not found");
+            ui.showLoadingError();
         }
 
         while (!isExit) {
@@ -100,19 +101,19 @@ public class Bobby {
             int entry;
             switch (command) {
             case "bye":
-                System.out.println("See you again soon!");
+                ui.showByeMessage();
                 isExit = true;
                 break;
             case "mark":
                 entry = Integer.parseInt(input.substring(5));
                 if (entry > 0 && entry <= tasks.size()) {
                     tasks.get(entry - 1).setDone(true);
-                    System.out.println("Marked as done");
+                    ui.showMarkMessage();
                     System.out.println(entry + "." + tasks.get(entry - 1));
                     try {
                         writeToFile(tasks);
                     } catch (IOException e) {
-                        System.out.println("Error saving changes.");
+                        ui.showSavingError();
                     }
                 }
                 break;
@@ -120,96 +121,79 @@ public class Bobby {
                 entry = Integer.parseInt(input.substring(7));
                 if (entry > 0 && entry <= tasks.size()) {
                     tasks.get(entry - 1).setDone(false);
-                    System.out.println("Unmarked");
+                    ui.showUnmarkMessage();
                     System.out.println(entry + "." + tasks.get(entry - 1));
                     try {
                         writeToFile(tasks);
                     } catch (IOException e) {
-                        System.out.println("Error saving changes.");
+                        ui.showSavingError();
                     }
                 }
                 break;
             case "list":
-                for (int i = 0; i < tasks.size(); i += 1) {
-                    System.out.println((i + 1) + "." + tasks.get(i));
-                }
+                ui.showList(tasks);
                 break;
             case "todo":
                 try {
                     description = obtainTodoDescription(input);
                 } catch (BobbyException e) {
-                    System.out.println("Please enter a valid task.");
+                    ui.showInvalidTodoMessage();
                     break;
                 }
                 tasks.add(new Todo(description, false));
-                System.out.println("Okay, added:\n" + tasks.get(tasks.size() - 1));
-                System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+                ui.showValidTodoMessage(tasks);
                 try {
                     writeToFile(tasks);
                 } catch (IOException e) {
-                    System.out.println("Error saving changes.");
+                    ui.showSavingError();
                 }
                 break;
             case "deadline":
-                if (!input.contains("/by")) {
-                    System.out.println("Please enter a valid deadline.");
-                    break;
-                }
                 try {
                     description = input.substring(9, input.indexOf("/by") - 1);
                     by = input.substring(input.indexOf("/by") + 4);
                 } catch (StringIndexOutOfBoundsException e) {
-                    System.out.println("Please enter a valid deadline task in this format:");
-                    System.out.println("deadline Return Book /by Sunday");
+                    ui.showInvalidDeadlineMessage();
                     break;
                 }
                 tasks.add(new Deadline(description, false, by));
-                System.out.println("Okay, added:\n" + tasks.get(tasks.size() - 1));
-                System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+                ui.showValidDeadlineMessage(tasks);
                 try {
                     writeToFile(tasks);
                 } catch (IOException e) {
-                    System.out.println("Error saving changes.");
+                    ui.showSavingError();
                 }
                 break;
             case "event":
-                if (!input.contains("/to") || !input.contains("/from")) {
-                    System.out.println("Please enter a valid start and end date.");
-                    break;
-                }
                 try {
                     description = input.substring(6, input.indexOf("/from") - 1);
                     by = input.substring(input.indexOf("/to") + 4);
                     from = input.substring(input.indexOf("/from") + 6, input.indexOf("/to") - 1);
                 } catch (StringIndexOutOfBoundsException e) {
-                    System.out.println("Please enter a valid event in this format:");
-                    System.out.println("event Project Meeting /from Mon 2pm /to 4pm");
+                    ui.showInvalidEventMessage();
                     break;
                 }
                 tasks.add(new Event(description, false, by, from));
-                System.out.println("Okay, added:\n" + tasks.get(tasks.size() - 1));
-                System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+                ui.showValidEventMessage(tasks);
                 try {
                     writeToFile(tasks);
                 } catch (IOException e) {
-                    System.out.println("Error saving changes.");
+                   ui.showSavingError();
                 }
                 break;
             case "delete":
                 entry = Integer.parseInt(input.substring(7));
                 if (entry > 0 && entry <= tasks.size()) {
-                    System.out.println("Noted, I've removed this task:\n" + tasks.get(entry - 1));
-                    tasks.remove(entry - 1);
-                    System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+                    ui.showDeleteMessage(tasks, entry);
                     try {
                         writeToFile(tasks);
                     } catch (IOException e) {
-                        System.out.println("Error saving changes.");
+                        ui.showSavingError();
                     }
                 }
                 break;
             default:
-                System.out.println("Sorry, I didn't quite understand that.\nPlease enter a valid command.");
+                ui.showInvalidCommandError();
                 break;
             }
         }
---
+++ src/main/java/bobby/Ui.java
@@ -0,0 +1,79 @@
+package bobby;
+
+import java.util.ArrayList;
+
+public class Ui {
+    public void showWelcomeMessage() {
+        System.out.println("Hello I'm Bobby\n" + "What can I do for you?");
+    }
+
+    public void showByeMessage() {
+        System.out.println("See you again soon!");
+    }
+
+    public void showSavingError() {
+        System.out.println("Error saving changes.");
+    }
+
+    public void showInvalidCommandError() {
+        System.out.println("Sorry, I didn't quite understand that.\nPlease enter a valid command.");
+    }
+
+    public void showMarkMessage() {
+        System.out.println("Marked as done");
+    }
+
+    public void showUnmarkMessage() {
+        System.out.println("Unmarked");
+    }
+
+    public void showList(ArrayList<Task> tasks) {
+        for (int i = 0; i < tasks.size(); i += 1) {
+            System.out.println((i + 1) + "." + tasks.get(i));
+        }
+    }
+
+    public void showInvalidTodoMessage() {
+        System.out.println("Please enter a valid todo in this format:");
+        System.out.println("todo Attend CS2113 tutorial");
+    }
+
+    public void showValidTodoMessage(ArrayList<Task> tasks) {
+        System.out.println("Okay, added:\n" + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+    }
+
+    public void showInvalidDeadlineMessage() {
+        System.out.println("Please enter a valid deadline task in this format:");
+        System.out.println("deadline Return Book /by Sunday");
+    }
+
+    public void showValidDeadlineMessage(ArrayList<Task> tasks) {
+        System.out.println("Okay, added:\n" + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+    }
+
+    public void showInvalidEventMessage() {
+        System.out.println("Please enter a valid event in this format:");
+        System.out.println("event Project Meeting /from Mon 2pm /to 4pm");
+    }
+
+    public void showValidEventMessage(ArrayList<Task> tasks) {
+        System.out.println("Okay, added:\n" + tasks.get(tasks.size() - 1));
+        System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+    }
+
+    public void showDeleteMessage(ArrayList<Task> tasks, int entry) {
+        System.out.println("Noted, I've removed this task:\n" + tasks.get(entry - 1));
+        tasks.remove(entry - 1);
+        System.out.println("Now you have " + tasks.size() + " task(s) in the list.");
+    }
+
+    public void showLoadingError() {
+        System.out.println("Error loading saved tasks, file not found");
+    }
+
+    public void showTextFileError() {
+        System.out.println("Something went wrong :(");
+    }
+}
