af9c3c86b49c35c85afe3c05864a83e8c9eb4ba8
Joellimjr <joellimjr12@gmail.com>
2024-03-06 17:17:04 +0800

    Add ability to detect date and time for deadline command

--- src/main/java/ChelleCommands/Deadline.java
+++ src/main/java/ChelleCommands/Deadline.java
@@ -1,5 +1,12 @@
 package ChelleCommands;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.DateTimeParseException;
+import java.time.temporal.ChronoField;
+import java.util.Locale;
+
 public class Deadline extends Task {
     protected String by;
 
@@ -23,12 +30,39 @@ public class Deadline extends Task {
 
     private String parseBy(String description) {
         String[] byPart = description.split("/by", 2);
-        return byPart[1].trim();
-    }
+        LocalDateTime dateTime = parseDateTime(byPart[1].trim());
 
+        if (dateTime != null) {
+            return formatDateTime(dateTime);
+        } else {
+            return byPart[1].trim();
+        }
+    }
 
     @Override
     public String toString() {
         return "[D]" + super.toString() + " (by: " + by + ")";
+    }
+
+    private String formatDateTime(LocalDateTime dateTime) {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy[ HH:mm'hrs']");
+        return dateTime.getHour() == 0 && dateTime.getMinute() == 0
+                ? dateTime.format(DateTimeFormatter.ofPattern("dd MMM yyyy"))
+                : dateTime.format(formatter);
+    }
+
+    private LocalDateTime parseDateTime(String originalInput) {
+        try {
+            DateTimeFormatter formatter = new DateTimeFormatterBuilder()
+                    .parseCaseInsensitive()
+                    .appendPattern("[d][d]/[M][M]/yyyy[ [HHmm]]")
+                    .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
+                    .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
+                    .toFormatter(Locale.ENGLISH);
+
+            return LocalDateTime.parse(originalInput, formatter);
+        } catch (DateTimeParseException e) {
+            return null;
+        }
     }
 }
