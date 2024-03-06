accdf6ff6c46baa16e7ecf1b46523c03f608e218
yuhengr <e0957964@u.nus.edu>
2024-03-06 20:18:50 +0800

    Add imports for class Storage

--- src/main/java/storage/Storage.java
+++ src/main/java/storage/Storage.java
@@ -1,4 +1,26 @@
 package storage;
 
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import exception.EkudException;
+import task.Deadline;
+import task.Event;
+import task.Task;
+import task.Todo;
+
 public class Storage {
+
+    private static final String filePath = "tasks.txt";
+
+    public static void writeToFile(String filePath, ArrayList<Task> tasks) {
+
+    };
+
+    public static void readFromFile(String filePath, ArrayList<Task> tasks) {
+
+    };
 }
