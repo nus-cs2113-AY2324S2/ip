28fe3db5e82ff1f8a728dc2921def3ed8ef130b4
BenjoBurger <benjaminggr@gmail.com>
2024-03-06 11:25:32 +0800

    Fix delete function bug

--- src/main/java/burger/Storage/BurgerFileClass.java
+++ src/main/java/burger/Storage/BurgerFileClass.java
@@ -2,6 +2,7 @@ package burger.Storage;
 
 import burger.TaskList.Task;
 import burger.TaskList.TaskList;
+import burger.UI.BurgerException;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -9,6 +10,8 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Scanner;
+
+import static burger.UI.Utilities.printErrorMessage;
 
 public class BurgerFileClass {
     static final int TDE_INDEX = 0;
@@ -39,10 +42,12 @@ public class BurgerFileClass {
         try {
             readFromFile(newList);
             System.out.println("File is found!");
-            System.out.println("Current List: ");
+            System.out.println("Current List:");
             newList.printTaskList();
         } catch (IOException e) {
             System.out.println("File is not found! But we will create one for you!");
+        } catch (BurgerException e) {
+            printErrorMessage(e);
         }
     }
 
@@ -52,17 +57,21 @@ public class BurgerFileClass {
      * @param list the list to store the tasks from the save file.
      * @throws IOException if the file cannot be found from the file path.
      */
-    public static void readFromFile(TaskList list) throws IOException {
+    public static void readFromFile(TaskList list) throws IOException, BurgerException {
         Files.createDirectories(Paths.get("data"));
         File f = new File(pathName); // create a File for the given file path
         Scanner s = new Scanner(f); // create a Scanner using the File as the source
         while (s.hasNext()) {
-            String[] currLineArray = s.nextLine().split("\\|");
-            char currTDE = currLineArray[TDE_INDEX].charAt(0);
-            char currMark = currLineArray[MARK_INDEX].charAt(0);
-            String currTask = currLineArray[TASK_INDEX];
-            addFromSaveFile(currTDE, currMark, currTask, list);
-            list.totalTasks++;
+            try {
+                String[] currLineArray = s.nextLine().split("\\|");
+                char currTDE = currLineArray[TDE_INDEX].charAt(0);
+                char currMark = currLineArray[MARK_INDEX].charAt(0);
+                String currTask = currLineArray[TASK_INDEX];
+                addFromSaveFile(currTDE, currMark, currTask, list);
+                list.totalTasks++;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                throw new BurgerException("Invalid Task Format!");
+            }
         }
     }
 
@@ -74,7 +83,10 @@ public class BurgerFileClass {
      * @param task the name of the task.
      * @param list the list to store the tasks.
      */
-    public static void addFromSaveFile(char tde, char mark, String task, TaskList list) {
+    public static void addFromSaveFile(char tde, char mark, String task, TaskList list) throws BurgerException {
+        if (tde != 'T' && tde != 'D' && tde != 'E') {
+            throw new BurgerException("Invalid Task Format!");
+        }
         Task currTask = new Task(task, tde);
         if (mark == 'X') {
             currTask.markDone();
