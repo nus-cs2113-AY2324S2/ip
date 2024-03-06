490003d6ebad82198b91f1141b3d445dea65e44a
Mahesh1772 <87927591+Mahesh1772@users.noreply.github.com>
2024-03-06 17:39:43 +0800

    Added Javadoc comments

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -1,23 +1,57 @@
+/**
+ * The Parser class provides methods for parsing user input.
+ */
 public class Parser {
+    /**
+     * Splits an event task input into event description and timeline.
+     *
+     * @param userInput The user input specifying the event task.
+     * @return An array containing the event description and timeline.
+     */
     public static String[] splitEvent(String userInput) {
         String[] eventParts = userInput.split("/from");
         return new String[]{eventParts[0].trim(), eventParts[1].trim()};
     }
 
+    /**
+     * Splits a todo task input into description.
+     *
+     * @param userInput The user input specifying the todo task.
+     * @return An array containing the todo description.
+     */
     public static String[] splitTodo(String userInput) {
         return userInput.split("\\s+", 2);
     }
 
+    /**
+     * Splits a deadline task input into description and deadline.
+     *
+     * @param userInput The user input specifying the deadline task.
+     * @return An array containing the deadline description and deadline.
+     */
     public static String[] splitDeadline(String userInput) {
         String[] deadlineParts = userInput.split("/by");
         return new String[]{deadlineParts[0].trim(), deadlineParts[1].trim()};
     }
 
+    /**
+     * Splits a task number input.
+     *
+     * @param userInput The user input specifying the task number.
+     * @return An array containing the task number.
+     */
     public static String[] splitTaskNumber(String userInput) {
         return userInput.split(" ");
     }
 
+    /**
+     * Splits a timeline input into start and end dates.
+     *
+     * @param date The user input specifying the timeline.
+     * @return An array containing the start and end dates of the timeline.
+     */
     public static String[] splitTimeline(String date) {
         return date.split("/to");
     }
+
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -1,22 +1,35 @@
+/**
+ * The Todo class represents a simple task without a specific deadline.
+ * It is a subclass of the Task class.
+ */
 public class Todo extends Task {
 
-    protected String by;
-
+    /**
+     * Constructs a Todo object with the given description.
+     *
+     * @param description The description of the todo task.
+     */
     public Todo(String description) {
-
         super(description);
     }
 
+    /**
+     * Returns a string representation of the Todo object.
+     *
+     * @return A string representation of the Todo object.
+     */
     @Override
     public String toString() {
-
         return "[T]"  +"[" + this.getStatusIcon()+ "] " + super.toString();
     }
 
+    /**
+     * Returns a string representation of the Todo object for saving to a file.
+     *
+     * @return A string representation of the Todo object for saving to a file.
+     */
     @Override
     public String toFileString() {
-
         return "T|" + super.toFileString(); // Prefix with "T" to indicate Todo
     }
 }
-
