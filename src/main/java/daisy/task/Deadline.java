27df37dbcd79c59a01a76b28b70d5a6707888469
FeathersRe <e0969003@u.nus.edu>
2024-03-07 02:30:52 +0800

    Level-8 Commits

--- src/main/java/daisy/task/Deadline.java
+++ src/main/java/daisy/task/Deadline.java
@@ -1,20 +1,23 @@
 package daisy.task;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
 
-    protected String dueDate;
+    protected LocalDateTime dueDate;
 
     public Deadline(String taskName, String dueDate) {
         super(taskName);
-        this.dueDate = dueDate;
+        this.dueDate = LocalDateTime.parse(dueDate, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
     }
 
     public String toString() {
-        return String.format("[D][%s] %s (by: %s)", (this.isDone)? "X":" ", this.taskName, this.dueDate);
+        return String.format("[D][%s] %s (by: %s)", (this.isDone)? "X":" ", this.taskName, this.dueDate.format(DateTimeFormatter.ofPattern("MMM dd yyyy h:mma")));
     }
 
     public String save() {
-        return String.format("D,%s,%s,%s", (this.isDone)? "1":"0", this.taskName, this.dueDate);
+        return String.format("D,%s,%s,%s", (this.isDone)? "1":"0", this.taskName, this.dueDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm")));
     }
 
 }
