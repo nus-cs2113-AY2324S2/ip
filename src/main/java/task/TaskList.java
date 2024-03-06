616a5d436a816e8250130440cee76b20479fdb2d
yuhengr <e0957964@u.nus.edu>
2024-03-06 20:36:27 +0800

    Add addTodo method in class TaskList

--- src/main/java/task/TaskList.java
+++ src/main/java/task/TaskList.java
@@ -1,6 +1,8 @@
 package task;
 
 import java.util.ArrayList;
+import java.io.IOException;
+import exception.EkudException;
 
 public class TaskList {
 
@@ -13,5 +15,32 @@ public class TaskList {
 
     public int taskCount() {
         return this.taskCount;
+    }
+
+    public void addTodo(String userInput) {
+        int dividerPosition = userInput.indexOf(" ");
+        try {
+            if (dividerPosition == -1) {
+                throw new EkudException();
+            }
+            else {
+                int descriptionStart = dividerPosition + 1;
+                tasks.add(new Todo(userInput.substring(descriptionStart)));
+                System.out.println("Got it. I've added this task:");
+                System.out.println(tasks.get(taskCount));
+                taskCount++;
+                System.out.println("Now you have " + taskCount + " tasks in the list.");
+
+                // writeToFile(filePath, tasks);
+            }
+        }
+        catch (EkudException error) {
+            System.out.println("The description of a todo cannot be empty.");
+        }
+        /*
+        catch (IOException error) {
+            System.out.println("Something went wrong: " + error.getMessage());
+        }
+        */
     }
 }
