492a18e4c4a78a603d86763f8e7a5f4fd9cac5b8
haowern98 <111697767+haowern98@users.noreply.github.com>
2024-03-07 00:25:48 +0800

    Level 1. Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,29 @@
-public class Duke { 
+import java.util.Scanner;
+
+public class Duke {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("____________________________________________________________");
-        System.out.println("Hello! I'm [Spark]");
+        System.out.println("Hello! I'm [Sparky]");
         System.out.println("What can I do for you?");
         System.out.println("____________________________________________________________");
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+            System.out.println("____________________________________________________________");
+            System.out.println(input);
+            System.out.println("____________________________________________________________");
+            if (input.equalsIgnoreCase("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println("____________________________________________________________");
+                break;
+            }
+        }
+        scanner.close();
     }
 }
