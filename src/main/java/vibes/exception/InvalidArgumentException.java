210dfde6bd787f0f675331ddcfaa336513a77f77
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-06 20:22:40 +0800

    Add JavaDoc comments to classes in exception package

--- src/main/java/vibes/exception/InvalidArgumentException.java
+++ src/main/java/vibes/exception/InvalidArgumentException.java
@@ -1,7 +1,12 @@
 package vibes.exception;
 
+/**
+ * Signals that an invalid argument was provided.
+ */
 public class InvalidArgumentException extends Exception{
-
+    /**
+     * @param message should contain relevant information on the failed argument(s)
+     */
     public InvalidArgumentException(String message) {
         super(message);
     }
