9cd6eb62c584db0c4380a2541e30481db2095235
JackieNeoCEG <e0959140@u.nus.edu>
2024-03-06 23:51:27 +0800

    Java-Doc final

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -17,22 +17,6 @@ public class TaskList {
     }
 
     /**
-     * Returns the number of tasks in the TaskList.
-     * @return Number of tasks.
-     */
-    public int size() {
-        return tasks.size();
-    }
-
-    /**
-     * Checks if the TaskList is empty.
-     * @return true if the TaskList is empty, false otherwise.
-     */
-    public boolean isEmpty() {
-        return tasks.isEmpty();
-    }
-
-    /**
      * Adds a task to the TaskList.
      * @param task Task to be added.
      */
@@ -41,24 +25,11 @@ public class TaskList {
     }
 
     /**
-     * Removes a task from the TaskList based on its index.
-     * @param index Index of the task to be removed.
+     * Retrieves all tasks in ArrayList.
+     * @return task Tasks in ArrayList
      */
-    public void remove(int index) {
-        tasks.remove(index);
-    }
-
-
     public ArrayList<Task> getAllTasks() {
         return tasks;
-    }
-    /**
-     * Retrieves a task from the TaskList based on its index.
-     * @param index Index of the task to be retrieved.
-     * @return Task based on its index.
-     */
-    public Task get(int index) {
-        return tasks.get(index);
     }
 
     /**
