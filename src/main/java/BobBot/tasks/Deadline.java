29dee7b2bc47d4e40419d054886c881925d65512
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-03-06 23:57:12 +0800

    Create DateParser class

--- src/main/java/BobBot/tasks/Deadline.java
+++ src/main/java/BobBot/tasks/Deadline.java
@@ -1,10 +1,9 @@
 package BobBot.tasks;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 import BobBot.exceptions.InvalidDeadlineException;
+import BobBot.parser.DateParser;
 
 public class Deadline extends Task {
 
@@ -25,9 +24,9 @@ public class Deadline extends Task {
         this.by = this.description.substring(
                 this.description.indexOf("/by") + "/by".length()).trim();
 
-        if (containsValidDateString(this.by)) {
-            String dateString = detectDateFromByString(this.by);
-            this.parsedDateString = parseDateFromDateString(dateString).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+        if (DateParser.containsValidDateString(this.by)) {
+            String dateString = DateParser.detectDateFromByString(this.by);
+            this.parsedDateString = DateParser.parseDateFromDateString(dateString).format(DateTimeFormatter.ofPattern("MMM d yyyy"));
             this.by = this.by.replace(dateString, this.parsedDateString);
         }
 
@@ -41,27 +40,4 @@ public class Deadline extends Task {
         return "[D][" + this.getStatusIcon() + "] " + this.task + " (by: " + by + ")";
     }
 
-    public LocalDate parseDateFromDateString(String dateString) {
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-        LocalDate date = LocalDate.parse(dateString, formatter);
-        return date;
-    }
-
-    public String detectDateFromByString(String byString) {
-        final int DATE_LENGTH = 10;
-        int startingIndex = byString.indexOf("-") - "yyyy".length();
-        int endingIndex = startingIndex + DATE_LENGTH;
-        return byString.substring(startingIndex, endingIndex);
-    }
-
-    public boolean containsValidDateString(String byString) {
-        String dateString = "";
-
-        if (byString.contains("-") && byString.length() >= 10) {
-            dateString = detectDateFromByString(byString);
-        }
-
-        String regexString = ".*\\d{4}-\\d{2}-\\d{2}.*";
-        return dateString.matches(regexString);
-    }
 }
