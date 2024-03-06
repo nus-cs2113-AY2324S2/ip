2c1a41d004ba394a6cab9ac8c08f0660a2cbf70f
Isaac Eng <>
2024-03-06 16:08:09 +0800

    Remove redundant code

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -3,7 +3,6 @@ import exceptions.InvalidEventSyntaxException;
 import exceptions.InvalidFindTaskException;
 import exceptions.InvalidTodoSyntaxException;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class TaskList {
