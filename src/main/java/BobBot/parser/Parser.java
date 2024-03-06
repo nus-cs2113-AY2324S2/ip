abd8624e0be45d31f0bb32d8668aa29d90d88680
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-03-06 20:55:18 +0800

    Move parseTaskCommands into Parser class

--- src/main/java/BobBot/parser/Parser.java
+++ src/main/java/BobBot/parser/Parser.java
@@ -2,7 +2,14 @@ package BobBot.parser;
 
 import java.util.Scanner;
 
+import BobBot.exceptions.InvalidDeadlineException;
+import BobBot.exceptions.InvalidEventException;
+import BobBot.exceptions.InvalidTodoException;
 import BobBot.storage.Storage;
+import BobBot.tasks.Deadline;
+import BobBot.tasks.Event;
+import BobBot.tasks.Task;
+import BobBot.tasks.Todo;
 import BobBot.ui.Ui;
 import taskList.TaskList;
 
@@ -37,5 +44,25 @@ public class Parser {
             Storage.saveFile();
             line = in.nextLine();
         }
+    }
+
+    public static Task parseTaskCommands(String line, Task taskToParse) {
+        try {
+            if (line.startsWith("todo")) {
+                taskToParse = new Todo(line);
+            } else if (line.startsWith("deadline")) {
+                taskToParse = new Deadline(line);
+            } else if (line.startsWith("event")) {
+                taskToParse = new Event(line);
+            } else {
+                Ui.handleInvalidCommand();
+                return null;
+            }
+        } catch (InvalidTodoException | InvalidDeadlineException | InvalidEventException e) {
+            Ui.printCustomExceptionMessage(e);
+            return null;
+        }
+
+        return taskToParse;
     }
 }
