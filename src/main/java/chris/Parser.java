77fa869df786219532db3e9d4994f867ffd0263d
chowy <ych2k01@gmail.com>
2024-03-07 12:34:34 +0800

    Added JavaDoc for Chris, Parser and Storage classes

--- src/main/java/chris/Parser.java
+++ src/main/java/chris/Parser.java
@@ -11,6 +11,12 @@ public class Parser {
         return getCommand(commandString, description);
     }
 
+    /**
+     * Returns a Command object that can be executed.
+     * @param commandString command to be executed as a string
+     * @param description array of strings that contains information for use when executing the command
+     * @return Command object
+     */
     public Command getCommand(String commandString, String[] description) {
         switch (commandString) {
             case "quit":
@@ -38,6 +44,14 @@ public class Parser {
         return input.split(" ")[0];
     }
 
+    /**
+     * Given the whole user input, this function removes the command from the front of the input,
+     * then checks and splits the description accordingly.
+     * @param input User input string
+     * @param commandString command to be executed as a string
+     * @return array of strings that will be used when executing command
+     * @throws customExceptions if the input does not match command requirements
+     */
     public String[] getDescription(String input, String commandString) throws customExceptions{
         String[] description;
         input = input.trim();
@@ -88,6 +102,13 @@ public class Parser {
         }
     }
 
+    /**
+     * This function checks that the description is in the correct format and that there is a correct number of
+     * arguments to be passed into execute()
+     * @param description array of strings to be arguments
+     * @param commandString command to be executed as a string
+     * @throws customExceptions if the input does not match command requirements
+     */
     public void checkDescription(String[] description, String commandString) throws customExceptions {
         switch(commandString) {
             case "delete":
