8f71f4196639b5d7bf822aac473e2edb40961454
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-03-07 21:18:45 +0800

    Add JavaDoc for Event class

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,7 +1,18 @@
+/**
+ * Event class represents an event task that has a description, start time and end time.
+ * It extends the Task class.
+ */
 public class Event extends Task{
     protected String startOfEvent;
     protected String endOfEvent;
 
+    /**
+     * Constructs an Event object with the given description, start time and end time.
+     *
+     * @param description The description of the event.
+     * @param startOfEvent The start time of the event.
+     * @param endOfEvent The end time of the event.
+     */
     public Event(String description, String startOfEvent, String endOfEvent) {
         super(description);
         this.startOfEvent = startOfEvent;
@@ -9,31 +20,62 @@ public class Event extends Task{
         this.taskType = "[E]";
     }
 
+    /**
+     * Gets the start time of the event.
+     *
+     * @return The start time of the event.
+     */
     public String getStartOfEvent() {
         return startOfEvent;
     }
 
+    /**
+     * Sets the start time of the event.
+     *
+     * @param startOfEvent The start time of the event.
+     */
     public void setStartOfEvent(String startOfEvent) {
         this.startOfEvent = startOfEvent;
     }
 
+    /**
+     * Gets the end time of the event.
+     *
+     * @return The end time of the event.
+     */
     public String getEndOfEvent() {
         return endOfEvent;
     }
 
+    /**
+     * Sets the end time of the event.
+     *
+     * @param endOfEvent The end time of the event.
+     */
     public void setEndOfEvent(String endOfEvent) {
         this.endOfEvent = endOfEvent;
     }
 
+    /**
+     * Returns the event task in file format.
+     *
+     * @return The event task in file format.
+     */
     public String taskInFileFormat() {
         boolean isDone = false;
         if (getStatusIcon().equals("[X]")) {
             isDone = true;
         }
-        return "E | " + (isDone ? "1" : "0") + " | " + this.description+ " | " + getStartOfEvent() + "-" + getEndOfEvent() + "\n";
+        return "E | " + (isDone ? "1" : "0") + " | " + this.description+ " | " + getStartOfEvent()
+                + "-" + getEndOfEvent() + "\n";
     }
+
+    /**
+     * Prints the event task with its type, status icon, description, and time frame.
+     */
     @Override
     public void printTask() {
-        System.out.println(taskType + getStatusIcon() + " " + description + " (from: " + startOfEvent + " to: " + endOfEvent + ")");
+        System.out.println(taskType + getStatusIcon() + " " + description + " (from: " + startOfEvent
+                + " to: " + endOfEvent + ")");
     }
 }
