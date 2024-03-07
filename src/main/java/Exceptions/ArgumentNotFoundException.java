584777623aa5f31b70a7f7e916299fb053db9b93
ShyamKrishna33 <ashyamkrishna33@gmail.com>
2024-03-07 10:24:21 +0800

    Add comment header for ArgumentNotFoundException

--- src/main/java/Exceptions/ArgumentNotFoundException.java
+++ src/main/java/Exceptions/ArgumentNotFoundException.java
@@ -1,4 +1,8 @@
 package Exceptions;
 
+/**
+ * This exception deals with the case when a parameter is expected
+ * for a specific command but not received.
+ */
 public class ArgumentNotFoundException extends Exception{
 }
