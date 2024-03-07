233471f96289f7d2c7ff7e02f7514bb0de1b56ef
vibes-863 <vaibhavdpillai863@outlook.com>
2024-03-07 16:51:06 +0800

    Add JavaDoc comments to TextUi class

--- src/main/java/vibes/ui/TextUi.java
+++ src/main/java/vibes/ui/TextUi.java
@@ -6,6 +6,9 @@ import vibes.task.type.Task;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Text UI of the application.
+ */
 public class TextUi {
     private static final String CHATBOT_NAME = "Vibes";
     public static final String GREETING_MESSAGE = "\t Hello! I'm " + CHATBOT_NAME + "\n\t What can I do for you?";
@@ -16,21 +19,36 @@ public class TextUi {
     public static final String PRINT_TASK_ITEM_FORMAT = "\t %d. %s";
     public static final int TASK_NUMBER_OFFSET = 1;
 
-
+    /**
+     * Displays the goodbye message.
+     */
     public void showByeMessage() {
         System.out.println(Messages.BYE_MESSAGE);
     }
 
+    /**
+     * Displays a dashed line.
+     */
     public void showLine() {
         System.out.println(DASHED_LINE);
     }
 
+    /**
+     * Displays a message indicating a task has been added.
+     *
+     * @param tasks the list of tasks
+     */
     public void showTaskAddedMessage(ArrayList<Task> tasks) {
         System.out.println(Messages.TASK_ADDED_MESSAGE);
         System.out.println(INDENTED_TAB + tasks.get(tasks.size() - TASKS_SIZE_OFFSET));
         System.out.printf((PRINT_TASK_COUNT_FORMAT) + "%n", tasks.size());
     }
 
+    /**
+     * Lists all tasks.
+     *
+     * @param tasks the list of tasks to be listed
+     */
     public void listTasks(ArrayList<Task> tasks) {
         System.out.println(Messages.LIST_TASKS_MESSAGE);
         for (int i = 0; i < tasks.size(); i++) {
@@ -38,33 +56,64 @@ public class TextUi {
         }
     }
 
+    /**
+     * Displays a message indicating a task has been marked as done.
+     *
+     * @param tasks the list of tasks
+     * @param taskNumber the index of the task marked as done
+     */
     public void showMarkedMessage(ArrayList<Task> tasks, int taskNumber) {
         System.out.println(Messages.MARKED_DONE_MESSAGE);
         System.out.println(INDENTED_TAB + tasks.get(taskNumber));
     }
 
+    /**
+     * Displays a message indicating a task has been marked as not done.
+     *
+     * @param tasks the list of tasks
+     * @param taskNumber the index of the task marked as not done
+     */
     public void showUnmarkedMessage(ArrayList<Task> tasks, int taskNumber) {
         System.out.println(Messages.MARKED_UNDONE_MESSAGE);
         System.out.println(INDENTED_TAB + tasks.get(taskNumber));
     }
 
+    /**
+     * Displays a message indicating a task has been deleted.
+     *
+     * @param tasks the list of tasks
+     * @param taskToDelete the task that has been deleted
+     */
     public void showDeletedMessage(ArrayList<Task> tasks, Task taskToDelete) {
         System.out.println(Messages.TASK_REMOVED_MESSAGE);
         System.out.println(INDENTED_TAB + taskToDelete);
         System.out.printf((PRINT_TASK_COUNT_FORMAT) + "%n", tasks.size());
     }
 
+    /**
+     * Displays the welcome message.
+     */
     public void showWelcomeMessage() {
         System.out.println(DASHED_LINE);
         System.out.println(GREETING_MESSAGE);
         System.out.println(DASHED_LINE);
     }
 
+    /**
+     * Reads a command from the user.
+     *
+     * @return the command entered by the user
+     */
     public String readCommand() {
         Scanner in = new Scanner(System.in);
         return in.nextLine().trim();
     }
 
+    /**
+     * Displays an error message.
+     *
+     * @param message the error message to be displayed
+     */
     public void showError(String message) {
         System.out.println(message);
     }
