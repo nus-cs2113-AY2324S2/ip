19bdc8008048c85e5e1d6f199e389f63fa784ea2
cayennegoh <cayennegoh@gmail.com>
2024-03-08 01:08:22 +0800

    Level6

--- src/main/java/Avocado.java
+++ src/main/java/Avocado.java
@@ -1,12 +1,11 @@
-//import src.main.java.DukeException;
-
 import java.util.Scanner;
+import java.util.ArrayList;
 public class Avocado {
     public static void main(String[] args) {
         System.out.println("Hello from avocado");
         System.out.println("How can I help you ?");
         System.out.println("See you!");
-        Task[] list = new Task[100];
+        ArrayList <Task> list = new ArrayList<>();
         int i = 0;
         int n = 0;
         while (true) {
@@ -41,7 +40,13 @@ public class Avocado {
                     n++;
                 } else if (line.equals("list")) {
                     commandList(list, n);
-                } else {
+                }
+                else if (array[0].startsWith("delete")) {
+                    commandDelete(array, list);
+                    i--;
+                    n--;
+                }
+                else {
                     throw new IllegalArgumentException();
                 }
             } catch (IllegalArgumentException e) {
@@ -50,50 +55,54 @@ public class Avocado {
         }
     }
 
-
-
-    private static void commandMark(String[] array, Task[] list) {
-        list[Integer.parseInt(array[1]) - 1].setDone();
-        System.out.print(list[Integer.parseInt(array[1]) - 1].taskDescription());
+    private static void commandMark(String[] array, ArrayList <Task> list) {
+        list.get(Integer.parseInt(array[1]) - 1).setDone();
+        System.out.print(list.get(Integer.parseInt(array[1]) - 1).taskDescription());
     }
 
-    private static void commandUnmark(String[] array, Task[] list) {
-        list[Integer.parseInt(array[1]) - 1].setNotDone();
-        System.out.print(list[Integer.parseInt(array[1]) - 1].taskDescription());
+    private static void commandUnmark(String[] array, ArrayList <Task> list) {
+        list.get(Integer.parseInt(array[1]) - 1).setNotDone();
+        System.out.print(list.get(Integer.parseInt(array[1]) - 1).taskDescription());
     }
 
-    private static void commandTodo(Task[] list, int i, String line)
+    private static void commandTodo(ArrayList <Task> list, int i, String line)
             throws DukeException {
         try {
             ToDo newtodo = new ToDo(line.substring(5));
-            list[i] = newtodo;
-            System.out.println(list[i].taskDescription());
+            list.add(newtodo) ;
+            System.out.println(newtodo);
         }
         catch (IndexOutOfBoundsException e) {
             System.out.println("please enter description");
         }
     }
 
-
-    private static void commandLine(Task[] list, int i, String line) {
+    private static void commandLine(ArrayList <Task> list, int i, String line) {
         Line newline = new Line(line.substring(9, line.indexOf("/")), line.substring(line.indexOf("/") + 1));
-        list[i] = newline;
+        list.add(newline);
         System.out.println(newline);
     }
 
-    private static void commandEvent(Task[] list, int i, String line) {
+    private static void commandEvent(ArrayList <Task> list, int i, String line) {
         String lengthy = line.substring(0, line.indexOf("/") + 1);
-        int lengths = lengthy.length(); /////System.out.println(lengths);
+        int lengths = lengthy.length();
         Event newevent = new Event(line.substring(6, line.indexOf("/")),
                 line.substring(line.indexOf("/") + 5, line.indexOf("/",
                         lengths + 1)), line.substring(line.indexOf("/", lengths) + 4));
-        list[i] = newevent;
+        list.add(newevent);
         System.out.println(newevent);
     }
 
-    private static void commandList(Task[] list, int n) {
+    private static void commandList(ArrayList <Task> list, int n) {
         for (int i = 0; i < n; i++) {
-            System.out.println((i + 1) + "." + list[i].taskDescription());
+            System.out.println((i + 1) + "." + list.get(i).taskDescription());
         }
+    }
+
+    private static void commandDelete(String [] array, ArrayList <Task> list) {
+        int commandIndex = Integer.parseInt(array[1]) - 1;
+        System.out.println("Noted. I've removed this task: ");
+        System.out.println(" " + list.get(commandIndex).taskDescription());
+        list.remove(commandIndex);
     }
 }
