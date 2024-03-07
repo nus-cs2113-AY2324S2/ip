fe82abeaee8c851f3b623807d817ec80434daf3d
chowy <ych2k01@gmail.com>
2024-03-07 16:18:45 +0800

    Added JavaDoc for Command class

--- src/main/java/chris/commands/Command.java
+++ src/main/java/chris/commands/Command.java
@@ -9,10 +9,20 @@ public class Command {
         this.description = description;
     }
 
+    /**
+     * Executes the command when called
+     * @param tasks taskList of tasks to be executed on
+     * @return String that contains the response of the action
+     * @throws customExceptions if any of the inputs is in incorrect format
+     */
     public String execute(taskList tasks) throws customExceptions {
         return "Sorry, I do not recognise this command!";
     }
 
+    /**
+     * This function is called to tell the main loop whether to continue running or not
+     * @return isQuit of Command
+     */
     public boolean quit() {
         return this.isQuit;
     }
