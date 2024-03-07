b1aaf7497af995875603b477cc844e5d98fd5289
Incy <85068563+IncyBot@users.noreply.github.com>
2024-03-08 00:37:15 +0800

    Taught The Chatbot How to Understand DATES

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,18 +1,25 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task {
-    private String by;
+    private LocalDateTime by;
 
     public Deadline(String description, String by) {
         super(description);
-        this.by = by;
+        this.by = LocalDateTime.parse(by, DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
+    }
+
+    public LocalDateTime getBy() {
+        return by;
     }
 
     @Override
     public String toString() {
-        return "[D]" + super.getStatusIcon() + " " + description + " (by: " + by + ")";
+        return "[D]" + super.getStatusIcon() + " " + description + " (by: " + by.format(DateTimeFormatter.ofPattern("MMM dd yyyy, hh:mm a")) + ")";
     }
 
     @Override
     public String toFileString() {
-        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by;
+        return "D | " + (isDone ? "1" : "0") + " | " + description + " | " + by.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HHmm"));
     }
 }
