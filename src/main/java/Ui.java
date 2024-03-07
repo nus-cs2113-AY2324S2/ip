ae1b771c0ff477a0e2c8b8a54505e062318d330a
MonkeScripts <shaolianghe0.0@gmail.com>
2024-03-08 00:57:24 +0800

    fix string concatenate bug

--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,15 +1,14 @@
 public class Ui {
     private static final String greeting = "Hello clown I am JingXiang";
     private static final String inputMessage =
-            """
-            ------------stop clowning and type sth------------
-            Any dates must be in dd/mm/yyyy hh:mm:ss. Be precise!
-            sample task inputs
-            todo <description>
-            deadline <description> /by <dd/mm/yyyy hh:mm:ss>
-            event <description> /from <dd/mm/yyyy hh:mm:ss> /to <dd/mm/yyyy hh:mm:ss>
-            --------------------------------------------------
-            """;
+
+            "------------stop clowning and type sth------------\n" +
+            "Any dates must be in dd/mm/yyyy hh:mm:ss. Be precise!\n" +
+            "sample task inputs\n" +
+            "todo <description>\n" +
+            "deadline <description> /by <dd/mm/yyyy hh:mm:ss>\n" +
+            "event <description> /from <dd/mm/yyyy hh:mm:ss> /to <dd/mm/yyyy hh:mm:ss>\n" +
+            "--------------------------------------------------";
     private static final String leaveMessage = "Good day clown";
 
     /**
