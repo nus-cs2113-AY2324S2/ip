7df56dba020c68ef1d9f1cf96285d35910cd52c3
anneleong <anneleongsongen@gmail.com>
2024-03-07 11:58:07 +0800

    Add find function

--- src/main/java/n/Parser.java
+++ src/main/java/n/Parser.java
@@ -1,11 +1,25 @@
+/**
+ * Parser takes in user inputs/inputs from the .txt file
+ * and makes sense of it so that the corresponding action
+ * can be taken.
+ *
+ * @author  anneleong
+ * @version 1.0
+ * @since   2024-03-07
+ */
+
 package n;
 
 import n.exceptions.NoTaskTypeException;
 import n.task.Type;
-
-import static n.TaskList.taskList;
-
 public class Parser {
+    /**
+     * Determines the type of a task based on the provided task description.
+     *
+     * @param taskDescription The description of the task.
+     * @return The Type of task (Event, Deadline, ToDo).
+     * @throws NoTaskTypeException If the task type is not specified correctly.
+     */
     public static Type filterTask(String taskDescription) throws NoTaskTypeException {
         Type taskType;
         switch (taskDescription.split(" ")[0]) {
@@ -20,9 +34,17 @@ public class Parser {
                 break;
             default:
                 throw new NoTaskTypeException();
-        };
+        }
         return taskType;
     }
+    /**
+     * Gets the Type of task based on its task description
+     * as text in the .txt saved file.
+     *
+     * @param taskDescription The description of the task.
+     * @return The Type of the task (ToDo, Deadline, Event)
+     * @see Storage#loadTaskList()
+     */
     public static Type getTaskType(String taskDescription) {
         char taskTypeSymbol = taskDescription.charAt(4);
         Type taskType = Type.ToDo;
@@ -39,9 +61,19 @@ public class Parser {
         }
         return taskType;
     }
+    /**
+     * Converts a given task as a text from the .txt saved
+     * file into a formatted task description based on the
+     * specified Type of task.
+     *
+     * @param text      The input text containing task-related information.
+     * @param taskType  The type of the task (ToDo, Deadline, Event).
+     * @return The formatted task description.
+     * @see Storage#loadTaskList()
+     */
     public static String textToTaskDescription(String text, Type taskType) {
         String taskDescription = "";
-        String task = "";
+        String task;
         switch (taskType) {
             case ToDo:
                 taskDescription = text.substring(11);
