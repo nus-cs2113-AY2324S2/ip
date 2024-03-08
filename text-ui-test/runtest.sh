8d96349c18a9bdd382449e98fe504ffb7d1d85b2
StargazerX0 <lkjsgu@outlook.com>
2024-03-08 13:18:27 +0800

    update runtest.sh

--- text-ui-test/runtest.sh
+++ text-ui-test/runtest.sh
@@ -1,38 +1,44 @@
 #!/usr/bin/env bash
 
 # create bin directory if it doesn't exist
-if [ ! -d "../bin" ]
-then
+if [ ! -d "../bin" ]; then
     mkdir ../bin
 fi
 
 # delete output from previous run
-if [ -e "./ACTUAL.TXT" ]
-then
+if [ -f "ACTUAL.TXT" ]; then
     rm ACTUAL.TXT
 fi
 
 # compile the code into the bin folder, terminates if error occurred
-if ! javac -cp ../src/main/java -Xlint:none -d ../bin ../src/main/java/*.java
-then
+if ! javac -cp ../src/main/java \
+    -Xlint:none \
+    -d ../bin \
+    ../src/main/java/alpaca/*.java \
+    ../src/main/java/alpaca/commands/*.java \
+    ../src/main/java/alpaca/exceptions/*.java \
+    ../src/main/java/alpaca/parser/*.java \
+    ../src/main/java/alpaca/storage/*.java \
+    ../src/main/java/alpaca/tasks/*.java \
+    ../src/main/java/alpaca/ui/*.java; then
     echo "********** BUILD FAILURE **********"
     exit 1
 fi
 
-# run the program, feed commands from input.txt file and redirect the output to the ACTUAL.TXT
-java -classpath ../bin Duke < input.txt > ACTUAL.TXT
+# run the program, feed commands from input.txt file and redirect the output to ACTUAL.TXT
+java -classpath ../bin alpaca.Alpaca < input.txt > ACTUAL.TXT
 
-# convert to UNIX format
-cp EXPECTED.TXT EXPECTED-UNIX.TXT
-dos2unix ACTUAL.TXT EXPECTED-UNIX.TXT
+# compare the output to the expected output using 'diff'
+# '-Z' ignores trailing whitespace, '-q' provides brief output
+diff -Zq ACTUAL.TXT EXPECTED.TXT > /dev/null
+result=$?
 
-# compare the output to the expected output
-diff ACTUAL.TXT EXPECTED-UNIX.TXT
-if [ $? -eq 0 ]
-then
+if [ $result -eq 0 ]; then
     echo "Test result: PASSED"
-    exit 0
 else
     echo "Test result: FAILED"
-    exit 1
+    diff -Zy --suppress-common-lines ACTUAL.TXT EXPECTED.TXT
 fi
+
+# Exit with the result of comparison
+exit $result
