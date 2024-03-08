9ae60540485cce7e85cd5db2c4ce02289b803f77
NicholasTanYY <nicholastanyunyu@gmail.com>
2024-03-08 16:23:05 +0800

    Add docs for DateParser

--- src/main/java/BobBot/parser/DateParser.java
+++ src/main/java/BobBot/parser/DateParser.java
@@ -3,14 +3,31 @@ package BobBot.parser;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
+/*
+ * Implements a date parser that interprets date strings and performs the 
+ * necessary operations.
+ * 
+ * @author NicholasTanYY
+ * @since January 2024
+ * @version 1.0
+ */
 public class DateParser {
 
+    /*
+     * Converts date string to LocalDate format.
+     * 
+     * @param dateString The date string to parse.
+     * @return The date parsed from the date string.
+     */
     public static LocalDate parseDateFromDateString(String dateString) {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
         LocalDate date = LocalDate.parse(dateString, formatter);
         return date;
     }
 
+    /*
+     * Detects the date from a date string in the format by "yyyy-MM-dd".
+     */
     public static String detectDateFromByString(String byString) {
         final int DATE_LENGTH = 10;
         int startingIndex = byString.indexOf("-") - "yyyy".length();
@@ -18,6 +35,13 @@ public class DateParser {
         return byString.substring(startingIndex, endingIndex);
     }
 
+    /*
+     * Checks in the string contains a valid date string in the format 
+     * "yyyy-MM-dd".
+     * 
+     * @param byString The date string to check.
+     * @return True if the date string is in the specified format, false otherwise.
+     */
     public static boolean containsValidDateString(String byString) {
         String dateString = "";
 
