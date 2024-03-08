05cf060bd83de21d1f18fc08388219d95b3f094d
Cheong Jun Ning Jolene <jolenecheong211003@gmail.com>
2024-03-08 16:25:56 +0800

    Remove unused method from Deadline

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -1,6 +1,5 @@
 public class Deadline extends Task {
     protected String by;
-
     public Deadline(String description, String by) {
         super(description);
         this.by = by;
@@ -9,11 +8,6 @@ public class Deadline extends Task {
     public String getBy() {
         return by;
     }
-
-    public void setBy(String by) {
-        this.by = by;
-    }
-
     public String taskInFileFormat() {
         boolean isDone = false;
         if (getStatusIcon().equals("[X]")) {
