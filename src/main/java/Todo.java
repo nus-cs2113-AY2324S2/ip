73a83f2ff31c4a80d9475e294c8b3cbbaf85080b
haowern98 <111697767+haowern98@users.noreply.github.com>
2024-03-08 16:31:32 +0800

    Update Todo.java

--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -3,6 +3,15 @@ public class Todo extends Task {
         super(description);
     }
 
+    public Todo(String description, boolean isDone) {
+        super(description, isDone);
+    }
+
+    @Override
+    public String toFileString() {
+        return "T | " + (isDone ? 1 : 0) + " | " + description;
+    }
+
     @Override
     public String toString() {
         return "[T]" + getStatusIcon() + " " + description;
