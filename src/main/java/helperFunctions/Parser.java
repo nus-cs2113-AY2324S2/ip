68a5ceb6c496b80c4c94bb784785e0339ddfc3f0
ngxzs <seanng01@outlook.com>
2024-03-08 14:27:04 +0800

    Remove magic literals in Parser.java

--- src/main/java/helperFunctions/Parser.java
+++ src/main/java/helperFunctions/Parser.java
@@ -15,10 +15,11 @@ public class Parser {
      * @throws InvalidParamsException if invalid command
      */
     public static boolean processUserInput(TaskList tasks, String userInput, String FILE_NAME, boolean isReadMode) throws InvalidParamsException {
-        String[] userInputInParts = userInput.split(" ");
+        final int COMMAND_INDEX = 0;
         boolean isMark;
 
-        switch (userInputInParts[0].toUpperCase()) {
+        String[] userInputInParts = userInput.split(" ");
+        switch (userInputInParts[COMMAND_INDEX].toUpperCase()) {
         case "BYE":
             return false; // EXITS loop
         case "LIST":
@@ -51,37 +52,37 @@ public class Parser {
             throw new InvalidParamsException("No such command." + System.lineSeparator() + Ui.printCommandsList());
         }
         return true;
-}
+    }
 
-/**
- * Takes in deadline of "yyyy/mm/dd" and formats to "MMM d yyyy"
- *
- * @param deadline is unformatted
- * @return deadline of format "MMM d yyyy"
- */
-public static String formatDeadline(String deadline) {
-    String formattedDeadline = deadline.replaceAll("/", "-").trim();
-    LocalDate dateObject = LocalDate.parse(formattedDeadline);
-    return dateObject.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
-}
+    /**
+     * Takes in deadline of "yyyy/mm/dd" and formats to "MMM d yyyy"
+     *
+     * @param deadline is unformatted
+     * @return deadline of format "MMM d yyyy"
+     */
+    public static String formatDeadline(String deadline) {
+        String formattedDeadline = deadline.replaceAll("/", "-").trim();
+        LocalDate dateObject = LocalDate.parse(formattedDeadline);
+        return dateObject.format(DateTimeFormatter.ofPattern("MMM d yyyy"));
+    }
 
-/**
- * Checks for valid deadline format "yyyy/mm/dd"
- *
- * @param deadline of unknown format
- * @return true if valid, else false
- */
-public static boolean isValidDeadline(String deadline) {
-    int LENGTH_OF_DEADLINE_COMPONENTS = 3;
-    int LENGTH_OF_YEAR = 4;
-    int LENGTH_OF_MONTHS = 2;
-    int LENGTH_OF_DAYS = 2;
+    /**
+     * Checks for valid deadline format "yyyy/mm/dd"
+     *
+     * @param deadline of unknown format
+     * @return true if valid, else false
+     */
+    public static boolean isValidDeadline(String deadline) {
+        int LENGTH_OF_DEADLINE_COMPONENTS = 3;
+        int LENGTH_OF_YEAR = 4;
+        int LENGTH_OF_MONTHS = 2;
+        int LENGTH_OF_DAYS = 2;
 
-    String formattedDeadline = deadline.replaceAll("/", "-").trim();
-    String[] deadlineParts = formattedDeadline.split("-");
-    return ((deadlineParts.length == LENGTH_OF_DEADLINE_COMPONENTS)
-            && (deadlineParts[0].length() == LENGTH_OF_YEAR)
-            && (deadlineParts[1].length() == LENGTH_OF_MONTHS)
-            && (deadlineParts[2].length() == LENGTH_OF_DAYS));
-}
+        String formattedDeadline = deadline.replaceAll("/", "-").trim();
+        String[] deadlineParts = formattedDeadline.split("-");
+        return ((deadlineParts.length == LENGTH_OF_DEADLINE_COMPONENTS)
+                && (deadlineParts[0].length() == LENGTH_OF_YEAR)
+                && (deadlineParts[1].length() == LENGTH_OF_MONTHS)
+                && (deadlineParts[2].length() == LENGTH_OF_DAYS));
+    }
 }
