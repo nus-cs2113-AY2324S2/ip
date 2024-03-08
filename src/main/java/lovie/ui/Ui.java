9b7c77ded5f4e6bdb7662d035dd885e52b541f0f
nidhi-nayak <nidhirnayak@gmail.com>
2024-03-08 21:18:05 +0800

    Add new line constant in UI

--- src/main/java/lovie/ui/Ui.java
+++ src/main/java/lovie/ui/Ui.java
@@ -7,46 +7,49 @@ import java.util.ArrayList;
 public class Ui {
 
     public static final String DASHED_LINE = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~";
-    public static final String INTRODUCTION_MESSAGE = "Hey hey! My name is Lovie! How can I help you today?\n" +
-            "Just a heads up: you can type 'command' for a list of everything you can ask me!";
-    public static final String GOODBYE_MESSAGE = "Thanks for using me! See you next time ♡〜٩( ˃▿˂ )۶〜♡";
-    private static final String LOGO = "██╗░░░░░░█████╗░██╗░░░██╗██╗███████╗ \n" +
-            "██║░░░░░██╔══██╗██║░░░██║██║██╔════╝ \n" +
-            "██║░░░░░██║░░██║╚██╗░██╔╝██║█████╗░░ \n" +
-            "██║░░░░░██║░░██║░╚████╔╝░██║██╔══╝░░ \n" +
-            "███████╗╚█████╔╝░░╚██╔╝░░██║███████╗ \n" +
-            "╚══════╝░╚════╝░░░░╚═╝░░░╚═╝╚══════╝ \n";
-    public static final String FURTHER_HELP_MESSAGE = "\nCan I help you with anything else?";
-    public static final String NO_TASK_NUMBER_MESSAGE = "Sorry, there is no record of a task number ";
-    public static final String SPACE = " ";
-    public static final String NEED_A_KEYWORD_MESSAGE = "Oopsies. Please make sure to include a keyword while " +
-            "using the find command!";
-    public static final String ALL_COMMANDS_MESSAGE = "Here are all the possible commands you can ask me:\n\n" +
-            "(1) To add a todo: todo **description** \n(2) To add an event: event **description** /from **start**" +
-            "/to **end**\n(3) To add a deadline: deadline **description** /by **end**\n(4) To list all tasks: list\n"
-            + "(5) To mark a task as done: mark **task number**\n(6) To unmark a task as not done: unmark " +
-            "**task number**\n(7) To delete a task: delete **task number**\n" +
-            "(8) To find tasks with a keyword: find **keyword**\n" +
-            "(9) To ask for a list of all commands: command\n" +
-            "(10) To leave our session: bye";
-    public static final String ADD_TASK_MESSAGE = "How cutesie! Of course I'll add that task for you.\nAdded:";
-    public static final String EMPTY_LIST_MESSAGE = "Your list is empty right now. \nTry adding a task using one of the following " +
-            "command formats: \n\n" + "(1) todo **description** \n" + "(2) event **description** " +
-            "/from **start** /to **end**\n" + "(3) deadline **description** /by **end**";
-    public static final String MATCHING_TASKS_MESSAGE = "Here are your matching tasks!\n";
-    public static final String NO_MATCHING_TASKS_MESSAGE = "Aw, I'm sorry. There were no tasks that contained that keyword.\nTry searching for another.";
     public static final String LIST_NUMBER_BRACKET = ". [";
     public static final String OPEN_CLOSE_BRACKETS = "] [";
     public static final String CLOSE_BRACKET = "] ";
     public static final String NEW_LINE = "\n";
+    public static final String SPACE = " ";
+    public static final String INTRODUCTION_MESSAGE = "Hey hey! My name is Lovie! How can I help you today?" + NEW_LINE
+            + "Just a heads up: you can type 'command' for a list of everything you can ask me!";
+    public static final String GOODBYE_MESSAGE = "Thanks for using me! See you next time ♡〜٩( ˃▿˂ )۶〜♡";
+    private static final String LOGO = "██╗░░░░░░█████╗░██╗░░░██╗██╗███████╗" + NEW_LINE +
+            "██║░░░░░██╔══██╗██║░░░██║██║██╔════╝" + NEW_LINE +
+            "██║░░░░░██║░░██║╚██╗░██╔╝██║█████╗░░" + NEW_LINE +
+            "██║░░░░░██║░░██║░╚████╔╝░██║██╔══╝░░" + NEW_LINE +
+            "███████╗╚█████╔╝░░╚██╔╝░░██║███████╗" + NEW_LINE +
+            "╚══════╝░╚════╝░░░░╚═╝░░░╚═╝╚══════╝" + NEW_LINE ;
+    public static final String FURTHER_HELP_MESSAGE = NEW_LINE + "Can I help you with anything else?";
+    public static final String NO_TASK_NUMBER_MESSAGE = "Sorry, there is no record of a task number ";
+    public static final String NEED_A_KEYWORD_MESSAGE = "Oopsies. Please make sure to include a keyword while " +
+            "using the find command!";
+    public static final String ALL_COMMANDS_MESSAGE = "Here are all the possible commands you can ask me:" + NEW_LINE +
+            NEW_LINE + "(1) To add a todo: todo **description**" + NEW_LINE + "(2) To add an event: " +
+            "event **description** /from **start** /to **end**" + NEW_LINE + "(3) To add a deadline: deadline " +
+            "**description** /by **end**" + NEW_LINE + "(4) To list all tasks: list" + NEW_LINE +
+            "(5) To mark a task as done: " + "mark **task number**" + NEW_LINE + "(6) To unmark a task as " +
+            "not done: unmark **task number**" + NEW_LINE + "(7) To delete a task: delete **task number**" + NEW_LINE +
+            "(8) To find tasks with a keyword: " + "find **keyword**" + NEW_LINE + "(9) To ask for a list of all " +
+            "commands: command" + NEW_LINE + "(10) To leave our session: bye";
+    public static final String ADD_TASK_MESSAGE = "How cutesie! Of course I'll add that task for you." + NEW_LINE +
+            "Added:";
+    public static final String EMPTY_LIST_MESSAGE = "Your list is empty right now." + NEW_LINE + "Try adding a task " +
+            "using one of the following " + "command formats:" + NEW_LINE + NEW_LINE  + "(1) todo **description**" +
+            NEW_LINE + "(2) event **description** /from **start** /to **end**" + NEW_LINE + "(3) deadline " +
+            "**description** /by **end**";
+    public static final String MATCHING_TASKS_MESSAGE = "Here are your matching tasks!" + NEW_LINE;
+    public static final String NO_MATCHING_TASKS_MESSAGE = "Aw, I'm sorry. There were no tasks that contained that " +
+            "keyword." + NEW_LINE + "Try searching for another.";
     public static final String MARK_TASK_MESSAGE = "Woo hoo! Good job cutie. Way to go. I've marked this task as done:";
     public static final String OPEN_BRACKET = "[";
     public static final String EMPTY_BRACKETS = "[ ] ";
-    public static final String INVALID_COMMAND_MESSAGE = "Sorry, I can't understand what you are saying.\n" +
+    public static final String INVALID_COMMAND_MESSAGE = "Sorry, I can't understand what you are saying." + NEW_LINE +
             "Please try again!";
     public static final String SUPPORT_MESSAGE = "Of course girlie <3 ";
     public static final String HAVE_MESSAGE = "You now have ";
-    public static final String HAVE_MESSAGE_PART_TWO = " tasks in the list.\n";
+    public static final String HAVE_MESSAGE_PART_TWO = " tasks in the list." + NEW_LINE;
     public static final String UNMARK_MESSAGE = "I've unmarked this task for you:";
 
     /**
@@ -170,18 +173,19 @@ public class Ui {
      */
     public void listPrinter(ArrayList<Task> tasks) {
         int counter = 0;
-        String output = new String();
+        StringBuilder output = new StringBuilder();
         while (counter < tasks.size()) {
             String listNumber = Integer.toString(counter + 1);
             Task selected = tasks.get(counter);
-            output += listNumber + LIST_NUMBER_BRACKET + selected.getTaskIcon() + OPEN_CLOSE_BRACKETS +
-                    selected.getStatusIcon() + CLOSE_BRACKET + selected.getDescription() + selected.getTimespan();
+            output.append(listNumber).append(LIST_NUMBER_BRACKET).append(selected.getTaskIcon()).
+                    append(OPEN_CLOSE_BRACKETS).append(selected.getStatusIcon()).append(CLOSE_BRACKET).
+                    append(selected.getDescription()).append(selected.getTimespan());
             if (counter != tasks.size() - 1) {
-                output += NEW_LINE;
+                output.append(NEW_LINE);
             }
             counter += 1;
         }
-        print(output);
+        print(output.toString());
     }
 
     public void findPrinter(TaskList matches) {
@@ -189,14 +193,15 @@ public class Ui {
             print(NO_MATCHING_TASKS_MESSAGE);
         } else {
             int counter = 0;
-            String output = new String();
+            StringBuilder output = new StringBuilder();
             while (counter < matches.getSize()) {
                 String listNumber = Integer.toString(counter + 1);
                 Task selected = matches.get(counter);
-                output += listNumber + LIST_NUMBER_BRACKET + selected.getTaskIcon() + OPEN_CLOSE_BRACKETS +
-                        selected.getStatusIcon() + CLOSE_BRACKET + selected.getDescription() + selected.getTimespan();
+                output.append(listNumber).append(LIST_NUMBER_BRACKET).append(selected.getTaskIcon()).
+                        append(OPEN_CLOSE_BRACKETS).append(selected.getStatusIcon()).append(CLOSE_BRACKET).
+                        append(selected.getDescription()).append(selected.getTimespan());
                 if (counter != matches.getSize() - 1) {
-                    output += NEW_LINE;
+                    output.append(NEW_LINE);
                 }
                 counter += 1;
             }
