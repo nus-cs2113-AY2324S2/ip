2c9db56bc44131271d1ebbb1ba6cc1b97964e805
haowern98 <111697767+haowern98@users.noreply.github.com>
2024-03-08 23:32:09 +0800

    Update Event.java

--- src/main/java/Event.java
+++ src/main/java/Event.java
@@ -1,3 +1,16 @@
+/**
+ * Represents an event task in Duke.
+ *
+ * An Event object has information about an event task, including its description,
+ * start date, end date, and completion status. It provides methods to access and manipulate
+ * these attributes.
+ *
+ * @param description the description of the event task
+ * @param from the start date/time of the event
+ * @param to the end date/time of the event
+ * @return an Event object with the specified description, start date, and end date
+ * @throws NullPointerException if the description, start date, or end date is null
+ */
 public class Event extends Task {
     protected String from;
     protected String to;
