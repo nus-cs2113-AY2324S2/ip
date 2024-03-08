5c4ee7753ab0a52b589ba8d87d48df9735c28dbe
clarencepohh <110753446+clarencepohh@users.noreply.github.com>
2024-03-08 13:10:47 +0800

    Bug fix for file loading

--- src/main/java/hachi/Hachi.java
+++ src/main/java/hachi/Hachi.java
@@ -38,6 +38,7 @@ public class Hachi {
             tasksList = new TaskList(storage.load());
         } catch (HachiException | FileNotFoundException e) {
             ui.printFileLoadingError();
+            tasksList = new TaskList();
         }
     }
 
