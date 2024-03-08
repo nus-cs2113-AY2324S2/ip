8aa350c36425d545033ca7cb8efce2aff2404489
Praneet-25 <e0959984@u.nus.edu>
2024-03-08 18:44:30 +0800

    Add find functionality

--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -2,17 +2,14 @@ public class Task {
     protected String description;
     protected boolean isDone;
     protected String typeOfTask;
-
     public Task(String description) {
         this.description = description;
         this.isDone = false;
         typeOfTask = "";
     }
-
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
     }
-
     public void markAsCompleted() {
         isDone = true;
     }
@@ -24,7 +21,6 @@ public class Task {
     public String toFileString() {
         return description + "|" + (isDone ? "1" : "0"); // Example format: "description|status"
     }
-
     public static Task fromString(String fileString) {
         String[] parts = fileString.split("\\|");
         String taskType = parts[0]; // Extract task type
@@ -50,8 +46,6 @@ public class Task {
         if (task != null && isDone) {
             task.markAsCompleted(); // Mark task as done if status is '1'
         }
-
         return task;
     }
-
 }
